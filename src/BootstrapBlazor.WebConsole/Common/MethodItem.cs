74cf4c6460a8ff6e5ce99e7e3d5b6cfe2447f0f5
Argo-PD <argo@163.com>
2020-03-25 14:31:37 +0800

    feat: 增加方法表格组件

---
+++ src/BootstrapBlazor.WebConsole/Common/MethodItem.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace BootstrapBlazor.WebConsole.Common
+{
+    /// <summary>
+    /// 方法说明类
+    /// </summary>
+    public class MethodItem
+    {
+        /// <summary>
+        /// 获得/设置 参数
+        /// </summary>
+        [DisplayName("参数")]
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// 获得/设置 说明
+        /// </summary>
+        [DisplayName("说明")]
+        public string Description { get; set; } = "";
+
+        /// <summary>
+        /// 参数
+        /// </summary>
+        [DisplayName("参数")]
+        public string Parameters { get; set; } = "";
+
+        /// <summary>
+        /// 返回值
+        /// </summary>
+        [DisplayName("返回值")]
+        public string ReturnValue { get; set; } = "";
+    }
+}
