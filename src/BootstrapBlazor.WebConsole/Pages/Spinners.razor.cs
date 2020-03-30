c1a19ecba8c1427c61c79ebd63d90c062410fdd4
Nine <948825551@qq.com>
2020-03-30 13:13:39 +0800

    !21 新增功能：新增旋转图标 * feat: 添加用例到展示页面 * refactor: 删除不必要的命名空间，多余空行 * refactor: 重构代码格式 * feat: 新增旋转图标导航 * feat:新增旋转图标演示用例 * feat: 新增旋转图标演示页面 * feat: 新增旋转图标类型枚举 * feat: 新增旋转图标组件基类 * feat: 新增旋转图标基类

---
+++ src/BootstrapBlazor.WebConsole/Pages/Spinners.razor.cs
@@ -0,0 +1,41 @@
+﻿using BootstrapBlazor.WebConsole.Common;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.WebConsole.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Spinners
+    {
+        /// <summary>
+        /// 获得属性方法
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+        {
+            // TODO: 移动到数据库中
+            new AttributeItem() {
+                Name = "Color",
+                Description = "颜色",
+                Type = "Color",
+                ValueList = "Primary / Secondary / Success / Danger / Warning / Info / Dark",
+                DefaultValue = "Primary"
+            },
+            new AttributeItem() {
+                Name = "Class",
+                Description = "样式",
+                Type = "string",
+                ValueList = " — ",
+                DefaultValue = " — "
+            },
+            new AttributeItem() {
+                Name = "SpinnerType",
+                Description = "图标类型",
+                Type = "SpinnerType",
+                ValueList = " Border / Grow ",
+                DefaultValue = "SpinnerType.Border"
+            }
+        };
+    }
+}
---
+++ src/BootstrapBlazor/Components/Spinner/SpinnerType.cs
@@ -0,0 +1,18 @@
+﻿namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum SpinnerType
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Border = 1,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        Grow = 2,
+    }
+}
