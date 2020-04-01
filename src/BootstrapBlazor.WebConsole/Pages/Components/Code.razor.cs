ff766b0dc55af4e1a96ec630249c0e8e68cdd09c
Argo-PD <argo@163.com>
2020-04-01 12:13:53 +0800

    refactor: 重新设计 Block 组件
    #Comment
    方便录入代码演示说明部分

---
+++ src/BootstrapBlazor.WebConsole/Pages/Components/Code.razor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace BootstrapBlazor.WebConsole.Pages.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Code
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter] public string? CodeFile { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject] IWebHostEnvironment? Environment { get; set; }
+
+        private string? Content()
+        {
+            string? content = "未设置";
+            if (!string.IsNullOrEmpty(CodeFile) && Environment != null)
+            {
+                // 拼接实例文件路径
+                var filePath = Environment.WebRootPath;
+                var codeFile = Path.Combine(filePath, $"code{Path.DirectorySeparatorChar}{CodeFile}");
+                if (File.Exists(codeFile))
+                {
+                    content = File.ReadAllText(codeFile);
+                }
+            }
+            return content;
+        }
+    }
+}
