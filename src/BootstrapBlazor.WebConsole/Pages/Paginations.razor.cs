6fec3e22b45e281909d4ee15a78b5fe87f4c53c6
浪迹天涯 <806605284@qq.com>
2020-04-05 00:55:07 +0800

    !24 增加功能：增加分页组件 * 分页冲突 * feat:新增分页组件 * fix：Pagination 分页进一步完善 * feat: 增加 Pagination组件 同时新增文档

---
+++ src/BootstrapBlazor.WebConsole/Pages/Paginations.razor.cs
@@ -0,0 +1,39 @@
+﻿using BootstrapBlazor.WebConsole.Common;
+using BootstrapBlazor.WebConsole.Pages.Components;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.WebConsole.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Paginations
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        protected Logger? Trace { get; set; }
+
+        /// <summary>
+        /// 获得属性方法
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+        {  
+            new AttributeItem() {
+                Name = "Total",
+                Description = "分页总页数",
+                Type = "int",
+                ValueList = " — ",
+                DefaultValue = "1"
+            },
+            new AttributeItem() {
+                Name = "CurrentPage",
+                Description = "当前页",
+                Type = "int",
+                ValueList = " — ",
+                DefaultValue = "1"
+            }
+        };
+    }
+}
---
+++ src/BootstrapBlazor/Components/Pagination/PaginationBase.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Pagination 分页组件
+    /// </summary>
+    public abstract class PaginationBase : BootstrapComponentBase
+    {
+        /// <summary>
+        /// 获得/设置 当前页数
+        /// </summary>
+        [Parameter] public int CurrentPage { get; set; } = 1;
+
+        /// <summary>
+        /// 获得/设置 每页大小
+        /// </summary>
+        [Parameter] public int? PageSize { get; set; }
+
+        /// <summary>
+        /// 获得/设置 分页总页数
+        /// </summary>
+        [Parameter] public int Total { get; set; } = 1;
+
+        /// <summary>
+        /// 点击控件时触发上一页
+        /// </summary>
+        protected void PrevClick()
+        {
+            CurrentPage = CurrentPage == 1 ? Total : (CurrentPage - 1);
+        }
+
+        /// <summary>
+        /// 点击控件时触发下一页
+        /// </summary>
+        protected void NextClick()
+        {
+            CurrentPage = CurrentPage == Total ? 1 : (CurrentPage + 1);
+        }
+
+        /// <summary>
+        /// 设置点击页数
+        /// </summary>
+        protected void OnClick(string SetPage)
+        {
+            if (SetPage == "<<")
+            {
+                CurrentPage = CurrentPage - 4 <= 1 ? 1 : CurrentPage - 4;
+            }
+            else if (SetPage == ">>")
+            {
+                CurrentPage = CurrentPage + 4 >= Total ? Total : CurrentPage + 4;
+            }
+            else
+            {
+                CurrentPage = int.Parse(SetPage);
+            }    
+        }
+
+        /// <summary>
+        /// 设置显示页数
+        /// </summary>
+        /// <param name="CurrentPage"></param>
+        /// <param name="Total"></param>
+        /// <returns></returns>
+        protected List<string> GetShowPagination(int CurrentPage, int Total)
+        {
+            List<string> list = new List<string>();
+            if (Total <= 7)
+            {
+                for (int i = 1; i <= Total; i++)
+                {
+                    list.Add(i.ToString());
+                }
+                return list;
+            }
+            else
+            {
+                if (CurrentPage - 3 <= 1)
+                {
+                    for (int i = 1; i <= 6; i++)
+                    {
+                        list.Add(i.ToString());
+                    }
+                    list.Add(">>");
+                    list.Add(Total.ToString());
+                }
+                else if (CurrentPage + 3 >= Total)
+                {
+                    list.Add("1");
+                    list.Add("<<");
+                    for (int i = Total - 5; i <= Total; i++)
+                    {
+                        list.Add(i.ToString());
+                    }
+                }
+                else
+                {
+                    list.Add("1");
+                    list.Add("<<");
+                    for (int i = CurrentPage - 2; i <= CurrentPage + 2; i++)
+                    {
+                        list.Add(i.ToString());
+                    }
+                    list.Add(">>");
+                    list.Add(Total.ToString());
+                }
+            }
+            return list;
+        }
+
+    }
+}
