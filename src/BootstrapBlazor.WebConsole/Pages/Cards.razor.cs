f9397feb72ff73717d1c79974c76352a1d8c8dd0
Nine <948825551@qq.com>
2020-04-15 18:18:27 +0800

    !43 增加功能：增加 Card 组件 * feat:新增Card组件示例 * feat:新增Card组件 * doc:新增Card组件文档 * Merge branch 'dev' into dev-Card * Merge branch 'dev' into dev-Card * Merge branch 'dev' into dev-Card * Merge branch 'dev' into dev-Card * Merge branch 'dev' into dev-Card * Merge branch 'dev' into dev-Card

---
+++ src/BootstrapBlazor.WebConsole/Pages/Cards.razor.cs
@@ -0,0 +1,48 @@
+﻿using BootstrapBlazor.WebConsole.Common;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.WebConsole.Pages
+{
+    /// <summary>
+    /// Card展示组件
+    /// </summary>
+    public partial class Cards
+    {
+        /// <summary>
+        /// Card属性
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+        {
+            new AttributeItem{
+                Name = "Color",
+                Description = "设置卡片边框颜色",
+                Type = "Color",
+                ValueList = "None / Primary / Secondary / Success / Danger / Warning / Info / Light / Dark",
+                DefaultValue = " — "
+            },
+            new AttributeItem{
+                Name = "IsCenter",
+                Description = "通过设置,IsCenter=true 使内容居中",
+                Type = "boolean",
+                ValueList = "true / false",
+                DefaultValue = "false"
+            },
+            new AttributeItem{
+                Name = "IsShowHeader",
+                Description = "通过设置,IsShowHeader=true,显示Header区域",
+                Type = "boolean",
+                ValueList = "true / false",
+                DefaultValue = "false"
+            },
+            new AttributeItem{
+                Name = "IsShowFooter",
+                Description = "通过设置,IsShowFooter=true,显示Footer区域",
+                Type = "boolean",
+                ValueList = "true / false",
+                DefaultValue = "false"
+            }
+        };
+    }
+}
+
---
+++ src/BootstrapBlazor/Components/Card/CardBase.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Card组件基类
+    /// </summary>
+    public abstract class CardBase : BootstrapComponentBase
+    {
+        /// <summary>
+        ///  Card组件样式
+        /// </summary>
+        protected virtual string? ClassName => CssBuilder.Default("card")
+            .AddClass("text-center", IsCenter)
+            .AddClass($"border-{Color.ToDescriptionString()}", Color != Color.None)
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        ///  设置Body Class组件样式
+        /// </summary>
+        protected virtual string? BodyClassName => CssBuilder.Default("card-body")
+            .AddClass($"text-{Color.ToDescriptionString()}", Color != Color.None)
+            .AddClass(Class)
+            .Build();
+
+        /// <summary>
+        /// 设置Footer Class样式
+        /// </summary>
+        protected virtual string? FooterClassName => CssBuilder.Default("card-footer")
+            .AddClass("text-muted", IsCenter)
+            .Build();
+
+        /// <summary>
+        /// 设置Class样式
+        /// </summary>
+        [Parameter]
+        public string? Class { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// 获得/设置 CardHeard
+        /// </summary>
+        [Parameter]
+        public RenderFragment? CardHeard { get; set; }
+
+        /// <summary>
+        /// 获得/设置 CardBody
+        /// </summary>
+        [Parameter]
+        public RenderFragment? CardBody { get; set; }
+
+        /// <summary>
+        /// 获得/设置 CardFooter
+        /// </summary>
+        [Parameter]
+        public RenderFragment? CardFooter { get; set; }
+
+        /// <summary>
+        /// 获得/设置Card颜色
+        /// </summary>
+        [Parameter]
+        public Color Color { get; set; }
+
+        /// <summary>
+        /// 设置是否显示Footer
+        /// </summary>
+        [Parameter]
+        public bool IsShowFooter { get; set; } = false;
+
+        /// <summary>
+        /// 设置是否显示Header
+        /// </summary>
+        [Parameter]
+        public bool IsShowHeader { get; set; } = false;
+
+        /// <summary>
+        /// 设置是否居中
+        /// </summary>
+        [Parameter]
+        public bool IsCenter { get; set; }
+    }
+}
