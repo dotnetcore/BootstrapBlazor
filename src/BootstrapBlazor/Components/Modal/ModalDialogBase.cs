45695d6b591d38ea7b8083251a8b4df4a4989379
Argo <argo@163.com>
2020-04-22 12:14:56 +0800

    !49 增加功能：弹窗组件增加多级弹窗功能 * refactor: 移除不使用的变量 * doc: 增加返回上一个弹窗示例 * feat: 增加多层弹窗

---
+++ src/BootstrapBlazor/Components/Modal/ModalDialogBase.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// ModalBody 组件内嵌与 Modal 组件内
+    /// </summary>
+    public class ModalDialogBase : BootstrapComponentBase
+    {
+        /// <summary>
+        /// 获得 弹窗组件样式
+        /// </summary>
+        protected virtual string? ClassName => CssBuilder.Default("modal-dialog")
+            .AddClass("modal-dialog-centered", IsCentered)
+            .AddClass($"modal-{Size.ToDescriptionString()}", Size != Size.None)
+            .AddClass("modal-dialog-scrollable", IsScrolling)
+            .AddClass("d-none", !IsShown)
+            .Build();
+
+        /// <summary>
+        /// 获得/设置 是否显示对话框
+        /// </summary>
+        public bool IsShown { get; set; }
+
+        /// <summary>
+        /// 获得/设置 弹窗标题
+        /// </summary>
+        [Parameter]
+        public string Title { get; set; } = "未设置";
+
+        /// <summary>
+        /// 获得/设置 弹窗大小
+        /// </summary>
+        [Parameter]
+        public Size Size { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否垂直居中 默认为 true
+        /// </summary>
+        [Parameter]
+        public bool IsCentered { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否弹窗正文超长时滚动
+        /// </summary>
+        [Parameter] public bool IsScrolling { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否显示关闭按钮
+        /// </summary>
+        [Parameter] public bool ShowCloseButton { get; set; } = true;
+
+        /// <summary>
+        /// 获得/设置 是否显示 Footer 默认为 true
+        /// </summary>
+        [Parameter]
+        public bool ShowFooter { get; set; } = true;
+
+        /// <summary>
+        /// 获得/设置 ModalBody 组件
+        /// </summary>
+        [Parameter] public RenderFragment? BodyTemplate { get; set; }
+
+        /// <summary>
+        /// 获得/设置 ModalFooter 组件
+        /// </summary>
+        [Parameter] public RenderFragment? FooterTemplate { get; set; }
+
+        /// <summary>
+        /// 获得/设置 弹窗容器实例
+        /// </summary>
+        [CascadingParameter]
+        public ModalBase? Modal { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Modal?.AddDialogs(this);
+        }
+
+        /// <summary>
+        /// 弹窗状态切换方法
+        /// </summary>
+        public void Toggle() => Modal?.ShowDialog(this);
+    }
+}
