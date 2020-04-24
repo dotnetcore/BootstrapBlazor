09a1a81b1a68cedd7690e116eb02df7d8be59868
Argo <argo@163.com>
2020-04-24 21:32:28 +0800

    !54 增加功能：增加代码高亮显示组件 * feat: 增加拷贝成功提示 * feat: 高亮代码显示组件增加拷贝功能 * feat: 更新代码高亮样式 * feat: 增加代码高亮功能

---
+++ src/BootstrapBlazor.WebConsole/Pages/Components/Block.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.WebConsole.Pages.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    sealed partial class Block
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private ElementReference CodeElement { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter] public string Title { get; set; } = "未设置";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter] public string Introduction { get; set; } = "未设置";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter] public string? CodeFile { get; set; }
+
+        /// <summary>
+        /// 获得/设置 IJSRuntime 实例
+        /// </summary>
+        [Inject] private IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            JSRuntime.InvokeVoidAsync("$.highlight", CodeElement);
+        }
+    }
+}
