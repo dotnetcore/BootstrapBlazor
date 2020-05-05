4c6ddc2c96d459a1c43d7c742cba88579a6c5fd2
Argo <argo@163.com>
2020-05-06 00:11:27 +0800

    !75 增加功能：Blazor 增加头像框组件 * feat: 增加边框支持 * feat: 增加图片文件加载失败处理逻辑 * feat: 增加图标文字类型示例 * feat: 增加 Avatar 组件

---
+++ src/BootstrapBlazor/Components/Avatar/AvatarBase.cs
@@ -0,0 +1,114 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Avatar 头像框组件
+    /// </summary>
+    public abstract class AvatarBase : BootstrapComponentBase
+    {
+        /// <summary>
+        /// 获得/设置 DOM 组件实例
+        /// </summary>
+        protected ElementReference AvatarElement { get; set; }
+
+        /// <summary>
+        /// 获得 样式集合
+        /// </summary>
+        /// <returns></returns>
+        protected string? ClassName => CssBuilder.Default("avatar")
+            .AddClass("is-circle", IsCircle)
+            .AddClass("border border-info", IsBorder)
+            .AddClass("border-success", IsBorder && IsLoaded.HasValue && IsLoaded.Value && !IsIcon && !IsText)
+            .AddClass("border-danger", IsBorder && IsLoaded.HasValue && !IsLoaded.Value)
+            .AddClass($"is-{Size.ToDescriptionString()}", Size != Size.None && Size != Size.Medium)
+            .AddClassFromAttributes(AdditionalAttributes)
+            .Build();
+
+        /// <summary>
+        /// 获得 图片样式
+        /// </summary>
+        protected string? ImgClassString => (IsLoaded.HasValue && IsLoaded.Value) ? null : "d-none";
+
+        /// <summary>
+        /// 获得/设置 是否为圆形
+        /// </summary>
+        [Parameter]
+        public bool IsCircle { get; set; }
+
+        /// <summary>
+        /// 获得/设置 Image 头像路径地址
+        /// </summary>
+        [Parameter]
+        public string? Url { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否为图标
+        /// </summary>
+        [Parameter]
+        public bool IsIcon { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否为图标
+        /// </summary>
+        [Parameter]
+        public string Icon { get; set; } = "fa fa-user";
+
+        /// <summary>
+        /// 获得/设置 是否为显示为文字
+        /// </summary>
+        [Parameter]
+        public bool IsText { get; set; }
+
+        /// <summary>
+        /// 获得/设置 头像框显示文字
+        /// </summary>
+        [Parameter]
+        public string? Text { get; set; }
+
+        /// <summary>
+        /// 获得/设置 头像框大小
+        /// </summary>
+        [Parameter]
+        public Size Size { get; set; } = Size.Medium;
+
+        /// <summary>
+        /// 获得/设置 是否显示 Border 默认为 false
+        /// </summary>
+        [Parameter]
+        public bool IsBorder { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否显示图片
+        /// </summary>
+        protected bool? IsLoaded { get; set; }
+
+        /// <summary>
+        /// OnAfterRender 方法
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRender(firstRender);
+
+            if (firstRender) JSRuntime.Invoke(AvatarElement, "avatar");
+        }
+
+        /// <summary>
+        /// 图片加载失败时回调此方法
+        /// </summary>
+        protected void OnError()
+        {
+            IsIcon = true;
+            IsLoaded = false;
+        }
+
+        /// <summary>
+        /// 图片加载失败时回调此方法
+        /// </summary>
+        protected void OnLoad()
+        {
+            IsLoaded = true;
+        }
+    }
+}
