07718867a2f59aae1f2955ac92e122374eb07953
Nine <948825551@qq.com>
2020-05-08 22:08:15 +0800

    !78 增加功能：新增 Timeline 组件 * Merge branch 'dev' of https://gitee.com/LongbowEnterprise/BootstrapBla… * doc:增加时间线组件文档 * feat:增加时间线样式 * feat:增加时间线组件内容 * feat: 增加时间线组件基类 * feat:增加时间线组件 * Merge branch 'dev' into dev-Timeline-feat * Merge branch 'dev' into dev-Timeline-feat * Merge branch 'dev' into dev-Timeline-feat

---
+++ src/BootstrapBlazor/Components/Timeline/TimelineBase.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 时间线组件基类
+    /// </summary>
+    public abstract class TimelineBase : IdComponentBase
+    {
+        /// <summary>
+        /// 获得 Timeline 样式
+        /// </summary>
+        protected string? ClassName => CssBuilder.Default("timeline")
+            .Build();
+
+        /// <summary>
+        /// 获得/设置 绑定数据集
+        /// </summary>
+        [Parameter]
+        public IEnumerable<TimelineItem>? Items { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否反转
+        /// </summary>
+        [Parameter]
+        public bool Reverse { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            if (Reverse)
+            {
+                var arr = Items.Reverse();
+                Items = arr;
+            }
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/Timeline/TimelineItem.cs
@@ -0,0 +1,33 @@
+﻿namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 时间线选项
+    /// </summary>
+    public class TimelineItem
+    {
+        /// <summary>
+        /// 获得 / 设置 时间线内容
+        /// </summary>
+        public string? Content { get; set; }
+
+        /// <summary>
+        /// 获得 / 设置 时间线时间
+        /// </summary>
+        public string? DateTime { get; set; }
+
+        /// <summary>
+        /// 获得 / 设置 时间线颜色
+        /// </summary>
+        public Color? Color { get; set; }
+
+        /// <summary>
+        /// 获得 / 设置 时间线尺寸
+        /// </summary>
+        public Size? Size { get; set; }
+
+        /// <summary>
+        /// 获得 / 设置 时间线图标
+        /// </summary>
+        public string? Icon { get; set; }
+    }
+}
