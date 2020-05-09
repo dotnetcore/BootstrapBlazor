156d6b1dce6b9c58c66ee0ef880f4db5254721ee
Argo <argo@163.com>
2020-05-09 20:53:39 +0800

    !80 增加功能：增加 Rate 组件 * Merge branch 'dev' into dev-rate * feat: 增加回调方法 * feat: 增加 rate 脚本 * style: 增加 rate 样式 * feat: 增加 Rates 相关文件 * doc: 增加菜单

---
+++ src/BootstrapBlazor.WebConsole/Pages/Rates.razor.cs
@@ -0,0 +1,22 @@
+﻿using BootstrapBlazor.WebConsole.Pages.Components;
+
+namespace BootstrapBlazor.WebConsole.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class Rates
+    {
+        private int BindValue { get; set; } = 0;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Logger? Trace { get; set; }
+
+        private void OnValueChanged(int val)
+        {
+            Trace?.Log($"评星: {val}");
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/Rate/RateBase.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Rate 组件基类
+    /// </summary>
+    public abstract class RateBase : BootstrapComponentBase
+    {
+        private JSInterop<RateBase>? Interop { get; set; }
+
+        /// <summary>
+        /// 获得/设置 Rate DOM 元素实例
+        /// </summary>
+        protected ElementReference RateElement { get; set; }
+
+        /// <summary>
+        /// 获得/设置 组件值
+        /// </summary>
+        [Parameter]
+        public int Value { get; set; }
+
+        /// <summary>
+        /// 获得/设置 组件值变化时回调委托
+        /// </summary>
+        [Parameter]
+        public EventCallback<int> ValueChanged { get; set; }
+
+        /// <summary>
+        /// 获得/设置 组件值变化时回调委托
+        /// </summary>
+        [Parameter]
+        public Action<int>? OnValueChanged { get; set; }
+
+        /// <summary>
+        /// OnAfterRender 方法
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                if (Interop == null && JSRuntime != null) Interop = new JSInterop<RateBase>(JSRuntime);
+                Interop?.Invoke(this, RateElement, "rate", nameof(Clicked));
+            }
+        }
+
+        /// <summary>
+        /// 文件上传成功后回调此方法
+        /// </summary>
+        [JSInvokable]
+        public void Clicked(int val)
+        {
+            Value = val;
+            if (ValueChanged.HasDelegate) ValueChanged.InvokeAsync(Value);
+            OnValueChanged?.Invoke(Value);
+        }
+
+        /// <summary>
+        /// Dispose 方法
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing) Interop?.Dispose();
+        }
+    }
+}
