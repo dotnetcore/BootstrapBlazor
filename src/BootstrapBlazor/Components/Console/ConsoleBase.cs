2daf84b7e3212fe72d6b5140ce187506a86681fe
Argo <argo@163.com>
2020-05-20 21:01:15 +0800

    !95 增加功能：增加 Console 控制台组件 * feat: 增加通讯指示灯样式 * feat: 增加 Console 组件

---
+++ src/BootstrapBlazor/Components/Console/ConsoleBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Console 组件
+    /// </summary>
+    public abstract class ConsoleBase : BootstrapComponentBase
+    {
+        /// <summary>
+        /// 获得 Console Body Style 字符串
+        /// </summary>
+        protected string? BodyStyleString => CssBuilder.Default()
+            .AddClass($"height: {Height}px;", Height > 0)
+            .Build();
+
+        /// <summary>
+        /// 获得/设置 组件绑定数据源
+        /// </summary>
+        [Parameter]
+        public IEnumerable<string> Items { get; set; } = new string[0];
+
+        /// <summary>
+        /// 获得/设置 组件高度 默认为 126px;
+        /// </summary>
+        [Parameter]
+        public int Height { get; set; } = 126;
+
+        /// <summary>
+        /// 清空控制台消息方法
+        /// </summary>
+        public void ClearConsole()
+        {
+            Items = new string[0];
+        }
+    }
+}
