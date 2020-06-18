947a7c4fc91d361b93987276a2154a3597e30529
Argo-Supper <argo@163.com>
2020-06-18 15:34:36 +0800

    refactor: HeaderTemplate 更改为 TableColumns

--- src/BootstrapBlazor/Components/Table/TableBase.cs
+++ src/BootstrapBlazor/Components/Table/TableBase.cs
@@ -43,7 +43,7 @@ namespace BootstrapBlazor.Components
         /// 获得/设置 TableHeader 实例
         /// </summary>
         [Parameter]
-        public RenderFragment<TItem>? HeaderTemplate { get; set; }
+        public RenderFragment<TItem>? TableColumns { get; set; }
 
         /// <summary>
         /// 获得/设置 TableFooter 实例
