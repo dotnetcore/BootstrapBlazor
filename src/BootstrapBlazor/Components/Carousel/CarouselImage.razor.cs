7bc7696817d221136104c221871c18c91c7b3ad3
Argo <argo@163.com>
2020-06-23 01:17:11 +0800

    !248 feat(#I1LHUB)：Carousel 组件增加 OnClick 回调委托 * feat: 增加 Carousel 图片点击回调事件

---
+++ src/BootstrapBlazor/Components/Carousel/CarouselImage.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class CarouselImage : ComponentBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? ImageUrl { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public Func<string, Task>? OnClick { get; set; }
+
+        private async Task OnClickImage()
+        {
+            if (OnClick != null) await OnClick(ImageUrl ?? "");
+        }
+    }
+}
