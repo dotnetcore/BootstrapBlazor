66da5c64b3378f864cacc70e27ad243da13f66fd
Argo <argo@163.com>
2020-07-05 13:56:32 +0800

    !284 doc(#I1KSHN): 更新服务器端与客户端的说明文档 * doc: 重新规划菜单 * doc: 更正安装步骤说明文档

---
+++ src/BootstrapBlazor.Shared/Pages/Components/InstallContent.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class InstallContent
+    {
+        /// <summary>
+        /// 获得/设置 版本号字符串
+        /// </summary>
+        private string Version { get; set; } = "fetching";
+
+        /// <summary>
+        ///
+        /// </summary>
+        [Parameter]
+        public string Title { get; set; } = "服务器端渲染模式";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+
+        /// <summary>
+        /// OnInitializedAsync 方法
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task OnInitializedAsync()
+        {
+            Version = await VersionManager.GetVersionAsync();
+        }
+    }
+}
