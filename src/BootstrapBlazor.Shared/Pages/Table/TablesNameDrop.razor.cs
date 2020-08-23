463b43628438878137cbcafee72bd3ee4ccbaf16
Argo <argo@163.com>
2020-08-23 16:16:00 +0800

    !406 perf(#I1SEYK): Table 组件自定义编辑模板示例更新提高性能 * refactor: 移动自定义模板内容到组件中提高性能

---
+++ src/BootstrapBlazor.Shared/Pages/Table/TablesNameDrop.razor.cs
@@ -0,0 +1,28 @@
+﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BootstrapBlazor.Shared.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TablesNameDrop
+    {
+#nullable disable
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public BindItem Model { get; set; }
+#nullable restore
+
+        private readonly List<SelectedItem> items = new List<SelectedItem>()
+        {
+            new SelectedItem { Text = "自定义姓名1", Value = "自定义姓名1" },
+            new SelectedItem { Text = "自定义姓名2", Value = "自定义姓名2" },
+            new SelectedItem { Text = "自定义姓名3", Value = "自定义姓名3" },
+            new SelectedItem { Text = "自定义姓名4", Value = "自定义姓名4" },
+        };
+    }
+}
