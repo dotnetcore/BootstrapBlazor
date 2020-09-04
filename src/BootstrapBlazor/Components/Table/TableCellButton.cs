3c6fe625f1c02584c16869959bf9b2cf9479b7dd
Argo <argo@163.com>
2020-09-04 18:31:09 +0800

    !467 feat(#I1TWYZ): TableCellButton 按钮无刷新方法回调增加当前行实例 * feat: TableCellButton 无刷新回调方法增加参数返回

--- src/BootstrapBlazor/Components/Table/TableCellButton.cs
+++ src/BootstrapBlazor/Components/Table/TableCellButton.cs
@@ -13,12 +13,20 @@ namespace BootstrapBlazor.Components
         /// <summary>
         /// 获得/设置 当前行绑定数据
         /// </summary>
-        [Parameter] public TItem? Item { get; set; }
+        [Parameter] 
+        public TItem? Item { get; set; }
 
         /// <summary>
         /// 获得/设置 按钮点击后的回调方法
         /// </summary>
-        [Parameter] public Func<TItem, Task>? OnClickCallback { get; set; }
+        [Parameter] 
+        public Func<TItem, Task>? OnClickCallback { get; set; }
+
+        /// <summary>
+        /// 获得/设置 OnClick 事件不刷新父组件
+        /// </summary>
+        [Parameter]
+        public new Func<TItem, Task>? OnClickWithoutRender { get; set; }
 
         /// <summary>
         /// OnInitialized 方法
@@ -37,6 +45,7 @@ namespace BootstrapBlazor.Components
                     if (onClick.HasDelegate) await onClick.InvokeAsync(e);
 
                     if (Item != null && OnClickCallback != null) await OnClickCallback.Invoke(Item);
+                    if (Item != null && OnClickWithoutRender != null) await OnClickWithoutRender.Invoke(Item);
                 }
             });
         }
