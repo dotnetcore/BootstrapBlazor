49abd21126896b786ee4e5cdb0c87beceb369b09
Argo <argo@163.com>
2020-10-08 19:24:13 +0800

    !565 feat(#I1XJYH): 增加 Toast 组件快速调用方式 Merge pull request !565 from Argo/dev-toast

--- src/BootstrapBlazor/Components/Toast/ToastService.cs
+++ src/BootstrapBlazor/Components/Toast/ToastService.cs
@@ -5,6 +5,58 @@
     /// </summary>
     public class ToastService : PopupServiceBase<ToastOption>
     {
+        /// <summary>
+        /// Toast 调用成功快捷方法
+        /// </summary>
+        /// <param name="title">Title 属性</param>
+        /// <param name="content">Content 属性</param>
+        /// <param name="autoHide">自动隐藏属性默认为 true</param>
+        /// <returns></returns>
+        public void Success(string? title = null, string? content = null, bool autoHide = true)
+        {
+            Show(new ToastOption()
+            {
+                Category = ToastCategory.Success,
+                IsAutoHide = autoHide,
+                Title = title ?? "",
+                Content = content ?? ""
+            });
+        }
 
+        /// <summary>
+        /// Toast 调用错误快捷方法
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="content"></param>
+        /// <param name="autoHide"></param>
+        /// <returns></returns>
+        public void Error(string? title = null, string? content = null, bool autoHide = true)
+        {
+            Show(new ToastOption()
+            {
+                Category = ToastCategory.Error,
+                IsAutoHide = autoHide,
+                Title = title ?? "",
+                Content = content ?? ""
+            });
+        }
+
+        /// <summary>
+        /// Toast 调用提示信息快捷方法
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="content"></param>
+        /// <param name="autoHide"></param>
+        /// <returns></returns>
+        public void Information(string? title = null, string? content = null, bool autoHide = true)
+        {
+            Show(new ToastOption()
+            {
+                Category = ToastCategory.Information,
+                IsAutoHide = autoHide,
+                Title = title ?? "",
+                Content = content ?? ""
+            });
+        }
     }
 }
