15fe34eace063b870ffe3819bcde585a8c3423f5
Argo <argo@163.com>
2020-10-30 15:07:22 +0800

    !634 feat(#I23CD0): 增加项目模板功能 * doc: 更新服务器端模式生成文档 * doc: 更新模板文档 * refactor: 更新扩展组件依赖 * refactor: 更新解决方案 * refactor: 移动项目模板文件到 Pack 工程内 * refactor: 移动 config 文件夹 * feat: 增加 BootstrapBlazorServer 项目模板内容 * refactor: BootstrapBlazor 打包配置文件不输出 * refactor: 更新模板配置文件 * refactor: 删除 test 文件夹 * feat: 增加 Blazor 项目模板

---
+++ templates/BootstrapBlazorServerPack/templates/BootstrapBlazorServer/Shared/MainLayout.razor.cs
@@ -0,0 +1,63 @@
+﻿using BootstrapBlazor.Components;
+using System.Collections.Generic;
+
+namespace BootstrapBlazorServer.Shared
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class MainLayout
+    {
+        private IEnumerable<MenuItem> Menus { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // TODO: 菜单获取可以通过数据库获取，此处为示例直接拼装的菜单集合
+            Menus = GetIconSideMenuItems();
+        }
+
+        private IEnumerable<MenuItem> GetIconSideMenuItems()
+        {
+            var ret = new List<MenuItem>
+            {
+                new MenuItem() { Text = "返回组件库", Icon = "fa fa-fw fa-home", Url = "https://blazor.sdgxgz.com/components" },
+                new MenuItem() { Text = "Index", Icon = "fa fa-fw fa-fa", Url = "" },
+                new MenuItem() { Text = "Counter", Icon = "fa fa-fw fa-check-square-o", Url = "counter" },
+                new MenuItem() { Text = "FetchData", Icon = "fa fa-fw fa-database", Url = "fetchdata" },
+                new MenuItem() { Text = "系统设置", Icon = "fa fa-fw fa-gears", Url = "#" },
+                new MenuItem() { Text = "权限设置", Icon = "fa fa-fw fa-users", Url = "#" },
+                new MenuItem() { Text = "日志设置", Icon = "fa fa-fw fa-database", Url = "#" }
+            };
+
+            ret[4].AddItem(new MenuItem() { Text = "网站设置", Icon = "fa fa-fw fa-fa" });
+            ret[4].AddItem(new MenuItem() { Text = "任务设置", Icon = "fa fa-fw fa-tasks" });
+            ret[4].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
+            ret[4].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
+            ret[4].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
+            ret[4].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
+            ret[4].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
+            ret[4].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
+
+            ret[5].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
+            ret[5].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
+            ret[5].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
+            ret[5].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
+            ret[5].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
+            ret[5].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
+
+            ret[6].AddItem(new MenuItem() { Text = "用户设置", Icon = "fa fa-fw fa-user" });
+            ret[6].AddItem(new MenuItem() { Text = "菜单设置", Icon = "fa fa-fw fa-dashboard" });
+            ret[6].AddItem(new MenuItem() { Text = "角色设置", Icon = "fa fa-fw fa-sitemap" });
+            ret[6].AddItem(new MenuItem() { Text = "访问日志", Icon = "fa fa-fw fa-bars" });
+            ret[6].AddItem(new MenuItem() { Text = "登录日志", Icon = "fa fa-fw fa-user-circle-o" });
+            ret[6].AddItem(new MenuItem() { Text = "操作日志", Icon = "fa fa-fw fa-edit" });
+
+            return ret;
+        }
+    }
+}
