37e08a6c8cb4a1f6955b0c19eea0217c15a2fcca
Argo-Cloud <argo@163.com>
2020-12-09 18:53:49 +0800

    feat: 增加 Freesql 支持

---
+++ src/BootstrapBlazor.DataAcces.FreeSql/PetaPocoServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿// **********************************
+// 框架名称：BootstrapBlazor 
+// 框架作者：Argo Zhang
+// 开源地址：
+// Gitee : https://gitee.com/LongbowEnterprise/BootstrapBlazor
+// GitHub: https://github.com/ArgoZhang/BootstrapBlazor 
+// 开源协议：LGPL-3.0 (https://gitee.com/LongbowEnterprise/BootstrapBlazor/blob/dev/LICENSE)
+// **********************************
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.DataAcces.FreeSql;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// BootstrapBlazor 服务扩展类
+    /// </summary>
+    public static class PetaPocoServiceCollectionExtensions
+    {
+        /// <summary>
+        /// 增加 PetaPoco 数据库操作服务
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPetaPoco(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IDataService<>), typeof(DefaultDataService<>));
+            return services;
+        }
+    }
+}
