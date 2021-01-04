13acf2850856d0cd9705b06e82676cbb7ed817a9
Argo <argo@163.com>
2021-01-04 12:08:42 +0800

    !846 feat(#I2BQ9D): move table export script into extension lib * Merge branch 'dev' into dev-export-js-6 * refactor: 精简项目文件增加导出脚本 * refactor: 移动导出脚本到 TableExport 工程中 * refactor: 移动 culture 脚本到 share 工程 common 中 * refactor: 集成扩展脚本 * refactor: 拆分扩展脚本 * refactor: 集成 drag 脚本 * refactor: 拆分 drag 脚本 * refactor: 集成 tab 脚本 * refactor: 拆分 tab 脚本 * refactor: 集成 longbow.upload 脚本 * refactor: 拆分 longbow.upload 脚本文件 * refactor: 集成 longbow.captcha 脚本 * refactor: 拆分 captcha 脚本到独立文件中

--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
@@ -1,13 +1,4 @@
 ﻿(function ($) {
-    $.generatefile = (fileName, bytesBase64, contenttype) => {
-        var link = document.createElement('a');
-        link.download = fileName;
-        link.href = 'data:' + contenttype + ';base64,' + bytesBase64;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    };
-
     window.Toasts = [];
 
     $.extend({
---
+++ src/Extensions/Components/BootstrapBlazor.TableExport/wwwroot/js/export.js
@@ -0,0 +1,10 @@
+﻿(function ($) {
+    $.generatefile = function (fileName, bytesBase64, contenttype) {
+        var link = document.createElement('a');
+        link.download = fileName;
+        link.href = 'data:' + contenttype + ';base64,' + bytesBase64;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+})(jQuery);
