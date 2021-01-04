fc2937e79e6648ce066e84e74a2d6afd62ffc9ad
Argo <argo@163.com>
2021-01-04 11:49:06 +0800

    !844 feat(#I2BQ8O): split drag scripts in a sperate file * Merge branch 'dev' into dev-drag-js-4 * refactor: 集成 drag 脚本 * refactor: 拆分 drag 脚本 * refactor: 集成 tab 脚本 * refactor: 拆分 tab 脚本 * refactor: 集成 longbow.upload 脚本 * refactor: 拆分 longbow.upload 脚本文件 * refactor: 集成 longbow.captcha 脚本 * refactor: 拆分 captcha 脚本到独立文件中

--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
@@ -88,48 +88,6 @@
         }
     };
 
-    $.fn.extend({
-        drag: function (star, move, end) {
-            var $this = $(this);
-
-            var handleDragStart = function (e) {
-                e.stopPropagation();
-
-                document.addEventListener('mousemove', handleDragMove);
-                document.addEventListener('touchmove', handleDragMove);
-                document.addEventListener('mouseup', handleDragEnd);
-                document.addEventListener('touchend', handleDragEnd);
-
-                if ($.isFunction(star)) {
-                    star.call($this, e);
-                }
-            };
-
-            var handleDragMove = function (e) {
-                if ($.isFunction(move)) {
-                    move.call($this, e);
-                }
-            };
-
-            var handleDragEnd = function (e) {
-                // 结束拖动
-                if ($.isFunction(end)) {
-                    end.call($this, e);
-                }
-
-                window.setTimeout(function () {
-                    document.removeEventListener('mousemove', handleDragMove);
-                    document.removeEventListener('touchmove', handleDragMove);
-                    document.removeEventListener('mouseup', handleDragEnd);
-                    document.removeEventListener('touchend', handleDragEnd);
-                }, 100);
-            };
-
-            $this.on('mousedown', handleDragStart);
-            $this.on('touchstart', handleDragStart);
-        }
-    });
-
     $.extend({
         html5edit: function (el, options) {
             if (!$.isFunction($.fn.summernote)) {
