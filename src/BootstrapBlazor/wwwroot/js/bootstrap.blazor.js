521ac556eb7d7f70b2ba99f02d2098b88589eeee
Argo <argo@163.com>
2021-01-13 14:30:45 +0800

    !883 feat(#I2D2XY): QRCode css/javascript split * chore: 增加缺失的 Split 样式 * chore: QRCode 拆分样式与脚本

--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
@@ -520,30 +520,6 @@
                 $(this).parent().removeClass('hover');
             });
         },
-        bb_qrcode: function (el) {
-            var $el = $(el);
-            var $qr = $el.find('.qrcode-img');
-            $qr.html('');
-            var method = "";
-            var obj = null;
-            if (arguments.length === 2) method = arguments[1];
-            else {
-                method = arguments[2];
-                obj = arguments[1];
-            }
-            if (method === 'generate') {
-                var text = $el.find('.qrcode-text').val();
-                qrcode = new QRCode($qr[0], {
-                    text: text,
-                    width: 128,
-                    height: 128,
-                    colorDark: "#000000",
-                    colorLight: "#ffffff",
-                    correctLevel: QRCode.CorrectLevel.H
-                });
-                obj.invokeMethodAsync("Generated");
-            }
-        },
         bb_select: function (id) {
             var $el = $('#' + id);
             var $search = $el.find('input.search-text');
