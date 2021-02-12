b83b3d50dc0d625866b8a99cb5069387c74a5a6d
Argo <argo@163.com>
2021-02-12 14:01:47 +0800

    !1009 refactor(#I37XBV): Tree split css/javascript * refactor: Tree split css/javascript

---
+++ src/BootstrapBlazor/Components/Tree/Tree.razor.cs.js
@@ -0,0 +1,12 @@
+﻿(function ($) {
+    $.extend({
+        bb_tree: function (el) {
+            var $el = $(el);
+            $el.find('.tree-content').hover(function () {
+                $(this).parent().addClass('hover');
+            }, function () {
+                $(this).parent().removeClass('hover');
+            });
+        }
+    });
+})(jQuery);
--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
@@ -89,14 +89,6 @@
                 var $win = $body.find('.console-window');
                 $body.scrollTop($win.height());
             }
-        },
-        bb_tree: function (el) {
-            var $el = $(el);
-            $el.find('.tree-content').hover(function () {
-                $(this).parent().addClass('hover');
-            }, function () {
-                $(this).parent().removeClass('hover');
-            });
         }
     });
 
