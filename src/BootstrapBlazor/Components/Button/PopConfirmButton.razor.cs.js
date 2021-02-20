4d34c4745f0c3139542fa88b105a48abb3413fda
Argo <argo@163.com>
2021-02-20 23:04:07 +0800

    !1044 refactor(#I38DIG): PopConfirmButton split javascript * refactor: 拆分 PopConfirmButton 脚本

---
+++ src/BootstrapBlazor/Components/Button/PopConfirmButton.razor.cs.js
@@ -0,0 +1,16 @@
+﻿(function ($) {
+    $.extend({
+        bb_confirm: function (id) {
+            var $ele = $('[data-target="' + id + '"]');
+            var $button = $('#' + id);
+
+            $button.popover({
+                toggle: 'confirm',
+                html: true,
+                sanitize: false,
+                content: $ele.find('.popover-body').html()
+            });
+            $button.popover('show');
+        }
+    });
+})(jQuery);
--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.js
@@ -49,18 +49,6 @@
                 $ele.popover(method);
             }
         },
-        bb_confirm: function (id) {
-            var $ele = $('[data-target="' + id + '"]');
-            var $button = $('#' + id);
-
-            $button.popover({
-                toggle: 'confirm',
-                html: true,
-                sanitize: false,
-                content: $ele.find('.popover-body').html()
-            });
-            $button.popover('show');
-        }
     });
 
     $(function () {
