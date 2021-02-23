e27ca79a7214f00e0f25b7364e0bba120552ba6d
Argo <argo@163.com>
2021-02-23 21:24:05 +0800

    !1055 docs(#I38S0X): update TablesWrap sample * docs: 更新 TablesWrap 示例 * docs: 增加资源文件 * feat: Table 组件显示支持枚举类型 Display 标签 * docs: 增加 Hobbys 资源文件

--- src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesWrap.razor.cs
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesWrap.razor.cs
@@ -2,22 +2,54 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BootstrapBlazor.Shared.Pages
+namespace BootstrapBlazor.Shared.Pages.Table
 {
     /// <summary>
-    /// 
+    /// 折行演示示例代码
     /// </summary>
     public sealed partial class TablesWrap
     {
+        private static readonly Random random = new Random();
+
+        [NotNull]
+        private IEnumerable<Foo>? CellItems { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
         /// <summary>
-        /// 
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            CellItems = Enumerable.Range(1, 4).Select(i => new Foo()
+            {
+                Id = i,
+                Name = Localizer["Foo.Name", $"{i:d4}"],
+                DateTime = DateTime.Now.AddDays(i - 1),
+                Address = Localizer["Foo.Address", $"{random.Next(1000, 2000)}"],
+                Count = random.Next(1, 100),
+                Complete = random.Next(1, 100) > 50,
+                Education = random.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middel
+            }).ToList();
+        }
+
+        /// <summary>
+        /// OnAfterRenderAsync 方法
         /// </summary>
         /// <param name="firstRender"></param>
         /// <returns></returns>
@@ -30,22 +62,5 @@ namespace BootstrapBlazor.Shared.Pages
                 await JSRuntime.InvokeVoidAsync("$.table_wrap");
             }
         }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        private List<Foo> GenerateCellItems() => Enumerable.Range(1, 4).Select(i => new Foo()
-        {
-            Id = i,
-            Name = $"张三 {i:d4}",
-            DateTime = DateTime.Now.AddDays(i - 1),
-            Address = $"地球、中国、上海市普陀区金沙江路 {random.Next(1000, 2000)} 弄 这里是超长单元格示例",
-            Count = random.Next(1, 100),
-            Complete = random.Next(1, 100) > 50,
-            Education = random.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middel
-        }).ToList();
-
-        private IEnumerable<Foo> CellItems => GenerateCellItems();
     }
 }
