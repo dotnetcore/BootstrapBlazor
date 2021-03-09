85d20ccdfa14d4acc612751c2059fea04249548a
Argo <argo@163.com>
2021-03-10 00:28:57 +0800

    !1121 docs(#I3APUU): add TableRow sample code * refactor: 增加 TablesRow 脚本 * refactor: 精简代码 * docs: 加粗字体

---
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesRow.razor.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Table
+{
+    /// <summary>
+    /// Table 组件行示例代码
+    /// </summary>
+    public partial class TablesRow
+    {
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        [Inject]
+        [NotNull]
+        private ToastService? ToastService { get; set; }
+
+        private static IEnumerable<int> PageItemsSource => new int[] { 4, 10, 20 };
+
+        [NotNull]
+        private List<Foo>? Items { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Items = Foo.GenerateFoo(Localizer);
+        }
+
+        private static Task<Foo> OnAddAsync() => Task.FromResult(new Foo() { DateTime = DateTime.Now });
+
+        private Task<bool> OnSaveAsync(Foo item)
+        {
+            // 增加数据演示代码
+            if (item.Id == 0)
+            {
+                item.Id = Items.Max(i => i.Id) + 1;
+                Items.Add(item);
+            }
+            else
+            {
+                var oldItem = Items.FirstOrDefault(i => i.Id == item.Id);
+                if (oldItem != null)
+                {
+                    oldItem.Name = item.Name;
+                    oldItem.Address = item.Address;
+                    oldItem.DateTime = item.DateTime;
+                    oldItem.Count = item.Count;
+                    oldItem.Complete = item.Complete;
+                    oldItem.Education = item.Education;
+                }
+            }
+            return Task.FromResult(true);
+        }
+
+        private Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+        {
+            IEnumerable<Foo> items = Items;
+
+            // 设置记录总数
+            var total = items.Count();
+
+            // 内存分页
+            items = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
+
+            return Task.FromResult(new QueryData<Foo>()
+            {
+                Items = items,
+                TotalCount = total,
+                IsSorted = true,
+                IsFiltered = true,
+                IsSearch = true
+            });
+        }
+
+        private async Task DoubleClickRowCallback(Foo item)
+        {
+            var cate = ToastCategory.Success;
+            var title = "双击行回调委托示例";
+            var content = $"选中行数据为名称 {item.Name} 的数据";
+            await ToastService.Show(new ToastOption()
+            {
+                Category = cate,
+                Title = title,
+                Content = content
+            });
+        }
+
+        private static string? SetRowClassFormatter(Foo item) => item.Count > 60 ? "highlight" : null;
+
+        private Foo? CurrentItem { get; set; }
+
+        private Task ClickRow(Foo item)
+        {
+            CurrentItem = item;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}
