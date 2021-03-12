426d5a2446a11bc350ba5fd2875743ebfbe83489
Shelden Xie <1060093735@qq.com>
2021-03-12 15:44:39 +0800

    !1134 fix(#I3B50S): update table not render after edit data in tree * update src/BootstrapBlazor/Components/Table/Table.razor.cs.

--- src/BootstrapBlazor/Components/Table/Table.razor.cs
+++ src/BootstrapBlazor/Components/Table/Table.razor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
@@ -271,6 +271,10 @@ namespace BootstrapBlazor.Components
         private IEnumerable<TItem> GetTreeRows()
         {
             var ret = new List<TItem>();
+            TreeRows = Items.Select(item => new TableTreeNode<TItem>(item)
+            {
+                HasChildren = CheckTreeChildren(item)
+            }).ToList();
             ReloadTreeNodes(ret, TreeRows);
             return ret;
         }
