ee21d06ffa809bff02feae77fd76ea07b1082271
Argo <argo@163.com>
2021-03-20 00:15:21 +0800

    !1168 fix(#I3C8PH): update Tab miss under line style in Dialog when first render * fix: 修复 Tab 组件在弹窗中 active 蓝条初始化不正确问题

--- src/BootstrapBlazor/Components/Tab/Tab.razor.cs.js
+++ src/BootstrapBlazor/Components/Tab/Tab.razor.cs.js
@@ -1,7 +1,13 @@
 ﻿(function ($) {
     $.extend({
         bb_tab: function (el) {
-            $(el).tab('active');
+            var $el = $(el);
+            var handler = window.setInterval(function () {
+                if ($el.is(':visible')) {
+                    window.clearInterval(handler);
+                    $el.tab('active');
+                }
+            }, 200);
         }
     });
 })(jQuery);
