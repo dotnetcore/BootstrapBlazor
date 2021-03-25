eb992f8ada27b807863d0e47998a655c967e3085
Argo <argo@163.com>
2021-03-25 22:23:30 +0800

    !1201 docs(#I3DDGK): update table filter sample code * docs: 更新示例文档链接 * docs: 更新过滤示例代码

---
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesFilter.razor.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Table
+{
+    /// <summary>
+    /// 过滤示例代码
+    /// </summary>
+    public partial class TablesFilter
+    {
+        private static readonly ConcurrentDictionary<Type, Func<IEnumerable<Foo>, string, SortOrder, IEnumerable<Foo>>> SortLambdaCache = new();
+
+        private static IEnumerable<int> PageItemsSource => new int[] { 4, 10, 20 };
+
+        [NotNull]
+        private List<Foo>? Items { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Items = Foo.GenerateFoo(Localizer);
+        }
+
+        private Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+        {
+            IEnumerable<Foo> items = Items;
+
+            // 过滤
+            var isFiltered = false;
+            if (options.Filters.Any())
+            {
+                items = items.Where(options.Filters.GetFilterFunc<Foo>());
+                isFiltered = true;
+            }
+
+            // 排序
+            var isSorted = false;
+            if (!string.IsNullOrEmpty(options.SortName))
+            {
+                // 外部未进行排序，内部自动进行排序处理
+                var invoker = SortLambdaCache.GetOrAdd(typeof(Foo), key => LambdaExtensions.GetSortLambda<Foo>().Compile());
+                items = invoker(items, options.SortName, options.SortOrder);
+                isSorted = true;
+            }
+
+            // 设置记录总数
+            var total = items.Count();
+
+            // 内存分页
+            items = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
+
+            return Task.FromResult(new QueryData<Foo>()
+            {
+                Items = items,
+                TotalCount = total,
+                IsSorted = isSorted,
+                IsFiltered = isFiltered
+            });
+        }
+    }
+}
