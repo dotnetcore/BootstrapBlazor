131473a5c04c93ba546384b3f6daacf3c20d9649
Argo <argo@163.com>
2021-03-25 23:21:46 +0800

    !1202 docs(#I3DECD): update table export sample code * docs: 更新导出示例链接 * docs: 更新导出功能示例

---
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesExport.razor.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Table
+{
+    /// <summary>
+    /// Table 组件数据导出示例
+    /// </summary>
+    public partial class TablesExport
+    {
+        private static readonly ConcurrentDictionary<Type, Func<IEnumerable<Foo>, string, SortOrder, IEnumerable<Foo>>> SortLambdaCache = new();
+
+        /// <summary>
+        /// 获得/设置 版本号字符串
+        /// </summary>
+        private string Version { get; set; } = "fetching";
+
+        private static IEnumerable<int> PageItemsSource => new int[] { 4, 10, 20 };
+
+        [NotNull]
+        private List<Foo>? Items { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        [Inject]
+        [NotNull]
+        private NugetVersionService? VersionManager { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Items = Foo.GenerateFoo(Localizer);
+        }
+
+        private Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+        {
+            IEnumerable<Foo> items = Items;
+
+            // 设置记录总数
+            var total = items.Count();
+
+            // 内存分页
+            items = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
+
+            return Task.FromResult(new QueryData<Foo>()
+            {
+                Items = items,
+                TotalCount = total
+            });
+        }
+
+        private static Task<bool> ExportAsync(IEnumerable<Foo> Items) => Task.FromResult(true);
+
+        /// <summary>
+        /// OnInitializedAsync 方法
+        /// </summary>
+        /// <returns></returns>
+        protected override async Task OnInitializedAsync()
+        {
+            Version = await VersionManager.GetVersionAsync("bootstrapblazor.tableexport");
+        }
+    }
+}
