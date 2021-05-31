589ae936eaf100a62926efa3b4c3062ef3f8c610
Argo <argo@163.com>
2021-06-01 00:04:14 +0800

    !1456 feat(#I3TQ96): add Align parameter on Table footer * feat: 增加 TableFooter 对齐功能 * refactor: 精简级联参数

---
+++ src/BootstrapBlazor/Components/Table/TableFooterCell.razor.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TableFooterCell
+    {
+        private string? ClassString => CssBuilder.Default("table-cell")
+            .AddClass("justify-content-start", Align == Alignment.Left)
+            .AddClass("justify-content-center", Align == Alignment.Center)
+            .AddClass("justify-content-end", Align == Alignment.Right)
+            .AddClassFromAttributes(AdditionalAttributes)
+            .Build();
+
+        /// <summary>
+        /// 获得/设置 单元格内容
+        /// </summary>
+        [Parameter]
+        public string? Text { get; set; }
+
+        /// <summary>
+        /// 获得/设置 文字对齐方式 默认为 Alignment.None
+        /// </summary>
+        [Parameter]
+        public Alignment Align { get; set; }
+
+        /// <summary>
+        /// 获得/设置 是否为移动端模式
+        /// </summary>
+        [CascadingParameter(Name = "IsMobileMode")]
+        public bool IsMobileMode { get; set; }
+    }
+}
