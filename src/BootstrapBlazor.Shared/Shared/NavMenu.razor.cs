1dc586c6b7a720f4fe0188c40b75447a9a321e7b
Argo <argo@163.com>
2021-06-05 00:41:58 +0800

    !1471 doc(#I3UFK7): update lookup menu * doc: 增加 Lookup 菜单资源文件

--- src/BootstrapBlazor.Shared/Shared/NavMenu.razor.cs
+++ src/BootstrapBlazor.Shared/Shared/NavMenu.razor.cs
@@ -516,7 +516,7 @@ namespace BootstrapBlazor.Shared.Shared
                 new()
                 {
                     IsNew = true,
-                    Text = Localizer["TableLook"],
+                    Text = Localizer["TableLookup"],
                     Url = "tables/lookup"
                 },
                 new()
