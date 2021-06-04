5d8f04c92a5a8ee37cdd47cef6dd84b36587df17
Argo <argo@163.com>
2021-06-05 00:54:16 +0800

    !1474 doc(#I3UFKY): add cell merge sample code * doc: 增加单元格示例 * doc: 更新菜单文档 * feat: 增加合并单元格功能 * doc: 删除不使用的 TablesBase 基类 * feat: 显示继承接口 Lookup 与 OnCellRender 方法 * feat: 增加 OnCellRender 回调方法

---
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesCell.razor.cs
@@ -0,0 +1,69 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Shared.Common;
+using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazor.Shared.Pages.Table
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class TablesCell
+    {
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
+
+        [NotNull]
+        private List<Foo>? Items { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Items = Foo.GenerateFoo(Localizer);
+        }
+
+        private static void OnCellRenderHandler(TableCellArgs args)
+        {
+            var foo = args.Row as Foo;
+            if (foo != null && args.ColumnName == "Name")
+            {
+                if (foo.Name == "张三 0002" || foo.Name == "Zhangsan 0002")
+                {
+                    args.Colspan = 2;
+                    args.Class = "cell-demo";
+                    args.Value = $"{foo.Name} -- {foo.Address} -- {foo.Count}";
+                }
+            }
+        }
+
+        private static IEnumerable<AttributeItem> GetTableColumnAttributes() => new[]
+        {
+            new AttributeItem() {
+                Name = "Sortable",
+                Description = "是否排序",
+                Type = "boolean",
+                ValueList = "true|false",
+                DefaultValue = "false"
+            },
+            new AttributeItem() {
+                Name = "Filterable",
+                Description = "是否可过滤数据",
+                Type = "boolean",
+                ValueList = "true|false",
+                DefaultValue = "false"
+            }
+        };
+    }
+}
