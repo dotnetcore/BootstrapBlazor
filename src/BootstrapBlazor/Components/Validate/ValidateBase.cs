a72cc715b10455839bb8cbb7111bebc8858f95fb
Argo <argo@163.com>
2021-06-26 08:39:13 +0000

    !1516 fix(#I3XY2H): resolve show bootstrap validate tooltip when set SkipValidate * chore: bump version to 5.0.37-beta01 * fix: 修复设置 SkipValidate 后 bootstrap 仍然进行验证

--- src/BootstrapBlazor/Components/Validate/ValidateBase.cs
+++ src/BootstrapBlazor/Components/Validate/ValidateBase.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -254,7 +253,7 @@ namespace BootstrapBlazor.Components
         /// 'class' attribute.
         /// </summary>
         protected string? CssClass => CssBuilder.Default()
-            .AddClass(FieldClass)
+            .AddClass(FieldClass, !SkipValidate)
             .AddClassFromAttributes(AdditionalAttributes)
             .Build();
 
