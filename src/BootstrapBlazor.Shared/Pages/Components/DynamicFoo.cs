6f74499c5289239b456babd9762e8a5576a902db
Argo-Tianyi <argo@163.com>
2021-06-27 12:14:26 +0800

    doc: 增加动态显示 DataTable 列示例

--- src/BootstrapBlazor.Shared/Pages/Components/DynamicFoo.cs
+++ src/BootstrapBlazor.Shared/Pages/Components/DynamicFoo.cs
@@ -2,47 +2,21 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using BootstrapBlazor.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BootstrapBlazor.Components.Table.Dynamic;
 
 namespace BootstrapBlazor.Shared.Pages.Components
 {
     /// <summary>
     /// IDyanmicObject 实现类用于动态表格示例
     /// </summary>
-    public class DynamicFoo : IDynamicObject
+    public class DynamicFoo : DynamicObjectContext
     {
         /// <summary>
         /// 
         /// </summary>
-        /// <returns></returns>
-        public object Clone()
+        protected override void OnCreating()
         {
-            throw new NotImplementedException();
-        }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="propertyName"></param>
-        /// <returns></returns>
-        public object? GetValue(string propertyName)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="propertyName"></param>
-        /// <param name="value"></param>
-        public void SetValue(string propertyName, object? value)
-        {
-            throw new NotImplementedException();
         }
     }
 }
--- src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesDynamic.razor.cs
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesDynamic.razor.cs
@@ -2,21 +2,29 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared.Pages.Components;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
 
 namespace BootstrapBlazor.Shared.Pages.Table
 {
     /// <summary>
     /// 动态表格示例
     /// </summary>
-    public partial class TablesDynamic
+    public partial class TablesDynamic : IDisposable
     {
         private IEnumerable<DynamicFoo>? Items { get; set; }
+
+        private DataTable UserData { get; } = new DataTable();
+
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Foo>? Localizer { get; set; }
 
         /// <summary>
         /// OnInitialized 方法
@@ -25,11 +33,43 @@ namespace BootstrapBlazor.Shared.Pages.Table
         {
             base.OnInitialized();
 
+            InitDataTable();
+
             // 拼装动态类型
-            Items = new List<DynamicFoo>()
+            Items = UserData.ToDynamicTableColumns<DynamicFoo>();
+        }
+
+        private void InitDataTable()
+        {
+            UserData.Columns.Add(nameof(Foo.DateTime), typeof(DateTime));
+            UserData.Columns.Add(nameof(Foo.Name), typeof(string));
+            UserData.Columns.Add(nameof(Foo.Count), typeof(int));
+
+            Foo.GenerateWrapFoo(Localizer).ForEach(f =>
+            {
+                UserData.Rows.Add(f.DateTime, f.Name, f.Count);
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
             {
 
-            };
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
