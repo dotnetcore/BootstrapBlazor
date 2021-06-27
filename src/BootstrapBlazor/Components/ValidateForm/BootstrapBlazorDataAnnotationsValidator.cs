d94940b55191494be5bb4af5539a539d9604671d
Argo-Tianyi <argo@163.com>
2021-06-27 18:43:16 +0800

    refactor: 移除不使用的命名空间

--- src/BootstrapBlazor/Components/ValidateForm/BootstrapBlazorDataAnnotationsValidator.cs
+++ src/BootstrapBlazor/Components/ValidateForm/BootstrapBlazorDataAnnotationsValidator.cs
@@ -5,7 +5,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace BootstrapBlazor.Components
 {
