49c466d6a72cb8771b47f8ce588028d67c27a0b2
Argo-Tianyi <argo@163.com>
2021-06-27 10:12:43 +0800

    doc: 更新 DynamicFoo 增加动态表格示例

---
+++ src/BootstrapBlazor.Shared/Pages/Components/DynamicFoo.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Components
+{
+    /// <summary>
+    /// IDyanmicObject 实现类用于动态表格示例
+    /// </summary>
+    public class DynamicFoo : IDynamicObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public object? GetValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public void SetValue(string propertyName, object? value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesDynamic.razor.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared.Pages.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages.Table
+{
+    /// <summary>
+    /// 动态表格示例
+    /// </summary>
+    public partial class TablesDynamic
+    {
+        private IEnumerable<DynamicFoo>? Items { get; set; }
+
+        /// <summary>
+        /// OnInitialized 方法
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // 拼装动态类型
+            Items = new List<DynamicFoo>()
+            {
+
+            };
+        }
+    }
+}
