212190c0c3544b82c5acfe12e27ab6c5c8da30b0
Argo Zhang <argo@163.com>
2021-06-28 13:11:16 +0800

    feat: 增加动态对象与上下文

---
+++ src/BootstrapBlazor/Components/Table/Dynamic/DataTableDynamicObject.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.Data;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// DataTable 动态类实例
+    /// </summary>
+    internal class DataTableDynamicObject : DynamicObject
+    {
+        /// <summary>
+        /// 获得/设置 DataRow 实例
+        /// </summary>
+        public DataRow? Row { get; set; }
+
+        /// <summary>
+        /// Clone 方法
+        /// </summary>
+        /// <returns></returns>
+        public override object Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public override object? GetValue(string propertyName) => Row?[propertyName];
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public override void SetValue(string propertyName, object? value)
+        {
+            if (Row != null)
+            {
+                Row[propertyName] = value;
+            }
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/Table/Dynamic/DynamicObject.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 动态类型实体类 <see cref="IDynamicObject" /> 实例
+    /// </summary>
+    public class DynamicObject : IDynamicObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public virtual object Clone() => new DynamicObject();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        public virtual object? GetValue(string propertyName) => null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        public virtual void SetValue(string propertyName, object? value) { }
+    }
+}
