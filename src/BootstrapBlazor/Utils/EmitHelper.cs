1974d083d68aa6d4ec1a4d556b98d1e504d0e768
Argo <argo@163.com>
2021-06-29 16:09:23 +0000

    !1525 feat(#I3YF59): add EmitHelper static class * feat: 更新 EmitHelper 操作类 * feat: 增加动态类型逻辑 * feat: 增加动态类生成辅助类

---
+++ src/BootstrapBlazor/Utils/EmitHelper.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// Emit 方法帮助类
+    /// </summary>
+    public static class EmitHelper
+    {
+        /// <summary>
+        /// 通过 ITableColumn 创建动态类
+        /// </summary>
+        public static Type? CreateTypeByName(string typeName, IEnumerable<IEditorItem> cols, Type? parent = null)
+        {
+            var typeBuilder = CreateTypeBuilderByName(typeName, parent);
+
+            foreach (var col in cols)
+            {
+                typeBuilder.CreateProperty(col);
+            }
+
+            return typeBuilder.CreateType();
+        }
+
+        private static TypeBuilder CreateTypeBuilderByName(string typeName, Type? parent = null)
+        {
+            var assemblyName = new AssemblyName("BootstrapBlazor_DynamicAssembly");
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect);
+            var moduleBuilder = assemblyBuilder.DefineDynamicModule("BootstrapBlazor_DynamicAssembly_Module");
+            var typeBuilder = moduleBuilder.DefineType(typeName, TypeAttributes.Public, parent);
+            return typeBuilder;
+        }
+
+        private static void CreateProperty(this TypeBuilder typeBuilder, IEditorItem col)
+        {
+            var fieldName = col.GetFieldName();
+            var field = typeBuilder.DefineField($"_{fieldName}", col.PropertyType, FieldAttributes.Private);
+
+            var methodGetField = typeBuilder.DefineMethod($"Get{fieldName}", MethodAttributes.Public, col.PropertyType, null);
+            var methodSetField = typeBuilder.DefineMethod(
+              $"Set{fieldName}", MethodAttributes.Public, null, new Type[] { col.PropertyType });
+
+            var ilOfGetId = methodGetField.GetILGenerator();
+            ilOfGetId.Emit(OpCodes.Ldarg_0);
+            ilOfGetId.Emit(OpCodes.Ldfld, field);
+            ilOfGetId.Emit(OpCodes.Ret);
+
+            var ilOfSetId = methodSetField.GetILGenerator();
+            ilOfSetId.Emit(OpCodes.Ldarg_0);
+            ilOfSetId.Emit(OpCodes.Ldarg_1);
+            ilOfSetId.Emit(OpCodes.Stfld, field);
+            ilOfSetId.Emit(OpCodes.Ret);
+
+            var propertyId = typeBuilder.DefineProperty(fieldName, PropertyAttributes.None, col.PropertyType, null);
+            propertyId.SetGetMethod(methodGetField);
+            propertyId.SetSetMethod(methodSetField);
+        }
+    }
+}
---
+++ test/UnitTest/Emit/TypeTest.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest.Emit
+{
+    public class TypeTest
+    {
+        [Fact]
+        public void CreateType_Ok()
+        {
+            var cols = new Foo[]
+            {
+                new("Id", typeof(int)),
+                new("Name", typeof(string))
+            };
+
+            // 创建动态类型基类是 DynamicObject
+            var instanceType = EmitHelper.CreateTypeByName("Test", cols, typeof(DynamicObject));
+            Assert.Equal(typeof(DynamicObject), instanceType.BaseType);
+
+            // 创建动态类型实例
+            var instance = Activator.CreateInstance(instanceType);
+            Assert.NotNull(instance);
+
+            var properties = instance.GetType().GetProperties().Select(p => p.Name);
+            Assert.True(cols.Select(c => c.FieldName).SequenceEqual(properties));
+        }
+
+        private class Foo : IEditorItem
+        {
+            public Foo(string fieldName, Type propertyType) => (FieldName, PropertyType) = (fieldName, propertyType);
+
+            public string FieldName { get; }
+
+            public Type PropertyType { get; }
+
+            public bool Editable { get; set; }
+
+            public bool Readonly { get; set; }
+
+            public bool SkipValidate { get; set; }
+
+            public string Text { get; set; }
+
+            public IEnumerable<SelectedItem> Data { get; set; }
+
+            public object Step { get; set; }
+
+            public int Rows { get; set; }
+
+            public RenderFragment<object> EditTemplate { get; set; }
+
+            public System.Type ComponentType { get; set; }
+
+            public IEnumerable<SelectedItem> Lookup { get; set; }
+
+            public string GetDisplayName() => Text ?? FieldName;
+
+            public string GetFieldName() => FieldName;
+        }
+    }
+}
