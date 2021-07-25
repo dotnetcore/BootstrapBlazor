53ecc3867d53ebaf6080d72dfdcde9addf1a2670
Argo <argo@163.com>
2021-07-25 16:19:36 +0000

    !1629 doc(#I42B4N): refactor introduction page * refactor: 增加 cs 文件 * doc: 更新资源文件内容

---
+++ src/BootstrapBlazor.Shared/Pages/Introduction.razor.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Shared.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Introduction : IAsyncDisposable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject]
+        [NotNull]
+        private IOptions<WebsiteOptions>? WebsiteOption { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Introduction>? Localizer { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("$.bb_open");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async ValueTask DisposeAsync()
+        {
+            await JSRuntime.InvokeVoidAsync("$.bb_open", "dispose");
+            GC.SuppressFinalize(this);
+        }
+    }
+}
