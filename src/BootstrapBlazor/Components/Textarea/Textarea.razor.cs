ec6218e8c1d4c042e1d81b70cebf3753380858b2
Argo <argo@163.com>
2021-08-22 03:34:36 +0000

    !1747 fix(#I46OBU): textarea element not fill the width in Dialog body * chore: bump version to 5.7.2 * chore: 打包脚本与样式 * style: 微调 inline 模式下 textarea 组件宽度 * feat: 重构 Textarea 组件结构

--- src/BootstrapBlazor/Components/Textarea/Textarea.razor.cs
+++ src/BootstrapBlazor/Components/Textarea/Textarea.razor.cs
@@ -5,14 +5,14 @@
 namespace BootstrapBlazor.Components
 {
     /// <summary>
-    /// Textarea 组件基类
+    /// 
     /// </summary>
-    public abstract class TextareaBase : ValidateBase<string?>
+    public partial class Textarea
     {
         /// <summary>
         /// 获得 class 样式集合
         /// </summary>
-        protected string? ClassName => CssBuilder.Default("form-control")
+        private string? ClassName => CssBuilder.Default("form-control")
             .AddClass(CssClass).AddClass(ValidCss)
             .Build();
     }
