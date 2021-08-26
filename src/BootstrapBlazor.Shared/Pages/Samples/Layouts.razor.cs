5004c525b2b4ca08a6f20fcb7007f830d952e226
Nine <948825551@qq.com>
2021-08-26 02:03:06 +0000

    !1760 doc(#I4793D): add english translation of layout page * fix: 修复layout资源文件 * Merge branch 'dev' of https://gitee.com/LongbowEnterprise/BootstrapBla… * Merge branch 'dev' of https://gitee.com/LongbowEnterprise/BootstrapBla… * Merge branch 'dev' of https://gitee.com/LongbowEnterprise/BootstrapBla… * doc: 添加 Layout 示例文档的资源文件

--- src/BootstrapBlazor.Shared/Pages/Samples/Layouts.razor.cs
+++ src/BootstrapBlazor.Shared/Pages/Samples/Layouts.razor.cs
@@ -22,6 +22,10 @@ namespace BootstrapBlazor.Shared.Pages
         [NotNull]
         private IStringLocalizer<Menus>? Localizer { get; set; }
 
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<Layouts>? LayoutsLocalizer { get; set; }
+
         private IEnumerable<MenuItem>? IconSideMenuItems { get; set; }
 
         /// <summary>
@@ -35,124 +39,124 @@ namespace BootstrapBlazor.Shared.Pages
             IconSideMenuItems = await MenusDataGerator.GetIconSideMenuItemsAsync(Localizer);
         }
 
-        private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+        private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
         {
             // TODO: 移动到数据库中
             new AttributeItem() {
                 Name = "Header",
-                Description = "页头组件模板",
+                Description = LayoutsLocalizer["Desc1"]!,
                 Type = "RenderFragment",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "Side",
-                Description = "侧边栏组件模板",
+                Description = LayoutsLocalizer["Desc2"]!,
                 Type = "RenderFragment",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "SideWidth",
-                Description = "侧边栏宽度，支持百分比，设置 0 时关闭宽度功能",
+                Description = LayoutsLocalizer["Desc3"]!,
                 Type = "string",
                 ValueList = " — ",
                 DefaultValue = "300px"
             },
             new AttributeItem() {
                 Name = "Main",
-                Description = "内容组件模板",
+                Description = LayoutsLocalizer["Desc4"]!,
                 Type = "RenderFragment",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "Footer",
-                Description = "页脚组件模板",
+                Description = LayoutsLocalizer["Desc2"]!,
                 Type = "RenderFragment",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "Menus",
-                Description = "整页面布局时侧边栏菜单数据集合",
+                Description = LayoutsLocalizer["Desc6"]!,
                 Type = "IEnumerable<MenuItem>",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "IsFullSide",
-                Description = "侧边栏是否占满整个左边",
+                Description = LayoutsLocalizer["Desc7"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "IsPage",
-                Description = "是否为整页面布局",
+                Description = LayoutsLocalizer["Desc8"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "IsFixedFooter",
-                Description = "是否固定 Footer 组件",
+                Description = LayoutsLocalizer["Desc9"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "IsFixedHeader",
-                Description = "是否固定 Header 组件",
+                Description = LayoutsLocalizer["Desc10"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "ShowCollapseBar",
-                Description = "是否显示收缩展开 Bar",
+                Description =  LayoutsLocalizer["Desc11"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "ShowFooter",
-                Description = "是否显示 Footer 模板",
+                Description =  LayoutsLocalizer["Desc12"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "ShowGotoTop",
-                Description = "是否显示返回顶端按钮",
+                Description =  LayoutsLocalizer["Desc13"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "UseTabSet",
-                Description = "是否开启多标签模式",
+                Description =  LayoutsLocalizer["Desc14"]!,
                 Type = "bool",
                 ValueList = "true|false",
                 DefaultValue = "false"
             },
             new AttributeItem() {
                 Name = "AdditionalAssemblies",
-                Description = "额外程序集合，传递给 Tab 组件使用",
+                Description =  LayoutsLocalizer["Desc15"]!,
                 Type = "IEnumerable<Assembly>",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "OnCollapsed",
-                Description = "收缩展开回调委托",
+                Description =  LayoutsLocalizer["Desc16"]!,
                 Type = "Func<bool, Task>",
                 ValueList = " — ",
                 DefaultValue = " — "
             },
             new AttributeItem() {
                 Name = "OnClickMenu",
-                Description = "点击菜单项时回调委托",
+                Description =  LayoutsLocalizer["Desc17"]!,
                 Type = "Func<bool, MenuItem>",
                 ValueList = " — ",
                 DefaultValue = " — "
