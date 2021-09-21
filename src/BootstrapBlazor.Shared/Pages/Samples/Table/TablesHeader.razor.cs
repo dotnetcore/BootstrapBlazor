04ce42d8b375fe7ded6cffa15e8af620ca0315f6
Argo <argo@163.com>
2021-09-21 16:07:42 +0000

    !1881 doc(#I4B7W9): update multiple header Sort/Filter sample code * chore: bump version to 5.11.2 * doc: 更新多表头排序筛选示例

--- src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesHeader.razor.cs
+++ src/BootstrapBlazor.Shared/Pages/Samples/Table/TablesHeader.razor.cs
@@ -2,11 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared.Pages.Components;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BootstrapBlazor.Shared.Pages.Table
 {
@@ -22,6 +27,8 @@ namespace BootstrapBlazor.Shared.Pages.Table
         [NotNull]
         private IStringLocalizer<Foo>? Localizer { get; set; }
 
+        private static readonly ConcurrentDictionary<Type, Func<IEnumerable<Foo>, string, SortOrder, IEnumerable<Foo>>> SortLambdaCache = new();
+
         /// <summary>
         /// OnInitialized 方法
         /// </summary>
@@ -30,6 +37,43 @@ namespace BootstrapBlazor.Shared.Pages.Table
             base.OnInitialized();
 
             Items = Foo.GenerateFoo(Localizer);
+        }
+
+        private Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+        {
+            IEnumerable<Foo> items = Items;
+
+            // 过滤
+            var isFiltered = false;
+            if (options.Filters.Any())
+            {
+                items = items.Where(options.Filters.GetFilterFunc<Foo>());
+                isFiltered = true;
+            }
+
+            // 排序
+            var isSorted = false;
+            if (!string.IsNullOrEmpty(options.SortName))
+            {
+                var invoker = SortLambdaCache.GetOrAdd(typeof(Foo), key => LambdaExtensions.GetSortLambda<Foo>().Compile());
+                items = invoker(items, options.SortName, options.SortOrder);
+                isSorted = true;
+            }
+
+            // 设置记录总数
+            var total = items.Count();
+
+            // 内存分页
+            items = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
+
+            return Task.FromResult(new QueryData<Foo>()
+            {
+                Items = items,
+                TotalCount = total,
+                IsSorted = isSorted,
+                IsFiltered = isFiltered,
+                IsSearch = true
+            });
         }
     }
 }
