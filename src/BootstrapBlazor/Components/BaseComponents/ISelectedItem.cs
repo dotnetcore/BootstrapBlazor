2f72a12ddc682a6c2d432698a89202117ad1474f
Argo <argo@163.com>
2021-10-09 05:35:22 +0000

    !1939 feat(#I4D34X): add ISelectedItem interface * refactor: 更新 ISelectedItem 接口

--- src/BootstrapBlazor/Components/BaseComponents/ISelectedItem.cs
+++ src/BootstrapBlazor/Components/BaseComponents/ISelectedItem.cs
@@ -5,43 +5,33 @@
 namespace BootstrapBlazor.Components
 {
     /// <summary>
-    /// 选项类
+    /// 
     /// </summary>
-    public class SelectedItem : ISelectedItem
+    public interface ISelectedItem
     {
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public SelectedItem() { }
-
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public SelectedItem(string value, string text) => (Value, Text) = (value, text);
-
         /// <summary>
         /// 获得/设置 显示名称
         /// </summary>
-        public string Text { get; set; } = "";
+        string Text { get; set; }
 
         /// <summary>
         /// 获得/设置 选项值
         /// </summary>
-        public string Value { get; set; } = "";
+        string Value { get; set; }
 
         /// <summary>
         /// 获得/设置 是否选中
         /// </summary>
-        public bool Active { get; set; }
+        bool Active { get; set; }
 
         /// <summary>
         /// 获得/设置 是否禁用
         /// </summary>
-        public bool IsDisabled { get; set; }
+        bool IsDisabled { get; set; }
 
         /// <summary>
         /// 获得/设置 分组名称
         /// </summary>
-        public string GroupName { get; set; } = "";
+        string GroupName { get; set; }
     }
 }
