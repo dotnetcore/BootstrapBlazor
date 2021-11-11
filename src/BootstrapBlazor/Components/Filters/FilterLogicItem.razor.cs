4219505814668446e8be978ae80fe89577f3826f
Nine <948825551@qq.com>
2021-11-11 04:43:22 +0000

    !2071 feat(#I4HTDJ): add a table component to filter the resource files for relationships in the pop-up window * refactor: 分离 cs 文件 * feat: 添加 Table 组件过滤弹窗中关系的资源文件

---
+++ src/BootstrapBlazor/Components/Filters/FilterLogicItem.razor.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class FilterLogicItem
+    {
+        private FilterLogic _value;
+        private FilterLogic Value
+        {
+            get
+            {
+                _value = Logic;
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                if (LogicChanged.HasDelegate) LogicChanged.InvokeAsync(value);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public FilterLogic Logic { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<FilterLogic> LogicChanged { get; set; }
+
+        private IEnumerable<SelectedItem>? Items { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IStringLocalizer<FilterLogicItem>? Localizer { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Items = new List<SelectedItem>()
+            {
+                new SelectedItem("And",Localizer["And"]?.Value ?? "And"),
+                new SelectedItem("Or",Localizer["Or"]?.Value ?? "Or")
+            };
+        }
+    }
+}
