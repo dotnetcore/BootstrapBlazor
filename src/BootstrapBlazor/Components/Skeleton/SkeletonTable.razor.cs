74803146e52beb0c0c74d28cf60a4495f21fa9aa
Argo <argo@163.com>
2021-11-14 07:47:37 +0000

    !2087 feat(#I4I5JA): update style for Table Skeleton component * feat: 更新表格骨架屏

---
+++ src/BootstrapBlazor/Components/Skeleton/SkeletonTable.razor.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class SkeletonTable
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public int Rows { get; set; } = 7;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public int Columns { get; set; } = 3;
+    }
+}
