07dd1afecea71eaf629ce1a1e81c8c184ca34266
Nine <948825551@qq.com>
2021-11-26 03:08:30 +0000

    !2135 test(#I4JXA2): add a badge component unit test * Merge remote-tracking branch 'gitee.com/main' into dev-test-badge * test: 添加 test 组件单元测试

---
+++ test/UnitTest/Components/BadgeTest.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components
+{
+    public class BadgeTest : TestBase
+    {
+        [Fact]
+        public void Color_Ok()
+        {
+            var cut = Context.RenderComponent<Badge>(new ComponentParameter[]
+            {
+                ComponentParameter.CreateParameter(nameof(Badge.Color),Color.Primary)
+            });
+
+            Assert.Contains("primary", cut.Markup);
+        }
+
+        [Fact]
+        public void IsPill_Ok()
+        {
+            var cut = Context.RenderComponent<Badge>(new ComponentParameter[]
+            {
+                ComponentParameter.CreateParameter(nameof(Badge.IsPill),true)
+            });
+
+            Assert.Contains("rounded-pill", cut.Markup);
+        }
+
+        [Fact]
+        public void ChildContent_Ok()
+        {
+            var cut = Context.RenderComponent<Badge>(new ComponentParameter[]
+            {
+                ComponentParameter.CreateParameter(nameof(Badge.ChildContent),CreateComponent())
+            });
+
+            Assert.Contains("badge", cut.Markup);
+        }
+
+        private static RenderFragment CreateComponent() => builder =>
+        {
+            builder.OpenElement(1, "div");
+            builder.AddContent(2, "badge");
+            builder.CloseComponent();
+        };
+    }
+}
