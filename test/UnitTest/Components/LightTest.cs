86e8ebead4b1b534cd365964533ce2445d8b1a31
Nine <948825551@qq.com>
2021-11-27 11:37:24 +0000

    !2144 test(#I4K3PL): add alert component unit tests * Merge remote-tracking branch 'gitee.com/main' into dev-test-light * test: 添加 Light 组件单元测试

---
+++ test/UnitTest/Components/LightTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components
+{
+    public class LightTest : TestBase
+    {
+        [Fact]
+        public void IsFlash_Ok()
+        {
+            var cut = Context.RenderComponent<Light>(builder => builder.Add(s => s.IsFlash, true));
+
+            Assert.Contains("flash", cut.Markup);
+        }
+
+        [Fact]
+        public void Title_Ok()
+        {
+            var cut = Context.RenderComponent<Light>(builder => builder.Add(s => s.Title, "I am Light"));
+
+            Assert.Contains("I am Light", cut.Markup);
+        }
+
+        [Fact]
+        public void Color_Ok()
+        {
+            var cut = Context.RenderComponent<Light>(builder => builder.Add(s => s.Color, Color.Success));
+
+            Assert.Contains("light-success", cut.Markup);
+        }
+    }
+}
