b45f3f0fbe2a31f6e633384e5bc552c0cbd6d37b
Nine <948825551@qq.com>
2021-11-27 11:18:22 +0000

    !2143 add spinner component unit tests * Merge remote-tracking branch 'gitee.com/main' into dev-test-spinner * test: 添加 Spinner 组件单元测试

---
+++ test/UnitTest/Components/SpinnerTest.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components
+{
+    public class SpinnerTest : TestBase
+    {
+        [Fact]
+        public void Color_Ok()
+        {
+            var cut = Context.RenderComponent<Spinner>(builder => builder.Add(s => s.Color, Color.Primary));
+
+            Assert.Contains("text-primary", cut.Markup);
+        }
+
+        [Fact]
+        public void Size_Ok()
+        {
+            var cut = Context.RenderComponent<Spinner>(builder => builder.Add(s => s.Size, Size.Small));
+
+            Assert.Contains("spinner-border-sm", cut.Markup);
+        }
+
+        [Fact]
+        public void SpinnerType_Ok()
+        {
+            var cut = Context.RenderComponent<Spinner>(builder => builder.Add(s => s.SpinnerType, SpinnerType.Grow));
+
+            Assert.Contains("spinner-grow", cut.Markup);
+        }
+    }
+}
