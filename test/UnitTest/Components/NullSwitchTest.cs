9bc75795815f723d3d0f453fc95e46d200467a7e
Argo <argo@163.com>
2021-12-08 17:27:03 +0000

    !2197 test(#I4LPGB):  add NullSwitch unit test * Merge branch 'main' into dev-test-nullswitch * test: 补充单元测试 * test: 补充单元测试 * test: 添加 NullSwitch 组件单元测试

---
+++ test/UnitTest/Components/NullSwitchTest.cs
@@ -0,0 +1,230 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components
+{
+    public class NullSwitchTest : BootstrapBlazorTestBase
+    {
+        [Fact]
+        public void OnColor_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, true);
+                builder.Add(a => a.OnColor, Color.Success);
+            });
+
+            Assert.Contains("bg-success", cut.Markup);
+        }
+
+        [Fact]
+        public void OffColor_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.OffColor, Color.Danger);
+            });
+
+            Assert.Contains("bg-danger", cut.Markup);
+        }
+
+        [Fact]
+        public void Width_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.Width, 100);
+            });
+
+            Assert.Contains("width: 100px;", cut.Markup);
+        }
+
+        [Fact]
+        public void Height_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.Height, 20);
+            });
+
+            Assert.Contains("height: 20px;", cut.Markup);
+        }
+
+        [Fact]
+        public void OnInnerText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, true);
+                builder.Add(a => a.OnInnerText, "On");
+                builder.Add(a => a.ShowInnerText, true);
+            });
+
+            Assert.Contains("On", cut.Markup);
+        }
+
+        [Fact]
+        public void OffInnerText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.OffInnerText, "Off");
+                builder.Add(a => a.ShowInnerText, true);
+            });
+
+            Assert.Contains("Off", cut.Markup);
+        }
+
+
+        [Fact]
+        public void ShowInnerText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.OffInnerText, "Off");
+                builder.Add(a => a.ShowInnerText, true);
+            });
+
+            var text = cut.Find("span").GetAttribute("data-inner-text");
+
+            Assert.Equal("Off", text);
+        }
+
+        [Fact]
+        public void OnValueChanged_Ok()
+        {
+            bool? value = false;
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.OnValueChanged, e => { value = e; return Task.CompletedTask; });
+            });
+
+            cut.Find("span").Click();
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void ValueChanged_Ok()
+        {
+            bool? value = false;
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, false);
+                builder.Add(a => a.ValueChanged, EventCallback.Factory.Create<bool?>(this, e => { value = e; }));
+            });
+
+            cut.Find("span").Click();
+
+            Assert.True(value);
+        }
+
+        [Fact]
+        public void IsDisabled_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.IsDisabled, true);
+            });
+
+            Assert.Contains("disable", cut.Markup);
+        }
+
+        [Fact]
+        public void Value_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.Value, true);
+            });
+
+            Assert.Contains("is-checked", cut.Markup);
+        }
+
+        [Fact]
+        public void DisplayText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.DisplayText, "custome label");
+                builder.Add(a => a.ShowLabel, true);
+                builder.Add(a => a.Value, true);
+            });
+
+            var text = cut.Find("label").TextContent;
+
+            Assert.Equal("custome label", text);
+        }
+
+        [Fact]
+        public void ShowLabel_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.DisplayText, "custome label");
+                builder.Add(a => a.ShowLabel, true);
+                builder.Add(a => a.Value, true);
+            });
+
+            var label = cut.Find("label");
+
+            Assert.NotNull(label);
+        }
+
+        [Fact]
+        public void DefaultValueWhenNull_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.DefaultValueWhenNull, false);
+                builder.Add(a => a.Value, null);
+            });
+
+            cut.Find("span").Click();
+
+            Assert.True(cut.Instance.Value);
+        }
+
+        [Fact]
+        public void OnText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.OnText, "On");
+                builder.Add(a => a.Value, true);
+            });
+
+            Assert.Equal("On", cut.Find(".switch-label").TextContent);
+        }
+
+        [Fact]
+        public void OffText_Ok()
+        {
+            var cut = Context.RenderComponent<NullSwitch>(builder =>
+            {
+                builder.Add(a => a.OffText, "Off");
+                builder.Add(a => a.Value, false);
+            });
+
+            Assert.Equal("Off", cut.Find(".switch-label").TextContent);
+        }
+    }
+}
