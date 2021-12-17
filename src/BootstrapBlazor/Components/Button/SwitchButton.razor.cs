16c351e8662024cbd9bfba0c0b577b0877ca19a4
Argo <argo@163.com>
2021-12-17 05:06:34 +0000

    !2222 feat(#I4MZDX): add SwitchButton component * feat: 增加 SwitchButton 组件

---
+++ src/BootstrapBlazor/Components/Button/SwitchButton.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class SwitchButton
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? OnText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public string? OffText { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public bool ToggleState { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<bool> ToggleStateChanged { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClick { get; set; }
+
+        private async Task Toggle()
+        {
+            ToggleState = !ToggleState;
+            if (ToggleStateChanged.HasDelegate)
+            {
+                await ToggleStateChanged.InvokeAsync(ToggleState);
+            }
+            if (OnClick.HasDelegate)
+            {
+                await OnClick.InvokeAsync();
+            }
+        }
+
+        private string? GetText() => ToggleState ? OnText : OffText;
+    }
+}
