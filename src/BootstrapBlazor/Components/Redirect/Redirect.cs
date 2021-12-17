e87adbfd17e49bd7f3afe43041c30f6960617a58
Argo <argo@163.com>
2021-12-17 04:45:21 +0000

    !2221 feat(#I4MZCQ): add Redirect component * refactor: 更新组件名称 * feat: 增加 RedirectTo 组件

---
+++ src/BootstrapBlazor/Components/Redirect/Redirect.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Redirect : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        private NavigationManager? Navigation { get; set; }
+
+        [Parameter]
+        public string Url { get; set; } = "Account/Login";
+
+#if DEBUG
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        protected override void OnAfterRender(bool firstRender)
+        {
+            Navigation.NavigateTo(Url, true);
+        }
+#else
+        protected override void OnInitialized()
+        {
+            Navigation.NavigateTo(Url, true);
+        }
+#endif
+    }
+}
