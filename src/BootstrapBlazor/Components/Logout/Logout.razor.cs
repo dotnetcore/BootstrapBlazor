211ada64867c8c909c24c6a5c7ba3620439b3b7c
Argo <argo@163.com>
2021-12-22 04:51:48 +0000

    !2242 feat(#I4NMID): add Logout component * chore: bump version beta12 * feat: 增加多语言支持 * style: 增加 Logout 组件样式 * feat: 增加 Logout 组件

---
+++ src/BootstrapBlazor/Components/Logout/Logout.razor.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ListView 组件基类
+/// </summary>
+public partial class Logout
+{
+    private string? DropdownClassString => CssBuilder.Default("dropdown-menu dropdown-menu-right shadow")
+        .AddClass("show", IsShow)
+        .Build();
+
+    private bool IsShow { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? ImageUrl { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? DisplayName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? PrefixDisplayNameText { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? UserName { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public string? PrefixUserNameText { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public RenderFragment? HeaderTemplate { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public RenderFragment? LinkTemplate { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Logout>? Localizer { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        PrefixDisplayNameText ??= Localizer[nameof(PrefixDisplayNameText)];
+        PrefixUserNameText ??= Localizer[nameof(PrefixUserNameText)];
+    }
+}
