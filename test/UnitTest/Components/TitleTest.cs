1be0a04934fba43a6e52897fb567cb6d0ed75652
Argo <argo@163.com>
2021-12-23 15:58:25 +0000

    !2247 test(#I4NW9R): add TitleService unit test * test: 增加  TitleService 单元测试 * refactor: 更新代码移除 set  属性

---
+++ test/UnitTest/Components/TitleTest.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Bunit;
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics.CodeAnalysis;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components;
+
+public class TitleTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void ChildContent_Ok()
+    {
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<MockTitleTest>();
+        });
+        var titleService = cut.FindComponent<MockTitleTest>().Instance.TitleService;
+        cut.InvokeAsync(async () => await titleService.SetTitle("test"));
+    }
+
+    private class MockTitleTest : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        public TitleService? TitleService { get; set; }
+    }
+}
