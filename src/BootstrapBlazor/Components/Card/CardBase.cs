0243feaf105294315e0a807a4459ad47387c8140
Argo <argo@163.com>
2021-12-26 05:01:49 +0000

    !2254 feat(#I4O3RS): add IsCollapsible on Card * feat: Card 组件增加 IsCollapsible 参数

--- src/BootstrapBlazor/Components/Card/CardBase.cs
+++ src/BootstrapBlazor/Components/Card/CardBase.cs
@@ -3,73 +3,101 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
 
-namespace BootstrapBlazor.Components
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Card组件基类
+/// </summary>
+public abstract class CardBase : BootstrapComponentBase
 {
     /// <summary>
-    /// Card组件基类
+    /// Card 组件样式
     /// </summary>
-    public abstract class CardBase : BootstrapComponentBase
+    protected virtual string? ClassName => CssBuilder.Default("card")
+        .AddClass("text-center", IsCenter)
+        .AddClass($"border-{Color.ToDescriptionString()}", Color != Color.None)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    ///  设置 Body Class 组件样式
+    /// </summary>
+    protected virtual string? BodyClassName => CssBuilder.Default("card-body")
+        .AddClass($"text-{Color.ToDescriptionString()}", Color != Color.None)
+        .Build();
+
+    /// <summary>
+    /// 设置 Footer Class 样式
+    /// </summary>
+    protected virtual string? FooterClassName => CssBuilder.Default("card-footer")
+        .AddClass("text-muted", IsCenter)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 CardHeader 显示文本
+    /// </summary>
+    [Parameter]
+    public string? HeaderText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 CardHeard 模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? CardHeader { get; set; }
+
+    /// <summary>
+    /// 获得/设置 CardBody 模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? CardBody { get; set; }
+
+    /// <summary>
+    /// 获得/设置 CardFooter 模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? CardFooter { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Card 颜色
+    /// </summary>
+    [Parameter]
+    public Color Color { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否居中 默认 false
+    /// </summary>
+    [Parameter]
+    public bool IsCenter { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否可收缩 默认 false
+    /// </summary>
+    [Parameter]
+    public bool IsCollapsible { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    protected ElementReference CardEelement { get; set; }
+
+    /// <summary>
+    /// OnAfterRenderAsync 方法
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
     {
-        /// <summary>
-        ///  Card组件样式
-        /// </summary>
-        protected virtual string? ClassName => CssBuilder.Default("card")
-            .AddClass("text-center", IsCenter)
-            .AddClass($"border-{Color.ToDescriptionString()}", Color != Color.None)
-            .AddClass(Class)
-            .Build();
+        await base.OnAfterRenderAsync(firstRender);
 
-        /// <summary>
-        ///  设置Body Class组件样式
-        /// </summary>
-        protected virtual string? BodyClassName => CssBuilder.Default("card-body")
-            .AddClass($"text-{Color.ToDescriptionString()}", Color != Color.None)
-            .AddClass(Class)
-            .Build();
-
-        /// <summary>
-        /// 设置Footer Class样式
-        /// </summary>
-        protected virtual string? FooterClassName => CssBuilder.Default("card-footer")
-            .AddClass("text-muted", IsCenter)
-            .Build();
-
-        /// <summary>
-        /// 设置Class样式
-        /// </summary>
-        [Parameter]
-        public string? Class { get; set; }
-
-
-        /// <summary>
-        /// 获得/设置 CardHeard
-        /// </summary>
-        [Parameter]
-        public RenderFragment? CardHeader { get; set; }
-
-        /// <summary>
-        /// 获得/设置 CardBody
-        /// </summary>
-        [Parameter]
-        public RenderFragment? CardBody { get; set; }
-
-        /// <summary>
-        /// 获得/设置 CardFooter
-        /// </summary>
-        [Parameter]
-        public RenderFragment? CardFooter { get; set; }
-
-        /// <summary>
-        /// 获得/设置Card颜色
-        /// </summary>
-        [Parameter]
-        public Color Color { get; set; }
-
-        /// <summary>
-        /// 设置是否居中
-        /// </summary>
-        [Parameter]
-        public bool IsCenter { get; set; }
+        if (firstRender)
+        {
+            if (IsCollapsible)
+            {
+                await JSRuntime.InvokeVoidAsync(CardEelement, "bb_card_collapse");
+            }
+        }
     }
 }
