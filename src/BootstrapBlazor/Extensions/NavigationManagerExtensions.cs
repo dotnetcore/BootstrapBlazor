d9c5f0cfbec28e889f99c2a62c0ef2b7f784da3b
Argo <argo@163.com>
2021-12-29 04:44:06 +0000

    !2263 refactor(#I4OJRD): redesign Tab component * doc: 格式化文档 * refactor: 更新 TabLink 内部逻辑

--- src/BootstrapBlazor/Extensions/NavigationManagerExtensions.cs
+++ src/BootstrapBlazor/Extensions/NavigationManagerExtensions.cs
@@ -6,30 +6,29 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace BootstrapBlazor.Components
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NavigationManager 扩展方法
+/// </summary>
+public static class NavigationManagerExtensions
 {
     /// <summary>
-    /// 
+    /// 导航并添加 TabItem 方法
     /// </summary>
-    public static class NavigationManagerExtensions
+    /// <param name="navigation"></param>
+    /// <param name="provider"></param>
+    /// <param name="url"></param>
+    /// <param name="text"></param>
+    /// <param name="icon"></param>
+    /// <param name="closable"></param>
+    public static void NavigateTo(this NavigationManager navigation, IServiceProvider provider, string url, string text, string? icon = null, bool? closable = null)
     {
-        /// <summary>
-        /// 导航并添加 TabItem 方法
-        /// </summary>
-        /// <param name="navigation"></param>
-        /// <param name="provider"></param>
-        /// <param name="url"></param>
-        /// <param name="text"></param>
-        /// <param name="icon"></param>
-        /// <param name="closable"></param>
-        public static void NavigateTo(this NavigationManager navigation, IServiceProvider provider, string url, string text, string? icon = null, bool? closable = null)
-        {
-            var option = provider.GetRequiredService<TabItemTextOptions>();
-            option.Text = text;
-            option.Icon = icon;
-            option.IsActive = true;
-            option.Closable = closable ?? true;
-            navigation.NavigateTo(url);
-        }
+        var option = provider.GetRequiredService<TabItemTextOptions>();
+        option.Text = text;
+        option.Icon = icon;
+        option.IsActive = true;
+        option.Closable = closable ?? true;
+        navigation.NavigateTo(url);
     }
 }
