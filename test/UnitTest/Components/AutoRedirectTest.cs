e13d8b45081c0af70ea4486a03cac8ce4ab5bec4
Argo <argo@163.com>
2022-01-16 04:12:58 +0000

    !2323 test(#I4QZC6): add AutoRedirect unit test * test: 增加 AutoRedirect 单元测试 * doc: 微调 timeline 示例样式 * doc: 代码格式化

---
+++ test/UnitTest/Components/AutoRedirectTest.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using UnitTest.Core;
+using Xunit;
+
+namespace UnitTest.Components;
+
+public class AutoRedirectTest : TestBase
+{
+    [Fact]
+    public void LogoutUrl_Ok()
+    {
+        var cut = Context.RenderComponent<AutoRedirect>(pb =>
+        {
+            pb.Add(a => a.LogoutUrl, "/Account/Logout");
+        });
+
+        Assert.Equal("/Account/Logout", cut.Instance.LogoutUrl);
+    }
+
+    [Fact]
+    public void Interval_Ok()
+    {
+        var cut = Context.RenderComponent<AutoRedirect>(pb =>
+        {
+            pb.Add(a => a.Interval, 50);
+        });
+
+        Assert.Equal(50, cut.Instance.Interval);
+    }
+
+    [Fact]
+    public void Lock_Ok()
+    {
+        var cut = Context.RenderComponent<AutoRedirect>(pb =>
+        {
+            pb.Add(a => a.LogoutUrl, "/Account/Logout");
+        });
+        cut.Instance.Lock();
+    }
+}
