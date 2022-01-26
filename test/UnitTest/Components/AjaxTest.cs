ba687fb0db9f0521418cc8698f414457c92ff770
Argo <argo@163.com>
2022-01-26 08:14:28 +0000

    !2360 test(#I4SAKJ): add unit test for ajax * test: 增加 Ajax 单元测试

---
+++ test/UnitTest/Components/AjaxTest.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class AjaxTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Ajax_Test()
+    {
+        var option = new AjaxOption
+        {
+            Url = "/api/Login",
+            Method = "POST",
+            Data = new { UserName = "admin", Password = "1234567" }
+        };
+        var service = Context.Services.GetRequiredService<AjaxService>();
+        _ = service.GetMessage(option);
+
+        _ = Context.RenderComponent<Ajax>();
+        _ = service.GetMessage(option);
+    }
+}
