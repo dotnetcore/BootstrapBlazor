dc84f9c3fe40b8ebd5beb8a490244f559d5d0cb9
Argo <argo@163.com>
2022-01-27 04:40:39 +0000

    !2363 doc(#I4SDE0): update sample IsShadow/IsCollapsible for Card component * doc: 更新示例 * doc: 更新资源文件

--- src/BootstrapBlazor.Shared/Samples/Cards.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Cards.razor.cs
@@ -18,62 +18,67 @@ public sealed partial class Cards
     /// <returns></returns>
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-            new AttributeItem() {
-                Name = "CardBody",
-                Description = Localizer["CardBody"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "CardFooter",
-                Description = Localizer["CardFooter"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "CardHeader",
-                Description = Localizer["CardHeader"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Class",
-                Description = Localizer["Class"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem{
-                Name = "Color",
-                Description = Localizer["Color"],
-                Type = "Color",
-                ValueList = "None / Primary / Secondary / Success / Danger / Warning / Info / Light / Dark",
-                DefaultValue = " — "
-            },
-            new AttributeItem{
-                Name = "IsCenter",
-                Description = Localizer["IsCenter"],
-                Type = "boolean",
-                ValueList = "true / false",
-                DefaultValue = "false"
-            },
-            new AttributeItem{
-                Name = "IsCollapsible",
-                Description = Localizer["IsCollapsible"],
-                Type = "boolean",
-                ValueList = "true / false",
-                DefaultValue = "false"
-            },
-            new AttributeItem{
-                Name = "IsShadow",
-                Description = Localizer["IsShadow"],
-                Type = "boolean",
-                ValueList = "true / false",
-                DefaultValue = "false"
-            },
+        new AttributeItem
+        {
+            Name = "CardBody",
+            Description = Localizer["CardBody"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "CardFooter",
+            Description = Localizer["CardFooter"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem
+        {
+            Name = "CardHeader",
+            Description = Localizer["CardHeader"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem
+        {
+            Name = "Class",
+            Description = Localizer["Class"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem {
+            Name = "Color",
+            Description = Localizer["Color"],
+            Type = "Color",
+            ValueList = "None / Primary / Secondary / Success / Danger / Warning / Info / Light / Dark",
+            DefaultValue = " — "
+        },
+        new AttributeItem {
+            Name = "IsCenter",
+            Description = Localizer["IsCenter"],
+            Type = "boolean",
+            ValueList = "true / false",
+            DefaultValue = "false"
+        },
+        new AttributeItem
+        {
+            Name = "IsCollapsible",
+            Description = Localizer["IsCollapsible"],
+            Type = "boolean",
+            ValueList = "true / false",
+            DefaultValue = "false"
+        },
+        new AttributeItem
+        {
+            Name = "IsShadow",
+            Description = Localizer["IsShadow"],
+            Type = "boolean",
+            ValueList = "true / false",
+            DefaultValue = "false"
+        }
     };
 }
 
