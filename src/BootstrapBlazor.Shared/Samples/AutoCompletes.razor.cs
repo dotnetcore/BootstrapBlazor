abbdc4b931d9ede619507894d19be05ef6a7b2a5
Argo <argo@163.com>
2022-02-01 06:30:06 +0000

    !2375 doc(#I4SKWG): update AutoComplete sample code * doc: 更新 AutoComplete 示例文档 * doc: 更新 AutoCompletes 组件资源文件

--- src/BootstrapBlazor.Shared/Samples/AutoCompletes.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/AutoCompletes.razor.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using BootstrapBlazor.Components;
 using BootstrapBlazor.Shared.Common;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -37,79 +38,95 @@ public sealed partial class AutoCompletes
     /// <returns></returns>
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-            // TODO: 移动到数据库中
-            new AttributeItem() {
-                Name = "ShowLabel",
-                Description = Localizer["Att1"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "true"
-            },
-            new AttributeItem() {
-                Name = "ChildContent",
-                Description = Localizer["Att2"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Items",
-                Description = Localizer["Att3"],
-                Type = "IEnumerable<string>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "NoDataTip",
-                Description = Localizer["Att4"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = Localizer["Att4DefaultValue"]!
-            },
-            new AttributeItem() {
-                Name = "DisplayCount",
-                Description = Localizer["Att5"],
-                Type = "int?",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "ValueChanged",
-                Description = Localizer["Att6"],
-                Type = "Action<string>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "IsLikeMatch",
-                Description = Localizer["Att7"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem()
-            {
-                Name = "IgnoreCase",
-                Description = Localizer["Att8"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "true"
-            },
-            new AttributeItem()
-            {
-                Name = "CustomFilter",
-                Description = Localizer["Att9"],
-                Type = "Func<Task<IEnumerable<string>>>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem()
-            {
-                Name = "Debounce",
-                Description = Localizer["Att10"],
-                Type = "int",
-                ValueList = " — ",
-                DefaultValue = "0"
-            }
+        // TODO: 移动到数据库中
+        new AttributeItem() {
+            Name = "ShowLabel",
+            Description = Localizer["Att1"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new AttributeItem() {
+            Name = "ChildContent",
+            Description = Localizer["Att2"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "Items",
+            Description = Localizer["Att3"],
+            Type = "IEnumerable<string>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "NoDataTip",
+            Description = Localizer["Att4"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = Localizer["Att4DefaultValue"]!
+        },
+        new AttributeItem() {
+            Name = "DisplayCount",
+            Description = Localizer["Att5"],
+            Type = "int?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "ValueChanged",
+            Description = Localizer["Att6"],
+            Type = "Action<string>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "IsLikeMatch",
+            Description = Localizer["Att7"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem()
+        {
+            Name = "IgnoreCase",
+            Description = Localizer["Att8"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new AttributeItem()
+        {
+            Name = "CustomFilter",
+            Description = Localizer["Att9"],
+            Type = "Func<Task<IEnumerable<string>>>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem()
+        {
+            Name = "Debounce",
+            Description = Localizer["Att10"],
+            Type = "int",
+            ValueList = " — ",
+            DefaultValue = "0"
+        },
+        new AttributeItem()
+        {
+            Name = nameof(AutoComplete.SkipEnter),
+            Description = Localizer[nameof(AutoComplete.SkipEnter)],
+            Type = "bool",
+            ValueList = "true/false",
+            DefaultValue = "false"
+        },
+        new AttributeItem()
+        {
+            Name = nameof(AutoComplete.SkipEsc),
+            Description = Localizer[nameof(AutoComplete.SkipEsc)],
+            Type = "bool",
+            ValueList = "true/false",
+            DefaultValue = "false"
+        }
     };
 }
