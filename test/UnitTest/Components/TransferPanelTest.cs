44594c9975d46b99a88a805c81e2005ea4cd5281
Argo <argo@163.com>
2022-02-19 11:29:23 +0000

    !2443 test(#I4UDO7): add TransferPanel unit test * test: 增加 TransferPanel 单元测试 * refactor: 删除代码

---
+++ test/UnitTest/Components/TransferPanelTest.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class TransferPanelTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void HeaderCheckState_Ok()
+    {
+        var cut = Context.RenderComponent<TransferPanel>();
+
+        // Items 为空时全选 Checkbox
+        var checkbox = cut.FindComponent<Checkbox<SelectedItem>>();
+        cut.InvokeAsync(() => checkbox.Instance.SetState(CheckboxState.UnChecked));
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Items, new List<SelectedItem>
+            {
+                new("1", "Test1"),
+                new("2", "Test2")
+            });
+        });
+        checkbox = cut.FindComponent<Checkbox<SelectedItem>>();
+        cut.InvokeAsync(() => checkbox.Instance.SetState(CheckboxState.Checked));
+        cut.InvokeAsync(() => checkbox.Instance.SetState(CheckboxState.UnChecked));
+
+        // 显示 Search
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.ShowSearch, true);
+        });
+        var input = cut.Find(".input-inner");
+        input.Input(new ChangeEventArgs()
+        {
+            Value = "1"
+        });
+
+        // Items 为空时全选 Checkbox
+        checkbox = cut.FindComponent<Checkbox<SelectedItem>>();
+        cut.InvokeAsync(() => checkbox.Instance.SetState(CheckboxState.Checked));
+
+        input.KeyUp("Escape");
+    }
+
+    [Fact]
+    public void GetShownItems_Ok()
+    {
+        var cut = Context.RenderComponent<TransferPanel>(pb =>
+        {
+            pb.Add(a => a.ShowSearch, true);
+        });
+
+        var input = cut.Find(".input-inner");
+        input.Input(new ChangeEventArgs()
+        {
+            Value = "No"
+        });
+
+        // Items 为空时全选 Checkbox
+        var checkbox = cut.FindComponent<Checkbox<SelectedItem>>();
+        cut.InvokeAsync(() => checkbox.Instance.SetState(CheckboxState.Checked));
+
+        // 未设置 Items 显示 Search
+    }
+
+    [Fact]
+    public void IsDisabled_Ok()
+    {
+        var cut = Context.RenderComponent<TransferPanel>(pb =>
+        {
+            pb.Add(a => a.ShowSearch, true);
+            pb.Add(a => a.IsDisabled, true);
+        });
+        cut.Contains("disabled=\"disabled\"");
+    }
+}
