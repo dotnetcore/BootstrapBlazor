628820ac0275b035a0e7732992fe70570ea25b6e
Argo <argo@163.com>
2022-02-20 07:48:32 +0000

    !2446 test(#I4UF4J): add unit test for AvatarUpload * test: 增加 AvatarUpload 单元测试

--- test/UnitTest/Components/UploadTest.cs
+++ test/UnitTest/Components/UploadTest.cs
@@ -112,6 +112,112 @@ public class UploadTest : BootstrapBlazorTestBase
         Assert.False(invalid);
     }
 
+    [Fact]
+    public void AvatarUpload_Ok()
+    {
+        UploadFile? uploadFile = null;
+        var cut = Context.RenderComponent<AvatarUpload<string>>(pb =>
+        {
+            pb.Add(a => a.IsSingle, true);
+            pb.Add(a => a.OnChange, file =>
+            {
+                uploadFile = file;
+                return Task.CompletedTask;
+            });
+        });
+        var input = cut.FindComponent<InputFile>();
+        cut.InvokeAsync(() => input.Instance.OnChange.InvokeAsync(new InputFileChangeEventArgs(new List<MockBrowserFile>()
+        {
+            new MockBrowserFile()
+        })));
+
+        // Height/Width
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Height, 40);
+            pb.Add(a => a.Width, 50);
+        });
+        cut.Contains("width: 50px;");
+        cut.Contains("height: 40px;");
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.IsCircle, true);
+        });
+        cut.Contains("height: 50px;");
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Height, 0);
+        });
+        cut.Contains("height: 50px;");
+
+        // DefaultFileList
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.OnChange, null);
+            pb.Add(a => a.ShowProgress, true);
+            pb.Add(a => a.DefaultFileList, new List<UploadFile>()
+            {
+                new UploadFile() { FileName  = "Test-File" }
+            });
+        });
+        input = cut.FindComponent<InputFile>();
+        cut.InvokeAsync(() => input.Instance.OnChange.InvokeAsync(new InputFileChangeEventArgs(new List<MockBrowserFile>()
+        {
+            new MockBrowserFile()
+        })));
+
+        // upload-item-delete
+        var button = cut.Find(".upload-item-delete");
+        cut.InvokeAsync(() => button.Click());
+
+        // isdisable
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.IsDisabled, true);
+        });
+    }
+
+    [Fact]
+    public void AvatarUpload_ValidateForm_Ok()
+    {
+        var invalid = false;
+        var foo = new Foo();
+        var cut = Context.RenderComponent<ValidateForm>(pb =>
+        {
+            pb.Add(a => a.Model, foo);
+            pb.AddChildContent<AvatarUpload<string>>(pb =>
+            {
+                pb.Add(a => a.Value, foo.Name);
+                pb.Add(a => a.ValueExpression, foo.GenerateValueExpression());
+            });
+            pb.Add(a => a.OnValidSubmit, context =>
+            {
+                invalid = false;
+                return Task.CompletedTask;
+            });
+            pb.Add(a => a.OnInvalidSubmit, context =>
+            {
+                invalid = true;
+                return Task.CompletedTask;
+            });
+        });
+
+        // 提交表单
+        var form = cut.Find("form");
+        cut.InvokeAsync(() => form.Submit());
+        Assert.True(invalid);
+
+        var input = cut.FindComponent<InputFile>();
+        cut.InvokeAsync(() => input.Instance.OnChange.InvokeAsync(new InputFileChangeEventArgs(new List<MockBrowserFile>()
+        {
+            new MockBrowserFile()
+        })));
+        cut.InvokeAsync(() => form.Submit());
+        Assert.False(invalid);
+    }
+
     [ExcludeFromCodeCoverage]
     private class MockBrowserFile : IBrowserFile
     {
