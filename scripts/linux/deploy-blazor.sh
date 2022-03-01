992122d6195af7cd32d3fe37d92cb5c388ece2a4
Argo <argo@163.com>
2022-03-01 05:30:24 +0000

    !2469 doc(#I4VQAV): update document for table search feature * doc: 更新文档修正自定义模型 * chore: 更新部署脚本增加 nopager 参数

--- scripts/linux/deploy-blazor.sh
+++ scripts/linux/deploy-blazor.sh
@@ -8,4 +8,4 @@ dotnet publish src/BootstrapBlazor.Server -c Release
 systemctl stop ba.blazor
 \cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net6.0/publish/* /usr/local/ba/blazor
 systemctl start ba.blazor
-systemctl status ba.blazor -l
+systemctl status ba.blazor -l --no-pager
