bbf5ac338c930f9f3a58f15d7bbc94d3d17a4e15
Argo <argo@163.com>
2022-03-05 06:34:25 +0000

    !2488 fix(#I4WG4N): not show filename when bind value type is string on InputUpload component * chore: bump version beta02 * fix: 修复 InputUpload 丢失文件名问题

--- src/BootstrapBlazor/Components/Upload/UploadBase.cs
+++ src/BootstrapBlazor/Components/Upload/UploadBase.cs
@@ -74,7 +74,7 @@ public abstract class UploadBase<TValue> : ValidateBase<TValue>, IUpload
     /// </summary>
     /// <param name="item"></param>
     /// <returns></returns>
-    protected string? GetFileName(UploadFile? item) => item?.OriginFileName ?? item?.FileName;
+    protected string? GetFileName(UploadFile? item) => item?.OriginFileName ?? item?.FileName ?? Value?.ToString();
 
     /// <summary>
     /// 显示/隐藏验证结果方法
