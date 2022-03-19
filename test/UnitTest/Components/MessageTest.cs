a66a6beb1e650f77592de0440f8e7807e6529134
Argo <argo@163.com>
2022-03-19 06:22:30 +0000

    !2548 test(#I4YL54): add unit test for message * feat: 增加 OnDismiss 回调委托 * refactor: 重构代码 * test: add OnDismiss test * test: add set placement test * test: add test for message

---
+++ test/UnitTest/Components/MessageTest.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class MessageTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public async Task Message_Ok()
+    {
+        var dismiss = false;
+        var service = Context.Services.GetRequiredService<MessageService>();
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Button>(pb =>
+            {
+                pb.Add(a => a.OnClick, async () =>
+                {
+                    await service.Show(new MessageOption()
+                    {
+                        Color = Color.Danger,
+                        Content = "Test Content",
+                        Delay = 4000,
+                        ForceDelay = false,
+                        Icon = "fa fa-fa",
+                        IsAutoHide = true,
+                        ShowBar = true,
+                        ShowDismiss = true,
+                        OnDismiss = () =>
+                        {
+                            dismiss = true;
+                            return Task.CompletedTask;
+                        }
+                    });
+                });
+            });
+        });
+        var btn = cut.Find("button");
+        await cut.InvokeAsync(() => btn.Click());
+
+        var btnClose = cut.Find(".btn-close");
+        await cut.InvokeAsync(() => btnClose.Click());
+        Assert.True(dismiss);
+
+        var message = cut.FindComponent<Message>();
+        await message.InvokeAsync(() => message.Instance.Clear());
+    }
+
+    [Fact]
+    public async Task SetPlacement_Ok()
+    {
+        var cut = Context.RenderComponent<Message>(pb =>
+        {
+            pb.Add(a => a.Placement, Placement.Left);
+        });
+
+        await cut.InvokeAsync(() => cut.Instance.SetPlacement(Placement.Top));
+        Assert.Equal(Placement.Top, cut.Instance.Placement);
+    }
+}
