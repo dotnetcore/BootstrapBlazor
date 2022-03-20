335a49918fd0b888344d69ffb4a9ef9d0c9e41bd
Argo <argo@163.com>
2022-03-20 07:28:13 +0000

    !2551 test(#I4YN9P): add IpAddress unit test * test: 增加表单内使用单元测试 * test: 增加超范围值单元测试 * test: 增加 Null 值单元测试 * test: 增加默认设置单元测试 * test: 增加 IpAddress 单元测试

---
+++ test/UnitTest/Components/IpAddressTest.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared;
+
+namespace UnitTest.Components;
+
+public class IpAddressTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public async Task IpAddress_Ok()
+    {
+        var cut = Context.RenderComponent<IpAddress>();
+        cut.Contains("ipaddress form-control");
+        Assert.Equal("0.0.0.0", cut.Instance.Value);
+
+        var inputs = cut.FindAll(".ipv4-cell");
+        await cut.InvokeAsync(() => inputs[0].Change("1"));
+        await cut.InvokeAsync(() => inputs[1].Change("1"));
+        await cut.InvokeAsync(() => inputs[2].Change("1"));
+        await cut.InvokeAsync(() => inputs[3].Change("1"));
+        Assert.Equal("1.1.1.1", cut.Instance.Value);
+    }
+
+    [Fact]
+    public async Task IpAddress_Null()
+    {
+        var cut = Context.RenderComponent<IpAddress>();
+        var inputs = cut.FindAll(".ipv4-cell");
+        await cut.InvokeAsync(() => inputs[0].Change(new ChangeEventArgs() { Value = null }));
+        await cut.InvokeAsync(() => inputs[1].Change(new ChangeEventArgs() { Value = null }));
+        await cut.InvokeAsync(() => inputs[2].Change(new ChangeEventArgs() { Value = null }));
+        await cut.InvokeAsync(() => inputs[3].Change(new ChangeEventArgs() { Value = null }));
+        Assert.Equal("0.0.0.0", cut.Instance.Value);
+    }
+
+    [Fact]
+    public async Task IpAddress_Value()
+    {
+        var cut = Context.RenderComponent<IpAddress>();
+        var inputs = cut.FindAll(".ipv4-cell");
+        await cut.InvokeAsync(() => inputs[0].Change(new ChangeEventArgs() { Value = "1234" }));
+        await cut.InvokeAsync(() => inputs[1].Change(new ChangeEventArgs() { Value = "1234" }));
+        await cut.InvokeAsync(() => inputs[2].Change(new ChangeEventArgs() { Value = "1234" }));
+        await cut.InvokeAsync(() => inputs[3].Change(new ChangeEventArgs() { Value = "1234" }));
+        Assert.Equal("123.123.123.123", cut.Instance.Value);
+    }
+
+    [Fact]
+    public void ValidateForm_Ok()
+    {
+        var foo = new Foo() { Name = "1.1.1.1" };
+        var cut = Context.RenderComponent<ValidateForm>(pb =>
+        {
+            pb.Add(a => a.Model, foo);
+            pb.AddChildContent<IpAddress>(pb =>
+            {
+                pb.Add(a => a.Value, foo.Name);
+            });
+        });
+        cut.Contains("form-label");
+    }
+}
