3006fef7bb3ca7fd9a7ed0ffbaf70af6e456d501
Argo <argo@163.com>
2022-03-22 13:25:19 +0000

    !2560 test(#I4Z2SM): add textarea unit test * test: 精简代码 * test: 添加 Textarea 组件单元测试

---
+++ test/UnitTest/Components/TextareaTest.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class TextareaTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void IsShowLabel_OK()
+    {
+        var cut = Context.RenderComponent<Textarea>(builder => builder.Add(s => s.ShowLabel, true));
+
+        var component = cut.FindComponent<BootstrapLabel>();
+        Assert.NotNull(component);
+    }
+}
