858f5010d14451204748c44790c6379ce9d144ac
Argo <argo@163.com>
2022-03-22 13:57:26 +0000

    !2563 test(#I4Z2UI): add unit test for LogoutLink * test: 增加断言 * test: 重构代码 * test: 添加 logoutLink 组件单元测试

---
+++ test/UnitTest/Components/LogoutLinkTest.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Bunit.TestDoubles;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class LogoutLinkTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Icon_Ok()
+    {
+        var cut = Context.RenderComponent<LogoutLink>(builder => builder.Add(s => s.Icon, "fa fa-key"));
+        var ele = cut.Find(".fa-key");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void Text_Ok()
+    {
+        var cut = Context.RenderComponent<LogoutLink>(builder => builder.Add(s => s.Text, "Logout"));
+        Assert.Contains("Logout", cut.Markup);
+    }
+
+    [Fact]
+    public void Url_Ok()
+    {
+        var cut = Context.RenderComponent<LogoutLink>(builder => builder.Add(s => s.Url, "/Account/Logout"));
+        cut.Contains("href=\"#\"");
+    }
+
+    [Fact]
+    public void ForceLoad_Ok()
+    {
+        var cut = Context.RenderComponent<LogoutLink>(builder => builder.Add(s => s.ForceLoad, true));
+        cut.Contains("href=\"#\"");
+    }
+
+    [Fact]
+    public void OnLogout_Ok()
+    {
+        var navMan = Context.Services.GetRequiredService<FakeNavigationManager>();
+        var cut = Context.RenderComponent<LogoutLink>();
+        cut.Find("a").Click();
+        Assert.Equal("http://localhost/Account/Logout", navMan.Uri);
+    }
+}
