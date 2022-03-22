b346991e88aef785646e5d930f510cab1d90dfa9
Argo <argo@163.com>
2022-03-22 14:34:37 +0000

    !2567 test(#I4Z2WF): add Skeleton unit test * refactor: 格式化代码 * Merge branch 'main' into dev-test-skeleton * test: 添加骨架组件单元测试

---
+++ test/UnitTest/Components/SkeletonTest.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class SkeletonTest : TestBase
+{
+    [Fact]
+    public void Circle_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonAvatar>(buildr => buildr.Add(s => s.Circle, true));
+
+        var ele = cut.Find(".circle");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void Round_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonAvatar>(buildr => buildr.Add(s => s.Round, true));
+
+        var ele = cut.Find(".round");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void Active_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonAvatar>(buildr => buildr.Add(s => s.Active, true));
+
+        var ele = cut.Find(".active");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void ShowToolbar_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonTable>(buildr => buildr.Add(s => s.ShowToolbar, true));
+
+        var ele = cut.Find(".skeleton-toolbar");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void Rows_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonTable>(buildr => buildr.Add(s => s.Rows, 10));
+
+        var count = cut.FindAll(".skeleton-row").Count;
+        Assert.Equal(10, count);
+    }
+
+    [Fact]
+    public void Columns_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonTable>(buildr => buildr.Add(s => s.Columns, 10));
+
+        var count = cut.FindAll(".skeleton-col").Count;
+        Assert.Equal(70, count);
+    }
+
+    [Fact]
+    public void Tree_Ok()
+    {
+        var cut = Context.RenderComponent<SkeletonTree>();
+
+        var ele = cut.Find(".tree");
+        Assert.NotNull(ele);
+    }
+}
