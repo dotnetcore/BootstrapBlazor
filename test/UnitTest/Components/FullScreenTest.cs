c998b3bf7d69659b79c1a2d740ea4171e595e588
Argo <argo@163.com>
2022-03-23 15:01:32 +0000

    !2571 test(#I4Z9ZV): add FullScreen unit test * test: 增加代码覆盖率 * test: 补充 fullscreen 组件单元测试 * test: 补充 FullScreen 组件单元测试 * test: 添加 FullScreen 组件单元测试

---
+++ test/UnitTest/Components/FullScreenTest.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class FullScreenTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void ButtonIcon_Ok()
+    {
+        var cut = Context.RenderComponent<FullScreenButton>(builder => builder.Add(s => s.ButtonIcon, "fa fa-arrows-alt"));
+        var ele = cut.Find(".fa-arrows-alt");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void FullScreenIcon_Ok()
+    {
+        var cut = Context.RenderComponent<FullScreenButton>(builder => builder.Add(s => s.FullScreenIcon, "fa"));
+        var ele = cut.Find(".fa");
+        Assert.NotNull(ele);
+    }
+
+    [Fact]
+    public void Title_Ok()
+    {
+        var cut = Context.RenderComponent<FullScreenButton>(builder => builder.Add(s => s.Title, "FullScreen Title"));
+    }
+
+    [Fact]
+    public void ToggleFullScreen_Ok()
+    {
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(builder =>
+        {
+            builder.Add(s => s.ChildContent, new RenderFragment(builder =>
+            {
+                builder.OpenComponent<FullScreenButton>(0);
+                builder.CloseComponent();
+            }));
+        });
+        cut.Find(".bb-fs").Click();
+    }
+
+    [Fact]
+    public async Task ToggleByElement_Ok()
+    {
+        ElementReference element = default;
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(builder =>
+        {
+            builder.Add(s => s.ChildContent, new RenderFragment(builder =>
+            {
+                builder.OpenElement(0, "div");
+                builder.AddElementReferenceCapture(1, e =>
+                {
+                    element = e;
+                });
+                builder.CloseElement();
+                builder.OpenComponent<FullScreenServiceTest>(0);
+                builder.CloseComponent();
+            }));
+        });
+        var fs = cut.FindComponent<FullScreenServiceTest>();
+        await cut.InvokeAsync(() => fs.Instance.Test(element));
+    }
+
+    private class FullScreenServiceTest : ComponentBase
+    {
+        [Inject]
+        [NotNull]
+        public FullScreenService? FullScreenService { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+        }
+
+        public async Task Test(ElementReference ele) => await FullScreenService.Toggle(new FullScreenOption()
+        {
+            Element = ele,
+            Id = "test"
+        });
+    }
+}
