1072a703ae8cf28b7193c29b7076b6e08ce9c79b
Argo <argo@163.com>
2022-04-12 19:56:43 +0000

    !2651 test(#I52EWU): add ShowToolbar unit test * test: 增加 ShowToolbar 单元测试

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -139,4 +139,27 @@ public class TableTest : TableTestBase
         });
         cut.Contains("table-search");
     }
+
+    [Fact]
+    public void ShowToolbar_Ok()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Table<Foo>>(pb =>
+            {
+                pb.Add(a => a.ShowToolbar, true);
+                pb.Add(a => a.SearchMode, SearchMode.Top);
+                pb.Add(a => a.Items, Foo.GenerateFoo(localizer));
+                pb.Add(a => a.TableColumns, foo => builder =>
+                {
+                    builder.OpenComponent<TableColumn<Foo, string>>(0);
+                    builder.AddAttribute(1, "Field", "Name");
+                    builder.AddAttribute(2, "FieldExpression", Utility.GenerateValueExpression(foo, "Name", typeof(string)));
+                    builder.CloseComponent();
+                });
+            });
+        });
+        cut.Contains("float-end table-toolbar-button");
+    }
 }
