e287bfac9371525819726c37816780405c9db8a2
Nine <948825551@qq.com>
2022-05-13 05:47:19 +0000

    !2779 doc(#I579YF): add english translation of speechwave page * doc: 添加 SpeechWaves 组件翻译文档

--- src/BootstrapBlazor.Shared/Samples/Speechs/SpeechWaves.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Speechs/SpeechWaves.razor.cs
@@ -14,7 +14,7 @@ public partial class SpeechWaves
 {
     private bool IsShow { get; set; }
 
-    private string ButtonText => IsShow ? "隐藏" : "显示";
+    private string ButtonText => IsShow ? Localizer["ValueButtonText1"] : Localizer["ValueButtonText2"];
 
     private void OnClickShow()
     {
@@ -25,33 +25,33 @@ public partial class SpeechWaves
     /// 获得属性方法
     /// </summary>
     /// <returns></returns>
-    private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         // TODO: 移动到数据库中
         new AttributeItem() {
             Name = nameof(SpeechWave.Show),
-            Description = "是否开始",
+            Description = Localizer["ShowAttr"],
             Type = "bool",
             ValueList = "true/false",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = nameof(SpeechWave.ShowUsedTime),
-            Description = "是否显示时长",
+            Description = Localizer["ShowUsedTimeAttr"],
             Type = "bool",
             ValueList = "true/false",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = nameof(SpeechWave.OnTimeout),
-            Description = "识别结束后超时回调方法",
+            Description = Localizer["OnTimeoutAttr"],
             Type = "Func<Task>",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = nameof(SpeechWave.TotalTimeSecond),
-            Description = "语音识别设置总时长超出过调用 OnTimeout 回调",
+            Description = Localizer["TotalTimeSecondAttr"],
             Type = "int",
             ValueList = " — ",
             DefaultValue = "60"
