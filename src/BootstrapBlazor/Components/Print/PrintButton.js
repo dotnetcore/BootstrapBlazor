bf3adf05a06db0e618afc13fff6fb3b84fcdc8c0
Argo <argo@163.com>
2022-05-21 07:55:40 +0000

    !2802 fix(#I58G2R): not value on Modal when click print button * chore: update project file * chore: bump version 6.6.11 * Merge branch 'main' into test-print * fix: 更新脚本修复为赋值问题 * refactor: 精简代码 * Revert "test: 更改打印逻辑" * chore: 增加对 Topology 包引用 * test: 更改打印逻辑

--- src/BootstrapBlazor/Components/Print/PrintButton.js
+++ src/BootstrapBlazor/Components/Print/PrintButton.js
@@ -12,16 +12,40 @@
                 $modalBody = $modalHeader.parentsUntil('.modal-content').parent().find('.modal-body');
             }
             if ($modalBody != null && $modalBody.length > 0) {
+                $modalBody.find(":text, :checkbox, :radio").each(function (index, el) {
+                    var $el = $(el);
+                    var id = $el.attr('id');
+                    if (!id) {
+                        $el.attr('id', $.getUID());
+                    }
+                });
                 var printContenxt = $modalBody.html();
                 var $body = $('body').addClass('bb-printview-open');
                 var $dialog = $('<div></div>').addClass('bb-printview').html(printContenxt).appendTo($body);
 
-                window.print();
-                $body.removeClass('bb-printview-open')
-                $dialog.remove();
+                // assign value
+                $dialog.find(":input").each(function (index, el) {
+                    var $el = $(el);
+                    var id = $el.attr('id');
+
+                    if ($el.attr('type') === 'checkbox') {
+                        $el.prop('checked', $('#' + id).prop('checked'));
+                    }
+                    else {
+                        $el.val($('#' + id).val());
+                    }
+                });
+
+                window.setTimeout(function () {
+                    window.print();
+                    $body.removeClass('bb-printview-open')
+                    $dialog.remove();
+                }, 50);
             }
             else {
-                window.print();
+                window.setTimeout(function () {
+                    window.print();
+                }, 50);
             }
         }
     });
