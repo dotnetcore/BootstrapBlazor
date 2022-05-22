3e410130f66213ed7e3614b730bf17bb08ae32b4
Argo <argo@163.com>
2022-05-22 03:18:27 +0000

    !2803 feat(#I58M7G): add Title parameter on TopologyItem * chore: bump verson 6.0.3 * feat: 增加 Title 参数

--- src/Extensions/Components/BootstrapBlazor.Topology/Components/TopologyItem.cs
+++ src/Extensions/Components/BootstrapBlazor.Topology/Components/TopologyItem.cs
@@ -10,27 +10,32 @@ namespace BootstrapBlazor.Components;
 public class TopologyItem
 {
     /// <summary>
-    /// 
+    /// 获得/设置 对象 Id
     /// </summary>
     public string? ID { get; set; }
 
     /// <summary>
-    /// 
+    /// 获得/设置 对象关联 Tag 位号值
     /// </summary>
     public string? Tag { get; set; }
 
     /// <summary>
-    /// 
+    /// 获得/设置 显示文字
     /// </summary>
     public string? Text { get; set; }
 
     /// <summary>
-    /// 
+    /// 获得/设置 组合组件状态切换值 一般从 0 开始
     /// </summary>
     public double ShowChild { get; set; }
 
     /// <summary>
-    /// 
+    /// 获得/设置 显示文字颜色
     /// </summary>
     public string? TextColor { get; set; }
+
+    /// <summary>
+    /// 获得/设置 对象 title 属性一般用于 tooltip 显示
+    /// </summary>
+    public string? Title { get; set; }
 }
