557ebbf530a75b49b70a7e96309a8ad6299883ab
Argo <argo@163.com>
2022-07-17 10:37:41 +0000

    !3018 test(#I5HG9N): add unit test for menu extensions method * test: add unit test for menu extension method

---
+++ test/UnitTest/Extensions/MenuItemExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Extensions;
+
+public class MenuItemExtensionsTest
+{
+    [Fact]
+    public void CascadingSetActive_Ok()
+    {
+        var menu = new MenuItem()
+        {
+            Parent = new MenuItem()
+            {
+                ParentId = "0"
+            }
+        };
+        menu.CascadingSetActive(true);
+
+        Assert.True(menu.Parent.IsActive);
+        Assert.False(menu.Parent.IsCollapsed);
+    }
+}
