102bb990a26638d871ecf053261999ecce908153
Argo <argo@163.com>
2022-08-27 07:33:47 +0000

    !3168 feat(#I5OE6J): update FAIconList component * feat: 增加 solid regular 切换功能 * refactor: 精简 FAIconList 组件更新样式 * feat: 组件增加多语言支持 * refactor: 增加资源文件 * feat: 更新 FAIconList 组件 * chore: FontAwesom 组件依赖更改到 shared 工程 * refactor: 更新 IconDialog 组件 * Merge branch 'main' into feat-fa-icon * Revert "!3150 doc(#I5NS3B): update document for font-awesome"

---
+++ src/BootstrapBlazor/Components/FAIcons/IconDialog.razor.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.Localization;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class IconDialog
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [NotNull]
+#if NET6_0_OR_GREATER
+    [EditorRequired]
+#endif
+    public string? IconName { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Label 显示文字
+    /// </summary>
+    [Parameter]
+    public string? LabelText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Label 显示文字
+    /// </summary>
+    [Parameter]
+    public string? LabelFullText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 拷贝按钮显示文字
+    /// </summary>
+    [Parameter]
+    public string? ButtonText { get; set; }
+
+    private string IconFullName => $"<i class=\"{IconName}\" aria-hidden=\"true\"></i>";
+
+    private ElementReference IconDialogElement { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<IconDialog>? Localizer { get; set; }
+
+    private IEnumerable<SelectedItem> Items { get; } = new List<SelectedItem>()
+    {
+        new("solid", "Solid"),
+        new("regular", "Regular")
+    };
+
+    private string IconStyle { get; set; } = "solid";
+
+    /// <summary>
+    /// OnParametersSet 方法
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        LabelText ??= Localizer[nameof(LabelText)];
+        LabelFullText ??= Localizer[nameof(LabelFullText)];
+        ButtonText ??= Localizer[nameof(ButtonText)];
+
+        IconName = IconName.Replace("fas", "fa-solid", StringComparison.OrdinalIgnoreCase)
+            .Replace("far", "fa-regular", StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// OnAfterRenderAsync 方法
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync(IconDialogElement, "bb_iconDialog");
+        }
+    }
+
+    private Task OnValueChanged(string val)
+    {
+        if(val == "solid")
+        {
+            IconName = IconName.Replace("fa-regular", "fa-solid");
+        }
+        else
+        {
+            IconName = IconName.Replace("fa-solid", "fa-regular");
+        }
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.FontAwesome/Components/FAIconList.razor.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class FAIconList
+{
+    private ElementReference IconListElement { get; set; }
+
+    private string? ClassString => CssBuilder.Default("icon-list")
+        .AddClass("is-catalog", ShowCatalog)
+        .AddClass("is-dialog", ShowCopyDialog)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 点击时是否显示高级拷贝弹窗 默认 false 直接拷贝到粘贴板
+    /// </summary>
+    [Parameter]
+    public bool ShowCopyDialog { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否显示目录 默认 false 开启后需要自行增加样式
+    /// </summary>
+    [Parameter]
+    public bool ShowCatalog { get; set; }
+
+    /// <summary>
+    /// 获得/设置 高级弹窗 Header 显示文字
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public string? DialogHeaderText { get; set; }
+
+    [Inject]
+    [NotNull]
+    private DialogService? DialogService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<IconDialog>? Localizer { get; set; }
+
+    private JSInterop<FAIconList>? Interop { get; set; }
+
+    /// <summary>
+    /// OnParametersSet 方法
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        DialogHeaderText ??= Localizer[nameof(DialogHeaderText)];
+    }
+
+    /// <summary>
+    /// OnAfterRenderAsync 方法
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            Interop ??= new(JSRuntime);
+            await Interop.InvokeVoidAsync(this, IconListElement, "bb_iconList", nameof(ShowDialog));
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [JSInvokable]
+    public Task ShowDialog(string text) => DialogService.ShowCloseDialog<IconDialog>(DialogHeaderText, parameters =>
+    {
+        parameters.Add(nameof(IconDialog.IconName), text);
+    });
+}
