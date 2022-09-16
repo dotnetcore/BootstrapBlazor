4683a8d3d5cdb0d9a4d9e3e8d64e2d35f21b0df2
Nine <948869991@qq.com>
2022-09-16 05:36:54 +0000

    !3254 test(#I5QNBR): add unit test for selectTree component * Merge branch 'main' into dev-test-selecttree * test: 增加表单认证单元测试 * test: 增加 RetrieveId 单元测试 * refactor: 移除级联类型标签 * feat: 增加 DropdownIcon 参数用于设置下拉箭头图标 * refactor: 删除注释代码 * test: 补充单元测试 * refactor: 修改 SelectTree 逻辑 * test: 增加 SelectTree 单元测试 * !3251 feat(#I5R4FJ): add ShownCallbackAsync parameter on Modal/Dialog component * !3253 feat(#I5R5IS): add DisplayTemplate parameter on Select * !3252 feat(#I5R51W): add DropdownIcon parameter on Select * !3250 feat(#I5R3YG): update FontAwesome pacakge to 6.1.5 * !3249 feat(#I5R3SM): set FixHeader and AllowResize on Table columns ar… * !3248 feat(#I5QWTM): update IconDialog component * !3247 fix(#I5QWG4): slider bar overflow when Max greater than 100 * !3246 fix(#I5QTY6): slider can't drag in dialog * !3245 feat(#I5QSH6): add OnValueChanged callback on Slider component * !3244 feat(#I5QONZ): add unit test for DisableAutoSubmitFormByEnter * !3243 fix(#I5QOG6): update css class for empty display component * !3242 test(#I5QOGE): add unit test for IconDialog * !3241 fix(#I5QOEA): can not get the iamge data * !3240 test(#I5QODJ): add unit test of Carousel * !3239 feat(#I5QOD7): add DisableTouchSwiping parameter on Carousel * !3238 feat(#I5QOCD): add Interval parameter on Carousel component * test: 添加 TestTree 单元测试

--- src/BootstrapBlazor/Components/Select/SelectTree.razor.cs
+++ src/BootstrapBlazor/Components/Select/SelectTree.razor.cs
@@ -10,22 +10,8 @@ namespace BootstrapBlazor.Components;
 /// Select 组件实现类
 /// </summary>
 /// <typeparam name="TValue"></typeparam>
-#if NET6_0_OR_GREATER
-[CascadingTypeParameter(nameof(TValue))]
-#endif
 public partial class SelectTree<TValue>
 {
-    private ElementReference SelectElement { get; set; }
-
-    private JSInterop<SelectTree<TValue>>? Interop { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [Inject]
-    [NotNull]
-    private SwalService? SwalService { get; set; }
-
     /// <summary>
     /// 获得 样式集合
     /// </summary>
@@ -53,51 +39,16 @@ public partial class SelectTree<TValue>
         .Build();
 
     /// <summary>
-    /// 设置当前项是否 Active 方法
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    private string? ActiveItem(TreeViewItem<TValue> item) => CssBuilder.Default("dropdown-item")
-        .AddClass("active", () => FormatValueAsString(item.Value) == CurrentValueAsString)
-        .AddClass("disabled", item.IsDisabled)
-        .Build();
-
-    /// <summary>
     /// 获得/设置 颜色 默认 Color.None 无设置
     /// </summary>
     [Parameter]
     public Color Color { get; set; }
 
     /// <summary>
-    /// 获得/设置 搜索文本发生变化时回调此方法
-    /// </summary>
-    [Parameter]
-    [NotNull]
-    public Func<string, IEnumerable<TreeViewItem<TValue>>>? OnSearchTextChanged { get; set; }
-
-    /// <summary>
-    /// 获得/设置 是否显示搜索框 默认为 false 不显示
-    /// </summary>
-    [Parameter]
-    public bool ShowSearch { get; set; }
-
-    /// <summary>
-    /// 获得/设置 选中候选项后是否自动清空搜索框内容 默认 false 不清空
-    /// </summary>
-    [Parameter]
-    public bool AutoClearSearchText { get; set; }
-
-    /// <summary>
     /// 获得 PlaceHolder 属性
     /// </summary>
     [Parameter]
     public string? PlaceHolder { get; set; }
-
-    /// <summary>
-    /// 获得/设置 选项模板支持静态数据
-    /// </summary>
-    [Parameter]
-    public RenderFragment? Options { get; set; }
 
     /// <summary>
     /// 获得/设置 字符串比较规则 默认 StringComparison.OrdinalIgnoreCase 大小写不敏感 
@@ -117,36 +68,6 @@ public partial class SelectTree<TValue>
     /// </summary>
     [Parameter]
     public Func<TValue, Task>? OnSelectedItemChanged { get; set; }
-
-    /// <summary>
-    /// 获得/设置 下拉框项目改变前回调委托方法 返回 true 时选项值改变，否则选项值不变
-    /// </summary>
-    [Parameter]
-    public Func<TValue, Task<bool>>? OnBeforeSelectedItemChange { get; set; }
-
-    /// <summary>
-    /// 获得/设置 Swal 图标 默认 Question
-    /// </summary>
-    [Parameter]
-    public SwalCategory SwalCategory { get; set; } = SwalCategory.Question;
-
-    /// <summary>
-    /// 获得/设置 Swal 标题 默认 null
-    /// </summary>
-    [Parameter]
-    public string? SwalTitle { get; set; }
-
-    /// <summary>
-    /// 获得/设置 Swal 内容 默认 null
-    /// </summary>
-    [Parameter]
-    public string? SwalContent { get; set; }
-
-    /// <summary>
-    /// 获得/设置 Footer 默认 null
-    /// </summary>
-    [Parameter]
-    public string? SwalFooter { get; set; }
 
     /// <summary>
     /// 获得/设置 点击节点获取子数据集合回调方法
@@ -173,6 +94,12 @@ public partial class SelectTree<TValue>
     [Parameter]
     public bool ShowIcon { get; set; }
 
+    /// <summary>
+    /// 获得/设置 下拉箭头 Icon 图标 默认 "fa-solid fa-angle-up"
+    /// </summary>
+    [Parameter]
+    public string? DropdownIcon { get; set; }
+
     [Inject]
     [NotNull]
     private IStringLocalizer<SelectTree<TValue>>? Localizer { get; set; }
@@ -188,28 +115,7 @@ public partial class SelectTree<TValue>
     /// </summary>
     private string? InputId => $"{Id}_input";
 
-    /// <summary>
-    /// 获得/设置 搜索文字
-    /// </summary>
-    private string SearchText { get; set; } = "";
-
-    [NotNull]
-    private List<TreeViewItem<TValue>>? DataSource { get; set; }
-
     private TreeViewItem<TValue>? SelectedItem { get; set; }
-
-    /// <summary>
-    /// OnInitialized 方法
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-
-        if (OnSearchTextChanged == null)
-        {
-            OnSearchTextChanged = text => Items.Where(i => i.Text?.Contains(text, StringComparison) ?? false);
-        }
-    }
 
     /// <summary>
     /// OnParametersSet 方法
@@ -219,35 +125,9 @@ public partial class SelectTree<TValue>
         base.OnParametersSet();
 
         Items ??= new();
+        DropdownIcon ??= "fa-solid fa-angle-up";
         PlaceHolder ??= Localizer[nameof(PlaceHolder)];
     }
-
-    //private void ResetSelectedItem()
-    //{
-    //    if (string.IsNullOrEmpty(SearchText))
-    //    {
-    //        DataSource = Items;
-    //    }
-    //    else
-    //    {
-    //        DataSource = OnSearchTextChanged(SearchText).ToList();
-    //    }
-
-    //    SelectedItem = DataSource.FirstOrDefault(i => ComparerItem(i.Value, SelectedItem?.Value))
-    //        ?? DataSource.FirstOrDefault(i => i.IsActive)
-    //        ?? DataSource.FirstOrDefault();
-
-    //    // 检查 Value 值是否在候选项中存在
-    //    // Value 不等于 选中值即不存在
-    //    if (SelectedItem != null && SelectedItem.Value != null)
-    //    {
-    //        var v = FormatValueAsString(SelectedItem.Value);
-    //        if (CurrentValueAsString != v)
-    //        {
-    //            CurrentValueAsString = v;
-    //        }
-    //    }
-    //}
 
     /// <summary>
     /// OnAfterRenderAsync 方法
@@ -257,36 +137,6 @@ public partial class SelectTree<TValue>
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
-
-        if (firstRender)
-        {
-            if (Interop == null)
-            {
-                Interop = new JSInterop<SelectTree<TValue>>(JSRuntime);
-            }
-            await Interop.InvokeVoidAsync(this, SelectElement, "bb_select", nameof(ConfirmSelectedItem));
-
-            // 选项值不为 null 后者 string.Empty 时触发一次 OnSelectedItemChanged 回调
-            if (SelectedItem != null && OnSelectedItemChanged != null)
-            {
-                await OnSelectedItemChanged.Invoke(SelectedItem.Value);
-            }
-        }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="index"></param>
-    /// <returns></returns>
-    [JSInvokable]
-    public async Task ConfirmSelectedItem(int index)
-    {
-        var ds = string.IsNullOrEmpty(SearchText)
-            ? DataSource
-            : OnSearchTextChanged.Invoke(SearchText);
-        var item = ds.ElementAt(index);
-        await OnItemClick(item);
     }
 
     /// <summary>
@@ -294,42 +144,8 @@ public partial class SelectTree<TValue>
     /// </summary>
     private async Task OnItemClick(TreeViewItem<TValue> item)
     {
-        var ret = true;
-        if (OnBeforeSelectedItemChange != null)
-        {
-            ret = await OnBeforeSelectedItemChange(item.Value);
-            if (ret)
-            {
-                // 返回 True 弹窗提示
-                var option = new SwalOption()
-                {
-                    Category = SwalCategory,
-                    Title = SwalTitle,
-                    Content = SwalContent
-                };
-                if (!string.IsNullOrEmpty(SwalFooter))
-                {
-                    option.ShowFooter = true;
-                    option.FooterTemplate = builder => builder.AddContent(0, SwalFooter);
-                }
-                ret = await SwalService.ShowModal(option);
-            }
-            else
-            {
-                // 返回 False 直接运行
-                ret = true;
-            }
-        }
-        if (ret)
-        {
-            await ItemChanged(item);
-        }
-
+        await ItemChanged(item);
         SelectedItem = item;
-
-        // 关闭弹窗
-        await JSRuntime.InvokeVoidAsync(SelectElement, "bb_select_tree");
-
         StateHasChanged();
     }
 
@@ -348,22 +164,5 @@ public partial class SelectTree<TValue>
         {
             await OnSelectedItemChanged.Invoke(item.Value);
         }
-
-        if (AutoClearSearchText)
-        {
-            SearchText = string.Empty;
-        }
     }
-
-    /// <summary>
-    /// 比较数据是否相同
-    /// </summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    protected bool ComparerItem(TValue a, TValue b) => ModelEqualityComparer?.Invoke(a, b)
-        ?? Utility.GetKeyValue<TValue, object>(a, CustomKeyAttribute)?.Equals(Utility.GetKeyValue<TValue, object>(b, CustomKeyAttribute))
-        ?? ModelComparer.EqualityComparer(a, b)
-        ?? a?.Equals(b)
-        ?? false;
 }
---
+++ test/UnitTest/Components/SelectTreeTest.cs
@@ -0,0 +1,177 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace UnitTest.Components;
+public class SelectTreeTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Items_Ok()
+    {
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.ShowIcon, true);
+            builder.Add(p => p.ModelEqualityComparer, (s1, s2) => { return true; });
+            builder.Add(p => p.OnExpandNodeAsync, (s) => { return Task.FromResult(new List<TreeViewItem<string>>().AsEnumerable()); });
+            builder.Add(p => p.CustomKeyAttribute, typeof(string));
+        });
+        cut.Contains("tree-root");
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(p => p.Items, BindItems);
+        });
+        cut.Contains("tree-root");
+    }
+
+    [Fact]
+    public void Color_Ok()
+    {
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+            builder.Add(p => p.Color, Color.Primary);
+        });
+
+        Assert.Contains("border-primary", cut.Markup);
+    }
+
+    [Fact]
+    public void IsShowLabel_Ok()
+    {
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+            builder.Add(p => p.ShowLabel, true);
+            builder.Add(p => p.DisplayText, "test-showLabel");
+        });
+
+        Assert.Contains("test-showLabel", cut.Markup);
+    }
+
+    [Fact]
+    public async Task InValid_Ok()
+    {
+        var model = new Foo();
+        var cut = Context.RenderComponent<ValidateForm>(builder =>
+        {
+            builder.Add(c => c.Model, model);
+            builder.AddChildContent<SelectTree<string>>(c =>
+            {
+                c.Add(p => p.Items, BindItems);
+                c.Add(p => p.Value, model.Name);
+                c.Add(p => p.ValueChanged, EventCallback.Factory.Create<string>(this, s => model.Name = s));
+                c.Add(p => p.ValueExpression, Utility.GenerateValueExpression(model, nameof(model.Name), typeof(string)));
+            });
+        });
+        await cut.InvokeAsync(() => cut.Find("form").Submit());
+        cut.Contains("border-danger invalid is-invalid");
+    }
+
+    [Fact]
+    public async Task Valid_Ok()
+    {
+        var model = new Foo() { Name = "Test" };
+        var cut = Context.RenderComponent<ValidateForm>(builder =>
+        {
+            builder.Add(c => c.Model, model);
+            builder.AddChildContent<SelectTree<string>>(c =>
+            {
+                c.Add(p => p.Items, BindItems);
+                c.Add(p => p.Value, model.Name);
+                c.Add(p => p.ValueChanged, EventCallback.Factory.Create<string>(this, s => model.Name = s));
+                c.Add(p => p.ValueExpression, Utility.GenerateValueExpression(model, nameof(model.Name), typeof(string)));
+            });
+        });
+        await cut.InvokeAsync(() => cut.Find("form").Submit());
+        cut.Contains("border-success valid is-valid");
+    }
+
+    [Fact]
+    public void PlaceHolder_Ok()
+    {
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+            builder.Add(p => p.PlaceHolder, "Please input value");
+        });
+        cut.Contains("Please input value");
+    }
+
+    [Fact]
+    public void StringComparison_Ok()
+    {
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+            builder.Add(p => p.StringComparison, StringComparison.CurrentCultureIgnoreCase);
+        });
+
+        Assert.Equal(StringComparison.CurrentCultureIgnoreCase, cut.Instance.StringComparison);
+    }
+
+    [Fact]
+    public async Task OnSelectedItemChanged_Ok()
+    {
+        var res = false;
+        var cut = Context.RenderComponent<SelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+            builder.Add(p => p.OnSelectedItemChanged, new Func<string, Task>(s =>
+            {
+                res = true;
+                return Task.CompletedTask;
+            }));
+        });
+        await cut.InvokeAsync(() => cut.Find(".tree-node").Click());
+
+        Assert.True(res);
+    }
+
+    [Fact]
+    public void RetrieveId_Ok()
+    {
+        var cut = Context.RenderComponent<MockSelectTree<string>>(builder =>
+        {
+            builder.Add(p => p.Items, BindItems);
+        });
+        Assert.True(cut.Instance.TestRetrieveId());
+    }
+
+    private List<TreeViewItem<string>> BindItems { get; } = new List<TreeViewItem<string>>()
+    {
+        new TreeViewItem<string>("Test1")
+        {
+            Text ="Test1",
+            Icon = "fa-solid fa-folder",
+            ExpandIcon = "fa-solid fa-folder-open",
+            CheckedState =CheckboxState.Checked,
+            IsActive = true,
+            Items = new List<TreeViewItem<string>>()
+            {
+                new TreeViewItem<string>("Test1-1")
+                {
+                    Text ="Test1-1",
+                    Icon = "fa-solid fa-folder",
+                    ExpandIcon = "fa-solid fa-folder-open",
+                    Items = new List<TreeViewItem<string>>()
+                    {
+                        new TreeViewItem<string>("Test1-1-1") { Text = "Test1-1-1", Icon = "fa-solid fa-file" },
+                        new TreeViewItem<string>("Test1-1-2") { Text = "Test1-1-2", Icon = "fa-solid fa-file" }
+                    }
+                }
+            }
+        }
+    };
+
+    private class MockSelectTree<TValue> : SelectTree<TValue>
+    {
+        public bool TestRetrieveId()
+        {
+            return base.RetrieveId() == $"{Id}_input";
+        }
+    }
+}
