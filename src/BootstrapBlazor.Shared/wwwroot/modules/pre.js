772b7bd631fb045b0d34699ee677e06435f128b5
Argo <argo@163.com>
2022-10-23 11:02:25 +0000

    !3431 doc(#I5X7OL): update pre integrade highlight module * feat: 更新 pre 模块脚本更新 highlight 动态加载机制 * doc: 精简 layout/index.html 入口路由文件

--- src/BootstrapBlazor.Shared/wwwroot/modules/pre.js
+++ src/BootstrapBlazor.Shared/wwwroot/modules/pre.js
@@ -1,10 +1,12 @@
 ﻿import BlazorComponent from "../../../_content/BootstrapBlazor/modules/base/blazor-component.js"
 import EventHandler from "../../../_content/BootstrapBlazor/modules/base/event-handler.js"
-import { copy, getDescribedElement } from "../../../_content/BootstrapBlazor/modules/base/utility.js"
+import { copy, getDescribedElement, addLink, addScript } from "../../../_content/BootstrapBlazor/modules/base/utility.js"
 import { Tooltip } from "../../../_content/BootstrapBlazor/modules/tooltip.js"
 
 export class Pre extends BlazorComponent {
     _init() {
+        addLink('_content/BootstrapBlazor.Shared/lib/highlight/vs.css')
+        addScript('_content/BootstrapBlazor.Shared/lib/highlight/highlight.min.js')
         this._setListeners()
     }
 
@@ -23,13 +25,30 @@ export class Pre extends BlazorComponent {
     _execute(args) {
         if (args[0] === 'highlight') {
             if (window.hljs) {
-                const code = this._element.querySelector('code')
-                window.hljs.highlightBlock(code)
+                this._highlight()
+            }
+            else {
+                this._handler = window.setInterval(() => {
+                    if (window.hljs) {
+                        window.clearInterval(this._handler)
+                        delete this._handler
+                        this._highlight()
+                    }
+                }, 100)
             }
         }
     }
 
+    _highlight() {
+        const code = this._element.querySelector('code')
+        window.hljs.highlightBlock(code)
+    }
+
     _dispose() {
+        if (this._handler) {
+            window.clearInterval(this._handler)
+            delete this._handler
+        }
         EventHandler.off(this._element, 'click', 'button');
     }
 }
