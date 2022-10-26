dffcdb71d2f940acd31273d39ee88199f724e8f3
Argo <argo@163.com>
2022-10-26 08:23:38 +0000

    !3461 doc(#I5XU7U): update Layout sample code * refactor: 修复 Layout 示例文档中 Header 位移问题 * doc: 更新资源文件 * doc: 更新资源文件

--- src/BootstrapBlazor.Shared/wwwroot/modules/header.js
+++ src/BootstrapBlazor.Shared/wwwroot/modules/header.js
@@ -5,7 +5,7 @@ export class Header {
         const scrollTop = () => (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
         let prevScrollTop = 0;
         EventHandler.on(document, 'scroll',  () => {
-            const items = document.querySelectorAll('header, .coms-search')
+            const items = document.querySelectorAll('.navbar-header, .coms-search')
             const currentScrollTop = scrollTop()
             if (currentScrollTop > prevScrollTop) {
                 items.forEach(item => item.classList.add('hide'))
