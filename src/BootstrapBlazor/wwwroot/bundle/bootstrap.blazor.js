913cc65d1617ddd21e07dbaf1e0aa2751d6cee51
Argo <argo@163.com>
2022-11-15 08:59:41 +0000

    !3532 fix(#I619LS): redesign console component * doc: 增加 HeaderTemplate 说明文档 * test: 增加 HeaderTemplate 单元测试 * feat: 增加 HeaderTemplate 参数 * doc: 更新 Console 参数文档 * test: 删除冗余单元测试 * test: 增加 IsFlashLight 单元测试 * test: 增加 LightColor 单元测试 * test: 增加 ShowLight 单元测试 * test: 增加 FooterTemplate 单元测试 * chore: 增加 IsFlashLight 参数控制是否闪烁 * feat: 增加 ShowLight 参数 * feat: 增加 LightColor 参数 * test: 增加 FooterTemplate 单元测试 * doc: 增加 FooterTemplate 说明文档 * feat: 增加 FooterTemplate 模板 * doc: 更新 Consoles 示例 * chore: 更改为动态加载脚本 * chore: 移除 ConsoleBase 基类

--- src/BootstrapBlazor/wwwroot/bundle/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/bundle/bootstrap.blazor.js
@@ -1294,19 +1294,6 @@
 
 (function ($) {
     $.extend({
-        bb_console: function (el) {
-            var $el = $(el);
-            var $body = $el.find('[data-scroll="auto"]');
-            if ($body.length > 0) {
-                var $win = $body.find('.console-window');
-                $body.scrollTop($win.height());
-            }
-        }
-    });
-})(jQuery);
-
-(function ($) {
-    $.extend({
         bb_form_load: function (el, method) {
             var $el = $(el);
             if (method === 'show')
--- src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.bundle.js
+++ src/BootstrapBlazor/wwwroot/js/bootstrap.blazor.bundle.js
@@ -19281,19 +19281,6 @@ return jQuery;
 
 (function ($) {
     $.extend({
-        bb_console: function (el) {
-            var $el = $(el);
-            var $body = $el.find('[data-scroll="auto"]');
-            if ($body.length > 0) {
-                var $win = $body.find('.console-window');
-                $body.scrollTop($win.height());
-            }
-        }
-    });
-})(jQuery);
-
-(function ($) {
-    $.extend({
         bb_form_load: function (el, method) {
             var $el = $(el);
             if (method === 'show')
