4e6c63ce17eafd3676cea4b6be9bdbca9133bb7c
Alex Chow <zhouchuanglin@gmail.com>
2022-12-07 15:38:00 +0100

    Revert "Update BluetoothBatteryLevel.razor.cs"
    This reverts commit 51cbb2fa4c4ba80aeb591d2776d1989d78f0cd16.

--- src/BootstrapBlazor.Shared/Samples/Bluetooth/BluetoothBatteryLevel.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Bluetooth/BluetoothBatteryLevel.razor.cs
@@ -12,7 +12,7 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class BluetoothBatteryLevel
 {
-    BluetoothBatteryLevel batteryLevel { get; set; } = new BatteryLevel();
+    BatteryLevel batteryLevel { get; set; } = new BatteryLevel();
 
     private decimal? value = 0;
     private string? message;
