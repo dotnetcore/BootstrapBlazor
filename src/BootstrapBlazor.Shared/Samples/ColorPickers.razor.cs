72974f5c0f2ee4a28449ccb1d37d9e0980700263
Lambert Lee <1708416@qq.com>
2022-12-30 10:18:06 +0000

    !3706 doc(#I68715)：update colorpicker demos * update ColorPicker Demos

--- src/BootstrapBlazor.Shared/Samples/ColorPickers.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/ColorPickers.razor.cs
@@ -5,39 +5,19 @@
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// ColorPickers
 /// </summary>
 public partial class ColorPickers
 {
-    [NotNull]
-    private BlockLogger? Trace { get; set; }
-
-    private string? Value1 { get; set; }
-
-    private string Value2 { get; set; } = "#FFFFFF";
-
-    private string Value3 { get; set; } = "#DDDDDD";
-
-    private string? Value5 { get; set; }
-
-    [NotNull]
-    private Foo? Dummy { get; set; } = new Foo() { Name = "#dddddd" };
-
-    private Task OnColorChanged(string color)
-    {
-        Trace.Log($"Selected color: {color}");
-        return Task.CompletedTask;
-    }
-
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-            new AttributeItem()
-            {
-                Name = "OnValueChanged",
-                Description = Localizer["Event1"],
-                Type = "Func<string, Task>",
-                ValueList = "",
-                DefaultValue = ""
-            }
+        new AttributeItem()
+        {
+            Name = "OnValueChanged",
+            Description = Localizer["Event1"],
+            Type = "Func<string, Task>",
+            ValueList = "",
+            DefaultValue = ""
+        }
     };
 }
