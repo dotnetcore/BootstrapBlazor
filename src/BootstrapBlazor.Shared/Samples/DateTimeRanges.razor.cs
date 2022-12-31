012059b1aaf010029442a457bc044ad4872aac07
Lambert Lee <1708416@qq.com>
2022-12-31 09:37:20 +0000

    !3710 doc(#I68A6E)：update datetimerange demos * doc: 更新资源文件 * doc: 格式化文档 * update DateTimeRange demos

--- src/BootstrapBlazor.Shared/Samples/DateTimeRanges.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/DateTimeRanges.razor.cs
@@ -5,92 +5,10 @@
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// DateTimeRanges
 /// </summary>
 public sealed partial class DateTimeRanges
 {
-    [NotNull]
-    private BlockLogger? DateLogger { get; set; }
-
-    private DateTimeRangeValue DateTimeRangeValue1 { get; set; } = new DateTimeRangeValue();
-
-    private DateTimeRangeValue DateTimeRangeValue2 { get; set; } = new DateTimeRangeValue();
-
-    private DateTimeRangeValue DateTimeRangeValue3 { get; set; } = new DateTimeRangeValue() { Start = DateTime.Today, End = DateTime.Today.AddDays(3) };
-
-    private DateTimeRangeValue DateTimeRangeValue4 { get; set; } = new DateTimeRangeValue();
-
-    private DateTimeRangeValue DateTimeRangeValue5 { get; set; } = new DateTimeRangeValue() { Start = DateTime.Today, End = DateTime.Today.AddDays(3) };
-
-    private string? range;
-    private string? range2;
-
-    private bool IsDisabled { get; set; } = true;
-
-    [NotNull]
-    private RangeFoo? Model { get; set; }
-
-    [Inject]
-    [NotNull]
-    private IStringLocalizer<Foo>? LocalizerFoo { get; set; }
-
-    private RowType FormRowType { get; set; }
-
-    private string? GroupFormClassString => CssBuilder.Default("row g-3")
-        .AddClass("form-inline", FormRowType == RowType.Inline)
-        .Build();
-
-    /// <summary>
-    /// OnInitialized 方法
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        Model = new RangeFoo()
-        {
-            DateTime = DateTime.Now,
-            Range = new DateTimeRangeValue()
-            {
-                Start = DateTime.Today.AddMonths(-1),
-                End = DateTime.Today
-            }
-        };
-    }
-
-    private Task OnValueChanged(DateTimeRangeValue val, int index)
-    {
-        var ret = "";
-        if (val.Start != DateTime.MinValue)
-        {
-            ret = val.Start.ToString("yyyy-MM-dd");
-        }
-        if (val.End != DateTime.MinValue)
-        {
-            ret = $"{ret} - {val.End.ToString("yyyy-MM-dd")}";
-        }
-        if (index == 1)
-        {
-            range2 = ret;
-        }
-        else
-        {
-            range = ret;
-        }
-        return Task.CompletedTask;
-    }
-
-    private Task OnConfirm(DateTimeRangeValue value)
-    {
-        DateLogger?.Log($"选择的时间范围是: {value.Start:yyyy-MM-dd} - {value.End:yyyy-MM-dd}");
-        return Task.CompletedTask;
-    }
-
-    private class RangeFoo : Foo
-    {
-        [Required(ErrorMessage = "{0}不可为空")]
-        [Display(Name = "时间范围")]
-        public DateTimeRangeValue Range { get; set; } = new();
-    }
-
     /// <summary>
     /// 获得事件方法
     /// </summary>
