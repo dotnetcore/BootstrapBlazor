fc295fe9aa59a6283d9a58778c3288b5ec5e0f58
Lambert Lee <1708416@qq.com>
2023-01-06 05:42:26 +0000

    !3728 doc(#I69636): update dragdrop demos * update DragDrop demos

--- src/BootstrapBlazor.Shared/Samples/DragDrops.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/DragDrops.razor.cs
@@ -5,65 +5,12 @@
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-///
+/// DragDrops
 /// </summary>
 public partial class DragDrops
 {
-    [NotNull]
-    private List<string>? StrList1 { get; set; }
-
-    [NotNull]
-    private List<string>? StrList2 { get; set; }
-
-    [NotNull]
-    private BlockLogger? Trace { get; set; }
-
     /// <summary>
-    ///
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-        StrList1 = new List<string>()
-        {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5"
-        };
-        StrList2 = new List<string>()
-        {
-            "6",
-            "7",
-            "8",
-            "9",
-            "10"
-        };
-    }
-
-    private void OnReplacedItemDrop(string item)
-    {
-        Trace?.Log($"新元素放在{item}下");
-    }
-
-    private void OnItemDrop(string item)
-    {
-        Trace?.Log($"{item}被放下");
-    }
-
-    private void OnItemDropRejected(string item)
-    {
-        Trace?.Log($"{item}被拒绝");
-    }
-
-    private void OnItemDropRejectedByMaxItemLimit(string item)
-    {
-        Trace?.Log($"{item}由于超过最大数量限制被禁止");
-    }
-
-    /// <summary>
-    ///
+    /// GetAttributes
     /// </summary>
     /// <returns></returns>
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
@@ -86,6 +33,10 @@ public partial class DragDrops
         },
     };
 
+    /// <summary>
+    /// GetMethods
+    /// </summary>
+    /// <returns></returns>
     private IEnumerable<MethodItem> GetMethods() => new MethodItem[]
     {
         new()
