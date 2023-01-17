7201266204697e6155408d06d323c325d94be586
Argo <argo@163.com>
2023-01-17 05:41:56 +0000

    !3769 test(#I6AUXY): add unit test for table ShowTips * test: 增加 ShowTips 单元测试

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -3604,6 +3604,32 @@ public class TableTest : TableTestBase
     }
 
     [Fact]
+    public void ShowTips_Ok()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var items = Foo.GenerateFoo(localizer, 2);
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Table<Foo>>(pb =>
+            {
+                pb.Add(a => a.RenderMode, TableRenderMode.Table);
+                pb.Add(a => a.Items, items);
+                pb.Add(a => a.TableColumns, foo => builder =>
+                {
+                    builder.OpenComponent<TableColumn<Foo, string>>(0);
+                    builder.AddAttribute(1, "Field", "Name");
+                    builder.AddAttribute(2, "FieldExpression", Utility.GenerateValueExpression(foo, "Name", typeof(string)));
+                    builder.AddAttribute(3, "Editable", true);
+                    builder.AddAttribute(7, "Text", "test");
+                    builder.AddAttribute(9, "ShowTips", true);
+                    builder.CloseComponent();
+                });
+            });
+        });
+        cut.Contains("data-bs-toggle=\"tooltip\"");
+    }
+
+    [Fact]
     public void TableColumn_Property()
     {
         var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
