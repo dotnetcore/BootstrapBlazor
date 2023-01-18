41e2b38c538b5bc5b0c5d8da0c94b47429cc4205
Argo-Asicotech <argo@163.com>
2023-01-18 11:36:46 +0800

    refactor: GetBytes 更改为扩展方法
    # Conflicts:
    #	src/BootstrapBlazor/Components/Upload/UploadFile.cs

--- src/BootstrapBlazor/Extensions/UploadFileExtensions.cs
+++ src/BootstrapBlazor/Extensions/UploadFileExtensions.cs
@@ -42,4 +42,82 @@ public static class UploadFileExtensions
             }
         }
     }
+
+    /// <summary>
+    /// 获得图片字节数组方法
+    /// </summary>
+    /// <param name="upload"></param>
+    /// <param name="format"></param>
+    /// <param name="maxWidth"></param>
+    /// <param name="maxHeight"></param>
+    /// <param name="maxAllowedSize"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [ExcludeFromCodeCoverage]
+    [Obsolete("Use GetBytes method")]
+    public static Task<byte[]?> GetByteArray(this UploadFile upload, string format, int maxWidth, int maxHeight, long maxAllowedSize = 512000, CancellationToken token = default) => upload.GetBytes(format, maxWidth, maxHeight, maxAllowedSize, token);
+
+    /// <summary>
+    /// 获得图片字节数组方法
+    /// </summary>
+    /// <param name="upload"></param>
+    /// <param name="format"></param>
+    /// <param name="maxWidth"></param>
+    /// <param name="maxHeight"></param>
+    /// <param name="maxAllowedSize"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [ExcludeFromCodeCoverage]
+    public static async Task<byte[]?> GetBytes(this UploadFile upload, string format, int maxWidth, int maxHeight, long maxAllowedSize = 512000, CancellationToken token = default)
+    {
+        byte[]? ret = null;
+        if (upload.File != null)
+        {
+            try
+            {
+                var imageFile = await upload.File.RequestImageFileAsync(format, maxWidth, maxHeight);
+                using var fileStream = imageFile.OpenReadStream(maxAllowedSize, token);
+                using var memoryStream = new MemoryStream();
+                await fileStream.CopyToAsync(memoryStream, token);
+                ret = memoryStream.ToArray();
+            }
+            catch (Exception ex)
+            {
+                upload.Code = 1004;
+                upload.Error = ex.Message;
+                upload.PrevUrl = null;
+            }
+        }
+        return ret;
+    }
+
+    /// <summary>
+    /// 获得图片字节数组方法
+    /// </summary>
+    /// <param name="upload"></param>
+    /// <param name="maxAllowedSize"></param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    [ExcludeFromCodeCoverage]
+    public static async Task<byte[]?> GetBytes(this UploadFile upload, long maxAllowedSize = 512000, CancellationToken token = default)
+    {
+        byte[]? ret = null;
+        if (upload.File != null)
+        {
+            try
+            {
+                using var fileStream = upload.File.OpenReadStream(maxAllowedSize, token);
+                using var memoryStream = new MemoryStream();
+                await fileStream.CopyToAsync(memoryStream, token);
+                ret = memoryStream.ToArray();
+            }
+            catch (Exception ex)
+            {
+                upload.Code = 1004;
+                upload.Error = ex.Message;
+                upload.PrevUrl = null;
+            }
+        }
+        return ret;
+    }
 }
