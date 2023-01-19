9bb99392296de8075bc02b8f729ff6bae54daa35
Argo Zhang <argo@163.com>
2023-01-20 00:27:58 +0800

    feat: 增加税票验真功能
    Co-authored-by: alex chow <zhouchuanglin@gmail.com>

--- src/Extensions/Components/BootstrapBlazor.BaiduOcr/Services/BaiduOcr.cs
+++ src/Extensions/Components/BootstrapBlazor.BaiduOcr/Services/BaiduOcr.cs
@@ -4,6 +4,7 @@
 
 using Baidu.Aip.Ocr;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace BootstrapBlazor.Components;
 
@@ -11,13 +12,17 @@ internal class BaiduOcr : IBaiduOcr
 {
     protected IOptionsMonitor<BaiduOcrOption> Options { get; set; }
 
+    protected IHttpClientFactory HttpClientFactory { get; set; }
+
     /// <summary>
     /// 构造函数
     /// </summary>
     /// <param name="options"></param>
-    public BaiduOcr(IOptionsMonitor<BaiduOcrOption> options)
+    /// <param name="httpClientFactory"></param>
+    public BaiduOcr(IOptionsMonitor<BaiduOcrOption> options, IHttpClientFactory httpClientFactory)
     {
         Options = options;
+        HttpClientFactory = httpClientFactory;
     }
 
     /// <summary>
@@ -55,6 +60,91 @@ internal class BaiduOcr : IBaiduOcr
         {
             ret.ErrorCode = resp.Value<int>("error_code");
             ret.ErrorMessage = resp.Value<string>("error_msg");
+        }
+        return ret;
+    }, token);
+
+    /// <summary>
+    /// 获得 Baidu AI AccessToken 方法
+    /// </summary>
+    /// <returns></returns>
+    protected async Task<string> GetAccessToken()
+    {
+        var client = HttpClientFactory.CreateClient();
+        var para = new Dictionary<string, string>()
+        {
+            { "grant_type", "client_credentials" },
+            { "client_id", Options.CurrentValue.ApiKey },
+            { "client_secret", Options.CurrentValue.Secret }
+        };
+
+        var resp = await client.PostAsync("https://aip.baidubce.com/oauth/2.0/token", new FormUrlEncodedContent(para));
+        var ret = await resp.Content.ReadAsStringAsync();
+        var doc = JObject.Parse(ret);
+        return doc.Value<string>("access_token") ?? string.Empty;
+    }
+
+    /// <summary>
+    /// 增值税发票验真方法
+    /// </summary>
+    /// <param name="invoiceCode">发票代码</param>
+    /// <param name="invoiceNum">发票号码</param>
+    /// <param name="invoiceDate">开票日期格式 YYYYMMDD</param>
+    /// <param name="invoiceType">发票种类 增值税专用发票：special_vat_invoice 增值税电子专用发票：elec_special_vat_invoice 增值税普通发票：normal_invoice 增值税普通发票（电子）：elec_normal_invoice 增值税普通发票（卷式）：roll_normal_invoice 通行费增值税电子普通发票：toll_elec_normal_invoice 区块链电子发票（目前仅支持深圳地区）：blockchain_invoice 全电发票（专用发票）：elec_invoice_special 全电发票（普通发票）：elec_invoice_normal 货运运输业增值税专用发票：special_freight_transport_invoice 机动车销售发票：motor_vehicle_invoice 二手车销售发票：used_vehicle_invoice</param>
+    /// <param name="checkCode">校验码 后六位</param>
+    /// <param name="totalAmount">发票金额</param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public Task<BaiduOcrResult<InvoiceVerifyResult>> VerifyInvoiceAsync(string invoiceCode, string invoiceNum, string invoiceDate, string invoiceType, string? checkCode, string? totalAmount, CancellationToken token = default) => Task.Run(async () =>
+    {
+        var token = await GetAccessToken();
+        var url = $"https://aip.baidubce.com/rest/2.0/ocr/v1/vat_invoice_verification?access_token={token}";
+        var client = HttpClientFactory.CreateClient();
+
+        // 拼装参数
+        var para = new Dictionary<string, string>()
+        {
+            { "invoice_code", invoiceCode },
+            { "invoice_num", invoiceNum },
+            { "invoice_date", invoiceDate },
+            { "invoice_type", invoiceType },
+        };
+
+        if (!string.IsNullOrEmpty(checkCode))
+        {
+            para["check_code"] = checkCode;
+        }
+        if (!string.IsNullOrEmpty(totalAmount))
+        {
+            para["total_amount"] = totalAmount;
+        }
+
+        // 提交数据
+        var resp = await client.PostAsync(url, new FormUrlEncodedContent(para));
+        var payload = await resp.Content.ReadAsStringAsync();
+        var doc = JObject.Parse(payload);
+
+        var ret = new BaiduOcrResult<InvoiceVerifyResult>();
+        if (doc.TryGetValue("words_result", out var v))
+        {
+            ret.Entity = new InvoiceVerifyResult()
+            {
+                MachineCode = doc.Value<string>("MachineCode"),
+                InvoiceNum = doc.Value<string>("InvoiceNum"),
+                InvoiceDate = doc.Value<string>("InvoiceDate"),
+                VerifyResult = doc.Value<string>("VerifyResult"),
+                InvalidSign = doc.Value<string>("InvalidSign"),
+                InvoiceCode = doc.Value<string>("InvoiceCode"),
+                InvoiceType = doc.Value<string>("InvoiceType"),
+                CheckCode = doc.Value<string>("CheckCode"),
+                VerifyMessage = doc.Value<string>("VerifyMessage"),
+                Invoice = v.ToObject(typeof(InvoiceEntity)) as InvoiceEntity
+            };
+        }
+        else
+        {
+            ret.ErrorCode = doc.Value<int>("error_code");
+            ret.ErrorMessage = doc.Value<string>("error_msg");
         }
         return ret;
     }, token);
--- src/Extensions/Components/BootstrapBlazor.BaiduOcr/Services/IBaiduOcr.cs
+++ src/Extensions/Components/BootstrapBlazor.BaiduOcr/Services/IBaiduOcr.cs
@@ -23,4 +23,17 @@ public interface IBaiduOcr
     /// <param name="token"></param>
     /// <returns></returns>
     Task<string> CheckVatInvoiceJsonAsync(byte[] image, CancellationToken token = default);
+
+    /// <summary>
+    /// 增值税发票验真方法
+    /// </summary>
+    /// <param name="invoiceCode">发票代码</param>
+    /// <param name="invoiceNum">发票号码</param>
+    /// <param name="invoiceDate">开票日期格式 YYYYMMDD</param>
+    /// <param name="invoiceType">发票种类 增值税专用发票：special_vat_invoice 增值税电子专用发票：elec_special_vat_invoice 增值税普通发票：normal_invoice 增值税普通发票（电子）：elec_normal_invoice 增值税普通发票（卷式）：roll_normal_invoice 通行费增值税电子普通发票：toll_elec_normal_invoice 区块链电子发票（目前仅支持深圳地区）：blockchain_invoice 全电发票（专用发票）：elec_invoice_special 全电发票（普通发票）：elec_invoice_normal 货运运输业增值税专用发票：special_freight_transport_invoice 机动车销售发票：motor_vehicle_invoice 二手车销售发票：used_vehicle_invoice</param>
+    /// <param name="checkCode">校验码</param>
+    /// <param name="totalAmount">发票金额</param>
+    /// <param name="token"></param>
+    /// <returns></returns>
+    public Task<BaiduOcrResult<InvoiceVerifyResult>> VerifyInvoiceAsync(string invoiceCode, string invoiceNum, string invoiceDate, string invoiceType, string? checkCode, string? totalAmount, CancellationToken token = default);
 }
