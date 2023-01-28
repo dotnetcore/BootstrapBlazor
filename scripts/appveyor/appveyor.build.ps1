bae35415898120e2480a3a91e85537a6210ed437
Argo <argo@live.ca>
2023-01-28 16:30:05 +0800

    chore: merge comment label action into auto assign action (#409)
    * chore: 移除不使用的脚本
    
    * chore: 增加文档
    
    * chore: 更新解决方案文档
    
    * chore: 消除预览版本警告信息
    
    * chore: 更新部署脚本
    
    * chore: 更新 linux 配置脚本
    
    * chore: 更新 nginx.conf 配置项
    
    * chore: 删除自动 comment 脚本
    
    * chore: 增加自动标签后根据不同标签自动 comment 功能
    
    * chore: 输出信息更改为首字母大写
    
    * chore: 增加 Reviewer 是仓库作者判断
    
    * chore: 移除对 auto_comment 脚本引用
    
    * revert: 撤销更改
    
    * chore: 增加 Reviewer 是仓库作者判断

--- scripts/appveyor/appveyor.build.ps1
+++
@@ -1,6 +0,0 @@
-﻿function runCmd ($cmd) {
-    write-host $cmd -ForegroundColor Cyan
-    cmd.exe /c $cmd
-}
-runCmd "dotnet build src\BootstrapBlazor.Server --configuration Release"
-runCmd "dotnet pack src\BootstrapBlazor --configuration Release"
