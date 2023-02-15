42d2c2f8e6b74e19a17d6db49e1db6126247b2c8
lambert lee <lish@live.cn>
2023-02-15 22:18:07 +0800

    doc: update the Layouts component demos

--- src/BootstrapBlazor.Shared/Samples/Layouts.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Layouts.razor.cs
@@ -5,166 +5,145 @@
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// Layouts
 /// </summary>
 public sealed partial class Layouts
 {
-    [Inject]
-    [NotNull]
-    private IStringLocalizer<Menus>? LocalizerMenu { get; set; }
-
-    [Inject]
-    [NotNull]
-    private IStringLocalizer<Layouts>? Localizer { get; set; }
-
-    private IEnumerable<MenuItem>? IconSideMenuItems { get; set; }
-
-    /// <summary>
-    /// OnInitializedAsync 方法
-    /// </summary>
-    /// <returns></returns>
-    protected override async Task OnInitializedAsync()
+    private IEnumerable<AttributeItem> GetAttributes() => new[]
     {
-        await base.OnInitializedAsync();
-
-        IconSideMenuItems = await MenusDataGerator.GetIconSideMenuItemsAsync(LocalizerMenu);
-    }
-
-    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
-    {
-            // TODO: 移动到数据库中
-            new AttributeItem() {
-                Name = "Header",
-                Description = Localizer["Desc1"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Side",
-                Description = Localizer["Desc2"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "SideWidth",
-                Description = Localizer["Desc3"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = "300px"
-            },
-            new AttributeItem() {
-                Name = "Main",
-                Description = Localizer["Desc4"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Footer",
-                Description = Localizer["Desc5"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Menus",
-                Description = Localizer["Desc6"],
-                Type = "IEnumerable<MenuItem>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "IsFullSide",
-                Description = Localizer["Desc7"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "IsPage",
-                Description = Localizer["Desc8"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "IsFixedFooter",
-                Description = Localizer["Desc9"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "IsFixedHeader",
-                Description = Localizer["Desc10"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "IsAccordion",
-                Description = Localizer["IsAccordion"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "ShowCollapseBar",
-                Description =  Localizer["Desc11"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "ShowFooter",
-                Description =  Localizer["Desc12"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "ShowGotoTop",
-                Description =  Localizer["Desc13"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "UseTabSet",
-                Description =  Localizer["Desc14"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "AdditionalAssemblies",
-                Description =  Localizer["Desc15"],
-                Type = "IEnumerable<Assembly>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "OnCollapsed",
-                Description =  Localizer["Desc16"],
-                Type = "Func<bool, Task>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "OnClickMenu",
-                Description =  Localizer["Desc17"],
-                Type = "Func<bool, MenuItem>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "TabDefaultUrl",
-                Description =  Localizer["TabDefaultUrl"],
-                Type = "string?",
-                ValueList = " — ",
-                DefaultValue = " — "
-            }
+        // TODO: 移动到数据库中
+        new AttributeItem() {
+            Name = "Header",
+            Description = Localizer["Layouts_Header_Description"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "Side",
+            Description = Localizer["Layouts_Side_Description"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "SideWidth",
+            Description = Localizer["Layouts_SideWidth_Description"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "300px"
+        },
+        new AttributeItem() {
+            Name = "Main",
+            Description = Localizer["Layouts_Main_Description"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "Footer",
+            Description = Localizer["Layouts_Footer_Description"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "Menus",
+            Description = Localizer["Layouts_Menus_Description"],
+            Type = "IEnumerable<MenuItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "IsFullSide",
+            Description = Localizer["Layouts_IsFullSide_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "IsPage",
+            Description = Localizer["Layouts_IsPage_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "IsFixedFooter",
+            Description = Localizer["Layouts_IsFixedFooter_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "IsFixedHeader",
+            Description = Localizer["Layouts_IsFixedHeader_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "IsAccordion",
+            Description = Localizer["Layouts_IsAccordion_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "ShowCollapseBar",
+            Description =  Localizer["Layouts_ShowCollapseBar_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "ShowFooter",
+            Description =  Localizer["Layouts_ShowFooter_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "ShowGotoTop",
+            Description =  Localizer["Layouts_ShowGotoTop_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "UseTabSet",
+            Description =  Localizer["Layouts_UseTabSet_Description"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "AdditionalAssemblies",
+            Description =  Localizer["Layouts_AdditionalAssemblies_Description"],
+            Type = "IEnumerable<Assembly>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "OnCollapsed",
+            Description =  Localizer["Layouts_OnCollapsed_Description"],
+            Type = "Func<bool, Task>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "OnClickMenu",
+            Description =  Localizer["Layouts_OnClickMenu_Description"],
+            Type = "Func<bool, MenuItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "TabDefaultUrl",
+            Description =  Localizer["Layouts_TabDefaultUrl_Description"],
+            Type = "string?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
     };
 }
