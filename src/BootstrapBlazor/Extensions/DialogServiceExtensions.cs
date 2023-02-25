da578d42d0c263063633adb9a19f014245c75d2a
Argo Zhang <argo@live.ca>
2023-02-26 02:31:17 +0800

    refactor: 精简代码
    # Conflicts:
    #	src/BootstrapBlazor/Extensions/DialogServiceExtensions.cs

--- src/BootstrapBlazor/Extensions/DialogServiceExtensions.cs
+++ src/BootstrapBlazor/Extensions/DialogServiceExtensions.cs
@@ -66,7 +66,6 @@ public static class DialogServiceExtensions
             [nameof(EditDialog<TModel>.ShowLoading)] = option.ShowLoading,
             [nameof(EditDialog<TModel>.ShowLabel)] = option.ShowLabel,
             [nameof(EditDialog<TModel>.Items)] = option.Items ?? Utility.GenerateColumns<TModel>(item => item.Editable),
-            [nameof(EditDialog<TModel>.OnCloseAsync)] = option.OnCloseAsync,
             [nameof(EditDialog<TModel>.OnSaveAsync)] = new Func<EditContext, Task>(async context =>
             {
                 if (option.OnEditAsync != null)
