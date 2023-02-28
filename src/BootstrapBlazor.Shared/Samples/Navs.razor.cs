60e26b1e43671e4b01f720de38ebc5e93c99ff10
Old Li <lish@live.cn>
2023-02-28 16:32:52 +0800

    doc: update nav demos

--- src/BootstrapBlazor.Shared/Samples/Navs.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Navs.razor.cs
@@ -7,31 +7,10 @@ using Microsoft.AspNetCore.Components.Routing;
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// Navs
 /// </summary>
 public sealed partial class Navs
 {
-    private IEnumerable<NavLink> Items => GetItems();
-
-    private IEnumerable<NavLink> GetItems()
-    {
-        var ret = new List<NavLink>();
-        var link = new NavLink();
-        var parameters = new Dictionary<string, object?>()
-        {
-            ["href"] = WebsiteOption.CurrentValue.AdminUrl,
-            ["class"] = "nav-link nav-item",
-            ["target"] = "_blank",
-            ["ChildContent"] = new RenderFragment(builder =>
-            {
-                builder.AddContent(0, "BootstrapAdmin");
-            })
-        };
-        link.SetParametersAsync(ParameterView.FromDictionary(parameters));
-        ret.Add(link);
-        return ret;
-    }
-
     /// <summary>
     /// 获得属性方法
     /// </summary>
@@ -41,42 +20,42 @@ public sealed partial class Navs
         // TODO: 移动到数据库中
         new AttributeItem() {
             Name = "ChildContent",
-            Description = Localizer["Desc1"],
+            Description = Localizer["NavsChildContent"],
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "Alignment",
-            Description = Localizer["Desc2"],
+            Description = Localizer["NavsAlignment"],
             Type = "Alignment",
             ValueList = "Left|Center|Right",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "IsVertical",
-            Description = Localizer["Desc3"],
+            Description = Localizer["NavsIsVertical"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "IsPills",
-            Description = Localizer["Desc4"],
+            Description = Localizer["NavsIsPills"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "IsFill",
-            Description = Localizer["Desc5"],
+            Description = Localizer["NavsIsFill"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "IsJustified",
-            Description = Localizer["Desc6"],
+            Description = Localizer["NavsIsJustified"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
