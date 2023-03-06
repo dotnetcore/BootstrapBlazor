3c831aeec4a8aafbc722785392b8238cff2cbfcf
Argo Zhang <argo@live.ca>
2023-03-06 12:50:40 +0800

    Revert "refactor: 调整 ValueChanged 触发顺序"
    This reverts commit 85659c9e3f91a58c670b6ca73aa9f9aac7dd2de4.

--- src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
+++ src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
@@ -299,18 +299,19 @@ public partial class DateTimeRange
         {
             Value.End = Value.End.AddDays(1).AddSeconds(-1);
         }
-        if (ValueChanged.HasDelegate)
+        if (OnValueChanged != null)
         {
-            await ValueChanged.InvokeAsync(Value);
+            await OnValueChanged(Value);
         }
         if (OnConfirm != null)
         {
             await OnConfirm(Value);
         }
-        if (OnValueChanged != null)
+        if (ValueChanged.HasDelegate)
         {
-            await OnValueChanged(Value);
+            await ValueChanged.InvokeAsync(Value);
         }
+
         if (IsNeedValidate && EditContext != null && FieldIdentifier != null)
         {
             EditContext.NotifyFieldChanged(FieldIdentifier.Value);
