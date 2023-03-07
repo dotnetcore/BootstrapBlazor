2bac8371c9fad3d9647ddfb87c51411c3d2fc501
Old Li <lish@live.cn>
2023-03-07 13:22:01 +0800

    doc: update Popconfirm demos #622

--- src/BootstrapBlazor.Shared/Samples/PopoverConfirms.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/PopoverConfirms.razor.cs
@@ -7,75 +7,10 @@ using Microsoft.AspNetCore.Components.Forms;
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// PopoverConfirms
 /// </summary>
 public sealed partial class PopoverConfirms
 {
-    [NotNull]
-    private Foo? Model { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [NotNull]
-    private BlockLogger? Trace { get; set; }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [NotNull]
-    private BlockLogger? Trace1 { get; set; }
-
-    /// <summary>
-    /// OnInitialized
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-
-        Model = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private Task OnClose()
-    {
-        // This method is called back when the confirm button is clicked, and this method will not be called when the cancel button is clicked
-        Trace.Log("OnClose Trigger");
-        return Task.CompletedTask;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    private Task OnConfirm()
-    {
-        // This method is called back when the confirm button is clicked, and this method will not be called when the cancel button is clicked
-        Trace.Log("OnConfirm Trigger");
-        return Task.CompletedTask;
-    }
-
-    private static Task OnAsyncConfirm() => Task.Delay(3000);
-
-    private async Task OnAsyncSubmit()
-    {
-        Trace1.Log("异步提交");
-        await Task.Delay(3000);
-    }
-
-    private async Task OnValidSubmit(EditContext context)
-    {
-        await Task.Delay(3000);
-        Trace1.Log("数据合规");
-    }
-
-    private Task OnInValidSubmit(EditContext context)
-    {
-        Trace1.Log("数据非法");
-        return Task.CompletedTask;
-    }
-
     /// <summary>
     /// Get property method
     /// </summary>
