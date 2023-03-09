9c212a1807e5692b0cf8de67f9afec5bdfbb7a1f
Argo Zhang <argo@live.ca>
2023-03-09 13:49:45 +0800

    feat: 增加 Meterial 扩展图标包

---
+++ src/Extensions/Components/BootstrapBlazor.Icons.Meterial/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// BootstrapBlazor 服务扩展类
+/// </summary>
+public static class IconMapperOptionsExtensions
+{
+    /// <summary>
+    /// 添加 Meterial 图标到系统
+    /// </summary>
+    /// <param name="options"></param>
+    public static void AddMeterialIconMapper(this IconMapperOptions options)
+    {
+        options.Items = new()
+        {
+            { BootstrapIcons.AnchorLinkIcon, "mdi mdi-link-variant" }
+        };
+    }
+}
