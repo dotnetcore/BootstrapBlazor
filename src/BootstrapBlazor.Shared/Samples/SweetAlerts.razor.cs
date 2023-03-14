7efd4e175227113af523debc64c81504d66bb993
Old Li <lish@live.cn>
2023-03-14 15:14:50 +0800

    doc: update SweetAlerts demos

--- src/BootstrapBlazor.Shared/Samples/SweetAlerts.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/SweetAlerts.razor.cs
@@ -7,111 +7,12 @@ using Microsoft.AspNetCore.Components.Web;
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// SweetAlerts
 /// </summary>
 public partial class SweetAlerts
 {
-    private Task OnSwal(SwalCategory cate) => SwalService.Show(new SwalOption()
-    {
-        Category = cate,
-        Title = "Sweet"
-    });
-
-    private Task ShowTitle() => SwalService.Show(new SwalOption()
-    {
-        Category = SwalCategory.Success,
-        Title = "Title"
-    });
-
-    private Task ShowContent() => SwalService.Show(new SwalOption()
-    {
-        Category = SwalCategory.Success,
-        Content = "Content"
-    });
-
-    [Inject]
-    [NotNull]
-    private SwalService? SwalService { get; set; }
-
-    private async Task ShowButtons()
-    {
-        var op = new SwalOption
-        {
-            Category = SwalCategory.Success,
-            Title = "Title",
-            Content = "Content",
-            ShowClose = false,
-            ButtonTemplate = new RenderFragment(builder =>
-            {
-                builder.OpenComponent<DialogCloseButton>(0);
-                builder.AddAttribute(1, nameof(Button.Text), "Close");
-                builder.CloseComponent();
-            })
-        };
-        await SwalService.Show(op);
-    }
-
-    private async Task ShowComponent()
-    {
-        var op = new SwalOption()
-        {
-            BodyTemplate = new RenderFragment(builder =>
-            {
-                builder.OpenElement(0, "div");
-                builder.AddAttribute(1, "class", "text-center");
-                builder.OpenComponent<Counter>(2);
-                builder.CloseComponent();
-                builder.CloseElement();
-            })
-        };
-        await SwalService.Show(op);
-    }
-
-    private async Task ShowFooterComponent()
-    {
-        var op = new SwalOption()
-        {
-            Category = SwalCategory.Error,
-            Title = "Oops...",
-            Content = "Something went wrong!",
-            ShowFooter = true,
-            FooterTemplate = BootstrapDynamicComponent.CreateComponent<SwalFooter>().Render()
-        };
-        await SwalService.Show(op);
-    }
-
-    private async Task ShowAutoCloseSwal()
-    {
-        var op = new SwalOption()
-        {
-            Category = SwalCategory.Error,
-            Title = "Oops...",
-            Content = "Something went wrong!",
-            ShowFooter = true,
-            IsAutoHide = true,
-            Delay = 4000,
-            FooterTemplate = BootstrapDynamicComponent.CreateComponent<SwalFooter>().Render()
-        };
-        await SwalService.Show(op);
-    }
-
-    [NotNull]
-    private BlockLogger? Trace { get; set; }
-
-    private async Task ShowModal()
-    {
-        var op = new SwalOption()
-        {
-            Title = Localizer["SwalOptionTitle"],
-            Content = Localizer["SwalOptionContent"]
-        };
-        var ret = await SwalService.ShowModal(op);
-
-        Trace.Log($"{Localizer["SwalConsoleInfo"]}：{ret}");
-    }
-
     /// <summary>
-    /// 
+    /// GetAttributes
     /// </summary>
     /// <returns></returns>
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
