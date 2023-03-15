e65c44bf27fb30c3d846de5cad6123041e92a4fd
zhangpeihang <948869991@qq.com>
2023-03-15 19:24:48 +0800

    feat: 添加  repeater  组件

---
+++ src/BootstrapBlazor/Components/Repeater/Repeater.razor.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="TValue"></typeparam>
+public partial class Repeater<TValue>
+{
+
+    private string? RepeaterClassString => CssBuilder.Default("repeater")
+        .Build();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public IEnumerable<TValue>? Items { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public RenderFragment<IEnumerable<TValue>>? BodyTemplate { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public RenderFragment? HeaderTemplate { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public RenderFragment? FooterTemplate { get; set; }
+}
