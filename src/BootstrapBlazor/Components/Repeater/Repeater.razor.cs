44d928b4942cfa3971c3b388b05f878bf2aa2904
Argo Zhang <argo@live.ca>
2023-03-16 15:02:35 +0800

    feat: 增加 EmptyTemplate

--- src/BootstrapBlazor/Components/Repeater/Repeater.razor.cs
+++ src/BootstrapBlazor/Components/Repeater/Repeater.razor.cs
@@ -2,13 +2,15 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.Extensions.Localization;
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// Repeat 组件
 /// </summary>
-/// <typeparam name="TValue"></typeparam>
-public partial class Repeater<TValue>
+/// <typeparam name="TItem"></typeparam>
+public partial class Repeater<TItem>
 {
     private string? RepeaterClassString => CssBuilder.Default("repeater")
         .AddClassFromAttributes(AdditionalAttributes)
@@ -18,7 +20,7 @@ public partial class Repeater<TValue>
     /// 获得/设置 数据源
     /// </summary>
     [Parameter]
-    public IEnumerable<TValue>? Items { get; set; }
+    public IEnumerable<TItem>? Items { get; set; }
 
     /// <summary>
     /// 获得/设置 是否显示正在加载信息 默认 true 显示
@@ -30,11 +32,43 @@ public partial class Repeater<TValue>
     /// 获得/设置 正在加载模板
     /// </summary>
     [Parameter]
-    public RenderFragment<TValue>? LoadingTemplate { get; set; }
+    public RenderFragment? LoadingTemplate { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否显示无数据信息 默认 true 显示
+    /// </summary>
+    [Parameter]
+    public bool ShowEmpty { get; set; } = true;
+
+    /// <summary>
+    /// 获得/设置 无数据时提示信息 默认 null
+    /// </summary>
+    [Parameter]
+    public string? EmptyText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 正在加载模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? EmptyTemplate { get; set; }
 
     /// <summary>
     /// 获得/设置 模板
     /// </summary>
     [Parameter]
-    public RenderFragment<TValue>? ItemTemplate { get; set; }
+    public RenderFragment<TItem>? ItemTemplate { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Repeater<TItem>>? Localizer { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        EmptyText ??= Localizer[nameof(EmptyText)];
+    }
 }
