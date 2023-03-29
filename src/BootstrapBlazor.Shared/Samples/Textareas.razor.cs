f987da00eb83b275a59b8f4c520cbcaf8b4c1bf9
Argo Zhang <argo@live.ca>
2023-03-29 11:33:30 +0800

    doc: 更新注释文档

--- src/BootstrapBlazor.Shared/Samples/Textareas.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Textareas.razor.cs
@@ -9,64 +9,61 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Textareas
 {
-    private IEnumerable<AttributeItem> GetAttributes()
+    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        return new AttributeItem[]
+        new AttributeItem() {
+            Name = "ShowLabel",
+            Description = Localizer["TextareasShowLabel"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem() {
+            Name = "DisplayText",
+            Description = Localizer["TextareasDisplayText"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem()
         {
-            new AttributeItem() {
-                Name = "ShowLabel",
-                Description = Localizer["TextareasShowLabel"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem() {
-                Name = "DisplayText",
-                Description = Localizer["TextareasDisplayText"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem()
-            {
-                Name = "IsDisabled",
-                Description = Localizer["TextareasIsDisabled"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-            new AttributeItem()
-            {
-                Name = "ScrollToTop",
-                Description = Localizer["TextareasScrollToTop"],
-                Type = "Task",
-                ValueList = "-",
-                DefaultValue = "-"
-            },
-            new AttributeItem()
-            {
-                Name = "ScrollTo",
-                Description = Localizer["TextareasScrollTo"],
-                Type = "Task",
-                ValueList = "-",
-                DefaultValue = "-"
-            },
-            new AttributeItem()
-            {
-                Name = "ScrollToBottom",
-                Description = Localizer["TextareasScrollToBottom"],
-                Type = "Task",
-                ValueList = "-",
-                DefaultValue = "-"
-            },
-            new AttributeItem(){
-                Name = nameof(BootstrapBlazor.Components.Textarea.IsAutoScroll),
-                Description = Localizer["TextareasAutoScroll"],
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "false"
-            },
-
-        };
-    }
+            Name = "IsDisabled",
+            Description = Localizer["TextareasIsDisabled"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new AttributeItem()
+        {
+            Name = "ScrollToTop",
+            Description = Localizer["TextareasScrollToTop"],
+            Type = "Task",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem()
+        {
+            Name = "ScrollTo",
+            Description = Localizer["TextareasScrollTo"],
+            Type = "Task",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem()
+        {
+            Name = "ScrollToBottom",
+            Description = Localizer["TextareasScrollToBottom"],
+            Type = "Task",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem(){
+            Name = nameof(BootstrapBlazor.Components.Textarea.IsAutoScroll),
+            Description = Localizer["TextareasAutoScroll"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        }
+    };
 }
+
