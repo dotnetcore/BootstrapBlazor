9779f91420e31f071707e8d7b9896f35cedb0cec
Argo Zhang <argo@live.ca>
2023-03-31 11:35:33 +0800

    doc(Toast): update Toast document (#928)
    * feat: 移除 Toast 组件对 text-color 的样式依赖
    
    * doc: 更新 Toast 组件示例文档

--- src/BootstrapBlazor/Components/Toast/Toast.razor.cs
+++ src/BootstrapBlazor/Components/Toast/Toast.razor.cs
@@ -12,30 +12,37 @@ public partial class Toast
     /// <summary>
     /// 获得/设置 弹出框类型
     /// </summary>
-    protected string? AutoHide => Options.IsAutoHide ? null : "false";
+    private string? AutoHide => Options.IsAutoHide ? null : "false";
 
     /// <summary>
     /// 获得/设置 弹出框类型
     /// </summary>
-    protected string? ClassString => CssBuilder.Default("toast")
+    private string? ClassString => CssBuilder.Default("toast")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
 
     /// <summary>
     /// 获得/设置 进度条样式
     /// </summary>
-    protected string? ProgressClass => CssBuilder.Default("toast-progress")
+    private string? ProgressClass => CssBuilder.Default("toast-progress")
         .AddClass($"bg-{Options.Category.ToDescriptionString()}")
         .Build();
 
     /// <summary>
     /// 获得/设置 图标样式
     /// </summary>
-    protected string? IconString => CssBuilder.Default()
+    private string? IconString => CssBuilder.Default()
         .AddClass(Options.SuccessIcon, Options.Category == ToastCategory.Success)
         .AddClass(Options.InformationIcon, Options.Category == ToastCategory.Information)
         .AddClass(Options.ErrorIcon, Options.Category == ToastCategory.Error)
         .AddClass(Options.WarningIcon, Options.Category == ToastCategory.Warning)
+        .Build();
+
+    private string? IconBarString => CssBuilder.Default("toast-bar me-2")
+        .AddClass("text-success", Options.Category == ToastCategory.Success)
+        .AddClass("text-info", Options.Category == ToastCategory.Information)
+        .AddClass("text-danger", Options.Category == ToastCategory.Error)
+        .AddClass("text-warning", Options.Category == ToastCategory.Warning)
         .Build();
 
     /// <summary>
@@ -76,12 +83,20 @@ public partial class Toast
     {
         base.OnInitialized();
 
+        Options.Toast = this;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
         Options.SuccessIcon ??= IconTheme.GetIconByKey(ComponentIcons.ToastSuccessIcon);
         Options.InformationIcon ??= IconTheme.GetIconByKey(ComponentIcons.ToastInformationIcon);
         Options.WarningIcon ??= IconTheme.GetIconByKey(ComponentIcons.ToastWarningIcon);
         Options.ErrorIcon ??= IconTheme.GetIconByKey(ComponentIcons.ToastErrorIcon);
-
-        Options.Toast = this;
     }
 
     /// <summary>
