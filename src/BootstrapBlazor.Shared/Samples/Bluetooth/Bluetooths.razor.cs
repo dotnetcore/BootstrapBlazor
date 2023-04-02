69a1eb47c706e1753725b33350d5240f7f825461
Old li <lish@live.cn>
2023-04-02 15:01:25 +0800

    doc: update Bluetooths demos

--- src/BootstrapBlazor.Shared/Samples/Bluetooth/Bluetooths.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Bluetooth/Bluetooths.razor.cs
@@ -8,61 +8,10 @@ using BootstrapBlazor.Shared.Common;
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// 
+/// Bluetooths
 /// </summary>
 public partial class Bluetooths
 {
-    Printer printer { get; set; } = new Printer();
-
-    /// <summary>
-    /// 显示内置界面
-    /// </summary>
-    bool ShowUI { get; set; } = false;
-
-    private string? message;
-    private string? statusmessage;
-    private string? errmessage;
-
-    private Task OnResult(string message)
-    {
-        this.message = message;
-        StateHasChanged();
-        return Task.CompletedTask;
-    }
-
-    private Task OnUpdateStatus(string message)
-    {
-        this.statusmessage = message;
-        StateHasChanged();
-        return Task.CompletedTask;
-    }
-
-    private Task OnError(string message)
-    {
-        this.errmessage = message;
-        StateHasChanged();
-        return Task.CompletedTask;
-    }
-
-    private Task OnGetDevices(List<string>? devices)
-    {
-        this.message = "";
-        if (devices == null || devices!.Count == 0) return Task.CompletedTask;
-        this.message += $"已配对设备{devices.Count}:{Environment.NewLine}";
-        devices.ForEach(a => this.message += $"   {a}{Environment.NewLine}");
-        //this.message = this.message.Replace(Environment.NewLine, "<br/>");
-        StateHasChanged();
-        return Task.CompletedTask;
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    public void SwitchUI()
-    {
-        ShowUI = !ShowUI;
-    }
-
     /// <summary>
     /// 获得属性方法
     /// </summary>
