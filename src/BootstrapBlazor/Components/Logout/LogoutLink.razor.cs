c8db54ff872ec65cd8d1a0ab713529cdeabc5c12
Argo Zhang <argo@live.ca>
2023-04-02 11:43:40 +0800

    refactor: 精简 LogoutLink 代码

--- src/BootstrapBlazor/Components/Logout/LogoutLink.razor.cs
+++ src/BootstrapBlazor/Components/Logout/LogoutLink.razor.cs
@@ -13,10 +13,6 @@ public partial class LogoutLink
 {
     [Inject]
     [NotNull]
-    private NavigationManager? NavigationManager { get; set; }
-
-    [Inject]
-    [NotNull]
     private IStringLocalizer<LogoutLink>? Localizer { get; set; }
 
     /// <summary>
@@ -37,7 +33,7 @@ public partial class LogoutLink
     /// </summary>
     [Parameter]
     [NotNull]
-    public string Url { get; set; } = "/Account/Logout";
+    public string? Url { get; set; }
 
     /// <summary>
     /// 获得/设置 是否强制加载导航页面 默认 true 
@@ -59,7 +55,7 @@ public partial class LogoutLink
 
         Text ??= Localizer[nameof(Text)];
         Icon ??= IconTheme.GetIconByKey(ComponentIcons.LogoutLinkIcon);
-    }
 
-    private void OnLogout() => NavigationManager.NavigateTo(Url, ForceLoad);
+        Url ??= "/Account/Logout";
+    }
 }
