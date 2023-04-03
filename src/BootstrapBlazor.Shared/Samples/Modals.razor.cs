079779d8caadd38f1cf39089898a3b33de2cbd49
Old Li <33386249+azlis@users.noreply.github.com>
2023-04-03 09:52:41 +0800

    doc(Modal): update documentation for localizer (#945)
    * doc(Modal): Update Modal Attribute Document
    
    * doc(Modal): Update Modal Attribute Document
    
    Co-authored-by: Argo Zhang <argo@live.ca>

--- src/BootstrapBlazor.Shared/Samples/Modals.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Modals.razor.cs
@@ -13,130 +13,130 @@ public sealed partial class Modals
     /// GetAttributes
     /// </summary>
     /// <returns></returns>
-    private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         new AttributeItem() {
             Name = "FirstAfterRenderCallbackAsync",
-            Description = "Modal first after render callback",
-            Type = "Func<Task>",
+            Description = Localizer["ModalsAttributesFirstAfterRenderCallbackAsync"],
+            Type = "Func<Modal,Task>",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "HeaderTemplate",
-            Description = "Modal body ModalHeader template",
+            Description = Localizer["ModalsAttributeHeaderTemplate"],
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "BodyTemplate",
-            Description = "Modal body ModalBody component",
+            Description = Localizer["ModalsAttributeBodyTemplate"],
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "ChildContent",
-            Description = "Content",
+                Description = Localizer["ModalsAttributeChildContent"],
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "FooterTemplate",
-            Description = "ModalFooter component at the bottom of the modal",
+            Description = Localizer["ModalsAttributeFooterTemplate"],
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
             Name = "IsBackdrop",
-            Description = "Whether to close the popup in the background",
+              Description = Localizer["ModalsAttributeIsBackdrop"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "IsKeyboard",
-            Description = "Whether to respond to ESC keyboard",
+            Description = Localizer["ModalsAttributeIsKeyboard"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = "IsCentered",
-            Description = "Whether to center vertically",
+            Description = Localizer["ModalsAttributeIsCentered"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = "IsScrolling",
-            Description = "Whether to scroll when the text of the pop-up window is too long",
+                    Description = Localizer["ModalsAttributeIsScrolling"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "IsFade",
-            Description = "Whether to enable the fade-in and fade-out animation effect",
+                       Description = Localizer["ModalsAttributeIsFade"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = "IsDraggable",
-            Description = "Whether to enable the draggable effect",
+                     Description = Localizer["ModalsAttributeIsDraggable"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "ShowCloseButton",
-            Description = "whether to show the close button",
+              Description = Localizer["ModalsAttributeShowCloseButton"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = "ShowFooter",
-            Description = "Whether to show Footer",
+            Description = Localizer["ModalsAttributeShowFooter"],
             Type = "boolean",
             ValueList = " — ",
             DefaultValue = "true"
         },
         new AttributeItem() {
             Name = "Size",
-            Description = "Size",
+            Description = Localizer["ModalsAttributeSize"],
             Type = "Size",
             ValueList = "None / ExtraSmall / Small / Medium / Large / ExtraLarge / ExtraExtraLarge",
             DefaultValue = "ExtraExtraLarge"
         },
         new AttributeItem() {
             Name = nameof(ModalDialog.FullScreenSize),
-            Description = "Full screen when smaller than a certain size",
+            Description = Localizer["ModalsAttributeFullScreenSize"],
             Type = "Size",
             ValueList = "None / Always / Small / Medium / Large / ExtraLarge / ExtraExtraLarge",
             DefaultValue = "None"
         },
         new AttributeItem() {
             Name = "Title",
-            Description = "Popup title",
+            Description = Localizer["ModalsAttributeTitle"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " not set "
         },
         new AttributeItem() {
             Name = nameof(ModalDialog.ShowMaximizeButton),
-            Description = "Whether to show the popup maximize button",
+            Description = Localizer["ModalsAttributeShowMaximizeButton"],
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "false"
         },
         new AttributeItem() {
             Name = "ShownCallbackAsync",
-            Description = "弹窗已显示回调方法",
+            Description = Localizer["ModalsAttributeShownCallbackAsync"],
             Type = "Func<Task>",
             ValueList = " — ",
             DefaultValue = " — "
