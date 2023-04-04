386448c12b012be114ded4d3a4ee46ff4432edd8
Argo Zhang <argo@live.ca>
2023-04-04 18:14:44 +0800

    refactor: 代码重构增加 IsHtml 参数

---
+++ src/BootstrapBlazor.Shared/Components/BlockLogger.razor.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.Collections.Concurrent;
+
+namespace BootstrapBlazor.Shared.Components;
+
+/// <summary>
+/// Logger 组件
+/// </summary>
+public partial class BlockLogger
+{
+    /// <summary>
+    /// 获得/设置 用户自定义属性
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// 获得/设置 最大行数 默认 3 行
+    /// </summary>
+    [Parameter]
+    public int Max { get; set; } = 3;
+
+    /// <summary>
+    /// 获得/设置 是否为 Html 代码 默认 false
+    /// </summary>
+    [Parameter]
+    public bool IsHtml { get; set; }
+
+    private ConcurrentQueue<string> Message { get; set; } = new();
+
+    /// <summary>
+    /// 获得 按钮样式集合
+    /// </summary>
+    /// <returns></returns>
+    private string? ClassName => CssBuilder.Default("card-log").AddClassFromAttributes(AdditionalAttributes).AddClass(Class).Build();
+
+    private string Class { get; set; } = "collapse";
+
+    /// <summary>
+    /// 输入日志方法
+    /// </summary>
+    /// <param name="message"></param>
+    public void Log(string message)
+    {
+        Message.Enqueue($"{DateTimeOffset.Now}: {message}");
+        Class = "";
+        if (Message.Count > Max)
+        {
+            Message.TryDequeue(out _);
+        }
+        StateHasChanged();
+    }
+}
