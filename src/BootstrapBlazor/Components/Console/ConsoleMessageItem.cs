23b11292f8cfeb55ad0d299e946d5771fb8d9ac0
Argo Zhang <argo@live.ca>
2023-04-05 12:11:52 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Components/Console/ConsoleMessageItem.cs
+++ src/BootstrapBlazor/Components/Console/ConsoleMessageItem.cs
@@ -12,10 +12,10 @@ public class ConsoleMessageItem
     /// <summary>
     /// 获得/设置 控制台输出消息
     /// </summary>
-    public string Message { get; set; } = "";
+    public string? Message { get; set; }
 
     /// <summary>
     /// 获得/设置 控制台消息颜色 默认为 White 白色
     /// </summary>
-    public Color Color { get; set; } = Color.None;
+    public Color Color { get; set; }
 }
