92e5b0745cd25cc2b50e247f9183f99d24a4898b
Old li <lish@live.cn>
2023-04-07 21:19:24 +0800

    doc(LineChart): 新增一个通过JS生成LineChart的Demo

---
+++ src/BootstrapBlazor.Shared/Demos/Charts/Line/LineChartJS.razor.js
@@ -0,0 +1,48 @@
+﻿//通过相对路径导入BootstrapBlazor.Chart的Chart.JS模块
+import '/../_content/BootstrapBlazor.Chart/js/bootstrap.blazor.chart.bundle.min.js'
+
+export function lineChart(chartData) {
+    const ctx = document.getElementById('lineChartJS');
+
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'BootStrapBlazor.Chart',
+            data: chartData,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            animations: {
+                tension: {
+                    duration: 1000,
+                    easing: 'linear',
+                    from: 1,
+                    to: 0,
+                    loop: true
+                }
+            },
+            scales: {
+                y: {
+                    min: 0,
+                    max: 100
+                }
+            }
+        }
+    };
+
+    var chart = new Chart(ctx, config);
+    window['lineChart'] = chart;
+}
+
+export function randomize(chartData) {
+    window['lineChart'].data.datasets.forEach(dataset => {
+        dataset.data = chartData;
+    });
+    window['lineChart'].update();
+}
