b6d092a8f32b54e1bb81436eb0dfcddad78e480e
Old Li <33386249+azlis@users.noreply.github.com>
2023-04-08 21:38:56 +0800

    doc(LineChart): add a demo to generate charts using the Chart.JS (#969)
    * doc(LineChart): 新增一个通过JS生成LineChart的Demo
    
    * doc(LineChartJS): 实现DisposeAsync
    
    * doc(LineChart): canvas id 改为参数带入js
    
    * refactor: 增加 Dispose 方法
    
    * refactor: 重构代码
    
    * refactor: 重构 JS 脚本
    
    * refactor: 格式化文档间隙
    
    * refactor: 重构读取 JS 脚本代码适配 linux
    
    ---------
    
    Co-authored-by: Argo Zhang <argo@live.ca>

---
+++ src/BootstrapBlazor.Shared/Demos/Charts/Line/LineChartJS.razor.js
@@ -0,0 +1,49 @@
+﻿//通过相对路径导入BootstrapBlazor.Chart的Chart.JS模块
+import '../../../../../_content/BootstrapBlazor.Chart/js/bootstrap.blazor.chart.bundle.min.js'
+
+const instance = []
+
+export function lineChart(canvasId, chartData) {
+    const ctx = document.getElementById(canvasId);
+
+    const data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'BootStrapBlazor.Chart',
+            data: chartData,
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            animations: {
+                tension: {
+                    duration: 1000,
+                    easing: 'linear',
+                    from: 1,
+                    to: 0,
+                    loop: true
+                }
+            },
+            scales: {
+                y: {
+                    min: 0,
+                    max: 100
+                }
+            }
+        }
+    };
+
+    instance[canvasId] = new Chart(ctx, config);
+}
+
+export function randomize(canvasId, chartData) {
+    instance[canvasId].data.datasets.forEach(dataset => {
+        dataset.data = chartData;
+    });
+    instance[canvasId].update();
+}
