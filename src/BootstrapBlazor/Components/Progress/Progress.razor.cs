257311126c16034eecd02b1b30b846c85cddc195
Argo-AscioTech <argo@live.ca>
2023-04-12 11:28:31 +0800

    refactor: 精简 ProgressBase 类

--- src/BootstrapBlazor/Components/Progress/Progress.razor.cs
+++ src/BootstrapBlazor/Components/Progress/Progress.razor.cs
@@ -9,6 +9,57 @@ namespace BootstrapBlazor.Components;
 /// </summary>
 public partial class Progress
 {
+    /// <summary>
+    /// 获得/设置 控件高度 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public int? Height { get; set; }
+
+    /// <summary>
+    /// 获得/设置 颜色 默认为 Color.Primary
+    /// </summary>
+    [Parameter]
+    public Color Color { get; set; } = Color.Primary;
+
+    /// <summary>
+    /// 获得/设置 是否显示进度条值 默认 false
+    /// </summary>
+    /// <value></value>
+    [Parameter]
+    public bool IsShowValue { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否显示为条纹 默认 false
+    /// </summary>
+    /// <value></value>
+    [Parameter]
+    public bool IsStriped { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否动画 默认 false
+    /// </summary>
+    /// <value></value>
+    [Parameter]
+    public bool IsAnimated { get; set; }
+
+    /// <summary>
+    /// 获得/设置 组件进度值
+    /// </summary>
+    [Parameter]
+    public double Value { get; set; }
+
+    /// <summary>
+    /// 获得/设置 进度值修约小数位数, 默认 0 (即保留为整数)
+    /// </summary>
+    [Parameter]
+    public int Round { get; set; }
+
+    /// <summary>
+    /// 获得/设置 进度标签文本
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
     private string? ClassString => CssBuilder.Default("progress")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
@@ -27,7 +78,7 @@ public partial class Progress
     /// 获得 Style 集合
     /// </summary>
     private string? StyleName => CssBuilder.Default()
-        .AddClass($"width: {Math.Round(Value, Round)}%;")
+        .AddClass($"width: {InternalValue}%;")
         .Build();
 
     /// <summary>
@@ -37,15 +88,12 @@ public partial class Progress
         .AddClass($"height: {Height}px;", Height.HasValue)
         .Build();
 
-    /// <summary>
-    /// 获得 当前值
-    /// </summary>
-    private string ValueString => Math.Round(Value, Round).ToString();
+    private double InternalValue => Round == 0 ? Value : Math.Round(Value, Round, MidpointRounding.AwayFromZero);
 
     /// <summary>
     /// 获得 当前值百分比标签文字
     /// </summary>
-    private string? ValueLabelString => IsShowValue ? string.IsNullOrEmpty(Text) ? $"{Math.Round(Value, Round)}%" : Text : null;
+    private string? ValueLabelString => IsShowValue ? string.IsNullOrEmpty(Text) ? $"{InternalValue}%" : Text : null;
 
     /// <summary>
     /// OnParametersSet 方法
