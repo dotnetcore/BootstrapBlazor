87221e08fe7249130d022ff9fb9dee4e32fc6326
Argo-AscioTech <argo@live.ca>
2023-04-16 00:57:36 +0800

    refactor: 重构代码使用脚本隔离

--- src/Extensions/Components/BootstrapBlazor.Topology/Components/Topology/Topology.razor.js
+++ src/Extensions/Components/BootstrapBlazor.Topology/Components/Topology/Topology.razor.js
@@ -1,75 +1,70 @@
 ﻿import '../../topology.js'
 import Data from '../../../BootstrapBlazor/modules/data.js'
-import EventHandler from "../../../BootstrapBlazor/modules/event-handler.js"
 
-export class BlazorTopology extends BlazorComponent {
-    _init() {
-        if (window.Topology) {
-            Topology.prototype.lock = function (status) {
-                this.store.data.locked = status
-                this.finishDrawLine(!0)
-                this.canvas.drawingLineName = ""
-                this.stopPencil()
-            }
-
-            const obj = this._config.arguments[0]
-            const data = this._config.arguments[1]
-            const method = this._config.arguments[2]
-            const isSupportTouch = this._config.supportTouch === true;
-            const isFitView = this._config.fitView === true;
-            const isCenterView = this._config.centerView === true;
-            this._topology = new Topology(this._element, {}, isSupportTouch)
-            this._topology.connectSocket = function () {
-            }
-            this._topology.open(JSON.parse(data))
-            this._topology.lock(1)
-            if (isFitView) {
-                this._topology.fitView()
-            }
-            if (isCenterView) {
-                this._topology.centerView()
-            }
-            obj.invokeMethodAsync(method)
-        }
+export function init(el, invoker, data, callback) {
+    Topology.prototype.lock = function (status) {
+        this.store.data.locked = status
+        this.finishDrawLine(!0)
+        this.canvas.drawingLineName = ""
+        this.stopPencil()
     }
 
-    _execute(args) {
-        if (this._topology) {
-            var methodName = args[1];
-            if (methodName === "reset") {
-                this._topology.fitView()
-                this._topology.centerView()
-            }
-            else if (methodName === "scale" && args.length > 2) {
-                var t = args[2]
-                if (t > 0) {
-                    this._topology.scale(t)
-                    this._topology.centerView()
-                }
-            }
-            else if (methodName == "resize") {
-                this._topology.canvas.dirty = true
-                if (args.lengh > 4) {
-                    const width = args[2]
-                    const height = args[3]
-                    this._topology.resize(width, height)
-                }
-                else {
-                    this._topology.resize()
-                }
-                this._topology.fitView()
-                this._topology.centerView()
-            }
-            else {
-                this._topology.doSocket(JSON.stringify(methodName))
-            }
-        }
-    }
+    const meta = {}
+    Data.set(el, meta)
 
-    _dispose() {
-        if (this._topology) {
-            this._topology.destroy()
-            this._topology = null
-        }
+    const isSupportTouch = el.getAttribute('data-bb-support-touch') === 'true'
+    const isFitView = el.getAttribute('data-bb-fit-view') === 'true'
+    const isCenterView = el.getAttribute('data-bb-center-view') === 'true'
+
+    meta._topology = new Topology(el, {}, isSupportTouch)
+    meta._topology.connectSocket = function () {
     }
+    meta._topology.open(JSON.parse(data))
+    meta._topology.lock(1)
+    if (isFitView) {
+        meta._topology.fitView()
+    }
+    if (isCenterView) {
+        meta._topology.centerView()
+    }
+    invoker.invokeMethodAsync(method)
+}
+
+export function update(el, data) {
+    const meta = Data.get(el)
+    meta._topology.doSocket(JSON.stringify(data))
+}
+
+export function scale(el, rate) {
+    const meta = Data.get(el)
+    meta._topology.scale(rate)
+    meta._topology.centerView()
+}
+
+export function reset(el) {
+    const meta = Data.get(el)
+    meta._topology.fitView()
+    meta._topology.centerView()
+}
+
+export function resize(el, width, height) {
+    const meta = Data.get(el)
+    meta._topology.canvas.dirty = true
+    if (width !== null && height !== null) {
+        meta._topology.resize(width, height)
+    }
+    else {
+        meta._topology.resize()
+    }
+    meta._topology.fitView()
+    meta._topology.centerView()
+}
+
+export function dispose(el) {
+    const meta = Data.get(el)
+    if (meta._topology) {
+        meta._topology.destroy()
+        delete meta._topology
+    }
+    Data.remove(el)
 }
