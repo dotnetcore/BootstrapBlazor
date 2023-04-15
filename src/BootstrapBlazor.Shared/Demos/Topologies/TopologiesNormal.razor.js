bc3f81d833804ff6f3bb7f8e093e69c1e77d4e3c
Argo-AscioTech <argo@live.ca>
2023-04-16 01:41:57 +0800

    refactor: HMI 示例脚本更改为隔离模式

---
+++ src/BootstrapBlazor.Shared/Demos/Topologies/TopologiesNormal.razor.js
@@ -0,0 +1,20 @@
+﻿import Data from '../../../BootstrapBlazor/modules/data.js'
+
+export function init(el, invoker, callback) {
+    const meta = {}
+    Data.set(el, meta)
+
+    window.BlazorDemoTopology = {}
+    window.BlazorDemoTopology.handler = tagName => {
+        const element = el.querySelector('.topology > div')
+        invoker.invokeMethodAsync(callback, tagName)
+    }
+}
+
+export function execute() {
+    window.topology.setOptions({ hoverColor: '', hoverCursor: '', activeColor: '' });
+}
+
+export function dispose(el) {
+    Data.remove(el)
+}
