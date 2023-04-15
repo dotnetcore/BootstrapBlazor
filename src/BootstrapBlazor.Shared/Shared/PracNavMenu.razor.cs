b89be757e9509cec542863eb7e4d59d24754279c
Old Li <lish@live.cn>
2023-04-15 09:25:11 +0800

    add PracLaout,新增实战分类

---
+++ src/BootstrapBlazor.Shared/Shared/PracNavMenu.razor.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Shared.Services;
+
+namespace BootstrapBlazor.Shared.Shared;
+
+/// <summary>
+///
+/// </summary>
+[JSModuleAutoLoader("menu", ModuleName = "Menu")]
+public partial class PracNavMenu
+{
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<App>? AppLocalizer { get; set; }
+
+    [Inject]
+    [NotNull]
+    private TitleService? TitleService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<NavMenu>? Localizer { get; set; }
+
+    [Inject]
+    [NotNull]
+    private MenuService? MenuService { get; set; }
+
+    [NotNull]
+    private List<MenuItem>? Menus { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Menus = new List<MenuItem>
+        {
+            new MenuItem() { Text="仪表盘 Dashboard",Url="dashboard"}
+        };
+
+
+    }
+
+    private async Task OnClickMenu(MenuItem item)
+    {
+        if (!item.Items.Any() && !string.IsNullOrEmpty(item.Text))
+        {
+            await TitleService.SetTitle($"{item.Text} - {AppLocalizer["Title"]}");
+        }
+    }
+}
