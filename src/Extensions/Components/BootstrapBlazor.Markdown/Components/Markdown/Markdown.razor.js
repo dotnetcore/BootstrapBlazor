c71278c9a7c0ff3d7fe53fc924960d0bfef17a68
Argo Zhang <argo@live.ca>
2023-04-15 17:08:04 +0800

    feat(Markdown): use javascript isolation (#1011)
    * chore: 更新配置
    
    * chore: bump version 7.1.0
    
    * refactor: 重构 Markdown 脚本
    
    * chore: 更新依赖包
    
    * refactor: 更新样式文件
    
    * refactor: 删除 min 文件
    
    * refactor: 更正动态加载 JavaScript 脚本文件名

---
+++ src/Extensions/Components/BootstrapBlazor.Markdown/Components/Markdown/Markdown.razor.js
@@ -0,0 +1,47 @@
+﻿import '../../lib/tui.editor/toastui-editor-all.min.js'
+import '../../lib/tui.highlight/toastui-editor-plugin-code-syntax-highlight-all.min.js'
+import { addLink } from '../../../BootstrapBlazor/modules/utility.js'
+import Data from '../../../BootstrapBlazor/modules/data.js'
+
+export async function init(el, invoker, options, callback) {
+    await addLink('./_content/BootstrapBlazor.Markdown/css/bootstrap.blazor.markdown.min.css')
+
+    const md = {}
+    Data.set(el, md)
+
+    md._invoker = invoker
+    md._options = options
+    md._invokerMethod = callback
+    md._element = el
+    md._options.el = el
+    md._options.plugins = [];
+    if (md._options.enableHighlight) {
+        md._options.plugins.push(toastui.Editor.plugin.codeSyntaxHighlight)
+    }
+    md._editor = toastui.Editor.factory(md._options)
+    md._editor.on('blur', () => {
+        const val = md._editor.getMarkdown()
+        const html = md._editor.getHTML()
+        md._invoker.invokeMethodAsync(md._invokerMethod, [val, html])
+    })
+}
+
+export function update(el, val) {
+    const md = Data.get(el)
+    md._editor.setMarkdown(val)
+}
+
+export function invoke(el, method, parameters) {
+    const md = Data.get(el)
+    md._editor[method](...parameters);
+    const val = md._editor.getMarkdown()
+    const html = md._editor.getHTML()
+    md._invoker.invokeMethodAsync('Update', [val, html])
+}
+
+export function dispose(el) {
+    const md = Data.get(el)
+    md._editor.off('blur')
+    md._editor.destroy()
+    Data.remove(el)
+}
