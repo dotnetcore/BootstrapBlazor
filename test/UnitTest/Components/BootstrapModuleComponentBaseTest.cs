232f4ffd454b554916fcc92e8026821674bc8c89
Argo-AscioTech <argo@live.ca>
2023-04-17 21:36:21 +0800

    test: 更新单元测试

--- test/UnitTest/Components/BootstrapModuleComponentBaseTest.cs
+++ test/UnitTest/Components/BootstrapModuleComponentBaseTest.cs
@@ -32,6 +32,7 @@ public class BootstrapModuleComponentBaseTest : BootstrapBlazorTestBase
         var jsRuntime = Context.Services.GetRequiredService<IJSRuntime>();
         _ = jsRuntime.LoadModule("test.js", false);
         _ = jsRuntime.LoadModule3<MockClass>("test", new MockClass(), false);
+        _ = jsRuntime.LoadModule3<MockClass>("test", new MockClass(), true);
     }
 
     class MockClass
