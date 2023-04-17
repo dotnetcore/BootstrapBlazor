af6b8f141cc986a7bebae1595e4b84777f5e0223
Argo Zhang <argo@live.ca>
2023-04-17 22:07:28 +0800

    feat(AnchorLink): use JavaScript isolation (#1037)
    * refactor: 移动 AnchorLink 为单独文件夹
    
    * chore: 更新项目文件

---
+++ src/BootstrapBlazor/Components/AnchorLink/AnchorLink.razor.js
@@ -0,0 +1,42 @@
+﻿import { copy } from "../../modules/utility.js"
+import Data from "../../modules/data.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(id) {
+    const anchorLink = {}
+    Data.set(id, anchorLink)
+
+    const el = document.getElementById(id)
+    const title = el.getAttribute('data-bb-title')
+    anchorLink._element = el
+
+    EventHandler.on(el, 'click', e => {
+        e.preventDefault()
+        const href = location.origin + location.pathname + '#' + id
+        copy(href)
+
+        if (title) {
+            const tooltip = bootstrap.Tooltip.getOrCreateInstance(el, { title })
+            tooltip.show()
+
+            anchorLink._tooltip = tooltip
+            anchorLink.handler = setTimeout(() => {
+                clearTimeout(anchorLink.handler)
+                tooltip.dispose()
+            }, 1000)
+        }
+    })
+}
+
+export function dispose(id) {
+    const anchorLink = Data.get(id)
+
+    EventHandler.off(anchorLink._element, 'click')
+
+    if (anchorLink.handler) {
+        clearTimeout(anchorLink.handler)
+    }
+    if (anchorLink.tooltip) {
+        anchorLink.tooltip.dispose()
+    }
+}
