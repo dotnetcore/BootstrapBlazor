008190fc8b068454fe656b84d87a0ec1dcabe319
Argo-AscioTech <argo@live.ca>
2023-04-17 15:10:15 +0800

    refactor: 重构 CountUp 组件

---
+++ src/BootstrapBlazor/Components/CountUp/CountUp.razor.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// CountUp 组件
+/// </summary>
+[JSModuleAutoLoader()]
+public partial class CountUp<TValue>
+{
+    /// <summary>
+    /// 获得/设置 Value 值
+    /// </summary>
+    [Parameter]
+    public TValue? Value { get; set; }
+
+    /// <summary>
+    /// 获得/设置 ValueChanged 回调
+    /// </summary>
+    [Parameter]
+    public EventCallback<TValue> ValueChanged { get; set; }
+
+    private TValue? _previousValue;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (!typeof(TValue).IsNumber())
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            _previousValue = Value;
+        }
+        else if (_previousValue != null && !_previousValue.Equals(Value))
+        {
+            await Update(Value);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Value);
+
+    /// <summary>
+    /// 更新数据方法
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    private async ValueTask Update(TValue? value)
+    {
+        _previousValue = value;
+
+        if (Module != null)
+        {
+            await Module.InvokeVoidAsync("update", Id, Value);
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/CountUp/CountUp.razor.js
@@ -0,0 +1,30 @@
+﻿import { CountUp } from "../../lib/countUp/countUp.min.js"
+import Data from "../../modules/data.js"
+
+export function init(id) {
+    var count = {}
+    Data.set(id, count)
+
+    var val = parseFloat(document.getElementById(id).innerHTML)
+    if (val) {
+        count._countUp = new CountUp(id, 5234)
+        if (!countUp.error) {
+            countUp.start()
+        }
+        else {
+            console.error(countUp.error)
+        }
+    }
+}
+
+export function update(id, value) {
+    const count = Data.get(id)
+    if (count._countUp) {
+        couont._countUp.update(value)
+    }
+}
+
+export function dispose(id) {
+    Data.remove(id)
+}
+
