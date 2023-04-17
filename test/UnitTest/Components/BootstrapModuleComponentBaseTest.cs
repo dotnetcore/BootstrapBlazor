8017338611f7a39485c11c4a31adff2bab38f4eb
Argo-AscioTech <argo@live.ca>
2023-04-17 10:43:36 +0800

    test: 增加 BootstrapModuleComponentBaseTest 单元测试

--- test/UnitTest/Components/BootstrapModuleComponentBaseTest.cs
+++ test/UnitTest/Components/BootstrapModuleComponentBaseTest.cs
@@ -7,6 +7,62 @@ namespace UnitTest.Components;
 public class BootstrapModuleComponentBaseTest : BootstrapBlazorTestBase
 {
     [Fact]
+    public async Task InvokeVoidAsync_Ok()
+    {
+        var cut = Context.RenderComponent<MockComponent>();
+        await cut.Instance.InvokeVoidAsyncTest();
+        Assert.True(cut.Instance.InvokeVoidRunned);
+    }
+
+    [Fact]
+    public async Task InvokeAsync_Ok()
+    {
+        var cut = Context.RenderComponent<MockObjectReferenceComponent>();
+        await cut.Instance.InvokeAsyncTest();
+        Assert.True(cut.Instance.InvokeRunned);
+        Assert.Equal("Mock", cut.Instance.GetModuleName());
+    }
+
+    [JSModuleAutoLoader]
+    class MockComponent : BootstrapModuleComponentBase
+    {
+        public bool InvokeVoidRunned { get; set; }
+
+        public async Task InvokeVoidAsyncTest()
+        {
+            await base.InvokeVoidAsync(Id);
+            await base.InvokeVoidAsync(Id, TimeSpan.FromMinutes(1));
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(1000);
+            await base.InvokeVoidAsync(Id, cancellationTokenSource.Token);
+
+            InvokeVoidRunned = true;
+        }
+    }
+
+    [JSModuleAutoLoader(JSObjectReference = true, ModuleName = "Mock", Relative = true)]
+    class MockObjectReferenceComponent : BootstrapModuleComponentBase
+    {
+        public bool InvokeRunned { get; set; }
+
+        public string? GetModuleName() => ModuleName;
+
+        public async Task InvokeAsyncTest()
+        {
+            await base.InvokeAsync<string>(Id);
+            await base.InvokeAsync<string>(Id, TimeSpan.FromMinutes(1));
+
+            using CancellationTokenSource cancellationTokenSource = new CancellationTokenSource(1000);
+            await base.InvokeAsync<string>(Id, cancellationTokenSource.Token);
+
+            InvokeRunned = true;
+        }
+    }
+}
+
+public class BootstrapModule2ComponentBaseTest : BootstrapBlazorTestBase
+{
+    [Fact]
     public void OnLoadJSModule_Ok()
     {
         var cut = Context.RenderComponent<MockComponent>();
