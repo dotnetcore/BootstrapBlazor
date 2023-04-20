0f5a079c1e0086bae139232554b700fc745afb23
Argo-AscioTech <argo@live.ca>
2023-04-20 10:51:12 +0800

    refactor: 更改类名

---
+++ src/BootstrapBlazor.Shared/Components/WebSiteModuleComponentBase.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Components;
+
+/// <summary>
+/// JSModuleComponentBase 组件
+/// </summary>
+public abstract class WebSiteModuleComponentBase : BootstrapModuleComponentBase
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnLoadJSModule()
+    {
+        base.OnLoadJSModule();
+
+        ModulePath = $"./_content/BootstrapBlazor.Shared/{ModulePath}";
+        Relative = false;
+    }
+}
