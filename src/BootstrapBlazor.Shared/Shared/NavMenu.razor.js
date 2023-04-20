9c32f3370d7c57994fc30ef15237b62410e8abd4
zhangpeihang <948869991@qq.com>
2023-04-20 14:44:03 +0800

    refactor: NavMenu 组件js改为隔离

---
+++ src/BootstrapBlazor.Shared/Shared/NavMenu.razor.js
@@ -0,0 +1,29 @@
+﻿import EventHandler from "../../../_content/BootstrapBlazor/modules/event-handler.js"
+import Data from "../../../_content/BootstrapBlazor/modules/data.js"
+
+export function init(id) {
+    const navmenu = {
+        navbar: document.querySelector('.navbar-toggler'),
+        menu: document.querySelector('.sidebar-content'),
+    }
+    Data.set(id, navmenu)
+
+    EventHandler.on(navmenu.navbar, 'click', () => {
+        navmenu.menu.classList.toggle('show')
+    })
+    EventHandler.on(navmenu.menu, 'click', '.nav-link', e => {
+        const link = e.delegateTarget
+        const url = link.getAttribute('href');
+        if (url !== '#') {
+            navmenu.menu.classList.remove('show')
+        }
+    })
+}
+
+export function dispose(id) {
+    const data = Data.get(id);
+    EventHandler.off(data.navbar, 'click');
+    EventHandler.off(data.menu, 'click', '.nav-link');
+    Data.remove(id);
+}
+
