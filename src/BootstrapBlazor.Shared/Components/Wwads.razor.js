be0d02367422fe9b94a5af4ab7f47008a3f917b6
zhangpeihang <948869991@qq.com>
2023-04-20 14:43:24 +0800

    refactor: Wwads 组件js改为隔离

---
+++ src/BootstrapBlazor.Shared/Components/Wwads.razor.js
@@ -0,0 +1,19 @@
+﻿export function init(id) {
+    const ad = document.createElement('div')
+    ad.setAttribute("data-id", 72)
+    ad.classList.add("wwads-cn")
+    ad.classList.add("wwads-horizontal")
+    var element = document.getElementById(id)
+    if (element) {
+        if (element.getAttribute('data-bb-debug') === 'true') {
+            ad.classList.add('debug')
+        }
+
+        const parent = element.parentNode
+        parent.insertBefore(ad, element.nextSibling)
+    }
+}
+
+export function dispose(id) {
+}
+
