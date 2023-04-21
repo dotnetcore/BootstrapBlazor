d45b97a2a9d5b7d3f067d452ccdf364dff4b41eb
Argo-AscioTech <argo@live.ca>
2023-04-21 20:40:13 +0800

    refactor: 重构 Upload 脚本

--- src/BootstrapBlazor/wwwroot/modules/upload.js
+++ src/BootstrapBlazor/wwwroot/modules/upload.js
@@ -1,94 +1,87 @@
-﻿import EventHandler from "./base/event-handler.js"
-import BlazorComponent from "./base/blazor-component.js"
-import { ImagePreviewer } from "./image-previewer.js"
+﻿import { ImagePreviewer } from "../Components/ImagePreviewer/ImagePreviewer.razor.js"
+import Data from "../../modules/data.js"
+import EventHandler from "../../modules/event-handler.js"
 
-export class Upload extends BlazorComponent {
-    static get Default() {
-        return {
-            browserClass: '.btn-browser'
+export function init(id) {
+    const el = document.getElementById(id)
+    const upload = {
+        el,
+        browserClass: '.btn-browser',
+        inputFile: el.querySelector('[type="file"]'),
+        preventFn: e => {
+            e.preventDefault()
         }
     }
+    Data.set(id, upload)
 
-    _init() {
-        this._inputFile = this._element.querySelector('[type="file"]')
-        this._setListeners()
-    }
+    EventHandler.on(el, 'click', upload.browserClass, () => {
+        upload.inputFile.click()
+    })
 
-    _setListeners() {
-        EventHandler.on(this._element, 'click', this._config.browserClass, () => {
-            this._inputFile.click()
-        })
+    //阻止浏览器默认行为
+    EventHandler.on(document, "dragleave", preventFn)
+    EventHandler.on(document, 'drop', preventFn)
+    EventHandler.on(document, 'dragenter', preventFn)
+    EventHandler.on(document, 'dragover', preventFn)
 
-        //阻止浏览器默认行为
-        EventHandler.on(document, "dragleave", e => {
-            e.preventDefault()
-        })
-        EventHandler.on(document, 'drop', e => {
-            e.preventDefault()
-        })
-        EventHandler.on(document, 'dragenter', e => {
-            e.preventDefault()
-        })
-        EventHandler.on(document, 'dragover', e => {
-            e.preventDefault()
-        })
+    EventHandler.on(el, 'drop', e => {
+        try {
+            //获取文件对象
+            const fileList = e.dataTransfer.files
 
-        EventHandler.on(this._element, 'drop', e => {
-            try {
-                //获取文件对象
-                const fileList = e.dataTransfer.files
-
-                //检测是否是拖拽文件到页面的操作
-                if (fileList.length === 0) {
-                    return false;
-                }
-
-                this._inputFile.files = e.dataTransfer.files;
-                const event = new Event('change', { bubbles: true });
-                this._inputFile.dispatchEvent(event);
-            } catch (e) {
-                console.error(e);
+            //检测是否是拖拽文件到页面的操作
+            if (fileList.length === 0) {
+                return false;
             }
-        })
 
-        EventHandler.on(this._element, 'paste', e => {
-            this._inputFile.files = e.clipboardData.files;
+            upload.inputFile.files = e.dataTransfer.files;
             const event = new Event('change', { bubbles: true });
-            this._inputFile.dispatchEvent(event);
-        });
-
-        EventHandler.on(this._element, 'click', '.btn-zoom', e => {
-            if (!this._previewer) {
-                const previewerId = document.getElementById(this._config.previewerId)
-                this._previewer = ImagePreviewer.getOrCreateInstance(previewerId)
-            }
-            const button = e.delegateTarget
-            const buttons = [...this._element.querySelectorAll('.btn-zoom')]
-            this._previewer.show(buttons.indexOf(button))
-        })
-    }
-
-    _execute(args) {
-        const tooltipId = args[0]
-        const method = args[1]
-        if (method === 'disposeTooltip' && tooltipId) {
-            const element = document.getElementById(tooltipId)
-            if (element) {
-                const tooltip = bootstrap.Tooltip.getInstance(element)
-                if (tooltip) {
-                    tooltip.dispose()
-                }
-            }
+            upload.inputFile.dispatchEvent(event);
+        } catch (e) {
+            console.error(e);
         }
-    }
+    })
 
-    _dispose() {
-        EventHandler.off(this._element, 'click', this._config.browserClass)
-        EventHandler.off(document, 'dragleave');
-        EventHandler.off(document, 'drop');
-        EventHandler.off(document, 'dragenter');
-        EventHandler.off(document, 'dragover');
-        EventHandler.off(this._element, 'drop');
-        EventHandler.off(this._element, 'paste');
-    }
+    EventHandler.on(el, 'paste', e => {
+        upload.inputFile.files = e.clipboardData.files;
+        const event = new Event('change', { bubbles: true });
+        upload.inputFile.dispatchEvent(event);
+    });
+
+    EventHandler.on(el, 'click', '.btn-zoom', e => {
+        if (!upload.previewer) {
+            //const previewerId = document.getElementById(this._config.previewerId)
+            //this._previewer = ImagePreviewer.getOrCreateInstance(previewerId)
+        }
+        const button = e.delegateTarget
+        const buttons = [...el.querySelectorAll('.btn-zoom')]
+        //this._previewer.show(buttons.indexOf(button))
+    })
+}
+
+//_execute(args) {
+//    const tooltipId = args[0]
+//    const method = args[1]
+//    if (method === 'disposeTooltip' && tooltipId) {
+//        const element = document.getElementById(tooltipId)
+//        if (element) {
+//            const tooltip = bootstrap.Tooltip.getInstance(element)
+//            if (tooltip) {
+//                tooltip.dispose()
+//            }
+//        }
+//    }
+//}
+
+export function dispose(id) {
+    const upload = Data.get(id)
+    Data.remove(id)
+
+    EventHandler.off(el, 'click', upload.browserClass)
+    EventHandler.off(document, 'dragleave', upload.preventFn);
+    EventHandler.off(document, 'drop', upload.preventFn);
+    EventHandler.off(document, 'dragenter', upload.preventFn);
+    EventHandler.off(document, 'dragover', upload.preventFn);
+    EventHandler.off(el, 'drop');
+    EventHandler.off(el, 'paste');
 }
