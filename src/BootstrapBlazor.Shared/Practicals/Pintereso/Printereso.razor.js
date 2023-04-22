ef99b197b6a17cff525a2df072e6d24241d84ca9
Old Li <lish@live.cn>
2023-04-22 17:05:02 +0800

    每次滚动到底部，就给他塞5张新照片。

--- src/BootstrapBlazor.Shared/Practicals/Pintereso/Printereso.razor.js
+++ src/BootstrapBlazor.Shared/Practicals/Pintereso/Printereso.razor.js
@@ -1,17 +1,13 @@
-﻿import EventHandler from "../../../BootstrapBlazor/modules/event-handler.js"
-
-const scrollHandler = [];
-
-export function init(invoke, method, el) {
-    scrollHandler = () => {
-        var b_height = document.body.clientHeight;
-        if (parseInt(el.pageYOffset + el.innerHeight) > b_height - 10) {
+﻿export function init(invoke, method) {
+    window.onscroll = function () {
+        var windowH = document.documentElement.clientHeight;//可视区窗口高度；
+        var scrollH = document.documentElement.scrollTop || document.body.scrollTop;//滚动条的上边距；
+        var documentH = document.documentElement.scrollHeight || document.body.scrollHeight;//滚动条的高度；
+        var h1 = windowH + scrollH;
+        var h2 = documentH;
+        if (Math.abs(h1 - h2) < 50) {
             invoke.invokeMethodAsync(method);
+            window.scrollTo(0, scrollH - 40);//把滚动条往回滚一点
         }
     }
-    EventHandler.on(window, 'scroll', scrollHandler)
-}
-
-export function dispose(el) {
-    EventHandler.off(window, 'scroll', scrollHandler)
 }
