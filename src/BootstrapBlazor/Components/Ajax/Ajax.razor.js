cebcb1d65bb46898b9ea6fc71dc76d19ff805933
Argo-AscioTech <argo@live.ca>
2023-04-23 09:00:26 +0800

    refactor: 重构 Ajax 组件

---
+++ src/BootstrapBlazor/Components/Ajax/Ajax.razor.js
@@ -0,0 +1,27 @@
+﻿export async function execute(option) {
+    option = {
+        method: 'POST',
+        url: null,
+        data: null,
+        ...option
+    }
+
+    if (option.url === null) {
+        window.error('url not allow null')
+        return null
+    }
+
+    const response = await fetch(option.url, {
+        method: option.method,
+        body: JSON.stringify(option.data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+    return JSON.stringify(json)
+}
+
+export function goto(url) {
+    window.location.href = url
+}
