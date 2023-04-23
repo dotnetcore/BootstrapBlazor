e991caca59ae45c0647ccbf7ced57f42ef65953e
Argo-AscioTech <argo@live.ca>
2023-04-23 19:38:11 +0800

    refactor: Button 组件使用动态加载

---
+++ src/BootstrapBlazor/Components/Button/Button.razor.js
@@ -0,0 +1,32 @@
+﻿import Data from "../../modules/data.js"
+
+const showTooltip = (id, title) => {
+    const el = document.getElementById(id)
+
+    if (el) {
+        bootstrap.Tooltip.getOrCreateInstance(el, {
+            title: title
+        })
+    }
+}
+
+const removeTooltip = id => {
+    const el = document.getElementById(id)
+
+    if (el) {
+        const tip = bootstrap.Tooltip.getInstance(el)
+        if (tip) {
+            tip.dispose()
+        }
+    }
+}
+
+const dispose = id => {
+    removeTooltip(id)
+}
+
+export {
+    showTooltip,
+    removeTooltip,
+    dispose
+}
