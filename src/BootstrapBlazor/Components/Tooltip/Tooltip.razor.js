615ee7fdf9b2b5602eb9f83b51fbd21415df647c
Argo Zhang <argo@live.ca>
2023-04-24 09:55:50 +0800

    feat(Tooltip/Popover): use JavaScript isolation (#1099)
    * refactor: 删除按钮脚本
    
    * refactor: 重构 Tooltip 组件
    
    * doc: 微调 Toolitp 示例样式
    
    * refactor: 更新 Tooltips 脚本
    
    * feat: support selector config parameter
    
    * refactor: 重构 popover 脚本
    
    * refactor: 跟心 Popover 组件
    
    * refactor: 删除不使用的变量
    
    * refactor: 删除冗余代码
    
    * refactor: 精简代码

---
+++ src/BootstrapBlazor/Components/Tooltip/Tooltip.razor.js
@@ -0,0 +1,26 @@
+﻿import Data from "../../modules/data.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+    if (el) {
+        const tip = {
+            tooltip: new bootstrap.Tooltip(el, { sanitize: el.getAttribute('data-bs-sanitize') !== 'false', title: el.getAttribute('data-bs-original-title') })
+        }
+        Data.set(id, tip)
+    }
+}
+
+export function dispose(id) {
+    const tip = Data.get(id)
+    Data.remove(id)
+
+    if (tip) {
+        const handler = setTimeout(() => {
+            clearTimeout(handler)
+            if (tip.tooltip) {
+                tip.tooltip.dispose()
+                delete tip.tooltip
+            }
+        }, 10)
+    }
+}
