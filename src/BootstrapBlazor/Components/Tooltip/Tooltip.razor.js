731896461aaa9ecf805d7959a0a7106d9184cff3
Argo-AscioTech <argo@live.ca>
2023-04-24 09:30:10 +0800

    feat: support selector config parameter

--- src/BootstrapBlazor/Components/Tooltip/Tooltip.razor.js
+++ src/BootstrapBlazor/Components/Tooltip/Tooltip.razor.js
@@ -4,7 +4,7 @@ export function init(id) {
     const el = document.getElementById(id)
     if (el) {
         const tip = {
-            tooltip: new bootstrap.Tooltip(el, { sanitize: el.getAttribute('data-bs-sanitize') !== 'false' })
+            tooltip: new bootstrap.Tooltip(el, { sanitize: el.getAttribute('data-bs-sanitize') !== 'false', title: el.getAttribute('data-bs-original-title') })
         }
         Data.set(id, tip)
     }
@@ -15,13 +15,12 @@ export function dispose(id) {
     Data.remove(id)
 
     if (tip) {
-        const delay = 10
         const handler = setTimeout(() => {
             clearTimeout(handler)
             if (tip.tooltip) {
                 tip.tooltip.dispose()
                 delete tip.tooltip
             }
-        }, delay)
+        }, 10)
     }
 }
