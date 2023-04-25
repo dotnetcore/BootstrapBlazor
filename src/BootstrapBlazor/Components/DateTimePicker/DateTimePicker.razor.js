6c54ea8c09491b772d4dcea7520d8d56a473d72f
zhangpeihang <948869991@qq.com>
2023-04-25 21:05:48 +0800

    refactor: datetimepicker、daatetimerange 脚本改为隔离加载

---
+++ src/BootstrapBlazor/Components/DateTimePicker/DateTimePicker.razor.js
@@ -0,0 +1,45 @@
+﻿import Data from "../../modules/Data.js"
+import EventHandler from "../../modules/event-handler.js"
+import Popover from "../../modules/base-popover.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+
+    if (el == null) {
+        return
+    }
+
+    const dismissSelector = el.getAttribute("data-bb-dismiss")
+    const popover = Popover.init(el);
+    const dateTimePicker = {
+        el,
+        dismissSelector,
+        popover
+    }
+
+    if (dismissSelector) {
+        EventHandler.on(popover.toggleMenu, 'click', dismissSelector, () => {
+            popover.hide()
+        })
+    }
+
+    Data.set(id, dateTimePicker)
+}
+
+export function hide(id) {
+    const data = Data.get(id)
+    if (data) {
+        data.popover.hide()
+    }
+}
+
+export function dispose(id) {
+    const data = Data.get(id)
+    if (data) {
+        if (data.dismissSelector) {
+            EventHandler.off(data.popover.toggleMenu, 'click', data.dismissSelector)
+        }
+        Popover.dispose(data.el)
+    }
+    Data.remove(id)
+}
