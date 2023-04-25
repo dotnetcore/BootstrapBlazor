d6b1696f9c3749334ff260c50d78f143cc6af700
Argo-AscioTech <argo@live.ca>
2023-04-25 09:45:48 +0800

    refactor: 重构 TableFilter 脚本

--- src/BootstrapBlazor/Components/Filters/TableFilter.razor.js
+++ src/BootstrapBlazor/Components/Filters/TableFilter.razor.js
@@ -1,57 +1,51 @@
-﻿import DropdownBase from "../../modules/base/base-dropdown1.js"
+﻿import Data from "../../modules/data.js"
 import EventHandler from "../../modules/event-handler.js"
-import Data from "../../modules/data.js"
-
-
-const Default = {
-    class: 'popover-dropdown',
-    toggleClass: '[data-bs-toggle="bb.dropdown"]',
-    dropdown: '.filter-item'
-}
+import Popover from "../../modules/base-popover.js"
 
 export function init(id) {
     const el = document.getElementById(id)
-
     if (el == null) {
         return;
     }
 
-    const action = el.querySelector(Default.dropdown)
-    const dismiss = el.getAttribute("data-bb-dismiss")
-    const dropdown = DropdownBase.init(el);
+    const config = {
+        class: 'popover-dropdown',
+        toggleClass: '[data-bs-toggle="bb.dropdown"]',
+        dropdownSelector: '.filter-item',
+        isDisabled: () => {
+            return false
+        }
+    }
+    const action = el.querySelector(config.dropdownSelector)
+    const dismissSelector = el.getAttribute('data-bb-dismiss')
+    const dropdown = Popover.init(el, config)
+
     const tableFilter = {
         el,
-        dismiss,
         action,
+        dismissSelector,
         dropdown
     }
+    Data.set(id, tableFilter)
 
-    EventHandler.on(action, 'click', dismiss, () => {
-        if (dropdown.popover) {
-            dropdown.popover.hide()
-        }
+    const buttons = el.querySelectorAll(dismissSelector)
+    EventHandler.on(action, 'click', dismissSelector, () => {
+        dropdown.popover.hide()
     })
-
-    const buttons = el.querySelectorAll(dismiss)
     EventHandler.on(el, 'keyup', e => {
         if (e.key === 'Escape') {
             buttons.item(0).click()
-        } else if (e.key === 'Enter') {
+        }
+        else if (e.key === 'Enter') {
             buttons.item(1).click()
         }
     });
-
-    Data.set(id, tableFilter)
-}
-
-export function isDisabled() {
-    return false
 }
 
 export function dispose(id) {
     const data = Data.get(id)
-    if (data.action) {
-        EventHandler.off(data.action, 'click', data.dismiss)
-        DropdownBase.dispose(data.dropdown)
+    if (data) {
+        EventHandler.off(data.action, 'click', data.dismissSelector)
+        Popover.dispose(data.dropdown)
     }
 }
