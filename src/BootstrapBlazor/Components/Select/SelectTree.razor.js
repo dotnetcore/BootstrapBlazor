1cb7b216f105067ac2c7e15cb0ec2cefddec34a7
zhangpeihang <948869991@qq.com>
2023-04-25 21:17:44 +0800

    refactor: SelectTree 组件改为隔离加载

---
+++ src/BootstrapBlazor/Components/Select/SelectTree.razor.js
@@ -0,0 +1,40 @@
+﻿import Data from "../../modules/data.js"
+import EventHandler from "../../modules/event-handler.js"
+import Popover from "../../modules/base-popover.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+
+    if (el != null) {
+        return
+    }
+
+    const popover = Popover.init(el)
+    const selectTree = {
+        el,
+        input: el.querySelector(".form-select"),
+        popover
+    }
+
+    EventHandler.on(popover.toggleMenu, 'click', '.tree-node', e => {
+        if (popover.popover.isPopover) {
+            popover.popover.hide()
+        } else {
+            const dropdown = bootstrap.Dropdown.getInstance(popover.toggleElement)
+            if (dropdown) {
+                dropdown.hide()
+            }
+        }
+    })
+    Data.set(id, selectTree)
+}
+
+export function dispose(id) {
+    const data = Data.get(id)
+    if (data) {
+        EventHandler.off(data.popover.toggleMenu, 'click', '.tree-node')
+        Popover.dispose(data.el)
+
+    }
+    Data.remove(id)
+}
