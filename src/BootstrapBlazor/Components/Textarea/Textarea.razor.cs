78d880d59b10e6b8d759b710725c526fc95d969b
zhangpeihang <948869991@qq.com>
2023-04-25 21:12:38 +0800

    refactor: Textare  组件脚本改为隔离加载

--- src/BootstrapBlazor/Components/Textarea/Textarea.razor.cs
+++ src/BootstrapBlazor/Components/Textarea/Textarea.razor.cs
@@ -20,19 +20,19 @@ public partial class Textarea
     /// 滚动到顶部
     /// </summary>
     /// <returns></returns>
-    public Task ScrollToTop() => InvokeExecuteAsync(Id, "toTop");
+    public Task ScrollToTop() => InvokeVoidAsync("execute", Id, "toTop");
 
     /// <summary>
     /// 滚动到数值
     /// </summary>
     /// <returns></returns>
-    public Task ScrollTo(int value) => InvokeExecuteAsync(Id, "to", value);
+    public Task ScrollTo(int value) => InvokeVoidAsync("execute", Id, "to", value);
 
     /// <summary>
     /// 滚动到底部
     /// </summary>
     /// <returns></returns>
-    public Task ScrollToBottom() => InvokeExecuteAsync(Id, "toBottom");
+    public Task ScrollToBottom() => InvokeVoidAsync("execute", Id, "toBottom");
 
     /// <summary>
     /// 获得/设置 是否自动滚屏 默认 false
@@ -49,5 +49,9 @@ public partial class Textarea
     /// <inheritdoc/>
     /// </summary>
     /// <returns></returns>
-    protected override Task ModuleExecuteAsync() => InvokeExecuteAsync(Id, "refresh");
+    protected override async Task InvokeInitAsync()
+    {
+        await base.InvokeInitAsync();
+        await InvokeVoidAsync("execute", Id, "refresh");
+    }
 }
---
+++ src/BootstrapBlazor/Components/Textarea/Textarea.razor.js
@@ -0,0 +1,47 @@
+﻿import Data from "../../modules/data.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+
+    if (el != null) {
+        return
+    }
+
+    const prevMethod = ""
+    const textArea = {
+        el,
+        autoScroll: el.getAttribute('data-bb-scroll') === 'auto',
+        prevMethod,
+        position: 0
+    }
+
+    Data.set(id, textArea)
+}
+
+
+export function execute(id, method, position) {
+    const data = Data.get(id)
+    if (data) {
+        if (method === 'refresh') {
+            method = data.prevMethod
+        }
+        if (method === 'toTop') {
+            position = 0;
+        }
+        if (autoScroll || method === 'toBottom') {
+            position = data.el.scrollHeight
+        }
+
+        if (!isNaN(position)) {
+            data.el.scrollTop = position;
+        }
+
+        if (method !== 'refresh') {
+            data.prevMethod = method;
+        }
+    }
+}
+
+export function dispose(id) {
+    Data.remove(id)
+}
