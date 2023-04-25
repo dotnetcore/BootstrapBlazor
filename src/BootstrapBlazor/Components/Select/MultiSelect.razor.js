0697317a472f20889d82403c0361c8bd6622f76c
zhangpeihang <948869991@qq.com>
2023-04-25 21:13:49 +0800

    refactor: MultiSelect 组件脚本改为隔离加载

---
+++ src/BootstrapBlazor/Components/Select/MultiSelect.razor.js
@@ -0,0 +1,44 @@
+﻿import { isDisabled } from "../../modules/utility.js"
+import Data from "../../modules/data.js"
+import DropdownBase from "../../modules/base-dropdown.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(id, invoke, callback) {
+    const el = document.getElementById(id)
+    const ms = {
+        el, invoke, callback,
+        itemsElement: el.querySelector('.multi-select-items'),
+        closeButtonSelector: '.multi-select-close',
+        dropdown: DropdownBase.init(el)
+    }
+
+    if (!ms.dropdown.isPopover) {
+        EventHandler.on(ms.itemsElement, 'click', ms.closeButtonSelector, () => {
+            const popover = ms.dropdown.popover
+            if (popover && popover._isShown()) {
+                popover.hide()
+            }
+        })
+    }
+    ms.dropdown.clickToggle = e => {
+        const element = e.target.closest(ms._closeButtonSelector);
+        if (element) {
+            e.stopPropagation()
+
+            invoke.invokeMethodAsync(callback, element.getAttribute('data-bb-val'))
+        }
+    }
+    ms.dropdown.isDisabled = () => {
+        return isDisabled(ms.dropdown.toggleElement)
+    }
+}
+
+export function dispose(id) {
+    const ms = Data.get(id)
+    Data.remove(id)
+
+    if (!ms.dropdonw.isPopover) {
+        EventHandler.off(ms.itemsElement, 'click', ms.closeButtonSelector)
+    }
+    ms.dropdonw.dispose(ms.dropdonw)
+}
