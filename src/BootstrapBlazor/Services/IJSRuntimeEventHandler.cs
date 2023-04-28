69c4bdabd36df1666f92a062a4951a2ae98f63fe
Argo-AscioTech <argo@live.ca>
2023-04-28 13:34:51 +0800

    refactor: 重构代码更改命名空间

--- src/BootstrapBlazor/Services/IJSRuntimeEventHandler.cs
+++ src/BootstrapBlazor/Services/IJSRuntimeEventHandler.cs
@@ -2,7 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-namespace BootstrapBlazor.Services;
+namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// 浏览器事件,通用属性帮助类接口
@@ -69,187 +69,187 @@ public interface IJSRuntimeEventHandler : IAsyncDisposable
     /// <summary>
     /// 鼠标点击时触发些事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnClick;
+    event BootstrapBlazorJSRuntimeEventHandler OnClick;
     /// <summary>
     /// 在单个元素上单击两次鼠标的指针设备按钮时，将触发 dblclick 事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnDblclick;
+    event BootstrapBlazorJSRuntimeEventHandler OnDblclick;
     /// <summary>
     /// 当指针在元素中时， mouseup事件在指针设备按钮放开时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMouseup;
+    event BootstrapBlazorJSRuntimeEventHandler OnMouseup;
     /// <summary>
     /// 事件在指针设备按钮按下时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMousedown;
+    event BootstrapBlazorJSRuntimeEventHandler OnMousedown;
     /// <summary>
     /// 全局属性是指用于某个元素的“上下文菜单”的ID属性。上下文菜单是指在用户交互（例如右键点击）时出现的菜单。
     /// </summary>
-    event BootStrapBlazorEventHandler OnContextmenu;
+    event BootstrapBlazorJSRuntimeEventHandler OnContextmenu;
     /// <summary>
     /// 过时且非标准的 mousewheel 事件在元素上异步触发，以在操作鼠标滚轮或类似设备时提供更新。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMousewheel;
+    event BootstrapBlazorJSRuntimeEventHandler OnMousewheel;
     /// <summary>
     /// 当操作鼠标滚轮或类似设备并且自上次事件以来累积滚动量超过 1 行或 1 页时，将异步触发 DOM 事件。它由 MouseScrollEvent 接口表示。
     /// </summary>
-    event BootStrapBlazorEventHandler OnDOMMouseScroll;
+    event BootstrapBlazorJSRuntimeEventHandler OnDOMMouseScroll;
     /// <summary>
     /// 用来获取或设置当前元素的 mouseover 事件的事件处理函数。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMouseover;
+    event BootstrapBlazorJSRuntimeEventHandler OnMouseover;
     /// <summary>
     /// 当移动指针设备（通常是鼠标），使指针不再包含在这个元素或其子元素中时，mouseout 事件被触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMouseout;
+    event BootstrapBlazorJSRuntimeEventHandler OnMouseout;
     /// <summary>
     /// 当指针设备 ( 通常指鼠标 ) 在元素上移动时，mousemove 事件被触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnMousemove;
+    event BootstrapBlazorJSRuntimeEventHandler OnMousemove;
     /// <summary>
     /// 当文本内容选择将开始发生时触发的事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnSelectstart;
+    event BootstrapBlazorJSRuntimeEventHandler OnSelectstart;
     /// <summary>
     /// 当文本内容选择结束时发生时触发的事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnSelectend;
+    event BootstrapBlazorJSRuntimeEventHandler OnSelectend;
     /// <summary>
     /// 当键盘上的某个键被按下时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnKeydown;
+    event BootstrapBlazorJSRuntimeEventHandler OnKeydown;
     /// <summary>
     /// 当键盘上的某个键被按下并且释放时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnKeypress;
+    event BootstrapBlazorJSRuntimeEventHandler OnKeypress;
     /// <summary>
     /// 当键盘上的某个键被按开时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnKeyup;
+    event BootstrapBlazorJSRuntimeEventHandler OnKeyup;
     /// <summary>
     /// 事件在设备的纵横方向改变时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnOrientationchange;
+    event BootstrapBlazorJSRuntimeEventHandler OnOrientationchange;
     /// <summary>
     /// 事件在一个或多个触点与触控设备表面接触时被触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnTouchstart;
+    event BootstrapBlazorJSRuntimeEventHandler OnTouchstart;
     /// <summary>
     /// 事件在触点于触控平面上移动时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnTouchmove;
+    event BootstrapBlazorJSRuntimeEventHandler OnTouchmove;
     /// <summary>
     /// 当触点离开触控平面时触发touchend事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnTouchend;
+    event BootstrapBlazorJSRuntimeEventHandler OnTouchend;
     /// <summary>
     /// 事件在触点被中断时触发，中断方式基于特定实现而有所不同（例如，创建了太多的触点）。
     /// </summary>
-    event BootStrapBlazorEventHandler OnTouchcancel;
+    event BootstrapBlazorJSRuntimeEventHandler OnTouchcancel;
     /// <summary>
     /// 当指针变为活动状态时，将触发该事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnPointerdown;
+    event BootstrapBlazorJSRuntimeEventHandler OnPointerdown;
     /// <summary>
     /// 一个global event handler(全局事件) pointermove 事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnPointermove;
+    event BootstrapBlazorJSRuntimeEventHandler OnPointermove;
     /// <summary>
     /// 当某指针不再活跃时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnPointerup;
+    event BootstrapBlazorJSRuntimeEventHandler OnPointerup;
     /// <summary>
     /// 就像在Element或Window中点击类似，在某目标区域内，发生触点（鼠标指针，触摸等）行为时会触发源于 pointerleave 事件global event handler行为。这个事件本身属于 Pointer Events API 的一部分。
     /// </summary>
-    event BootStrapBlazorEventHandler OnPointerleave;
+    event BootstrapBlazorJSRuntimeEventHandler OnPointerleave;
     /// <summary>
     /// 当浏览器认为某指针不会再生成新的后续事件时触发（例如某设备不再活跃）
     /// </summary>
-    event BootStrapBlazorEventHandler OnPointercancel;
+    event BootstrapBlazorJSRuntimeEventHandler OnPointercancel;
     /// <summary>
     /// 当多个手指接触触摸表面时，将触发 Gesturestart 事件，从而启动新手势。在手势期间，将触发手势更改事件。手势结束后，将触发手势结束事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnGesturestart;
+    event BootstrapBlazorJSRuntimeEventHandler OnGesturestart;
     /// <summary>
     /// 当数字在触摸手势期间移动时，将触发 Gesturechange 事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnGesturechange;
+    event BootstrapBlazorJSRuntimeEventHandler OnGesturechange;
     /// <summary>
     /// 当不再有多个手指接触触摸表面时，将触发 Gestureend 事件，从而结束手势。
     /// </summary>
-    event BootStrapBlazorEventHandler OnGestureend;
+    event BootstrapBlazorJSRuntimeEventHandler OnGestureend;
     /// <summary>
     /// 当某个元素获得焦点时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnFocus;
+    event BootstrapBlazorJSRuntimeEventHandler OnFocus;
     /// <summary>
     /// 当元素失去焦点时，将触发模糊事件。事件不会冒泡，但后面的相关焦点事件会冒泡。
     /// </summary>
-    event BootStrapBlazorEventHandler OnBlur;
+    event BootstrapBlazorJSRuntimeEventHandler OnBlur;
     /// <summary>
     /// 当用户修改元素的值时，将为input、select和textarea 元素触发该事件。与输入事件不同，不必为每次更改元素的 .changechangevalue
     /// </summary>
-    event BootStrapBlazorEventHandler OnChange;
+    event BootstrapBlazorJSRuntimeEventHandler OnChange;
     /// <summary>
     /// 当表单中RESET的属性被激发时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnReset;
+    event BootstrapBlazorJSRuntimeEventHandler OnReset;
     /// <summary>
     /// 当文本内容被选择时的事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnSelect;
+    event BootstrapBlazorJSRuntimeEventHandler OnSelect;
     /// <summary>
     /// SubmitEvent 接口定义用于表示 HTML 表单的提交事件的对象。调用表单的提交操作时，将在表单处触发此事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnSubmit;
+    event BootstrapBlazorJSRuntimeEventHandler OnSubmit;
     /// <summary>
     /// 当元素在焦点事件之后获得焦点时，将触发 focusin 事件。这两个事件的不同之处在于泡沫，而没有。focusin focus
     /// </summary>
-    event BootStrapBlazorEventHandler OnFocusin;
+    event BootstrapBlazorJSRuntimeEventHandler OnFocusin;
     /// <summary>
     /// 在模糊事件之后，当元素失去焦点时，将触发焦点事件。这两个事件的不同之处在于泡沫，而没有。focusout blur
     /// </summary>
-    event BootStrapBlazorEventHandler OnFocusout;
+    event BootstrapBlazorJSRuntimeEventHandler OnFocusout;
     /// <summary>
     /// 加载整个页面（包括样式表、脚本、iframe 和图像等所有依赖资源）时，将触发 load 事件。 这与 DOMContentLoaded 相反，后者在加载页面 DOM 后立即触发，而无需等待资源完成加载。
     /// </summary>
-    event BootStrapBlazorEventHandler OnLoad;
+    event BootstrapBlazorJSRuntimeEventHandler OnLoad;
     /// <summary>
     /// 卸载文档或子资源时，将触发 unload 事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnUnload;
+    event BootstrapBlazorJSRuntimeEventHandler OnUnload;
     /// <summary>
     /// 当窗口、文档及其资源即将卸载时，将触发 beforeunload 事件。此时，文档仍然可见，并且事件仍可取消。
     /// </summary>
-    event BootStrapBlazorEventHandler OnBeforeunload;
+    event BootstrapBlazorJSRuntimeEventHandler OnBeforeunload;
     /// <summary>
     /// 当浏览器的窗口大小被改变时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnResize;
+    event BootstrapBlazorJSRuntimeEventHandler OnResize;
     /// <summary>
     /// 浏览器的窗口被移动时触发此事件
     /// </summary>
-    event BootStrapBlazorEventHandler OnMove;
+    event BootstrapBlazorJSRuntimeEventHandler OnMove;
     /// <summary>
     /// 当 HTML 文档已完全解析，并且所有延迟脚本（和）都已下载并执行时，将触发 DOMContentLoaded 事件。它不会等待图像、子帧和异步脚本等其他内容完成加载。
     /// </summary>
-    event BootStrapBlazorEventHandler OnDOMContentLoaded;
+    event BootstrapBlazorJSRuntimeEventHandler OnDOMContentLoaded;
     /// <summary>
     /// 当文档的 readyState 属性发生更改时，将触发 readystatechange 事件。
     /// </summary>
-    event BootStrapBlazorEventHandler OnReadystatechange;
+    event BootstrapBlazorJSRuntimeEventHandler OnReadystatechange;
     /// <summary>
     /// 运行错误触发onerror
     /// </summary>
-    event BootStrapBlazorEventHandler OnError;
+    event BootstrapBlazorJSRuntimeEventHandler OnError;
     /// <summary>
     /// 该事件在多媒体数据终止加载时触发，而不是发生错误时触发。
     /// </summary>
-    event BootStrapBlazorEventHandler OnAbort;
+    event BootstrapBlazorJSRuntimeEventHandler OnAbort;
     /// <summary>
     /// 滚动元素后将触发滚动事件。 若要检测滚动何时完成，请参阅 Element： scrollend 事件。
     /// 由于 scroll 事件可被高频触发，事件处理程序不应该执行高性能消耗的操作，如 DOM 操作。
     /// </summary>
-    event BootStrapBlazorEventHandler OnScroll;
+    event BootstrapBlazorJSRuntimeEventHandler OnScroll;
     #endregion
 }
---
+++ src/BootstrapBlazor/Services/JSRuntimeEventHandler.Event.cs
@@ -0,0 +1,238 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+partial class JSRuntimeEventHandler
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnClick;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnDblclick;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMouseup;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMousedown;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnContextmenu;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMousewheel;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnDOMMouseScroll;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMouseover;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMouseout;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMousemove;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnSelectstart;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnSelectend;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnKeydown;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnKeypress;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnKeyup;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnOrientationchange;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnTouchstart;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnTouchmove;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnTouchend;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnTouchcancel;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnPointerdown;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnPointermove;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnPointerup;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnPointerleave;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnPointercancel;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnGesturestart;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnGesturechange;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnGestureend;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnFocus;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnBlur;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnChange;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnReset;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnSelect;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnSubmit;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnFocusin;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnFocusout;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnLoad;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnUnload;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnBeforeunload;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnResize;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnMove;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnDOMContentLoaded;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnReadystatechange;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnError;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnAbort;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public event BootstrapBlazorJSRuntimeEventHandler OnScroll;
+}
