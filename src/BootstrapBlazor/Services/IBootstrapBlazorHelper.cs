f85b30cdc8c76c4ac5a43b259e7a6a34c663777e
Argo-AscioTech <argo@live.ca>
2023-04-28 11:33:33 +0800

    refactor 移除 event 变量可为空设置

--- src/BootstrapBlazor/Services/IBootstrapBlazorHelper.cs
+++ src/BootstrapBlazor/Services/IBootstrapBlazorHelper.cs
@@ -69,187 +69,187 @@ public interface IBootstrapBlazorHelper : IAsyncDisposable
     /// <summary>
     /// 鼠标点击时触发些事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnClick;
+    public event BootStrapBlazorEventHandler OnClick;
     /// <summary>
     /// 在单个元素上单击两次鼠标的指针设备按钮时，将触发 dblclick 事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnDblclick;
+    public event BootStrapBlazorEventHandler OnDblclick;
     /// <summary>
     /// 当指针在元素中时， mouseup事件在指针设备按钮放开时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMouseup;
+    public event BootStrapBlazorEventHandler OnMouseup;
     /// <summary>
     /// 事件在指针设备按钮按下时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMousedown;
+    public event BootStrapBlazorEventHandler OnMousedown;
     /// <summary>
     /// 全局属性是指用于某个元素的“上下文菜单”的ID属性。上下文菜单是指在用户交互（例如右键点击）时出现的菜单。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnContextmenu;
+    public event BootStrapBlazorEventHandler OnContextmenu;
     /// <summary>
     /// 过时且非标准的 mousewheel 事件在元素上异步触发，以在操作鼠标滚轮或类似设备时提供更新。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMousewheel;
+    public event BootStrapBlazorEventHandler OnMousewheel;
     /// <summary>
     /// 当操作鼠标滚轮或类似设备并且自上次事件以来累积滚动量超过 1 行或 1 页时，将异步触发 DOM 事件。它由 MouseScrollEvent 接口表示。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnDOMMouseScroll;
+    public event BootStrapBlazorEventHandler OnDOMMouseScroll;
     /// <summary>
     /// 用来获取或设置当前元素的 mouseover 事件的事件处理函数。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMouseover;
+    public event BootStrapBlazorEventHandler OnMouseover;
     /// <summary>
     /// 当移动指针设备（通常是鼠标），使指针不再包含在这个元素或其子元素中时，mouseout 事件被触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMouseout;
+    public event BootStrapBlazorEventHandler OnMouseout;
     /// <summary>
     /// 当指针设备 ( 通常指鼠标 ) 在元素上移动时，mousemove 事件被触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMousemove;
+    public event BootStrapBlazorEventHandler OnMousemove;
     /// <summary>
     /// 当文本内容选择将开始发生时触发的事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnSelectstart;
+    public event BootStrapBlazorEventHandler OnSelectstart;
     /// <summary>
     /// 当文本内容选择结束时发生时触发的事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnSelectend;
+    public event BootStrapBlazorEventHandler OnSelectend;
     /// <summary>
     /// 当键盘上的某个键被按下时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnKeydown;
+    public event BootStrapBlazorEventHandler OnKeydown;
     /// <summary>
     /// 当键盘上的某个键被按下并且释放时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnKeypress;
+    public event BootStrapBlazorEventHandler OnKeypress;
     /// <summary>
     /// 当键盘上的某个键被按开时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnKeyup;
+    public event BootStrapBlazorEventHandler OnKeyup;
     /// <summary>
     /// 事件在设备的纵横方向改变时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnOrientationchange;
+    public event BootStrapBlazorEventHandler OnOrientationchange;
     /// <summary>
     /// 事件在一个或多个触点与触控设备表面接触时被触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnTouchstart;
+    public event BootStrapBlazorEventHandler OnTouchstart;
     /// <summary>
     /// 事件在触点于触控平面上移动时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnTouchmove;
+    public event BootStrapBlazorEventHandler OnTouchmove;
     /// <summary>
     /// 当触点离开触控平面时触发touchend事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnTouchend;
+    public event BootStrapBlazorEventHandler OnTouchend;
     /// <summary>
     /// 事件在触点被中断时触发，中断方式基于特定实现而有所不同（例如，创建了太多的触点）。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnTouchcancel;
+    public event BootStrapBlazorEventHandler OnTouchcancel;
     /// <summary>
     /// 当指针变为活动状态时，将触发该事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnPointerdown;
+    public event BootStrapBlazorEventHandler OnPointerdown;
     /// <summary>
     /// 一个global event handler(全局事件) pointermove 事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnPointermove;
+    public event BootStrapBlazorEventHandler OnPointermove;
     /// <summary>
     /// 当某指针不再活跃时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnPointerup;
+    public event BootStrapBlazorEventHandler OnPointerup;
     /// <summary>
     /// 就像在Element或Window中点击类似，在某目标区域内，发生触点（鼠标指针，触摸等）行为时会触发源于 pointerleave 事件global event handler行为。这个事件本身属于 Pointer Events API 的一部分。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnPointerleave;
+    public event BootStrapBlazorEventHandler OnPointerleave;
     /// <summary>
     /// 当浏览器认为某指针不会再生成新的后续事件时触发（例如某设备不再活跃）
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnPointercancel;
+    public event BootStrapBlazorEventHandler OnPointercancel;
     /// <summary>
     /// 当多个手指接触触摸表面时，将触发 Gesturestart 事件，从而启动新手势。在手势期间，将触发手势更改事件。手势结束后，将触发手势结束事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnGesturestart;
+    public event BootStrapBlazorEventHandler OnGesturestart;
     /// <summary>
     /// 当数字在触摸手势期间移动时，将触发 Gesturechange 事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnGesturechange;
+    public event BootStrapBlazorEventHandler OnGesturechange;
     /// <summary>
     /// 当不再有多个手指接触触摸表面时，将触发 Gestureend 事件，从而结束手势。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnGestureend;
+    public event BootStrapBlazorEventHandler OnGestureend;
     /// <summary>
     /// 当某个元素获得焦点时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnFocus;
+    public event BootStrapBlazorEventHandler OnFocus;
     /// <summary>
     /// 当元素失去焦点时，将触发模糊事件。事件不会冒泡，但后面的相关焦点事件会冒泡。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnBlur;
+    public event BootStrapBlazorEventHandler OnBlur;
     /// <summary>
     /// 当用户修改元素的值时，将为input、select和textarea 元素触发该事件。与输入事件不同，不必为每次更改元素的 .changechangevalue
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnChange;
+    public event BootStrapBlazorEventHandler OnChange;
     /// <summary>
     /// 当表单中RESET的属性被激发时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnReset;
+    public event BootStrapBlazorEventHandler OnReset;
     /// <summary>
     /// 当文本内容被选择时的事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnSelect;
+    public event BootStrapBlazorEventHandler OnSelect;
     /// <summary>
     /// SubmitEvent 接口定义用于表示 HTML 表单的提交事件的对象。调用表单的提交操作时，将在表单处触发此事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnSubmit;
+    public event BootStrapBlazorEventHandler OnSubmit;
     /// <summary>
     /// 当元素在焦点事件之后获得焦点时，将触发 focusin 事件。这两个事件的不同之处在于泡沫，而没有。focusin focus
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnFocusin;
+    public event BootStrapBlazorEventHandler OnFocusin;
     /// <summary>
     /// 在模糊事件之后，当元素失去焦点时，将触发焦点事件。这两个事件的不同之处在于泡沫，而没有。focusout blur
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnFocusout;
+    public event BootStrapBlazorEventHandler OnFocusout;
     /// <summary>
     /// 加载整个页面（包括样式表、脚本、iframe 和图像等所有依赖资源）时，将触发 load 事件。 这与 DOMContentLoaded 相反，后者在加载页面 DOM 后立即触发，而无需等待资源完成加载。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnLoad;
+    public event BootStrapBlazorEventHandler OnLoad;
     /// <summary>
     /// 卸载文档或子资源时，将触发 unload 事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnUnload;
+    public event BootStrapBlazorEventHandler OnUnload;
     /// <summary>
     /// 当窗口、文档及其资源即将卸载时，将触发 beforeunload 事件。此时，文档仍然可见，并且事件仍可取消。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnBeforeunload;
+    public event BootStrapBlazorEventHandler OnBeforeunload;
     /// <summary>
     /// 当浏览器的窗口大小被改变时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnResize;
+    public event BootStrapBlazorEventHandler OnResize;
     /// <summary>
     /// 浏览器的窗口被移动时触发此事件
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnMove;
+    public event BootStrapBlazorEventHandler OnMove;
     /// <summary>
     /// 当 HTML 文档已完全解析，并且所有延迟脚本（和）都已下载并执行时，将触发 DOMContentLoaded 事件。它不会等待图像、子帧和异步脚本等其他内容完成加载。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnDOMContentLoaded;
+    public event BootStrapBlazorEventHandler OnDOMContentLoaded;
     /// <summary>
     /// 当文档的 readyState 属性发生更改时，将触发 readystatechange 事件。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnReadystatechange;
+    public event BootStrapBlazorEventHandler OnReadystatechange;
     /// <summary>
     /// 运行错误触发onerror
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnError;
+    public event BootStrapBlazorEventHandler OnError;
     /// <summary>
     /// 该事件在多媒体数据终止加载时触发，而不是发生错误时触发。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnAbort;
+    public event BootStrapBlazorEventHandler OnAbort;
     /// <summary>
     /// 滚动元素后将触发滚动事件。 若要检测滚动何时完成，请参阅 Element： scrollend 事件。
     /// 由于 scroll 事件可被高频触发，事件处理程序不应该执行高性能消耗的操作，如 DOM 操作。
     /// </summary>
-    public event BootStrapBlazorEventHandler? OnScroll;
+    public event BootStrapBlazorEventHandler OnScroll;
     #endregion
 }
