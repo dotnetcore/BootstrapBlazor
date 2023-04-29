730045179349a619a22690191e20f9f5ae188258
Argo-AscioTech <argo@live.ca>
2023-04-29 20:23:48 +0800

    refactor: 重构 AlertBase 基类

--- src/BootstrapBlazor/Components/Message/MessageItem.razor.cs
+++ src/BootstrapBlazor/Components/Message/MessageItem.razor.cs
@@ -13,13 +13,27 @@ public partial class MessageItem
     /// 获得 样式集合
     /// </summary>
     /// <returns></returns>
-    protected override string? ClassName => CssBuilder.Default("alert")
+    private string? ClassName => CssBuilder.Default("alert")
         .AddClass($"alert-{Color.ToDescriptionString()}", Color != Color.None)
-        .AddClass("is-bar", ShowBar)
+        .AddClass($"border-{Color.ToDescriptionString()}", ShowBorder)
+        .AddClass("shadow", ShowShadow)
+        .AddClass("alert-bar", ShowBar)
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
 
     private string? AutoHideString => IsAutoHide ? "true" : null;
+
+    /// <summary>
+    /// 获得/设置 是否显示阴影 默认 false 不显示
+    /// </summary>
+    [Parameter]
+    public bool ShowShadow { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否显示边框 默认 false 不显示
+    /// </summary>
+    [Parameter]
+    public bool ShowBorder { get; set; }
 
     /// <summary>
     /// 获得/设置 Toast Body 子组件
