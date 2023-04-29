07e7158f7cf08da77d2797865e84ff369d811b83
Argo-AscioTech <argo@live.ca>
2023-04-29 22:18:55 +0800

    wip: 临时提交

---
+++ src/BootstrapBlazor/wwwroot/modules/drag.js
@@ -0,0 +1,57 @@
+﻿import EventHandler from './event-handler.js'
+
+export default {
+    init: element => {
+
+    },
+
+    drag: (element, start, move, end) => {
+        const handleDragStart = e => {
+            let notDrag = false
+            if (isFunction(start)) {
+                notDrag = start(e) || false
+            }
+
+            if (!notDrag) {
+                e.preventDefault()
+                e.stopPropagation()
+
+                document.addEventListener('mousemove', handleDragMove)
+                document.addEventListener('touchmove', handleDragMove)
+                document.addEventListener('mouseup', handleDragEnd)
+                document.addEventListener('touchend', handleDragEnd)
+            }
+        }
+
+        const handleDragMove = e => {
+            if (e.touches && e.touches.length > 1) {
+                return;
+            }
+
+            if (isFunction(move)) {
+                move(e)
+            }
+        }
+
+        const handleDragEnd = e => {
+            if (isFunction(end)) {
+                end(e)
+            }
+
+            const handler = window.setTimeout(() => {
+                window.clearTimeout(handler)
+                document.removeEventListener('mousemove', handleDragMove)
+                document.removeEventListener('touchmove', handleDragMove)
+                document.removeEventListener('mouseup', handleDragEnd)
+                document.removeEventListener('touchend', handleDragEnd)
+            }, 10)
+        }
+
+        element.addEventListener('mousedown', handleDragStart)
+        element.addEventListener('touchstart', handleDragStart)
+    },
+
+    dispose: el => {
+
+    }
+}
--- src/BootstrapBlazor/wwwroot/modules/utility.js
+++ src/BootstrapBlazor/wwwroot/modules/utility.js
@@ -174,52 +174,6 @@ const insertAfter = (element, newEl) => {
     }
 }
 
-const drag = (element, start, move, end) => {
-    const handleDragStart = e => {
-        let notDrag = false
-        if (isFunction(start)) {
-            notDrag = start(e) || false
-        }
-
-        if (!notDrag) {
-            e.preventDefault()
-            e.stopPropagation()
-
-            document.addEventListener('mousemove', handleDragMove)
-            document.addEventListener('touchmove', handleDragMove)
-            document.addEventListener('mouseup', handleDragEnd)
-            document.addEventListener('touchend', handleDragEnd)
-        }
-    }
-
-    const handleDragMove = e => {
-        if (e.touches && e.touches.length > 1) {
-            return;
-        }
-
-        if (isFunction(move)) {
-            move(e)
-        }
-    }
-
-    const handleDragEnd = e => {
-        if (isFunction(end)) {
-            end(e)
-        }
-
-        const handler = window.setTimeout(() => {
-            window.clearTimeout(handler)
-            document.removeEventListener('mousemove', handleDragMove)
-            document.removeEventListener('touchmove', handleDragMove)
-            document.removeEventListener('mouseup', handleDragEnd)
-            document.removeEventListener('touchend', handleDragEnd)
-        }, 10)
-    }
-
-    element.addEventListener('mousedown', handleDragStart)
-    element.addEventListener('touchstart', handleDragStart)
-}
-
 const getDescribedElement = (element, selector = 'aria-describedby') => {
     if (isElement(element)) {
         let id = element.getAttribute(selector)
@@ -383,7 +337,6 @@ export {
     addLink,
     addScript,
     copy,
-    drag,
     insertBefore,
     insertAfter,
     isDisabled,
