7238fe9dae52b3a1ba903850fa1195f58f2aafa1
Argo Zhang <argo@live.ca>
2023-05-01 17:10:40 +0800

    fix(Table): throw exception when model include static property (#1183)

---
+++ src/BootstrapBlazor/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.Reflection;
+
+namespace BootstrapBlazor.Components;
+
+internal static class PropertyInfoExtensions
+{
+    /// <summary>
+    /// 判断属性是否为静态属性
+    /// </summary>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static bool IsStatic(this PropertyInfo p)
+    {
+        var mi = p.GetMethod ?? p.SetMethod;
+        return mi!.IsStatic;
+    }
+}
