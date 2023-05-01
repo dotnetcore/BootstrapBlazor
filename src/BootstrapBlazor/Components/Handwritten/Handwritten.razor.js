ca3bc0d9cb630d592a70b0f7b763a8fe6cfea102
Argo-AscioTech <argo@live.ca>
2023-05-01 16:37:31 +0800

    refactor: HandWritten 使用脚本隔离

--- src/BootstrapBlazor/Components/Handwritten/Handwritten.razor.js
+++ src/BootstrapBlazor/Components/Handwritten/Handwritten.razor.js
@@ -1,108 +1,126 @@
-﻿(function ($) {
-    $.extend({
-        bb_handwritten: function (el, obj, autoStop, method) {
-            //当页面高度超过设备可见高度时，阻止掉touchmove事件。
-            document.body.addEventListener('touchmove', function (e) {
-                e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
-            }, { passive: false }); //passive 参数不能省略，用来兼容ios和android
+﻿import Data from "../../modules/data.js"
+import EventHandler from "../../modules/event-handler.js"
 
-            new lineCanvas({
-                el: el.getElementsByClassName("hw-body")[0], //绘制canvas的父级div
-                clearEl: el.getElementsByClassName('btn-secondary')[0], //清除按钮
-                saveEl: el.getElementsByClassName('btn-primary')[0], //保存按钮
-                obj: obj
-            });
+export function init(id, invoke, callback) {
+    //当页面高度超过设备可见高度时，阻止掉touchmove事件。
+    const el = document.getElementById(id)
+    const clearEl = el.querySelector('.btn-clear')
+    const saveEl = el.querySelector('.btn-save')
+    const elBody = el.querySelector('.hw-body')
+    const canvas = elBody.querySelector('canvas')
 
-            function lineCanvas(obj) {
-                this.linewidth = 1;
-                this.color = "#000000";
-                this.background = "#fff";
-                for (var i in obj) {
-                    this[i] = obj[i];
-                };
-                this.canvas = document.createElement("canvas");
-                this.el.appendChild(this.canvas);
-                this.cxt = this.canvas.getContext("2d");
-                this.canvas.clientTop = this.el.clientWidth;
-                this.canvas.width = this.el.clientWidth;
-                this.canvas.height = this.el.clientHeight;
+    const hw = {
+        el, invoke, callback, clearEl, saveEl, canvas
+    }
+    Data.set(id, hw)
 
-                this.cxt.fillStyle = this.background;
-                this.cxt.fillRect(2, 2, this.canvas.width, this.canvas.height);
+    canvas.width = elBody.offsetWidth;
+    canvas.height = elBody.offsetHeight;
+    createCanvas(hw, {
+        linewidth: 1,
+        color: "#000000",
+        background: "#fff"
+    })
 
-                this.cxt.fillStyle = this.background;
-                this.cxt.strokeStyle = this.color;
-                this.cxt.lineWidth = this.linewidth;
-                this.cxt.lineCap = "round";
+    //阻止默认的处理方式(阻止下拉滑动的效果)
+    hw.preventHandler = e => e.preventDefault()
+    //passive 参数不能省略，用来兼容ios和android
+    hw.preventEventOption = { passive: false }
+    //当页面高度超过设备可见高度时，阻止掉touchmove事件。
+    document.body.addEventListener('touchmove', hw.preventHandler, hw.preventEventOption);
+}
 
-                var isSupportTouch = 'ontouchend' in window;
-                var that = this;
-                var isStart = false;
+export function dispose(id) {
+    const hw = Data.get(id)
+    if (hw !== null) {
+        document.body.removeEventListener('touchmove', hw.preventHandler, hw.preventEventOption)
 
-                //开始绘制
-                var hw_star = function (e) {
-                    isStart = true;
-                    this.cxt.beginPath();
-                    var parentLeft = e.target.offsetParent.offsetLeft;
-                    var parentTop = e.target.offsetParent.offsetTop;
-                    if (isSupportTouch) {
-                        this.cxt.moveTo(e.changedTouches[0].pageX + 2 - parentLeft, e.changedTouches[0].pageY + 2 - parentTop);
-                    }
-                    else {
-                        this.cxt.moveTo(e.pageX + 2 - parentLeft, e.pageY + 2 - parentTop);
-                    }
-                };
-                if (isSupportTouch) {
-                    this.canvas.addEventListener("touchstart", hw_star.bind(this), false);
-                }
-                else {
-                    this.canvas.addEventListener('mousedown', hw_star.bind(this), false);
-                }
+        EventHandler.off(hw.clearEl, 'click')
+        EventHandler.off(hw.saveEl, 'click')
 
-                //绘制中
-                var hw_move = function (e) {
-                    if (isStart) {
-                        var parentLeft = e.target.offsetParent.offsetLeft;
-                        var parentTop = e.target.offsetParent.offsetTop;
-                        if (isSupportTouch) {
-                            this.cxt.lineTo(e.changedTouches[0].pageX + 2 - parentLeft, e.changedTouches[0].pageY + 2 - parentTop);
-                        }
-                        else {
-                            this.cxt.lineTo(e.pageX + 2 - parentLeft, e.pageY + 2 - parentTop);
-                        }
-                        this.cxt.stroke();
-                    }
-                };
-                if (isSupportTouch) {
-                    this.canvas.addEventListener("touchmove", hw_move.bind(this), false);
-                }
-                else {
-                    this.canvas.addEventListener('mousedown', hw_move.bind(this), false);
-                }
+        EventHandler.off(hw.canvas, 'mousedown')
+        EventHandler.off(hw.canvas, 'mousemove')
+        EventHandler.off(hw.canvas, 'mousup')
+        EventHandler.off(hw.canvas, 'touchstart')
+        EventHandler.off(hw.canvas, 'touchmove')
+        EventHandler.off(hw.canvas, 'touchend')
+    }
+}
 
-                //结束绘制
-                var hw_end = function () {
-                    isStart = false;
-                    this.cxt.closePath();
-                };
-                if (isSupportTouch) {
-                    this.canvas.addEventListener("touchend", hw_end.bind(this), false);
-                }
-                else {
-                    this.canvas.addEventListener('mousedown', hw_end.bind(this), false);
-                }
+const createCanvas = (hw, op) => {
+    const canvas = hw.canvas
+    const clearEl = hw.clearEl
+    const saveEl = hw.saveEl
+    const cxt = canvas.getContext("2d", { willReadFrequently: true })
 
-                //清除画布
-                this.clearEl.addEventListener("click", function () {
-                    this.cxt.clearRect(2, 2, this.canvas.width, this.canvas.height);
-                }.bind(this), false);
-                //保存图片，直接转base64
-                this.saveEl.addEventListener("click", function () {
-                    var imgBase64 = this.canvas.toDataURL();
-                    //console.log(imgBase64);
-                    return this.obj.invokeMethodAsync(method, imgBase64);
-                }.bind(this), false);
-            };
+    cxt.fillStyle = op.background
+    cxt.fillRect(0, 0, canvas.width, canvas.height)
+    cxt.fillStyle = op.background
+    cxt.strokeStyle = op.color
+    cxt.lineWidth = op.linewidth
+    cxt.lineCap = "round"
+
+    var isStart = false
+
+    const getOffsetX = e => {
+        let offsetX = e.offsetX
+        if (offsetX === undefined) {
+            const rect = canvas.getBoundingClientRect()
+            offsetX = e.touches[0].clientX - rect.left
         }
-    });
-})(jQuery);
+        return offsetX
+    }
+
+    const getOffsetY = e => {
+        let offsetY = e.offsetY
+        if (offsetY === undefined) {
+            const rect = canvas.getBoundingClientRect()
+            offsetY = e.touches[0].clientY - rect.top
+        }
+        return offsetY
+    }
+
+    //开始绘制
+    const star = e => {
+        isStart = true
+        cxt.beginPath()
+
+        const originX = getOffsetX(e)
+        const originY = getOffsetY(e)
+        cxt.moveTo(originX, originY)
+    }
+
+    //绘制中
+    const move = e => {
+        if (isStart) {
+            const originX = getOffsetX(e)
+            const originY = getOffsetY(e)
+            cxt.lineTo(originX, originY)
+            cxt.stroke()
+        }
+    }
+
+    //结束绘制
+    const end = () => {
+        isStart = false
+        cxt.closePath()
+    }
+
+    EventHandler.on(canvas, "touchstart", star)
+    EventHandler.on(canvas, "touchmove", move)
+    EventHandler.on(canvas, "touchend", end)
+
+    EventHandler.on(canvas, 'mousedown', star)
+    EventHandler.on(canvas, 'mousemove', move)
+    EventHandler.on(canvas, 'mouseup', end)
+
+    //清除画布
+    EventHandler.on(clearEl, "click", () => {
+        cxt.clearRect(0, 0, canvas.width, canvas.height)
+    })
+    //保存图片，直接转base64
+    EventHandler.on(saveEl, "click", () => {
+        var imgBase64 = canvas.toDataURL()
+        hw.invoke.invokeMethodAsync(hw.callback, imgBase64)
+    })
+}
