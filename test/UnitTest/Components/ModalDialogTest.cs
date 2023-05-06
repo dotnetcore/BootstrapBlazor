aed720efd2f61f5322d72cebfb694ecf3f92a210
Argo-AscioTech <argo@live.ca>
2023-05-06 14:07:18 +0800

    test: 更新单元测试

--- test/UnitTest/Components/ModalDialogTest.cs
+++ test/UnitTest/Components/ModalDialogTest.cs
@@ -68,10 +68,10 @@ public class ModalDialogTest : BootstrapBlazorTestBase
         Assert.Contains("btn-maximize", cut.Markup);
 
         var button = cut.Find(".btn-maximize");
-        button.Click();
+        cut.InvokeAsync(() => button.Click());
         Assert.Contains("modal-fullscreen", cut.Markup);
 
-        button.Click();
+        cut.InvokeAsync(() => button.Click());
         Assert.DoesNotContain("modal-fullscreen", cut.Markup);
     }
 
