1764d7a3d8986e20a8372d63dcfc653c24a61c74
Argo Zhang <argo@live.ca>
2023-05-06 13:51:19 +0800

    refactor(Tree): use JavaScript isolation (#1223)
    * refactor: 移除注入服务使用父类服务
    
    * chore: 更新重连脚本
    
    * refactor: 重构重连组件使用脚本隔离
    
    * refactor: 更新配置
    
    * refactor: 更新脚本路径
    
    * refactor: 微调 Pre 组件样式
    
    * chore: 更新 Tree 脚本
    
    * fix: 修复空对象问题
    
    * refactor: 重构组件

---
+++ src/BootstrapBlazor/Components/Tree/Tree.razor.js
@@ -0,0 +1,41 @@
+﻿import Data from "../../modules/data.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+
+    const tree = {el}
+    Data.set(id, tree)
+
+    EventHandler.on(el, 'mouseenter', '.tree-content', e => {
+        const ele = e.delegateTarget.parentNode
+        ele.classList.add('hover')
+    })
+
+    EventHandler.on(el, 'mouseleave', '.tree-content', e => {
+        const ele = e.delegateTarget.parentNode
+        ele.classList.remove('hover')
+    })
+
+    // 支持 Radio
+    EventHandler.on(el, 'click', '.tree-node', e => {
+        const node = e.delegateTarget
+        const prev = node.previousElementSibling;
+        const radio = prev.querySelector('[type="radio"]')
+        if (radio && radio.getAttribute('disabled') !== 'disabled') {
+            radio.click();
+        }
+    })
+}
+
+export function dispose(id) {
+    const tree = Data.get(id)
+    if (tree) {
+        EventHandler.off(tree.el, 'mouseenter')
+        EventHandler.off(tree.el, 'mouseleave')
+        EventHandler.off(tree.el, 'click', '.tree-node')
+    }
+}
