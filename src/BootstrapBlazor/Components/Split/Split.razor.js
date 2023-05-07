3107d103961e0c2612cc110d5bbfce89acc62cb6
Argo Zhang <argo@live.ca>
2023-05-07 15:16:21 +0800

    refactor(Split): use JavaScript isolation (#1227)
    * refactor: 移除 Split 脚本
    
    * refactor: 重构组件

---
+++ src/BootstrapBlazor/Components/Split/Split.razor.js
@@ -0,0 +1,67 @@
+﻿import { getHeight, getInnerHeight } from "../../modules/utility.js"
+import Data from "../../modules/data.js"
+import Drag from "../../modules/drag.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(id) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+    const split = { el }
+    Data.set(id, split)
+
+    let splitWidth = el.offsetWidth
+    let splitHeight = el.offsetHeight
+    let curVal = 0
+    let newVal = 0
+    let originX = 0
+    let originY = 0
+    const splitWrapper = el.firstElementChild
+    const isVertical = !splitWrapper.classList.contains('is-horizontal')
+    const splitBar = splitWrapper.children[1]
+    const splitLeft = splitWrapper.children[0]
+    const splitRight = splitWrapper.children[2]
+    Drag.drag(splitBar,
+        e => {
+            splitWidth = el.offsetWidth
+            splitHeight = el.offsetHeight
+            if (isVertical) {
+                originY = e.clientY || e.touches[0].clientY
+                curVal = splitLeft.offsetHeight * 100 / splitHeight
+            }
+            else {
+                originX = e.clientX || e.touches[0].clientX
+                curVal = splitLeft.offsetWidth * 100 / splitWidth
+            }
+            el.classList.add('dragging')
+        },
+        e => {
+            if (isVertical) {
+                const eventY = e.clientY || e.changedTouches[0].clientY
+                newVal = Math.ceil((eventY - originY) * 100 / splitHeight) + curVal
+            }
+            else {
+                const eventX = e.clientX || e.changedTouches[0].clientX
+                newVal = Math.ceil((eventX - originX) * 100 / splitWidth) + curVal
+            }
+
+            if (newVal <= 0) newVal = 0
+            if (newVal >= 100) newVal = 100
+
+            splitLeft.style.flexBasis = `${newVal}%`
+            splitRight.style.flexBasis = `${100 - newVal}%`
+        },
+        () => {
+            el.classList.remove('dragging')
+        })
+}
+
+export function dispose(id) {
+    const split = Data.get(id)
+    Data.remove(id)
+
+    if (split) {
+        Drag.dispose(split.el)
+    }
+}
--- src/BootstrapBlazor/wwwroot/bundle/bootstrap.blazor.js
+++ src/BootstrapBlazor/wwwroot/bundle/bootstrap.blazor.js
@@ -611,57 +611,6 @@
 
 (function ($) {
     $.extend({
-        bb_split: function (el) {
-            var $split = $(el);
-
-            var splitWidth = $split.innerWidth();
-            var splitHeight = $split.innerHeight();
-            var curVal = 0;
-            var newVal = 0;
-            var originX = 0;
-            var originY = 0;
-            var isVertical = !$split.children().hasClass('is-horizontal');
-
-            $split.children().children('.split-bar').drag(
-                function (e) {
-                    splitWidth = $split.innerWidth();
-                    splitHeight = $split.innerHeight();
-                    if (isVertical) {
-                        originY = e.clientY || e.touches[0].clientY;
-                        curVal = $split.children().children('.split-left').innerHeight() * 100 / splitHeight;
-                    }
-                    else {
-                        originX = e.clientX || e.touches[0].clientX;
-                        curVal = $split.children().children('.split-left').innerWidth() * 100 / splitWidth;
-                    }
-                    $split.toggleClass('dragging');
-                },
-                function (e) {
-                    if (isVertical) {
-                        var eventY = e.clientY || e.changedTouches[0].clientY;
-                        newVal = Math.ceil((eventY - originY) * 100 / splitHeight) + curVal;
-                    }
-                    else {
-                        var eventX = e.clientX || e.changedTouches[0].clientX;
-                        newVal = Math.ceil((eventX - originX) * 100 / splitWidth) + curVal;
-                    }
-
-                    if (newVal <= 0) newVal = 0;
-                    if (newVal >= 100) newVal = 100;
-
-                    $split.children().children('.split-left').css({ 'flex-basis': newVal.toString() + '%' });
-                    $split.children().children('.split-right').css({ 'flex-basis': (100 - newVal).toString() + '%' });
-                    $split.attr('data-split', newVal);
-                },
-                function (e) {
-                    $split.toggleClass('dragging');
-                });
-        }
-    });
-})(jQuery);
-
-(function ($) {
-    $.extend({
         bb_tab: function (el) {
             var $el = $(el);
             var handler = window.setInterval(function () {
