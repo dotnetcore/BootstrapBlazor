a7670230f7b6f8e6acb7c71844d186de5f326eea
Argo-AscioTech <argo@live.ca>
2023-05-07 20:48:25 +0800

    refactor: 更新脚本

--- src/BootstrapBlazor/Components/Tab/Tab.razor.js
+++ src/BootstrapBlazor/Components/Tab/Tab.razor.js
@@ -1,4 +1,18 @@
 ﻿(function ($) {
+    $.extend({
+        bb_tab: function (el) {
+            var $el = $(el);
+            var handler = window.setInterval(function () {
+                if ($el.is(':visible')) {
+                    window.clearInterval(handler);
+                    $el.lgbTab('active');
+                }
+            }, 200);
+        }
+    });
+})(jQuery);
+
+(function ($) {
     /**
      * Tab
      * @param {any} element
