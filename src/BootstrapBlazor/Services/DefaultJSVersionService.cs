1ca75cd0c7211f360e952b55e0ffbd7d6dd3e6b3
Argo-AscioTech <argo@live.ca>
2023-05-12 10:22:05 +0800

    feat: 增加 IVersionService 服务

---
+++ src/BootstrapBlazor/Services/DefaultJSVersionService.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+class DefaultJSVersionService : IVersionService
+{
+    private static string? _tick;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public string GetVersion()
+    {
+        _tick ??= DateTime.Now.ToString("HHmmss");
+        return _tick;
+    }
+}
