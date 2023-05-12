e2942cb84c172630cfbce37bc5ed6e463de233ca
Argo-AscioTech <argo@live.ca>
2023-05-12 17:47:33 +0800

    refactor: 移除 VersionHelper

--- src/Tools/JSVersionMaker/Program.cs
+++ src/Tools/JSVersionMaker/Program.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using System;
-using System.Linq;
 using System.Xml;
 
 namespace JSVersionMaker;
@@ -12,9 +10,6 @@ class Program
 {
     static void Main(params string[] args)
     {
-#if DEBUG
-        args = new string[] { "D:\\Argo\\src\\BootstrapBlazor\\src\\BootstrapBlazor" };
-#endif
         if (args.Length == 0)
         {
             Console.WriteLine("未提供项目路径");
@@ -25,10 +20,10 @@ class Program
         ProcessFiles(args[0]);
     }
 
-    static void ProcessFiles(string projectPath)
+    static void ProcessFiles(string projectFileName)
     {
-        var version = GetVersion(projectPath);
-
+        var version = GetVersion(projectFileName);
+        var projectPath = Path.GetDirectoryName(projectFileName);
         if (Directory.Exists(projectPath))
         {
             foreach (var file in Directory.EnumerateFiles(projectPath, "*.js", SearchOption.AllDirectories))
@@ -44,10 +39,14 @@ class Program
         }
     }
 
-    static string GetVersion(string projectPath)
+    /// <summary>
+    /// 通过项目文件获得版本号信息
+    /// </summary>
+    /// <param name="projectFile"></param>
+    /// <returns></returns>
+    public static string GetVersion(string projectFile)
     {
         string? ver = null;
-        var projectFile = Path.Combine(projectPath, "BootstrapBlazor.csproj");
         if (File.Exists(projectFile))
         {
             try
