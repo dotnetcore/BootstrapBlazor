edcfbc28905b6a813d1f50d202180c85f13bb0f1
Argo-AscioTech <argo@live.ca>
2023-05-22 12:53:50 +0800

    feat: 增加 Cookie 认证

---
+++ src/BootstrapBlazor.Server/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Longbow.GiteeAuth;
+using Longbow.GitHubAuth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BootstrapBlazor.Server.Controllers
+{
+    /// <summary>
+    /// Account controller.
+    /// </summary>
+    public class AccountController : Controller
+    {
+        /// <summary>
+        /// Gitee 认证
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Gitee()
+        {
+            return Challenge(GiteeDefaults.AuthenticationScheme);
+        }
+
+        /// <summary>
+        /// GitHub 认证
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GitHub()
+        {
+            return Challenge(GitHubDefaults.AuthenticationScheme);
+        }
+    }
+}
