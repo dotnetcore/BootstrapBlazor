ee65f775c8fa75473b0e74b39ddd8f4bf91ca3b3
Argo-AscioTech <argo@live.ca>
2023-05-23 15:07:28 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Services/HtmlRendererTest.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Services;
+
+public class HtmlRendererTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public async Task Render_Ok()
+    {
+        var renderService = Context.Services.GetRequiredService<IComponentHtmlRenderer>();
+        var html = await renderService.RenderAsync<Button>();
+        Assert.NotEmpty(html);
+    }
+}
