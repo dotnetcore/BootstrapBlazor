fbd56782bf89e26b91497441c6dd25b4db7642ba
Argo Zhang <argo@live.ca>
2023-05-23 20:32:46 +0800

    feat(AzureOpenAI): add GetChatStreaming api (#1295)
    * refactor: 增加异步流支持
    
    * doc: 更改为流推送模式
    
    * chore: 删除样式
    
    * feat: 增加自动获得焦点功能
    
    * chore: 移除接口修饰符
    
    * chore: 删除冗余代码
    
    * chore: bump version 7.0.0
    
    * chore: 更新 OpenAI 依赖包
    
    * doc: 增加空内容保护

--- src/BootstrapBlazor.Shared/Demos/AzureOpenAI/AzureOpenAIChat.razor.js
+++ src/BootstrapBlazor.Shared/Demos/AzureOpenAI/AzureOpenAIChat.razor.js
@@ -4,17 +4,23 @@ export function init(id) {
     const el = document.getElementById(id)
 
     EventHandler.on(el, 'keyup', e => {
-        if (e.key === 'Enter' && !e.shiftKey) {
+        if (e.key === 'Enter' && !e.shiftKey && el.value.trim('\n').length > 0) {
             el.blur()
             document.querySelector('.btn-send').click()
         }
     })
 }
 
-export function scroll() {
+export function scroll(id) {
     const body = document.querySelector('.chat-body')
-    if(body.offsetHeight < body.scrollHeight) {
+    if (body.offsetHeight < body.scrollHeight) {
         body.scrollTop = body.scrollHeight
+    }
+
+    // focus input
+    const el = document.getElementById(id)
+    if (el) {
+        el.focus()
     }
 }
 
--- src/Extensions/Components/BootstrapBlazor.AzureOpenAI/Services/IAzureOpenAIService.cs
+++ src/Extensions/Components/BootstrapBlazor.AzureOpenAI/Services/IAzureOpenAIService.cs
@@ -8,16 +8,24 @@ namespace BootstrapBlazor.Components;
 public interface IAzureOpenAIService
 {
     /// <summary>
-    /// 
+    /// 获得聊天响应集合
     /// </summary>
     /// <param name="context"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    public Task<IEnumerable<AzureOpenAIChatMessage>> GetChatCompletionsAsync(string context, CancellationToken cancellationToken = default);
+    Task<IEnumerable<AzureOpenAIChatMessage>> GetChatCompletionsAsync(string context, CancellationToken cancellationToken = default);
 
     /// <summary>
-    /// 
+    /// 获得聊天响应集合流
+    /// </summary>
+    /// <param name="context"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    IAsyncEnumerable<AzureOpenAIChatMessage> GetChatCompletionsStreamingAsync(string context, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// 创建话题
     /// </summary>
     /// <returns></returns>
-    public Task CreateNewTopic();
+    Task CreateNewTopic();
 }
