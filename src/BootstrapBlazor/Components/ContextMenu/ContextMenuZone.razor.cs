e1d4f87fc87bb7acbd307aed1ae34e5e02d68c42
Argo-AscioTech <argo@live.ca>
2023-05-26 14:15:11 +0800

    feat: 增加右键菜单组件

---
+++ src/BootstrapBlazor/Components/ContextMenu/ContextMenuZone.razor.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ContextMenuZone 组件
+/// </summary>
+public partial class ContextMenuZone : BootstrapModuleComponentBase
+{
+    /// <summary>
+    /// 获得/设置 子组件
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 包裹组件 TagName 默认为 div
+    /// </summary>
+    [Parameter]
+    public string WrapperTag { get; set; } = "div";
+
+    /// <summary>
+    /// 获得/设置 是否停止广播 默认 true
+    /// </summary>
+    [Parameter]
+    public bool StopPropagation { get; set; } = true;
+
+    private string? ClassString => CssBuilder.Default()
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private Task OnContextMenu(MouseEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
+}
