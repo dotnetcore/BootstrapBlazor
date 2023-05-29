4884c4fe45dd7cd233be0894470d00600a02af30
Argo Zhang <argo@live.ca>
2023-05-29 13:08:12 +0800

    feat(BootstrapDynamicComponent): add new overload method for CreateComponent (#1320)

--- src/BootstrapBlazor/Utils/BootstrapDynamicComponent.cs
+++ src/BootstrapBlazor/Utils/BootstrapDynamicComponent.cs
@@ -36,7 +36,7 @@ public class BootstrapDynamicComponent
     /// <typeparam name="TCom"></typeparam>
     /// <param name="parameters">TCom 组件所需要的参数集合</param>
     /// <returns></returns>
-    public static BootstrapDynamicComponent CreateComponent<TCom>(IDictionary<string, object?>? parameters = null) where TCom : IComponent => new(typeof(TCom), parameters);
+    public static BootstrapDynamicComponent CreateComponent<TCom>(IDictionary<string, object?>? parameters = null) where TCom : IComponent => CreateComponent(typeof(TCom), parameters);
 
     /// <summary>
     /// 创建自定义组件方法
@@ -44,6 +44,14 @@ public class BootstrapDynamicComponent
     /// <typeparam name="TCom"></typeparam>
     /// <returns></returns>
     public static BootstrapDynamicComponent CreateComponent<TCom>() where TCom : IComponent => CreateComponent<TCom>(new Dictionary<string, object?>());
+
+    /// <summary>
+    /// 创建自定义组件方法
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    public static BootstrapDynamicComponent CreateComponent(Type type, IDictionary<string, object?>? parameters = null) => new(type, parameters);
 
     /// <summary>
     /// 创建组件实例并渲染
