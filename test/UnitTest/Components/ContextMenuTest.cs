045db424bcddc068de68f03ea161eea70750b218
Argo-AscioTech <argo@live.ca>
2023-05-31 01:02:00 +0800

    test: 增加单元测试

--- test/UnitTest/Components/ContextMenuTest.cs
+++ test/UnitTest/Components/ContextMenuTest.cs
@@ -10,6 +10,63 @@ namespace UnitTest.Components;
 
 public class ContextMenuTest : BootstrapBlazorTestBase
 {
+    [Fact]
+    public void ContextMenu_Ok()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var foo = Foo.Generate(localizer);
+        var clicked = false;
+
+        var cut = Context.RenderComponent<ContextMenuZone>(pb =>
+        {
+            pb.AddChildContent<ContextMenuTrigger>(pb =>
+            {
+                pb.Add(a => a.WrapperTag, "div");
+                pb.Add(a => a.ContextItem, foo);
+                pb.AddChildContent(pb =>
+                {
+                    pb.OpenElement(0, "div");
+                    pb.AddAttribute(1, "class", "context-trigger");
+                    pb.AddContent(1, foo.Name);
+                    pb.CloseElement();
+                });
+            });
+            pb.AddChildContent<ContextMenu>(pb =>
+            {
+                pb.Add(a => a.ShowShadow, true);
+                pb.AddChildContent<ContextMenuItem>(pb =>
+                {
+                    pb.Add(a => a.Icon, "fa fa-test");
+                    pb.Add(a => a.Text, "Test");
+                    pb.Add(a => a.Disabled, false);
+                    pb.Add(a => a.OnClick, (item, value) =>
+                    {
+                        clicked = true;
+                        return Task.CompletedTask;
+                    });
+                });
+            });
+        });
+
+        cut.InvokeAsync(() =>
+        {
+            var row = cut.Find(".context-trigger");
+            row.ContextMenu(0, 10, 10, 10, 10, 2, 2);
+
+            var menu = cut.FindComponent<ContextMenu>();
+            menu.Contains("shadow");
+            var pi = typeof(ContextMenu).GetProperty("ContextItem", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            Assert.NotNull(pi);
+
+            var v = pi.GetValue(menu.Instance);
+            Assert.NotNull(v);
+
+            var item = menu.Find(".dropdown-item");
+            item.Click();
+            Assert.True(clicked);
+        });
+    }
+
     [Theory]
     [InlineData(TableRenderMode.Table)]
     [InlineData(TableRenderMode.CardView)]
@@ -42,7 +99,7 @@ public class ContextMenuTest : BootstrapBlazorTestBase
                     pb.Add(a => a.Disabled, false);
                     pb.Add(a => a.OnClick, (item, value) =>
                     {
-                        clicked = true; 
+                        clicked = true;
                         return Task.CompletedTask;
                     });
                 });
@@ -51,7 +108,66 @@ public class ContextMenuTest : BootstrapBlazorTestBase
 
         cut.InvokeAsync(() =>
         {
-            var row = cut.Find(".table-row");
+            var row = renderMode == TableRenderMode.CardView ? cut.Find(".table-row") : cut.Find("tbody tr");
+            row.ContextMenu(0, 10, 10, 10, 10, 2, 2);
+
+            var menu = cut.FindComponent<ContextMenu>();
+            var pi = typeof(ContextMenu).GetProperty("ContextItem", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            Assert.NotNull(pi);
+
+            var v = pi.GetValue(menu.Instance);
+            Assert.NotNull(v);
+
+            var item = menu.Find(".dropdown-item");
+            item.Click();
+            Assert.True(clicked);
+        });
+    }
+
+    [Fact]
+    public void ContextMenu_TreeView()
+    {
+        var items = new List<TreeFoo>
+        {
+            new() { Text = "Node1", Id = "1010" },
+            new() { Text = "Node2", Id = "1020" },
+
+            new() { Text = "Node11", Id = "1011", ParentId = "1010" },
+            new() { Text = "Node12", Id = "1011", ParentId = "1010" },
+
+            new() { Text = "Node21", Id = "1021", ParentId = "1020" },
+            new() { Text = "Node22", Id = "1021", ParentId = "1020" }
+        };
+
+        // 根节点
+        var nodes = TreeFoo.CascadingTree(items).ToList();
+        var clicked = false;
+
+        var cut = Context.RenderComponent<ContextMenuZone>(pb =>
+        {
+            pb.AddChildContent<TreeView<TreeFoo>>(pb =>
+            {
+                pb.Add(a => a.Items, nodes);
+            });
+            pb.AddChildContent<ContextMenu>(pb =>
+            {
+                pb.AddChildContent<ContextMenuItem>(pb =>
+                {
+                    pb.Add(a => a.Icon, "fa fa-test");
+                    pb.Add(a => a.Text, "Test");
+                    pb.Add(a => a.Disabled, false);
+                    pb.Add(a => a.OnClick, (item, value) =>
+                    {
+                        clicked = true;
+                        return Task.CompletedTask;
+                    });
+                });
+            });
+        });
+
+        cut.InvokeAsync(() =>
+        {
+            var row = cut.Find(".tree-content");
             row.ContextMenu(0, 10, 10, 10, 10, 2, 2);
 
             var menu = cut.FindComponent<ContextMenu>();
