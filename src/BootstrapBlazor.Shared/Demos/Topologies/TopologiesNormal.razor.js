6f401f56d63398fca5eb9aaacfe5f5345fad541a
Argo Zhang <argo@live.ca>
2023-06-04 19:02:16 +0800

    refactor: 精简代码

--- src/BootstrapBlazor.Shared/Demos/Topologies/TopologiesNormal.razor.js
+++ src/BootstrapBlazor.Shared/Demos/Topologies/TopologiesNormal.razor.js
@@ -1,19 +1,11 @@
-﻿import Data from '../../../BootstrapBlazor/modules/data.js'
-
-export function init(el, invoker, callback) {
-    const meta = {}
-    Data.set(el, meta)
-
-    window.BlazorDemoTopology = {}
-    window.BlazorDemoTopology.handler = tagName => {
-        invoker.invokeMethodAsync(callback, tagName)
+﻿export function init(invoke, callback) {
+    window.BlazorDemoTopology = {
+        handler: tagName => {
+            invoke.invokeMethodAsync(callback, tagName)
+        }
     }
 }
 
 export function execute() {
     window.topology.setOptions({ hoverColor: '', hoverCursor: '', activeColor: '' });
-}
-
-export function dispose(el) {
-    Data.remove(el)
 }
