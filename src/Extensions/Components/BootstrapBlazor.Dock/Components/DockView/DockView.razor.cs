5687e532992d9370600e56f0f3ee1bfeae68aef2
zhangpeihang <948869991@qq.com>
2023-06-05 17:47:11 +0800

    feat:  添加 dock 组件

---
+++ src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockView.razor.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class DockView
+    {
+        [NotNull]
+        private IJSObjectReference? DockViewModule { get; set; }
+
+        [NotNull]
+        private ElementReference? Element { get; set; }
+
+        [Inject]
+        [NotNull]
+        private IJSRuntime? JSRuntime { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Parameter]
+#if NET6_0_OR_GREATER
+        [EditorRequired]
+#endif
+        [NotNull]
+        public DockViewConfig? Config { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                // import JavaScript
+                DockViewModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BootstrapBlazor.Dock/Components/DockView/DockView.razor.js");
+                await DockViewModule.InvokeVoidAsync("init", Element, Config);
+            }
+        }
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockView.razor.js
@@ -0,0 +1,36 @@
+﻿import Data from '../../../BootstrapBlazor/modules/data.js'
+import { addLink } from '../../../BootstrapBlazor/modules/utility.js'
+import "../../js/golden-layout.js"
+
+let components = [];
+
+export function init(el, config) {
+    addLink("./_content/BootstrapBlazor.Dock/css/goldenlayout-base.css");
+    addLink("./_content/BootstrapBlazor.Dock/css/goldenlayout-light-theme.css")
+    try {
+        console.log(config)
+        expandConfig(config);
+        console.log(components)
+        const layout = new goldenLayout.GoldenLayout(config, el);
+        // 循环注册 component
+        for (var i = 0; i < components.length; i++) {
+            layout.registerComponent(components[i], (container, componentState) => {
+            })
+        }
+        layout.init();
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+function expandConfig(val) {
+    if (val.content == null) {
+        components.push(val.componentName)
+    } else {
+        if (val.content) {
+            for (var i = 0; i < val.content.length; i++) {
+                expandConfig(val.content[i])
+            }
+        }
+    }
+}
