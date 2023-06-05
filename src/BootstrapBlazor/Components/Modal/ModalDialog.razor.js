4ae1705e53d56d6943bc5d138f894c5c228ff27b
Argo-AscioTech <argo@live.ca>
2023-06-05 19:32:28 +0800

    feat: 增加 resize 脚本

--- src/BootstrapBlazor/Components/Modal/ModalDialog.razor.js
+++ src/BootstrapBlazor/Components/Modal/ModalDialog.razor.js
@@ -4,51 +4,104 @@ import EventHandler from "../../modules/event-handler.js?v=$version"
 
 export function init(id) {
     const el = document.getElementById(id)
-    const modal = {
+    const dialog = {
         el
     }
-    Data.set(id, modal)
+    Data.set(id, dialog)
 
-    modal.disposeDrag = () => {
-        if (modal.header) {
-            EventHandler.off(modal.header, 'mousedown')
-            EventHandler.off(modal.header, 'touchstart')
-            modal.header = null
+    dialog.draggable = el.classList.contains('is-draggable')
+    if (dialog.draggable) {
+        dialog.disposeDrag = () => {
+            if (dialog.header) {
+                EventHandler.off(dialog.header, 'mousedown')
+                EventHandler.off(dialog.header, 'touchstart')
+                dialog.header = null
+            }
         }
+
+        dialog.disposeDrag()
+
+        dialog.originX = 0;
+        dialog.originY = 0;
+        dialog.dialogWidth = 0;
+        dialog.dialogHeight = 0;
+        dialog.pt = { top: 0, left: 0 };
+
+        dialog.header = el.querySelector('.modal-header')
+        drag(dialog.header,
+            e => {
+                if (e.target.closest('.modal-header-buttons')) {
+                    return true
+                }
+                dialog.originX = e.clientX || e.touches[0].clientX;
+                dialog.originY = e.clientY || e.touches[0].clientY;
+
+                const rect = el.querySelector('.modal-content').getBoundingClientRect()
+                dialog.dialogWidth = rect.width
+                dialog.dialogHeight = rect.height
+                dialog.pt.top = rect.top
+                dialog.pt.left = rect.left
+
+                el.style.margin = `${dialog.pt.top}px 0 0 ${dialog.pt.left}px`
+                el.style.width = `${dialog.dialogWidth}px`
+                el.classList.add('is-drag')
+            },
+            e => {
+                if (el.classList.contains('is-drag')) {
+                    const eventX = e.clientX || (e.touches != null && e.touches[0].clientX) || 0;
+                    const eventY = e.clientY || (e.touches != null && e.touches[0].clientY) || 0;
+
+                    let newValX = dialog.pt.left + Math.ceil(eventX - dialog.originX);
+                    let newValY = dialog.pt.top + Math.ceil(eventY - dialog.originY);
+
+                    if (newValX <= 0) newValX = 0;
+                    if (newValY <= 0) newValY = 0;
+
+                    if (newValX + dialog.dialogWidth < window.innerWidth) {
+                        el.style.marginLeft = `${newValX}px`
+                    }
+                    if (newValY + dialog.dialogHeight < window.innerHeight) {
+                        el.style.marginTop = `${newValY}px`
+                    }
+                }
+            },
+            () => {
+                el.classList.remove('is-drag')
+            }
+        )
     }
 
     const resize = el.querySelector('.modal-resizer')
     if (resize) {
-        modal.resize = resize
-
-        modal.disposeResizeDrag = () => {
+        dialog.resize = resize
+        dialog.disposeResizeDrag = () => {
             EventHandler.off(resize, 'mousedown')
             EventHandler.off(resize, 'touchstart')
-            modal.resize = null
+            dialog.resize = null
         }
 
-        const dialog = el.querySelector('.modal-content')
+        const content = el.querySelector('.modal-content')
         drag(resize,
             e => {
-                dialog.originX = e.clientX || e.touches[0].clientX;
-                dialog.originY = e.clientY || e.touches[0].clientY;
+                content.originX = e.clientX || (e.touches != null && e.touches[0].clientX) || 0;
+                content.originY = e.clientY || (e.touches != null && e.touches[0].clientY) || 0;
 
-                const rect = dialog.getBoundingClientRect()
-                dialog.dialogWidth = rect.width
-                dialog.dialogHeight = rect.height
+                const rect = content.getBoundingClientRect()
+                content.dialogWidth = rect.width
+                content.dialogHeight = rect.height
 
-                dialog.style.maxWidth = 'auto'
-                dialog.style.width = `${dialog.dialogWidth}px`
-                dialog.style.height = `${dialog.dialogHeight}px`
-                dialog.classList.add('is-resize')
+                content.style.maxWidth = 'auto'
+                content.style.width = `${content.dialogWidth}px`
+                content.style.height = `${content.dialogHeight}px`
+                content.classList.add('is-resize')
             },
             e => {
-                if (dialog.classList.contains('is-resize')) {
+                if (content.classList.contains('is-resize')) {
                     const eventX = e.clientX || e.changedTouches[0].clientX;
                     const eventY = e.clientY || e.changedTouches[0].clientY;
 
-                    let newValX = dialog.dialogWidth + Math.ceil(eventX - dialog.originX);
-                    let newValY = dialog.dialogHeight + Math.ceil(eventY - dialog.originY);
+                    let newValX = content.dialogWidth + Math.ceil(eventX - content.originX);
+                    let newValY = content.dialogHeight + Math.ceil(eventY - content.originY);
 
                     if (newValX > window.innerWidth) {
                         newValX = window.innerWidth
@@ -57,30 +110,30 @@ export function init(id) {
                         newValY = window.innerHeight
                     }
 
-                    dialog.style.maxWidth = `${newValX}px`
-                    dialog.style.width = `${newValX}px`
-                    dialog.style.height = `${newValY}px`
+                    content.style.maxWidth = `${newValX}px`
+                    content.style.width = `${newValX}px`
+                    content.style.height = `${newValY}px`
                     el.style.setProperty('--bs-modal-width', `${newValX}px`)
                 }
             },
             () => {
-                dialog.classList.remove('is-resize')
+                content.classList.remove('is-resize')
             }
         )
     }
 }
 
 export function dispose(id) {
-    const modal = Data.get(id)
+    const dialog = Data.get(id)
     Data.remove(id)
 
-    if (modal) {
-        if (modal.draggable) {
-            modal.disposeDrag()
+    if (dialog) {
+        if (dialog.draggable) {
+            dialog.disposeDrag()
         }
 
-        if (modal.resize) {
-            modal.disposeResizeDrag()
+        if (dialog.resize) {
+            dialog.disposeResizeDrag()
         }
     }
 }
