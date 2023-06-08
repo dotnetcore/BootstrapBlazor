3c32bb4343347709953154025f5383c74b098444
Argo-AscioTech <argo@live.ca>
2023-06-08 10:25:14 +0800

    feat: 增加 LocalStorage 功能

--- src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockView.razor.js
+++ src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockView.razor.js
@@ -1,6 +1,7 @@
 ﻿import "../../js/golden-layout.js"
 import { addLink } from '../../../BootstrapBlazor/modules/utility.js'
 import Data from '../../../BootstrapBlazor/modules/data.js'
+import EventHandler from '../../../BootstrapBlazor/modules/event-handler.js'
 
 export async function init(id, option, invoke, callback) {
     const el = document.getElementById(id)
@@ -9,22 +10,21 @@ export async function init(id, option, invoke, callback) {
     }
 
     await addLink("./_content/BootstrapBlazor.Dock/css/goldenlayout-bb.css")
-    const dock = { el, invoke, callback, layout: createGoldenLayout(option.content, el) }
+    const dock = { el, option, invoke, callback, layout: createGoldenLayout(option, el) }
     Data.set(id, dock)
 }
 
 export function update(id, option) {
     const dock = Data.get(id)
-    console.log(option.content)
 
     if (dock) {
         // 处理 toogle 逻辑
-        const items = getAllContentItems(option.content)
+        const items = getAllContentItems(option.option.content)
         const comps = dock.layout.getAllContentItems().filter(s => s.isComponent);
 
         // gt 没有 items 有时添加
         items.forEach(v => {
-            const c = comps.find(i => i.id == v.id)
+            const c = comps.find(i => i.id === v.id)
             if (c === undefined) {
                 dock.layout.root.contentItems[0].addItem(v, 0)
             }
@@ -32,7 +32,7 @@ export function update(id, option) {
 
         // gt 有 items 没有时移除
         comps.forEach(v => {
-            const c = items.find(i => i.id == v.id)
+            const c = items.find(i => i.id === v.id)
             if (c === undefined) {
                 closeItem(dock.el, v)
             }
@@ -41,14 +41,21 @@ export function update(id, option) {
 }
 
 export function dispose(id) {
+    const dock = Data.get(id)
+    Data.remove(id)
 
+    if (dock == null) {
+        return
+    }
+
+    saveConfig(dock.option, dock.layout)
 }
 
 const getAllContentItems = content => {
     const items = []
 
     content.forEach(v => {
-        if (v.type == 'component') {
+        if (v.type === 'component') {
             items.push(v)
         }
 
@@ -59,21 +66,28 @@ const getAllContentItems = content => {
     return items
 }
 
-const createGoldenLayout = (content, el) => {
-    const config = getConfig(content)
+const createGoldenLayout = (option, el) => {
+    const config = getConfig(option)
 
     const layout = new goldenLayout.GoldenLayout(config, el)
     layout.registerComponentFactoryFunction("component", (container, state) => {
-        // 当从缓存拿config时, id 为没刷新前的 id，页面元素已经改变
-        //const el = document.getElementById(state.id)
-        //el.classList.remove('d-none')
-        //container.element.append(el)
+        const el = document.getElementById(state.id)
+        if (el) {
+            el.classList.remove('d-none')
+            container.element.append(el)
+        }
     })
     layout.init()
     layout.resizeWithContainerAutomatically = true
 
-    layout.on("stateChanged", () => saveConfig(layout))
-    layout.on("tabClosed", component => closeItem(el, component))
+    //layout.on("tabClosed", component => closeItem(el, component))
+
+    EventHandler.on(el, 'click', '.lm_close_tab', e => {
+        console.log(e.delegateTarget)
+        const stack = e.delegateTarget.closest('.lm_item.lm_stack')
+        const comp = stack.querySelector('.bb-dock-item')
+        console.log(comp)
+    })
     return layout
 }
 
@@ -84,31 +98,61 @@ const closeItem = (el, component) => {
     el.append(item)
 }
 
-const getConfig = content => {
+const getConfig = option => {
     let config = null
-    const localConfig = localStorage.getItem("uni_gl_layout");
-    if (localConfig) {
-        // 当tab全部关闭时，没有root节点
-        const configItem = JSON.parse(localConfig)
-        if (configItem.root) {
-            config = configItem
+    if (option.enableLocalStorage) {
+        const localConfig = localStorage.getItem(`uni_gl_layout_${option.name}_${option.version}`);
+        if (localConfig) {
+            // 当tab全部关闭时，没有root节点
+            const configItem = JSON.parse(localConfig)
+            if (configItem.root) {
+                config = configItem
+                resetComponentId(config, option.option.content)
+            }
         }
     }
 
-    config = config || {
-        dimensions: {
-            borderWidth: 5,
-            minItemHeight: 10,
-            minItemWidth: 10,
-            headerHeight: 26,
-            dragProxyWidth: 300,
-            dragProxyHeight: 200
-        },
-        content
+    return {
+        ...(config || { content: option.option.content }),
+        ...{
+            dimensions: {
+                borderWidth: 5,
+                minItemHeight: 10,
+                minItemWidth: 10,
+                headerHeight: 26,
+                dragProxyWidth: 300,
+                dragProxyHeight: 200
+            },
+            header: {
+                close: true,
+                maximise: false,
+                minimise: false
+            }
+        }
     }
-    return config
 }
 
-const saveConfig = layout => {
-    localStorage.setItem("uni_gl_layout", JSON.stringify(layout.toConfig()));
+const saveConfig = (option, layout) => {
+    removeConfig(option)
+    localStorage.setItem(`uni_gl_layout_${option.name}_${option.version}`, JSON.stringify(layout.saveLayout()));
+}
+
+const removeConfig = option => {
+    for (let index = localStorage.length; index > 0; index--) {
+        const k = localStorage.key(index - 1);
+        if (k.indexOf(`uni_gl_layout_${option.name}`) > -1) {
+            localStorage.removeItem(k);
+        }
+    }
+}
+
+const resetComponentId = (config, content) => {
+    const items = getAllContentItems(content)
+    const components = getAllContentItems(config.root.content)
+    components.forEach((com, index) => {
+        if (index < items.length) {
+            com.componentState = items[index].componentState
+            com.id = items[index].id
+        }
+    })
 }
