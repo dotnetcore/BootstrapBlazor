b34db3aee21c2ee5562b2ec316a127ff14a16df0
Argo-AscioTech <argo@live.ca>
2023-06-09 15:05:25 +0800

    doc: 增加侧边栏布局

---
+++ src/BootstrapBlazor.Shared/Shared/DockLayout.razor.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.Shared.Shared;
+
+public partial class DockLayout : IAsyncDisposable
+{
+    /// <summary>
+    /// Instance of <see cref="JSModule"/>
+    /// </summary>
+    private JSModule? Module { get; set; }
+
+    /// <summary>
+    /// 获得/设置 IJSRuntime 实例
+    /// </summary>
+    [Inject]
+    [NotNull]
+    private IJSRuntime? JSRuntime { get; set; }
+
+    /// <summary>
+    /// 获得 IVersionService 服务实例
+    /// </summary>
+    [Inject]
+    [NotNull]
+    private IVersionService? JSVersionService { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Module = await JSRuntime.LoadModule("./_content/BootstrapBlazor.Shared/Shared/DockLayout.razor.js", JSVersionService.GetVersion());
+            await Module.InvokeVoidAsync("init");
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="disposing"></param>
+    /// <returns></returns>
+    protected virtual async ValueTask DisposeAsync(bool disposing)
+    {
+        if (disposing)
+        {
+            // 销毁 JSModule
+            if (Module != null)
+            {
+                await Module.InvokeVoidAsync("dispose");
+                await Module.DisposeAsync();
+                Module = null;
+            }
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsync(true);
+        GC.SuppressFinalize(this);
+    }
+}
