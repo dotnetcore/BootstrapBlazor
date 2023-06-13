ce54e684a1b6d5a1e2bb04f5ba2907b5117112b4
Argo Zhang <argo@live.ca>
2023-06-13 17:41:55 +0800

    feat(Topology): support use in Modal (#1379)
    * feat: 修复 Topology 组件由于隐藏导致报错问题
    
    * chore: bump version 7.4.3

--- src/Extensions/Components/BootstrapBlazor.Topology/Components/Topology/Topology.razor.js
+++ src/Extensions/Components/BootstrapBlazor.Topology/Components/Topology/Topology.razor.js
@@ -2,55 +2,24 @@
 import Data from '../../../BootstrapBlazor/modules/data.js'
 
 export function init(id, invoker, data, callback) {
-    Topology.prototype.lock = function (status) {
-        this.store.data.locked = status
-        this.finishDrawLine(!0)
-        this.canvas.drawingLineName = ""
-        this.stopPencil()
-    }
-
     const el = document.getElementById(id)
-    const isSupportTouch = el.getAttribute('data-bb-support-touch') === 'true'
-    const isFitView = el.getAttribute('data-bb-fit-view') === 'true'
-    const isCenterView = el.getAttribute('data-bb-center-view') === 'true'
 
     const initCanvas = () => {
-        const topology = new Topology(el, {}, isSupportTouch)
-        topology.connectSocket = function () {
-        }
-        topology.open(JSON.parse(data))
-        topology.lock(1)
-        if (isFitView) {
-            topology.fitView()
-        }
-        if (isCenterView) {
-            topology.centerView()
-        }
-        new ResizeObserver(() => {
-            resize(id)
-        }).observe(el)
+        const topology = createTopology(el, data)
+        Data.set(id, { topology, el })
+
         invoker.invokeMethodAsync(callback)
-        Data.set(id, { topology })
     }
 
     // make sure el has height
-    if (el.offsetHeight > 0) {
+    if (el.offsetHeight > 0 && el.offsetWidth > 0) {
         initCanvas()
     }
     else {
-        let timers = 0;
         const handler = setInterval(() => {
-            if (el.offsetHeight > 0) {
+            if (el.offsetHeight > 0 && el.offsetWidth > 0) {
                 clearInterval(handler)
                 initCanvas()
-            }
-            else {
-                timers++
-                if (timers > 10) {
-                    clearInterval(handler)
-                    console.log(el)
-                    console.error(`el no height can't init'`)
-                }
             }
         }, 200)
     }
@@ -79,18 +48,25 @@ export function reset(id) {
     }
 }
 
-export function resize(id, width, height) {
-    const meta = Data.get(id)
-    if (meta) {
-        meta.topology.canvas.dirty = true
+export function resize(id, width, height, el) {
+    let topology = id
+    if (typeof (id) === 'string') {
+        const meta = Data.get(id)
+        if (meta) {
+            topology = meta.topology
+            el = meta.el
+        }
+    }
+    if (el.offsetHeight > 0 && el.offsetWidth > 0) {
+        topology.canvas.dirty = true
         if (width && height) {
-            meta.topology.resize(width, height)
+            topology.resize(width, height)
         }
         else {
-            meta.topology.resize()
+            topology.resize()
         }
-        meta.topology.fitView()
-        meta.topology.centerView()
+        topology.fitView()
+        topology.centerView()
     }
 }
 
@@ -102,4 +78,53 @@ export function dispose(id) {
         meta.topology.destroy()
         delete meta.topology
     }
+}
+
+const createTopology = (el, data) => {
+    const isSupportTouch = el.getAttribute('data-bb-support-touch') === 'true'
+    const isFitView = el.getAttribute('data-bb-fit-view') === 'true'
+    const isCenterView = el.getAttribute('data-bb-center-view') === 'true'
+
+    const topology = hackTopology(el, isSupportTouch)
+    topology.open(JSON.parse(data))
+    topology.lock(1)
+
+    if (isFitView) {
+        topology.fitView()
+    }
+    if (isCenterView) {
+        topology.centerView()
+    }
+    el.observer = new ResizeObserver(() => {
+        resize(topology, null, null, el)
+    })
+    el.observer.observe(el)
+    return topology
+}
+
+const hackTopology = (el, isSupportTouch) => {
+    if (Topology.isHack === undefined) {
+        Topology.isHack = true
+        Topology.prototype.lock = function (status) {
+            this.store.data.locked = status
+            this.finishDrawLine(!0)
+            this.canvas.drawingLineName = ""
+            this.stopPencil()
+        }
+        Topology.prototype.connectSocket = function () {
+
+        }
+    }
+
+    const topology = new Topology(el, {}, isSupportTouch)
+
+    const originalCanvasResize = topology.canvas.resize
+    topology.canvas.resize = function () {
+        const width = this.parentElement.clientWidth
+        const height = this.parentElement.clientHeight
+        if (width > 0 && height > 0) {
+            originalCanvasResize.call(this)
+        }
+    }
+    return topology;
 }
