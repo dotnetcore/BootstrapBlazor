4078a243f3984cb6473fc91136b90162da029e6f
Old Li <lish@live.cn>
2023-06-21 17:09:56 +0800

    feat(MouseFollower): add MouseFollower component

---
+++ src/BootstrapBlazor/Components/MouseFollower/MouseFollower.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// MouseFollower
+/// 鼠标跟随器组件
+/// </summary>
+[BootstrapModuleAutoLoader("MouseFollower/MouseFollower.razor.js", JSObjectReference = true, AutoInvokeInit = false)]
+public partial class MouseFollower
+{
+    /// <summary>
+    /// 获得/设置 RenderFragment 实例
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 EChart DOM 元素实例
+    /// </summary>
+    private ElementReference Element { get; set; }
+
+    /// <summary>
+    /// OnAfterRenderAsync 方法
+    /// </summary>
+    /// <param name="firstRender"></param>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await InvokeVoidAsync("init");
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/MouseFollower/MouseFollower.razor.js
@@ -0,0 +1,10 @@
+﻿import { addLink, addScript } from "../../modules/utility.js?v=$version"
+
+export async function init() {
+    await addLink("_content/BootstrapBlazor/lib/mouseFollower/mouse-follower.min.css")
+    await addScript("_content/BootstrapBlazor/lib/mouseFollower/gsap.min.js");
+    await addScript("_content/BootstrapBlazor/lib/mouseFollower/mouse-follower.min.js");
+
+    const cursor = new MouseFollower();
+    cursor.show();
+}
