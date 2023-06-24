1c0800c01e196516bf75b9fc354c1dd4cd800755
Argo Zhang <argo@live.ca>
2023-06-24 15:33:44 +0800

    feat(LazyLoading): add LazyLoading component (#1446)
    * feat: 增加 LazyLoading 组件
    
    * refactor: 更改方法名
    
    * test: 增加单元测试
    
    * chore: bump version 7.7.6-beta02

---
+++ test/UnitTest/Components/LazyLoadingTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class LazyLoadingTest : TestBase
+{
+    [Fact]
+    public void LazyLoading_Ok()
+    {
+        var cut = Context.RenderComponent<LazyLoading>(pb =>
+        {
+            pb.Add(a => a.ChildContent, "<div>content</div>");
+        });
+        Assert.Equal("<div>content</div>", cut.Markup);
+    }
+
+    [Fact]
+    public void OnLoadCallbackAsync_Ok()
+    {
+        var cut = Context.RenderComponent<LazyLoading>(pb =>
+        {
+            pb.Add(a => a.OnLoadCallbackAsync, () => Task.FromResult(false));
+            pb.Add(a => a.ChildContent, "<div>content</div>");
+        });
+        Assert.Equal("", cut.Markup);
+    }
+
+    [Fact]
+    public void OnFirstLoadCallbackAsync_Ok()
+    {
+        var load = false;
+        var cut = Context.RenderComponent<LazyLoading>(pb =>
+        {
+            pb.Add(a => a.OnLoadCallbackAsync, () => Task.FromResult(true));
+            pb.Add(a => a.OnFirstLoadCallbackAsync, () =>
+            {
+                load = true;
+                return Task.CompletedTask;
+            });
+            pb.Add(a => a.ChildContent, "<div>content</div>");
+        });
+        Assert.True(load);
+    }
+}
