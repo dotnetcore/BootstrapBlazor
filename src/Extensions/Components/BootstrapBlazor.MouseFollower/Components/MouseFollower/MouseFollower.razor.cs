30e8293b1d962f87d27a86ff6cdf95eacd8deba0
Argo-AscioTech <argo@live.ca>
2023-06-24 16:27:17 +0800

    refactor: 修复编译错误

--- src/Extensions/Components/BootstrapBlazor.MouseFollower/Components/MouseFollower/MouseFollower.razor.cs
+++ src/Extensions/Components/BootstrapBlazor.MouseFollower/Components/MouseFollower/MouseFollower.razor.cs
@@ -1,12 +1,15 @@
-﻿using BootstrapBlazor.Enums;
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
 
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// MouseFollower
-/// 鼠标跟随器组件
+/// MouseFollower 鼠标跟随器组件
 /// </summary>
-[BootstrapModuleAutoLoader("MouseFollower/MouseFollower.razor.js", JSObjectReference = true)]
+[JSModuleAutoLoader("./_content/BootstrapBlazor.MouseFollower/Components/MouseFollower/MouseFollower.razor.js", JSObjectReference = true)]
 public partial class MouseFollower
 {
     /// <summary>
@@ -24,7 +27,7 @@ public partial class MouseFollower
     /// 获得/设置 GlobalMode 全局模式
     /// </summary>
     [Parameter]
-    public bool GlobalMode { get; set; } = false;
+    public bool GlobalMode { get; set; }
 
     /// <summary>
     /// 获得/设置 Content 文本，图标，图片路径，视频路径
@@ -39,13 +42,20 @@ public partial class MouseFollower
     [NotNull]
     public RenderFragment? ChildContent { get; set; }
 
+    private string? ClassString => CssBuilder.Default("bb-mf")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
     /// <summary>
-    /// MouseFollowerOptions
+    /// 获得/设置 MouseFollowerOptions
     /// </summary>
     [Parameter]
     public MouseFollowerOptions? Options { get; set; }
 
+    /// <summary>
     /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
     protected override async Task InvokeInitAsync()
     {
         Options ??= new MouseFollowerOptions();
