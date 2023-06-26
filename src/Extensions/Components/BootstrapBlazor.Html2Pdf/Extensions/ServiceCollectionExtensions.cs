32c4569dde4cce953a67b70b76c3bb31ba3aa5af
Argo Zhang <argo@live.ca>
2023-06-26 16:08:09 +0800

    feat(PDF): add Html2Pdf service (#1454)
    * feat: 增加 Pdf 服务
    
    * feat: 增加 Pdf 导出工程
    
    * chore: 精简项目配置文件
    
    * chore: 更新解决方案
    
    * chore: 更新依赖

---
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// BootstrapBlazor 服务扩展类
+/// </summary>
+public static class BootstrapBlazorHtml2PdfServiceExtensions
+{
+    /// <summary>
+    /// 添加 AzureOpenAIService 服务
+    /// </summary>
+    /// <param name="services"></param>
+    public static IServiceCollection AddBootstrapBlazorAzureOpenAIService(this IServiceCollection services)
+    {
+        services.TryAddTransient<IHtml2Pdf, DefaultPdfService>();
+
+        return services;
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/Services/IHtml2Pdf.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Html export PDF service
+/// </summary>
+public interface IHtml2Pdf
+{
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">snippet html</param>
+    /// <param name="fileName">the file name of pdf</param>
+    /// <returns></returns>
+    Task<bool> ExportAsync(string html, string? fileName = null);
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="snippets">the collection of snippet html</param>
+    /// <param name="fileName">the file name of pdf</param>
+    /// <returns></returns>
+    Task<bool> ExportAsync(List<string> snippets, string? fileName = null);
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="parameters">blazor component</param>
+    /// <param name="fileName">the file name of pdf</param>
+    /// <returns></returns>
+    Task<bool> ExportAsync<TComponent>(IDictionary<string, object?>? parameters = null, string? fileName = null) where TComponent : IComponent;
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="componentType"></param>
+    /// <param name="parameters">blazor component</param>
+    /// <param name="fileName">the file name of pdf</param>
+    /// <returns></returns>
+    Task<bool> ExportAsync(Type componentType, IDictionary<string, object?>? parameters = null, string? fileName = null);
+}
