8664c229ca5d6bf4715d175dcf9b9c758ce2de43
Argo-AscioTech <argo@live.ca>
2023-06-29 16:35:38 +0800

    chore: 增加导出 pdf 脚本

---
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/wwwroot/export.js
@@ -0,0 +1,27 @@
+﻿import './html2pdf.bundle.min.js'
+
+export function exportPdf(html, fileName) {
+    const element = document.createElement("div")
+    element.innerHTML = html
+    html2pdf().from(element).save(fileName)
+    return true
+}
+
+export function exportPdfById(id, fileName) {
+    let ret = false
+    const element = document.getElementById(id)
+    if (element) {
+        html2pdf().from(element).save(fileName)
+        ret = true
+    }
+    return ret
+}
+
+export function exportPdfByElement(el, fileName) {
+    let ret = false
+    if (el) {
+        html2pdf().from(el).save(fileName)
+        ret = true
+    }
+    return ret
+}
