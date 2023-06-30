51b87afd2360648a0714e7b70c83753f266966e3
Argo Zhang <argo@live.ca>
2023-06-30 18:04:17 +0800

    doc(IHtml2Pdf): add IHtml2Pdf document (#1476)
    * doc: 更新注释文档
    
    * refactor: 重构 ITableExcelExport 接口
    
    * refactor: 增加 ITablePdfExport 接口
    
    * refactor: 增加 ITablePdfExport 服务
    
    * chore: 增加 Pdf 导出按钮图标
    
    * feat: 增加导出 Pdf 默认实现
    
    * chore: 网站增加 Pdf 导出服务
    
    * refactor: 增加 Pdf 导出菜单
    
    * doc: 增加新参数文档
    
    * doc: add Html2Pdf sample
    
    * chore: 消除错误信息
    
    * refactor: 重构 JSModule 增加无版本号脚本支持
    
    * feat: 增加 ITablePdfExport 默认实现
    
    * feat: 增加 Pdf 导出菜单
    
    * feat: 增加 Html2Pdf 接口实现
    
    * chore: 增加导出 pdf 脚本
    
    * refactor: 更新接口移除集合方法
    
    * doc: 增加演示示例
    
    * chore: 提交项目配置
    
    * chore: 处理 \x3C 字符
    
    * <!--!--> 正则替换为 空
    
    * refactor: 更新示例
    
    * refactor: 更新 ITablePdfExport 接口定义
    
    * chore: 精简 IHtml2Pdf 实现
    
    * doc: 更新示例
    
    * refactor: 移除项目依赖
    
    * chore: 更新项目依赖
    
    * chore: 更新项目配置
    
    * chore: 更新组件包指定版本号
    
    ---------
    
    Co-authored-by: Alex chow <zhouchuanglin@gmail.com>

---
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/wwwroot/export.js
@@ -0,0 +1,20 @@
+﻿import './html2pdf.bundle.min.js'
+
+export function exportPdf(html, fileName) {
+    const opt = {
+        ...{
+            image: { type: 'jpeg', quality: 0.95 },
+            pagebreak: { mode: ['avoid-all', 'css', 'legacy'] },
+            html2canvas: {
+                scale: 2,
+                useCORS: true,
+            },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        },
+        ...{ filename: fileName }
+    };
+    const element = document.createElement("div")
+    element.innerHTML = html
+    html2pdf(element, opt)
+    return true
+}
