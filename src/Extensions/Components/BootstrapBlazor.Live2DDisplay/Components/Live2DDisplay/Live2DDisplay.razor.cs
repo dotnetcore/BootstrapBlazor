df928f3ef3d9748487377be3dd762095582fa4c0
Old Li <lish@live.cn>
2023-07-03 14:21:54 +0800

    feat(Live2DDisplay): 重构项目

---
+++ src/Extensions/Components/BootstrapBlazor.Live2DDisplay/Components/Live2DDisplay/Live2DDisplay.razor.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+using System.Reflection.Metadata;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Live2DDisplay Live2D组件
+/// </summary>
+[JSModuleAutoLoader("./_content/BootstrapBlazor.L2Dwidget/Components/L2Dwidget/L2Dwidget.razor.js")]
+public partial class Live2DDisplay
+{
+    /// <summary>
+    /// 获得/设置 Source 模型源
+    /// </summary>
+    [Parameter]
+    public string? Source { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Scale 比例
+    /// </summary>
+    [Parameter]
+    public double Scale { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public int X { get; set; }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public int Y { get; set; }
+
+
+    private string? ClassString => CssBuilder.Default().AddClassFromAttributes(AdditionalAttributes).Build();
+
+    /// <inheritdoc/>
+    protected override async Task OnParametersSetAsync()
+    {
+        if (!string.IsNullOrEmpty(Source))
+        {
+            await InvokeVoidAsync("reload", Id, Source, Scale, X, Y);
+        }
+    }
+
+    /// <inheritdoc/>
+    protected override async Task InvokeInitAsync()
+    {
+        await InvokeVoidAsync("init", Id, Source, Scale, X, Y);
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.Live2DDisplay/Components/Live2DDisplay/Live2DDisplay.razor.js
@@ -0,0 +1,94 @@
+﻿import { addScript } from '../../../BootstrapBlazor/modules/utility.js'
+import Data from '../../../BootstrapBlazor/modules/data.js'
+
+export async function init(id, src, scale, x, y) {
+    await addScript("_content/BootstrapBlazor.Live2DDisplay/live2dcubismcore.min.js")
+    await addScript("_content/BootstrapBlazor.Live2DDisplay/live2d.min.js")
+    await addScript("_content/BootstrapBlazor.Live2DDisplay/pixi.min.js")
+    await addScript("_content/BootstrapBlazor.Live2DDisplay/index.min.js")
+    await addScript("_content/BootstrapBlazor.Live2DDisplay/extra.min.js")
+
+    const model = await createModel(src, scale, x, y);
+
+    var h = Math.ceil(model.height) + 'px';
+    var w = Math.ceil(model.width) + 'px';
+
+    const el = document.getElementById(id);
+    el.style.width = w;
+    el.style.height = h;
+    el.style.zIndex = '99999';
+    el.style.opacity = '1';
+    el.style.pointerEvents = 'none';
+
+    const canvas = document.createElement('canvas');
+    canvas.style.width = w;
+    canvas.style.height = h;
+
+    el.appendChild(canvas);
+
+    const app = new PIXI.Application({
+        view: canvas,
+        autoStart: true,
+        resizeTo: el,
+        backgroundAlpha: 0
+    });
+    app.stage.addChild(model);
+
+    addHitAreaFrames(model);
+
+    Data.set(id, { app, model, src, scale, el, canvas });
+}
+
+export async function reload(id, src, scale, x, y) {
+    const op = Data.get(id);
+    if (op.src != src) {
+        op.src = src;
+        op.app.stage.removeChild(op.model);
+        op.model = await createModel(op.src, scale, x, y);
+        op.app.stage.addChild(op.model);
+    }
+    if (op.scale != scale) {
+        op.scale = scale;
+        op.model.scale.set(scale);
+    }
+
+    var h = Math.ceil(op.model.height);
+    var w = Math.ceil(op.model.width);
+
+    op.el.style.width = w + 'px';
+    op.el.style.height = h + 'px';
+
+    op.canvas.style.width = w + 'px';
+    op.canvas.style.height = h + 'px';
+
+    op.canvas.width = w;
+    op.canvas.height = h;
+    op.app.resizeTo = op.el;
+}
+
+async function createModel(src, scale, x, y) {
+    const model = await PIXI.live2d.Live2DModel.from(src);
+    model.scale.set(scale);
+    model.x = x;
+    model.y = y;
+
+    // handle tapping
+    model.on("hit", (hitAreas) => {
+        if (hitAreas.includes("Body")) {
+            model.motion("Tap");
+            model2.motion("tap_body");
+        }
+
+        if (hitAreas.includes("Head")) {
+            model.expression();
+        }
+    });
+
+    return model;
+}
+
+function addHitAreaFrames(model) {
+    const hitAreaFrames = new PIXI.live2d.HitAreaFrames();
+
+    model.addChild(hitAreaFrames);
+}
