d610a608f0fb08b02f9e9cdc06b67b3e8852b068
Old Li <lish@live.cn>
2023-07-07 11:58:34 +0800

    feat(Live2DDisplay): update demo

--- src/Extensions/Components/BootstrapBlazor.Live2DDisplay/Components/Live2DDisplay/Live2DDisplay.razor.cs
+++ src/Extensions/Components/BootstrapBlazor.Live2DDisplay/Components/Live2DDisplay/Live2DDisplay.razor.cs
@@ -2,14 +2,17 @@
 
 namespace BootstrapBlazor.Components;
 
+/// <summary>
+/// Live2DDisplay Live2D模型组件
+/// </summary>
 public partial class Live2DDisplay
 {
     private bool _isInit;
 
     private string? _source;
     private double _scale;
-    private int _x;
-    private int _y;
+    private int _xoffset;
+    private int _yoffset;
     private bool _isDraggble;
     private bool _addHitAreaFrames;
     private string? _backgroundColor;
@@ -17,18 +20,63 @@ public partial class Live2DDisplay
 
     private LivePosition _position;
 
-    [Parameter] public string? Source { get; set; }
-    [Parameter] public double Scale { get; set; }
-    [Parameter] public int X { get; set; }
-    [Parameter] public int Y { get; set; }
-    [Parameter] public bool IsDraggble { get; set; }
-    [Parameter] public bool AddHitAreaFrames { get; set; }
-    [Parameter] public LivePosition Position { get; set; }
-    [Parameter] public string? BackgroundColor { get; set; }
-    [Parameter] public bool BackgroundAlpha { get; set; }
-
     private string? ClassString { get; set; }
 
+    /// <summary>
+    /// 获得/设置 ModelSource 模型源
+    /// </summary>
+    [Parameter]
+    public string? Source { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Scale 模型比例
+    /// </summary>
+    [Parameter]
+    public double Scale { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Xoffset 模型X轴偏移
+    /// </summary>
+    [Parameter]
+    public int Xoffset { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Yoffset 模型Y轴偏移
+    /// </summary>
+    [Parameter]
+    public int Yoffset { get; set; }
+
+    /// <summary>
+    /// 获得/设置 IsDraggble 是否可以拖动模型
+    /// </summary>
+    [Parameter]
+    public bool IsDraggble { get; set; }
+
+    /// <summary>
+    /// 获得/设置 HitAreaFrames 是否渲染鼠标命中区域框
+    /// </summary>
+    [Parameter]
+    public bool AddHitAreaFrames { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Position 模型默认显示位置
+    /// </summary>
+    [Parameter]
+    public LivePosition Position { get; set; }
+
+    /// <summary>
+    /// 获得/设置 BackgroundColor 模型画板背景颜色
+    /// </summary>
+    [Parameter]
+    public string? BackgroundColor { get; set; }
+
+    /// <summary>
+    /// 获得/设置 BackgroundAlpha 模型画板是否透明
+    /// </summary>
+    [Parameter]
+    public bool BackgroundAlpha { get; set; }
+
+    /// <inheritdoc/>
     protected override void OnInitialized()
     {
         base.OnInitialized();
@@ -37,13 +85,14 @@ public partial class Live2DDisplay
             .Build();
     }
 
+    /// <inheritdoc/>
     protected override async Task OnParametersSetAsync()
     {
         if (_isInit)
         {
             if (Source != _source)
             {
-                await InvokeVoidAsync("changeSource", new { Id, Source, Scale, X, Y, IsDraggble, AddHitAreaFrames });
+                await InvokeVoidAsync("changeSource", new { Id, Source, Scale, X = Xoffset, Y = Yoffset, IsDraggble, AddHitAreaFrames });
                 _source = Source;
             }
             if (Scale != _scale)
@@ -51,11 +100,11 @@ public partial class Live2DDisplay
                 await InvokeVoidAsync("changeScale", Id, Scale);
                 _scale = Scale;
             }
-            if (X != _x || Y != _y)
+            if (Xoffset != _xoffset || Yoffset != _yoffset)
             {
-                await InvokeVoidAsync("changeXY", Id, X, Y);
-                _x = X;
-                _y = Y;
+                await InvokeVoidAsync("changeXY", Id, Xoffset, Yoffset);
+                _xoffset = Xoffset;
+                _yoffset = Yoffset;
             }
             if (!(IsDraggble && _isDraggble))
             {
@@ -81,13 +130,19 @@ public partial class Live2DDisplay
         }
     }
 
-    /// <summary>
     /// <inheritdoc/>
-    /// </summary>
-    /// <returns></returns>
     protected override async Task InvokeInitAsync()
     {
-        await InvokeVoidAsync("init", new { Id, Source, Scale, X, Y, IsDraggble, AddHitAreaFrames, BackgroundAlpha, BackgroundColor });
+        await InvokeVoidAsync("init", new { Id, Source, Scale, X = Xoffset, Y = Yoffset, IsDraggble, AddHitAreaFrames, BackgroundAlpha, BackgroundColor });
         _isInit = true;
+        _source = Source;
+        _scale = Scale;
+        _xoffset = Xoffset;
+        _yoffset = Yoffset;
+        _position = Position;
+        _backgroundColor = BackgroundColor;
+        _backgroundAlpha = BackgroundAlpha;
+        _addHitAreaFrames = AddHitAreaFrames;
+        _isDraggble = IsDraggble;
     }
 }
