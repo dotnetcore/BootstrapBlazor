7f2a29bea0662de413784d7a9a9c1bcd0c2282cf
Alex chow <zhouchuanglin@gmail.com>
2023-07-10 15:53:20 +0800

    feat: Camera capture with JPEG format
    Set the <code>CaptureJpeg</code> property to control the photo format as Jpeg, default is Png. The <code>Quality</code> property controls the image quality, default is 0.9

--- src/BootstrapBlazor/Components/Camera/Camera.razor.js
+++ src/BootstrapBlazor/Components/Camera/Camera.razor.js
@@ -9,7 +9,7 @@ const stop = (video, track) => {
     }
 }
 
-export function init(id, invoke, auto, videoWidth, videoHeight) {
+export function init(id, invoke, auto, videoWidth, videoHeight, captureJpeg, quality) {
     const el = document.getElementById(id)
     if (el === null) {
         return
@@ -42,7 +42,7 @@ export function init(id, invoke, auto, videoWidth, videoHeight) {
             const data_method = button.getAttribute('data-method')
             if (data_method === 'play') {
                 const deviceId = el.getAttribute('data-device-id')
-                const constrains = { video: { facingMode: 'environment', width: videoWidth, height: videoHeight }, audio: false }
+                const constrains = { video: { facingMode: 'environment', focusMode: "continuous", width: videoWidth, height: videoHeight }, audio: false }
                 if (deviceId) {
                     constrains.video.deviceId = { exact: deviceId }
                 }
@@ -61,13 +61,20 @@ export function init(id, invoke, auto, videoWidth, videoHeight) {
             }
             else if (data_method === 'capture') {
                 context.drawImage(camera.video, 0, 0, videoWidth, videoHeight)
-                let url = canvas.toDataURL()
-                const maxLength = 30 * 1024
-                while (url.length > maxLength) {
-                    const data = url.substring(0, maxLength)
-                    await invoke.invokeMethodAsync("Capture", data)
-                    url = url.substring(data.length)
-                }
+                let url = "";
+                if (captureJpeg)
+                {
+                    url = canvas.toDataURL("image/jpeg", quality);
+
+                } else {
+                    url = canvas.toDataURL()
+                    const maxLength = 30 * 1024
+                    while (url.length > maxLength) {
+                        const data = url.substring(0, maxLength)
+                        await invoke.invokeMethodAsync("Capture", data)
+                        url = url.substring(data.length)
+                    } 
+                } 
 
                 if (url.length > 0) {
                     await invoke.invokeMethodAsync("Capture", url)
