01ea16a436ab6c23f0a719b05090105e9846a8ce
Argo-AscioTech <argo@live.ca>
2023-07-12 21:14:04 +0800

    revert: 撤销对 Zone 的更改

--- src/BootstrapBlazor/Components/ContextMenu/ContextMenuZone.razor.cs
+++ src/BootstrapBlazor/Components/ContextMenu/ContextMenuZone.razor.cs
@@ -9,7 +9,7 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// ContextMenuZone 组件
 /// </summary>
-public partial class ContextMenuZone : IDisposable
+public partial class ContextMenuZone
 {
     /// <summary>
     /// 获得/设置 子组件
@@ -25,18 +25,6 @@ public partial class ContextMenuZone : IDisposable
     private string? ClassString => CssBuilder.Default("bb-cm-zone")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
-
-    /// <summary>
-    /// 是否触摸
-    /// </summary>
-    private bool TouchStart { get; set; }
-
-    /// <summary>
-    /// 触摸定时器工作指示
-    /// </summary>
-    private bool IsBusy { get; set; }
-
-    private CancellationTokenSource CancellationSource { get; } = new();
 
     /// <summary>
     /// Trigger 调用
@@ -58,69 +46,4 @@ public partial class ContextMenuZone : IDisposable
     /// </summary>
     /// <param name="contextMenu"></param>
     internal void RegisterContextMenu(ContextMenu contextMenu) => ContextMenu = contextMenu;
-
-    /// <summary>
-    /// 触摸事件
-    /// </summary>
-    /// <param name="e"></param>
-    private async Task OnTouchStart(TouchEventArgs e)
-    {
-        if (!IsBusy)
-        {
-            IsBusy = true;
-            TouchStart = true;
-            var args = new MouseEventArgs()
-            {
-                ClientX = e.Touches[0].ClientX,
-                ClientY = e.Touches[0].ClientY,
-                ScreenX = e.Touches[0].ScreenX,
-                ScreenY = e.Touches[0].ScreenY,
-            };
-
-            try
-            {
-                await Task.Delay(200, CancellationSource.Token);
-                if (TouchStart)
-                {
-                    // 弹出关联菜单
-                    if (ContextMenu != null)
-                    {
-                        await ContextMenu.Show(args, null);
-                    }
-                    //延时防止重复激活菜单功能
-                    await Task.Delay(200, CancellationSource.Token);
-                }
-                IsBusy = false;
-            }
-            catch (TaskCanceledException) { }
-        }
-    }
-
-    private void OnTouchEnd()
-    {
-        TouchStart = false;
-    }
-
-    /// <summary>
-    /// 释放资源
-    /// </summary>
-    /// <param name="disposing"></param>
-    protected virtual void Dispose(bool disposing)
-    {
-        if (disposing)
-        {
-            CancellationSource.Cancel();
-            CancellationSource.Dispose();
-        }
-    }
-
-    /// <summary>
-    /// 释放资源
-    /// </summary>
-    /// <returns></returns>
-    public void Dispose()
-    {
-        Dispose(true);
-        GC.SuppressFinalize(this);
-    }
 }
