5685dfc73907851e848cc88c173aeb491c8c4981
Argo-AscioTech <argo@live.ca>
2023-07-12 21:26:09 +0800

    refactor: Trigger 支持移动端

--- src/BootstrapBlazor/Components/ContextMenu/ContextMenuTrigger.cs
+++ src/BootstrapBlazor/Components/ContextMenu/ContextMenuTrigger.cs
@@ -10,7 +10,7 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// ContextMenuTrigger 组件
 /// </summary>
-public class ContextMenuTrigger : BootstrapComponentBase
+public class ContextMenuTrigger : BootstrapComponentBase, IDisposable
 {
     /// <summary>
     /// 获得/设置 子组件
@@ -45,11 +45,13 @@ public class ContextMenuTrigger : BootstrapComponentBase
     protected override void BuildRenderTree(RenderTreeBuilder builder)
     {
         builder.OpenElement(0, WrapperTag);
-        builder.AddMultipleAttributes(1, AdditionalAttributes);
-        builder.AddAttribute(2, "class", ClassString);
-        builder.AddAttribute(3, "oncontextmenu", EventCallback.Factory.Create<MouseEventArgs>(this, OnContextMenu));
-        builder.AddEventPreventDefaultAttribute(4, "oncontextmenu", true);
-        builder.AddContent(5, ChildContent);
+        builder.AddMultipleAttributes(10, AdditionalAttributes);
+        builder.AddAttribute(20, "class", ClassString);
+        builder.AddAttribute(30, "oncontextmenu", EventCallback.Factory.Create<MouseEventArgs>(this, OnContextMenu));
+        builder.AddAttribute(35, "ontouchstart", EventCallback.Factory.Create<TouchEventArgs>(this, OnTouchStart));
+        builder.AddAttribute(36, "ontouchend", EventCallback.Factory.Create<TouchEventArgs>(this, OnTouchEnd));
+        builder.AddEventPreventDefaultAttribute(40, "oncontextmenu", true);
+        builder.AddContent(50, ChildContent);
         builder.CloseElement();
     }
 
@@ -58,4 +60,76 @@ public class ContextMenuTrigger : BootstrapComponentBase
     /// </summary>
     /// <returns></returns>
     public Task OnContextMenu(MouseEventArgs args) => ContextMenuZone.OnContextMenu(args, ContextItem);
+
+    /// <summary>
+    /// 是否触摸
+    /// </summary>
+    private bool TouchStart { get; set; }
+
+    /// <summary>
+    /// 触摸定时器工作指示
+    /// </summary>
+    private bool IsBusy { get; set; }
+
+    private CancellationTokenSource CancellationSource { get; } = new();
+
+    private async Task OnTouchStart(TouchEventArgs e)
+    {
+        if (!IsBusy)
+        {
+            IsBusy = true;
+            TouchStart = true;
+
+            try
+            {
+                // 延时保持 TouchStart 状态
+                await Task.Delay(200, CancellationSource.Token);
+                if (TouchStart)
+                {
+                    var args = new MouseEventArgs()
+                    {
+                        ClientX = e.Touches[0].ClientX,
+                        ClientY = e.Touches[0].ClientY,
+                        ScreenX = e.Touches[0].ScreenX,
+                        ScreenY = e.Touches[0].ScreenY,
+                    };
+                    // 弹出关联菜单
+                    await OnContextMenu(args);
+
+                    //延时防止重复激活菜单功能
+                    await Task.Delay(200, CancellationSource.Token);
+                }
+                IsBusy = false;
+            }
+            catch (TaskCanceledException) { }
+        }
+    }
+
+    private void OnTouchEnd()
+    {
+        TouchStart = false;
+    }
+
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            CancellationSource.Cancel();
+            CancellationSource.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// 释放资源
+    /// </summary>
+    /// <returns></returns>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
 }
