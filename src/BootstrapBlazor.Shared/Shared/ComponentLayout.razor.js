985a451c858efebd9cccdc3db83fb5af3fa331c8
Argo-AscioTech <argo@live.ca>
2023-07-12 15:01:55 +0800

    doc: 增加自动滚动功能

---
+++ src/BootstrapBlazor.Shared/Shared/ComponentLayout.razor.js
@@ -0,0 +1,11 @@
+﻿export function init() {
+    const hash = location.hash
+    if (hash) {
+        const el = document.querySelector(hash)
+        if (el) {
+            const rect = el.getBoundingClientRect()
+            const top = rect.top - 50
+            scrollTo(0, top)
+        }
+    }
+}
