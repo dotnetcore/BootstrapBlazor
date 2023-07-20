fea1378e27527b6ffd700bb44271068402529e76
Argo-AscioTech <argo@live.ca>
2023-07-20 20:23:16 +0800

    test: 更新单元测试

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -4825,20 +4825,24 @@ public class TableTest : TableTestBase
         {
             pb.AddChildContent<Table<Foo>>(pb =>
             {
-                pb.Add(a => a.RenderMode, TableRenderMode.Auto);
+                pb.Add(a => a.RenderMode, TableRenderMode.Table);
                 pb.Add(a => a.Items, items);
                 pb.Add(a => a.TableColumns, foo => builder =>
                 {
                     builder.OpenComponent<TableColumn<Foo, string>>(0);
                     builder.AddAttribute(1, "Field", "Name");
                     builder.AddAttribute(2, "FieldExpression", Utility.GenerateValueExpression(foo, "Name", typeof(string)));
+                    builder.AddAttribute(3, "ShownWithBreakPoint", BreakPoint.Large);
                     builder.CloseComponent();
                 });
             });
         });
 
-        var resp = cut.FindComponent<ResizeNotification>().Instance;
-        cut.InvokeAsync(() => resp.OnResize(BreakPoint.Large));
+        cut.InvokeAsync(() =>
+        {
+            var resp = cut.FindComponent<ResizeNotification>().Instance;
+            resp.OnResize(BreakPoint.Large);
+        });
     }
 
     [Fact]
