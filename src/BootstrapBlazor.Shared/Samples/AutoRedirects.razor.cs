29173e22117c4bd4cf4245dd05233e13877ffd87
Old Li <lish@live.cn>
2023-07-22 16:19:42 +0800

    合并code代码

--- src/BootstrapBlazor.Shared/Samples/AutoRedirects.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/AutoRedirects.razor.cs
@@ -9,6 +9,15 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class AutoRedirects
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private Task<bool> OnBeforeRedirectAsync()
+    {
+        Logger.Log("Ready to redirect");
+        return Task.FromResult(true);
+    }
+
     /// <summary>
     /// Get property method
     /// </summary>
