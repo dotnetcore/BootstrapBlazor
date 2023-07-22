75004f394a34949778fd6d14aa8b7a976d2cdd9c
Old Li <lish@live.cn>
2023-07-22 16:27:48 +0800

    merge barcodereader demos

--- src/BootstrapBlazor.Shared/Samples/BarcodeReaders.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/BarcodeReaders.razor.cs
@@ -9,6 +9,54 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class BarcodeReaders
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private Task OnInit(IEnumerable<DeviceItem> devices)
+    {
+        var cams = string.Join("", devices.Select(i => i.Label));
+        Logger.Log($"{Localizer["InitLog"]} {cams}");
+        return Task.CompletedTask;
+    }
+    private Task OnResult(string barcode)
+    {
+        Logger.Log($"{Localizer["ScanCodeLog"]} {barcode}");
+        return Task.CompletedTask;
+    }
+
+    private Task OnError(string error)
+    {
+        Logger.Log($"{Localizer["ErrorLog"]} {error}");
+        return Task.CompletedTask;
+    }
+
+    private Task OnStart()
+    {
+        Logger.Log(Localizer["OpenCameraLog"]);
+        return Task.CompletedTask;
+    }
+
+    private Task OnClose()
+    {
+        Logger.Log(Localizer["CloseCameraLog"]);
+        return Task.CompletedTask;
+    }
+
+    [NotNull]
+    private ConsoleLogger? ImageLogger { get; set; }
+
+    private Task OnImageResult(string barcode)
+    {
+        ImageLogger.Log($"{Localizer["ScanCodeLog"]} {barcode}");
+        return Task.CompletedTask;
+    }
+
+    private Task OnImageError(string err)
+    {
+        ImageLogger.Log($"{Localizer["ErrorLog"]} {err}");
+        return Task.CompletedTask;
+    }
+
     /// <summary>
     /// 获得属性
     /// </summary>
