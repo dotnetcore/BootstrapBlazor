f42db4c77a2a79783b0b14b7110b85ccf779e997
Old Li <lish@live.cn>
2023-07-22 15:57:52 +0800

    拆分code代码

---
+++ src/BootstrapBlazor.Shared/Samples/Anchors.razor.cs
@@ -0,0 +1,48 @@
+﻿namespace BootstrapBlazor.Shared.Samples;
+
+public partial class Anchors
+{
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Anchors>? Localizer { get; set; }
+
+    /// <summary>
+    /// 获得属性方法
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<AttributeItem> GetAttributes() => new[]
+    {
+        new AttributeItem()
+        {
+            Name = "Target",
+            Description = Localizer["Desc1"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem()
+        {
+            Name = "Container",
+            Description = Localizer["Desc2"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem()
+        {
+            Name = "Offset",
+            Description = Localizer["Desc3"],
+            Type = "int",
+            ValueList = " — ",
+            DefaultValue = "0"
+        },
+        new AttributeItem()
+        {
+            Name = "ChildContent",
+            Description = Localizer["Desc4"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
+    };
+}
