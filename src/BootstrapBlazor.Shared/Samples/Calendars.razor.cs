7a61466425ef36433da6311d8f265f97e4301892
Old Li <lish@live.cn>
2023-07-22 17:13:07 +0800

    merge calendar demos

--- src/BootstrapBlazor.Shared/Samples/Calendars.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Calendars.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using System.Collections.Concurrent;
+
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
@@ -9,6 +11,26 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Calendars
 {
+    [NotNull]
+    private ConsoleLogger? NormalLogger { get; set; }
+
+    private void OnValueChanged(DateTime ts)
+    {
+        NormalLogger.Log($"{ts:yyyy-MM-dd}");
+    }
+
+    private DateTime BindValue { get; set; } = DateTime.Today;
+
+    private static string Formatter(DateTime ts) => ts.ToString("yyyy-MM-dd");
+
+    private ConcurrentDictionary<DateTime, List<Crew>> Data { get; } = new();
+
+    private DateTime CrewInfoValue { get; set; } = DateTime.Today;
+
+    private List<Crew> GetCrewsByDate(DateTime d) => Data.GetOrAdd(d, CalendarDemoDataHelper.GetCrewsByDate);
+
+    private int GetSumByName(string name) => Data.Where(d => d.Key.Month == CrewInfoValue.Month).Sum(d => d.Value.FirstOrDefault(v => v.Name == name)?.Value ?? 0);
+
     /// <summary>
     /// 获得事件方法
     /// </summary>
