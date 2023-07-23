cf4e6aa802b5edad15a32754030e0dd12fcddbf0
Old Li <lish@live.cn>
2023-07-23 13:03:00 +0800

    merge cascader demos

--- src/BootstrapBlazor.Shared/Samples/Cascaders.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Cascaders.razor.cs
@@ -10,6 +10,67 @@ namespace BootstrapBlazor.Shared.Samples;
 public sealed partial class Cascaders
 {
     /// <summary>
+    /// Foo 类为Demo测试用，如有需要请自行下载源码查阅
+    /// Foo class is used for Demo test, please download the source code if necessary
+    /// https://gitee.com/LongbowEnterprise/BootstrapBlazor/blob/main/src/BootstrapBlazor.Shared/Data/Foo.cs
+    /// </summary>
+    private Foo Model { get; set; } = new Foo();
+
+    private Guid CurrentGuid { get; set; }
+
+    private readonly IEnumerable<CascaderItem> _guidItems = new CascaderItem[]
+    {
+        new CascaderItem(Guid.NewGuid().ToString(), "Guid1"),
+        new CascaderItem(Guid.NewGuid().ToString(), "Guid2")
+    };
+
+    private string Value { get; set; } = "Shanghai";
+
+    [NotNull]
+    private ConsoleLogger? NormalLogger { get; set; }
+
+    private List<CascaderItem> _items = new List<CascaderItem>();
+
+    /// <summary>
+    /// 下拉选项改变时调用此方法
+    /// </summary>
+    /// <param name="items"></param>
+    private Task OnItemChanged(CascaderItem[] items)
+    {
+        NormalLogger.Log($"SelectedItem Text: {items[^1].Text} Value: {items[^1].Value} Selected");
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        Value = Localizer["item1"];
+
+        _items = new List<CascaderItem>
+        {
+            new CascaderItem(Localizer["item1"], Localizer["item1"]),
+            new CascaderItem(Localizer["item2"], Localizer["item2"]),
+            new CascaderItem(Localizer["item3"], Localizer["item3"]),
+        };
+
+        _items[0].AddItem(new CascaderItem("item1_child1", Localizer["item1_child1"]));
+        _items[0].AddItem(new CascaderItem("item1_child2", Localizer["item1_child2"]));
+        _items[0].AddItem(new CascaderItem("item1_child3", Localizer["item1_child3"]));
+        _items[0].AddItem(new CascaderItem("item1_child4", Localizer["item1_child4"]));
+
+        _items[0].Items.ElementAt(0).AddItem(new CascaderItem("item1_child1_child", Localizer["item1_child1_child"]));
+
+        _items[1].AddItem(new CascaderItem("item2_child1", Localizer["item2_child1"]));
+        _items[1].AddItem(new CascaderItem("item2_child2", Localizer["item2_child2"]));
+        _items[1].AddItem(new CascaderItem("item2_child3", Localizer["item2_child3"]));
+
+        _items[2].AddItem(new CascaderItem("item3_child1", Localizer["item3_child1"]));
+        _items[2].AddItem(new CascaderItem("item3_child2", Localizer["item3_child2"]));
+    }
+
+    /// <summary>
     /// 获得事件方法
     /// </summary>
     /// <returns></returns>
