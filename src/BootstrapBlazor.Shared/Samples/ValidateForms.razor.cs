d4b88d119809b257c430ccc61be5cdc9bf5c1d4a
Argo-AscioTech <argo@live.ca>
2023-07-24 20:19:49 +0800

    refactor: 更新示例

--- src/BootstrapBlazor.Shared/Samples/ValidateForms.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/ValidateForms.razor.cs
@@ -2,6 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using DocumentFormat.OpenXml.EMMA;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
@@ -9,6 +14,210 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class ValidateForms
 {
+    [NotNull]
+    private Foo? Model1 { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? Logger1 { get; set; }
+
+    private async Task OnInvalidSubmit1(EditContext context)
+    {
+        await Task.Delay(1000);
+        Logger1.Log(Localizer["OnInvalidSubmitLog"]);
+    }
+
+    private async Task OnValidSubmit1(EditContext context)
+    {
+        await Task.Delay(1000);
+        Logger1.Log(Localizer["OnValidSubmitLog"]);
+    }
+
+    private void OnFiledChanged(string field, object? value)
+    {
+        Logger1.Log($"{field}:{value}");
+    }
+
+    /// <summary>
+    /// OnInitializedAsync 方法
+    /// </summary>
+    /// <returns></returns>
+    protected override async Task OnInitializedAsync()
+    {
+        CustomerRules.Add(new FormItemValidator(new EmailAddressAttribute()));
+
+        // 切换线程 模拟异步通过 webapi 加载数据
+        await Task.Yield();
+
+        Model1 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model2 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model3 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model4 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model7 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model8 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model9 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+        Model10 = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+
+        // 初始化参数
+        Hobbies2 = Foo.GenerateHobbies(LocalizerFoo);
+        Hobbies3 = Foo.GenerateHobbies(LocalizerFoo);
+        Hobbies4 = Foo.GenerateHobbies(LocalizerFoo);
+        Hobbies7 = Foo.GenerateHobbies(LocalizerFoo);
+
+        ComplexModel = new ComplexFoo()
+        {
+            Dummy = new Dummy1() { Dummy2 = new Dummy2() },
+        };
+    }
+
+    [NotNull]
+    private Foo? Model2 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies2 { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? Logger2 { get; set; }
+
+    private async Task OnValidSubmit2(EditContext context)
+    {
+        Logger2.Log(Localizer["OnValidSubmitStartingLog"]);
+        await Task.Delay(3000);
+        Logger2.Log(Localizer["OnValidSubmitDoneLog"]);
+    }
+
+    private Task OnInvalidSubmit2(EditContext context)
+    {
+        Logger2.Log(Localizer["OnInvalidSubmitCallBackLog"]);
+        return Task.CompletedTask;
+    }
+
+    [NotNull]
+    private Foo? Model3 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies3 { get; set; }
+
+    [NotNull]
+    private ValidateForm? FooForm { get; set; }
+
+    private Task OnValidSetError(EditContext context)
+    {
+        FooForm.SetError<Foo>(f => f.Name, Localizer["DatabaseExistLog"]);
+        return Task.CompletedTask;
+    }
+
+    [NotNull]
+    private Foo? Model4 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies4 { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? Logger4 { get; set; }
+
+    private Task OnInvalidSubmitAddress(EditContext context)
+    {
+        Logger4.Log(Localizer["OnInvalidSubmitCallBackLog"]);
+        return Task.CompletedTask;
+    }
+
+    [NotNull]
+    private ConsoleLogger? Logger5 { get; set; }
+
+    [NotNull]
+    private ValidateForm? ComplexForm { get; set; }
+
+    [NotNull]
+    private ComplexFoo? ComplexModel { get; set; }
+
+    private ConcurrentDictionary<FieldIdentifier, object?> GetValueChangedFieldCollection() => ComplexForm?.ValueChagnedFields ?? new ConcurrentDictionary<FieldIdentifier, object?>();
+
+    private class ComplexFoo : Foo
+    {
+        [NotNull]
+        public Dummy1? Dummy { get; set; }
+    }
+
+    private class Dummy1
+    {
+        [NotNull]
+        public Dummy2? Dummy2 { get; set; }
+    }
+
+    private class Dummy2
+    {
+        [Required]
+        public string? Name { get; set; }
+    }
+
+    private Task OnInvalidComplexModel(EditContext context)
+    {
+        Logger5.Log(Localizer["OnInvalidSubmitCallBackLog"]);
+        return Task.CompletedTask;
+    }
+
+    private Task OnValidComplexModel(EditContext context)
+    {
+        Logger5.Log(Localizer["OnValidSubmitCallBackLog"]);
+        ComplexForm.SetError("Dummy.Dummy2.Name", Localizer["DatabaseExistLog"]);
+        return Task.CompletedTask;
+    }
+
+    private bool ShowAddress { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? Logger6 { get; set; }
+
+    private Foo DynamicModel { get; set; } = new Foo();
+
+    private Task OnInvalidDynamicModel(EditContext context)
+    {
+        Logger6.Log(Localizer["OnInvalidSubmitCallBackLog"]);
+        return Task.CompletedTask;
+    }
+
+    private Task OnValidDynamicModel(EditContext context)
+    {
+        Logger6.Log(Localizer["OnValidSubmitCallBackLog"]);
+        return Task.CompletedTask;
+    }
+
+    private void OnValidateChange()
+    {
+        ShowAddress = true;
+    }
+
+    private void OnValidateReset()
+    {
+        ShowAddress = false;
+    }
+
+    [NotNull]
+    private Foo? Model7 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies7 { get; set; }
+
+    private List<IValidator> CustomerRules { get; } = new();
+
+    [NotNull]
+    private Foo? Model8 { get; set; }
+
+    [NotNull]
+    private Foo? Model9 { get; set; }
+
+    [NotNull]
+    private ValidateForm? ValidatorForm { get; set; }
+
+    private Task OnValidator()
+    {
+        ValidatorForm.Validate();
+        return Task.CompletedTask;
+    }
+
+    [NotNull]
+    private Foo? Model10 { get; set; }
+
     #region 参数说明
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
