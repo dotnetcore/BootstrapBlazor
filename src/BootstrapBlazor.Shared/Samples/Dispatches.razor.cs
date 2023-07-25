9ca3470cae200bfc8dea7984ad6dbea6ed6f32e8
Old Li <lish@live.cn>
2023-07-25 21:39:09 +0800

    merge dispatche demos

---
+++ src/BootstrapBlazor.Shared/Samples/Dispatches.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace BootstrapBlazor.Shared.Samples;
+
+public partial class Dispatches
+{
+    private async Task OnDispatch()
+    {
+        DispatchService.Dispatch(new DispatchEntry<MessageItem>() { Name = nameof(MessageItem), Entry = new MessageItem() { Message = $"{DateTime.Now:HH:mm:ss} {Localizer["DispatchNoticeMessage"]}" } });
+        await Task.Delay(30 * 1000);
+    }
+}
