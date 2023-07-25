b2c3b98c1bbbf6544eab8f02034df30f0c852d87
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2023-07-25 12:24:11 +0800

    doc(Transfer): refactor the transfer document (#1633)
    * doc: 重构 transfer 文档
    
    * doc: 更改资源文件
    
    * test: 更新单元测试
    
    * refactor: 更正单词拼写错误
    
    * refactor: 更新代码
    
    * doc: 更新资源文件
    
    * refactor: 样式变量化
    
    * refactor: 更新样式
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor.Shared/Samples/Transfers.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Transfers.razor.cs
@@ -9,6 +9,97 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Transfers : ComponentBase
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items1 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items2 { get; set; }
+
+    [NotNull]
+    private List<SelectedItem>? Items3 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items4 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items5 { get; set; }
+
+    private Foo Model { get; set; } = new();
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items6 { get; set; }
+
+    private Foo Model6 { get; set; } = new();
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items7 { get; set; }
+
+    private IEnumerable<SelectedItem> SelectedValue { get; set; } = Enumerable.Empty<SelectedItem>();
+
+    private Task OnSelectedItemsChanged(IEnumerable<SelectedItem> items)
+    {
+        Logger.Log(string.Join(" ", items.Select(i => i.Text)));
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// OnInitializedAsync 方法
+    /// </summary>
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        // 模拟异步加载数据源
+        await Task.Delay(100);
+
+        Items = GeneratorItems();
+        Items1 = GeneratorItems();
+        Items2 = GeneratorItems();
+
+        Items3 = Enumerable.Range(1, 15).Select(i => new SelectedItem()
+        {
+            Text = $"{Localizer["Backup"]} {i:d2}",
+            Value = i.ToString()
+        }).ToList();
+
+        Items4 = GeneratorItems();
+        Items5 = GeneratorItems();
+        Items6 = GeneratorItems();
+        Items7 = GeneratorItems();
+
+        var v = SelectedValue.ToList();
+        v.AddRange(Items.Take(2));
+        v.AddRange(Items.Skip(4).Take(1));
+        SelectedValue = v;
+    }
+
+    private void OnAddItem()
+    {
+        var count = Items3.Count + 1;
+        Items3.Add(new SelectedItem(count.ToString(), $"{Localizer["Backup"]} {count:d2}"));
+    }
+
+    private IEnumerable<SelectedItem> GeneratorItems() => Enumerable.Range(1, 15).Select(i => new SelectedItem()
+    {
+        Text = $"{Localizer["Data"]} {i:d2}",
+        Value = i.ToString()
+    });
+
+    private static string? SetItemClass(SelectedItem item) => item.Value switch
+    {
+        "2" => "bg-success text-white",
+        "4" => "bg-info text-white",
+        "6" => "bg-primary text-white",
+        "8" => "bg-warning text-white",
+        _ => null
+    };
+
     /// <summary>
     /// 获得属性方法
     /// </summary>
@@ -59,15 +150,15 @@ public sealed partial class Transfers : ComponentBase
             DefaultValue = "false"
         },
         new AttributeItem() {
-            Name = "LeftPannelSearchPlaceHolderString",
-            Description = Localizer["LeftPannelSearchPlaceHolderString"],
+            Name = "LeftPanelSearchPlaceHolderString",
+            Description = Localizer["LeftPanelSearchPlaceHolderString"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
         new AttributeItem() {
-            Name = "RightPannelSearchPlaceHolderString",
-            Description = Localizer["RightPannelSearchPlaceHolderString"],
+            Name = "RightPanelSearchPlaceHolderString",
+            Description = Localizer["RightPanelSearchPlaceHolderString"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
