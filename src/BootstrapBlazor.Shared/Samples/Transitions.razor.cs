544890e0f3ce95a00dd0c618cd17513a557eab1a
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2023-07-25 12:35:49 +0800

    doc(Transtion): refactor the transition document  (#1634)
    * doc: 添加 logger 和 person 属性
    
    * doc: 重构 transition 文档
    
    * doc: 删除 code 代码块
    
    * refactor: 移除日志输出
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor.Shared/Samples/Transitions.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Transitions.razor.cs
@@ -9,6 +9,52 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Transitions
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private bool TransitionEndShow { get; set; }
+
+    private bool Show { get; set; }
+
+    private bool FadeInShow { get; set; }
+
+    private void OnShow()
+    {
+        Show = true;
+    }
+
+    private Task OnShowEnd()
+    {
+        Show = false;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private void OnTransitionShow()
+    {
+        TransitionEndShow = true;
+    }
+
+    private Task OnTransitionEndShow()
+    {
+        TransitionEndShow = false;
+        Logger.Log("animation ends");
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private void OnFadeInShow()
+    {
+        FadeInShow = true;
+    }
+
+    private Task OnFadeInEndShow()
+    {
+        FadeInShow = false;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
     private static IEnumerable<AttributeItem> GetAttributes() => new[]
     {
         new AttributeItem() {
