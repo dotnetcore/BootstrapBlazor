577e63b0ac3091373c5982217bfdc2180577cede
zhangpeihang <948869991@qq.com>
2023-07-25 15:24:30 +0800

    doc: 重构 textarea 文档

--- src/BootstrapBlazor.Shared/Samples/Textareas.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Textareas.razor.cs
@@ -9,6 +9,107 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Textareas
 {
+    [NotNull]
+    private Textarea? TextareaElement { get; set; }
+
+    private string? Text { get; set; }
+
+    private string? ChatText { get; set; }
+
+    private int ScrollValue { get; set; }
+
+    private bool IsAutoScroll { get; set; }
+
+    private CancellationTokenSource? CancelTokenSource { get; set; }
+
+    private int Index { get; set; } = 5;
+
+    private Task ScrollToTop() => TextareaElement.ScrollToTop();
+
+    private Task ScrollToBottom() => TextareaElement.ScrollToBottom();
+
+    private Task ScrollTo20() => TextareaElement.ScrollTo(ScrollValue += 20);
+
+    private void SwitchAutoScroll() => IsAutoScroll = !IsAutoScroll;
+
+    private bool IsRunMock { get; set; }
+
+    private string ChatLocalizerName => IsRunMock ? "TextareasMockChatStop" : "TextareasMockChatRun";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        for (int i = 0; i < Index; i++)
+        {
+            Text += $"{((i % 2 == 0) ? "A" : "B")} : {Localizer["TextareasMockChat"]}{i}{Environment.NewLine}";
+        }
+    }
+
+    /// <summary>
+    /// MockChat
+    /// </summary>
+    private void MockChat()
+    {
+        if (CancelTokenSource != null)
+        {
+            CancelTokenSource.Cancel();
+            CancelTokenSource.Dispose();
+            CancelTokenSource = null;
+        }
+
+        IsRunMock = !IsRunMock;
+
+        if (IsRunMock)
+        {
+            Task.Run(async () =>
+            {
+                CancelTokenSource ??= new();
+                while (CancelTokenSource != null && !CancelTokenSource.IsCancellationRequested)
+                {
+                    Text += $"{((Index % 2 == 0) ? "A" : "B")} : {Localizer["TextareasMockChat"]}{Index}{Environment.NewLine}";
+                    Index++;
+                    await InvokeAsync(StateHasChanged);
+
+                    try
+                    {
+                        if (CancelTokenSource != null)
+                        {
+                            await Task.Delay(500, CancelTokenSource.Token);
+                        }
+                    }
+                    catch (TaskCanceledException) { }
+                }
+            });
+        }
+    }
+
+    /// <summary>
+    /// Dispose
+    /// </summary>
+    /// <param name="disposing"></param>
+    private void Dispose(bool disposing)
+    {
+        if (disposing && CancelTokenSource != null)
+        {
+            CancelTokenSource.Cancel();
+            CancelTokenSource.Dispose();
+            CancelTokenSource = null;
+        }
+    }
+
+    /// <summary>
+    /// Dispose
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         new AttributeItem() {
