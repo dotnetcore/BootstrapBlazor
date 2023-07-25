1b17213f2c2a75d05a1323926c2e1e5aa62c6761
YSMC <ysmc_w@163.com>
2023-07-26 00:35:44 +0800

    doc(Drawer): refactor the drawer document (#1659)

--- src/BootstrapBlazor.Shared/Samples/Drawers.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Drawers.razor.cs
@@ -9,6 +9,49 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Drawers
 {
+    private bool IsOpen { get; set; }
+
+    private Placement DrawerAlign { get; set; }
+
+    private IEnumerable<SelectedItem> DrawerDirection { get; } = new SelectedItem[]
+    {
+        new SelectedItem("left", "left to right")
+        {
+            Active = true
+        },
+        new SelectedItem("right", "right to left"),
+        new SelectedItem("top", "top to bottom"),
+        new SelectedItem("bottom", "bottom to top")
+    };
+
+    private Task OnStateChanged(IEnumerable<SelectedItem> values, SelectedItem val)
+    {
+        DrawerAlign = val.Value switch
+        {
+            "right" => Placement.Right,
+            "top" => Placement.Top,
+            "bottom" => Placement.Bottom,
+            _ => Placement.Left
+        };
+        IsOpen = false;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private bool IsBackdropOpen { get; set; }
+
+    private void OpenDrawer()
+    {
+        IsBackdropOpen = true;
+    }
+
+    private bool IsShowBackdropOpen { get; set; }
+
+    private void OpenNoBackdropDrawer()
+    {
+        IsShowBackdropOpen = true;
+    }
+
     /// <summary>
     /// Get property method
     /// </summary>
