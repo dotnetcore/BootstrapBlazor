946652b3707d698fb7df12f7d3b0f5679c6d680e
zhangpeihang <948869991@qq.com>
2023-07-26 13:43:09 +0800

    doc: 重构 row 文档

--- src/BootstrapBlazor.Shared/Samples/Rows.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Rows.razor.cs
@@ -9,6 +9,63 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Rows
 {
+    private RowFoo Model { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbys { get; set; }
+
+    private RowFoo RowFormModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    private RowFoo NestedModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    private RowFoo SpanModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Hobbys = Foo.GenerateHobbies(LocalizerFoo);
+    }
+
+    private class RowFoo : Foo
+    {
+        [Required(ErrorMessage = "请选择学历")]
+        [Display(Name = "学历")]
+        [AutoGenerateColumn(Order = 60)]
+        public List<EnumEducation>? Educations { get; set; }
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         new AttributeItem() {
