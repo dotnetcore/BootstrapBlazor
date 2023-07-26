3194aa121bd6eeea5357ae5398fc14b2608ce286
Argo-AscioTech <argo@live.ca>
2023-07-26 15:24:51 +0800

    doc: 更新文档

--- src/BootstrapBlazor.Shared/Samples/Dispatches.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Dispatches.razor.cs
@@ -1,5 +1,12 @@
-﻿namespace BootstrapBlazor.Shared.Samples;
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
 
+namespace BootstrapBlazor.Shared.Samples;
+
+/// <summary>
+/// Dispatches 组件
+/// </summary>
 public partial class Dispatches
 {
     private async Task OnDispatch()
