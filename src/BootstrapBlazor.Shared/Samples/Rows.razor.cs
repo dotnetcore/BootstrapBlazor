02d5e68ad723872bc8aa205dbaf1b47b448351f4
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2023-07-26 16:33:24 +0800

    doc(Row): refactor the row document (#1690)
    * doc: 重构 row 文档
    
    * doc: 更新路由
    
    * doc: 更正单词拼写错误
    
    * doc: 更新变量
    
    * doc: 变量分开
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor.Shared/Samples/Rows.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Rows.razor.cs
@@ -9,30 +9,99 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Rows
 {
+    private RowFoo Model { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies1 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies2 { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Hobbies3 { get; set; }
+
+    private RowFoo RowFormModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    private RowFoo NestedModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    private RowFoo SpanModel { get; } = new()
+    {
+        Name = "张三",
+        Count = 23,
+        Address = "测试地址",
+        DateTime = new DateTime(1997, 12, 05),
+        Educations = new List<EnumEducation> { EnumEducation.Primary, EnumEducation.Middle }
+    };
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Hobbies1 = Foo.GenerateHobbies(LocalizerFoo);
+        Hobbies2 = Foo.GenerateHobbies(LocalizerFoo);
+        Hobbies3 = Foo.GenerateHobbies(LocalizerFoo);
+    }
+
+    private class RowFoo : Foo
+    {
+        [Required(ErrorMessage = "请选择学历")]
+        [Display(Name = "学历")]
+        [AutoGenerateColumn(Order = 60)]
+        public List<EnumEducation>? Educations { get; set; }
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new AttributeItem() {
+        new()
+        {
             Name = "ItemsPerRow",
             Description = Localizer["RowsItemsPerRow"],
             Type = "enum",
             ValueList = " One,Two,Three,Four,Six,Twelve ",
             DefaultValue = " One "
         },
-        new AttributeItem() {
+        new()
+        {
             Name = "RowType",
             Description = Localizer["RowsRowType"],
             Type = "enum?",
             ValueList = "Normal, Inline",
             DefaultValue = "null"
         },
-        new AttributeItem() {
+        new()
+        {
             Name = "ColSpan",
             Description = Localizer["RowsColSpan"],
             Type = "int?",
             ValueList = "-",
             DefaultValue = "null"
         },
-        new AttributeItem() {
+        new()
+        {
             Name = "MaxCount",
             Description = Localizer["RowsMaxCount"],
             Type = "int?",
