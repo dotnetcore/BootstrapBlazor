ee3ebc49f492b68c7d276dbe371ffc661a50230c
zhangpeihang <948869991@qq.com>
2023-07-26 14:01:04 +0800

    doc: 重构 rate 文档

--- src/BootstrapBlazor.Shared/Samples/Rates.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Rates.razor.cs
@@ -9,6 +9,47 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Rates
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private double BindValue { get; set; } = 3.0;
+
+    private double BindValue1 { get; set; } = 2;
+
+    private double BindValue2 { get; set; } = 2;
+
+    private double BindValue3 { get; set; } = 2.8;
+
+    private bool IsDisable { get; set; }
+
+    private void OnValueChanged(double val)
+    {
+        BindValue = val;
+        Logger.Log($"{Localizer["RatesLog"]} {val}");
+    }
+
+    private double IconListValue { get; set; } = 1;
+
+    private List<string> IconList { get; } = new List<string>()
+    {
+        "fa-solid fa-face-sad-cry",
+        "fa-solid fa-face-sad-tear",
+        "fa-solid fa-face-smile",
+        "fa-solid fa-face-surprise",
+        "fa-solid fa-face-grin-stars"
+    };
+
+    private string GetIconList(int index) => IconList[index - 1];
+
+    private string GetIconValueChanged() => (IconListValue - 1) switch
+    {
+        0 => Localizer["RatesCry"],
+        1 => Localizer["RatesTear"],
+        2 => Localizer["RatesSmile"],
+        3 => Localizer["RatesSurprise"],
+        _ => Localizer["RatesGrin"]
+    };
+
     private IEnumerable<EventItem> GetEvents() => new EventItem[]
     {
         new EventItem()
