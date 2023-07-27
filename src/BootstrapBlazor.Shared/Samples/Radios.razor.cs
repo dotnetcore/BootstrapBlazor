a6bce4d91e8e5913e04c29073bcc9816001da82a
zhangpeihang <948869991@qq.com>
2023-07-27 10:37:42 +0800

    doc: 重构 radio 文档

--- src/BootstrapBlazor.Shared/Samples/Radios.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Radios.razor.cs
@@ -9,6 +9,96 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Radios
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? DemoValues { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? DisableValues { get; set; }
+
+    [NotNull]
+    private EnumEducation? SelectedEnumItem { get; set; }
+
+    [NotNull]
+    private EnumEducation? SelectedEnumItem2 { get; set; }
+
+    [NotNull]
+    private EnumEducation? SelectedEnumItem3 { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? BindLogger { get; set; }
+
+    [NotNull]
+    private IEnumerable<SelectedItem>? Items { get; set; }
+
+    private SelectedItem BindRadioItem { get; set; } = new SelectedItem();
+
+    [NotNull]
+    private IEnumerable<IconSelectedItem>? IconDemoValues { get; set; }
+
+    [NotNull]
+    private Foo? Model { get; set; }
+
+    [NotNull]
+    private List<SelectedItem>? FooItems { get; set; }
+
+    private Task OnSelectedChanged(IEnumerable<SelectedItem> values, string val)
+    {
+        var value = values.FirstOrDefault();
+        Logger.Log($"{Localizer["RadiosLog1"]} {value?.Value}  {Localizer["RadiosLog2"]}{value?.Text}  {Localizer["RadiosLog3"]}{val}");
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        DemoValues = new List<SelectedItem>(2)
+        {
+            new SelectedItem("1", Localizer["RadiosItem1"]),
+            new SelectedItem("2", Localizer["RadiosItem2"])
+        };
+
+        DisableValues = new List<SelectedItem>(2)
+        {
+            new SelectedItem("1", Localizer["RadiosItem1"]),
+            new SelectedItem("2", Localizer["RadiosItem2"]) { IsDisabled = true }
+        };
+
+        Items = new SelectedItem[]
+        {
+            new SelectedItem("1", Localizer["RadiosAdd1"]),
+            new SelectedItem("2", Localizer["RadiosAdd2"])
+        };
+
+        IconDemoValues = new List<IconSelectedItem>()
+        {
+            new IconSelectedItem() { Text = "Item1", Value = "1", Icon = "fa-solid fa-users" },
+            new IconSelectedItem() { Text = "Item2", Value = "2", Icon = "fa-solid fa-users-gear" }
+        };
+
+        Model = Foo.Generate(LocalizerFoo);
+        FooItems = Foo.GetCompleteItems(LocalizerFoo);
+    }
+
+    private Task OnItemChanged(IEnumerable<SelectedItem> values, SelectedItem val)
+    {
+        var value = values.FirstOrDefault();
+        BindLogger.Log($"{Localizer["RadiosLog1"]} {value?.Value} {Localizer["RadiosLog1"]} {value?.Text}");
+        return Task.CompletedTask;
+    }
+
+
+    class IconSelectedItem : SelectedItem
+    {
+        public string? Icon { get; set; }
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new[]
     {
         new AttributeItem() {
