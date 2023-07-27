d217ab1672fe9e4d278cab4ebf0fb79dbb6c5f2d
zhangpeihang <948869991@qq.com>
2023-07-27 12:17:04 +0800

    doc: 重构 pdfreader 文档

--- src/BootstrapBlazor.Shared/Samples/PdfReaders.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/PdfReaders.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using System.ComponentModel;
+
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
@@ -9,6 +11,103 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class PdfReaders
 {
+    [DisplayName("the file in the wwwroot relative path or url")]
+    private string FileName { get; set; } = "/samples/sample.pdf";
+
+    [DisplayName("the file in the wwwroot relative path or url")]
+    private string Filename { get; set; } = "/samples/sample.pdf";
+
+    [NotNull]
+    PdfReader? PdfReader { get; set; }
+
+    private string FilenameStream { get; set; } = "https://blazor.app1.es/_content/DemoShared/samples/sample2.pdf";
+
+    private async Task Apply()
+    {
+        await PdfReader!.Refresh();
+    }
+
+    [NotNull]
+    PdfReader? AdvancedPdfReader { get; set; }
+
+    [DisplayName("流模式")]
+    private bool StreamMode { get; set; }
+
+    [DisplayName("禁用复制/打印/下载")]
+    public bool ReadOnly { get; set; }
+
+    [DisplayName("水印内容")]
+    public string Watermark { get; set; } = "www.blazor.zone";
+
+    private EnumZoomMode Zoom { get; set; } = EnumZoomMode.PageHeight;
+
+    private EnumPageMode Pagemode { get; set; } = EnumPageMode.None;
+
+    [DisplayName("搜索")]
+    private string? Search { get; set; } = "Performance";
+
+    private int Page { get; set; } = 3;
+
+    private bool ForcePDFJS { get; set; } = true;
+
+    private async Task ApplyZoom()
+    {
+        Zoom = Zoom switch
+        {
+            EnumZoomMode.Auto => EnumZoomMode.PageActual,
+            EnumZoomMode.PageActual => EnumZoomMode.PageFit,
+            EnumZoomMode.PageFit => EnumZoomMode.PageWidth,
+            EnumZoomMode.PageWidth => EnumZoomMode.PageHeight,
+            EnumZoomMode.PageHeight => EnumZoomMode.Zoom75,
+            EnumZoomMode.Zoom75 => EnumZoomMode.Zoom50,
+            EnumZoomMode.Zoom50 => EnumZoomMode.Zoom25,
+            EnumZoomMode.Zoom25 => EnumZoomMode.Zoom200,
+            _ => EnumZoomMode.Auto
+        };
+        await Refresh();
+    }
+
+    private async Task ApplyPagemode()
+    {
+        Pagemode = Pagemode switch
+        {
+            EnumPageMode.Thumbs => EnumPageMode.Outline,
+            EnumPageMode.Outline => EnumPageMode.Attachments,
+            EnumPageMode.Attachments => EnumPageMode.Layers,
+            EnumPageMode.Layers => EnumPageMode.None,
+            _ => EnumPageMode.Thumbs
+        };
+        await Refresh();
+    }
+
+    async Task Refresh()
+    {
+        if (PdfReader != null)
+            await PdfReader.Refresh(Search, Page, Pagemode, Zoom, ReadOnly, Watermark);
+    }
+
+    private async Task ApplyPage()
+    {
+        Search = null;
+        await Refresh();
+    }
+
+    private async Task ApplyPagePrevious()
+    {
+        Page--;
+        Search = null;
+        await Refresh();
+    }
+
+    private async Task ApplyPageNext()
+    {
+        Page++;
+        Search = null;
+        await Refresh();
+    }
+
+    private async Task ApplySearch() => await Refresh();
+
     /// <summary>
     /// GetAttributes
     /// </summary>
