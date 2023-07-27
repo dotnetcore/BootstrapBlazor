5b10441da5dd332e0c524cd14eba8bdde10fe740
Old Li <lish@live.cn>
2023-07-27 20:41:55 +0800

    merge dropdownwidget demos

--- src/BootstrapBlazor.Shared/Samples/DropdownWidgets.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/DropdownWidgets.razor.cs
@@ -9,72 +9,71 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class DropdownWidgets
 {
-
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-            // TODO: 移动到数据库中
-            new AttributeItem() {
-                Name = "Icon",
-                Description = Localizer["Icon"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = "fa-regular fa-bell"
-            },
-            new AttributeItem() {
-                Name = "BadgeColor",
-                Description = Localizer["BadgeColor"],
-                Type = "Color",
-                ValueList = " — ",
-                DefaultValue = "Success"
-            },
-            new AttributeItem() {
-                Name = "HeaderColor",
-                Description = Localizer["HeaderColor"],
-                Type = "Color",
-                ValueList = "Primary / Secondary / Info / Warning / Danger ",
-                DefaultValue = "Primary"
-            },
-            new AttributeItem() {
-                Name = "BadgeNumber",
-                Description = Localizer["BadgeNumber"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "ShowArrow",
-                Description = Localizer["ShowArrow"],
-                Type = "boolean",
-                ValueList = "true/false",
-                DefaultValue = "true"
-            },
-            new AttributeItem() {
-                Name = "MenuAlignment",
-                Description = Localizer["MenuAlignment"],
-                Type = "Alignment",
-                ValueList = "None / Left / Center / Right ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "HeaderTemplate",
-                Description = Localizer["HeaderTemplate"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "BodyTemplate",
-                Description = Localizer["BodyTemplate"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "FooterTemplate",
-                Description = Localizer["FooterTemplate"],
-                Type = "RenderFragment",
-                ValueList = " — ",
-                DefaultValue = " — "
-            }
+        // TODO: 移动到数据库中
+        new AttributeItem() {
+            Name = "Icon",
+            Description = Localizer["Icon"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "fa-regular fa-bell"
+        },
+        new AttributeItem() {
+            Name = "BadgeColor",
+            Description = Localizer["BadgeColor"],
+            Type = "Color",
+            ValueList = " — ",
+            DefaultValue = "Success"
+        },
+        new AttributeItem() {
+            Name = "HeaderColor",
+            Description = Localizer["HeaderColor"],
+            Type = "Color",
+            ValueList = "Primary / Secondary / Info / Warning / Danger ",
+            DefaultValue = "Primary"
+        },
+        new AttributeItem() {
+            Name = "BadgeNumber",
+            Description = Localizer["BadgeNumber"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "ShowArrow",
+            Description = Localizer["ShowArrow"],
+            Type = "boolean",
+            ValueList = "true/false",
+            DefaultValue = "true"
+        },
+        new AttributeItem() {
+            Name = "MenuAlignment",
+            Description = Localizer["MenuAlignment"],
+            Type = "Alignment",
+            ValueList = "None / Left / Center / Right ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "HeaderTemplate",
+            Description = Localizer["HeaderTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "BodyTemplate",
+            Description = Localizer["BodyTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "FooterTemplate",
+            Description = Localizer["FooterTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
     };
 }
