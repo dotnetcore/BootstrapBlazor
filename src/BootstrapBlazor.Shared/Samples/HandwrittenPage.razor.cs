557183e359aaa0391d71fd27c6c4b2e2713e54d5
Old Li <lish@live.cn>
2023-07-27 20:59:49 +0800

    merge handwritten demos

--- src/BootstrapBlazor.Shared/Samples/HandwrittenPage.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/HandwrittenPage.razor.cs
@@ -9,39 +9,44 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class HandwrittenPage
 {
+    /// <summary>
+    /// 签名Base64
+    /// </summary>
+    public string? DrawBase64 { get; set; }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-            new AttributeItem()
-            {
-                Name = "SaveButtonText",
-                Description = Localizer["SaveButtonText"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = Localizer["SaveButtonTextDefaultValue"]
-            },
-            new AttributeItem()
-            {
-                Name = "ClearButtonText",
-                Description = Localizer["ClearButtonText"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = Localizer["ClearButtonTextDefaultValue"]
-            },
-            new AttributeItem()
-            {
-                Name = "Result",
-                Description = Localizer["Result"],
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem()
-            {
-                Name = "HandwrittenBase64",
-                Description = Localizer["HandwrittenBase64"],
-                Type = "EventCallback<string>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            }
+        new AttributeItem()
+        {
+            Name = "SaveButtonText",
+            Description = Localizer["SaveButtonText"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = Localizer["SaveButtonTextDefaultValue"]
+        },
+        new AttributeItem()
+        {
+            Name = "ClearButtonText",
+            Description = Localizer["ClearButtonText"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = Localizer["ClearButtonTextDefaultValue"]
+        },
+        new AttributeItem()
+        {
+            Name = "Result",
+            Description = Localizer["Result"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem()
+        {
+            Name = "HandwrittenBase64",
+            Description = Localizer["HandwrittenBase64"],
+            Type = "EventCallback<string>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
     };
 }
