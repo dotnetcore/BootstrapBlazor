23f2ca1eae5829165587d427580f5bbce3178ad4
Old Li <lish@live.cn>
2023-07-27 21:12:34 +0800

    merge locator demos

---
+++ src/BootstrapBlazor.Shared/Samples/Locators.razor.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Samples;
+
+public partial class Locators
+{
+    private string? Ip { get; set; }
+    private string? Location { get; set; }
+
+    /// <summary>
+    /// OnAfterRenderAsync
+    /// </summary>
+    /// <param name = "firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+        if (firstRender)
+        {
+            var clientInfo = await ClientService.GetClientInfo();
+            Ip = clientInfo.Ip;
+            StateHasChanged();
+        }
+    }
+
+    private async Task OnClick()
+    {
+        if (!string.IsNullOrEmpty(Ip))
+        {
+            Location = await IPLocator.Locate(Ip);
+        }
+    }
+}
