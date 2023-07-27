9094ae80e4432c060b2af3c7ffa03fcad46248d1
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2023-07-27 11:48:49 +0800

    doc(Progress): refactor the progress document (#1704)
    * doc: 重构 progress 文档
    
    * refactor: 重构组件
    
    * doc: 更新路由
    
    * doc: 更新资源文件
    
    * doc: 更改 Progress 防止冲突
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor.Shared/Extensions/MenusLocalizerExtensions.cs
+++ src/BootstrapBlazor.Shared/Extensions/MenusLocalizerExtensions.cs
@@ -936,7 +936,7 @@ internal static class MenusLocalizerExtensions
                 new()
                 {
                     Text = Localizer["Progress"],
-                    Url = "progresss"
+                    Url = "progress"
                 },
                 new()
                 {
