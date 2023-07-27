72670603a632385b006ff95ae7c06c8f4fdce7cb
zhangpeihang <948869991@qq.com>
2023-07-27 11:47:30 +0800

    doc: 重构 popoverconfirm 文档

--- src/BootstrapBlazor.Shared/Samples/PopoverConfirms.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/PopoverConfirms.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.AspNetCore.Components.Forms;
+
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
@@ -9,6 +11,59 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class PopoverConfirms
 {
+    private static Task OnAsyncConfirm() => Task.Delay(3000);
+
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    [NotNull]
+    private Foo? Model { get; set; }
+
+    [NotNull]
+    private ConsoleLogger? FormLogger { get; set; }
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Model = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+    }
+
+    private Task OnClose()
+    {
+        // This method is called back when the confirm button is clicked, and this method will not be called when the cancel button is clicked
+        Logger.Log("OnClose Trigger");
+        return Task.CompletedTask;
+    }
+
+    private Task OnConfirm()
+    {
+        // This method is called back when the confirm button is clicked, and this method will not be called when the cancel button is clicked
+        Logger.Log("OnConfirm Trigger");
+        return Task.CompletedTask;
+    }
+
+    private async Task OnAsyncSubmit()
+    {
+        FormLogger.Log("异步提交");
+        await Task.Delay(3000);
+    }
+
+    private async Task OnValidSubmit(EditContext context)
+    {
+        await Task.Delay(3000);
+        FormLogger.Log("数据合规");
+    }
+
+    private Task OnInValidSubmit(EditContext context)
+    {
+        FormLogger.Log("数据非法");
+        return Task.CompletedTask;
+    }
+
     /// <summary>
     /// Get property method
     /// </summary>
