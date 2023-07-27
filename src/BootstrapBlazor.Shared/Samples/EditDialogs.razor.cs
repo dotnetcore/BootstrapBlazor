eff750ce1882551c51799d9f06e369a987a3f069
Argo-AscioTech <argo@live.ca>
2023-07-27 21:44:11 +0800

    doc: 更改参数文档

--- src/BootstrapBlazor.Shared/Samples/EditDialogs.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/EditDialogs.razor.cs
@@ -127,80 +127,87 @@ public sealed partial class EditDialogs
     /// get property method
     /// </summary>
     /// <returns></returns>
-    private static IEnumerable<AttributeItem> GetAttributes()
+    private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        return new AttributeItem[]
+        new()
         {
-            new AttributeItem() {
-                Name = "ShowLabel",
-                Description = "Whether to show labels",
-                Type = "bool",
-                ValueList = "true|false",
-                DefaultValue = "true"
-            },
-            new AttributeItem() {
-                Name = "Model",
-                Description = "Generic parameters are used to render the UI",
-                Type = "TModel",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "Items",
-                Description = "Edit item collection",
-                Type = "IEnumerable<IEditorItem>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "DialogBodyTemplate",
-                Description = "EditDialog Body template",
-                Type = "RenderFragment<TModel>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "CloseButtonText",
-                Description = "Close button text",
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = "closure"
-            },
-            new AttributeItem() {
-                Name = "SaveButtonText",
-                Description = "Save button text",
-                Type = "string",
-                ValueList = " — ",
-                DefaultValue = "save"
-            },
-            new AttributeItem() {
-                Name = "OnSaveAsync",
-                Description = "Save callback delegate",
-                Type = "Func<Task>",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "ItemsPerRow",
-                Description = "Displays the number of components per line",
-                Type = "int?",
-                ValueList = " — ",
-                DefaultValue = " — "
-            },
-            new AttributeItem() {
-                Name = "RowType",
-                Description = "Set the component layout",
-                Type = "RowType",
-                ValueList = "Row|Inline",
-                DefaultValue = "Row"
-            },
-            new AttributeItem() {
-                Name = "LabelAlign",
-                Description = "Inline Label alignment in layout mode",
-                Type = "Alignment",
-                ValueList = "None|Left|Center|Right",
-                DefaultValue = "None"
-            }
-        };
-    }
+            Name = "ShowLabel",
+            Description = "Whether to show labels",
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new()
+        {
+            Name = "Model",
+            Description = "Generic parameters are used to render the UI",
+            Type = "TModel",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "Items",
+            Description = "Edit item collection",
+            Type = "IEnumerable<IEditorItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "DialogBodyTemplate",
+            Description = "EditDialog Body template",
+            Type = "RenderFragment<TModel>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "CloseButtonText",
+            Description = "Close button text",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "closure"
+        },
+        new()
+        {
+            Name = "SaveButtonText",
+            Description = "Save button text",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "save"
+        },
+        new()
+        {
+            Name = "OnSaveAsync",
+            Description = "Save callback delegate",
+            Type = "Func<Task>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "ItemsPerRow",
+            Description = "Displays the number of components per line",
+            Type = "int?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "RowType",
+            Description = "Set the component layout",
+            Type = "RowType",
+            ValueList = "Row|Inline",
+            DefaultValue = "Row"
+        },
+        new()
+        {
+            Name = "LabelAlign",
+            Description = "Inline Label alignment in layout mode",
+            Type = "Alignment",
+            ValueList = "None|Left|Center|Right",
+            DefaultValue = "None"
+        }
+    };
 }
