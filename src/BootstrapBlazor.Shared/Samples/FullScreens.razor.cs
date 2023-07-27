4e04a55902f0eef5d780ef81592b4509853b4f20
Old Li <lish@live.cn>
2023-07-27 20:53:39 +0800

    merge fullscreen demos

---
+++ src/BootstrapBlazor.Shared/Samples/FullScreens.razor.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Samples;
+
+public partial class FullScreens
+{
+    [Inject]
+    [NotNull]
+    private FullScreenService? FullScreenService { get; set; }
+
+    private async Task ToggleFullScreen()
+    {
+        await FullScreenService.Toggle();
+    }
+}
