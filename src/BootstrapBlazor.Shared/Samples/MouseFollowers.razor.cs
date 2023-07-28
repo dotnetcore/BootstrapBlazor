98dcc90e85b02627e444721d97b942f68a26de5c
zhangpeihang <948869991@qq.com>
2023-07-28 16:37:57 +0800

    doc: 重构 mousefollower 文档

---
+++ src/BootstrapBlazor.Shared/Samples/MouseFollowers.razor.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Samples;
+
+/// <summary>
+/// MouseFollowers
+/// </summary>
+public partial class MouseFollowers
+{
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<MouseFollowers>? Localizer { get; set; }
+
+    [NotNull]
+    private static string NugetPackageName => "BootstrapBlazor.MouseFollower";
+
+    private MouseFollowerOptions FollowerOptions = new() { ClassName = "mf-cursor bb-cursor" };
+
+    private MouseFollowerOptions FollowerImageOptions = new MouseFollowerOptions()
+    {
+        ClassName = "mf-cursor bb-cursor",
+        MediaClassName = "mf-cursor-media bb-cursor-media"
+    };
+
+    /// <summary>
+    /// 获得属性方法
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    {
+        // TODO: 移动到数据库中
+        new AttributeItem()
+        {
+            Name = "FollowerMode",
+            Description = Localizer["MouseFollowersFollowerMode"],
+            Type = "Enum",
+            ValueList = " — ",
+            DefaultValue = "MouseFollowerMode.Normal"
+        },
+        new AttributeItem()
+        {
+            Name = "GlobalMode",
+            Description = Localizer["MouseFollowersGlobalMode"],
+            Type = "bool",
+            ValueList = " — ",
+            DefaultValue = "false"
+        },
+        new AttributeItem()
+        {
+            Name = "Content",
+            Description = Localizer["MouseFollowersContent"],
+            Type = "string?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
+    };
+}
