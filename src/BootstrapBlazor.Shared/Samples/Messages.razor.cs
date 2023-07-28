f18949afaafdbd9e8bfa52f8fe44f9fa60c98dbd
zhangpeihang <948869991@qq.com>
2023-07-28 16:58:55 +0800

    doc: 重构 message 文档

--- src/BootstrapBlazor.Shared/Samples/Messages.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Messages.razor.cs
@@ -9,9 +9,73 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Messages
 {
+    [NotNull]
+    private Message? Message { get; set; }
+
+    [NotNull]
+    private Message? Message1 { get; set; }
+
+    private async Task ShowMessage()
+    {
+        Message.SetPlacement(Placement.Top);
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a reminder message"
+        });
+    }
+
+    private async Task ShowIconMessage()
+    {
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a reminder message",
+            Icon = "fa-solid fa-circle-info"
+        });
+    }
+
+    private async Task ShowCloseMessage()
+    {
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a reminder message",
+            Icon = "fa-solid fa-circle-info",
+            ShowDismiss = true,
+        });
+    }
+
+    private async Task ShowBarMessage()
+    {
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a reminder message",
+            Icon = "fa-solid fa-circle-info",
+            ShowBar = true,
+        });
+    }
+
+    private async Task ShowColorMessage(Color color)
+    {
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a colored message",
+            Icon = "fa-solid fa-circle-info",
+            Color = color
+        });
+    }
+
+    private async Task ShowBottomMessage()
+    {
+        await MessageService.Show(new MessageOption()
+        {
+            Content = "This is a reminder message",
+            Icon = "fa-solid fa-circle-info",
+        }, Message1);
+    }
+
     private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "Placement",
             Description = "message popup location",
             Type = "Placement",
@@ -26,42 +90,48 @@ public sealed partial class Messages
     /// <returns></returns>
     private static IEnumerable<AttributeItem> GetMessageItemAttributes() => new AttributeItem[]
     {
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "ChildContent",
             Description = "Content",
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "Class",
             Description = "Style",
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "Color",
             Description = "Color",
             Type = "Color",
             ValueList = "Primary / Secondary / Success / Danger / Warning / Info / Dark",
             DefaultValue = "Primary"
         },
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "Icon",
             Description = "Icon",
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "ShowDismiss",
             Description = "Show close button",
             Type = "bool",
             ValueList = " — ",
             DefaultValue = "false"
         },
-        new AttributeItem() {
+        new AttributeItem()
+        {
             Name = "ShowBar",
             Description = "Whether to show the left Bar",
             Type = "bool",
