2839f36a803e448c31c4dc2ed42fa20934bf1279
Argo-AscioTech <argo@live.ca>
2023-07-28 17:19:12 +0800

    doc: 更新参数说明文档

--- src/BootstrapBlazor.Shared/Samples/Dialogs.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Dialogs.razor.cs
@@ -311,119 +311,136 @@ public sealed partial class Dialogs
     /// <returns></returns>
     private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new() {
+        new()
+        {
             Name = "Component",
             Description = "Parameters of the component referenced in the dialog Body",
             Type = "DynamicComponent",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "BodyContext",
             Description = "pop-up window",
             Type = "object",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "HeaderTemplate",
             Description = "Modal body ModalHeader template",
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "BodyTemplate",
             Description = "Modal body ModalBody component",
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "FooterTemplate",
             Description = "ModalFooter component at the bottom of the modal",
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "IsCentered",
             Description = "Whether to center vertically",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "true"
         },
-        new() {
+        new()
+        {
             Name = "IsScrolling",
             Description = "Whether to scroll when the text of the pop-up window is too long",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = "ShowCloseButton",
             Description = "whether to show the close button",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "true"
         },
-        new() {
+        new()
+        {
             Name = "ShowHeaderCloseButton",
             Description = "Whether to display the close button on the right side of the title bar",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "true"
         },
-        new() {
+        new()
+        {
             Name = "ShowFooter",
             Description = "whether to display Footer",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "true"
         },
-        new() {
+        new()
+        {
             Name = nameof(DialogOption.ShowPrintButton),
             Description = "Whether to show the print button",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = nameof(DialogOption.ShowPrintButtonInHeader),
             Description = "Whether the print button is displayed in the Header",
             Type = "boolean",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = "Size",
             Description = "Size of dialog",
             Type = "Size",
             ValueList = "None / ExtraSmall / Small / Medium / Large / ExtraLarge",
             DefaultValue = "Large"
         },
-        new() {
+        new()
+        {
             Name = nameof(DialogOption.FullScreenSize),
             Description = "Full screen when smaller than a certain size",
             Type = "Size",
             ValueList = "None / Always / Small / Medium / Large / ExtraLarge",
             DefaultValue = "None"
         },
-        new() {
+        new()
+        {
             Name = "Title",
             Description = "Popup title",
             Type = "string",
             ValueList = " — ",
             DefaultValue = " not set "
         },
-        new() {
+        new()
+        {
             Name = nameof(DialogOption.PrintButtonText),
             Description = "print button display text",
             Type = "string",
             ValueList = " — ",
             DefaultValue = "The value set in the resource file"
         },
-        new() {
+        new()
+        {
             Name = nameof(DialogOption.ShowMaximizeButton),
             Description = "Whether to show the maximize button",
             Type = "boolean",
