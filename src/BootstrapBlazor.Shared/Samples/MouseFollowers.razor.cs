cd7b530e924c14d08cfc9b1c058b904977e7be03
Argo-AscioTech <argo@live.ca>
2023-07-28 17:00:36 +0800

    doc: 更新说明文档

--- src/BootstrapBlazor.Shared/Samples/MouseFollowers.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/MouseFollowers.razor.cs
@@ -30,8 +30,7 @@ public partial class MouseFollowers
     /// <returns></returns>
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        // TODO: 移动到数据库中
-        new AttributeItem()
+        new()
         {
             Name = "FollowerMode",
             Description = Localizer["MouseFollowersFollowerMode"],
@@ -39,7 +38,7 @@ public partial class MouseFollowers
             ValueList = " — ",
             DefaultValue = "MouseFollowerMode.Normal"
         },
-        new AttributeItem()
+        new()
         {
             Name = "GlobalMode",
             Description = Localizer["MouseFollowersGlobalMode"],
@@ -47,7 +46,7 @@ public partial class MouseFollowers
             ValueList = " — ",
             DefaultValue = "false"
         },
-        new AttributeItem()
+        new()
         {
             Name = "Content",
             Description = Localizer["MouseFollowersContent"],
