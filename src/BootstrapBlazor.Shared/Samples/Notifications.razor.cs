c2b46c3dd5d32fd6fec7e3dfc86b2281cf68d9e3
zhangpeihang <948869991@qq.com>
2023-07-28 10:16:50 +0800

    doc: 重构 notification 文档

--- src/BootstrapBlazor.Shared/Samples/Notifications.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Notifications.razor.cs
@@ -9,6 +9,58 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Notifications
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private bool Permission { get; set; }
+
+    private NotificationItem Model { get; set; } = new NotificationItem()
+    {
+        Icon = "./images/Argo-C.png"
+    };
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Model.Title ??= Localizer["NotificationsNormalTitleSampleText"];
+        Model.Message ??= Localizer["NotificationsNormalMessageSampleText"];
+        Model.OnClick = OnClickNotificationCallback;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await BrowserNotification.CheckPermission(false);
+        }
+    }
+
+    private async Task CheckPermission()
+    {
+        Permission = await BrowserNotification.CheckPermission();
+        Logger.Log(Localizer["NotificationsNormalGetPermissionCallbackText"] + (Permission ? "OK" : "No permission"));
+    }
+
+    private async Task Dispatch()
+    {
+        await BrowserNotification.Dispatch(Model);
+    }
+
+    private Task OnClickNotificationCallback()
+    {
+        Logger.Log($"{Localizer["NotificationsNormalOnClickText"]}");
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
     private IEnumerable<AttributeItem> GetNotificationItem() => new AttributeItem[]
     {
         new()
