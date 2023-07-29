73c34e4bee07a2a5821caac103552727fe5b094a
Argo Zhang <argo@live.ca>
2023-07-29 21:52:48 +0800

    doc(Download): refactor Download sample document (#1761)

--- src/BootstrapBlazor.Shared/Samples/Downloads.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Downloads.razor.cs
@@ -9,11 +9,23 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Downloads
 {
-    [Inject]
-    [NotNull]
-    private DownloadService? DownloadService { get; set; }
+    private string TempUrl { get; set; } = "./favicon.png";
 
     private static bool IsWasm => OperatingSystem.IsBrowser();
+
+    private async Task DownloadPhysicalFileAsync()
+    {
+        try
+        {
+            var filePath = Path.Combine(SiteOptions.CurrentValue.WebRootPath, "favicon.png");
+            await using var stream = File.OpenRead(filePath);
+            await DownloadService.DownloadFromStreamAsync("favicon.png", stream);
+        }
+        catch (FileNotFoundException msg)
+        {
+            await ToastService.Error("下载", msg.Message);
+        }
+    }
 
     private async Task DownloadFileAsync()
     {
@@ -25,6 +37,38 @@ public partial class Downloads
             var ms = new MemoryStream();
             var writer = new StreamWriter(ms);
             await writer.WriteLineAsync("自行生成并写入的文本，这里可以换成图片或其他内容");
+            await writer.FlushAsync();
+            ms.Position = 0;
+            return ms;
+        }
+    }
+
+    private async Task DownloadFolderAsync()
+    {
+        try
+        {
+            await DownloadService.DownloadFolderAsync("test.zip", SiteOptions.CurrentValue.WebRootPath);
+        }
+        catch (FileNotFoundException msg)
+        {
+
+            await ToastService.Error("下载", msg.Message);
+        }
+    }
+
+    private async Task DownloadLargeFileAsync()
+    {
+        await using var stream = await GenerateFileStreamAsync();
+        await DownloadService.DownloadFromStreamAsync("测试大文件.txt", stream);
+
+        static async Task<Stream> GenerateFileStreamAsync()
+        {
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            for (var i = 0; i < 1000000; i++)
+            {
+                await writer.WriteLineAsync($"这里是一个大文件下载示例，共循环 100 万次");
+            }
             await writer.FlushAsync();
             ms.Position = 0;
             return ms;
