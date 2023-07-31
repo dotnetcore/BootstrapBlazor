2ff65e4f2f0509e22f6621e7f307fe16b49180e1
YSMC <ysmc_w@163.com>
2023-07-31 22:30:38 +0800

    doc(Dropdown): refactor the dropdown document

--- src/BootstrapBlazor.Shared/Samples/Dropdowns.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Dropdowns.razor.cs
@@ -9,6 +9,140 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Dropdowns
 {
+    [NotNull]
+    private ConsoleLogger? Logger { get; set; }
+
+    private List<SelectedItem> Items { get; set; } = new List<SelectedItem> { };
+
+    /// <summary>
+    /// ShowMessage
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    private Task ShowMessage(SelectedItem e)
+    {
+        Logger.Log($"Dropdown Item Clicked: Value={e.Value} Text={e.Text}");
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Items = new List<SelectedItem>
+        {
+            new SelectedItem{ Text=Localizer["Item1"], Value="0"},
+            new SelectedItem{ Text=Localizer["Item2"], Value="1"},
+            new SelectedItem{ Text=Localizer["Item3"], Value="2"},
+        };
+
+        Foos = new List<SelectedItem>
+        {
+            new SelectedItem{ Text=Localizer["Item1"], Value="0"},
+            new SelectedItem{ Text=Localizer["Item2"], Value="1"},
+            new SelectedItem{ Text=Localizer["Item3"], Value="2"},
+        };
+
+        RadioDropDownItems = new List<SelectedItem>
+        {
+            new SelectedItem("1", Localizer["Item1"]) { Active = true },
+            new SelectedItem("2", Localizer["Item2"]),
+            new SelectedItem("3", Localizer["Item3"])
+        };
+
+        RadioItems = new List<SelectedItem>
+        {
+            new SelectedItem("1", Localizer["Item1"]) { Active = true },
+            new SelectedItem("2", Localizer["Item2"])
+        };
+
+        Items3 = new SelectedItem[]
+        {
+            new SelectedItem ("", Localizer["DropdownCascadeItem1"]),
+            new SelectedItem ("Beijing", Localizer["DropdownCascadeItem2"]) { Active = true },
+            new SelectedItem ("Shanghai", Localizer["DropdownCascadeItem3"]),
+            new SelectedItem ("Hangzhou", Localizer["DropdownCascadeItem4"])
+        };
+    }
+
+    private static List<SelectedItem> EmptyList => new();
+
+    private List<SelectedItem> Foos { get; set; } = new List<SelectedItem>();
+
+    /// <summary>
+    /// AddItem
+    /// </summary>
+    private void AddItem()
+    {
+        Foos.Add(new SelectedItem($"{Foos.Count}", $"{Localizer["City"]} {Foos.Count}"));
+    }
+
+    private List<SelectedItem> RadioItems { get; set; } = new List<SelectedItem>();
+
+    private List<SelectedItem> RadioDropDownItems { get; set; } = new List<SelectedItem>();
+
+    /// <summary>
+    /// OnRadioItemChanged
+    /// </summary>
+    /// <param name="values"></param>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    private Task OnRadioItemChanged(IEnumerable<SelectedItem> values, SelectedItem item)
+    {
+        RadioDropDownItems.Add(new SelectedItem($"{RadioDropDownItems.Count + 1}", $"{Localizer["City"]} {RadioDropDownItems.Count}"));
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// RemoveItem
+    /// </summary>
+    private void RemoveItem()
+    {
+        if (Foos.Any())
+        {
+            Foos.RemoveAt(0);
+        }
+    }
+
+    private IEnumerable<SelectedItem>? Items2 { get; set; }
+
+    private IEnumerable<SelectedItem>? Items3 = new SelectedItem[] { };
+
+    /// <summary>
+    /// 级联绑定菜单
+    /// </summary>
+    /// <param name="item"></param>
+    private async Task OnCascadeBindSelectClick(SelectedItem item)
+    {
+        // 模拟异步通讯切换线程
+        await Task.Delay(10);
+        if (item.Value == "Beijing")
+        {
+            Items2 = new SelectedItem[]
+            {
+                new SelectedItem("1",Localizer["DropdownCascadeItem5"]) { Active = true },
+                new SelectedItem("2",Localizer["DropdownCascadeItem6"]),
+            };
+        }
+        else if (item.Value == "Shanghai")
+        {
+            Items2 = new SelectedItem[]
+            {
+                new SelectedItem("1",Localizer["DropdownCascadeItem7"]),
+                new SelectedItem("2",Localizer["DropdownCascadeItem8"]) { Active = true } ,
+            };
+        }
+        else
+        {
+            Items2 = Enumerable.Empty<SelectedItem>();
+        }
+        StateHasChanged();
+    }
+
     /// <summary>
     /// GetAttributes
     /// </summary>
