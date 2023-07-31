f862bda70ab91ab74a88fcdcd828b6de423d8a35
zhangpeihang <948869991@qq.com>
2023-07-31 21:45:37 +0800

    doc: 重构 editor 文档

--- src/BootstrapBlazor.Shared/Samples/Editors.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Editors.razor.cs
@@ -9,31 +9,136 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public sealed partial class Editors
 {
+    [NotNull]
+    private Editor? Editor { get; set; }
+
+    private string? EditorValue { get; set; }
+
+    private bool ShowSubmitButton { get; set; }
+
+    private void ShowSubmit() => ShowSubmitButton = !ShowSubmitButton;
+
+    private string ButtonText => ShowSubmitButton ? "Hide SubmitButton" : "Show SubmitButton";
+
+    private string? ValueChangedValue { get; set; }
+
+    private List<EditorToolbarButton>? EditorPluginItems { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        EditorValue = "This is bind-Value demo";
+
+        ValueChangedValue = Localizer["EditorOnValueChangedInitValue"];
+
+        EditorPluginItems = new List<EditorToolbarButton>()
+        {
+            new EditorToolbarButton()
+            {
+                IconClass = "fa-solid fa-pencil",
+                ButtonName = "plugin1",
+                Tooltip = Localizer["ToolTip1"]
+            },
+            new EditorToolbarButton()
+            {
+                IconClass = "fa-solid fa-house",
+                ButtonName = "plugin2",
+                Tooltip = Localizer["ToolTip2"]
+            }
+        };
+    }
+
+    private void SetValue()
+    {
+        ValueChangedValue = Localizer["EditorOnValueChangedUpdateValue"];
+    }
+
+    private async Task<string?> PluginClick(string pluginItemName)
+    {
+        var ret = "";
+        if (pluginItemName == "plugin1")
+        {
+            var op = new SwalOption()
+            {
+                Title = Localizer["Swal1Title"],
+                Content = Localizer["Swal1Content"]
+            };
+            if (await SwalService.ShowModal(op))
+            {
+                ret = Localizer["Ret1"];
+            }
+        }
+        if (pluginItemName == "plugin2")
+        {
+            var op = new SwalOption()
+            {
+                Title = Localizer["Swal2Title"],
+                Content = Localizer["Swal2Content"]
+            };
+            if (await SwalService.ShowModal(op))
+            {
+                ret = Localizer["Ret2"];
+            }
+        }
+        return ret;
+    }
+
+    private List<object> ToolbarItems = new List<object>
+    {
+        new List<object>
+        {
+            "style", new List<string>()
+            {
+                "style"
+            }
+        },
+        new List<object>
+        {
+            "font", new List<string>()
+            {
+                "bold", "underline", "clear"
+            }
+        }
+    };
+
+    private async Task InsertHtmlAsync()
+    {
+        await Editor.DoMethodAsync("pasteHTML", $"<h1>{Localizer["DoMethodAsyncPasteHTML"]}</h1>");
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
 
-        new() {
+        new()
+        {
             Name = "Placeholder",
             Description = Localizer["Att1"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = Localizer["Att1DefaultValue"]!
         },
-        new() {
+        new()
+        {
             Name = "IsEditor",
             Description = Localizer["Att2"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = "ShowSubmit",
             Description = Localizer["AttShowSubmit"],
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "true"
         },
-        new() {
+        new()
+        {
             Name = "Height",
             Description = Localizer["Att3"],
             Type = "int",
