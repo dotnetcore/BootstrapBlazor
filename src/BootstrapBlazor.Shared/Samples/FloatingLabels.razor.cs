a5ee9a66939d69b965d521d8445b4c833a4e3b62
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2023-07-31 20:11:55 +0800

    doc(FloatingLabel): refactor the floating label document (#1791)
    * doc: 重构 floatinglabel 文档
    
    * doc: 更新 floatinglabel 文档
    
    * doc: 格式化代码
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor.Shared/Samples/FloatingLabels.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/FloatingLabels.razor.cs
@@ -5,48 +5,81 @@
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
-/// FloatingLabels
+/// FloatingLabel 组件示例代码
 /// </summary>
 public partial class FloatingLabels
 {
+    [NotNull]
+    private Foo? Model { get; set; }
+
+    [NotNull]
+    private Foo? BindValueModel { get; set; }
+
+    [NotNull]
+    private Foo? FormatStringModel { get; set; }
+
+    private byte[] ByteArray { get; set; } = new byte[] { 0x01, 0x12, 0x34, 0x56 };
+
+    private static string ByteArrayFormatter(byte[] source) => Convert.ToBase64String(source);
+
+    private static string DateTimeFormatter(DateTime source) => source.ToString("yyyy-MM-dd");
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Model = new Foo() { Name = Localizer["FloatingLabelsTestName"] };
+        BindValueModel = new Foo() { Name = Localizer["FloatingLabelsTestName"] };
+        FormatStringModel = new Foo() { Name = Localizer["FloatingLabelsTestName"] };
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new() {
+        new()
+        {
             Name = "ChildContent",
             Description = Localizer["FloatingLabelsChildContent"].Value,
             Type = "RenderFragment",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "ShowLabel",
             Description = Localizer["FloatingLabelsShowLabel"].Value,
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = nameof(FloatingLabel<string>.IsGroupBox),
             Description = Localizer["FloatingLabelsGroupBox"].Value,
             Type = "bool",
             ValueList = "true|false",
             DefaultValue = "false"
         },
-        new() {
+        new()
+        {
             Name = "DisplayText",
             Description = Localizer["FloatingLabelsDisplayText"].Value,
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "FormatString",
             Description = Localizer["FloatingLabelsFormatString"].Value,
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new() {
+        new()
+        {
             Name = "Formatter",
             Description = Localizer["FloatingLabelsFormatter"].Value,
             Type = "RenderFragment<TItem>",
