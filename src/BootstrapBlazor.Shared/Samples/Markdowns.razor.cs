a340cead866005ca32d4dc44b80147e50d78a17b
zhangpeihang <948869991@qq.com>
2023-07-31 09:41:44 +0800

    doc: 重构 markdown 文档

--- src/BootstrapBlazor.Shared/Samples/Markdowns.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Markdowns.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using System.Globalization;
+
 namespace BootstrapBlazor.Shared.Samples;
 
 /// <summary>
@@ -9,65 +11,114 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Markdowns
 {
+    private string? MarkdownString { get; set; }
+
+    private string? HtmlString { get; set; }
+
+    private string? Language { get; set; }
+
+    private string? AsyncValue { get; set; }
+
+    [NotNull]
+    private Markdown? MarkdownSetValue { get; set; }
+
+    [NotNull]
+    private Markdown? Markdown { get; set; }
+
+    private string JsString { get; set; } = @"```js
+                   console.log('test');
+                   ```";
+
+    [NotNull]
+    private Foo? Model { get; set; }
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    /// <returns></returns>
+    protected override void OnInitialized()
+    {
+        Language = CultureInfo.CurrentUICulture.Name;
+        MarkdownString = $"### {Localizer["MarkdownString"]}";
+
+        Model = new() { Name = "Name", Education = EnumEducation.Primary, DateTime = DateTime.Now };
+    }
+
+    private async Task GetAsyncString()
+    {
+        await Task.Delay(600);
+        AsyncValue = $"### {DateTime.Now}";
+        await MarkdownSetValue.SetValue(AsyncValue);
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new(){
+        new()
+        {
             Name = "Height",
             Description = Localizer["Att1"],
             Type = "int",
             ValueList = " — ",
             DefaultValue = "300"
         },
-        new(){
+        new()
+        {
             Name = "MinHeight",
             Description = Localizer["Att2"],
             Type = "int",
             ValueList = " — ",
             DefaultValue = "200"
         },
-        new(){
+        new()
+        {
             Name = "InitialEditType",
             Description = Localizer["Att3"],
             Type = "InitialEditType",
             ValueList = "Markdown/Wysiwyg",
             DefaultValue = "Markdown"
         },
-        new(){
+        new()
+        {
             Name = "PreviewStyle",
             Description = Localizer["Att4"],
             Type = "PreviewStyle",
             ValueList = "Tab/Vertical",
             DefaultValue = "Vertical"
         },
-        new(){
+        new()
+        {
             Name = "Language",
             Description = Localizer["Att5"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new(){
+        new()
+        {
             Name = "Placeholder",
             Description = Localizer["Att6"],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
         },
-        new(){
+        new()
+        {
             Name = "IsViewer",
             Description = Localizer["Att7"],
             Type = "bool",
             ValueList = " true/false ",
             DefaultValue = " false "
         },
-        new(){
+        new()
+        {
             Name = "IsDark",
             Description = Localizer["Att8"],
             Type = "bool",
             ValueList = " true/false ",
             DefaultValue = " false "
         },
-        new(){
+        new()
+        {
             Name = "EnableHighlight",
             Description = Localizer["Att9"],
             Type = "bool",
