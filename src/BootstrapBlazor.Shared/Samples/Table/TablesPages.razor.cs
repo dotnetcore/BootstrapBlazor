02c3aff7d7851fe3cd5e2f61a4ba7983760d5891
Argo-AscioTech <argo@live.ca>
2023-08-01 12:24:40 +0800

    doc(Table/Pagination): refact table pagination sample code

---
+++ src/BootstrapBlazor.Shared/Samples/Table/TablesPages.razor.cs
@@ -0,0 +1,63 @@
+﻿using global::System;
+using global::System.Collections.Generic;
+using global::System.Linq;
+using global::Microsoft.AspNetCore.Components;
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Shared;
+using BootstrapBlazor.Shared.Common;
+using BootstrapBlazor.Shared.Components;
+using BootstrapBlazor.Shared.Samples;
+using BootstrapBlazor.Shared.Services;
+using BootstrapBlazor.Shared.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+using Microsoft.JSInterop;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace BootstrapBlazor.Shared.Samples.Table;
+
+public partial class TablesPages
+{
+    /// <summary>
+    /// Foo 类为Demo测试用，如有需要请自行下载源码查阅
+    /// Foo class is used for Demo test, please download the source code if necessary
+    /// https://gitee.com/LongbowEnterprise/BootstrapBlazor/blob/main/src/BootstrapBlazor.Shared/Data/Foo.cs
+    /// </summary>
+    [NotNull]
+    private List<Foo>? Items { get; set; }
+    private static IEnumerable<int> PageItemsSource => new int[]
+    {
+        4,
+        10,
+        20,
+        40,
+        80,
+        100
+    };
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Items = Foo.GenerateFoo(LocalizerFoo);
+    }
+
+    private Task<QueryData<Foo>> OnQueryAsync(QueryPageOptions options)
+    {
+        IEnumerable<Foo> items = Items;
+        var total = items.Count();
+        items = items.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
+        return Task.FromResult(new QueryData<Foo>() { Items = items, TotalCount = total, IsSorted = true, IsFiltered = true, IsSearch = true });
+    }
+}
