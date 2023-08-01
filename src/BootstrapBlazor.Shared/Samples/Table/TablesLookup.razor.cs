4df42f1e6f6c09ee60cc2f91fd5c34cdefa44248
Argo-AscioTech <argo@live.ca>
2023-08-01 12:16:01 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Shared/Samples/Table/TablesLookup.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Table/TablesLookup.razor.cs
@@ -1,17 +1,14 @@
-﻿@inject IStringLocalizer<Foo> Localizer
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
 
-@if (Items != null)
+namespace BootstrapBlazor.Shared.Samples.Table;
+
+/// <summary>
+/// 外键数据源示例代码
+/// </summary>
+public partial class TablesLookup
 {
-    <Table TItem="Foo" Items="@Items.Take(3)">
-        <TableColumns>
-            <TableColumn @bind-Field="@context.DateTime" Width="180" />
-            <TableColumn @bind-Field="@context.Name" />
-            <TableColumn @bind-Field="@context.Complete" LookupServiceKey="Foo.Complete" />
-        </TableColumns>
-    </Table>
-}
-
-@code {
     /// <summary>
     /// Foo 类为Demo测试用，如有需要请自行下载源码查阅
     /// Foo class is used for Demo test, please download the source code if necessary
@@ -25,11 +22,10 @@
     /// </summary>
     protected override async Task OnInitializedAsync()
     {
-        await base.OnInitializedAsync();
-
         await Task.Delay(200);
+
         //获取随机数据
         //Get random data
-        Items = Foo.GenerateFoo(Localizer);
+        Items = Foo.GenerateFoo(LocalizerFoo);
     }
 }
