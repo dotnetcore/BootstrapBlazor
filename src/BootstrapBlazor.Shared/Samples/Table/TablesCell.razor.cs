ab5de5412fc888376b795ea6ff44c45316667d6f
zhangpeihang <948869991@qq.com>
2023-08-01 09:17:36 +0800

    doc: 重构 tablecell 文档

--- src/BootstrapBlazor.Shared/Samples/Table/TablesCell.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Table/TablesCell.razor.cs
@@ -9,6 +9,48 @@ namespace BootstrapBlazor.Shared.Samples.Table;
 /// </summary>
 public partial class TablesCell
 {
+    /// <summary>
+    /// Foo 类为Demo测试用，如有需要请自行下载源码查阅
+    /// Foo class is used for Demo test, please download the source code if necessary
+    /// https://gitee.com/LongbowEnterprise/BootstrapBlazor/blob/main/src/BootstrapBlazor.Shared/Data/Foo.cs
+    /// </summary>
+    [NotNull]
+    private List<Foo>? Items { get; set; }
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        //获取随机数据
+        //Get random data
+        Items = Foo.GenerateFoo(Localizer);
+    }
+
+    private static void OnCellRenderHandler(TableCellArgs args)
+    {
+        if (args.Row is Foo foo && args.ColumnName == "Name")
+        {
+            if (foo.Name == "张三 0002" || foo.Name == "Zhangsan 0002")
+            {
+                args.Colspan = 2;
+                args.Class = "cell-demo";
+                args.Value = $"{foo.Name} -- {foo.Address} -- {foo.Count}";
+            }
+        }
+    }
+
+    private async Task OnDoubleClickCellCallback(string columnName, object row, object value)
+    {
+        var displayName = Utility.GetDisplayName(typeof(Foo), columnName);
+        await ToastService.Show(new ToastOption()
+        {
+            Title = Localizer["TableCellOnDoubleClickCellToastTitle"],
+            Content = $"{Localizer["TableCellOnDoubleClickCellCurrentCellName"]}{displayName} {Localizer["TableCellOnDoubleClickCellCurrentValue"]}{value}"
+        });
+    }
+
     private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
         new()
