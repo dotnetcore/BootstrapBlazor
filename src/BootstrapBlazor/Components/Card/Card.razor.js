ad5ed58f6e7c5064bb07a46a1b6794d77502a975
Argo-AscioTech <argo@live.ca>
2023-08-03 12:59:07 +0800

    feat: support card collapse animation

---
+++ src/BootstrapBlazor/Components/Card/Card.razor.js
@@ -0,0 +1,25 @@
+﻿import Data from "../../modules/data.js?v=$version"
+import EventHandler from "../../modules/event-handler.js?v=$version"
+
+export function init(id, invoke, callback) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+
+    EventHandler.on(el, 'click', '.card-collapse-bar', e => {
+        invoke.invokeMethodAsync(callback)
+    })
+    Data.set(id, { el, invoke, callback })
+}
+
+export function dispose(id) {
+    const card = Data.get(id)
+    Data.remove(id)
+
+    if (card === null) {
+        return
+    }
+
+    EventHandler.off(card.el, 'click', '.card-collapse-bar')
+}
