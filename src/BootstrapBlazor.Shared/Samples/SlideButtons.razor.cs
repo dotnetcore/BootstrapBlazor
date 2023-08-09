5edc4b0a4828b0dc85927ae5319fbc2063496006
Argo-AscioTech <argo@live.ca>
2023-08-09 23:14:10 +0800

    doc: 更新文档

---
+++ src/BootstrapBlazor.Shared/Samples/SlideButtons.razor.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Samples;
+
+/// <summary>
+/// FloatButton 组件示例
+/// </summary>
+public partial class SlideButtons
+{
+    private Placement CurrentValue { get; set; }
+
+    /// <summary>
+    /// GetAttributes
+    /// </summary>
+    /// <returns></returns>
+    private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    {
+        new()
+        {
+            Name = nameof(FileViewer.Filename),
+            Description = "Excel/Word 文件路径或者URL",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
+    };
+}
