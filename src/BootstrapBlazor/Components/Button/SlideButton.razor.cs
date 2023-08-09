45d053fed71c1fb6b9633b955cbc9090fbdbe10a
Argo-AscioTech <argo@live.ca>
2023-08-09 23:13:57 +0800

    feat: 增加 SlideButton 组件

---
+++ src/BootstrapBlazor/Components/Button/SlideButton.razor.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// SlideButton 组件
+/// </summary>
+public partial class SlideButton
+{
+    /// <summary>
+    /// 按钮模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ButtonTemplate { get; set; }
+
+    /// <summary>
+    /// 展开部分模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? SlideTemplate { get; set; }
+
+    /// <summary>
+    /// 展开项集合
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public IEnumerable<SelectedItem>? Items { get; set; }
+
+    /// <summary>
+    /// 展开项 Header 文本
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public string? HeaderText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 按钮颜色
+    /// </summary>
+    [Parameter]
+    public Color Color { get; set; } = Color.Primary;
+
+    /// <summary>
+    /// 获得/设置 按钮颜色
+    /// </summary>
+    [Parameter]
+    public Placement Placement { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Size 大小
+    /// </summary>
+    [Parameter]
+    public Size Size { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示图标
+    /// </summary>
+    [Parameter]
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示文本
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否禁用 默认为 false
+    /// </summary>
+    [Parameter]
+    public bool IsDisabled { get; set; }
+
+    /// <summary>
+    /// 获得/设置 OnClick 事件
+    /// </summary>
+    [Parameter]
+    public EventCallback<SelectedItem> OnClick { get; set; }
+
+    /// <summary>
+    /// 获得 按钮样式集合
+    /// </summary>
+    /// <returns></returns>
+    private string? ClassName => CssBuilder.Default("btn")
+        .AddClass($"btn-{Color.ToDescriptionString()}", Color != Color.None)
+        .AddClass($"btn-{Size.ToDescriptionString()}", Size != Size.None)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// 获得 按钮 disabled 属性
+    /// </summary>
+    private string? Disabled => IsDisabled ? "disabled" : null;
+
+    private SelectedItem? _selectedItem;
+
+    private string ButtonId => $"{Id}_button";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        Items ??= Enumerable.Empty<SelectedItem>();
+    }
+
+    private string? GetItemClass(SelectedItem item) => CssBuilder.Default("slide-item")
+        .AddClass("active", _selectedItem?.Value == item.Value)
+        .Build();
+
+    private async Task OnClickItem(SelectedItem item)
+    {
+        _selectedItem = item;
+        if (OnClick.HasDelegate)
+        {
+            await OnClick.InvokeAsync(item);
+        }
+    }
+}
---
+++ src/BootstrapBlazor/Components/Button/SlideButton.razor.js
@@ -0,0 +1,52 @@
+﻿import Data from "../../modules/data.js?v=$version"
+import EventHandler from "../../modules/event-handler.js?v=$version"
+
+export function init(id) {
+    const el = document.getElementById(id)
+    if (el === null) {
+        return
+    }
+    const button = document.getElementById(`${id}_button`)
+    const list = el.querySelector('.slide-list')
+    const slide = { el, button, list }
+    Data.set(id, slide);
+
+    EventHandler.on(button, 'click', () => {
+        if (!list.classList.contains('show')) {
+            reset(el, button, list)
+        }
+        list.classList.toggle('show')
+    })
+}
+
+export function dispose(id) {
+    const slide = Data.get(id)
+    Data.remove(id)
+
+    if (slide) {
+        EventHandler.off(slide.button, 'click')
+    }
+}
+
+const reset = (el, button, list) => {
+    const placement = el.getAttribute('data-bb-placement') || 'auto'
+    const buttonHeight = button.offsetHeight
+    const buttonWidth = button.offsetWidth
+    const listHeight = list.offsetHeight
+    const listWidth = list.offsetWidth
+    const offset = 8
+    if (placement === 'top' || placement === 'top-start') {
+        list.setAttribute('style', `bottom: ${buttonHeight + offset}px; left: 0;`)
+    }
+    else if (placement === 'top-center') {
+        list.setAttribute('style', `bottom: ${buttonHeight + offset}px; left: ${(buttonWidth - listWidth) / 2}px;`)
+    }
+    else if (placement === 'top-end') {
+        list.setAttribute('style', `bottom: ${buttonHeight + offset}px; right: 0;`)
+    }
+    else if (placement === 'left') {
+        const width = button.offsetWidth
+        const listWidth = list.offsetWidth
+        list.setAttribute('style', `left: ${0 - width - listWidth - 8}px;`)
+    }
+}
