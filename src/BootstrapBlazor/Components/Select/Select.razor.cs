b179a1fc6f6e36110c0ec6be728d2368bac35120
Argo Zhang <argo@live.ca>
2023-08-30 00:27:47 +0800

    feat(Select): add DefaultVirtualizeItemText parameter (#2020)
    * feat: 虚拟化时支持绑定值
    
    * feat: 支持虚拟化给定默认值
    
    * test: 增加单元测试
    
    * refactor: 更新代码
    
    * test: 更新单元测试
    
    * chore: bump version 7.9.4-beta01

--- src/BootstrapBlazor/Components/Select/Select.razor.cs
+++ src/BootstrapBlazor/Components/Select/Select.razor.cs
@@ -151,6 +151,13 @@ public partial class Select<TValue> : ISelect
     [Parameter]
     public int OverscanCount { get; set; } = 4;
 
+    /// <summary>
+    /// 获得/设置 默认文本 <see cref="IsVirtualize"/> 时生效 默认 null
+    /// </summary>
+    /// <remarks>开启 <see cref="IsVirtualize"/> 并且通过 <see cref="OnQueryAsync"/> 提供数据源时，由于渲染时还未调用或者调用后数据集未包含 <see cref="DisplayBase{TValue}.Value"/> 选项值，此时使用 DefaultText 值渲染</remarks>
+    [Parameter]
+    public string? DefaultVirtualizeItemText { get; set; }
+
     [NotNull]
     private Virtualize<SelectedItem>? VirtualizeElement { get; set; }
 
@@ -254,13 +261,15 @@ public partial class Select<TValue> : ISelect
 
     private bool TryParseSelectItem(string value, [MaybeNullWhen(false)] out TValue result, out string? validationErrorMessage)
     {
-        SelectedItem = (VirtualItems ?? DataSource).FirstOrDefault(i => i.Value == value);
+        SelectedItem = (VirtualItems ?? DataSource).FirstOrDefault(i => i.Value == value) ?? GetVirtualizeItem();
 
         // support SelectedItem? type
         result = SelectedItem != null ? (TValue)(object)SelectedItem : default;
         validationErrorMessage = "";
         return SelectedItem != null;
     }
+
+    private SelectedItem? GetVirtualizeItem() => OnQueryAsync == null ? null : ValueType == typeof(SelectedItem) ? (SelectedItem)(object)Value : new SelectedItem(CurrentValueAsString, DefaultVirtualizeItemText ?? CurrentValueAsString);
 
     private void ResetSelectedItem()
     {
@@ -270,6 +279,7 @@ public partial class Select<TValue> : ISelect
         {
             DataSource.AddRange(Items);
             DataSource.AddRange(Children);
+
             if (VirtualItems != null)
             {
                 DataSource.AddRange(VirtualItems);
@@ -277,7 +287,8 @@ public partial class Select<TValue> : ISelect
 
             SelectedItem = DataSource.FirstOrDefault(i => i.Value.Equals(CurrentValueAsString, StringComparison))
                 ?? DataSource.FirstOrDefault(i => i.Active)
-                ?? DataSource.FirstOrDefault();
+                ?? DataSource.FirstOrDefault()
+                ?? GetVirtualizeItem();
 
             if (SelectedItem != null)
             {
@@ -365,14 +376,14 @@ public partial class Select<TValue> : ISelect
             item.Active = true;
             SelectedItem = item;
 
-            // 触发 StateHasChanged
-            CurrentValueAsString = item.Value;
-
             // 触发 SelectedItemChanged 事件
             if (OnSelectedItemChanged != null)
             {
                 await OnSelectedItemChanged(SelectedItem);
             }
+
+            // 触发 StateHasChanged
+            CurrentValueAsString = item.Value;
         }
     }
 
