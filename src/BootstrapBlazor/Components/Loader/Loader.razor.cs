0b55930ea307c6966b292b7dcb7f417337c88b9c
Old Li <lish@live.cn>
2023-08-30 11:21:40 +0800

    add loader component

---
+++ src/BootstrapBlazor/Components/Loader/Loader.razor.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Loader
+/// </summary>
+public partial class Loader
+{
+    /// <summary>
+    /// 文本内容
+    /// </summary>
+    [Parameter]
+    public string Text { get; set; } = "Loading ...";
+
+    /// <summary>
+    /// 是否重复播放
+    /// </summary>
+    [Parameter]
+    public bool IsRepeat { get; set; } = true;
+
+    /// <summary>
+    /// 重复动画之前的延迟(秒)
+    /// </summary>
+    [Parameter]
+    public double RepeatDelay { get; set; } = 0.75;
+
+    /// <summary>
+    /// 动画持续时间(秒)
+    /// </summary>
+    [Parameter]
+    public double Duration { get; set; } = 0.25;
+    private string? ClassString { get; set; }
+
+    /// <inheritdoc/>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        ClassString = CssBuilder.Default("loader-box").AddClassFromAttributes(AdditionalAttributes).Build();
+    }
+
+    /// <inheritdoc/>
+    protected override async Task InvokeInitAsync() => await InvokeVoidAsync("init", IsRepeat, RepeatDelay, Duration);
+}
---
+++ src/BootstrapBlazor/Components/Loader/Loader.razor.js
@@ -0,0 +1,46 @@
+﻿import { addLink, removeLink, addScript, removeScript } from '../../modules/utility.js?v=$version'
+
+const spcss = "./splitting/splitting.css";
+const spcellscss = "./splitting/splitting-cells.css";
+const spjs = "./splitting/splitting.min.js";
+const gsapjs = "./gsap/gsap.min.js";
+
+export async function init(isRepeat, repeatDelay, duration) {
+    await addLink(spcss)
+    await addLink(spcellscss)
+    await addScript(spjs)
+    await addScript(gsapjs)
+
+    Splitting();
+
+    const repeat = isRepeat ? -1 : 0;
+
+    var tl = gsap.timeline({ repeat: repeat, repeatDelay: repeatDelay });
+    tl
+        .from(".loader-flip .cell", {
+            scale: 0,
+            transformOrigin: "center",
+            x: "1.5rem",
+            duration: duration,
+            ease: "circ.out",
+            stagger: {
+                amount: 3,
+                from: "start"
+            }
+        })
+        .to(".loader-flip .cell",
+            {
+                scale: 0,
+                xPercent: -900,
+                duration: duration,
+                stagger: { amount: repeatDelay, from: "start" }
+            }, "+=0.5"
+        );
+}
+
+export async function dispose() {
+    await removeLink(spcss)
+    await removeLink(spcellscss)
+    await removeScript(spjs)
+    await removeScript(gsapjs)
+}
