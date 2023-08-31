6ba8939ed6d479d563eccdc69c50cf98cd331263
Argo-AscioTech <argo@live.ca>
2023-08-31 12:30:39 +0800

    feat: Text 参数默认读取资源文件

--- src/BootstrapBlazor/Components/Loader/Loader.razor.cs
+++ src/BootstrapBlazor/Components/Loader/Loader.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.Extensions.Localization;
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -13,7 +15,7 @@ public partial class Loader
     /// 文本内容
     /// </summary>
     [Parameter]
-    public string Text { get; set; } = "Loading ...";
+    public string? Text { get; set; }
 
     /// <summary>
     /// 数据数量
@@ -21,11 +23,21 @@ public partial class Loader
     [Parameter]
     public int Columns { get; set; } = 10;
 
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Loader>? Localizer { get; set; }
+
     private string? ClassString => CssBuilder.Default("bb-loader")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
 
+    /// <summary>
     /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
     {
+        base.OnParametersSet();
+
+        Text ??= Localizer[nameof(Text)];
     }
 }
