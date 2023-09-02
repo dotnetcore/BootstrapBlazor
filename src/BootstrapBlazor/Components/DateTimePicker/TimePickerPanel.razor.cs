33c9d510eb05cb1e86d10c932bed0898fbe4280c
Old Li <lish@live.cn>
2023-09-02 13:40:06 +0800

    add timepickerpanel component

---
+++ src/BootstrapBlazor/Components/DateTimePicker/TimePickerPanel.razor.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// TimePickerPanel 子组件
+/// </summary>
+public partial class TimePickerPanel
+{
+    /// <summary>
+    /// 获得/设置 样式
+    /// </summary>
+    private string? ClassString => CssBuilder.Default("bb-time-panel").AddClassFromAttributes(AdditionalAttributes).Build();
+}
