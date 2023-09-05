2bc1015684cc11146d04381eaadb30c66a0dd1ec
Argo-AscioTech <argo@live.ca>
2023-09-05 16:57:42 +0800

    refactor: 移除不使用的代码

--- src/Extensions/Components/BootstrapBlazor.Splitting/Components/Splitting/Splitting.razor.cs
+++ src/Extensions/Components/BootstrapBlazor.Splitting/Components/Splitting/Splitting.razor.cs
@@ -3,7 +3,6 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.Localization;
 
 namespace BootstrapBlazor.Components;
 
