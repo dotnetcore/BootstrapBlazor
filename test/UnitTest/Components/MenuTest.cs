8e867fa273d2e55eafcb86ac717895fb42abd007
Argo-AscioTech <argo@live.ca>
2023-09-07 18:35:27 +0800

    test: 更新单元测试

--- test/UnitTest/Components/MenuTest.cs
+++ test/UnitTest/Components/MenuTest.cs
@@ -169,6 +169,7 @@ public class MenuTest : BootstrapBlazorTestBase
         {
             pb.Add(a => a.IsAccordion, true);
         });
+        cut.WaitForState(() => cut.Markup.Contains("accordion"));
         Assert.Contains("accordion", cut.Markup);
     }
 
