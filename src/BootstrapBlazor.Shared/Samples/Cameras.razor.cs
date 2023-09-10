dd2deee8bb55817a49f9436b6f7cabbca3de8528
Argo-M2 <argo@live.ca>
2023-09-10 12:40:25 +0800

    Revert "wip: 临时测试代码"
    This reverts commit a5549f45a6cd40fe61791b72280cc75ccc9391e3.

--- src/BootstrapBlazor.Shared/Samples/Cameras.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Cameras.razor.cs
@@ -84,7 +84,6 @@ public sealed partial class Cameras
     {
         if (devices.Any())
         {
-            Devices.Add(new SelectedItem("", "Test"));
             Devices.AddRange(devices.Select(d => new SelectedItem(d.DeviceId, d.Label)));
             PlayDisabled = false;
         }
