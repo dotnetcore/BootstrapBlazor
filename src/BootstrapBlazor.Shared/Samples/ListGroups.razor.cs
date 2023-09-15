e2177000de9fe7c25c0ed73e212bc5ef263fefc1
Argo-AscioTech <argo@live.ca>
2023-09-15 15:33:56 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Shared/Samples/ListGroups.razor.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Shared.Samples;
+
+/// <summary>
+/// ListGroup 示例代码
+/// </summary>
+public partial class ListGroups
+{
+    [NotNull]
+    private List<Foo>? Items { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Foo>? LocalizerFoo { get; set; }
+
+    [NotNull]
+    private Foo? Value { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Items = Foo.GenerateFoo(LocalizerFoo);
+    }
+
+    private string? GetItemDisplayText(Foo item) => item.Name;
+}
