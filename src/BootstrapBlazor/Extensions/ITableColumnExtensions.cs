ac45a66da6abd65dfe74fc3f6ee21cec2383a9dd
Argo-AscioTech <argo@live.ca>
2023-09-16 23:29:39 +0800

    fix: 更新 Order 参数

--- src/BootstrapBlazor/Extensions/ITableColumnExtensions.cs
+++ src/BootstrapBlazor/Extensions/ITableColumnExtensions.cs
@@ -58,7 +58,8 @@ public static class IEditItemExtensions
         if (!string.IsNullOrEmpty(source.GroupName)) dest.GroupName = source.GroupName;
         if (source.GroupOrder != 0) dest.GroupOrder = source.GroupOrder;
         if (!string.IsNullOrEmpty(source.PlaceHolder)) dest.PlaceHolder = source.PlaceHolder;
-        if(source.Step != null) dest.Step = source.Step;
+        if (source.Step != null) dest.Step = source.Step;
+        if (source.Order != 0) dest.Order = source.Order;
 
         if (source is ITableColumn col)
         {
