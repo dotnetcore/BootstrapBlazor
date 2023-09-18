1ee64401067805068476ca62f8df004fa88fb480
Argo-AscioTech <argo@live.ca>
2023-09-18 14:29:12 +0800

    test: 增加 TimePicker 单元测试

---
+++ test/UnitTest/Components/TimePickerTest.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class TimePickerTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void TimePicker_Ok()
+    {
+        var cut = Context.RenderComponent<TimePicker>();
+        cut.MarkupMatches("""
+            <div class="bb-time-picker" diff:ignore>
+                <div class="bb-time-panel">
+                    <div class="bb-time-header"></div>
+                    <div class="bb-time-body"></div>
+                    <div class="bb-time-footer"></div>
+                </div>
+            </div>
+        """);
+    }
+
+    [Fact]
+    public void OnValueChanged_Ok()
+    {
+        var changed = false;
+        var cut = Context.RenderComponent<TimePicker>(pb =>
+        {
+            pb.Add(a => a.Value, new TimeSpan(10, 10, 10));
+            pb.Add(a => a.OnValueChanged, ts =>
+            {
+                changed = true;
+                return Task.CompletedTask;
+            });
+        });
+
+        var panel = cut.FindComponent<TimePickerPanel>();
+        cut.InvokeAsync(() => panel.Instance.SetValue(new TimeSpan(11, 11, 11)));
+        cut.WaitForState(() => changed);
+        Assert.Equal(new TimeSpan(11, 11, 11), cut.Instance.Value);
+    }
+}
