2fc599ddaa0953155b1b039b0f1d80c0b54d7c0a
Argo-AscioTech <argo@live.ca>
2023-09-18 15:12:27 +0800

    test: 增加单元测试

--- test/UnitTest/Components/TimePickerPanelTest.cs
+++ test/UnitTest/Components/TimePickerPanelTest.cs
@@ -68,7 +68,7 @@ public class TimePickerPanelTest : BootstrapBlazorTestBase
     {
         var cut = Context.RenderComponent<TimePickerPanel>(pb =>
         {
-            pb.Add(a => a.Value, new TimeSpan(00, 10, 10));
+            pb.Add(a => a.Value, new TimeSpan(10, 10, 10));
         });
 
         // click am
@@ -79,6 +79,20 @@ public class TimePickerPanelTest : BootstrapBlazorTestBase
         // click pm
         cut.InvokeAsync(() => span.Click());
         cut.WaitForAssertion(() => cut.Contains("btn btn-am active"));
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Value, new TimeSpan(14, 10, 10));
+        });
+        cut.InvokeAsync(() => span.Click());
+        cut.WaitForAssertion(() => cut.Contains("btn btn-am active"));
+        cut.InvokeAsync(() => span.Click());
+        cut.WaitForAssertion(() => cut.Contains("btn btn-pm active"));
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Value, new TimeSpan(00, 10, 10));
+        });
 
         // click button
         var button = cut.Find(".btn-pm");
@@ -103,5 +117,25 @@ public class TimePickerPanelTest : BootstrapBlazorTestBase
 
         // UI is 0
         Assert.Equal("00", text.TextContent);
+    }
+
+    [Fact]
+    public void SetTime()
+    {
+        var cut = Context.RenderComponent<TimePickerPanel>(pb =>
+        {
+            pb.Add(a => a.Value, new TimeSpan(10, 10, 10));
+        });
+
+        var picker = cut.Instance;
+        picker.SetTime(13, 0, 0);
+        Assert.Equal(new TimeSpan(13, 0, 0), picker.Value);
+
+        // 第一次改变为下午再设置 11 时 实际为 23
+        picker.SetTime(11, 10, 0);
+        Assert.Equal(new TimeSpan(23, 10, 0), picker.Value);
+
+        picker.SetTime(11, 10, 10);
+        Assert.Equal(new TimeSpan(23, 10, 10), picker.Value);
     }
 }
