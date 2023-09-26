e793711e5595bdf6ca193fb718e003ddf4f4c411
Argo-AscioTech <argo@live.ca>
2023-09-26 20:27:41 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Components/TimePickerSettingTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class TimePickerSettingTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Setting_Ok()
+    {
+        var cut = Context.RenderComponent<DateTimePicker<DateTime>>(pb =>
+        {
+            pb.Add(a => a.ViewMode, DatePickerViewMode.DateTime);
+            pb.AddChildContent<TimePickerSetting>(pb =>
+            {
+                pb.Add(a => a.ShowMinute, false);
+                pb.Add(a => a.ShowSecond, false);
+                pb.Add(a => a.ShowClockScale, true);
+                pb.Add(a => a.IsAutoSwitch, false);
+            });
+        });
+
+        var picker = cut.FindComponent<TimePickerPanel>();
+        Assert.False(picker.Instance.ShowMinute);
+        Assert.False(picker.Instance.ShowSecond);
+        Assert.False(picker.Instance.IsAutoSwitch);
+        Assert.True(picker.Instance.ShowClockScale);
+    }
+}
