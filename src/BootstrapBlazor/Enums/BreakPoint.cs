c6bb664e58eb2112a8698543a9021c8641e352e5
Argo Zhang <argo@live.ca>
2023-10-05 01:30:03 +0800

    feat(Responsive): add ExtraExtraSmall breakpoint (#2215)
    * feat: 增加超小屏幕阈值
    
    * feat: 增加 ExtraExtraSmall 枚举
    
    * doc: 增加注释
    
    * test: 更新单元测试
    
    * chore: 更新打包文件

--- src/BootstrapBlazor/Enums/BreakPoint.cs
+++ src/BootstrapBlazor/Enums/BreakPoint.cs
@@ -7,7 +7,7 @@ using System.Text.Json.Serialization;
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 
+/// BreakPoint 枚举
 /// </summary>
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum BreakPoint
@@ -18,32 +18,37 @@ public enum BreakPoint
     None,
 
     /// <summary>
-    /// 未设置
+    /// 超小屏幕 小于 375px
+    /// </summary>
+    ExtraExtraSmall,
+
+    /// <summary>
+    /// 超小屏幕 大于等于 375px
     /// </summary>
     ExtraSmall,
 
     /// <summary>
-    /// 小屏幕 576px
+    /// 小屏幕 大于等于 576px
     /// </summary>
     Small,
 
     /// <summary>
-    /// 中屏幕 768px
+    /// 中屏幕 大于等于 768px
     /// </summary>
     Medium,
 
     /// <summary>
-    /// 大屏幕 992px
+    /// 大屏幕 大于等于 992px
     /// </summary>
     Large,
 
     /// <summary>
-    /// 超大屏幕 1200px
+    /// 超大屏幕 大于等于 1200px
     /// </summary>
     ExtraLarge,
 
     /// <summary>
-    /// 超大屏幕 1400px
+    /// 超大屏幕 大于等于 1400px
     /// </summary>
     ExtraExtraLarge
 }
