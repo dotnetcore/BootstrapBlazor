df5718acc5c7a8093b46558487070d8665e3c9a1
Argo-AscioTech <argo@live.ca>
2023-10-06 18:03:11 +0800

    refactor: 精简代码

--- src/BootstrapBlazor/Components/Marquee/Marquee.razor.cs
+++ src/BootstrapBlazor/Components/Marquee/Marquee.razor.cs
@@ -12,46 +12,60 @@ public partial class Marquee
     /// <summary>
     /// 获得/设置 组件值 显示文本 默认 Empty
     /// </summary>
-    [Parameter] public string Text { get; set; } = string.Empty;
+    [Parameter]
+    public string? Text { get; set; }
 
     /// <summary>
-    /// 获得/设置 组件值 文本颜色 默认 black
+    /// 获得/设置 组件值 文本颜色 默认 #000
     /// <para>支持16进制和颜色名称</para>
     /// </summary>
-    [Parameter] public string Color { get; set; } = "black";
+    [Parameter]
+    public string Color { get; set; } = "#000";
 
     /// <summary>
-    /// 获得/设置 组件值 背景颜色 默认 white
+    /// 获得/设置 组件值 背景颜色 默认 #fff
     /// <para>支持16进制和颜色名称</para>
     /// </summary>
-    [Parameter] public string BackgroundColor { get; set; } = "white";
+    [Parameter]
+    public string BackgroundColor { get; set; } = "#fff";
 
     /// <summary>
     /// 获得/设置 组件值 文本大小 默认 72
     /// </summary>
-    [Parameter] public int FontSize { get; set; } = 72;
+    [Parameter]
+    public int FontSize { get; set; } = 72;
 
     /// <summary>
     /// 获得/设置 组件值 动画时间 默认 14s
     /// <para>值越小滚动越快</para>
     /// </summary>
-    [Parameter] public int Duration { get; set; } = 14;
+    [Parameter]
+    public int Duration { get; set; } = 14;
 
     /// <summary>
     /// 获得/设置 组件值 滚动方向 默认 LeftToRight
     /// </summary>
-    [Parameter] public MarqueeDirection DirecTion { get; set; } = MarqueeDirection.LeftToRight;
+    [Parameter]
+    public MarqueeDirection Direction { get; set; }
 
-    private MarqueeDirection lastDirecTion { get; set; }
-
-    private string? BoxClassString => CssBuilder.Default("bb-marquee-box")
+    private string? ClassString => CssBuilder.Default("marquee")
+        .AddClass("marquee-vertical", Direction == MarqueeDirection.TopToBottom || Direction == MarqueeDirection.BottomToTop)
         .AddClassFromAttributes(AdditionalAttributes)
-        .AddClass($"bb-marquee-box-vertical", DirecTion == MarqueeDirection.TopToBottom || DirecTion == MarqueeDirection.BottomToTop)
         .Build();
 
-    private string? TextClassString => CssBuilder.Default("bb-marquee-text")
-        .AddClass($"bb-marquee-text-left", DirecTion == MarqueeDirection.LeftToRight || DirecTion == MarqueeDirection.RightToLeft)
-        .AddClass($"bb-marquee-text-top", DirecTion == MarqueeDirection.TopToBottom || DirecTion == MarqueeDirection.BottomToTop)
+    private string? StyleString => CssBuilder.Default()
+        .AddClass($"background-color: {BackgroundColor};")
+        .AddClass($"color: {Color};")
+        .AddClass($"font-size: {FontSize}px;")
         .Build();
 
+    private string? TextClassString => CssBuilder.Default("marquee-text")
+        .AddClass($"marquee-text-left", Direction == MarqueeDirection.LeftToRight || Direction == MarqueeDirection.RightToLeft)
+        .AddClass($"marquee-text-top", Direction == MarqueeDirection.TopToBottom || Direction == MarqueeDirection.BottomToTop)
+        .Build();
+
+    private string? TextStyleString => CssBuilder.Default()
+        .AddClass($"animation-duration: {Duration}s;")
+        .AddClass($"animation-name: {Direction.ToDescriptionString()}")
+        .Build();
 }
