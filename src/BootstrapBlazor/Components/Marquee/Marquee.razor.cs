1c8c33622503e4bd7899c70af376122f75d5e209
Old Li <lish@live.cn>
2023-10-06 13:27:42 +0800

    doc: 添加注释

--- src/BootstrapBlazor/Components/Marquee/Marquee.razor.cs
+++ src/BootstrapBlazor/Components/Marquee/Marquee.razor.cs
@@ -4,35 +4,41 @@
 
 namespace BootstrapBlazor.Components;
 
+/// <summary>
+/// Marquee 字幕滚动组件
+/// </summary>
 public partial class Marquee
 {
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 显示文本 默认 Empty
     /// </summary>
     [Parameter] public string Text { get; set; } = string.Empty;
 
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 文本颜色 默认 black
+    /// <para>支持16进制和颜色名称</para>
     /// </summary>
     [Parameter] public string Color { get; set; } = "black";
 
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 背景颜色 默认 white
+    /// <para>支持16进制和颜色名称</para>
     /// </summary>
     [Parameter] public string BackgroundColor { get; set; } = "white";
 
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 文本大小 默认 72
     /// </summary>
     [Parameter] public int FontSize { get; set; } = 72;
 
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 动画时间 默认 14s
+    /// <para>值越小滚动越快</para>
     /// </summary>
-    [Parameter] public int Speed { get; set; } = 14;
+    [Parameter] public int Duration { get; set; } = 14;
 
     /// <summary>
-    /// 
+    /// 获得/设置 组件值 滚动方向 默认 LeftToRight
     /// </summary>
     [Parameter] public MarqueeDirecTion DirecTion { get; set; } = MarqueeDirecTion.LeftToRight;
 }
