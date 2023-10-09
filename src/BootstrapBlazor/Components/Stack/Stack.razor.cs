33db206f5060f2ed63b56d3eaa05e9cfd6bb32a7
zhangpeihang <948869991@qq.com>
2023-10-09 16:21:21 +0800

    feat: 添加 stack 组件

---
+++ src/BootstrapBlazor/Components/Stack/Stack.razor.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components;
+public partial class Stack
+{
+    private string? ClassValue => CssBuilder.Default()
+        .AddClass("stack-vertical", Orientation == Orientation.Vertical)
+        .AddClass("stack-horizontal", Orientation == Orientation.Horizontal)
+        .Build();
+
+    private string? StyleValue => new StyleBuilder()
+        .AddStyle("align-items", GetHorizontalAlignment(), () => Orientation == Orientation.Vertical)
+        .AddStyle("justify-content", GetVerticalAlignment(), () => Orientation == Orientation.Vertical)
+
+        .AddStyle("justify-content", GetHorizontalAlignment(), () => Orientation == Orientation.Horizontal)
+        .AddStyle("align-items", GetVerticalAlignment(), () => Orientation == Orientation.Horizontal)
+
+        .AddStyle("column-gap", $"{HorizontalGap}px", () => HorizontalGap.HasValue)
+        .AddStyle("row-gap", $"{VerticalGap}px", () => VerticalGap.HasValue)
+        .AddStyle("width", Width, () => !string.IsNullOrEmpty(Width))
+        .AddStyle("flex-wrap", "wrap", () => Wrap)
+
+        .Build();
+
+
+    /// <summary>
+    /// Gets or set the orientation of the stacked components. 
+    /// </summary>
+    [Parameter]
+    public Orientation Orientation { get; set; } = Orientation.Horizontal;
+
+    /// <summary>
+    /// The horizontal alignment of the components in the stack. 
+    /// </summary>
+    [Parameter]
+    public HorizontalAlignment HorizontalAlignment { get; set; } = HorizontalAlignment.Left;
+
+    /// <summary>
+    /// The vertical alignment of the components in the stack.
+    /// </summary>
+    [Parameter]
+    public VerticalAlignment VerticalAlignment { get; set; } = VerticalAlignment.Top;
+
+    /// <summary>
+    /// The width of the stack as a percentage string (default = 100%).
+    /// </summary>
+    [Parameter]
+    public string? Width { get; set; } = "100%";
+
+    /// <summary>
+    /// Gets or sets if the stack wraps.
+    /// </summary>
+    [Parameter]
+    public bool Wrap { get; set; } = false;
+
+    /// <summary>
+    /// Gets or sets the gap between horizontally stacked components (in pixels).
+    /// Default is 10 pixels.
+    /// </summary>
+    [Parameter]
+    public int? HorizontalGap { get; set; } = 10;
+
+    /// <summary>
+    /// Gets or sets the gap between vertically stacked components (in pixels).
+    /// Default is 10 pixels
+    /// </summary>
+    [Parameter]
+    public int? VerticalGap { get; set; } = 10;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string GetHorizontalAlignment()
+    {
+        return HorizontalAlignment switch
+        {
+            HorizontalAlignment.Left => "start",
+            HorizontalAlignment.Center => "center",
+            HorizontalAlignment.Right => "end",
+            _ => "start",
+        };
+    }
+
+    private string GetVerticalAlignment()
+    {
+        return VerticalAlignment switch
+        {
+            VerticalAlignment.Top => "start",
+            VerticalAlignment.Center => "center",
+            VerticalAlignment.Bottom => "end",
+            _ => "start",
+        };
+    }
+}
---
+++ src/BootstrapBlazor/Utils/StyleBuilder.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Utils;
+
+/// <summary>
+/// 
+/// </summary>
+public readonly struct StyleBuilder
+{
+    private readonly HashSet<string> _styles;
+    private readonly string? _userStyles;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StyleBuilder"/> class.
+    /// </summary>
+    public StyleBuilder()
+    {
+        _styles = new HashSet<string>();
+        _userStyles = null;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StyleBuilder"/> class.
+    /// </summary>
+    /// <param name="userStyles">The user styles to include at the end.</param>
+    public StyleBuilder(string? userStyles)
+    {
+        _styles = new HashSet<string>();
+        _userStyles = string.IsNullOrWhiteSpace(userStyles)
+                    ? null
+                    : string.Join("; ", userStyles.Split(';', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                                                  .Where(i => !string.IsNullOrWhiteSpace(i)));
+    }
+
+    /// <summary>
+    /// Adds a conditional in-line style to the builder with space separator and closing semicolon.
+    /// </summary>
+    /// <param name="style"></param>
+    public StyleBuilder AddStyle(string? style) => AddRaw(style);
+
+    /// <summary>
+    /// Adds a conditional in-line style to the builder with space separator and closing semicolon..
+    /// </summary>
+    /// <param name="prop"></param>
+    /// <param name="value">Style to add</param>
+    /// <returns>StyleBuilder</returns>
+    public StyleBuilder AddStyle(string prop, string? value) => AddRaw($"{prop}: {value}");
+
+    /// <summary>
+    /// Adds a conditional in-line style to the builder with space separator and closing semicolon..
+    /// </summary>
+    /// <param name="prop"></param>
+    /// <param name="value">Style to conditionally add.</param>
+    /// <param name="when">Condition in which the style is added.</param>
+    /// <returns>StyleBuilder</returns>
+    public StyleBuilder AddStyle(string prop, string? value, bool when = true) => when ? this.AddStyle(prop, value) : this;
+
+    /// <summary>
+    /// Adds a conditional in-line style to the builder with space separator and closing semicolon..
+    /// </summary>
+    /// <param name="prop"></param>
+    /// <param name="value">Style to conditionally add.</param>
+    /// <param name="when">Condition in which the style is added.</param>
+    /// <returns>StyleBuilder</returns>
+    public StyleBuilder AddStyle(string prop, string? value, Func<bool> when) => this.AddStyle(prop, value, when != null && when());
+
+    /// <summary>
+    /// Finalize the completed Style as a string.
+    /// </summary>
+    /// <returns>string</returns>
+    public string? Build()
+    {
+        var allStyles = string.IsNullOrWhiteSpace(_userStyles)
+                      ? _styles
+                      : _styles.Union(new[] { _userStyles });
+
+        if (!allStyles.Any())
+        {
+            return null;
+        }
+
+        return string.Concat(allStyles.Select(s => $"{s}; ")).TrimEnd();
+    }
+
+    /// <summary>
+    /// ToString should only and always call Build to finalize the rendered string.
+    /// </summary>
+    /// <returns></returns>
+    public override string? ToString() => Build();
+
+    /// <summary>
+    /// Adds a raw string to the builder that will be concatenated with the next style or value added to the builder.
+    /// </summary>
+    /// <param name="style"></param>
+    /// <returns>StyleBuilder</returns>
+    private StyleBuilder AddRaw(string? style)
+    {
+        if (!string.IsNullOrWhiteSpace(style))
+        {
+            _styles.Add(style.Trim().TrimEnd(';'));
+        }
+
+        return this;
+    }
+}
