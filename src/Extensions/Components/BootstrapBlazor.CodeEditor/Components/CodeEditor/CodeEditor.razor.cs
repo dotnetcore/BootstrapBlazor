ce128c3216c160750e2fb3cc2a937e822ff963bd
zhangpeihang <948869991@qq.com>
2023-10-09 12:52:28 +0800

    feat: 添加 codeeditor 组件

---
+++ src/Extensions/Components/BootstrapBlazor.CodeEditor/Components/CodeEditor/CodeEditor.razor.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class CodeEditor
+    {
+        private const string MONACO_VS_PATH = "./_content/BootstrapBlazor.CodeEditor/monaco-editor/min/vs";
+
+        /// <summary>
+        /// Language used by the editor: csharp, javascript, ...
+        /// </summary>
+        [Parameter]
+        public string Language { get; set; } = "csharp";
+
+        /// <summary>
+        /// Theme of the editor (Light or Dark).
+        /// </summary>
+        [Parameter]
+        public bool IsDarkMode { get; set; } = false;
+
+        /// <summary>
+        /// Gets or sets the value of the input. This should be used with two-way binding.
+        /// </summary>
+        [Parameter]
+        public string? Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets a callback that updates the bound value.
+        /// </summary>
+        [Parameter]
+        public EventCallback<string?> ValueChanged { get; set; }
+
+        /// <summary>
+        /// Gets or sets a callback that updates the bound value.
+        /// </summary>
+        [Parameter]
+        public Func<string?, Task>? OnValueChanged { get; set; }
+
+        /// <summary />
+        protected override async Task OnParametersSetAsync()
+        {
+            await InvokeVoidAsync(
+                "monacoSetOptions",
+                Id,
+                new { Value, Theme = GetTheme(IsDarkMode), Language });
+        }
+
+        /// <summary />
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender)
+            {
+                var options = new
+                {
+                    Value,
+                    Language,
+                    Theme = GetTheme(IsDarkMode),
+                    Path = MONACO_VS_PATH,
+                    LineNumbers = true,
+                    ReadOnly = false,
+                };
+                await InvokeVoidAsync("init", Id, Interop, options);
+            }
+        }
+
+        /// <summary />
+        private static string GetTheme(bool isDarkMode)
+        {
+            return isDarkMode ? "vs-dark" : "light";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task Focus() => await InvokeVoidAsync("focus");
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task Resize() => await InvokeVoidAsync("resize");
+
+        /// <summary />
+        [JSInvokable]
+        public async Task UpdateValueAsync(string value)
+        {
+            Value = value;
+            if (ValueChanged.HasDelegate)
+            {
+                await ValueChanged.InvokeAsync(Value);
+            }
+            if (OnValueChanged != null)
+            {
+                await OnValueChanged(Value);
+            }
+        }
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.CodeEditor/Components/CodeEditor/CodeEditor.razor.js
@@ -0,0 +1,62 @@
+﻿import Data from '../../../BootstrapBlazor/modules/data.js'
+
+export function init(id, interop, options) {
+    // require is provided by loader.min.js.
+    require.config({
+        paths: { 'vs': options.path }
+    });
+
+    require(["vs/editor/editor.main"], () => {
+        var container = document.getElementById(id);
+        var body = container.querySelector(".code-editor-body");
+
+        // Hide the Progress Ring
+        monaco.editor.onDidCreateEditor((e) => {
+            var progress = container.querySelector(".spinner");
+            if (progress && progress.style) {
+                progress.style.display = "none";
+            }
+        });
+
+        const editor = {}
+
+        // Create the Monaco Editor
+        editor.editor = monaco.editor.create(body, {
+            ariaLabel: "online code editor",
+            value: options.value,
+            language: options.language,
+            theme: options.theme,
+            lineNumbers: options.lineNumbers ? "on" : "off",
+            readOnly: options.readOnly,
+        });
+
+        // Catch when the editor lost the focus (didType to immediate)
+        editor.editor.onDidBlurEditorText((e) => {
+            var code = editor.editor.getValue();
+            interop.invokeMethodAsync("UpdateValueAsync", code);
+        });
+
+        editor.interop = interop;
+
+        Data.set(id, editor)
+
+        editor.editor.layout();
+        window.editor = editor.editor;   // To debug
+    });
+}
+
+// Update the editor options
+export function monacoSetOptions(id, options) {
+    var editor = Data.get(id);
+    if (editor) {
+        editor.editor.setValue(options.value);
+        editor.editor.updateOptions({
+            language: options.language,
+            theme: options.theme
+        });
+    }
+}
+
+export function dispose(id) {
+    Data.remove(id)
+}
