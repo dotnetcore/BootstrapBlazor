0984671af2ab815a7455972bb475dad31e87af4a
Argo-AscioTech <argo@live.ca>
2023-10-10 16:07:53 +0800

    refactor: 更改 ViewMode 代码

--- src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
+++ src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
@@ -83,8 +83,8 @@ public partial class DateTimeRange
     /// <summary>
     /// 获得/设置 组件显示模式 默认为显示年月日模式
     /// </summary>
-    //[Parameter]
-    private DatePickerViewMode ViewMode { get; set; } = DatePickerViewMode.Date;
+    [Parameter]
+    public DatePickerViewMode ViewMode { get; set; } = DatePickerViewMode.Date;
 
     /// <summary>
     /// 获得/设置 今天按钮文字
