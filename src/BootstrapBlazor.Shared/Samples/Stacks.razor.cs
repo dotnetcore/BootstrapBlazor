07e1fde52e4b4b122568d2097970d65eee039394
Argo-AscioTech <argo@live.ca>
2023-10-13 13:52:53 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Shared/Samples/Stacks.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Stacks.razor.cs
@@ -9,11 +9,27 @@ namespace BootstrapBlazor.Shared.Samples;
 /// </summary>
 public partial class Stacks
 {
-    private bool IsRow { get; set; }
+    private bool IsRow => ShowModeString == "true";
 
     private StackJustifyContent Justify { get; set; }
 
     private StackAlignItems AlignItems { get; set; }
+
+    private StackJustifyContent JustifySelf { get; set; }
+
+    private StackAlignItems AlignSelf { get; set; }
+
+    private bool IsWrap { get; set; }
+
+    private bool IsReverse { get; set; }
+
+    private List<SelectedItem> ShowMode => new List<SelectedItem>()
+    {
+        new("true", Localizer["RowMode"]),
+        new("false", Localizer["ColumnMode"])
+    };
+
+    private string ShowModeString { get; set; } = "true";
 
     private IEnumerable<AttributeItem> GetAttributeItems() => new List<AttributeItem>()
     {
