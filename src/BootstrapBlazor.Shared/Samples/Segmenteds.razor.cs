d12cbe7d3c413dfb96e252e10af5037d7e96f59d
zhangpeihang <948869991@qq.com>
2023-10-17 22:53:20 +0800

    doc: 更新文档

--- src/BootstrapBlazor.Shared/Samples/Segmenteds.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/Segmenteds.razor.cs
@@ -19,16 +19,44 @@ public partial class Segmenteds
 
     private List<SegmentedItem> Items { get; set; } = new List<SegmentedItem>()
     {
-        new SegmentedItem(){ Value = "zhangsan", Text = "张三", Active = true },
-        new SegmentedItem(){ Value = "lisi", Text = "李四", Active = false},
-        new SegmentedItem(){ Value = "wangwu", Text = "王五", Active = false },
-        new SegmentedItem(){ Value = "xiaoming", Text = "小明", Active = false},
+        new SegmentedItem(){ Value = "Daily", Text = "Daily" },
+        new SegmentedItem(){ Value = "Weekly", Text = "Weekly" },
+        new SegmentedItem(){ Value = "Monthly", Text = "Monthly" },
+        new SegmentedItem(){ Value = "Quarterly", Text = "Quarterly" },
+        new SegmentedItem(){ Value = "Yearly", Text = "Yearly" }
     };
 
-    private string? Value { get; set; } = "lisi";
+    private List<SegmentedItem> LongTextItems { get; set; } = new List<SegmentedItem>()
+    {
+        new SegmentedItem(){ Value = "123", Text = "123" },
+        new SegmentedItem(){ Value = "456", Text = "456" },
+        new SegmentedItem(){ Value = "longtext-longtext-longtext-longtext", Text = "longtext-longtext-longtext-longtext" }
+    };
+
+    private List<SegmentedItem> DisableItems { get; set; } = new List<SegmentedItem>()
+    {
+        new SegmentedItem(){ Value = "123", Text = "123" },
+        new SegmentedItem(){ Value = "456", Text = "456", IsDisabled = true },
+        new SegmentedItem(){ Value = "789", Text = "789" },
+    };
+
+    private List<SegmentedItem> IconItems { get; set; } = new List<SegmentedItem>()
+    {
+        new SegmentedItem(){ Value = "list", Text = "List", Icon = "fas fa-bars" },
+        new SegmentedItem(){ Value = "chart", Text = "Chart", Icon = "fas fa-chart-column" }
+    };
+
+    private string? Value { get; set; }
+
+    private string? Value1 { get; set; } = "Daily";
+
+    private string? BlockValue { get; set; }
+
+    private string? DisableValue { get; set; }
 
     private void ValueChanged(string item)
     {
+        Value1 = item;
         ConsoleLogger?.Log(item);
     }
 }
