dca9272ef0fd81bc934d71fd88929c7355d9e531
Argo-AscioTech <argo@live.ca>
2023-10-31 13:27:45 +0800

    feat: 移除部分方法准备新开分支继续完善

--- src/BootstrapBlazor/wwwroot/modules/utility.js
+++ src/BootstrapBlazor/wwwroot/modules/utility.js
@@ -501,53 +501,6 @@ export function openUrl(url, target = '_blank', features = null) {
     window.open(url, target, features);
 }
 
-export function getElementProperties(id, tag) {
-    if (tag === null || tag === undefined || tag === "" || tag.trim() === "") {
-        return "wrong tag";
-    }
-
-    var el = document.getElementById(id);
-    if (el) {
-        try {
-            let tags = tag.split('.');
-            let tagsCopy = JSON.parse(JSON.stringify(tags));
-            var object = el;
-            tagsCopy.map(() => {
-                object = object[tags[0]];
-                tags.shift();
-            })
-            return object;
-        } catch (e) {
-            console.warn(e.message);
-            return e.message;
-        }
-    }
-}
-
-export function getCSSValue(id, propertyName) {
-    try {
-        const element = document.getElementById(id);
-        if (element) {
-            var computedStyle = getComputedStyle(element);
-            return computedStyle.getPropertyValue(propertyName);
-        }
-    } catch (e) {
-        console.error(e.message);
-    }
-}
-
-export function getElementRect(id) {
-    try {
-        var element = document.getElementById(id);
-        if (element) {
-            var rect = element.getBoundingClientRect();
-            return rect;
-        }
-    } catch (e) {
-        console.error(e.message);
-    }
-}
-
 export function runEval(code) {
     try {
         return eval(code);
@@ -566,39 +519,6 @@ export function runFunction(code, arg) {
     catch (e) {
         console.warn(e.message);
         return e.message;
-    }
-}
-
-export function changeMeta(isAdd, type, rel, href) {
-    try {
-        var head = document.getElementsByTagName('head')[0];
-        var links = head.getElementsByTagName(type);
-
-        // 检查是否存在相同项
-        for (var i = 0; i < links.length; i++) {
-            if (links[i].rel === rel && links[i].href === href) {
-                // 是否执行移除操作
-                if (!isAdd) {
-                    head.removeChild(links[i])
-                }
-                return true;
-            }
-        }
-
-        // 是否执行添加操作
-        if (isAdd) {
-            // 创建并添加新项
-            var link = document.createElement(type);
-            link.rel = rel;
-            link.href = href;
-            head.appendChild(link);
-            return true;
-        } else {
-            return false;
-        }
-
-    } catch (e) {
-        return false;
     }
 }
 
