ddcfba5123597ac854ad9c707606c24915025014
Old Li <33386249+azlis@users.noreply.github.com>
2023-10-31 14:03:15 +0800

    feat(JSRuntime): Added JSRuntime extension method support (#2232)
    * refactor: addLink 增加rel参数，默认值为stylesheet
    
    * feat: 新增runJSWithFunction方法，用于全局作用域内动态执行js代码
    
    * RunEval方法重命名为RunJSWithEval，新增RunJSWithFunction方法用于全局作用域
    
    * doc: 更新方法注释
    
    * feat: 导入utility工具类
    
    * feat: 实现C#调用方法
    
    * doc: 添加方法注释
    
    * refactor: JSRuntimeEventHandler重命名为BootstrapBlazorJSHelper
    
    * refactor: 默认导入整个utility模块
    
    * feat: 新增doAddLinkWithRel方法
    
    * doc: 添加文档
    
    * refactor: Mark IJSRuntimeEventHandler as an outdated interface
    
    * doc: 添加方法弃用标识
    
    * doc: 新增Utility菜单
    
    * doc: update RunJSWithEval demo
    
    * feat: 新增doAlert和doPrompt方法
    
    * refactor: 捕获动态执行js异常结果
    
    * doc: add js alert demo
    
    * doc: add js prompt demo
    
    * feat: 支持运行 JavaScript 文件
    
    * doc(JSHelpers): update demo
    
    * feat(JSHelpers): support console function
    
    * doc(JSHelper); 更新注释
    
    * refactor(JSHelpers); 重构runJSFile方法
    
    * feat(JSHelpers): 重构代码
    
    * doc(JSHelpers): update Localizer
    
    * doc: update demo
    
    * refactor: 重构代码
    
    * feat: 添加head标签元素类型枚举
    
    * refactor: 重构changeMeta方法
    
    * refactor: 添加ImportModuleAsync方法
    
    * refactor: 重构addEventListener方法
    
    * feat: 添加GetProperties方法
    
    * refactor: doConsole 添加 try
    
    * doc: update JSConsole demo
    
    * refactor: 重构代码
    
    * refactor: 重构代码
    
    * refactor: 重构代码
    
    * feat: add runEval function
    
    * doc: update demo
    
    * doc: update Localizer
    
    * refatory: update demo
    
    * feat: add runFunc
    
    * refator: 重构代码
    
    * refator: 重构代码
    
    * refator: 重构代码
    
    * doc: 重写waterfall demo
    
    * doc: 优化瀑布流触发间隔
    
    * refactor: 重构事件防抖
    
    * refactor: 合并拓展类代码
    
    * doc: add RunFunction demo
    
    * doc: add GetIsMobileDevice demo
    
    * doc: add OpenBlankUrldemo
    
    * doc: 添加方法注释
    
    * chore: 添加 region 注释
    
    * doc: 添加方法列表说明
    
    * doc; add ClipboardService demo
    
    * doc: update docs.json
    
    * refactor: 移除新语法
    
    * refactor: 精简示例
    
    * doc: 更新示例路由
    
    * refactor: 更改 GetModule 为 GetUtility
    
    * refactor: 重构扩展方法
    
    * doc: 补充segmented本地化资源
    
    * refactor: 重构 GetUrl 方法
    
    * doc: segmented本地化资源中文描述错误
    
    * refactor: 精简代码
    
    * refactor: 重构代码更新示例
    
    * doc: 更新方法注释
    
    * refactor: 更新代码
    
    * doc: 更新示例
    
    * doc: 更新资源文件
    
    * doc: 更新示例
    
    * doc: 格式化文档
    
    * refactor: 更新 Dispose 方法代码
    
    * doc: 更新示例和方法注释
    
    * doc: 更新样式
    
    * feat: 移除部分方法准备新开分支继续完善
    
    * doc: 更新示例文档
    
    * doc: 更新注释
    
    * test: 增加单元测试
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/BootstrapBlazor/Extensions/BootstrapBlazorServiceCollectionExtensions.cs
+++ src/BootstrapBlazor/Extensions/BootstrapBlazorServiceCollectionExtensions.cs
@@ -39,7 +39,6 @@ public static class BootstrapBlazorServiceCollectionExtensions
 
         services.TryAddTransient<ITableExcelExport, DefaultExcelExport>();
         services.TryAddTransient<ITablePdfExport, DefaultPdfExport>();
-        services.TryAddTransient<IJSRuntimeEventHandler, JSRuntimeEventHandler>();
 
         services.TryAddScoped(typeof(IDataService<>), typeof(NullDataService<>));
         services.TryAddScoped<IIPLocatorProvider, DefaultIPLocatorProvider>();
