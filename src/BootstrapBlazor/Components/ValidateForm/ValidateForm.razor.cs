cfe3c75ccd8d8800a7c8a2457d28baadb9fa8ac3
Argo-AscioTech <argo@live.ca>
2023-11-02 17:57:16 +0800

    chore: 更新单词拼写错误

--- src/BootstrapBlazor/Components/ValidateForm/ValidateForm.razor.cs
+++ src/BootstrapBlazor/Components/ValidateForm/ValidateForm.razor.cs
@@ -556,7 +556,7 @@ public partial class ValidateForm
     /// 获取 当前表单值改变的属性集合
     /// </summary>
     /// <returns></returns>
-    [Obsolete("已过期，单词拼写错误，请使用，Please use ValueChangedFields instead. wrong typo")]
+    [Obsolete("已过期，单词拼写错误，请使用 ValueChangedFields，Please use ValueChangedFields instead. wrong typo")]
     [ExcludeFromCodeCoverage]
     public ConcurrentDictionary<FieldIdentifier, object?> ValueChagnedFields { get; } = new();
 }
