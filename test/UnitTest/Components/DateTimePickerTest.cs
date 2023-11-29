752520df01f7423070693bad911b9d871b52c784
Alex chow <zhouchuanglin@gmail.com>
2023-11-30 00:14:53 +0100

    feat: <TimePickerBody> 添加设置是否显示秒 #2449

--- test/UnitTest/Components/DateTimePickerTest.cs
+++ test/UnitTest/Components/DateTimePickerTest.cs
@@ -610,6 +610,43 @@ public class DateTimePickerTest : BootstrapBlazorTestBase
         Assert.True(res);
         Assert.True(value);
     }
+
+    [Fact]
+    public void HasSeconds_Ok()
+    {
+        var res = false;
+        var value = false;
+        var cut = Context.RenderComponent<TimePickerBody>(builder =>
+        {
+            builder.Add(a => a.Value, TimeSpan.FromDays(1));
+            builder.Add(a => a.ValueChanged, EventCallback.Factory.Create<TimeSpan>(this, t =>
+            {
+                value = true;
+            }));
+        });
+
+        var ele = cut.Find(".time-panel-content .has-seconds");
+        Assert.NotNull(ele); 
+    }
+
+    [Fact]
+    public void HaveNotSeconds_Ok()
+    {
+        var res = false;
+        var value = false;
+        var cut = Context.RenderComponent<TimePickerBody>(builder =>
+        {
+            builder.Add(a => a.Value, TimeSpan.FromDays(1));
+            builder.Add(a => a.ValueChanged, EventCallback.Factory.Create<TimeSpan>(this, t =>
+            {
+                value = true;
+            }));
+            builder.Add(a => a.HasSeconds, false);
+        });
+
+        var ele = cut.Find(".time-panel-content .havenot-seconds");
+        Assert.NotNull(ele); 
+    }
     #endregion
 
     #region TimeCell
