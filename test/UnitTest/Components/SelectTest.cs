e563749c9ac6243c91ada95c6c8b79a661afa00c
Alex chow <zhouchuanglin@gmail.com>
2023-12-01 02:01:46 +0100

    feat: <Select> 选项添加 IsMarkupString 特性支持多式样显示 #2459

--- test/UnitTest/Components/SelectTest.cs
+++ test/UnitTest/Components/SelectTest.cs
@@ -784,4 +784,20 @@ public class SelectTest : BootstrapBlazorTestBase
         value = "1";
         mi?.Invoke(select, new object?[] { value, result, msg });
     }
+
+    [Fact]
+    public void IsMarkupString_Ok()
+    {
+        var cut = Context.RenderComponent<Select<string>>(pb =>
+        {
+            pb.Add(a => a.Items, new SelectedItem[]
+            {
+                new SelectedItem("1", "<div>Test1</div>"),
+                new SelectedItem("2", "<div>Test2</div>")
+            });
+            pb.Add(a => a.Value, "2");
+            pb.Add(a => a.IsMarkupString, true); 
+        });
+        Assert.Contains("<div>Test1</div>", cut.Markup); 
+    }
 }
