2af3c866da7cf8212c4b491b72f10796407ca5b1
Argo Zhang <argo@live.ca>
2023-12-07 14:30:55 +0800

    feat(Utility): add Copy extension method (#2507)
    * refactor: 增加 Copy 扩展方法
    
    * chore: bump version 8.0.5-beta03
    
    * test: 增加单元测试
    
    * test: 增加静态关键字

---
+++ test/UnitTest/Extensions/DirectoryInfoExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Extensions;
+
+public class DirectoryInfoExtensionsTest
+{
+    [Fact]
+    public void Copy_Ok()
+    {
+        var rootDir = Path.Combine(AppContext.BaseDirectory, "test");
+        if (!Directory.Exists(rootDir))
+        {
+            Directory.CreateDirectory(rootDir);
+        }
+
+
+        // 创建 SourceDir
+        var sourceDir = CreateDir(Path.Combine(rootDir, "test1"));
+        // 创建临时测试目录
+        CreateDir(Path.Combine(sourceDir, "test"));
+        // 创建临时测试文件
+        using var file = File.OpenWrite(Path.Combine(sourceDir, "test.log"));
+        file.Close();
+
+        var destDir = Path.Combine(rootDir, "test2");
+        if (Directory.Exists(destDir))
+        {
+            Directory.Delete(destDir, true);
+        }
+
+        var sourceDirInfo = new DirectoryInfo(sourceDir);
+        sourceDirInfo.Copy(destDir);
+        Assert.True(Directory.Exists(destDir));
+    }
+
+    private static string CreateDir(string dirName)
+    {
+        if (!Directory.Exists(dirName))
+        {
+            Directory.CreateDirectory(dirName);
+        }
+        return dirName;
+    }
+}
