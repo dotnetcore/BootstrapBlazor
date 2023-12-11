ffd00c1148f54e648f773b7c8a0c23f84c1f1f91
Argo-AscioTech <argo@live.ca>
2023-12-11 13:12:29 +0800

    refactor: 增加 PdfStreamAsync 方法

--- src/Extensions/Components/BootstrapBlazor.Html2Pdf/Services/DefaultPdfService.cs
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/Services/DefaultPdfService.cs
@@ -26,4 +26,20 @@ class DefaultPdfService : IHtml2Pdf
         var content = await page.GetContentAsync();
         return await page.PdfDataAsync();
     }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public async Task<Stream> PdfStreamAsync(string url)
+    {
+        using var browserFetcher = new BrowserFetcher();
+        await browserFetcher.DownloadAsync();
+
+        await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions() { Headless = true });
+        await using var page = await browser.NewPageAsync();
+        await page.GoToAsync(url);
+
+        var content = await page.GetContentAsync();
+        return await page.PdfStreamAsync();
+    }
 }
