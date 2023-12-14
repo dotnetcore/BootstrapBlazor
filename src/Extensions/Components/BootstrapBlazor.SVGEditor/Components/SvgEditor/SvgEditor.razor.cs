e837c16223357bcdefe386c4aa1b7a196329b948
zhangpeihang <948869991@qq.com>
2023-12-14 11:58:40 +0800

    feat: 添加 svgeditor 组件

---
+++ src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components.Components.SvgEditor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class SvgEditor
+    {
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.js
@@ -0,0 +1,18 @@
+﻿import Editor from "../../editor/Editor.js"
+import { addLink } from '../../../BootstrapBlazor/modules/utility.js'
+import Data from '../../../BootstrapBlazor/modules/data.js'
+
+export async function init() {
+    await addLink("./_content/BootstrapBlazor.SvgEditor/editor/svgedit.css")
+
+    /* for available options see the file `docs/tutorials/ConfigOptions.md */
+    const svgEditor = new Editor(document.getElementById('container'))
+    svgEditor.setConfig({
+        allowInitialUserOverride: true,
+        imgPath: "./_content/BootstrapBlazor.SvgEditor/editor/images/",
+        extensions: [],
+        noDefaultExtensions: false,
+        userExtensions: [/* { pathName: '/packages/react-test/dist/react-test.js' } */]
+    })
+    svgEditor.init()
+}
