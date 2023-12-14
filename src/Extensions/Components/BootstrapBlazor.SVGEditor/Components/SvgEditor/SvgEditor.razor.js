12aa2b06fe5bff000303768f8155ae195466bed5
zhangpeihang <948869991@qq.com>
2023-12-14 13:57:24 +0800

    feat: 调整样式

--- src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.js
+++ src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.js
@@ -6,10 +6,13 @@ export async function init() {
     await addLink("./_content/BootstrapBlazor.SvgEditor/editor/svgedit.css")
 
     /* for available options see the file `docs/tutorials/ConfigOptions.md */
-    const svgEditor = new Editor(document.getElementById('container'))
+    const svgEditor = new Editor(document.querySelector('.svg-editor'))
     svgEditor.setConfig({
+        lang: "zh-CN",
+        canvas_expansion: 20,
         allowInitialUserOverride: true,
         imgPath: "./_content/BootstrapBlazor.SvgEditor/editor/images/",
+        showGrid: true,
         extensions: [],
         noDefaultExtensions: false,
         userExtensions: [/* { pathName: '/packages/react-test/dist/react-test.js' } */]
