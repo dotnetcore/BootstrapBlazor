f93fa3865c2dd2e3d905e0efbce17b31436b1ecd
az <lish@live.cn>
2023-12-16 16:57:23 +0800

    重构代码

--- src/BootstrapBlazor.Server/Components/Samples/Practices/Waterfall.razor.js
+++ src/BootstrapBlazor.Server/Components/Samples/Practices/Waterfall.razor.js
@@ -1,11 +1,11 @@
-﻿import { addScript } from '/_content/BootstrapBlazor/modules/utility.js'
+﻿import { addScript } from "/_content/BootstrapBlazor/modules/utility.js"
 
 let el, msnry, bb, waterfallInvoke, waterfallMethod, lastScrollTop = 0;
 
 const getBlazorAttributeName = (element) => {
     var attrs = element.attributes;
-    for (var i = 0; i < attrs.length; i++) {
-        if (attrs[i].name.startsWith('b-')) {
+    for (let i = 0; i < attrs.length; i++) {
+        if (attrs[i].name.startsWith("b-")) {
             return attrs[i].name;
         }
     }
@@ -18,15 +18,17 @@ const loadImage = (item) => {
     img.src = item.getAttribute("data-wurl");
     img.onload = () => {
         item.src = img.src;
-        item.classList.add('waterfall-ok');
-        throttle(msnry.layout(), 500, 1000);
+        item.classList.add("waterfall-ok");
+        requestAnimationFrame(() => {
+            throttle(msnry.layout(), 500, 1000);
+        });
     }
     img.onerror = () => {
-        item.src = '/lib/waterfall/Fail.svg';
-        item.classList.add('waterfall-ok', 'waterfall-contain');
+        item.src = "/lib/waterfall/Fail.svg";
+        item.classList.add("waterfall-ok", "waterfall-contain");
         console.info(`Image loading failed：${img.src}`);
     }
-    item.removeAttribute('data-wurl');
+    item.removeAttribute("data-wurl");
 }
 
 const lazyLoadImages = (element) => {
@@ -40,7 +42,7 @@ const lazyLoadImages = (element) => {
         const rect = item.getBoundingClientRect();
 
         if (rect.bottom >= 0 && rect.top < viewHeight) {
-            if (item.nodeName === 'IMG') {
+            if (item.nodeName === "IMG") {
                 loadImage(item);
             }
         }
@@ -75,15 +77,15 @@ const isScrollToBottom = (element) => {
     return !isScrollingUp && scrollHeight + clientHeight >= totalHeight - 300;
 }
 
-const Loadimages = (element) => {
+const appendImage = (element) => {
     return new Promise((resolve) => {
-        imagesLoaded(element).on('progress', (imgLoad, e) => {
-            e.img.parentNode.classList.add('in-loaded');
+        imagesLoaded(element).on("progress", (imgLoad, e) => {
+            e.img.parentNode.classList.add("in-loaded");
             msnry.appended(e.img.parentNode);
             msnry.layout();
         }
-        ).on('done', () => {
-            msnry.once('layoutComplete', () => {
+        ).on("done", () => {
+            msnry.once("layoutComplete", () => {
                 resolve();
             });
         });
@@ -92,38 +94,39 @@ const Loadimages = (element) => {
 
 const fetchData = async (num) => {
     try {
-        return await waterfallInvoke.invokeMethodAsync(waterfallMethod, num)
+        return await waterfallInvoke.invokeMethodAsync(waterfallMethod, num);
     } catch (error) {
         console.log(error);
+        return null;
     }
 }
 
 const onScroll = throttle(async () => {
     lazyLoadImages(el);
     if (isScrollToBottom(el)) {
-        console.info('滚动到底部');
+        console.info("滚动到底部");
         setTimeout(async () => {
-            const datas = await fetchData(30);
-            if (datas === null) {
+            const data = await fetchData(30);
+            if (data === null) {
 
             } else {
-                await update(datas);
+                await update(data);
             }
         }, 2000); // 延迟2秒
     }
 }, 500, 1000);
 
 const addImage = (parentElement, src) => {
-    var item = document.createElement('div');
-    item.className = 'waterfall-item in-load p-2';
-    item.setAttribute(bb, '');
+    var item = document.createElement("div");
+    item.className = "waterfall-item in-load p-2";
+    item.setAttribute(bb, "");
 
-    var img = document.createElement('img');
-    img.className = 'shadow-sm waterfall-img scrollLoading';
-    img.setAttribute(bb, '');
-    img.src = './lib/waterfall/load.gif';
+    var img = document.createElement("img");
+    img.className = "shadow-sm waterfall-img scrollLoading";
+    img.setAttribute(bb, "");
+    img.src = "./lib/waterfall/load.gif";
     img.dataset.wurl = src;
-    img.referrerPolicy = 'no-referrer';
+    img.referrerPolicy = "no-referrer";
 
     item.appendChild(img);
 
@@ -131,17 +134,17 @@ const addImage = (parentElement, src) => {
     return item;
 }
 
-const update = async (datas) => {
-    const wfel = el.getElementsByClassName('waterfall-container')[0];
-    let items = [];
+const update = async (data) => {
+    const element = el.getElementsByClassName("waterfall-container")[0];
+    const items = [];
 
-    datas.forEach((src, index) => {
-        const item = addImage(wfel, src);
+    data.forEach((src, index) => {
+        const item = addImage(element, src);
         items.push(item);
     });
 
     await new Promise((resolve) => {
-        Loadimages(items).then(resolve);
+        appendImage(items).then(resolve);
     });
 }
 
@@ -149,17 +152,17 @@ export const init = async (id, invoke, method) => {
     waterfallInvoke = invoke;
     waterfallMethod = method;
 
-    await addScript('./lib/waterfall/masonry.pkgd.min.js')
-    await addScript('./lib/waterfall/imagesloaded.pkgd.min.js')
+    await addScript("./lib/waterfall/masonry.pkgd.min.js")
+    await addScript("./lib/waterfall/imagesloaded.pkgd.min.js")
 
     el = document.getElementById(id);
     bb = getBlazorAttributeName(el);
-    const wfel = el.getElementsByClassName('waterfall-container')[0];
-    msnry = new Masonry(wfel, { itemSelector: '.waterfall-item' });
-    const datas = await fetchData(30);
-    await update(datas);
+    const element = el.getElementsByClassName("waterfall-container")[0];
+    msnry = new Masonry(element, { itemSelector: ".waterfall-item" });
+    const data = await fetchData(30);
+    await update(data);
 
-    el.addEventListener('scroll', onScroll);
+    el.addEventListener("scroll", onScroll);
 
     lazyLoadImages(el);
 }
