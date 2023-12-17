8eebc3333bea8466cd4351bfbba77cef13af9072
Argo-AscioTech <argo@live.ca>
2023-12-17 10:07:50 +0800

    feat: 增加地址跳转监控

--- src/BootstrapBlazor/Components/Menu/Menu.razor.cs
+++ src/BootstrapBlazor/Components/Menu/Menu.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.AspNetCore.Components.Routing;
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -120,6 +122,20 @@ public partial class Menu
 
         _isAccordion = IsAccordion;
         _isExpandAll = IsExpandAll;
+
+        if (IsVertical && !DisableNavigation)
+        {
+            Navigator.LocationChanged += Navigator_LocationChanged;
+        }
+    }
+
+    private bool _showScrollView;
+
+    private void Navigator_LocationChanged(object? sender, LocationChangedEventArgs e)
+    {
+        _showScrollView = true;
+        InitMenus(null, Items, GetUrl());
+        StateHasChanged();
     }
 
     /// <summary>
@@ -164,7 +180,15 @@ public partial class Menu
 
         if (!firstRender)
         {
-            await InvokeUpdateAsync();
+            if (_showScrollView)
+            {
+                _showScrollView = false;
+                await InvokeVoidAsync("scrollToView", Id);
+            }
+            else
+            {
+                await InvokeUpdateAsync();
+            }
         }
     }
 
@@ -174,7 +198,7 @@ public partial class Menu
     /// <returns></returns>
     private async Task InvokeUpdateAsync()
     {
-        if (ShouldInvoke() && Module != null)
+        if (ShouldInvoke())
         {
             _isAccordion = IsAccordion;
             _isExpandAll = IsExpandAll;
