d6d76c874d99bea5686a207822a5e25668b22bd2
Argo-AscioTech <argo@live.ca>
2023-12-18 20:33:20 +0800

    doc: 增加注释

--- src/BootstrapBlazor.Server/Program.cs
+++ src/BootstrapBlazor.Server/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Options;
 using System.Text;
 
+// 增加中文编码支持用于定位服务
 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var builder = WebApplication.CreateBuilder(args);
