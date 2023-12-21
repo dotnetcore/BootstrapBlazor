b410e42e04a3df5a60b04558488397931be12f58
Argo-AscioTech <argo@live.ca>
2023-12-21 11:08:34 +0800

    feat: 增加主题脚本

--- src/BootstrapBlazor.Server/wwwroot/lib/theme.js
+++ src/BootstrapBlazor.Server/wwwroot/lib/theme.js
@@ -1,21 +1,3 @@
-﻿const getStoredTheme = () => localStorage.getItem('theme')
-const setStoredTheme = theme => localStorage.setItem('theme', theme)
+﻿import { getPreferredTheme, setTheme } from "../../_content/BootstrapBlazor/modules/theme.js?v=$version"
 
-const getPreferredTheme = () => {
-    const storedTheme = getStoredTheme()
-    if (storedTheme) {
-        return storedTheme
-    }
-
-    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-}
-
-const setTheme = theme => {
-    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'light')
-    } else {
-        document.documentElement.setAttribute('data-bs-theme', theme);
-    }
-    setStoredTheme(theme);
-}
 setTheme(getPreferredTheme())
