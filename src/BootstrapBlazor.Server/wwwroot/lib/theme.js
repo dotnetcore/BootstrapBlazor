390c5660821ff47e50c09d777b0f1bbe57162218
Argo-AscioTech <argo@live.ca>
2023-12-21 11:02:01 +0800

    feat: 客户端存储主题

---
+++ src/BootstrapBlazor.Server/wwwroot/lib/theme.js
@@ -0,0 +1,21 @@
+﻿const getStoredTheme = () => localStorage.getItem('theme')
+const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme()
+    if (storedTheme) {
+        return storedTheme
+    }
+
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = theme => {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'light')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    }
+    setStoredTheme(theme);
+}
+setTheme(getPreferredTheme())
