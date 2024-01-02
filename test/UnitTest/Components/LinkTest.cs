d82a94e950eca82d06c83223ae78ba89f10e5cb2
Argo Zhang <argo@live.ca>
2024-01-02 19:15:46 +0800

    feat(Link): add Link component (#2712)
    * refactor: 增加 Link 组件
    
    * doc: 更新 App 组件使用 Link 组件更新文档
    
    * refactor: 移除关键字
    
    * test: 增加单元测试
    
    * test: 增加 Version 单元测试

---
+++ test/UnitTest/Components/LinkTest.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class LinkTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Link_Ok()
+    {
+        var cut = Context.RenderComponent<Link>(pb =>
+        {
+            pb.Add(a => a.Href, "http://www.blazor.zone");
+        });
+        var versionService = cut.Services.GetRequiredService<IVersionService>();
+        Assert.Equal($"<link href=\"http://www.blazor.zone?v={versionService.GetVersion()}\" rel=\"stylesheet\" />", cut.Markup);
+    }
+
+    [Fact]
+    public void Link_Version()
+    {
+        var cut = Context.RenderComponent<Link>(pb =>
+        {
+            pb.Add(a => a.Href, "http://www.blazor.zone");
+            pb.Add(a => a.Version, "20220202");
+        });
+        Assert.Equal($"<link href=\"http://www.blazor.zone?v=20220202\" rel=\"stylesheet\" />", cut.Markup);
+    }
+}
