f6ed86a9bdb687385675fb4bc05a47c42d88461d
Argo-AscioTech <argo@live.ca>
2024-01-02 19:10:54 +0800

    test: 增加 Version 单元测试

--- test/UnitTest/Components/LinkTest.cs
+++ test/UnitTest/Components/LinkTest.cs
@@ -18,4 +18,15 @@ public class LinkTest : BootstrapBlazorTestBase
         var versionService = cut.Services.GetRequiredService<IVersionService>();
         Assert.Equal($"<link href=\"http://www.blazor.zone?v={versionService.GetVersion()}\" rel=\"stylesheet\" />", cut.Markup);
     }
+
+    [Fact]
+    public void Link_Version()
+    {
+        var cut = Context.RenderComponent<Link>(pb =>
+        {
+            pb.Add(a => a.Href, "http://www.blazor.zone");
+            pb.Add(a => a.Version, "20220202");
+        });
+        Assert.Equal($"<link href=\"http://www.blazor.zone?v=20220202\" rel=\"stylesheet\" />", cut.Markup);
+    }
 }
