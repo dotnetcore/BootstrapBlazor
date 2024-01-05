e690e8a4b55a64bf29121b5c6f4e2608bc22dbdb
az <lish@live.cn>
2024-01-05 09:49:43 +0800

    doc(tutorials): Fix incorrect demo source code path in tutorials documentation

--- src/BootstrapBlazor.Server/Components/Layout/TutorialsNavMenu.razor.cs
+++ src/BootstrapBlazor.Server/Components/Layout/TutorialsNavMenu.razor.cs
@@ -2,8 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using BootstrapBlazor.Server.Components.Pages;
 using Microsoft.AspNetCore.Components.Web;
+
 using System.Text;
 
 namespace BootstrapBlazor.Server.Components.Layout;
@@ -53,59 +53,29 @@ public partial class TutorialsNavMenu
                 Url = "tutorials/login",
                 Items =
                 [
-                    //new()
-                    //{
-                    //    Template = CreateDownloadButtonComponent("template1", Template1),
-                    //    Text = "模板 Template 1",
-                    //    Url = "template1"
-                    //},
-                    //new()
-                    //{
-                    //    Template = CreateDownloadButtonComponent("template2", Template2),
-                    //    Text = "模板 Template 2",
-                    //    Url = "template2",
-                    //},
-                    //new()
-                    //{
-                    //    Template = CreateDownloadButtonComponent("template3", Template3),
-                    //    Text = "模板 Template 3",
-                    //    Url = "template3"
-                    //},
-                    //new()
-                    //{
-                    //    Template = CreateDownloadButtonComponent("template4", Template4),
-                    //    Text = "模板 Template 4",
-                    //    Url = "template4"
-                    //},
-                    //new()
-                    //{
-                    //    Template = CreateDownloadButtonComponent("template5", Template5),
-                    //    Text = "模板 Template 5",
-                    //    Url = "template5"
-                    //},
                     new()
                     {
-                        Template = CreateDownloadButtonComponent("template6", Template6),
-                        Text = "模板 Template 6",
-                        Url = "tutorials/template6"
+                        Template = CreateDownloadButtonComponent("template1", Template1),
+                        Text = "模板 Template 1",
+                        Url = "tutorials/template1"
                     },
                     new()
                     {
-                        Template = CreateDownloadButtonComponent("template7", Template7),
-                        Text = "模板 Template 7",
-                        Url = "tutorials/template7"
+                        Template = CreateDownloadButtonComponent("template2", Template2),
+                        Text = "模板 Template 2",
+                        Url = "tutorials/template2"
                     },
                     new()
                     {
-                        Template = CreateDownloadButtonComponent("template8", Template8),
-                        Text = "模板 Template 8",
-                        Url = "tutorials/template8"
+                        Template = CreateDownloadButtonComponent("template3", Template3),
+                        Text = "模板 Template 3",
+                        Url = "tutorials/template3"
                     },
                     new()
                     {
-                        Template = CreateDownloadButtonComponent("template9", Template9),
-                        Text = "模板 Template 9",
-                        Url = "tutorials/template9"
+                        Template = CreateDownloadButtonComponent("template4", Template4),
+                        Text = "模板 Template 4",
+                        Url = "tutorials/template4"
                     }
                 ]
             },
@@ -117,12 +87,13 @@ public partial class TutorialsNavMenu
             },
             new()
             {
+                Template = CreateDownloadButtonComponent("translate", translateFileList),
                 Text = "翻译工具 Translate",
                 Url = "tutorials/translate"
             }
         ];
     }
-
+    
     /// <summary>
     /// OnClickMenu 回调
     /// </summary>
@@ -170,6 +141,14 @@ public partial class TutorialsNavMenu
         stream.Close();
     }
 
+    private static readonly string[] layoutFileList =
+    [
+        "/../Layout/TutorialsLayout.razor",
+        "/../Layout/TutorialsLayout.razor.css",
+        "/../Layout/TutorialsLoginLayout.razor",
+        "/../Layout/TutorialsLoginLayout.razor.css"
+    ];
+
     private readonly string[] dashboardFileList =
     [
         "Tutorials/Dashboard.razor",
@@ -183,76 +162,27 @@ public partial class TutorialsNavMenu
     [
         "Tutorials/LoginAndRegister/Template1.razor",
         "Tutorials/LoginAndRegister/Template1.razor.css",
+        ..layoutFileList
     ];
 
     private readonly string[] Template2 =
     [
         "Tutorials/LoginAndRegister/Template2.razor",
-        "Tutorials/LoginAndRegister/Template2.razor.css",
+        ..layoutFileList
     ];
 
     private readonly string[] Template3 =
     [
         "Tutorials/LoginAndRegister/Template3.razor",
         "Tutorials/LoginAndRegister/Template3.razor.css",
+        ..layoutFileList
     ];
 
     private readonly string[] Template4 =
     [
         "Tutorials/LoginAndRegister/Template4.razor",
         "Tutorials/LoginAndRegister/Template4.razor.css",
-        "Tutorials/LoginAndRegister/Template4.razor.js",
-    ];
-
-    private readonly string[] Template5 =
-    [
-        "Tutorials/LoginAndRegister/Template5.razor",
-        "Tutorials/LoginAndRegister/Template5.razor.css",
-        "Tutorials/LoginAndRegister/Template5.razor.js",
-    ];
-
-    private readonly string[] Template6 =
-    [
-        "Tutorials/LoginAndRegister/Template6.razor",
-        "Tutorials/LoginAndRegister/Template6.razor.css",
-        "Shared/TutorialsLayout.razor",
-        "Shared/TutorialsLayout.razor.cs",
-        "Shared/TutorialsLayout.razor.css",
-        "Shared/TutorialsLoginLayout.razor",
-        "Shared/TutorialsLoginLayout.razor.css"
-    ];
-
-    private readonly string[] Template7 =
-    [
-        "Tutorials/LoginAndRegister/Template7.razor",
-        "Tutorials/LoginAndRegister/Template7.razor.css",
-        "Shared/TutorialsLayout.razor",
-        "Shared/TutorialsLayout.razor.cs",
-        "Shared/TutorialsLayout.razor.css",
-        "Shared/TutorialsLoginLayout.razor",
-        "Shared/TutorialsLoginLayout.razor.css"
-    ];
-
-    private readonly string[] Template8 =
-    [
-        "Tutorials/LoginAndRegister/Template8.razor",
-        "Tutorials/LoginAndRegister/Template8.razor.css",
-        "Shared/TutorialsLayout.razor",
-        "Shared/TutorialsLayout.razor.cs",
-        "Shared/TutorialsLayout.razor.css",
-        "Shared/TutorialsLoginLayout.razor",
-        "Shared/TutorialsLoginLayout.razor.css"
-    ];
-
-    private readonly string[] Template9 =
-    [
-        "Tutorials/LoginAndRegister/Template9.razor",
-        "Tutorials/LoginAndRegister/Template9.razor.css",
-        "Shared/TutorialsLayout.razor",
-        "Shared/TutorialsLayout.razor.cs",
-        "Shared/TutorialsLayout.razor.css",
-        "Shared/TutorialsLoginLayout.razor",
-        "Shared/TutorialsLoginLayout.razor.css"
+        ..layoutFileList
     ];
 
     private readonly string[] waterfallFileList =
@@ -261,4 +191,14 @@ public partial class TutorialsNavMenu
         "Tutorials/Waterfall.razor.cs",
         "Tutorials/Waterfall.razor.css"
     ];
+
+    private readonly string[] translateFileList =
+    [
+        "Tutorials/Translation/Translator.razor",
+        "Tutorials/Translation/Translator.razor.cs",
+        "Tutorials/Translation/Translator.razor.css",
+        "Tutorials/Translation/LanguageWriter.cs",
+        "Tutorials/Translation/LanguageDataTable.cs",
+        "Tutorials/Translation/AzureTranslatorServiceExtensions.cs"
+    ];  
 }
