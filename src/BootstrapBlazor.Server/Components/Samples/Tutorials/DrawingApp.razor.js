23f9d0a468ee8d6f44ae9ce3295c33c916eaed61
Argo-AscioTech <argo@live.ca>
2024-01-09 12:38:56 +0800

    refactor: 增加版本号

--- src/BootstrapBlazor.Server/Components/Samples/Tutorials/DrawingApp.razor.js
+++ src/BootstrapBlazor.Server/Components/Samples/Tutorials/DrawingApp.razor.js
@@ -1,6 +1,6 @@
-﻿import Data from '../../../_content/BootstrapBlazor/modules/data.js'
+﻿import Data from '../../../_content/BootstrapBlazor/modules/data.js?v=$version'
 import EventHandler from "../../../_content/BootstrapBlazor/modules/event-handler.js?v=$version"
-import { isMobile } from "../../../_content/BootstrapBlazor/modules/utility.js"
+import { isMobile } from "../../../_content/BootstrapBlazor/modules/utility.js?v=$version"
 
 /**
  * 缓存绘图步骤
