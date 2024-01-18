41969188e6dd32b14ac8a0db847f54cc9bb84fd8
Argo-AscioTech <argo@live.ca>
2024-01-18 19:10:32 +0800

    feat: 增加 IsPopover 支持

---
+++ src/BootstrapBlazor/Components/Select/SelectTable.razor.js
@@ -0,0 +1,28 @@
+﻿import Data from "../../modules/data.js?v=$version"
+import Popover from "../../modules/base-popover.js?v=$version"
+
+export function init(id) {
+    const el = document.getElementById(id)
+
+    if (el == null) {
+        return
+    }
+
+    const popover = Popover.init(el);
+    const selectTable = {
+        el,
+        input: el.querySelector(".form-select"),
+        popover
+    }
+
+    Data.set(id, selectTable)
+}
+
+export function dispose(id) {
+    const data = Data.get(id)
+    Data.remove(id)
+
+    if (data) {
+        Popover.dispose(data.popover)
+    }
+}
