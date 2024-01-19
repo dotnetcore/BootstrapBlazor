60a58253cb22efc00b0a2670e40a7c6b327df826
Argo-AscioTech <argo@live.ca>
2024-01-19 12:59:27 +0800

    doc: 增加 Color 示例

--- src/BootstrapBlazor.Server/Components/Samples/SelectTables.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/SelectTables.razor.cs
@@ -11,10 +11,20 @@ public partial class SelectTables
 {
     [Inject]
     [NotNull]
-    private IStringLocalizer<Foo>? Localizer { get; set; }
+    private IStringLocalizer<Foo>? LocalizerFoo { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<SelectTables>? Localizer { get; set; }
 
     [NotNull]
     private List<Foo>? Items { get; set; }
+
+    [NotNull]
+    private List<Foo>? ColorItems { get; set; }
+
+    [NotNull]
+    private List<Foo>? TemplateItems { get; set; }
 
     private Foo? Foo { get; set; }
 
@@ -23,8 +33,90 @@ public partial class SelectTables
     /// </summary>
     protected override void OnInitialized()
     {
-        Items = Foo.GenerateFoo(Localizer);
+        Items = Foo.GenerateFoo(LocalizerFoo);
+        ColorItems = Foo.GenerateFoo(LocalizerFoo);
+        TemplateItems = Foo.GenerateFoo(LocalizerFoo);
     }
 
     private static string? GetTextCallback(Foo? foo) => foo?.Name;
+
+    /// <summary>
+    /// 获得属性方法
+    /// </summary>
+    /// <returns></returns>
+    private AttributeItem[] GetAttributes() =>
+    [
+        new()
+        {
+            Name = "Items",
+            Description = Localizer["AttributeItems"],
+            Type = "IEnumerable<TItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "TableColumns",
+            Description = Localizer["AttributeTableColumns"],
+            Type = "RenderFragment<TItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "Color",
+            Description = Localizer["AttributeColor"],
+            Type = "Color",
+            ValueList = "Primary / Secondary / Success / Danger / Warning / Info / Dark",
+            DefaultValue = "Primary"
+        },
+        new()
+        {
+            Name = "IsDisabled",
+            Description = Localizer["AttributeIsDisabled"],
+            Type = "boolean",
+            ValueList = "true / false",
+            DefaultValue = "false"
+        },
+        new()
+        {
+            Name = "ShowAppendArrow",
+            Description = Localizer["AttributeShowAppendArrow"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new()
+        {
+            Name = "GetTextCallback",
+            Description = Localizer["AttributeGetTextCallback"],
+            Type = "Func<TItem, string>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "PlaceHolder",
+            Description = Localizer["AttributePlaceHolder"],
+            Type = "string?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "Height",
+            Description = Localizer["AttributeHeight"],
+            Type = "int",
+            ValueList = " — ",
+            DefaultValue = "486"
+        },
+        new()
+        {
+            Name = "Template",
+            Description = Localizer["AttributeTemplate"],
+            Type = "RenderFragment<TItem>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
+    ];
 }
