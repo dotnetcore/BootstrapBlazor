5b2ae3a418d04573bf6528db70f79b8fc37e9e7a
Argo-AscioTech <argo@live.ca>
2024-01-28 18:26:03 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Components/BrowserFinger/BrowserFinger.razor.cs
+++ src/BootstrapBlazor/Components/BrowserFinger/BrowserFinger.razor.cs
@@ -48,7 +48,7 @@ public partial class BrowserFinger : IDisposable
         if (string.IsNullOrEmpty(_fingerCode))
         {
             await _tcs.Task;
-            _fingerCode ??= await InvokeAsync<string?>("getFingerCode");
+            _fingerCode = await InvokeAsync<string?>("getFingerCode");
         }
         return _fingerCode;
     }
