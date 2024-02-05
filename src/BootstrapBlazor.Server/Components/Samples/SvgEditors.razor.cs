7f2b9d8c6721ed5db473bd0902792bf7af2e84a4
Argo-AscioTech <argo@live.ca>
2024-02-05 22:12:53 +0800

    doc: 更新 svg-editor 文档源码映射

---
+++ src/BootstrapBlazor.Server/Components/Samples/SvgEditors.razor.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples;
+public partial class SvgEditors
+{
+    [NotNull]
+    private BootstrapBlazor.Components.ConsoleLogger? Console { get; set; }
+
+    private Task OnSaveChanged(string content)
+    {
+        Console.Log(content);
+        return Task.CompletedTask;
+    }
+}
