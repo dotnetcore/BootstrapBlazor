9c2ffc89163259d6d23ed795c3994db36eecf1b6
Argo-AscioTech <argo@live.ca>
2024-02-16 00:05:56 +0800

    feat: 增加扩展方法

---
+++ src/BootstrapBlazor/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+internal static class DateTimeExtensions
+{
+    /// <summary>
+    /// 获得安全的月份时间
+    /// </summary>
+    /// <param name="dt"></param>
+    /// <param name="month"></param>
+    /// <returns></returns>
+    public static DateTime GetSafeMonthDateTime(this DateTime dt, int month)
+    {
+        var @base = dt;
+        if (month < 0)
+        {
+            if (DateTime.MinValue.AddMonths(0 - month) < dt)
+            {
+                @base = dt.AddMonths(month);
+            }
+            else
+            {
+                @base = DateTime.MinValue.Date;
+            }
+        }
+        else if (month > 0)
+        {
+            if (DateTime.MaxValue.AddMonths(0 - month) > dt)
+            {
+                @base = dt.AddMonths(month);
+            }
+            else
+            {
+                @base = DateTime.MaxValue.Date;
+            }
+        }
+        return @base;
+    }
+}
