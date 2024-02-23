b1432b7c8bb32da4c36716315792c0c1776c450e
Argo-AscioTech <argo@live.ca>
2024-02-23 19:37:35 +0800

    refactor: 重构类

--- src/BootstrapBlazor/Attributes/TableMetadataForAttribute.cs
+++ src/BootstrapBlazor/Attributes/TableMetadataForAttribute.cs
@@ -7,10 +7,10 @@ using System.Diagnostics;
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// Define a group of UI generation metadata for target data type <br/>
-/// Usually model types are at different layer to the blazor component UI layer. <br/>
-/// In this case, use <see cref="TableMetadataForAttribute"/> to define a metadata type for Table component. <br/>
-/// Then register metadata type with <seealso cref="TableMetadataTypeService"/> <br/>
+/// Define a group of UI generation metadata for target data type
+/// Usually model types are at different layer to the blazor component UI layer.
+/// In this case, use <see cref="TableMetadataForAttribute"/> to define a metadata type for Table component.
+/// Then register metadata type with <seealso cref="TableMetadataTypeService"/>
 ///
 /// <example>
 ///     Example:
@@ -31,11 +31,12 @@ namespace BootstrapBlazor.Components;
 ///     {
 ///         [AutoGenerateColumn(Ignore = true)]
 ///         public string? Name1 { get; set; }
+///         
 ///         [AutoGenerateColumn(Align = Alignment.Center, Order = -2)]
 ///         public string? Name2 { get; set; }
 ///     }
 ///     </code>
-///     before using the metadata, it needs to register the metadata types. <br/>
+///     before using the metadata, it needs to register the metadata types.
 ///     register metadata types in assembly
 ///     <code>
 ///     TableMetadataTypeService.RegisterMetadataTypes(typeof(Pig).Assembly);
@@ -48,20 +49,15 @@ namespace BootstrapBlazor.Components;
 ///     </code>
 /// </example>
 /// </summary>
-[DebuggerStepThrough()]
+/// <remarks>
+/// Constructor TableMetadataForAttribute for target data type
+/// </remarks>
+/// <param name="dataType">The target model/data type</param>
 [AttributeUsage(AttributeTargets.Class)]
-public class TableMetadataForAttribute : Attribute
+public class TableMetadataForAttribute(Type dataType) : Attribute
 {
-    /// <summary>
-    /// Constructor TableMetadataForAttribute for target data type
-    /// </summary>
-    /// <param name="dataType">The target model/data type</param>
-    public TableMetadataForAttribute(Type dataType)
-    {
-        DataType = dataType;
-    }
     /// <summary>
     /// The target model/data type
     /// </summary>
-    public Type DataType { get; private set; }
+    public Type DataType { get; private set; } = dataType;
 }
