49d05d46492d991c845b922456a10e72d0d72fea
Argo-AscioTech <argo@live.ca>
2024-03-01 14:43:19 +0800

    fix: 修复类型转化成功后 PreviousParsingAttemptFailed 参数仍然 false 问题

--- src/BootstrapBlazor/Components/Validate/ValidateBase.cs
+++ src/BootstrapBlazor/Components/Validate/ValidateBase.cs
@@ -114,7 +114,7 @@ public abstract class ValidateBase<TValue> : DisplayBase<TValue>, IValidateCompo
             }
             else if (TryParseValueFromString(value, out var parsedValue, out var validationErrorMessage))
             {
-                PreviousParsingAttemptFailed = false;
+                PreviousParsingAttemptFailed = true;
                 CurrentValue = parsedValue;
             }
             else
