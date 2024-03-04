89ff07d265b658b872968c26527f6ff2de09fd37
Argo-AscioTech <argo@live.ca>
2024-03-04 10:33:24 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/Tutorials/Waterfall.razor.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples.Tutorials;
+
+/// <summary>
+/// 瀑布流组件示例文档
+/// </summary>
+public partial class Waterfall
+{
+    private readonly List<string> _items = [];
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _items.AddRange(GetImags());
+    }
+
+    private static IEnumerable<string> GetImags() => Enumerable.Range(1, 18).Select(i => $"./images/waterfall/{i}.jpeg");
+}
