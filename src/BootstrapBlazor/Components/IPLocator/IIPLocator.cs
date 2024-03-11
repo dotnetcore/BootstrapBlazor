44a7b02d8c49c17202b4c8ed66ab0ebf0e8262ff
Argo-AscioTech <argo@live.ca>
2024-03-11 12:42:59 +0800

    revert: 撤销文件删除

---
+++ src/BootstrapBlazor/Components/IPLocator/IIPLocator.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// IP 地址定位接口
+/// </summary>
+[Obsolete("已弃用")]
+public interface IIPLocator
+{
+    /// <summary>
+    /// 定位方法
+    /// </summary>
+    /// <param name="option">定位器配置信息</param>
+    /// <returns>定位器定位结果</returns>
+    Task<string?> Locate(IPLocatorOption option);
+
+    /// <summary>
+    /// 获得/设置 接口地址
+    /// </summary>
+    public string? Url { get; set; }
+}
---
+++ src/BootstrapBlazor/Components/IPLocator/IPLocatorOption.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.Logging;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// IP定位器配置项
+/// </summary>
+[Obsolete("已弃用")]
+[ExcludeFromCodeCoverage]
+public class IPLocatorOption
+{
+    /// <summary>
+    /// 获得/设置 定位器创建方法未设置使用内部定位器
+    /// </summary>
+    public Func<IServiceProvider, IIPLocator>? LocatorFactory { get; set; }
+
+    /// <summary>
+    /// 获得/设置 IP地址请求超时时间 默认为 3000 毫秒
+    /// </summary>
+    public int RequestTimeout { get; set; } = 3000;
+
+    /// <summary>
+    /// 获得 本机地址列表
+    /// </summary>
+    public List<string> Localhosts { get; } = new List<string>(new string[] { "::1", "127.0.0.1" });
+
+    /// <summary>
+    /// 获得/设置 IP地址
+    /// </summary>
+    protected internal string? IP { get; set; }
+
+    /// <summary>
+    /// 获得/设置 HttpClient 实体类
+    /// </summary>
+    protected internal HttpClient? HttpClient { get; set; }
+
+    /// <summary>
+    /// 获得/设置 ILogger 实体类
+    /// </summary>
+    protected internal ILogger<IIPLocatorProvider>? Logger { get; set; }
+}
