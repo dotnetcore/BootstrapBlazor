ee117cf991b1d4879cb157fe7cb412a5a87a5e86
Argo-AscioTech <argo@live.ca>
2024-03-13 09:54:54 +0800

    feat: 增加 FlipClock 组件

---
+++ src/BootstrapBlazor/Components/FlipClock/FlipClock.razor.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// FlipClock 组件
+/// </summary>
+public partial class FlipClock
+{
+    private string? ClassString => CssBuilder.Default("bb-flip-clock")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
