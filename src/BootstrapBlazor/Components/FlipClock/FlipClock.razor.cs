bc25b044f75fcc5dc22b64780176447457601b02
Argo-AscioTech <argo@live.ca>
2024-03-13 10:36:40 +0800

    feat: 增加脚本支持

--- src/BootstrapBlazor/Components/FlipClock/FlipClock.razor.cs
+++ src/BootstrapBlazor/Components/FlipClock/FlipClock.razor.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -9,7 +10,43 @@ namespace BootstrapBlazor.Components;
 /// </summary>
 public partial class FlipClock
 {
+    /// <summary>
+    /// clock display hour
+    /// </summary>
+    [Parameter]
+    public bool ShowHour { get; set; } = true;
+
+    /// <summary>
+    /// clock display minute
+    /// </summary>
+    [Parameter]
+    public bool ShowMinute { get; set; } = true;
+
+    /// <summary>
+    /// Timing end callback method
+    /// </summary>
+    [Parameter]
+    public Func<Task>? OnCompletedAsync { get; set; }
+
     private string? ClassString => CssBuilder.Default("bb-flip-clock")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, nameof(OnCompleted));
+
+    /// <summary>
+    /// Timing end callback method called by js invoke
+    /// </summary>
+    [JSInvokable]
+    public async Task OnCompleted()
+    {
+        if (OnCompletedAsync != null)
+        {
+            await OnCompletedAsync();
+        }
+    }
 }
---
+++ src/BootstrapBlazor/Components/FlipClock/FlipClock.razor.js
@@ -0,0 +1,12 @@
+﻿import Data from "../../modules/data.js?v=$version"
+import EventHandler from "../../modules/event-handler.js?v=$version"
+
+export function init(id, invoke, method) {
+
+}
+
+export function dispose(id) {
+    const clock = Data.get(id)
+    if (clock) {
+    }
+}
