9d7d3dd08bdb3be6df2833d3bdafc5cfdb362a8d
Argo-AscioTech <argo@live.ca>
2024-03-14 18:08:43 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/FlipClocks.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/FlipClocks.razor.cs
@@ -17,4 +17,60 @@ public partial class FlipClocks
         StateHasChanged();
         return Task.CompletedTask;
     }
+
+    /// <summary>
+    /// GetAttributes
+    /// </summary>
+    /// <returns></returns>
+    private static AttributeItem[] GetAttributes() =>
+    [
+        new()
+        {
+            Name = nameof(FlipClock.ShowHour),
+            Description = "是否显示小时",
+            Type = "boolean",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new()
+        {
+            Name = nameof(FlipClock.ShowMinute),
+            Description = "是否显示分钟",
+            Type = "boolean",
+            ValueList = "true|false",
+            DefaultValue = "true"
+        },
+        new()
+        {
+            Name = nameof(FlipClock.ViewMode),
+            Description = "是否显示分钟",
+            Type = "FlipClockViewMode",
+            ValueList = "DateTime|Count|CountDown",
+            DefaultValue = "DateTime"
+        },
+        new()
+        {
+            Name = nameof(FlipClock.StartValue),
+            Description = "开始时间",
+            Type = "TimeSpan",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = nameof(FlipClock.OnCompletedAsync),
+            Description = "计时结束回调方法",
+            Type = "Func<Task>",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = nameof(FlipClock.ShowHour),
+            Description = "Excel/Word 文件路径或者URL",
+            Type = "boolean",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
+    ];
 }
