d92394c2ace6e6b40cb0c0614135db209578127a
Argo-AscioTech <argo@live.ca>
2024-03-17 00:22:06 +0800

    feat: 增加 Holiday 组件包

---
+++ src/Extensions/Components/BootstrapBlazor.Holiday/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.Holiday.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// BootstrapBlazor 服务扩展类
+/// </summary>
+public static class BootstrapBlazorAzureOpenAIServiceExtensions
+{
+    /// <summary>
+    /// 添加 AzureOpenAIService 服务
+    /// </summary>
+    /// <param name="services"></param>
+    public static IServiceCollection AddBootstrapHolidayService(this IServiceCollection services)
+    {
+        services.AddSingleton<ICalendarHolidays, DefaultCalendarHolidays>();
+
+        return services;
+    }
+}
