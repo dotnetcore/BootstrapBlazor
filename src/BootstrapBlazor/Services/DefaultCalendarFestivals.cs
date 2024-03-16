d28dc28138da3c4735d03ce8895c8f9a4fc05275
Argo-AscioTech <argo@live.ca>
2024-03-16 17:01:34 +0800

    refactor: 增加节假日服务

---
+++ src/BootstrapBlazor/Services/DefaultCalendarFestivals.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+
+namespace BootstrapBlazor.Components;
+
+class DefaultCalendarFestivals : ICalendarFestivals
+{
+    private static readonly Dictionary<string, string> Festivals = new() {
+        {"0101", "元旦"},
+        {"0214", "情人节"},
+        {"0308", "妇女节"},
+        {"0312", "植树节"},
+        {"0401", "愚人节"},
+        {"0501", "劳动节"},
+        {"0504", "青年节"},
+        {"0601", "儿童节"},
+        {"0701", "建党节"},
+        {"0801", "建军节"},
+        {"1001", "国庆节"},
+        {"1225", "圣诞节"}
+    };
+
+    private static readonly Dictionary<string, string> LunarFestivals = new() {
+        {"0101", "春节"},
+        {"0115", "元宵节"},
+        {"0505", "端午节"},
+        {"0815", "中秋节"},
+        {"0909", "重阳节"},
+        {"1208", "腊八节"}
+    };
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public string? GetFestival(DateTime dt)
+    {
+        string? ret = null;
+        var key = $"{dt:MMdd}";
+        if (LunarFestivals.TryGetValue(key, out var v1))
+        {
+            ret = v1;
+        }
+        else if (Festivals.TryGetValue(key, out var v2))
+        {
+            ret = v2;
+        }
+        return ret;
+    }
+}
---
+++ src/BootstrapBlazor/Services/ICalendarFestivals.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 中国节假日接口
+/// </summary>
+public interface ICalendarFestivals
+{
+    /// <summary>
+    /// 获得 节假日键值对
+    /// </summary>
+    /// <returns></returns>
+    string? GetFestival(DateTime dt);
+}
