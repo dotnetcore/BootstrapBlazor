113fc89af591568a397f7f4bff2610ecf6ccd146
Argo-MacAir <argo@live.ca>
2024-03-17 19:27:34 +0800

    chore: 更新脚本

--- scripts/linux/deploy-blazor.sh
+++ scripts/linux/deploy-blazor.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
 
 cd ~/BootstrapBlazor
+curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Publish
+
 git pull
-dotnet restore --no-cache
+dotnet build src/BootstrapBlazor.Server
 dotnet publish src/BootstrapBlazor.Server -c Release
+curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Reboot
 
 systemctl stop ba.blazor
 \cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net8.0/publish/* /usr/local/ba/blazor
