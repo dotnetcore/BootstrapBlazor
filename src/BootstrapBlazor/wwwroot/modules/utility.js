284d3069bc25026dbb1aa6296457cc9f9f5e41ff
Argo-AscioTech <argo@live.ca>
2024-03-23 10:11:25 +0800

    refactor: 增加指纹方法

--- src/BootstrapBlazor/wwwroot/modules/utility.js
+++ src/BootstrapBlazor/wwwroot/modules/utility.js
@@ -625,6 +625,40 @@ export function isMobile() {
     return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini|mobile/i.test(navigator.userAgent);
 }
 
+const hashCode = str => {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(1);
+        hash = (hash << 5) - hash + char;
+        hash |= 0;
+    }
+    return hash;
+}
+
+export function getFingerCode() {
+    const canvas = document.createElement('canvas');
+    canvas.width = 200;
+    canvas.height = 200;
+
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(128, 0, 0)';
+    ctx.fillRect(10, 10, 100, 100);
+
+    ctx.fillStyle = 'rgb(0, 128, 0)';
+    ctx.fillRect(50, 50, 100, 100);
+    ctx.strokeStyle = 'rgb(0, 0, 128)'
+    ctx.lineWidth = 5;
+    ctx.strokeRect(30, 30, 80, 80);
+
+    ctx.font = '20px Arial';
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillText('BootstrapBlazor', 60, 116);
+
+    const dataURL = canvas.toDataURL();
+    const hash = hashCode(dataURL);
+    return hash.toString();
+}
+
 export {
     autoAdd,
     autoRemove,
