ef7a51c088812cc0df3fff7465630c2803d3f176
Argo-AscioTech <argo@live.ca>
2024-03-23 10:15:06 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Components/ConnectionHubTest.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UnitTest.Components;
+
+public class ConnectionHubTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public async Task AddConnection_Ok()
+    {
+        var cut = Context.RenderComponent<ConnectionHub>();
+        await cut.InvokeAsync(() =>
+        {
+            cut.Instance.Callback("test-key");
+        });
+        await Task.Delay(1000);
+        await cut.InvokeAsync(() =>
+        {
+            cut.Instance.Callback("test-key");
+        });
+        await Task.Delay(5000);
+
+        var service = Context.Services.GetRequiredService<IConnectionService>();
+        Assert.True(service.TryGetValue("test-key", out var result));
+        Assert.Equal(0, service.Count);
+    }
+}
