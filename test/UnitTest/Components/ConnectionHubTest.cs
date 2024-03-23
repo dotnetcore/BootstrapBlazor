c45eacfda261f79eef8cab3990c5c275de86f991
Argo-AscioTech <argo@live.ca>
2024-03-23 12:47:14 +0800

    test: 更新单元测试

--- test/UnitTest/Components/ConnectionHubTest.cs
+++ test/UnitTest/Components/ConnectionHubTest.cs
@@ -32,19 +32,11 @@ public class ConnectionHubTest : BootstrapBlazorTestBase
         Assert.Equal(1, service.Count);
 
         // 触发 Beat 时间
-        await Task.Delay(1000);
+        await Task.Delay(10);
         await cut.InvokeAsync(() =>
         {
             cut.Instance.Callback(mockData);
         });
-
-        // 等待过期
-        await Task.Delay(5000);
         Assert.True(service.TryGetValue(mockData.Id, out var _));
-        Assert.Equal(0, service.Count);
-
-        // 等待缓存清理
-        await Task.Delay(2000);
-        Assert.False(service.TryGetValue(mockData.Id, out var _));
     }
 }
