4d0a6ada0512fae1d4ac6fdaf177d48120e1af73
Argo-AscioTech <argo@live.ca>
2024-03-25 16:07:30 +0800

    test: 更新单元测试

--- test/UnitTest/Services/IpLocatorTest.cs
+++ test/UnitTest/Services/IpLocatorTest.cs
@@ -4,18 +4,20 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System.Net;
-using System.Text;
 
-namespace UnitTest.Components;
+namespace UnitTest.Services;
 
 public class IpLocatorTest : BootstrapBlazorTestBase
 {
     [Fact]
-    public async Task BaiduIpLocatorProviderV2_Ok()
+    public async Task BaiduIpLocatorProvider_Ok()
     {
-        var factory = Context.Services.GetRequiredService<IIpLocatorFactory>();
-        var provider = factory.Create();
+        var factory = Context.Services.GetRequiredService<IHttpClientFactory>();
+        var option = Context.Services.GetRequiredService<IOptions<BootstrapBlazorOptions>>();
+        var logger = Context.Services.GetRequiredService<ILogger<MockProvider>>();
+        var provider = new MockProvider(factory, option, logger);
 
         var result = await provider.Locate("127.0.0.1");
         Assert.Equal("本地连接", result);
@@ -25,15 +27,26 @@ public class IpLocatorTest : BootstrapBlazorTestBase
 
         // 河南省漯河市舞阳县 中国移动
         result = await provider.Locate("223.91.188.112");
+        Assert.Equal("美国", result);
+
+        // 命中缓存
+        await provider.Locate("223.91.188.112");
+        Assert.Equal("美国", result);
+
+        // 关闭缓存
+        option.Value.IpLocatorOptions.EnableCache = false;
+        option.Value.IpLocatorOptions.SlidingExpiration = TimeSpan.FromMinutes(5);
+        await provider.Locate("223.91.188.112");
+        Assert.Equal("美国", result);
     }
 
     [Fact]
-    public async Task BaiduIpLocatorProvider_Ok()
+    public async Task BaiduIpLocatorProviderV2_Ok()
     {
-        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-        var factory = Context.Services.GetRequiredService<IIpLocatorFactory>();
-        var provider = factory.Create("BaiduIpLocatorProvider");
+        var factory = Context.Services.GetRequiredService<IHttpClientFactory>();
+        var option = Context.Services.GetRequiredService<IOptions<BootstrapBlazorOptions>>();
+        var logger = Context.Services.GetRequiredService<ILogger<MockProviderV2>>();
+        var provider = new MockProviderV2(factory, option, logger);
 
         var result = await provider.Locate("127.0.0.1");
         Assert.Equal("本地连接", result);
@@ -42,7 +55,17 @@ public class IpLocatorTest : BootstrapBlazorTestBase
         Assert.Equal("本地连接", result);
 
         // 河南省漯河市 移动
-        await provider.Locate("223.91.188.112");
+        result = await provider.Locate("223.91.188.112");
+        Assert.Equal("测试 测试", result);
+    }
+
+    [Fact]
+    public void Factory_Ok()
+    {
+        var factory = Context.Services.GetRequiredService<IIpLocatorFactory>();
+        Assert.NotNull(factory.Create("BaiduIpLocatorProviderV2"));
+        Assert.NotNull(factory.Create("BaiduIpLocatorProvider"));
+        Assert.NotNull(factory.Create());
     }
 
     [Fact]
@@ -56,8 +79,9 @@ public class IpLocatorTest : BootstrapBlazorTestBase
     public async Task Fetch_Error()
     {
         var factory = Context.Services.GetRequiredService<IHttpClientFactory>();
+        var option = Context.Services.GetRequiredService<IOptions<BootstrapBlazorOptions>>();
         var logger = Context.Services.GetRequiredService<ILogger<MockProviderFetchError>>();
-        var provider = new MockProviderFetchError(factory, logger);
+        var provider = new MockProviderFetchError(factory, option, logger);
         var result = await provider.Locate("223.91.188.112");
         Assert.Null(result);
     }
@@ -66,47 +90,89 @@ public class IpLocatorTest : BootstrapBlazorTestBase
     public async Task Fetch_Result_Fail()
     {
         var factory = Context.Services.GetRequiredService<IHttpClientFactory>();
-        var logger = Context.Services.GetRequiredService<ILogger<MockBaiduProviderHttpClient>>();
-        var provider = new MockBaiduProviderHttpClient(factory, logger);
+        var option = Context.Services.GetRequiredService<IOptions<BootstrapBlazorOptions>>();
+        option.Value.IpLocatorOptions.EnableCache = false;
+        var logger = Context.Services.GetRequiredService<ILogger<MockBaiduProvider>>();
+        var provider = new MockBaiduProvider(factory, option, logger);
         var result = await provider.Locate("223.91.188.112");
         Assert.Null(result);
 
-        var loggerV2 = Context.Services.GetRequiredService<ILogger<MockBaiduProviderV2HttpClient>>();
-        var providerV2 = new MockBaiduProviderV2HttpClient(factory, loggerV2);
+        var loggerV2 = Context.Services.GetRequiredService<ILogger<MockBaiduProviderV2>>();
+        var providerV2 = new MockBaiduProviderV2(factory, option, loggerV2);
         result = await providerV2.Locate("223.91.188.112");
         Assert.Null(result);
     }
 
-    class MockProviderFetchError(IHttpClientFactory httpClientFactory, ILogger<MockProviderFetchError> logger) : BaiduIpLocatorProvider(httpClientFactory, logger)
+    class MockProviderFetchError(IHttpClientFactory httpClientFactory, IOptions<BootstrapBlazorOptions> option, ILogger<MockProviderFetchError> logger) : BaiduIpLocatorProvider(httpClientFactory, option, logger)
     {
         protected override Task<string?> Fetch(string url, HttpClient client, CancellationToken token) => throw new InvalidOperationException();
     }
 
-    class MockBaiduProviderHttpClient(IHttpClientFactory httpClientFactory, ILogger<MockBaiduProviderHttpClient> logger) : BaiduIpLocatorProvider(httpClientFactory, logger)
+    class MockBaiduProvider(IHttpClientFactory httpClientFactory, IOptions<BootstrapBlazorOptions> option, ILogger<MockBaiduProvider> logger) : BaiduIpLocatorProvider(httpClientFactory, option, logger)
     {
         protected override Task<string?> Fetch(string url, HttpClient client, CancellationToken token)
         {
-            client = new HttpClient(new MockHttpMessageHandler(), true);
+            client = new HttpClient(new MockHttpNullMessageHandler(), true);
             return base.Fetch(url, client, token);
         }
     }
 
-    class MockBaiduProviderV2HttpClient(IHttpClientFactory httpClientFactory, ILogger<MockBaiduProviderV2HttpClient> logger) : BaiduIpLocatorProviderV2(httpClientFactory, logger)
+    class MockBaiduProviderV2(IHttpClientFactory httpClientFactory, IOptions<BootstrapBlazorOptions> option, ILogger<MockBaiduProviderV2> logger) : BaiduIpLocatorProviderV2(httpClientFactory, option, logger)
     {
         protected override Task<string?> Fetch(string url, HttpClient client, CancellationToken token)
         {
-            client = new HttpClient(new MockHttpMessageHandler(), true);
+            client = new HttpClient(new MockHttpNullMessageHandler(), true);
             return base.Fetch(url, client, token);
         }
     }
 
-    class MockHttpMessageHandler : HttpMessageHandler
+    class MockProvider(IHttpClientFactory httpClientFactory, IOptions<BootstrapBlazorOptions> option, ILogger<MockProvider> logger) : BaiduIpLocatorProvider(httpClientFactory, option, logger)
+    {
+        protected override Task<string?> Fetch(string url, HttpClient client, CancellationToken token)
+        {
+            client = new HttpClient(new MockHttpSuccessMessageHandler(), true);
+            return base.Fetch(url, client, token);
+        }
+    }
+
+    class MockProviderV2(IHttpClientFactory httpClientFactory, IOptions<BootstrapBlazorOptions> option, ILogger<MockProviderV2> logger) : BaiduIpLocatorProviderV2(httpClientFactory, option, logger)
+    {
+        protected override Task<string?> Fetch(string url, HttpClient client, CancellationToken token)
+        {
+            client = new HttpClient(new MockHttpSuccessMessageHandlerV2(), true);
+            return base.Fetch(url, client, token);
+        }
+    }
+
+    class MockHttpNullMessageHandler : HttpMessageHandler
     {
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
             {
                 Content = new StringContent("null")
+            });
+        }
+    }
+
+    class MockHttpSuccessMessageHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("{\"status\":\"0\",\"t\":\"\",\"set_cache_time\":\"\",\"data\":[{\"ExtendedLocation\":\"\",\"OriginQuery\":\"20.205.243.166\",\"appinfo\":\"\",\"disp_type\":0,\"fetchkey\":\"20.205.243.166\",\"location\":\"美国\",\"origip\":\"20.205.243.166\",\"origipquery\":\"20.205.243.166\",\"resourceid\":\"6006\",\"role_id\":0,\"shareImage\":1,\"showLikeShare\":1,\"showlamp\":\"1\",\"titlecont\":\"IP地址查询\",\"tplt\":\"ip\"}]}")
+            });
+        }
+    }
+
+    class MockHttpSuccessMessageHandlerV2 : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("{\"code\":\"Success\",\"data\": {\"prov\":\"测试\", \"isp\": \"测试\"}}")
             });
         }
     }
