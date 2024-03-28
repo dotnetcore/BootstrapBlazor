92039cad32795df3d70db57e34fa2e6bf43cd35d
Argo-AscioTech <argo@live.ca>
2024-03-28 20:44:22 +0800

    refactor: 精简代码合并 Ip 逻辑到客户端

--- src/BootstrapBlazor/wwwroot/modules/hub.js
+++ src/BootstrapBlazor/wwwroot/modules/hub.js
@@ -1,24 +1,27 @@
-﻿import { getFingerCode } from "./utility.js?v=$version"
+﻿import { getClientInfo } from "./client.js?v=$version"
+import { getFingerCode } from "./utility.js?v=$version"
 import Data from "./data.js?v=$version"
 
 export async function init(id, options) {
-    const { invoke, method, interval = 3000 } = options;
+    const { invoke, method, interval = 3000, url } = options;
 
     if (localStorage.getItem('bootstrapblazor_hub_id') === null) {
         localStorage.setItem('bootstrapblazor_hub_id', id);
     }
 
+    window.addEventListener('unload', () => localStorage.removeItem('bootstrapblazor_hub_id'));
+
+    const info = await getClientInfo(url);
+    info.id = getFingerCode();
     const handler = setInterval(async () => {
         const hubId = localStorage.getItem('bootstrapblazor_hub_id');
 
         if (hubId === id) {
-            await invoke.invokeMethodAsync(method, hub.code);
+            await invoke.invokeMethodAsync(method, info);
         }
     }, interval);
 
-    window.addEventListener('unload', () => localStorage.removeItem('bootstrapblazor_hub_id'));
-
-    const hub = { code: getFingerCode(), handler };
+    const hub = { handler };
     Data.set(id, hub);
 }
 
