be00f3966e9321d7de507601df220eaf9c56ac71
Argo-AscioTech <argo@live.ca>
2024-03-29 19:41:01 +0800

    refactor: 更新脚本

--- src/BootstrapBlazor/wwwroot/modules/hub.js
+++ src/BootstrapBlazor/wwwroot/modules/hub.js
@@ -7,13 +7,14 @@ export async function init(id, options) {
     const { invoke, method, interval = 3000, url } = options;
     const hubs = [];
     const chanel = new BroadcastChannel('bb_hubs_chanel');
+    const localStorageKey = 'bb_hub_id';
 
-    if (localStorage.getItem('bootstrapblazor_hub_id') === null) {
-        localStorage.setItem('bootstrapblazor_hub_id', id);
+    if (localStorage.getItem(localStorageKey) === null) {
+        localStorage.setItem(localStorageKey, id);
     }
     window.addEventListener('unload', () => {
         chanel.close();
-        localStorage.removeItem('bootstrapblazor_hub_id');
+        localStorage.removeItem(localStorageKey);
     });
 
     EventHandler.on(chanel, 'message', e => {
@@ -25,25 +26,22 @@ export async function init(id, options) {
 
     const info = await getClientInfo(url);
     info.id = getFingerCode();
-    let count = 0;
     const handler = setInterval(async () => {
         chanel.postMessage(id);
-        let hubId = localStorage.getItem('bootstrapblazor_hub_id');
+        let hubId = localStorage.getItem(localStorageKey);
 
         if (hubId === null) {
-            localStorage.setItem('bootstrapblazor_hub_id', id);
+            localStorage.setItem(localStorageKey, id);
             hubId = id;
         }
         console.log(hubId);
         if (hubId === id) {
-            info.ip = `${count++}`;
-            console.log(id);
             await invoke.invokeMethodAsync(method, info);
         }
         else if (hubs.length > 0) {
             const h = hubs.find(v => v === hubId);
             if (h === void 0) {
-                localStorage.removeItem('bootstrapblazor_hub_id');
+                localStorage.removeItem(localStorageKey);
             }
         }
     }, interval);
