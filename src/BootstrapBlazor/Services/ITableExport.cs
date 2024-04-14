6a0abbb9c9d35308fe6e0dd852b2c4ff3717a873
Argo Zhang <argo@live.ca>
2024-04-14 12:12:07 +0800

    doc(Web): use css variable (#3284)
    * refactor: 移除 footer-link 字体样式
    
    * style: 更新 footer 样式
    
    * style: 更新 footer-body 背景色与前景色变量
    
    * refactor: 移动分割条到 MainLayout
    
    * chore: 更新配置
    
    * refactor: 增加 scss 识别策略
    
    * refactor: 增加接口实现
    
    * test: 增加单元测试
    
    * style: 调整图标主题样式
    
    * style: Main 样式变量化
    
    * style: 拖动条样式变量化

--- src/BootstrapBlazor/Services/ITableExport.cs
+++ src/BootstrapBlazor/Services/ITableExport.cs
@@ -18,6 +18,15 @@ public interface ITableExport
     Task<bool> ExportAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, string? fileName = null);
 
     /// <summary>
+    /// 导出 方法
+    /// </summary>
+    /// <param name="items">导出数据集合</param>
+    /// <param name="cols">当前可见列数据集合 默认 null 导出全部列</param>
+    /// <param name="options">导出配置实例</param>
+    /// <param name="fileName">文件名 默认 null ExportData_{DateTime.Now:yyyyMMddHHmmss}.xlsx</param>
+    Task<bool> ExportAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => ExportAsync(items, cols, fileName);
+
+    /// <summary>
     /// 导出 Excel 方法
     /// </summary>
     /// <param name="items">导出数据集合</param>
@@ -32,7 +41,7 @@ public interface ITableExport
     /// <param name="cols">当前可见列数据集合 默认 null 导出全部列</param>
     /// <param name="options">导出配置实例</param>
     /// <param name="fileName">文件名 默认 null ExportData_{DateTime.Now:yyyyMMddHHmmss}.xlsx</param>
-    Task<bool> ExportExcelAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => Task.FromResult(false);
+    Task<bool> ExportExcelAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => ExportExcelAsync(items, cols, fileName);
 
     /// <summary>
     /// 导出 Csv 方法
@@ -49,7 +58,7 @@ public interface ITableExport
     /// <param name="cols">当前可见列数据集合 默认 null 导出全部列</param>
     /// <param name="options">导出配置实例</param>
     /// <param name="fileName">文件名 默认 null ExportData_{DateTime.Now:yyyyMMddHHmmss}.xlsx</param>
-    Task<bool> ExportCsvAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => Task.FromResult(false);
+    Task<bool> ExportCsvAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => ExportCsvAsync(items, cols, fileName);
 
     /// <summary>
     /// 导出 Pdf 方法
@@ -66,5 +75,5 @@ public interface ITableExport
     /// <param name="cols">当前可见列数据集合 默认 null 导出全部列</param>
     /// <param name="options">导出配置实例</param>
     /// <param name="fileName">文件名 默认 null ExportData_{DateTime.Now:yyyyMMddHHmmss}.xlsx</param>
-    Task<bool> ExportPdfAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => Task.FromResult(false);
+    Task<bool> ExportPdfAsync<TModel>(IEnumerable<TModel> items, IEnumerable<ITableColumn>? cols, TableExportOptions options, string? fileName = null) => ExportPdfAsync(items, cols, fileName);
 }
