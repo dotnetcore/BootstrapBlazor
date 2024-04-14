ad709c61c7d26e4619fb1457f4b004e6236b2b34
Argo-AscioTech <argo@live.ca>
2024-04-14 15:01:45 +0800

    refactor: 增加 AssemblyExtensions 提高代码复用率

--- src/BootstrapBlazor/Localization/Json/JsonStringLocalizer.cs
+++ src/BootstrapBlazor/Localization/Json/JsonStringLocalizer.cs
@@ -102,9 +102,7 @@ internal class JsonStringLocalizer(Assembly assembly, string typeName, string ba
     {
         string? ret = null;
         var cultureName = CultureInfo.CurrentUICulture.Name;
-        var cacheKey = $"{nameof(GetValueFromCache)}&name={name}&{(Assembly.IsCollectible
-            ? Assembly.GetHashCode()
-            : Assembly.GetName().Name)}&type={typeName}&culture={cultureName}";
+        var cacheKey = $"{nameof(GetValueFromCache)}&name={name}&{Assembly.GetUniqueName()}&type={typeName}&culture={cultureName}";
         if (!CacheManager.GetMissingLocalizerByKey(cacheKey))
         {
             var l = GetLocalizedString();
@@ -135,9 +133,7 @@ internal class JsonStringLocalizer(Assembly assembly, string typeName, string ba
     {
         string? ret = null;
         var cultureName = CultureInfo.CurrentUICulture.Name;
-        var cacheKey = $"{nameof(GetLocalizerValueFromCache)}&name={name}&{(Assembly.IsCollectible
-            ? Assembly.GetHashCode()
-            : Assembly.GetName().Name)}&type={typeName}&culture={cultureName}";
+        var cacheKey = $"{nameof(GetLocalizerValueFromCache)}&name={name}&{Assembly.GetUniqueName()}&type={typeName}&culture={cultureName}";
         if (!CacheManager.GetMissingLocalizerByKey(cacheKey))
         {
             var l = localizer[name];
