2effbc173de4654d365c0bb7b0c3bf7bb3931507
Argo Zhang <argo@live.ca>
2024-04-21 09:17:53 +0800

    refactor(DockView): missing ComponentName parameter (#3326)
    * doc: 更新注释
    
    * revert: 撤销删除操作
    
    * chore: bump version 8.0.7
    
    * chore: 更新 Dock 包
    
    * test: 更新单元测试框架包
    
    * doc: 调整高度
    
    * chore: 增加 watch 启动项

--- src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockComponent.cs
+++ src/Extensions/Components/BootstrapBlazor.Dock/Components/DockView/DockComponent.cs
@@ -13,6 +13,12 @@ namespace BootstrapBlazor.Components;
 public class DockComponent : DockComponentBase
 {
     /// <summary>
+    /// 获得/设置 组件名称 默认 component golden-layout 渲染使用
+    /// </summary>
+    [Parameter]
+    public string ComponentName { get; set; } = "component";
+
+    /// <summary>
     /// 获得/设置 组件 Title
     /// </summary>
     [Parameter]
