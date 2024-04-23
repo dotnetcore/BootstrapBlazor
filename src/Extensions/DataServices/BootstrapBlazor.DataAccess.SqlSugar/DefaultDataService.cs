103869fc36fa9ebaeea656e2173b58c421a2e501
Argo-AscioTech <argo@live.ca>
2024-04-23 16:03:04 +0800

    chore: bump version 8.0.0

---
+++ src/Extensions/DataServices/BootstrapBlazor.DataAccess.SqlSugar/DefaultDataService.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+
+namespace BootstrapBlazor.DataAccess.SqlSugar;
+
+
+/// <summary>
+/// SqlSugar ORM 的 IDataService 接口实现
+/// </summary>
+class DefaultDataService<TModel>(ISqlSugarClient db) : DataServiceBase<TModel> where TModel : class, new()
+{
+    /// <summary>
+    /// 删除方法
+    /// </summary>
+    /// <param name="models"></param>
+    /// <returns></returns>
+    public override async Task<bool> DeleteAsync(IEnumerable<TModel> models)
+    {
+        // 通过模型获取主键列数据
+        // 支持批量删除
+        await db.Deleteable<TModel>(models).ExecuteCommandAsync();
+        return true;
+    }
+
+    /// <summary>
+    /// 保存方法
+    /// </summary>
+    /// <param name="model"></param>
+    /// <param name="changedType"></param>
+    /// <returns></returns>
+    public override async Task<bool> SaveAsync(TModel model, ItemChangedType changedType)
+    {
+        await db.Storageable(model).ExecuteCommandAsync();
+        return true;
+    }
+
+    /// <summary>
+    /// 查询方法
+    /// </summary>
+    /// <param name="option"></param>
+    /// <returns></returns>
+    public override Task<QueryData<TModel>> QueryAsync(QueryPageOptions option)
+    {
+        int count = 0;
+
+        var filter = option.ToFilter();
+
+        var items = db.Queryable<TModel>()
+            .WhereIF(filter.HasFilters(), filter.GetFilterLambda<TModel>())
+            .OrderByIF(option.SortOrder != SortOrder.Unset, $"{option.SortName} {option.SortOrder}")
+            .ToPageList(option.PageIndex, option.PageItems, ref count);
+
+        var ret = new QueryData<TModel>()
+        {
+            TotalCount = count,
+            Items = items,
+            IsSorted = option.SortOrder != SortOrder.Unset,
+            IsFiltered = option.Filters.Count > 0,
+            IsAdvanceSearch = option.AdvanceSearches.Count > 0,
+            IsSearch = option.Searches.Count > 0 || option.CustomerSearches.Count > 0
+        };
+        return Task.FromResult(ret);
+    }
+}
---
+++ src/Extensions/DataServices/BootstrapBlazor.DataAccess.SqlSugar/SqlSugarServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using BootstrapBlazor.Components;
+using BootstrapBlazor.DataAccess.SqlSugar;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// BootstrapBlazor SqlSugar 服务扩展类
+/// </summary>
+public static class SqlSugarServiceCollectionExtensions
+{
+    /// <summary>
+    /// 增加 SqlSugar 数据库操作服务
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="optionsAction"></param>
+    /// <param name="configureAction"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddSqlSugar(this IServiceCollection services,
+        Action<ConnectionConfig> optionsAction,
+        Action<SqlSugarClient>? configureAction = null)
+    {
+        services.AddSingleton<ISqlSugarClient>(sp =>
+        {
+            var config = new ConnectionConfig();
+            optionsAction(config);
+            return new SqlSugarScope(config, configureAction);
+        });
+        services.AddScoped(typeof(IDataService<>), typeof(DefaultDataService<>));
+        return services;
+    }
+}
