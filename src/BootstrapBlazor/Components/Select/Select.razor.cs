bb6e743ea20a9909e57b5ea21baad345afefc9fb
lee <lish@live.cn>
2024-04-24 16:59:25 +0800

    新增OnClear回调方法

--- src/BootstrapBlazor/Components/Select/Select.razor.cs
+++ src/BootstrapBlazor/Components/Select/Select.razor.cs
@@ -172,6 +172,12 @@ public partial class Select<TValue> : ISelect
     public string? DefaultVirtualizeItemText { get; set; }
 
     /// <summary>
+    /// 获得/设置 清除文本内容 OnClear 回调方法 默认
+    /// </summary>
+    [Parameter]
+    public EventCallback OnClear { get; set; }
+
+    /// <summary>
     /// 获得/设置 禁止首次加载时触发 OnSelectedItemChanged 回调方法 默认 false
     /// </summary>
     [Parameter]
@@ -438,13 +444,17 @@ public partial class Select<TValue> : ISelect
     /// </summary>
     public void ClearSearchText() => SearchText = null;
 
-    private void OnClearValue()
+    private async Task OnClearValue()
     {
         if (ShowSearch)
         {
             ClearSearchText();
         }
         CurrentValue = default;
+        if (OnClear.HasDelegate)
+        {
+            await OnClear.InvokeAsync();
+        }
     }
 
     private async Task OnChange(ChangeEventArgs args)
