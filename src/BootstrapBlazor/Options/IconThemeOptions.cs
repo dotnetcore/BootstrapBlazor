d3e7be120b625529ea5e87a6855060dfda88af78
Argo-AscioTech <argo@live.ca>
2024-04-29 20:10:11 +0800

    feat: 增加 FullScreenExitButtonIcon 图标

--- src/BootstrapBlazor/Options/IconThemeOptions.cs
+++ src/BootstrapBlazor/Options/IconThemeOptions.cs
@@ -92,6 +92,7 @@ public class IconThemeOptions
         { ComponentIcons.TableFilterMinusIcon, "fa-solid fa-minus" },
 
         { ComponentIcons.FullScreenButtonIcon, "fa-solid fa-maximize" },
+        { ComponentIcons.FullScreenExitButtonIcon, "fa-solid fa-compress" },
 
         { ComponentIcons.GoTopIcon, "fa-solid fa-angle-up" },
 
