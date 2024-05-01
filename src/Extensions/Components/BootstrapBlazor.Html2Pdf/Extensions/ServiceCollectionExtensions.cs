a341f59c092dca0fb4de10331dd325c13b1fa99b
Argo-AscioTech <argo@live.ca>
2024-05-01 16:34:30 +0800

    refactor: 更改 IHtml2Pdf 服务生命周期为 Scope

--- src/Extensions/Components/BootstrapBlazor.Html2Pdf/Extensions/ServiceCollectionExtensions.cs
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/Extensions/ServiceCollectionExtensions.cs
@@ -18,7 +18,7 @@ public static class BootstrapBlazorHtml2PdfServiceExtensions
     /// <param name="services"></param>
     public static IServiceCollection AddBootstrapBlazorHtml2PdfService(this IServiceCollection services)
     {
-        services.TryAddSingleton<IHtml2Pdf, DefaultPdfService>();
+        services.AddScoped<IHtml2Pdf, DefaultPdfService>();
 
         return services;
     }
