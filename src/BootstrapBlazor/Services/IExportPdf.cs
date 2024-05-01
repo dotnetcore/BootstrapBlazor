4c5a07e2596779968cb5cc98188e40873f68ac5e
Argo-AscioTech <argo@live.ca>
2024-05-01 16:36:49 +0800

    refactor: 移除 IExportPdf 服务精简逻辑

--- src/BootstrapBlazor/Services/IExportPdf.cs
+++ src/BootstrapBlazor/Services/IExportPdf.cs
@@ -7,6 +7,7 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// 导出 Pdf 接口
 /// </summary>
+[Obsolete("已弃用，删除即可，组件内部已不使用，直接调用 IHtml2Pdf 接口方法")]
 public interface IExportPdf
 {
     /// <summary>
