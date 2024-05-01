1fc0f7ab25a111ac437ae807d41c2d2273c7c9e9
Argo-AscioTech <argo@live.ca>
2024-05-01 16:35:02 +0800

    feat: 实现 PdfDataFromHtmlAsync 方法

--- src/Extensions/Components/BootstrapBlazor.Html2Pdf/Services/DefaultPdfService.cs
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf/Services/DefaultPdfService.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.AspNetCore.Components;
 using PuppeteerSharp;
 
 namespace BootstrapBlazor.Components;
@@ -9,17 +10,14 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// 默认 Html to Pdf 实现 
 /// </summary>
-class DefaultPdfService : IHtml2Pdf
+class DefaultPdfService(NavigationManager navigationManager) : IHtml2Pdf
 {
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
     public async Task<byte[]> PdfDataAsync(string url)
     {
-        using var browserFetcher = new BrowserFetcher();
-        await browserFetcher.DownloadAsync();
-
-        await using var browser = await Puppeteer.LaunchAsync(CreateOptions());
+        await using var browser = await LaunchBrowserAsync();
         await using var page = await browser.NewPageAsync();
         await page.GoToAsync(url);
 
@@ -31,15 +29,108 @@ class DefaultPdfService : IHtml2Pdf
     /// </summary>
     public async Task<Stream> PdfStreamAsync(string url)
     {
-        using var browserFetcher = new BrowserFetcher();
-        await browserFetcher.DownloadAsync();
-
-        await using var browser = await Puppeteer.LaunchAsync(CreateOptions());
+        await using var browser = await LaunchBrowserAsync();
         await using var page = await browser.NewPageAsync();
         await page.GoToAsync(url);
 
         return await page.PdfStreamAsync();
     }
 
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public async Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
+    {
+        await using var browser = await LaunchBrowserAsync();
+        await using var page = await browser.NewPageAsync();
+        await page.SetContentAsync(html);
+
+        if (links != null)
+        {
+            foreach (var link in links)
+            {
+                await page.AddStyleTagAsync(link);
+            }
+        }
+        if (scripts != null)
+        {
+            foreach (var script in scripts)
+            {
+                await page.AddScriptTagAsync(script);
+            }
+        }
+
+        return await page.PdfDataAsync();
+    }
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public async Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
+    {
+        await using var browser = await LaunchBrowserAsync();
+        await using var page = await browser.NewPageAsync();
+        await page.SetContentAsync(html);
+
+        await AddWebsiteLinks(page, links);
+        await AddWebsiteScripts(page, scripts);
+
+        return await page.PdfStreamAsync();
+    }
+
+    private async Task AddWebsiteLinks(IPage page, IEnumerable<string>? links = null)
+    {
+        var baseUri = navigationManager.BaseUri;
+        var websiteLinks = new List<string>()
+        {
+#if DEBUG
+            $"{baseUri}_content/BootstrapBlazor.FontAwesome/css/font-awesome.min.css",
+            $"{baseUri}_content/BootstrapBlazor.MaterialDesign/css/md.min.css",
+            $"{baseUri}_content/BootstrapBlazor.BootstrapIcon/css/bootstrap-icons.min.css",
+            $"{baseUri}_content/BootstrapBlazor/css/bootstrap.blazor.bundle.min.css",
+            $"{baseUri}_content/BootstrapBlazor/css/motronic.min.css"
+#endif
+        };
+
+        if (links != null)
+        {
+            websiteLinks.AddRange(links);
+        }
+
+        foreach (var link in websiteLinks)
+        {
+            await page.AddStyleTagAsync(link);
+        }
+    }
+
+    private static async Task AddWebsiteScripts(IPage page, IEnumerable<string>? scripts = null)
+    {
+        var websiteScripts = new List<string>();
+
+        if (scripts != null)
+        {
+            websiteScripts.AddRange(scripts);
+        }
+
+        foreach (var script in websiteScripts)
+        {
+            await page.AddScriptTagAsync(script);
+        }
+    }
+
     private static LaunchOptions CreateOptions() => new() { Headless = true };
+
+    private static async Task<IBrowser> LaunchBrowserAsync()
+    {
+        using var browserFetcher = new BrowserFetcher();
+        await browserFetcher.DownloadAsync();
+
+        return await Puppeteer.LaunchAsync(CreateOptions());
+    }
 }
