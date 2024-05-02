7864edda76c20496c1699487322937275675206b
Argo-AscioTech <argo@live.ca>
2024-05-03 00:04:11 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/DockViews/DockViewCol.razor.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples.DockViews;
+public partial class DockViewCol
+{
+    [Inject, NotNull]
+    private ToastService? ToastService { get; set; }
+
+    private void OnClickGear()
+    {
+        ToastService.Success("DockView", "Trigger click event from gear icon");
+    }
+}
