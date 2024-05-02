f970f840951763f3c2c028aaaaea3598d6d0feb0
Argo Zhang <argo@live.ca>
2024-05-02 09:46:48 +0800

    doc(IHtml2Pdf): update document for IHtml2Pdf (#3401)
    * doc: 更新导出示例代码
    
    * refactor: 移动服务到代码文件中
    
    * doc: 更新 Html2Pdf 示例
    
    * refactor: 增加内置 IHtml2Pdf 服务实现
    
    * doc: 增加服务提示信息
    
    * doc: 更新文档
    
    * test: 增加单元测试
    
    * doc: 更新文档
    
    * doc: 本地化
    
    * doc: 增加 PuppeterSharp 说明

---
+++ src/BootstrapBlazor/Services/DefaultHtml2PdfService.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 默认 Html to Pdf 实现 
+/// </summary>
+class DefaultHtml2PdfService : IHtml2Pdf
+{
+    private const string ErrorMessage = "请增加依赖包 BootstrapBlazor.Html2Pdf 通过 AddBootstrapBlazorHtml2PdfService 进行服务注入; Please add BootstrapBlazor.Html2Pdf package and use AddBootstrapBlazorHtml2PdfService inject service";
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public Task<byte[]> PdfDataAsync(string url) => throw new NotImplementedException(ErrorMessage);
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public Task<Stream> PdfStreamAsync(string url) => throw new NotImplementedException(ErrorMessage);
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null) => throw new NotImplementedException(ErrorMessage);
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="html">html raw string</param>
+    /// <param name="links"></param>
+    /// <param name="scripts"></param>
+    public Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null) => throw new NotImplementedException(ErrorMessage);
+}
