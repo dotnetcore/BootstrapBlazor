808959d1cc7c54b4f627de8e33464bbb3c21be48
Argo-AscioTech <argo@live.ca>
2024-05-03 21:58:10 +0800

    feat: 增加 ExportPdfButton 组件

--- src/BootstrapBlazor/Components/Button/ExportPdfButton.cs
+++ src/BootstrapBlazor/Components/Button/ExportPdfButton.cs
@@ -7,8 +7,48 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// 导出 Pdf 按钮
 /// </summary>
+[BootstrapModuleAutoLoader(ModuleName = "utility", AutoInvokeInit = false, AutoInvokeDispose = false)]
 public class ExportPdfButton : Button
 {
+    /// <summary>
+    /// 获得/设置 导出 Pdf 选择器 默认为 null
+    /// </summary>
+    [Parameter]
+    public string? Selector { get; set; }
+
+    /// <summary>
+    /// 获得/设置 导出 Pdf 元素 Id 默认为 null 
+    /// </summary>
+    [Parameter]
+    public string? ElementId { get; set; }
+
+    /// <summary>
+    /// 获得/设置 导出 Pdf 所需样式表文件集合 默认为 null
+    /// </summary>
+    [Parameter]
+    public List<string>? StyleTags { get; set; }
+
+    /// <summary>
+    /// 获得/设置 导出 Pdf 所需脚本文件集合 默认为 null
+    /// </summary>
+    [Parameter]
+    public List<string>? ScriptTags { get; set; }
+
+    /// <summary>
+    /// 获得/设置 导出 Pdf 文件名 默认为 null 未设置时使用 pdf-时间戳.pdf
+    /// </summary>
+    [Parameter]
+    public string? PdfFileName { get; set; }
+
+    [Inject, NotNull]
+    private IHtml2Pdf? Html2PdfService { get; set; }
+
+    [Inject, NotNull]
+    private NavigationManager? NavigationManager { get; set; }
+
+    [Inject, NotNull]
+    private DownloadService? DownloadService { get; set; }
+
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
@@ -18,5 +58,48 @@ public class ExportPdfButton : Button
 
         Icon ??= IconTheme.GetIconByKey(ComponentIcons.TableExportPdfIcon);
         ButtonIcon = Icon;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override async Task HandlerClick()
+    {
+        await base.HandlerClick();
+
+        var html = await InvokeAsync<string?>("getHtml", new { Id = ElementId, Selector });
+        if (!string.IsNullOrEmpty(html))
+        {
+            // 通过模板生成完整的 Html
+            var htmlString = $"""
+                <!DOCTYPE html>
+
+                <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+                <head>
+                    <meta charset="utf-8" />
+                </head>
+                <body class="p-3">
+                    {html}
+                </body>
+                </html>
+                """;
+
+            // 增加网页所需样式表文件
+            List<string> styles = [$"{NavigationManager.BaseUri}_content/BootstrapBlazor/css/bootstrap.blazor.bundle.min.css"];
+            if (StyleTags != null)
+            {
+                styles.AddRange(StyleTags);
+            }
+
+            // 增加网页所需脚本文件
+            var scripts = ScriptTags ?? [];
+
+            // 生成 Pdf 流
+            using var stream = await Html2PdfService.PdfStreamFromHtmlAsync(htmlString, styles, scripts);
+
+            // 下载 Pdf 文件
+            await DownloadService.DownloadFromStreamAsync($"pdf-{DateTime.Now:HHmmss}.pdf", stream);
+        }
     }
 }
