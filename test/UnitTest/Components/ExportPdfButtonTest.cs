665e41d8c89403308575453e6c6dc89bd2d7a5fd
Argo-AscioTech <argo@live.ca>
2024-05-03 22:55:53 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Components/ExportPdfButtonTest.cs
@@ -0,0 +1,71 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class ExportPdfButtonTest : ExportPdfTestBase
+{
+    [Fact]
+    public async Task Normal_Ok()
+    {
+        Context.JSInterop.Setup<string?>("getHtml", v => true).SetResult("test-html-result");
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<ExportPdfButton>(pb =>
+            {
+                pb.Add(p => p.Text, "Export Pdf");
+                pb.Add(p => p.ElementId, "test-id");
+                pb.Add(p => p.Selector, ".modal-body");
+            });
+        });
+        var button = cut.Find("button");
+        await cut.InvokeAsync(() => button.Click());
+    }
+
+    [Fact]
+    public async Task Property_Ok()
+    {
+        Context.JSInterop.Setup<string?>("getHtml", v => true).SetResult("test-html-result");
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<ExportPdfButton>(pb =>
+            {
+                pb.Add(p => p.Text, "Export Pdf");
+                pb.Add(p => p.Icon, "fa-solid fa-pdf");
+                pb.Add(p => p.ElementId, "test-id");
+                pb.Add(p => p.Selector, ".modal-body");
+                pb.Add(p => p.StyleTags, ["test.css"]);
+                pb.Add(p => p.ScriptTags, ["test.js"]);
+                pb.Add(p => p.PdfFileName, "test.pdf");
+            });
+        });
+        var button = cut.Find("button");
+        await cut.InvokeAsync(() => button.Click());
+    }
+
+    [Fact]
+    public void ExportPdfButtonOptions_Ok()
+    {
+        var options = new ExportPdfButtonOptions()
+        {
+            ElementId = "test-id",
+            Selector = ".modal-body",
+            StyleTags = ["test.css"],
+            ScriptTags = ["test.js"],
+            FileName = "test.pdf",
+            Color = Color.Primary,
+            Text = "test",
+            Icon = "icon"
+        };
+
+        Assert.Equal("test-id", options.ElementId);
+        Assert.Equal(".modal-body", options.Selector);
+        Assert.Equal(["test.css"], options.StyleTags);
+        Assert.Equal(["test.js"], options.ScriptTags);
+        Assert.Equal(Color.Primary, options.Color);
+        Assert.Equal("test", options.Text);
+        Assert.Equal("icon", options.Icon);
+        Assert.Equal("test.pdf", options.FileName);
+    }
+}
---
+++ test/UnitTest/Core/ExportPdfTestBase.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace UnitTest.Core;
+
+[Collection("ExportPdfTestContext")]
+public class ExportPdfTestBase
+{
+    protected TestContext Context { get; }
+
+    public ExportPdfTestBase()
+    {
+        Context = ExportPdfTestHost.Instance;
+    }
+}
+
+[CollectionDefinition("ExportPdfTestContext")]
+public class ExportPdfTestCollection : ICollectionFixture<ExportPdfTestHost>
+{
+
+}
+
+public class ExportPdfTestHost : IDisposable
+{
+    [NotNull]
+    internal static TestContext? Instance { get; private set; }
+
+    public ExportPdfTestHost()
+    {
+        Instance = new TestContext();
+
+        // Mock 脚本
+        Instance.JSInterop.Mode = JSRuntimeMode.Loose;
+
+        ConfigureServices(Instance.Services);
+
+        ConfigureConfiguration(Instance.Services);
+
+        // 渲染 SwalRoot 组件 激活 ICacheManager 接口
+        Instance.Services.GetRequiredService<ICacheManager>();
+    }
+
+    protected virtual void ConfigureServices(IServiceCollection services)
+    {
+        services.AddBootstrapBlazor();
+        services.AddSingleton<IHtml2Pdf, MockHtml2PdfService>();
+    }
+
+    protected virtual void ConfigureConfiguration(IServiceCollection services)
+    {
+        // 增加单元测试 appsettings.json 配置文件
+        services.AddConfiguration();
+    }
+
+    public void Dispose()
+    {
+        Instance.Dispose();
+        GC.SuppressFinalize(this);
+    }
+}
+
+class MockHtml2PdfService : IHtml2Pdf
+{
+    public Task<byte[]> PdfDataAsync(string url)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> PdfStreamAsync(string url)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null) => Task.FromResult<Stream>(new MemoryStream(Encoding.UTF8.GetBytes("Hello World")));
+}
