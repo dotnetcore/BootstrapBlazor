8efe42a9ce2ceb24e7168a5d51d63f80e98ffb98
Argo-AscioTech <argo@live.ca>
2024-05-04 23:06:23 +0800

    refactor: 更新 Options 类

--- src/BootstrapBlazor/Options/ExportPdfButtonOptions.cs
+++ src/BootstrapBlazor/Options/ExportPdfButtonOptions.cs
@@ -48,4 +48,24 @@ public class ExportPdfButtonOptions
     /// 获得/设置 导出 Pdf 文件名 默认为 null 未设置时使用 pdf-时间戳.pdf
     /// </summary>
     public string? FileName { get; set; }
+
+    /// <summary>
+    /// 获得/设置 导出 Pdf 之前回调委托 默认为 null
+    /// </summary>
+    public Func<Task>? OnBeforeExport { get; set; }
+
+    /// <summary>
+    /// 获得/设置 下载 Pdf 之前回调委托 默认为 null
+    /// </summary>
+    public Func<Stream, Task>? OnBeforeDownload { get; set; }
+
+    /// <summary>
+    /// 获得/设置 下载 Pdf 之后回调委托 默认为 null
+    /// </summary>
+    public Func<string, Task>? OnAfterDownload { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否自动下载 Pdf 默认为 false
+    /// </summary>
+    public bool AutoDownload { get; set; }
 }
