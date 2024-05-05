4f7f285ea99f1e191cca508e17c0038ba229e3ce
Argo-AscioTech <argo@live.ca>
2024-05-05 12:56:01 +0800

    doc: 更新初始化主题逻辑

--- src/BootstrapBlazor.Server/wwwroot/lib/theme.js
+++ src/BootstrapBlazor.Server/wwwroot/lib/theme.js
@@ -1,3 +1,4 @@
-﻿import { getPreferredTheme, setTheme } from "../../_content/BootstrapBlazor/modules/theme.js"
+﻿import { getPreferredTheme } from "../../_content/BootstrapBlazor/modules/theme.js"
+import { setTheme } from "../../_content/BootstrapBlazor/modules/utility.js"
 
 setTheme(getPreferredTheme())
