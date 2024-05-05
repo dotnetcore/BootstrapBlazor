ebef4b8f2dd6e3bdffc3b421be7c3a863ed0a6a9
Argo-AscioTech <argo@live.ca>
2024-05-05 12:28:29 +0800

    doc: 增加本地化

--- src/BootstrapBlazor/Components/ThemeProvider/ThemeProvider.razor.cs
+++ src/BootstrapBlazor/Components/ThemeProvider/ThemeProvider.razor.cs
@@ -2,6 +2,8 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+using Microsoft.Extensions.Localization;
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -16,16 +18,34 @@ public partial class ThemeProvider
     public string? AutoModeIcon { get; set; }
 
     /// <summary>
+    /// 获得/设置 自动模式文本 默认 null 未设置使用本地化资源
+    /// </summary>
+    [Parameter]
+    public string? AutoModeText { get; set; }
+
+    /// <summary>
     /// 获得/设置 暗黑模式图标 默认 null
     /// </summary>
     [Parameter]
     public string? DarkModeIcon { get; set; }
 
     /// <summary>
+    /// 获得/设置 暗黑模式文本 默认 null 未设置使用本地化资源
+    /// </summary>
+    [Parameter]
+    public string? DarkModeText { get; set; }
+
+    /// <summary>
     /// 获得/设置 明亮模式图标 默认 null
     /// </summary>
     [Parameter]
     public string? LightModeIcon { get; set; }
+
+    /// <summary>
+    /// 获得/设置 明亮模式文本 默认 null 未设置使用本地化资源
+    /// </summary>
+    [Parameter]
+    public string? LightModeText { get; set; }
 
     /// <summary>
     /// 获得/设置 当前选中模式图标 默认 null
@@ -35,6 +55,9 @@ public partial class ThemeProvider
 
     [Inject, NotNull]
     private IIconTheme? IconTheme { get; set; }
+
+    [Inject, NotNull]
+    private IStringLocalizer<ThemeProvider>? Localizer { get; set; }
 
     private string? ClassString => CssBuilder.Default("dropdown bb-theme-mode")
         .AddClassFromAttributes(AdditionalAttributes)
@@ -51,5 +74,9 @@ public partial class ThemeProvider
         DarkModeIcon ??= IconTheme.GetIconByKey(ComponentIcons.ThemeProviderDarkModeIcon);
         LightModeIcon ??= IconTheme.GetIconByKey(ComponentIcons.ThemeProviderLightModeIcon);
         ActiveIcon ??= IconTheme.GetIconByKey(ComponentIcons.ThemeProviderActiveModeIcon);
+
+        AutoModeText ??= Localizer[nameof(AutoModeText)];
+        DarkModeText ??= Localizer[nameof(DarkModeText)];
+        LightModeText ??= Localizer[nameof(LightModeText)];
     }
 }
