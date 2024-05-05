34aad34f81793f39351c49ecba1a84b3ee43f051
Argo-AscioTech <argo@live.ca>
2024-05-05 12:19:52 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Components/ThemeProviderTest.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class ThemeProviderTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void ThemeProvider_Ok()
+    {
+        var cut = Context.RenderComponent<ThemeProvider>();
+        cut.Contains("dropdown bb-theme-mode");
+    }
+}
