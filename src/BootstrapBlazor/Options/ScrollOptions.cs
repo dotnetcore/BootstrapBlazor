52fc072f9f469a32a8c300d0872d1c49b3037a33
Argo-AscioTech <argo@live.ca>
2024-05-07 10:59:20 +0800

    refactor: 更新滚动条宽度

--- src/BootstrapBlazor/Options/ScrollOptions.cs
+++ src/BootstrapBlazor/Options/ScrollOptions.cs
@@ -10,12 +10,12 @@ namespace BootstrapBlazor.Components;
 public class ScrollOptions
 {
     /// <summary>
-    /// 获得/设置 滚动条宽度 默认 4px
+    /// 获得/设置 滚动条宽度 默认 5px
     /// </summary>
-    public int ScrollWidth { get; set; } = 4;
+    public int ScrollWidth { get; set; } = 5;
 
     /// <summary>
-    /// 获得/设置 滚动条鼠标悬浮宽度 默认 4px
+    /// 获得/设置 滚动条鼠标悬浮宽度 默认 5px
     /// </summary>
-    public int ScrollHoverWidth { get; set; } = 4;
+    public int ScrollHoverWidth { get; set; } = 5;
 }
