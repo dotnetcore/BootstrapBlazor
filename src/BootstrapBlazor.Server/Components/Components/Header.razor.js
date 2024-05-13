0a7f84ce8cd75914bc8cfdcaa942f950dc6b846a
Argo Zhang <argo@live.ca>
2024-05-13 22:25:24 +0800

    doc(Theme): add switch theme transition (#3496)
    * refactor: 更新搜索弹窗 z-index
    
    * feat: 修复手机模式切换明亮暗黑模式按钮
    
    * refactor: 增加点击事件释放逻辑
    
    * feat: 增加主题切换特效
    
    * chore: 更新变量名称

--- src/BootstrapBlazor.Server/Components/Components/Header.razor.js
+++ src/BootstrapBlazor.Server/Components/Components/Header.razor.js
@@ -1,6 +1,8 @@
-﻿import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
+﻿import { getTheme, setTheme } from "../../_content/BootstrapBlazor/modules/utility.js"
+import Data from "../../_content/BootstrapBlazor/modules/data.js"
+import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
 
-export function init() {
+export function init(id) {
     const scrollTop = () => (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
     let prevScrollTop = 0;
     EventHandler.on(document, 'scroll', () => {
@@ -13,9 +15,36 @@ export function init() {
             items.forEach(item => item.classList.remove('hide'))
         }
         prevScrollTop = currentScrollTop
-    })
+    });
+
+    const themeElement = document.querySelector('.icon-theme');
+    if (themeElement) {
+        EventHandler.on(themeElement, 'click', e => {
+            let theme = getTheme();
+            if (theme === 'dark') {
+                theme = 'light';
+            }
+            else {
+                theme = 'dark';
+            }
+            document.documentElement.style.setProperty('--bb-theme-x', `${window.innerWidth}px`);
+            document.documentElement.style.setProperty('--bb-theme-y', `${window.innerHeight}px`);
+            document.startViewTransition(() => {
+                setTheme(theme, true);
+            });
+        });
+    }
+
+    Data.set(id, themeElement);
 }
 
-export function dispose() {
-    EventHandler.off(document, 'scroll')
+export function dispose(id) {
+    EventHandler.off(document, 'scroll');
+
+    const themeElement = Data.get(id);
+    Data.remove(id);
+
+    if (themeElement) {
+        EventHandler.off(themeElement, 'click');
+    }
 }
