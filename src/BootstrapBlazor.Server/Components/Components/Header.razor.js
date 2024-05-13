1044818b460547691b6e43dd722b78127c070aee
Argo-AscioTech <argo@live.ca>
2024-05-13 20:43:40 +0800

    refactor: 增加点击事件释放逻辑

--- src/BootstrapBlazor.Server/Components/Components/Header.razor.js
+++ src/BootstrapBlazor.Server/Components/Components/Header.razor.js
@@ -1,7 +1,8 @@
 ﻿import { getTheme, setTheme } from "../../_content/BootstrapBlazor/modules/utility.js"
+import Data from "../../_content/BootstrapBlazor/modules/data.js"
 import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
 
-export function init() {
+export function init(id) {
     const scrollTop = () => (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop
     let prevScrollTop = 0;
     EventHandler.on(document, 'scroll', () => {
@@ -16,23 +17,30 @@ export function init() {
         prevScrollTop = currentScrollTop
     });
 
-    const themeElements = document.querySelectorAll('.icon-theme > i');
-    if (themeElements) {
-        themeElements.forEach(el => {
-            EventHandler.on(el, 'click', e => {
-                let theme = getTheme();
-                if (theme === 'dark') {
-                    theme = 'light';
-                }
-                else {
-                    theme = 'dark';
-                }
-                setTheme(theme, true);
-            });
+    const themeElement = document.querySelector('.icon-theme');
+    if (themeElement) {
+        EventHandler.on(themeElement, 'click', e => {
+            let theme = getTheme();
+            if (theme === 'dark') {
+                theme = 'light';
+            }
+            else {
+                theme = 'dark';
+            }
+            setTheme(theme, true);
         });
     }
+
+    Data.set(id, themeElement);
 }
 
-export function dispose() {
-    EventHandler.off(document, 'scroll')
+export function dispose(id) {
+    EventHandler.off(document, 'scroll');
+
+    const themeElement = Data.get(id);
+    Data.remove(id);
+
+    if (themeElement) {
+        EventHandler.off(themeElement, 'click');
+    }
 }
