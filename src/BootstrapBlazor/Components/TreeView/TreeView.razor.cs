0a3412ec349dc7de5d4b1f93d98212020ea7f7b0
Argo-AscioTech <argo@live.ca>
2024-05-15 13:23:14 +0800

    refactor: 代码格式化

--- src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
+++ src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
@@ -463,7 +463,7 @@ public partial class TreeView<TItem> : IModelEqualityComparer<TItem>
 
         if (ShowCheckbox)
         {
-            if(!AutoCheckChildren&&AutoCheckParent&&node.Items.Any())
+            if (!AutoCheckChildren && AutoCheckParent && node.Items.Any())
             {
                 node.Items[0].SetParentCheck(node.Items[0].CheckedState, TreeNodeStateCache);
             }
