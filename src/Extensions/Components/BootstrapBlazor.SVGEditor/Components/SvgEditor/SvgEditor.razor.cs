a141cbc414c50d6b9bf041014ebb1ea2326da620
zhangpeihang <49146231+Vision-Zhang@users.noreply.github.com>
2024-05-16 08:55:54 +0800

    feat(SvgEdtior): add UpdateAsync method for the SvgEditor (#3512)
    * feat: 添加更新内容实例方法
    
    * feat: 更新版本号
    
    * refactor: 精简代码
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.cs
+++ src/Extensions/Components/BootstrapBlazor.SVGEditor/Components/SvgEditor/SvgEditor.razor.cs
@@ -4,55 +4,51 @@
 
 using Microsoft.AspNetCore.Components;
 
-namespace BootstrapBlazor.Components
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// SvgEditor component
+/// </summary>
+public partial class SvgEditor
 {
     /// <summary>
-    /// SvgEditor component
+    /// 获得/设置 首次加载内容
     /// </summary>
-    public partial class SvgEditor
+    [Parameter]
+    [NotNull]
+    public string? PreContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 保存编辑器内容回调
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public Func<string, Task>? OnSaveChanged { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, new { Interop, Preload = PreContent, Callback = nameof(GetContent) });
+
+    /// <summary>
+    /// 更新内容方法
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns></returns>
+    public async Task UpdateAsync(string content) => await InvokeVoidAsync("update", Id, content);
+
+    /// <summary>
+    /// 获得内容方法 由 JavaScript 调用
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task GetContent(string value)
     {
-        /// <summary>
-        /// 获得/设置 首次加载内容
-        /// </summary>
-        [Parameter]
-        [NotNull]
-        public string? PreContent { get; set; }
-
-
-        /// <summary>
-        /// 获得/设置 保存编辑器内容回调
-        /// </summary>
-        [Parameter]
-        [NotNull]
-        public Func<string, Task>? OnSaveChanged { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="firstRender"></param>
-        /// <returns></returns>
-        protected override async Task OnAfterRenderAsync(bool firstRender)
+        if (OnSaveChanged != null)
         {
-            await base.OnAfterRenderAsync(firstRender);
-
-            if (firstRender)
-            {
-                await InvokeVoidAsync("init", Id, PreContent, Interop, nameof(GetContent));
-            }
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="value"></param>
-        /// <returns></returns>
-        [JSInvokable]
-        public async Task GetContent(string value)
-        {
-            if (OnSaveChanged != null)
-            {
-                await OnSaveChanged(value);
-            }
+            await OnSaveChanged(value);
         }
     }
 }
