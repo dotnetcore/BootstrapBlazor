41d807f070f2b8debb8ea9f64acfca8cf826d2f0
Argo-Supper <argo@live.ca>
2024-05-18 19:18:53 +0800

    feat: 增加 WebClientOptions 配置类

---
+++ src/BootstrapBlazor/Options/WebClientOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// WebClient 
+/// </summary>
+public class WebClientOptions
+{
+    /// <summary>
+    /// 获得/设置 是否开启 IP 定位功能 默认 false 未开启
+    /// </summary>
+    public bool EnableIpLocator { get; set; }
+}
