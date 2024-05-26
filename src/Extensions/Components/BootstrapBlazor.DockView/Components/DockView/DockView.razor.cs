f475af68356b91ab2ea259246765023b52fa06b2
Argo-AscioTech <argo@live.ca>
2024-05-26 15:59:43 +0800

    chore: 更改工程名称

---
+++ src/Extensions/Components/BootstrapBlazor.DockView/Components/DockView/DockView.razor.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class DockView
+{
+    /// <summary>
+    /// 
+    /// </summary>
+#if NET6_0_OR_GREATER
+    [EditorRequired]
+#endif
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private List<DockContent2>? Content { get; set; } = new();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            await InvokeVoidAsync("init", Id, Content);
+        }
+    }
+}
