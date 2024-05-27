6b51c62c247f6ce3e04fcc182cbc8fc57eed730d
Argo-Supper <argo@live.ca>
2024-05-27 14:36:45 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Components/Drawer/DrawerContainer.cs
+++ src/BootstrapBlazor/Components/Drawer/DrawerContainer.cs
@@ -36,33 +36,7 @@ public class DrawerContainer : ComponentBase, IDisposable
     {
         if (_option != null)
         {
-            var parameters = new Dictionary<string, object>()
-            {
-                [nameof(Drawer.IsOpen)] = true,
-                [nameof(Drawer.IsBackdrop)] = _option.IsBackdrop,
-                [nameof(Drawer.ShowBackdrop)] = _option.ShowBackdrop,
-                [nameof(Drawer.Placement)] = _option.Placement,
-                [nameof(Drawer.AllowResize)] = _option.AllowResize
-            };
-            if (!string.IsNullOrEmpty(_option.Width))
-            {
-                parameters.Add(nameof(Drawer.Width), _option.Width);
-            }
-            if (!string.IsNullOrEmpty(_option.Height))
-            {
-                parameters.Add(nameof(Drawer.Height), _option.Height);
-            }
-            if (_option.ChildContent != null)
-            {
-                parameters.Add(nameof(Drawer.ChildContent), _option.ChildContent);
-            }
-            parameters.Add(nameof(Drawer.IsOpenChanged), EventCallback.Factory.Create<bool>(this, v =>
-            {
-                _option = null;
-            }));
-            builder.OpenComponent<Drawer>(0);
-            builder.AddMultipleAttributes(1, parameters);
-            builder.CloseComponent();
+            RenderDrawer(builder, _option);
         }
     }
 
@@ -70,6 +44,52 @@ public class DrawerContainer : ComponentBase, IDisposable
     {
         _option = option;
         await InvokeAsync(StateHasChanged);
+    }
+
+    private void RenderDrawer(RenderTreeBuilder builder, DrawerOption option)
+    {
+        builder.OpenComponent<Drawer>(0);
+        builder.SetKey(option);
+        builder.AddMultipleAttributes(1, GetParameters(option));
+        builder.CloseComponent();
+    }
+
+    private Dictionary<string, object> GetParameters(DrawerOption option)
+    {
+        var parameters = new Dictionary<string, object>()
+        {
+            [nameof(Drawer.IsOpen)] = true,
+            [nameof(Drawer.IsBackdrop)] = option.IsBackdrop,
+            [nameof(Drawer.ShowBackdrop)] = option.ShowBackdrop,
+            [nameof(Drawer.Placement)] = option.Placement,
+            [nameof(Drawer.AllowResize)] = option.AllowResize,
+            [nameof(Drawer.OnCloseAsync)] = new Func<Task>(() => OnCloseAsync(option))
+        };
+        if (!string.IsNullOrEmpty(option.Width))
+        {
+            parameters.Add(nameof(Drawer.Width), option.Width);
+        }
+        if (!string.IsNullOrEmpty(option.Height))
+        {
+            parameters.Add(nameof(Drawer.Height), option.Height);
+        }
+        if (option.ChildContent != null)
+        {
+            parameters.Add(nameof(Drawer.ChildContent), option.ChildContent);
+        }
+        if (option.OnClickBackdrop != null)
+        {
+            parameters.Add(nameof(Drawer.OnClickBackdrop), option.OnClickBackdrop);
+        }
+        return parameters;
+    }
+
+    private async Task OnCloseAsync(DrawerOption option)
+    {
+        if (option.OnCloseAsync != null)
+        {
+            await option.OnCloseAsync();
+        }
     }
 
     /// <summary>
