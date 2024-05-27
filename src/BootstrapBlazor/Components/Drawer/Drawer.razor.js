03c5cd6a2de04bf84be1eee43ac002e3e8f0da47
Argo-Supper <argo@live.ca>
2024-05-27 13:04:13 +0800

    refactor: 更新脚本

--- src/BootstrapBlazor/Components/Drawer/Drawer.razor.js
+++ src/BootstrapBlazor/Components/Drawer/Drawer.razor.js
@@ -1,31 +1,25 @@
 ﻿import Data from "../../modules/data.js"
 import Drag from "../../modules/drag.js"
+import EventHandler from "../../modules/event-handler.js"
 
-export function init(id) {
-    const el = document.getElementById(id)
-    const dw = {
-        element: el,
-        body: document.querySelector('body'),
-        drawerBody: el.querySelector('.drawer-body'),
-        bar: el.querySelector('.drawer-bar')
-    }
-    Data.set(id, dw)
-
+const initDrag = el => {
     let originX = 0
     let originY = 0;
     let width = 0
     let height = 0;
     let isVertical = false;
-    Drag.drag(dw.bar,
+    const drawerBody = el.querySelector('.drawer-body');
+    const bar = el.querySelector('.drawer-bar');
+    Drag.drag(bar,
         e => {
-            isVertical = dw.drawerBody.classList.contains("top") || dw.drawerBody.classList.contains("bottom")
-            dw.bar.classList.add('drag')
+            isVertical = drawerBody.classList.contains("top") || drawerBody.classList.contains("bottom")
+            bar.classList.add('drag')
             if (isVertical) {
-                height = parseInt(getComputedStyle(dw.drawerBody).getPropertyValue('--bb-drawer-height'))
+                height = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-height'))
                 originY = e.clientY || e.touches[0].clientY
             }
             else {
-                width = parseInt(getComputedStyle(dw.drawerBody).getPropertyValue('--bb-drawer-width'))
+                width = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-width'))
                 originX = e.clientX || e.touches[0].clientX
             }
         },
@@ -34,7 +28,7 @@ export function init(id) {
                 const eventY = e.clientY || (e.touches.length || e.touches.length > 0 && e.touches[0].clientY)
                 const moveY = eventY - originY
                 let newHeight = 0;
-                if (dw.drawerBody.classList.contains("bottom")) {
+                if (drawerBody.classList.contains("bottom")) {
                     newHeight = height - moveY
                 }
                 else {
@@ -42,14 +36,14 @@ export function init(id) {
                 }
                 const maxHeight = window.innerHeight;
                 if (newHeight > 100 && newHeight < maxHeight) {
-                    dw.drawerBody.style.setProperty('--bb-drawer-height', `${newHeight}px`)
+                    drawerBody.style.setProperty('--bb-drawer-height', `${newHeight}px`)
                 }
             }
             else {
                 const eventX = e.clientX || (e.touches.length || e.touches.length > 0 && e.touches[0].clientX)
                 const moveX = eventX - originX
                 let newWidth = 0;
-                if (dw.drawerBody.classList.contains("right")) {
+                if (drawerBody.classList.contains("right")) {
                     newWidth = width - moveX
                 }
                 else {
@@ -57,36 +51,55 @@ export function init(id) {
                 }
                 const maxWidth = window.innerWidth;
                 if (newWidth > 100 && newWidth < maxWidth) {
-                    dw.drawerBody.style.setProperty('--bb-drawer-width', `${newWidth}px`)
+                    drawerBody.style.setProperty('--bb-drawer-width', `${newWidth}px`)
                 }
             }
         },
         e => {
-            dw.bar.classList.remove('drag')
+            bar.classList.remove('drag')
         }
     )
 }
 
+export function init(id, options) {
+    const el = document.getElementById(id)
+    const dw = {
+        el,
+        body: document.querySelector('body')
+    }
+    Data.set(id, dw)
+
+    EventHandler.on(el, 'click', '.drawer-backdrop', e => {
+        const element = e.delegateTarget;
+        if (element.getAttribute('data-bb-backdrop') === 'true') {
+            options.invoke.invokeMethodAsync(options.callback);
+        }
+    });
+    initDrag(el);
+}
+
 export function execute(id, open) {
     const dw = Data.get(id)
-    const el = dw.element
+    const { el, body } = dw
+    const drawerBody = el.querySelector('.drawer-body')
+
     if (open) {
         el.classList.add('show')
-        dw.body.classList.add('overflow-hidden')
+        body.classList.add('overflow-hidden')
         let handler = window.setTimeout(() => {
-            dw.drawerBody.classList.add('show')
+            drawerBody.classList.add('show')
             window.clearTimeout(handler)
             handler = null
         }, 20);
     }
     else {
         if (el.classList.contains('show')) {
-            dw.drawerBody.classList.remove('show')
+            drawerBody.classList.remove('show')
             let handler = window.setTimeout(() => {
                 window.clearTimeout(handler)
                 handler = null
                 el.classList.remove('show')
-                dw.body.classList.remove('overflow-hidden')
+                body.classList.remove('overflow-hidden')
             }, 320)
         }
     }
@@ -94,15 +107,23 @@ export function execute(id, open) {
 
 export function dispose(id) {
     const dw = Data.get(id)
-    const el = dw.element
+    Data.remove(id);
+
+    const { el, body } = dw
     if (el.classList.contains('show')) {
         el.classList.remove('show')
-        dw.drawerBody.classList.remove('show')
-        dw.body.classList.remove('overflow-hidden')
-    }
-    Data.remove(id)
 
-    if (dw.bar) {
-        Drag.dispose(dw.bar)
+        const drawerBody = el.querySelector('.drawer-body')
+        if (drawerBody) {
+            drawerBody.classList.remove('show')
+        }
+        body.classList.remove('overflow-hidden')
+    }
+
+    EventHandler.off(el, 'click', '.drawer-backdrop');
+
+    const bar = el.querySelector('.drawer-bar');
+    if (bar) {
+        Drag.dispose(bar)
     }
 }
