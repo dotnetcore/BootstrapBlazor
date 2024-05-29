e26636f6fe916cd52dd9666dcc4b201648552e2c
Argo Zhang <argo@live.ca>
2024-05-29 12:32:27 +0800

    feat(ColorPicker): add Formatter parameter (#3592)
    * feat: 增加 Template 参数
    
    * chore: bump version 8.5.12-beta03
    
    * test: 更新单元测试
    
    * doc: 更新示例
    
    * feat: 支持 Formatter 回调
    
    * doc: 增加示例
    
    * refactor: 更新写法
    
    * test: 增加单元测试

--- src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
@@ -5,7 +5,7 @@
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 
+/// ColorPicker 颜色拾取器组件
 /// </summary>
 public partial class ColorPicker
 {
@@ -15,4 +15,44 @@ public partial class ColorPicker
     protected string? ClassName => CssBuilder.Default("form-control")
         .AddClass(CssClass).AddClass(ValidCss)
         .Build();
+
+    /// <summary>
+    /// 获得/设置 显示模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment<string>? Template { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示颜色值格式化回调方法
+    /// </summary>
+    [Parameter]
+    public Func<string, Task<string>>? Formatter { get; set; }
+
+    private string? _formattedValueString;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+
+        await FormatValue();
+    }
+
+    private async Task Setter(string v)
+    {
+        CurrentValueAsString = v;
+        await FormatValue();
+    }
+
+    private async Task FormatValue()
+    {
+        if (Formatter != null)
+        {
+            // 此处未使用父类 FormatValueAsString 方法
+            // 使用者可能需要通过回调通过异步方式获得显示数据
+            _formattedValueString = await Formatter(CurrentValueAsString);
+        }
+    }
 }
