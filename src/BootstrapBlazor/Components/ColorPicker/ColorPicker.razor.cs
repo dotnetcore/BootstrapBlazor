f4a6636e0036521e504230ebd2696919d197a6c8
Argo-Supper <argo@live.ca>
2024-05-29 11:38:29 +0800

    feat: 支持 Formatter 回调

--- src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
@@ -21,4 +21,38 @@ public partial class ColorPicker
     /// </summary>
     [Parameter]
     public RenderFragment<string>? Template { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示颜色值格式化回调方法
+    /// </summary>
+    [Parameter]
+    public Func<string, Task<string>>? Formatter { get; set; }
+
+    private string? _formattedValueString;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+
+        await FormatValue();
+    }
+
+    private async Task Setter(string v)
+    {
+        CurrentValueAsString = v;
+        await FormatValue();
+    }
+
+    private async Task FormatValue()
+    {
+        if (Formatter != null)
+        {
+            // 此处未使用父类 FormatValueAsString 方法
+            // 使用者可能需要通过回调通过异步方式获得显示数据
+            _formattedValueString = await Formatter(CurrentValueAsString);
+        }
+    }
 }
