a0cb7ba4de2c6526217383f58500b693ac92768a
Argo-Supper <argo@live.ca>
2024-05-29 10:56:06 +0800

    feat: 增加 Template 参数

--- src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPicker.razor.cs
@@ -5,7 +5,7 @@
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 
+/// ColorPicker 颜色拾取器组件
 /// </summary>
 public partial class ColorPicker
 {
@@ -15,4 +15,10 @@ public partial class ColorPicker
     protected string? ClassName => CssBuilder.Default("form-control")
         .AddClass(CssClass).AddClass(ValidCss)
         .Build();
+
+    /// <summary>
+    /// 获得/设置 显示模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment<string>? Template { get; set; }
 }
