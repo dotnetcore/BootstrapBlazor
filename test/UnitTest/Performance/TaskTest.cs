8291a3eddee7fb813125d15013d675885cb649d9
Argo-AscioTech <argo@live.ca>
2024-06-01 14:11:23 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Performance/TaskTest.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Performance;
+
+public class TaskTest
+{
+    [Fact]
+    public async Task Cancel_Ok()
+    {
+        var cts = new CancellationTokenSource();
+        cts.Cancel();
+        await Assert.ThrowsAnyAsync<TaskCanceledException>(() => Task.Delay(10, cts.Token));
+    }
+}
