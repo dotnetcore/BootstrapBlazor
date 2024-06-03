ad3eaf0c19f0db5acd4fb2bedac20af6bb338ba4
Argo-Supper <argo@live.ca>
2024-06-03 18:27:05 +0800

    doc: 调整样式

---
+++ src/BootstrapBlazor.Server/Components/Samples/DockViews2/DockViewLock.razor.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples.DockViews2;
+public partial class DockViewLock
+{
+    private bool IsLock { get; set; } = true;
+
+    private string? LockText { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        LockText = IsLock ? "解锁" : "锁定";
+    }
+
+    private void OnToggleLock()
+    {
+        IsLock = !IsLock;
+        if (IsLock == true)
+        {
+            LockText = "解锁";
+        }
+        else
+        {
+            LockText = "锁定";
+        }
+    }
+
+    private Task OnLockChangedCallbackAsync(bool state)
+    {
+        IsLock = state;
+        return Task.CompletedTask;
+    }
+}
