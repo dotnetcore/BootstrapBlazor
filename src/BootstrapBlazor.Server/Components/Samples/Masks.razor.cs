cc9d4c91198eab8ab21f56f003dd1ae7b427a3cc
Argo-AscioTech <argo@live.ca>
2024-06-17 19:06:16 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/Masks.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// Mask 组件示例文档
+/// </summary>
+public partial class Masks
+{
+    [Inject, NotNull]
+    private MaskService? MaskService { get; set; }
+
+    private async Task ShowMask()
+    {
+        await MaskService.Show(new MaskOption()
+        {
+            ChildContent = BootstrapDynamicComponent.CreateComponent<Counter>().Render()
+        });
+    }
+}
