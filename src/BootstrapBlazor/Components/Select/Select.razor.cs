ab0bb74de53b209c0a1cfd3f5de008769521e07f
Argo Zhang <argo@live.ca>
2024-06-19 14:24:08 +0800

    fix(Select): DisableItemChangedWhenFirstRender not work (#3686)
    * fix: 修复首次仍然触发问题
    
    * chore: bump version 8.6.3-beta03

--- src/BootstrapBlazor/Components/Select/Select.razor.cs
+++ src/BootstrapBlazor/Components/Select/Select.razor.cs
@@ -187,7 +187,7 @@ public partial class Select<TValue> : ISelect
 
     private string _lastSelectedValueString = string.Empty;
 
-    private bool _init;
+    private bool _init = true;
 
     /// <summary>
     /// <inheritdoc/>
@@ -208,20 +208,6 @@ public partial class Select<TValue> : ISelect
         {
             var item = NullableUnderlyingType == null ? "" : PlaceHolder;
             Items = ValueType.ToSelectList(string.IsNullOrEmpty(item) ? null : new SelectedItem("", item));
-        }
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <param name="firstRender"></param>
-    protected override void OnAfterRender(bool firstRender)
-    {
-        base.OnAfterRender(firstRender);
-
-        if (firstRender)
-        {
-            _init = true;
         }
     }
 
@@ -320,9 +306,17 @@ public partial class Select<TValue> : ISelect
                 ?? DataSource.Where(i => !i.IsDisabled).FirstOrDefault()
                 ?? GetVirtualizeItem();
 
-            if (SelectedItem != null && ((_init || !DisableItemChangedWhenFirstRender)))
+            if (SelectedItem != null)
             {
-                _ = SelectedItemChanged(SelectedItem);
+                if (_init && DisableItemChangedWhenFirstRender)
+                {
+
+                }
+                else
+                {
+                    _ = SelectedItemChanged(SelectedItem);
+                    _init = false;
+                }
             }
         }
         else if (IsVirtualize)
