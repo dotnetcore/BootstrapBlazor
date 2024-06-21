ab211284f9d4ac0a82df2e8425de834562c83e79
Argo Zhang <argo@live.ca>
2024-06-21 11:23:59 +0800

    refactor(Dock): bump version 8.1.7 (#3702)
    * refactor: 更新 getAllItemsByType 方法
    
    * refactor: 重构扩展方法
    
    * refactor: 拆分 config 扩展
    
    * refactor: 拆分 Utility 扩展模块
    
    * refactor: 重构方法名
    
    * chore: bump version 8.1.7

---
+++ src/Extensions/Components/BootstrapBlazor.Dock/wwwroot/js/golden-layout-config.js
@@ -0,0 +1,25 @@
+﻿const saveConfig = (layout, option) => {
+    option = {
+        enableLocalStorage: false,
+        ...option
+    }
+    if (option.enableLocalStorage) {
+        removeConfig(option)
+        localStorage.setItem(option.localStorageKey, JSON.stringify(layout.saveLayout()));
+    }
+}
+
+const removeConfig = option => {
+    for (let index = localStorage.length; index > 0; index--) {
+        const k = localStorage.key(index - 1);
+        if (indexOfKey(k, option)) {
+            localStorage.removeItem(k);
+        }
+    }
+}
+
+const indexOfKey = (key, option) => {
+    return key.indexOf(`${option.prefix}-`) > -1
+}
+
+export { saveConfig, removeConfig }
