02f0f52c74493d2f05d172837e62e34c4f95d365
Argo-AscioTech <argo@live.ca>
2024-06-22 10:52:47 +0800

    test: 增加单元测试

--- test/UnitTest/Components/TableFilterTest.cs
+++ test/UnitTest/Components/TableFilterTest.cs
@@ -79,6 +79,82 @@ public class TableFilterTest : BootstrapBlazorTestBase
     }
 
     [Fact]
+    public async Task MultiFilter_Ok()
+    {
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Table<Cat>>(pb =>
+            {
+                pb.Add(a => a.Items, new List<Cat>
+                {
+                    new()
+                });
+                pb.Add(a => a.RenderMode, TableRenderMode.Table);
+                pb.Add(a => a.TableColumns, cat => builder =>
+                {
+                    var index = 0;
+                    builder.OpenComponent<TableColumn<Cat, string?>>(index++);
+                    builder.AddAttribute(index++, nameof(TableColumn<Cat, string?>.Field), cat.P8);
+                    builder.AddAttribute(index++, nameof(TableColumn<Cat, string?>.FieldExpression), Utility.GenerateValueExpression(cat, nameof(Cat.P8), typeof(string)));
+                    builder.AddAttribute(index++, nameof(TableColumn<Cat, string?>.Filterable), true);
+                    builder.AddAttribute(index++, nameof(TableColumn<Cat, string?>.FilterTemplate), new RenderFragment(b =>
+                    {
+                        b.OpenComponent<MultiFilter>(0);
+                        b.AddAttribute(1, nameof(MultiFilter.ShowSearch), true);
+                        b.AddAttribute(2, nameof(MultiFilter.Items), new SelectedItem[] {
+                            new("test1", "test1"),
+                            new("test2", "test2")
+                        });
+                        b.CloseComponent();
+                    }));
+                    builder.CloseComponent();
+                });
+            });
+        });
+
+        cut.DoesNotContain("multi-filter-placeholder");
+        cut.DoesNotContain("multi-filter-All");
+
+        var filter = cut.FindComponent<MultiFilter>();
+        filter.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.SearchPlaceHolderText, "multi-filter-placeholder");
+            pb.Add(a => a.SelectAllText, "multi-filter-All");
+        });
+        cut.Contains("multi-filter-placeholder");
+        cut.Contains("multi-filter-All");
+
+        await cut.InvokeAsync(() => filter.Instance.Reset());
+
+        // 选中选项
+        var checkboxs = cut.FindComponents<Checkbox<bool>>();
+        Assert.Equal(3, checkboxs.Count);
+        await cut.InvokeAsync(() => checkboxs[2].Instance.SetState(CheckboxState.Checked));
+
+        FilterKeyValueAction? action = null;
+        await cut.InvokeAsync(() =>
+        {
+            action = filter.Instance.GetFilterConditions();
+        });
+        Assert.NotNull(action);
+        Assert.Equal(FilterLogic.Or, action.FilterLogic);
+        Assert.NotNull(action.Filters);
+        Assert.NotNull(action.Filters[0]);
+        Assert.Equal("test2", action.Filters[0].FieldValue);
+        Assert.Equal("P8", action.Filters[0].FieldKey);
+        Assert.Equal(CheckboxState.Indeterminate, checkboxs[0].Instance.State);
+
+        // 测试全选
+        await cut.InvokeAsync(() => checkboxs[0].Instance.SetState(CheckboxState.Checked));
+        await cut.InvokeAsync(() => checkboxs[0].Instance.SetState(CheckboxState.UnChecked));
+
+        // 测试搜索
+        var input = cut.Find(".bb-multi-filter-search");
+        await cut.InvokeAsync(() => input.Input("test02"));
+        await cut.InvokeAsync(() => input.Input(""));
+    }
+
+    [Fact]
     public void NotInTable_Ok()
     {
         var cut = Context.RenderComponent<TableFilter>(pb =>
@@ -240,5 +316,7 @@ public class TableFilterTest : BootstrapBlazorTestBase
         public decimal P6 { get; set; }
 
         public Foo? P7 { get; set; }
+
+        public string? P8 { get; set; }
     }
 }
