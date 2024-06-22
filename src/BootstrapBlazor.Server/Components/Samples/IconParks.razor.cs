a0b2cf3cbdfcf40209f2661c15346e6ef5c54d7b
Argo-AscioTech <argo@live.ca>
2024-06-22 22:02:12 +0800

    chore: 增加后台文件

---
+++ src/BootstrapBlazor.Server/Components/Samples/IconParks.razor.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// IconPark 图标库示例
+/// </summary>
+public partial class IconParks
+{
+
+}
