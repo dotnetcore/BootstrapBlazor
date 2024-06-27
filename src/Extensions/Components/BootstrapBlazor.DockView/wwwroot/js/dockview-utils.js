f5360769dda2ef7b9de5d1fc42cef7833bba9f66
zhaijunlei <276318515@qq.com>
2024-06-27 09:46:16 +0800

    fix：修复初始化完成时initialized和panelVisibleChanged事件不触发

--- src/Extensions/Components/BootstrapBlazor.DockView/wwwroot/js/dockview-utils.js
+++ src/Extensions/Components/BootstrapBlazor.DockView/wwwroot/js/dockview-utils.js
@@ -62,16 +62,18 @@ const initDockview = (dockview, options, template) => {
     })
 
     dockview.onDidLayoutFromJSON(() => {
-        dockview._initialized?.fire()
-        const panels = dockview.panels
-        const delPanelsStr = localStorage.getItem(dockview.params.options.localStorageKey + panels)
-        const delPanels = delPanelsStr && JSON.parse(delPanelsStr) || []
-        panels.forEach(panel => {
-            dockview._panelVisibleChanged?.fire({ title: panel.title, status: true });
-        })
-        delPanels.forEach(panel => {
-            dockview._panelVisibleChanged?.fire({ title: panel.title, status: false });
-        })
+        setTimeout(() => {
+            dockview._initialized?.fire()
+            const panels = dockview.panels
+            const delPanelsStr = localStorage.getItem(dockview.params.options.localStorageKey + '-panels')
+            const delPanels = delPanelsStr && JSON.parse(delPanelsStr) || []
+            panels.forEach(panel => {
+                dockview._panelVisibleChanged?.fire({ title: panel.title, status: true });
+            })
+            delPanels.forEach(panel => {
+                dockview._panelVisibleChanged?.fire({ title: panel.title, status: false });
+            })
+        }, 0);
     })
 }
 
