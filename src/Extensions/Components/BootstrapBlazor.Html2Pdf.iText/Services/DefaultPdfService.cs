08e9ec8bbdded360d61a103b87a76cdd45001b0b
Argo-AscioTech <argo@live.ca>
2024-06-28 14:28:31 +0800

    refactor: 增加接口方法实现

--- src/Extensions/Components/BootstrapBlazor.Html2Pdf.iText/Services/DefaultPdfService.cs
+++ src/Extensions/Components/BootstrapBlazor.Html2Pdf.iText/Services/DefaultPdfService.cs
@@ -5,55 +5,30 @@
 using iText.Html2pdf;
 using iText.Html2pdf.Resolver.Font;
 using iText.IO.Font;
-using iText.Kernel.Pdf;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// 默认 Html to Pdf 实现 
 /// </summary>
-class DefaultPdfService(IServiceProvider provider) : IHtml2Pdf
+class DefaultPdfService : IHtml2Pdf
 {
-    public Task<byte[]> PdfDataAsync(string url)
+    public Task<byte[]> PdfDataAsync(string url) => throw new NotImplementedException();
+
+    public Task<Stream> PdfStreamAsync(string url) => throw new NotImplementedException();
+
+    public Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null) => GeneratePdfFromHtmlAsync(html, null);
+
+    public async Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
     {
-        throw new NotImplementedException();
+        var data = await GeneratePdfFromHtmlAsync(html, null);
+        return new MemoryStream(data);
     }
 
-    public Task<byte[]> PdfDataFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Stream> PdfStreamAsync(string url)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<Stream> PdfStreamFromHtmlAsync(string html, IEnumerable<string>? links = null, IEnumerable<string>? scripts = null)
-    {
-        return PdfStreamFromHtmlAsync(html, fonts: null);
-    }
-
-    /// <summary>
-    /// Export method
-    /// </summary>
-    /// <param name="html">html raw string</param>
-    /// <param name="fonts"></param>
-    public Task<Stream> PdfStreamFromHtmlAsync(string html, List<string>? fonts = null) => Task.Run(() =>
+    private static Task<byte[]> GeneratePdfFromHtmlAsync(string html, List<string>? fonts = null) => Task.Run(() =>
     {
         ConverterProperties? converterProperties = null;
-
-        if (CultureInfo.CurrentUICulture.Name == "zh-CN" && fonts == null)
-        {
-            var webHost = provider.CreateScope().ServiceProvider.GetRequiredService<IWebHostEnvironment>();
-            fonts =
-            [
-                Path.Combine(webHost.WebRootPath, "_content/BootstrapBlazor.Html2PdfiText/simhei.ttf")
-            ];
-        }
         if (fonts != null)
         {
             var fontProvider = new DefaultFontProvider(registerStandardPdfFonts: false, registerShippedFonts: false, registerSystemFonts: false);
@@ -69,9 +44,6 @@ class DefaultPdfService(IServiceProvider provider) : IHtml2Pdf
 
         var stream = new MemoryStream();
         HtmlConverter.ConvertToPdf(html, stream, converterProperties);
-        var data = stream.ToArray();
-
-        Stream ret = new MemoryStream(data);
-        return ret;
+        return stream.ToArray();
     });
 }
