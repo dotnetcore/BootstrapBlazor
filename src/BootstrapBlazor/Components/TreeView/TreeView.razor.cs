fae911658f81dc0cafb97fbdf28b6f261e3c2216
Argo Zhang <argo@live.ca>
2024-07-03 09:00:46 +0800

    feat(TreeView): add IsFixedSearch parameter (#3772)
    * feat: 增加 IsFixedSearch 参数
    
    * feat: 增加固定搜索栏样式
    
    * doc: 更新固定搜索栏示例
    
    * test: 更新单元测试
    
    * chore: bump version 8.7.1-beta04

--- src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
+++ src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
@@ -19,6 +19,7 @@ public partial class TreeView<TItem> : IModelEqualityComparer<TItem>
     /// 获得 按钮样式集合
     /// </summary>
     private string? ClassString => CssBuilder.Default("tree-view")
+        .AddClass("is-fixed-search", ShowSearch && IsFixedSearch)
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
 
@@ -126,6 +127,12 @@ public partial class TreeView<TItem> : IModelEqualityComparer<TItem>
     /// </summary>
     [Parameter]
     public bool ShowSearch { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否固定搜索栏 默认 false 不固定
+    /// </summary>
+    [Parameter]
+    public bool IsFixedSearch { get; set; }
 
     /// <summary>
     /// 获得/设置 是否显示重置搜索栏按钮 默认 true 显示
