578c5fb293b46e7c3cd94165059f58b953b99c13
lee <lish@live.cn>
2024-07-05 11:58:40 +0800

    doc: 更新方法注释

--- src/BootstrapBlazor/Components/Clipboard/ClipboardItem.cs
+++ src/BootstrapBlazor/Components/Clipboard/ClipboardItem.cs
@@ -1,36 +1,49 @@
 ﻿namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// ClipboardItem
+/// Represents an item on the clipboard with associated data and metadata.
 /// </summary>
 public class ClipboardItem
 {
     /// <summary>
-    /// Gets or sets the MIME type of the clipboard item.
+    /// Gets or sets the Internet Media Type (MIME type) of the content stored in the clipboard item.
     /// </summary>
+    /// <remarks>
+    /// The MIME type is used to specify the nature and format of the data stored in the clipboard.
+    /// It can be used to determine how the data should be handled or presented.
+    /// </remarks>
     public string? MimeType { get; set; }
 
     /// <summary>
-    /// Gets or sets the data of the clipboard item.
+    /// Gets or sets the raw data of the clipboard item.
     /// </summary>
+    /// <remarks>
+    /// The data is stored as a byte array, allowing for any binary or text content
+    /// to be represented. The interpretation of the data depends on the MIME type.
+    /// </remarks>
     public byte[]? Data { get; set; }
 
-
     /// <summary>
-    /// Gets the text representation of the clipboard item.
+    /// Retrieves a text representation of the clipboard item if the MIME type indicates text content.
     /// </summary>
+    /// <value>
+    /// A string containing the text representation of the data if the MIME type starts with "text/",
+    /// otherwise an empty string.
+    /// </value>
+    /// <remarks>
+    /// This property checks the MIME type of the clipboard item and, if it's a text type,
+    /// converts the byte array to a UTF-8 encoded string. If the data is not text or the MIME type
+    /// is not specified, it returns an empty string.
+    /// </remarks>
     public string Text
     {
         get
         {
-            if (Data is not null && !string.IsNullOrEmpty(MimeType) && MimeType.StartsWith("text"))
+            if (Data is not null && MimeType?.StartsWith("text/") == true)
             {
                 return System.Text.Encoding.UTF8.GetString(Data);
             }
-            else
-            {
-                return string.Empty;
-            }
+            return string.Empty;
         }
     }
 }
