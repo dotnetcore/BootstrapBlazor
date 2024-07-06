629d9f3443041d1e856b11aa7adbbb087fa51782
Argo Zhang <argo@live.ca>
2024-07-06 14:15:25 +0800

    doc(Table): update AutoRefresh sample code (#3795)

--- src/BootstrapBlazor.Server/Components/Samples/Table/TablesAutoRefresh.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Table/TablesAutoRefresh.razor.cs
@@ -15,29 +15,34 @@ public partial class TablesAutoRefresh
     /// https://gitee.com/LongbowEnterprise/BootstrapBlazor/blob/main/src/BootstrapBlazor.Server/Data/Foo.cs
     /// </summary>
     private List<Foo> Items { get; set; } = [];
+
     private bool IsAutoRefresh { get; set; }
 
     private static readonly Random random = new();
+
     private void ToggleAuto() => IsAutoRefresh = !IsAutoRefresh;
+
     private Task<QueryData<Foo>> OnAutoQueryAsync(QueryPageOptions options) => GenerateFoos(options, Items);
+
     private Task<QueryData<Foo>> OnManualQueryAsync(QueryPageOptions options) => GenerateFoos(options, Items);
+
+    private int _id = 0;
+
     private Task<QueryData<Foo>> GenerateFoos(QueryPageOptions options, List<Foo> foos)
     {
         // 设置记录总数
-        var total = foos.Count;
         var foo = Foo.Generate(LocalizerFoo);
-        foo.Id = total++;
-        foo.Name = LocalizerFoo["Foo.Name", total.ToString("D4")];
+        foo.Id = _id++;
+        foo.Name = LocalizerFoo["Foo.Name", foo.Id.ToString("D4")];
         foo.Address = LocalizerFoo["Foo.Address", $"{random.Next(1000, 2000)}"];
         foos.Insert(0, foo);
         if (foos.Count > 10)
         {
             foos.RemoveRange(10, 1);
-            total = 10;
         }
 
         // 内存分页
         var items = foos.Skip((options.PageIndex - 1) * options.PageItems).Take(options.PageItems).ToList();
-        return Task.FromResult(new QueryData<Foo>() { Items = items, TotalCount = total });
+        return Task.FromResult(new QueryData<Foo>() { Items = items, TotalCount = foos.Count });
     }
 }
