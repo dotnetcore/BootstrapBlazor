c8d7a57e7448018e6c2c36ef6b34ee2a1e97d261
Argo-AscioTech <argo@live.ca>
2024-07-08 11:25:03 +0800

    refactor: 实现模板逻辑

--- src/BootstrapBlazor/Components/Popover/Popover.razor.js
+++ src/BootstrapBlazor/Components/Popover/Popover.razor.js
@@ -1,18 +1,10 @@
-﻿export function init(id, content) {
+﻿import { getDescribedElement } from "../../modules/utility.js"
+import EventHandler from "../../modules/event-handler.js"
+
+export function init(id, options) {
     const el = document.getElementById(id)
     if (el) {
-        const pop = bootstrap.Popover.getInstance(el)
-        if (pop) {
-            pop._config.content = content
-        }
-        else {
-            new bootstrap.Popover(el, {
-                content,
-                title: () => {
-                    return el.getAttribute('data-bs-original-title')
-                }
-            })
-        }
+        createPopover(el, options);
     }
 }
 
@@ -23,5 +15,46 @@ export function dispose(id) {
         if (pop) {
             pop.dispose();
         }
+    }
+}
+
+const createPopover = (el, options) => {
+    const { content, template } = options;
+    const pop = bootstrap.Popover.getOrCreateInstance(el);
+    if (template) {
+        EventHandler.on(el, 'inserted.bs.popover', () => insertedPopover(el));
+        EventHandler.on(el, 'hide.bs.popover', () => hidePopover(el));
+    }
+    else if (content === void 0 || content === '') {
+        pop.disable();
+    }
+    else {
+        pop.enable();
+        pop._config.content = content;
+    }
+}
+
+const insertedPopover = el => {
+    const popover = getDescribedElement(el)
+    if (popover) {
+        const body = document.createElement('div');
+        body.className = 'popover-body';
+        const template = el.querySelector('template');
+        [...template.children].forEach(i => {
+            body.appendChild(i);
+        });
+        popover.appendChild(body);
+    }
+}
+
+const hidePopover = el => {
+    const popover = getDescribedElement(el)
+    if (popover) {
+        const body = popover.querySelector('.popover-body');
+        const template = el.querySelector('template');
+        [...body.children].forEach(i => {
+            template.appendChild(i);
+        });
+        body.remove();
     }
 }
