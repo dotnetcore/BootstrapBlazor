32ff06560858b244498462c8104ed282c36c8ee2
Argo-AscioTech <argo@live.ca>
2024-07-11 16:03:17 +0800

    refactor: 优化代码

--- test/UnitTest/Dynamic/DataTableDynamicContextTest.cs
+++ test/UnitTest/Dynamic/DataTableDynamicContextTest.cs
@@ -91,7 +91,7 @@ public class DataTableDynamicContextTest : BootstrapBlazorTestBase
         var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
         var fooData = GenerateDataTable(localizer);
         var context = new DataTableDynamicContext(fooData);
-        context.AddAttribute(nameof(Foo.Name), typeof(DisplayAttribute), Type.EmptyTypes, Array.Empty<object>(), null, null);
+        context.AddAttribute(nameof(Foo.Name), typeof(DisplayAttribute), Type.EmptyTypes, [], null, null);
     }
 
     [Fact]
@@ -134,7 +134,7 @@ public class DataTableDynamicContextTest : BootstrapBlazorTestBase
                 return Task.CompletedTask;
             }
         };
-        await context.AddAsync(Enumerable.Empty<IDynamicObject>());
+        await context.AddAsync([]);
         Assert.True(changed);
     }
 
@@ -188,7 +188,7 @@ public class DataTableDynamicContextTest : BootstrapBlazorTestBase
         Assert.Equal(4, context.GetItems().Count());
 
         // add empty
-        await context.AddAsync(Enumerable.Empty<IDynamicObject>());
+        await context.AddAsync([]);
 
         // 在选中行位置插入
         await context.AddAsync(context.GetItems().Take(2));
