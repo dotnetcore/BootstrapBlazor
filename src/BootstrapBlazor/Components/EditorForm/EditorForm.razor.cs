345cdd691b3594e186c18df26165f6a58aae2699
Argo-AscioTech <argo@live.ca>
2024-07-19 12:37:17 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Components/EditorForm/EditorForm.razor.cs
+++ src/BootstrapBlazor/Components/EditorForm/EditorForm.razor.cs
@@ -251,7 +251,7 @@ public partial class EditorForm<TModel> : IShowLabel
             _formItems = [];
             if (Items != null)
             {
-                _formItems.AddRange(Items.Where(i => !i.GetIgnore() && !string.IsNullOrEmpty(i.GetFieldName())));
+                _formItems.AddRange(Items.Where(i => !i.GetIgnore()));
             }
             else
             {
