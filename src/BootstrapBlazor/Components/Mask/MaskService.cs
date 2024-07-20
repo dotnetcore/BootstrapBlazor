75d9dc80f4bc60170e90bd979edcab1b461efda1
Argo Zhang <argo@live.ca>
2024-07-20 21:08:40 +0800

    feat(Mask): add ContainerId parameter (#3885)
    * feat: 增加 ContainerId 参数用于指定遮罩容器
    
    * doc: 更新示例文档
    
    * doc: 更新示例文档
    
    * refactor: 支持多个遮罩
    
    * test: 更新单元测试
    
    * chore: bump version 8.7.3-beta08
    
    * doc: 更新示例
    
    * feat: 支持多窗口遮罩
    
    * doc: 增加本地化语言设置
    
    * test: 更新单元测试
    
    * refactor: 更新代码

--- src/BootstrapBlazor/Components/Mask/MaskService.cs
+++ src/BootstrapBlazor/Components/Mask/MaskService.cs
@@ -13,12 +13,28 @@ public class MaskService : BootstrapServiceBase<MaskOption?>
     /// 显示 Mask 方法
     /// </summary>
     /// <param name="option">遮罩配置信息实体类</param>
+    /// <param name="mask"><see cref="Mask"/> 组件实例</param>
     /// <returns></returns>
-    public Task Show(MaskOption option) => Invoke(option);
+    public Task Show(MaskOption option, Mask? mask = null) => Invoke(option, mask);
 
     /// <summary>
     /// 关闭 Mask 方法
     /// </summary>
+    /// <param name="mask"><see cref="Mask"/> 组件实例</param>
+    /// <param name="all">是否关闭所有遮罩 默认 false 仅关闭当前或者指定遮罩</param>
     /// <returns></returns>
-    public Task Close() => Invoke(null);
+    public async Task Close(Mask? mask = null, bool all = false)
+    {
+        if (all)
+        {
+            foreach (var (key, callback) in Cache)
+            {
+                await callback(null);
+            }
+        }
+        else
+        {
+            await Invoke(null, mask);
+        }
+    }
 }
