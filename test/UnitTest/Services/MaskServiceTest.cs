18ab631b1de1a98927b5b147a1e4a22023114c7e
Argo-AscioTech <argo@live.ca>
2024-07-20 15:40:21 +0800

    test: 更新单元测试

--- test/UnitTest/Services/MaskServiceTest.cs
+++ test/UnitTest/Services/MaskServiceTest.cs
@@ -35,7 +35,7 @@ public class MaskServiceTest : TestBase
                 });
             });
         });
-        cut.Contains("<div class=\"bb-mask\"><div class=\"bb-mask-backdrop\"></div></div>");
+        cut.Contains("<div class=\"bb-mask-backdrop\"></div>");
 
         // 点击按钮显示
         var button = cut.Find("button");
@@ -43,7 +43,44 @@ public class MaskServiceTest : TestBase
         cut.Contains("bb-mask-content");
 
         // 关闭遮罩
-        await cut.InvokeAsync(maskService.Close);
+        await cut.InvokeAsync(() => maskService.Close());
+        cut.DoesNotContain("bb-mask-content");
+    }
+
+    [Fact]
+    public async Task Container_Ok()
+    {
+        var context = new TestContext();
+        context.JSInterop.Mode = JSRuntimeMode.Loose;
+        context.Services.AddBootstrapBlazor();
+
+        var maskService = context.Services.GetRequiredService<MaskService>();
+        var cut = context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<Button>(pb =>
+            {
+                pb.Add(a => a.OnClickWithoutRender, async () =>
+                {
+                    await maskService.Show(new MaskOption()
+                    {
+                        BackgroundColor = "#000",
+                        Opacity = 0.5f,
+                        ZIndex = 1050,
+                        ChildContent = builder => builder.AddContent(0, "test-mask-content"),
+                        ContainerId = "test-9527"
+                    });
+                });
+            });
+        });
+        cut.Contains("<div class=\"bb-mask-backdrop\"></div>");
+
+        // 点击按钮显示
+        var button = cut.Find("button");
+        await cut.InvokeAsync(() => button.Click());
+        cut.Contains("bb-mask-content");
+
+        // 关闭遮罩
+        await cut.InvokeAsync(() => maskService.Close());
         cut.DoesNotContain("bb-mask-content");
     }
 }
