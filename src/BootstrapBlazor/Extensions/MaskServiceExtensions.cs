35a51c150e2551aa09feba5e6bc05ea9fcbef3d8
Argo Zhang <argo@live.ca>
2024-07-21 18:31:27 +0800

    feat(Mask): add Show extensions method (#3892)
    * feat: 增加扩展方法
    
    * test: 更新单元测试
    
    * refactor: 增加 ContainerId 参数
    
    * chore: bump version 8.7.4-beta01

---
+++ src/BootstrapBlazor/Extensions/MaskServiceExtensions.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// MaskService 扩展方法
+/// </summary>
+public static class MaskServiceExtensions
+{
+    /// <summary>
+    /// Show 扩展方法
+    /// </summary>
+    /// <param name="maskService"></param>
+    /// <param name="parameters"></param>
+    /// <param name="containerId"></param>
+    /// <param name="backgroundColor"></param>
+    /// <param name="opacity"></param>
+    /// <param name="zIndex"></param>
+    /// <param name="mask"></param>
+    /// <returns></returns>
+    public static Task Show<TComponent>(this MaskService maskService, IDictionary<string, object?>? parameters = null, string? containerId = null, string? backgroundColor = null, float opacity = 0.5f, int zIndex = 1050, Mask? mask = null) where TComponent : ComponentBase => maskService.Show(new MaskOption()
+    {
+        BackgroundColor = backgroundColor,
+        Opacity = opacity,
+        ZIndex = zIndex,
+        ContainerId = containerId,
+        ChildContent = BootstrapDynamicComponent.CreateComponent<TComponent>(parameters).Render()
+    }, mask);
+
+    /// <summary>
+    /// Show 扩展方法
+    /// </summary>
+    /// <param name="maskService"></param>
+    /// <param name="type"></param>
+    /// <param name="parameters"></param>
+    /// <param name="containerId"></param>
+    /// <param name="backgroundColor"></param>
+    /// <param name="opacity"></param>
+    /// <param name="zIndex"></param>
+    /// <param name="mask"></param>
+    /// <returns></returns>
+    public static Task Show(this MaskService maskService, Type type, IDictionary<string, object?>? parameters = null, string? containerId = null, string? backgroundColor = null, float opacity = 0.5f, int zIndex = 1050, Mask? mask = null) => maskService.Show(new MaskOption()
+    {
+        BackgroundColor = backgroundColor,
+        Opacity = opacity,
+        ZIndex = zIndex,
+        ContainerId = containerId,
+        ChildContent = BootstrapDynamicComponent.CreateComponent(type, parameters).Render()
+    }, mask);
+}
