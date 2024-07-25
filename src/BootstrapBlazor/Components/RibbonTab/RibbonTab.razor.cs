488c637dc0201afcfe9e71fc5ffe99eedef5a32c
Argo Zhang <argo@live.ca>
2024-07-25 17:44:34 +0800

    perf(RibbonTab): add RibbonTabHeader component improve performance (#3928)
    * refactor: 增加 RibbonTabHeader 组件提高性能
    
    * refactor: 拆分代码到 Header 组件中
    
    * chore: bump version 8.7.4-beta07

--- src/BootstrapBlazor/Components/RibbonTab/RibbonTab.razor.cs
+++ src/BootstrapBlazor/Components/RibbonTab/RibbonTab.razor.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using System.Web;
-
 namespace BootstrapBlazor.Components;
 
 /// <summary>
@@ -59,14 +57,6 @@ public partial class RibbonTab
     [Parameter]
     public Func<string, string?>? DecodeAnchorCallback { get; set; }
 
-    private bool IsFloat { get; set; }
-
-    private string? ArrowIconClassString => CssBuilder.Default()
-        .AddClass(RibbonArrowUpIcon, !IsFloat)
-        .AddClass(RibbonArrowDownIcon, IsFloat && !IsExpand)
-        .AddClass(RibbonArrowPinIcon, IsFloat && IsExpand)
-        .Build();
-
     /// <summary>
     /// 获得/设置 数据源
     /// </summary>
@@ -107,25 +97,11 @@ public partial class RibbonTab
     [Parameter]
     public bool IsBorder { get; set; } = true;
 
-    [Inject]
-    [NotNull]
-    private IIconTheme? IconTheme { get; set; }
-
-    [Inject]
-    [NotNull]
-    private NavigationManager? NavigationManager { get; set; }
-
-    private bool IsExpand { get; set; }
+    private RibbonTabHeader _header = default!;
 
     private string? HeaderClassString => CssBuilder.Default("ribbon-tab")
-        .AddClass("is-float", IsFloat)
-        .AddClass("is-expand", IsFloat && IsExpand)
         .AddClass("border", IsBorder)
         .AddClassFromAttributes(AdditionalAttributes)
-        .Build();
-
-    private static string? GetClassString(RibbonTabItem item) => CssBuilder.Default()
-        .AddClass("active", item.IsActive)
         .Build();
 
     /// <summary>
@@ -135,110 +111,13 @@ public partial class RibbonTab
     protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, nameof(SetExpand));
 
     /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    protected override void OnParametersSet()
-    {
-        base.OnParametersSet();
-
-        RibbonArrowUpIcon ??= IconTheme.GetIconByKey(ComponentIcons.RibbonTabArrowUpIcon);
-        RibbonArrowDownIcon ??= IconTheme.GetIconByKey(ComponentIcons.RibbonTabArrowDownIcon);
-        RibbonArrowPinIcon ??= IconTheme.GetIconByKey(ComponentIcons.RibbonTabArrowPinIcon);
-
-        Items ??= [];
-
-        if (IsSupportAnchor)
-        {
-            var hash = DecodeAnchorCallback?.Invoke(NavigationManager.Uri) ?? HttpUtility.UrlDecode(NavigationManager.Uri.Split('#').LastOrDefault());
-            if (!string.IsNullOrEmpty(hash))
-            {
-                var item = Items.FirstOrDefault(i => i.Text == hash);
-                if (item != null)
-                {
-                    ResetActiveTabItem();
-                    item.IsActive = true;
-                }
-            }
-        }
-        else if (!Items.Any(i => i.IsActive))
-        {
-            var item = Items.FirstOrDefault();
-            if (item != null)
-            {
-                item.IsActive = true;
-            }
-        }
-    }
-
-    private void ResetActiveTabItem()
-    {
-        var activeItem = Items.FirstOrDefault(item => item.IsActive);
-        if (activeItem != null)
-        {
-            activeItem.IsActive = false;
-        }
-    }
-
-    /// <summary>
     /// SetExpand 方法
     /// </summary>
     [JSInvokable]
-    public void SetExpand()
-    {
-        IsExpand = false;
-        StateHasChanged();
-    }
-
-    private async Task OnClick(RibbonTabItem item)
-    {
-        if (OnItemClickAsync != null)
-        {
-            await OnItemClickAsync(item);
-        }
-    }
-
-    private async Task OnClickTabItemAsync(TabItem item)
-    {
-        if (IsSupportAnchor)
-        {
-            var url = EncodeAnchorCallback?.Invoke(NavigationManager.Uri, item.Text) ?? $"{NavigationManager.Uri.Split('#').FirstOrDefault()}#{HttpUtility.UrlEncode(item.Text)}";
-            if (!string.IsNullOrEmpty(url))
-            {
-                NavigationManager.NavigateTo(url);
-            }
-        }
-
-        ResetActiveTabItem();
-        var tab = Items.First(i => i.Text == item.Text);
-        tab.IsActive = true;
-        if (OnMenuClickAsync != null)
-        {
-            await OnMenuClickAsync(tab);
-        }
-        if (IsFloat)
-        {
-            IsExpand = true;
-            StateHasChanged();
-        }
-    }
-
-    private async Task OnToggleFloat()
-    {
-        IsFloat = !IsFloat;
-        if (!IsFloat)
-        {
-            IsExpand = false;
-        }
-        if (OnFloatChanged != null)
-        {
-            await OnFloatChanged(IsFloat);
-        }
-    }
-
-    private static RenderFragment? RenderTemplate(RibbonTabItem item) => item.Component?.Render() ?? item.Template;
+    public void SetExpand() => _header.SetExpand();
 
     /// <summary>
     /// 重新渲染组件
     /// </summary>
-    public void Render() => StateHasChanged();
+    public void Render() => _header.Render();
 }
