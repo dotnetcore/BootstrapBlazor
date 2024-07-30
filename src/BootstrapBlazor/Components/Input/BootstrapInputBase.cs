113e384f0d22cd60706a3aec49c4420e20fbff9a
Argo-AscioTech <argo@live.ca>
2024-07-30 19:18:14 +0800

    feat: add AutoSetDefaultWhenNull parameter

--- src/BootstrapBlazor/Components/Input/BootstrapInputBase.cs
+++ src/BootstrapBlazor/Components/Input/BootstrapInputBase.cs
@@ -83,6 +83,12 @@ public abstract class BootstrapInputBase<TValue> : ValidateBase<TValue>
     [Parameter]
     public bool IsTrim { get; set; }
 
+    /// <summary>
+    /// 获得/设置 用户删除后是否自动更改为默认值 0 默认 false
+    /// </summary>
+    [Parameter]
+    public bool AutoSetDefaultWhenNull { get; set; }
+
     [CascadingParameter]
     private Modal? Modal { get; set; }
 
@@ -187,7 +193,22 @@ public abstract class BootstrapInputBase<TValue> : ValidateBase<TValue>
     /// <param name="result"></param>
     /// <param name="validationErrorMessage"></param>
     /// <returns></returns>
-    protected override bool TryParseValueFromString(string value, [MaybeNullWhen(false)] out TValue result, out string? validationErrorMessage) => base.TryParseValueFromString(IsTrim ? value.Trim() : value, out result, out validationErrorMessage);
+    protected override bool TryParseValueFromString(string value, [MaybeNullWhen(false)] out TValue result, out string? validationErrorMessage)
+    {
+        bool ret;
+        var v = IsTrim ? value.Trim() : value;
+        if (AutoSetDefaultWhenNull && string.IsNullOrEmpty(v))
+        {
+            result = default!;
+            validationErrorMessage = null;
+            ret = true;
+        }
+        else
+        {
+            ret = base.TryParseValueFromString(v, out result, out validationErrorMessage);
+        }
+        return ret;
+    }
 
     /// <summary>
     /// 客户端 EnterCallback 回调方法
