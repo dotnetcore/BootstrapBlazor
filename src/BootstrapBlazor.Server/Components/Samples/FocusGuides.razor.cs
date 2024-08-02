99db080767f157fedf57dc5b8c94a44d813ae231
Argo-AscioTech <argo@live.ca>
2024-08-02 17:46:01 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/FocusGuides.razor.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// FocusGuide 组件示例代码
+/// </summary>
+public partial class FocusGuides
+{
+    private FocusGuide _guide1 = default!;
+
+    private async Task OnStart()
+    {
+        await _guide1.Start();
+    }
+}
