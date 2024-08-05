0c98aacbea4014b62d7f6947a44471433c78f762
Argo-AscioTech <argo@live.ca>
2024-08-05 19:29:07 +0800

    refactor: 增加脚本

--- src/BootstrapBlazor/Components/IntersectionObserver/IntersectionObserver.razor.cs
+++ src/BootstrapBlazor/Components/IntersectionObserver/IntersectionObserver.razor.cs
@@ -2,12 +2,13 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// 交叉检测组件
 /// </summary>
-public partial class IntersectionObserverProvider
+public partial class IntersectionObserver
 {
     /// <summary>
     /// The element that is used as the viewport for checking visibility of the target. Must be the ancestor of the target. Defaults to the browser viewport if not specified or if null
@@ -25,13 +26,34 @@ public partial class IntersectionObserverProvider
     /// Either a single number or an array of numbers which indicate at what percentage of the target's visibility the observer's callback should be executed. If you only want to detect when visibility passes the 50% mark, you can use a value of 0.5. If you want the callback to run every time visibility passes another 25%, you would specify the array [0, 0.25, 0.5, 0.75, 1]. The default is 0 (meaning as soon as even one pixel is visible, the callback will be run). A value of 1.0 means that the threshold isn't considered passed until every pixel is visible.
     /// </summary>
     [Parameter]
-    public string? Threshold { get; set; }
+    public float Threshold { get; set; }
+
+    /// <summary>
+    /// 获得/设置 子组件
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-intersection-observer")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
 
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
-    protected override void OnInitialized()
+    protected override void OnParametersSet()
     {
-        base.OnInitialized();
+        base.OnParametersSet();
+
+        if (Threshold < 0 || Threshold > 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(Threshold), $"{nameof(Threshold)} must be between 0 and 1");
+        }
     }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, new { Root = RootSelector, RootMargin, Threshold });
 }
---
+++ src/BootstrapBlazor/Components/IntersectionObserver/IntersectionObserver.razor.js
@@ -0,0 +1,21 @@
+﻿import Data from "../../modules/data.js"
+
+export function init(id, invoke, options) {
+    console.log(options);
+
+    const observer = new IntersectionObserver(entries => {
+
+    }, options);
+
+    Data.set(id, observer);
+}
+
+export function dispose(id) {
+    const observer = Data.get(id);
+    Data.remove(id);
+
+    if (observer) {
+        observer.disconnect();
+        observer = null;
+    }
+}
