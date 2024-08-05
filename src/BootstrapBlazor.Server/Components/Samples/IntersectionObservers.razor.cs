476c935645644baf97dbc6e45bcb3741999dc8d1
Argo Zhang <argo@live.ca>
2024-08-05 20:06:44 +0800

    feat(IntersectionObserver): add IntersectionObserver component (#3984)
    * doc: 增加源代码映射关系
    
    * doc: 增加交叉检测组件
    
    * feat: 增加交叉检测组件
    
    * doc: 增加示例
    
    * refactor: 增加示例
    
    * refactor: 增加脚本
    
    * refactor: 更新交叉检测逻辑
    
    * doc: 更新示例
    
    * doc: 更新文档本地化

---
+++ src/BootstrapBlazor.Server/Components/Samples/IntersectionObservers.razor.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// 交叉检测组件示例
+/// </summary>
+public partial class IntersectionObservers
+{
+    private List<string> _images = default!;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _images = Enumerable.Range(1, 100).Select(i => "../../images/default.jpeg").ToList();
+    }
+
+    private Task OnIntersectingAsync(int index)
+    {
+        _images[index] = GetImageUrl(index);
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private static string GetImageUrl(int index) => $"https://picsum.photos/160/160?random={index}";
+}
---
+++ src/BootstrapBlazor/Components/IntersectionObserver/IntersectionObserver.razor.js
@@ -0,0 +1,30 @@
+﻿import Data from "../../modules/data.js"
+
+export function init(id, invoke, options) {
+    console.log(options);
+
+    const el = document.getElementById(id);
+    const items = [...el.querySelectorAll(".bb-intersection-observer-item")];
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                observer.unobserve(entry.target);
+                const index = items.indexOf(entry.target);
+                invoke.invokeMethodAsync('OnIntersecting', index);
+            };
+        });
+    }, options);
+
+    items.forEach(item => observer.observe(item));
+    Data.set(id, observer);
+}
+
+export function dispose(id) {
+    const observer = Data.get(id);
+    Data.remove(id);
+
+    if (observer) {
+        observer.disconnect();
+        observer = null;
+    }
+}
