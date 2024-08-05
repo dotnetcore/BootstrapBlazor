acb8bd16a1e32dcbf609287d3839bc69c255163c
TonyGuo16 <451026920@qq.com>
2024-08-05 15:51:36 +0800

    feat(LoadMore): Add Observe element scroll to the Bottom Trigger Event
    新增：监听元素滚动底部触发新增数据方法，数据获取失败后重新获取数据的方法

---
+++ src/BootstrapBlazor/Components/LoadMore/LoadMore.razor.cs
@@ -0,0 +1,94 @@
+﻿namespace BootstrapBlazor.Components;
+
+public partial class LoadMore
+    {
+    /// <summary>
+    /// 初始化加载JS
+    /// </summary>
+    /// <returns></returns>
+        protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, new { Invoke = Interop, OnLoading = nameof(OnLoading)});
+    /// <summary>
+    /// 是否刷新数据成功
+    /// </summary>
+    private bool IsSuccess = true;
+    /// <summary>
+    /// 加载数据方法由 JSInvoke 调用
+    /// </summary>
+    [JSInvokable]
+    public async Task OnLoading()
+    {
+        if (OnLoadingAsync != null)
+        {
+            IsSuccess = await OnLoadingAsync();
+
+            if (!IsSuccess)
+            {
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+    }
+    /// <summary>
+    /// 加载数据的方法
+    /// </summary>
+    [Parameter]
+    public Func<Task<bool>>? OnLoadingAsync { get; set; }
+    /// <summary>
+    /// 加载数据的方法
+    /// </summary>
+    [Parameter]
+    public Func<Task<bool>>? OnFailRetryAsync { get; set; }
+    /// <summary>
+    /// 重新获取数据方法
+    /// </summary>
+    public async Task OnRetryAsync()
+    {
+        IsSuccess = true;
+        if (OnFailRetryAsync != null)
+        {
+            IsSuccess = await OnFailRetryAsync();
+        }
+        await InvokeAsync(StateHasChanged);
+    }
+    /// <summary>
+    /// 加载文字--默认无文字
+    /// </summary>
+    [Parameter]
+    public string? LoadingText { get; set; }
+    /// <summary>
+    /// 加载失败文字--默认获取失败
+    /// </summary>
+    [Parameter]
+    public string? FailText { get; set; } = "获取失败";
+    /// <summary>
+    /// 默认css样式
+    /// </summary>
+    private string? ClassString => CssBuilder.Default("bb-load-more")
+        .AddClass("text-center")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+    /// <summary>
+    /// 默认获取失败css样式
+    /// </summary>
+    private string? FailClassString => CssBuilder.Default("bb-load-more-fail-text")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+    /// <summary>
+    /// 默认获取失败css样式
+    /// </summary>
+    private string? FailButtonClassString => CssBuilder.Default("bb-load-more-fail-button")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+    /// <summary>
+    /// 默认加载图标样式
+    /// </summary>
+    private string? ClassLoadingIcon => CssBuilder.Default("fa fa-spinner fa-spin")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+    /// <summary>
+    /// Style样式
+    /// </summary>
+    private string? StyleString => CssBuilder.Default()
+        .AddStyleFromAttributes(AdditionalAttributes)
+        .Build();
+}
+
---
+++ src/BootstrapBlazor/Components/LoadMore/LoadMore.razor.js
@@ -0,0 +1,46 @@
+﻿import Data from "../../modules/data.js"
+export function init(id, options) {
+    options = {
+        ...{
+            onLoading: null,
+        },
+        ...options
+    }
+    var isLoading = false;
+    const ob = new IntersectionObserver(
+        async (entries) => {
+            const entry = entries[0];
+            if (entry.isIntersecting) {
+                if (isLoading) {
+                    return;
+                }
+                isLoading = true;
+                await more();
+                isLoading = false;
+            }
+        },
+        {
+            root: null,
+            threshold: 0,
+        }
+
+    );
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
+    }
+    ob.observe(el);
+
+    //执行C#中的方法加载数据
+    async function more() {
+      options.invoke.invokeMethodAsync(options.onLoading);
+    }
+    Data.set(id, { el, options });
+}
+
+export function dispose(id) {
+    const loadMore = Data.get(id)
+    if (loadMore) {
+       //这里是否要移除obServer订阅
+    }
+}
