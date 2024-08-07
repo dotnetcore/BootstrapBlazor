2666452826e81dcc9ee9f8c02b5771ec38a59394
Argo-AscioTech <argo@live.ca>
2024-08-07 18:43:27 +0800

    refactor: 精简代码

--- src/Extensions/Components/BootstrapBlazor.DriverJs/Components/DriverJsStep.cs
+++ src/Extensions/Components/BootstrapBlazor.DriverJs/Components/DriverJsStep.cs
@@ -106,45 +106,4 @@ public class DriverJsStep : ComponentBase, IDisposable
         Dispose(true);
         GC.SuppressFinalize(this);
     }
-
-    ///// <summary>
-    ///// The popover configuration for this step.
-    ///// </summary>
-    //public FocusGuidePopover? Popover { get; set; }
-
-    ///// <summary>
-    ///// Callback when the current step is deselected
-    ///// </summary>
-    ///// <param name="step"></param>
-    ///// <param name="config"></param>
-    ///// <param name="state"></param>
-    ///// <returns></returns>
-    //public Task OnDeselected(FocusGuideStep step, FocusGuideConfig config, FocusGuideState state)
-    //{
-    //    return Task.CompletedTask;
-    //}
-
-    ///// <summary>
-    ///// 
-    ///// </summary>
-    ///// <param name="step"></param>
-    ///// <param name="config"></param>
-    ///// <param name="state"></param>
-    ///// <returns></returns>
-    //public Task OnHighlightStarted(FocusGuideStep step, FocusGuideConfig config, FocusGuideState state)
-    //{
-    //    return Task.CompletedTask;
-    //}
-
-    ///// <summary>
-    ///// 
-    ///// </summary>
-    ///// <param name="step"></param>
-    ///// <param name="config"></param>
-    ///// <param name="state"></param>
-    ///// <returns></returns>
-    //public Task OnHighlighted(FocusGuideStep step, FocusGuideConfig config, FocusGuideState state)
-    //{
-    //    return Task.CompletedTask;
-    //}
 }
