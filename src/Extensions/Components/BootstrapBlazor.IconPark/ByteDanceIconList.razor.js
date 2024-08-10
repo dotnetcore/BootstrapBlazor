7d4ffb3f7de74565947f2581fd73a05bc5d9d96e
Argo-AscioTech <argo@live.ca>
2024-08-10 17:33:53 +0800

    refactor: 字节跳动图标列表组件

---
+++ src/Extensions/Components/BootstrapBlazor.IconPark/ByteDanceIconList.razor.js
@@ -0,0 +1,76 @@
+﻿import { copy } from "../BootstrapBlazor/modules/utility.js"
+import Data from "../BootstrapBlazor/modules/data.js"
+import EventHandler from "../BootstrapBlazor/modules/event-handler.js"
+
+export function init(id, invoke, updateMethod, copyIcon) {
+    const el = document.getElementById(id);
+    const faList = {
+        element: el,
+        invoke,
+        updateMethod,
+        copyIcon
+    }
+    Data.set(id, faList)
+
+    EventHandler.on(el, 'click', '.icons-body a', e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const i = e.delegateTarget.querySelector('i')
+        const css = i.getAttribute('class')
+        faList.invoke.invokeMethodAsync(faList.updateMethod, css)
+        if (faList.copyIcon) {
+            faList.copy(e.delegateTarget, css)
+        }
+    })
+
+    faList.copy = (element, text) => {
+        copy(text)
+
+        faList.tooltip = bootstrap.Tooltip.getInstance(element)
+        if (faList.tooltip) {
+            faList.reset(element)
+        }
+        else {
+            faList.show(element)
+        }
+    }
+
+    faList.show = element => {
+        faList.tooltip = new bootstrap.Tooltip(element, {
+            title: faList.element.getAttribute('data-bb-title')
+        })
+        faList.tooltip.show()
+        faList.tooltipHandler = setTimeout(() => {
+            clearTimeout(faList.tooltipHandler)
+            if (faList.tooltip) {
+                faList.tooltip.dispose()
+            }
+        }, 1000)
+    }
+
+    faList.reset = element => {
+        if (faList.tooltipHandler) {
+            clearTimeout(faList.tooltipHandler)
+        }
+        if (faList.tooltip) {
+            faList.tooltipHandler = setTimeout(() => {
+                clearTimeout(faList.tooltipHandler)
+                faList.tooltip.dispose()
+                faList.show()
+            }, 10)
+        }
+        else {
+            faList.show(element)
+        }
+    }
+}
+
+export function dispose(id) {
+    const faList = Data.get(id)
+    Data.remove(id)
+
+    if (faList) {
+        EventHandler.off(faList.element, 'click', '.icons-body a')
+    }
+}
