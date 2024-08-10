9d00f6f937be8649c4c8d08abebc319ae86cf673
Argo-AscioTech <argo@live.ca>
2024-08-11 01:02:41 +0800

    doc: 增加 Element 示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/Icons/ElementIconList.razor.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Server.Components.Samples.Icons;
+
+/// <summary>
+/// ElementIconList 组件
+/// </summary>
+[JSModuleAutoLoader("Samples/Icons/ElementIconList.razor.js")]
+public partial class ElementIconList
+{
+    private string? ClassString => CssBuilder.Default("icon-list")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 拷贝成功提示文字
+    /// </summary>
+    [Parameter]
+    public string? CopiedTooltipText { get; set; }
+}
