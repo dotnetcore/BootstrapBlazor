4c480fa4873573abff5af17827714df310f7a335
Argo-AscioTech <argo@live.ca>
2024-08-11 00:25:43 +0800

    test: 增加 Element Icon 单元测试

--- test/UniTestIconPark/UnitTest.cs
+++ test/UniTestIconPark/UnitTest.cs
@@ -68,6 +68,72 @@ public partial class UnitTest
         writer.Close();
     }
 
+    [Fact]
+    public void Element_Ok()
+    {
+        var category = "element";
+        var services = new ServiceCollection();
+        services.AddBootstrapBlazor();
+        var provider = services.BuildServiceProvider();
+        var zipService = provider.GetRequiredService<IZipArchiveService>();
+
+        var root = AppContext.BaseDirectory;
+        var downloadFile = Path.Combine(root, $"{category}.zip");
+        Assert.True(File.Exists(downloadFile));
+
+        var downloadFolder = Path.Combine(root, category);
+        if (Directory.Exists(downloadFile))
+        {
+            Directory.Delete(downloadFile, true);
+        }
+        zipService.ExtractToDirectory(downloadFile, downloadFolder, true);
+
+        var folder = new DirectoryInfo(downloadFolder);
+
+        // 处理 List 文件
+        var iconListFile = Path.Combine(root, $"../../../ElementIconList.razor");
+        if (File.Exists(iconListFile))
+        {
+            File.Delete(iconListFile);
+        }
+
+        // 处理 svg 文件
+        var svgFile = Path.Combine(root, $"../../../{category}.svg");
+        if (File.Exists(svgFile))
+        {
+            File.Delete(svgFile);
+        }
+        using var listWriter = new StreamWriter(File.OpenWrite(iconListFile));
+        using var writer = new StreamWriter(File.OpenWrite(svgFile));
+        writer.WriteLine("<svg xmlns=\"http://www.w3.org/2000/svg\">");
+        foreach (var icon in folder.EnumerateFiles())
+        {
+            var id = Path.GetFileNameWithoutExtension(icon.Name);
+            using var reader = new StreamReader(icon.OpenRead());
+            var data = reader.ReadToEnd();
+            reader.Close();
+
+            // find <svg
+            var index = data.IndexOf("<svg ");
+            if (index > -1)
+            {
+                data = data[index..];
+            }
+            index = data.IndexOf(">");
+            if (index > -1)
+            {
+                data = data[(index + 1)..];
+            }
+            var target = data.Replace("</svg>", "").Trim();
+            target = $"    <symbol viewBox=\"0 0 1024 1024\" id=\"{id}\">{target}</symbol>";
+            writer.WriteLine(target);
+
+            listWriter.WriteLine($"<a><ElementIcon Name=\"{id}\"></ElementIcon><span>{id}</span></a>");
+        }
+        writer.WriteLine("</svg>");
+        writer.Close();
+    }
+
     [GeneratedRegex("svg\">(.*)</svg>")]
     private static partial Regex SvgRegex();
 }
