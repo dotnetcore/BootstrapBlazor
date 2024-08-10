3b16eddac181c26c74e9c11c7c0cea846247c7d9
Argo-AscioTech <argo@live.ca>
2024-08-10 18:30:56 +0800

    doc: 增加 IconList 组件

---
+++ src/BootstrapBlazor.Server/Components/Samples/Icons/FAIconList.razor.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Microsoft.JSInterop;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// FAIconList 组件
+/// </summary>
+[JSModuleAutoLoader("./_content/BootstrapBlazor.FontAwesome/Components/FAIconList.razor.js", JSObjectReference = true)]
+public partial class FAIconList : IAsyncDisposable
+{
+    private string? ClassString => CssBuilder.Default("icon-list")
+        .AddClass("is-catalog", ShowCatalog)
+        .AddClass("is-dialog", ShowCopyDialog)
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 点击时是否显示高级拷贝弹窗 默认 false 直接拷贝到粘贴板
+    /// </summary>
+    [Parameter]
+    public bool ShowCopyDialog { get; set; }
+
+    /// <summary>
+    /// 获得/设置 是否显示目录 默认 false
+    /// </summary>
+    [Parameter]
+    public bool ShowCatalog { get; set; }
+
+    /// <summary>
+    /// 获得/设置 高级弹窗 Header 显示文字
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public string? DialogHeaderText { get; set; }
+
+    /// <summary>
+    /// 获得/设置 当前选择图标
+    /// </summary>
+    [Parameter]
+    public string? Icon { get; set; }
+
+    /// <summary>
+    /// 获得/设置 当前选择图标回调方法
+    /// </summary>
+    [Parameter]
+    public EventCallback<string?> IconChanged { get; set; }
+
+    /// <summary>
+    /// 获得/设置 点击图标是否进行拷贝处理 默认 false
+    /// </summary>
+    [Parameter]
+    public bool IsCopy { get; set; }
+
+    /// <summary>
+    /// 获得/设置 拷贝成功提示文字
+    /// </summary>
+    [Parameter]
+    public string? CopiedTooltipText { get; set; }
+
+    [Inject]
+    [NotNull]
+    private DialogService? DialogService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<IconDialog>? Localizer { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        DialogHeaderText ??= Localizer[nameof(DialogHeaderText)];
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, nameof(UpdateIcon), nameof(ShowDialog), IsCopy);
+
+    /// <summary>
+    /// UpdateIcon 方法由 JS Invoke 调用
+    /// </summary>
+    /// <param name="icon"></param>
+    [JSInvokable]
+    public async Task UpdateIcon(string icon)
+    {
+        Icon = icon;
+        if (IconChanged.HasDelegate)
+        {
+            await IconChanged.InvokeAsync(Icon);
+        }
+        else
+        {
+            StateHasChanged();
+        }
+    }
+
+    /// <summary>
+    /// ShowDialog 方法由 JS Invoke 调用
+    /// </summary>
+    /// <returns></returns>
+    [JSInvokable]
+    public Task ShowDialog(string text) => DialogService.ShowCloseDialog<IconDialog>(DialogHeaderText, parameters =>
+    {
+        parameters.Add(nameof(IconDialog.IconName), text);
+    });
+}
---
+++ src/BootstrapBlazor.Server/Components/Samples/Icons/FAIconList.razor.js
@@ -0,0 +1,115 @@
+﻿import { copy } from "../../BootstrapBlazor/modules/utility.js"
+import Data from "../../BootstrapBlazor/modules/data.js"
+import EventHandler from "../../BootstrapBlazor/modules/event-handler.js"
+
+export function init(id, invoke, updateMethod, showDialogMethod, copyIcon) {
+    const el = document.getElementById(id);
+    const faList = {
+        element: el,
+        invoke,
+        updateMethod,
+        showDialogMethod,
+        copyIcon
+    }
+    Data.set(id, faList)
+
+    if (el.classList.contains('is-catalog')) {
+        faList.body = el.querySelector('.icons-body')
+        faList.target = faList.body.getAttribute('data-bs-target')
+        faList.scrollspy = new bootstrap.ScrollSpy(faList.body, {
+            target: faList.target
+        })
+        faList.base = el.querySelector('#bb-fa-top')
+    }
+
+    EventHandler.on(el, 'click', '.nav-link', e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const targetId = e.delegateTarget.getAttribute('href')
+        const target = el.querySelector(targetId)
+
+        const rect = target.getBoundingClientRect()
+        let margin = rect.top
+        let marginTop = getComputedStyle(target).getPropertyValue('margin-top').replace('px', '')
+        if (marginTop) {
+            margin = margin - parseInt(marginTop)
+        }
+        const offset = el.getAttribute('bb-data-offset')
+        if (offset) {
+            margin = margin - parseInt(offset)
+        }
+        margin = margin - faList.base.getBoundingClientRect().top
+        faList.body.scrollTo(0, margin)
+    })
+
+    EventHandler.on(el, 'click', '.icons-body a', e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        const i = e.delegateTarget.querySelector('i')
+        const css = i.getAttribute('class')
+        faList.invoke.invokeMethodAsync(faList.updateMethod, css)
+        const dialog = el.classList.contains('is-dialog')
+        if (dialog) {
+            faList.invoke.invokeMethodAsync(faList.showDialogMethod, css)
+        } else if (faList.copyIcon) {
+            faList.copy(e.delegateTarget, css)
+        }
+    })
+
+    faList.copy = (element, text) => {
+        copy(text)
+
+        faList.tooltip = bootstrap.Tooltip.getInstance(element)
+        if (faList.tooltip) {
+            faList.reset(element)
+        }
+        else {
+            faList.show(element)
+        }
+    }
+
+    faList.show = element => {
+        faList.tooltip = new bootstrap.Tooltip(element, {
+            title: faList.element.getAttribute('data-bb-title')
+        })
+        faList.tooltip.show()
+        faList.tooltipHandler = setTimeout(() => {
+            clearTimeout(faList.tooltipHandler)
+            if (faList.tooltip) {
+                faList.tooltip.dispose()
+            }
+        }, 1000)
+    }
+
+    faList.reset = element => {
+        if (faList.tooltipHandler) {
+            clearTimeout(faList.tooltipHandler)
+        }
+        if (faList.tooltip) {
+            faList.tooltipHandler = setTimeout(() => {
+                clearTimeout(faList.tooltipHandler)
+                faList.tooltip.dispose()
+                faList.show()
+            }, 10)
+        }
+        else {
+            faList.show(element)
+        }
+    }
+}
+
+export function dispose(id) {
+    const faList = Data.get(id)
+    Data.remove(id)
+
+    if (faList) {
+        EventHandler.off(faList.element, 'click', '.nav-link')
+        EventHandler.off(faList.element, 'click', '.icons-body a')
+
+        if (faList.scrollspy) {
+            faList.scrollspy.dispose()
+        }
+    }
+}
