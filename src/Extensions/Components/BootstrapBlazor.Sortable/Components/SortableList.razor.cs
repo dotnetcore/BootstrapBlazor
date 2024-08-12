69c1b26fb11533cf231e5538c658635ed2c617df
Argo-AscioTech <argo@live.ca>
2024-08-12 09:21:32 +0800

    feat: 增加 SoratableList 组件

---
+++ src/Extensions/Components/BootstrapBlazor.Sortable/Components/SortableList.razor.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// SortableList 组件
+/// </summary>
+public partial class SortableList
+{
+    /// <summary>
+    /// 获得/设置 配置项实例 <see cref="SortableOption"/>
+    /// </summary>
+    [Parameter]
+    public SortableOption? Option {get;set;}
+
+    /// <summary>
+    /// 获得/设置 子组件 必填项不可为空
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-sortable")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, Option);
+}
---
+++ src/Extensions/Components/BootstrapBlazor.Sortable/Components/SortableList.razor.js
@@ -0,0 +1,38 @@
+﻿import Sortable from '../sortable.esm.js'
+import Data from '../../BootstrapBlazor/modules/data.js'
+
+export function init(id, invoke, options) {
+    const el = document.getElementById(id);
+    if(el === null) {
+        return;
+    }
+
+    const op = getOptions(options);
+    let element = el;
+    if(op.rootSelector) {
+        element = el.querySelector(options.rootSelector);
+        delete op.rootSelector;
+    }
+
+    if(element) {
+        const sortable = Sortable.create(element, op);
+        Data.set(id, {el, element, sortable});
+    }
+}
+
+const getOptions = options => {
+    return {
+        rootSelector: null,
+        animation: 150,
+        ...options
+    }
+}
+
+export function dispose(id) {
+    const sortable = Data.get(id);
+    Data.remove(id);
+
+    if(sortable) {
+
+    }
+}
