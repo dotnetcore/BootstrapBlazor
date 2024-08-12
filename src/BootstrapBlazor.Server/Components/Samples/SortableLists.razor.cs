ec1ebd6bb20e18a54300be7edd5f83e99268fa35
Argo-AscioTech <argo@live.ca>
2024-08-12 09:41:17 +0800

    doc: 增加 SortableList 示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/SortableLists.razor.cs
@@ -0,0 +1,40 @@
+﻿namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// SortableList 示例
+/// </summary>
+public partial class SortableLists
+{
+    [NotNull]
+    private List<Foo>? Items { get; set; }
+
+    private readonly SortableOption _option1 = new()
+    {
+        RootSelector = ".sl-list",
+        GhostClass = "sl-item-ghost"
+    };
+
+    private readonly SortableOption _option2 = new()
+    {
+        RootSelector = ".sl-list",
+        GhostClass = "sl-item-ghost",
+        Group = "group"
+    };
+
+    private readonly SortableOption _option = new()
+    {
+        RootSelector = "tbody"
+    };
+
+    /// <summary>
+    /// OnInitialized
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        //获取随机数据
+        //Get random data
+        Items = Foo.GenerateFoo(FooLocalizer);
+    }
+}
