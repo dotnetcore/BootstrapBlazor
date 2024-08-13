dc212e8da006989c9553e62b1aa71dbba2905314
Argo-AscioTech <argo@live.ca>
2024-08-13 16:48:28 +0800

    refactor: 增加 deepMerge 方法

--- src/BootstrapBlazor/wwwroot/modules/utility.js
+++ src/BootstrapBlazor/wwwroot/modules/utility.js
@@ -775,6 +775,20 @@ export function switchTheme(theme, x = 0, y = 0, sync = true) {
     }
 }
 
+const deepMerge = (obj1, obj2) => {
+    for (let key in obj2) {
+        if (obj2.hasOwnProperty(key)) {
+            if (obj2[key] instanceof Object && obj1[key] instanceof Object) {
+                obj1[key] = deepMerge(obj1[key], obj2[key]);
+            }
+            else {
+                obj1[key] = obj2[key];
+            }
+        }
+    }
+    return obj1;
+}
+
 export {
     autoAdd,
     autoRemove,
@@ -785,10 +799,11 @@ export {
     addLink,
     addScript,
     copy,
-    getTextFromClipboard,
-    getAllClipboardContents,
+    deepMerge,
     debounce,
     drag,
+    getTextFromClipboard,
+    getAllClipboardContents,
     insertBefore,
     insertAfter,
     isDisabled,
