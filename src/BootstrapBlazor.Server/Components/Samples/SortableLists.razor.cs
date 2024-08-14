798e6c0b5378c2eb8bc3dbc93ee4080e0f481b0f
Argo-AscioTech <argo@live.ca>
2024-08-14 16:59:55 +0800

    doc: 增加多项拖动示例

--- src/BootstrapBlazor.Server/Components/Samples/SortableLists.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/SortableLists.razor.cs
@@ -14,6 +14,9 @@ public partial class SortableLists
     [NotNull]
     private List<Foo>? Items2 { get; set; }
 
+    [NotNull]
+    private List<Foo>? ItemsMultiDrags { get; set; }
+
     private readonly SortableOption _option1 = new()
     {
         RootSelector = ".sl-list",
@@ -97,10 +100,13 @@ public partial class SortableLists
         Items = Foo.GenerateFoo(FooLocalizer, 8);
         Items1 = Foo.GenerateFoo(FooLocalizer, 4);
         Items2 = Foo.GenerateFoo(FooLocalizer, 8).Skip(4).ToList();
+        ItemsMultiDrags = Foo.GenerateFoo(FooLocalizer, 8);
     }
 
-    private Task OnUpdate(int oldIndex, int newIndex)
+    private Task OnUpdate(List<SortableListItem> items)
     {
+        var oldIndex = items[0].OldIndex;
+        var newIndex = items[0].NewIndex;
         var item = Items[oldIndex];
         Items.RemoveAt(oldIndex);
         Items.Insert(newIndex, item);
@@ -108,8 +114,10 @@ public partial class SortableLists
         return Task.CompletedTask;
     }
 
-    private Task OnUpdate1(int oldIndex, int newIndex)
+    private Task OnUpdate1(List<SortableListItem> items)
     {
+        var oldIndex = items[0].OldIndex;
+        var newIndex = items[0].NewIndex;
         var item = Items1[oldIndex];
         Items1.RemoveAt(oldIndex);
         Items1.Insert(newIndex, item);
@@ -117,8 +125,10 @@ public partial class SortableLists
         return Task.CompletedTask;
     }
 
-    private Task OnUpdate2(int oldIndex, int newIndex)
+    private Task OnUpdate2(List<SortableListItem> items)
     {
+        var oldIndex = items[0].OldIndex;
+        var newIndex = items[0].NewIndex;
         var item = Items[oldIndex];
         Items2.RemoveAt(oldIndex);
         Items2.Insert(newIndex, item);
@@ -126,8 +136,10 @@ public partial class SortableLists
         return Task.CompletedTask;
     }
 
-    private Task OnRemove1(int oldIndex, int newIndex)
+    private Task OnRemove1(List<SortableListItem> items)
     {
+        var oldIndex = items[0].OldIndex;
+        var newIndex = items[0].NewIndex;
         var item = Items1[oldIndex];
         Items1.RemoveAt(oldIndex);
         Items2.Insert(newIndex, item);
@@ -135,12 +147,34 @@ public partial class SortableLists
         return Task.CompletedTask;
     }
 
-    private Task OnRemove2(int oldIndex, int newIndex)
+    private Task OnRemove2(List<SortableListItem> items)
     {
+        var oldIndex = items[0].OldIndex;
+        var newIndex = items[0].NewIndex;
         var item = Items2[oldIndex];
         Items2.RemoveAt(oldIndex);
         Items1.Insert(newIndex, item);
         StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private Task OnUpdateMultiDrag(List<SortableListItem> items)
+    {
+        // 找到移除元素
+        var removeItems = new List<Foo>();
+        for (var index = items.Count - 1; index >= 0; index--)
+        {
+            var item = ItemsMultiDrags[items[index].OldIndex];
+            removeItems.Insert(0, item);
+            ItemsMultiDrags.RemoveAt(items[index].OldIndex);
+        }
+
+        // 插入元素
+        for (var index = 0; index < items.Count; index++)
+        {
+            var item = removeItems[index];
+            ItemsMultiDrags.Insert(items[index].NewIndex, item);
+        }
         return Task.CompletedTask;
     }
 }
