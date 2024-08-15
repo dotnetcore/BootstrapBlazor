e8d983a4424a08c05c4fe4f92d9abfaed3d2ef60
momijijin <2648863351@qq.com>
2024-08-15 11:58:42 +0800

    feat: Add new version of color picker associated file (file contents copied from old version)

---
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPickerV2.razor.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+public partial class ColorPickerV2
+{
+    /// <summary>
+    /// 获得 class 样式集合
+    /// </summary>
+    protected string? ClassName => CssBuilder.Default("form-control")
+        .AddClass(CssClass).AddClass(ValidCss)
+        .Build();
+
+    /// <summary>
+    /// 获得/设置 显示模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment<string>? Template { get; set; }
+
+    /// <summary>
+    /// 获得/设置 显示颜色值格式化回调方法
+    /// </summary>
+    [Parameter]
+    public Func<string, Task<string>>? Formatter { get; set; }
+
+    private string? _formattedValueString;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+
+        await FormatValue();
+    }
+
+    private async Task Setter(string v)
+    {
+        CurrentValueAsString = v;
+        await FormatValue();
+    }
+
+    private async Task FormatValue()
+    {
+        if (Formatter != null)
+        {
+            // 此处未使用父类 FormatValueAsString 方法
+            // 使用者可能需要通过回调通过异步方式获得显示数据
+            _formattedValueString = await Formatter(CurrentValueAsString);
+        }
+    }
+}
+
---
+++ src/BootstrapBlazor/Components/ColorPicker/ColorPickerV2.razor.js
@@ -0,0 +1,18 @@
+﻿import Data from "../../modules/data.js"
+
+/**
+ * 暂时不知道有啥用
+ * @param id
+ */
+export function init(id) {
+    const el = document.getElementById(id)
+    Data.set(id, el)
+}
+
+/**
+ * 暂时不知道有啥用
+ * @param id
+ */
+export function dispose(id) {
+    Data.remove(id)
+}
