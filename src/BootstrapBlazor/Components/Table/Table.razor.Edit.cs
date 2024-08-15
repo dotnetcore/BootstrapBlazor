ac2075209a59a8ec0936d57065759de9d981525b
Argo-AscioTech <argo@live.ca>
2024-08-15 23:32:33 +0800

    Revert "fix: 修复 ResetSelectedRows 未回调问题"
    This reverts commit f1484a526f848c8ce3d8b5a83e109a922afeea9c.

--- src/BootstrapBlazor/Components/Table/Table.razor.Edit.cs
+++ src/BootstrapBlazor/Components/Table/Table.razor.Edit.cs
@@ -608,11 +608,6 @@ public partial class Table<TItem>
         if (SelectedRows.Count > 0)
         {
             SelectedRows = items.Where(i => SelectedRows.Any(row => Equals(i, row))).ToList();
-
-            if (SelectedRowsChanged.HasDelegate)
-            {
-                SelectedRowsChanged.InvokeAsync(SelectedRows);
-            }
         }
     }
 
