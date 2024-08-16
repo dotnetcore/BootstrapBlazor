3ff0af23f59ff50d408bd7082c7868c9da2cda86
Argo-AscioTech <argo@live.ca>
2024-08-16 10:17:59 +0800

    doc: 增加注释文档

--- src/BootstrapBlazor/Services/IEntityFrameworkCoreDataService.cs
+++ src/BootstrapBlazor/Services/IEntityFrameworkCoreDataService.cs
@@ -5,18 +5,18 @@
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// 
+/// IEntityFrameworkCoreDataService 接口
 /// </summary>
 public interface IEntityFrameworkCoreDataService
 {
     /// <summary>
-    /// 
+    /// 取消方法，由于编辑时使用的是克隆数据，常见取消用法不需要写任何代码，可用于保存数据下次编辑时恢复
     /// </summary>
     /// <returns></returns>
     Task CancelAsync();
 
     /// <summary>
-    /// 
+    /// 编辑方法，可对未提供编辑 UI 的数据进行填充
     /// </summary>
     /// <param name="model"></param>
     /// <returns></returns>
