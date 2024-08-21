74633f6adc57ffc85e427f0cc8824602ef919b93
Argo-AscioTech <argo@live.ca>
2024-08-21 16:24:59 +0800

    feat: 增加 WinBox 组件

---
+++ src/Extensions/Components/BootstrapBlazor.WinBox/WinBox.razor.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.AspNetCore.Components;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// WinBox 组件
+/// </summary>
+public partial class WinBox : IDisposable
+{
+    /// <summary>
+    /// DialogServices 服务实例
+    /// </summary>
+    [Inject]
+    [NotNull]
+    private WinBoxService? WinBoxService { get; set; }
+
+    /// <summary>
+    /// OnInitialized 方法
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        // 注册 Dialog 弹窗事件
+        WinBoxService.Register(this, Show);
+    }
+
+    private Task Show(WinBoxOption option) => InvokeVoidAsync("show", option);
+
+    /// <summary>
+    /// Dispose 方法
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            WinBoxService.UnRegister(this);
+        }
+    }
+
+    /// <summary>
+    /// Dispose 方法
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
---
+++ src/Extensions/Components/BootstrapBlazor.WinBox/WinBox.razor.js
@@ -0,0 +1,18 @@
+﻿import './js/winbox.min.js'
+import Data from '../BootstrapBlazor/modules/data.js'
+import EventHanlder from '../BootstrapBlazor/modules/event-handler.js'
+import { addLink } from "../BootstrapBlazor/modules/utility.js"
+
+export async function init(id, invoke, options) {
+    await addLink('./_content/BootstrapBlazor.WinBox/css/winbox.min.css')
+}
+
+export function show(options) {
+    new WinBox({
+        title: 'Test'
+    });
+}
+
+export function dispose(id) {
+
+}
