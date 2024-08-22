be83f7812f24b2d38521fd49d81aabc51967515f
Argo Zhang <argo@live.ca>
2024-08-22 15:58:35 +0800

    feat(WinBox): add WinBox component (#4118)
    * chore: 增加组件资源
    
    * chore: 增加服务
    
    * feat: 增加 WinBox 服务
    
    * feat: 增加 WinBox 组件
    
    * chore: 增加 Logo
    
    * chore: 增加 WinBox 工程
    
    * doc: 增加 WinBox 代码映射配置
    
    * doc: 代码格式化
    
    * doc: 增加示例组件
    
    * chore: 增加服务
    
    * doc: 增加菜单
    
    * doc: 移除不使用的脚本
    
    * chore: 增加 WinBox 工程
    
    * refactor: 增加扩展方法
    
    * feat: 增加 Close 回调方法
    
    * doc: 增加本地化菜单
    
    * doc: 增加默认示例
    
    * refactor: 更新弹窗状态改变回调方法
    
    * refactor: 增加回调方法
    
    * doc: 更新示例
    
    * doc: 增加参数说明文档
    
    * doc: 更新注释
    
    * chore: 增加依赖包
    
    * refactor: 增加自定义样式
    
    * chore: bump version 8.0.0-beta02

---
+++ src/Extensions/Components/BootstrapBlazor.WinBox/WinBox.razor.js
@@ -0,0 +1,71 @@
+﻿import './js/winbox.min.js'
+import Data from '../BootstrapBlazor/modules/data.js'
+import EventHanlder from '../BootstrapBlazor/modules/event-handler.js'
+import { addLink } from "../BootstrapBlazor/modules/utility.js"
+
+export async function init(id, invoke, options) {
+    await addLink('./_content/BootstrapBlazor.WinBox/css/winbox.bundle.css')
+}
+
+export function show(id, invoke, option) {
+    const el = document.getElementById(id);
+    const content = el.querySelector('.bb-win-box-content');
+    const config = {
+        ...option,
+        title: 'Test',
+        mount: content,
+        onclose: () => {
+            invoke.invokeMethodAsync("OnClose", option.id);
+        }
+    }
+    if (option.triggerOnCreate) {
+        config.oncreate = () => {
+            invoke.invokeMethodAsync("OnCreate", option.id);
+        }
+    }
+    if (option.triggerOnShown) {
+        config.onshown = () => {
+            invoke.invokeMethodAsync("OnShown", option.id);
+        }
+    }
+    if (option.triggerOnHide) {
+        config.onhide = () => {
+            invoke.invokeMethodAsync("OnHide", option.id);
+        }
+    }
+    if (option.triggerOnFocus) {
+        config.onfocus = () => {
+            invoke.invokeMethodAsync("OnFocus", option.id);
+        }
+    }
+    if (option.triggerOnBlur) {
+        config.onblur = () => {
+            invoke.invokeMethodAsync("OnBlur", option.id);
+        }
+    }
+    if (option.triggerOnFullscreen) {
+        config.onfullscreen = () => {
+            invoke.invokeMethodAsync("OnFullscreen", option.id);
+        }
+    }
+    if (option.triggerOnRestore) {
+        config.onrestore = () => {
+            invoke.invokeMethodAsync("OnRestore", option.id);
+        }
+    }
+    if (option.triggerOnMaximize) {
+        config.onmaximize = () => {
+            invoke.invokeMethodAsync("OnMaximize", option.id);
+        }
+    }
+    if (option.triggerOnMinimize) {
+        config.onminimize = () => {
+            invoke.invokeMethodAsync("OnMinimize", option.id);
+        }
+    }
+    new WinBox(config);
+}
+
+export function dispose(id) {
+
+}
