579c826e56e84039c6c98ddf04b2bc6432d88a4f
Argo-AscioTech <argo@live.ca>
2024-08-22 15:03:44 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/WinBoxes.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/WinBoxes.razor.cs
@@ -12,6 +12,8 @@ public partial class WinBoxes
     [Inject, NotNull]
     private WinBoxService? WinBoxService { get; set; }
 
+    private ConsoleLogger _logger = default!;
+
     private static WinBoxOption DefaultOptions => new()
     {
         Top = "50px",
@@ -20,6 +22,57 @@ public partial class WinBoxes
 
     private async Task OpenWinBox()
     {
-        await WinBoxService.Show<Counter>("Test", option: DefaultOptions);
+        var option = DefaultOptions;
+        option.OnCloseAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnCloseAsync");
+            return Task.CompletedTask;
+        };
+        option.OnCreateAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnCreateAsync");
+            return Task.CompletedTask;
+        };
+        option.OnShownAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnShownAsync");
+            return Task.CompletedTask;
+        };
+        option.OnHideAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnHideAsync");
+            return Task.CompletedTask;
+        };
+        option.OnFocusAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnFocusAsync");
+            return Task.CompletedTask;
+        };
+        option.OnBlurAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnBlurAsync");
+            return Task.CompletedTask;
+        };
+        option.OnFullscreenAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnFullscreenAsync");
+            return Task.CompletedTask;
+        };
+        option.OnRestoreAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnRestoreAsync");
+            return Task.CompletedTask;
+        };
+        option.OnMaximizeAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnMaximizeAsync");
+            return Task.CompletedTask;
+        };
+        option.OnMinimizeAsync = () =>
+        {
+            _logger.Log($"WinBoxOption({option.Id}) Trigger OnMinimizeAsync");
+            return Task.CompletedTask;
+        };
+        await WinBoxService.Show<Counter>("Test", option: option);
     }
 }
