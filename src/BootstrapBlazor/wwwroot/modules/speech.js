26044b7fdb1edde683427c3e738d64900f81e422
Argo-AscioTech <argo@live.ca>
2024-08-23 20:07:50 +0800

    feat: 增加 WebSpeechService 服务

---
+++ src/BootstrapBlazor/wwwroot/modules/speech.js
@@ -0,0 +1,33 @@
+﻿import Data from "./data.js"
+
+export function speak(id, invoke, option) {
+    const synth = window.speechSynthesis;
+    if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+    }
+    const { text, lang } = option;
+    if (text !== "") {
+        const utter = new SpeechSynthesisUtterance(text);
+        if (lang) {
+            utter.lang = lang;
+        }
+
+        utter.onend = function (event) {
+            console.log(event, "SpeechSynthesisUtterance.onend");
+        };
+
+        utter.onerror = function (event) {
+            console.error("SpeechSynthesisUtterance.onerror");
+        };
+        synth.speak(utter);
+    }
+}
+
+export function pause(id) {
+
+}
+
+export function resume(id) {
+
+}
