133e636cd13dd204a81f12ecd3b522a76af2aa21
Argo-AscioTech <argo@live.ca>
2024-08-23 08:57:58 +0800

    feat: 增加 ShowModal 方法

--- src/Extensions/Components/BootstrapBlazor.WinBox/Extensions/WinBoxServiceExtensions.cs
+++ src/Extensions/Components/BootstrapBlazor.WinBox/Extensions/WinBoxServiceExtensions.cs
@@ -36,4 +36,31 @@ public static class WinBoxServiceExtensions
         option.ContentTemplate = BootstrapDynamicComponent.CreateComponent(type, parameters).Render();
         await service.Show(option);
     }
+
+    /// <summary>
+    /// <see cref="WinBoxService"/> 扩展方法
+    /// </summary>
+    /// <typeparam name="TComponent">内容组件</typeparam>
+    /// <param name="service"><see cref="WinBoxService"/> 实例</param>
+    /// <param name="title">指定窗口标题</param>
+    /// <param name="parameters">指定组件参数集合</param>
+    /// <param name="option">额外参数集合</param>
+    public static Task ShowModal<TComponent>(this WinBoxService service, string title, IDictionary<string, object?>? parameters = null, WinBoxOption? option = null) where TComponent : ComponentBase => ShowModal(service, title, typeof(TComponent), parameters, option);
+
+    /// <summary>
+    /// <see cref="WinBoxService"/> 扩展方法
+    /// </summary>
+    /// <param name="service"><see cref="WinBoxService"/> 实例</param>
+    /// <param name="title">指定窗口标题</param>
+    /// <param name="type">指定内容组件类型</param>
+    /// <param name="parameters">指定组件参数集合</param>
+    /// <param name="option">额外参数集合</param>
+    public static async Task ShowModal(this WinBoxService service, string title, Type type, IDictionary<string, object?>? parameters = null, WinBoxOption? option = null)
+    {
+        option ??= new WinBoxOption();
+        option.Title = title;
+        option.Modal = true;
+        option.ContentTemplate = BootstrapDynamicComponent.CreateComponent(type, parameters).Render();
+        await service.Show(option);
+    }
 }
