c9a7c8897f29268eefa5ce503c80d21e0e2e6fbe
Argo-AscioTech <argo@live.ca>
2024-08-25 00:34:27 +0800

    test: 移除共享

--- test/UnitTest/Core/BootstrapBlazorTestBase.cs
+++ test/UnitTest/Core/BootstrapBlazorTestBase.cs
@@ -3,11 +3,9 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace UnitTest.Core;
 
-[Collection("BlazorTestContext")]
 public class BootstrapBlazorTestBase
 {
     protected TestContext Context { get; }
@@ -16,38 +14,16 @@ public class BootstrapBlazorTestBase
 
     public BootstrapBlazorTestBase()
     {
-        Context = BootstrapBlazorTestHost.Instance;
-        Cache = BootstrapBlazorTestHost.Cache;
-    }
-}
-
-[CollectionDefinition("BlazorTestContext")]
-public class BootstrapBlazorTestCollection : ICollectionFixture<BootstrapBlazorTestHost>
-{
-
-}
-
-public class BootstrapBlazorTestHost : IDisposable
-{
-    [NotNull]
-    internal static TestContext? Instance { get; private set; }
-
-    [NotNull]
-    internal static ICacheManager? Cache { get; private set; }
-
-    public BootstrapBlazorTestHost()
-    {
-        Instance = new TestContext();
-
+        Context = new TestContext();
         // Mock 脚本
-        Instance.JSInterop.Mode = JSRuntimeMode.Loose;
+        Context.JSInterop.Mode = JSRuntimeMode.Loose;
 
-        ConfigureServices(Instance.Services);
+        ConfigureServices(Context.Services);
 
-        ConfigureConfiguration(Instance.Services);
+        ConfigureConfiguration(Context.Services);
 
         // 渲染 BootstrapBlazorRoot 组件 激活 ICacheManager 接口
-        Cache = Instance.Services.GetRequiredService<ICacheManager>();
+        Cache = Context.Services.GetRequiredService<ICacheManager>();
     }
 
     protected virtual void ConfigureServices(IServiceCollection services)
@@ -64,12 +40,6 @@ public class BootstrapBlazorTestHost : IDisposable
     {
         // 增加单元测试 appsettings.json 配置文件
         services.AddConfiguration();
-    }
-
-    public void Dispose()
-    {
-        Instance.Dispose();
-        GC.SuppressFinalize(this);
     }
 
     class FooLookupService : LookupServiceBase
