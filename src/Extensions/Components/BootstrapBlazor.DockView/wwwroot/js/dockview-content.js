5945b0fb3cc1c68e44e6aee1e7e39e6546c22739
chengKun <49547008+zhaijunlei955@users.noreply.github.com>
2024-08-26 17:15:43 +0800

    feat(DockViewV2): support Renderer parameter (#4153)
    * fix: 修复dockview切换tab或拖动时iframe重新加载
    
    * fix: 给拖动时新增的group添加ResizeObserver
    
    * fix: 修复把floating group拖入结构中时浮动按钮显示状态异常
    
    * refactor：panel添加内边距
    
    * refactor: 代码格式化
    
    * refactor: 增加异常保护
    
    * fix：修复title拼接错误导致自定义布局页面报错
    
    * chore: bump version 8.1.1
    
    * doc: 更新依赖包
    
    ---------
    
    Co-authored-by: Argo-AscioTech <argo@live.ca>

--- src/Extensions/Components/BootstrapBlazor.DockView/wwwroot/js/dockview-content.js
+++ src/Extensions/Components/BootstrapBlazor.DockView/wwwroot/js/dockview-content.js
@@ -11,7 +11,7 @@
         if (template) {
             this._element = key
                 ? template.querySelector(`[data-bb-key="${key}"]`)
-                : (template.querySelector(`#${this.option.id}`) ?? template.querySelector(`[data-bb-title="${title}]"`))
+                : (template.querySelector(`#${this.option.id}`) ?? template.querySelector(`[data-bb-title="${title}"]`))
         }
 
         if (titleClass) {
