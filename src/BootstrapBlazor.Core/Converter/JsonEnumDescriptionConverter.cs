1fdc09d928cc67e8133604eeaf63939a3d7b821b
Argo-AscioTech <argo@live.ca>
2024-09-04 12:45:24 +0800

    chore: 增加 Core 工程

---
+++ src/BootstrapBlazor.Core/Converter/JsonEnumDescriptionConverter.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BootstrapBlazor.Core.Converter;
+
+/// <summary>
+/// 枚举类型转换器
+/// </summary>
+/// <remarks>序列化时把枚举类型序列化成字符串</remarks>
+public class JsonEnumDescriptionConverter<T> : JsonConverter<T> where T : struct, Enum
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="reader"></param>
+    /// <param name="typeToConvert"></param>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        T ret = default;
+        if (reader.TokenType == JsonTokenType.String)
+        {
+            var enumStringValue = reader.GetString();
+            if (Enum.TryParse<T>(enumStringValue, true, out T v))
+            {
+                ret = v;
+            }
+        }
+        return ret;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="writer"></param>
+    /// <param name="value"></param>
+    /// <param name="options"></param>
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToDescriptionString());
+    }
+}
---
+++ src/BootstrapBlazor.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using System.ComponentModel;
+using System.Reflection;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 枚举类型扩展方法
+/// </summary>
+public static class EnumExtensions
+{
+    /// <summary>
+    /// 获取 DescriptionAttribute 标签方法
+    /// </summary>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public static string ToDescriptionString<TEnum>(this TEnum val) where TEnum : Enum => typeof(TEnum).ToDescriptionString(val.ToString());
+
+    /// <summary>
+    /// 通过字段名称获取 DescriptionAttribute 标签值
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="fieldName"></param>
+    /// <returns></returns>
+    public static string ToDescriptionString(this Type? type, string? fieldName)
+    {
+        var ret = string.Empty;
+        if (type != null && !string.IsNullOrEmpty(fieldName))
+        {
+            var t = Nullable.GetUnderlyingType(type) ?? type;
+            var attributes = t.GetField(fieldName)?.GetCustomAttribute<DescriptionAttribute>(true);
+            ret = attributes?.Description ?? fieldName;
+        }
+        return ret;
+    }
+}
