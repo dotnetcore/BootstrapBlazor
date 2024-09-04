113f67ce82940b003297402947891426bcc91bbf
Argo-AscioTech <argo@live.ca>
2024-09-04 15:49:41 +0800

    refactor: 移除转化器使用 BB 内置的

--- src/Extensions/Components/BootstrapBlazor.DockView/Components/DockViewRenderMode.cs
+++ src/Extensions/Components/BootstrapBlazor.DockView/Components/DockViewRenderMode.cs
@@ -2,26 +2,23 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
-using System.ComponentModel;
-using System.Text.Json.Serialization;
+using BootstrapBlazor.Core.Converter;
 
 namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// DockViewRenderMode 渲染模式枚举类型
 /// </summary>
-[JsonConverter(typeof(DockViewTypeConverter<DockViewRenderMode>))]
+[JsonEnumConverter(true)]
 public enum DockViewRenderMode
 {
     /// <summary>
     /// 可见时渲染
     /// </summary>
-    [Description("onlyWhenVisible")]
     OnlyWhenVisible,
 
     /// <summary>
     /// 始终渲染
     /// </summary>
-    [Description("always")]
     Always
 }
