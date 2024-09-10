6687ab8b6d0de5b4021879ae0066dc45f68aa5fb
Argo-AscioTech <argo@live.ca>
2024-09-10 08:39:55 +0800

    revert: 撤销命名空间更改

--- src/BootstrapBlazor/Converter/JsonEnumConverter.cs
+++ src/BootstrapBlazor/Converter/JsonEnumConverter.cs
@@ -5,7 +5,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace BootstrapBlazor.Components;
+namespace BootstrapBlazor.Core.Converter;
 
 /// <summary>
 /// JsonEnumConverter 枚举转换器
