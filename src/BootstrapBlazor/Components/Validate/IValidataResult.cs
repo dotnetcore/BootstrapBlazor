b6c1736f857f86591272e6fb66517a7573bbf4a1
Argo-AscioTech <argo@live.ca>
2024-09-11 12:26:39 +0800

    feat: 增加 IValidataResult 接口

---
+++ src/BootstrapBlazor/Components/Validate/IValidataResult.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ValidataResult 验证结果接口配合 <see cref="IValidatableObject"/> 接口使用
+/// </summary>
+public interface IValidataResult
+{
+    /// <summary>
+    /// 返回要清除错误结果成员集合
+    /// </summary>
+    /// <returns></returns>
+    List<string> ClearMembers();
+}
