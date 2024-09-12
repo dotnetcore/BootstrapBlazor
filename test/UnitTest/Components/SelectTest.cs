a99d797ecb6eef6263f8b52743a8c17c5acd8441
Argo-AscioTech <argo@live.ca>
2024-09-12 15:58:15 +0800

    test: 更新单元测试

--- test/UnitTest/Components/SelectTest.cs
+++ test/UnitTest/Components/SelectTest.cs
@@ -3,6 +3,7 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 using System.Reflection;
 
@@ -620,8 +621,56 @@ public class SelectTest : BootstrapBlazorTestBase
     }
 
     [Fact]
-    public void IsVirtualize_OnQueryAsync()
+    public async Task IsVirtualize_Items_Clearable_Ok()
     {
+        var cut = Context.RenderComponent<Select<string>>(pb =>
+        {
+            pb.Add(a => a.Items, new SelectedItem[]
+            {
+                new("1", "Test1"),
+                new("2", "Test2")
+            });
+            pb.Add(a => a.Value, "2");
+            pb.Add(a => a.IsVirtualize, true);
+            pb.Add(a => a.RowHeight, 33f);
+            pb.Add(a => a.OverscanCount, 4);
+            pb.Add(a => a.IsClearable, true);
+            pb.Add(a => a.ShowSearch, true);
+        });
+
+        // 覆盖有搜索条件时，点击清空按钮
+        // 期望 UI 显示值为默认值
+        // 期望 下拉框为全数据
+        var input = cut.Find(".search-text");
+        await cut.InvokeAsync(() => input.Input("2"));
+
+        // 下拉框仅显示一个选项 Test2
+        var items = cut.FindAll(".dropdown-item");
+        Assert.Single(items);
+
+        // UI 值为 Test2
+        await cut.InvokeAsync(() => items[0].Click());
+        var el = cut.Find(".form-select") as IHtmlInputElement;
+        Assert.NotNull(el);
+        Assert.Equal("Test2", el.Value);
+        Assert.Equal("2", cut.Instance.Value);
+
+        // 点击 Clear 按钮
+        var button = cut.Find(".clear-icon");
+        await cut.InvokeAsync(() => button.Click());
+
+        // UI 恢复 Test1
+        Assert.Equal("Test1", el.Value);
+
+        // 下拉框显示所有选项
+        items = cut.FindAll(".dropdown-item");
+        Assert.Equal(2, items.Count);
+    }
+
+    [Fact]
+    public async Task IsVirtualize_OnQueryAsync_Clearable_Ok()
+    {
+        var query = false;
         var startIndex = 0;
         var requestCount = 0;
         var searchText = string.Empty;
@@ -629,32 +678,55 @@ public class SelectTest : BootstrapBlazorTestBase
         {
             pb.Add(a => a.OnQueryAsync, option =>
             {
+                query = true;
                 startIndex = option.StartIndex;
                 requestCount = option.Count;
                 searchText = option.SearchText;
                 return Task.FromResult(new QueryData<SelectedItem>()
                 {
-                    Items = new SelectedItem[]
-                    {
-                        new("1", "Test1"),
-                        new("2", "Test2")
-                    },
-                    TotalCount = 2
+                    Items = string.IsNullOrEmpty(searchText)
+                        ? new SelectedItem[]
+                        {
+                            new("", "All"),
+                            new("1", "Test1"),
+                            new("2", "Test2")
+                        } : [new("2", "Test2")],
+                    TotalCount = string.IsNullOrEmpty(searchText) ? 2 : 1
                 });
             });
-            pb.Add(a => a.Value, "2");
+            pb.Add(a => a.Value, "");
             pb.Add(a => a.IsVirtualize, true);
+            pb.Add(a => a.IsClearable, true);
+            pb.Add(a => a.ShowSearch, true);
         });
 
-        cut.SetParametersAndRender(pb => pb.Add(a => a.ShowSearch, true));
-        cut.InvokeAsync(async () =>
-        {
-            // 搜索 T
-            cut.Find(".search-text").Input("T");
-            await cut.Instance.ConfirmSelectedItem(0);
+        // 覆盖有搜索条件时，点击清空按钮
+        // 期望 UI 显示值为默认值
+        // 期望 下拉框为全数据
+        var input = cut.Find(".search-text");
+        await cut.InvokeAsync(() => input.Input("2"));
 
-            Assert.Equal(string.Empty, searchText);
-        });
+        // 下拉框仅显示一个选项 Test2
+        var items = cut.FindAll(".dropdown-item");
+        Assert.Single(items);
+
+        // UI 值为 Test2
+        await cut.InvokeAsync(() => items[0].Click());
+        var el = cut.Find(".form-select") as IHtmlInputElement;
+        Assert.NotNull(el);
+        Assert.Equal("Test2", el.Value);
+        Assert.Equal("2", cut.Instance.Value);
+
+        query = false;
+        // 点击 Clear 按钮
+        var button = cut.Find(".clear-icon");
+        await cut.InvokeAsync(() => button.Click());
+
+        // UI 恢复 Test1
+        Assert.Equal("All", el.Value);
+
+        // 下拉框显示所有选项
+        Assert.True(query);
     }
 
     [Fact]
