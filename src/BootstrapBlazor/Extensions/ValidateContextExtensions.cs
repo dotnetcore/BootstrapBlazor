d8b0458294803d70c799a09ca8455918536f05b5
AiZhen <53560110+AiYuZhen@users.noreply.github.com>
2024-09-20 13:54:29 +0800

    feat(MetadataTypeAttribute): support IValidatableObject/IValidateCollection (#4323)
    * 增增 MetadataTypeAttribute 方式，以支持 IValidatableObject、IValidateCollection 接口验证方式。
    增加简单示例。
    
    * 增强 MetadataTypeAttribute 方式，以支持 IValidatableObject、IValidateCollection 接口验证方式。
    增加简单示例。
    修复文档测试错误。
    
    * 修复代码覆盖率。
    
    * 添加测试代码。
    
    * 完善代码覆盖率。
    
    * 调整示例。
    
    * refactor: 代码格式化与规范化
    
    * refactor: 移除不需要的命名空间
    
    * doc: 更新示例代码
    
    * refactor: 增加扩展方法提高代码复用率
    
    * doc: 更新示例
    
    * doc: 更新 MetadataType 示例
    
    * refactor: 更新验证逻辑
    
    * test: 更新单元测试
    
    * chore: bump version 8.9.3-beta01
    
    ---------
    
    Co-authored-by: Argo Zhang <argo@live.ca>

---
+++ src/BootstrapBlazor/Extensions/ValidateContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ValidationContext 扩展方法
+/// </summary>
+public static class ValidationContextExtensions
+{
+    /// <summary>
+    /// 从 <see cref="MetadataTypeAttribute"/> 中获取指定类型实例
+    /// </summary>
+    /// <typeparam name="T">验证接口类型</typeparam>
+    /// <param name="context"></param>
+    /// <returns>没有实现 <typeparamref name="T"/> 接口，则返回 <see langword="null"/></returns>
+    public static T? GetInstanceFromMetadataType<T>(this ValidationContext context) where T : class
+    {
+        T? ret = default;
+        var attribute = context.ObjectInstance.GetType().GetCustomAttribute<MetadataTypeAttribute>();
+        if (attribute != null && attribute.MetadataClassType.GetInterfaces().Any(x => x.Equals(typeof(T))))
+        {
+            //此处是否需要缓存？
+            ret = ActivatorUtilities.CreateInstance(context, attribute.MetadataClassType) as T;
+        }
+        return ret;
+    }
+}
