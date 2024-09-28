d720ebb4644c0c0dae4eca9e3198778d5042e98b
Argo-AscioTech <argo@live.ca>
2024-09-28 10:40:21 +0800

    doc: 移除不使用的命名空间

--- src/BootstrapBlazor/Components/DateTimePicker/DateTimePicker.razor.cs
+++ src/BootstrapBlazor/Components/DateTimePicker/DateTimePicker.razor.cs
@@ -3,7 +3,6 @@
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using Microsoft.Extensions.Localization;
-using System.ComponentModel.Design;
 using System.Globalization;
 
 namespace BootstrapBlazor.Components;
