75de658f15a6afd49a5a7fcce63b497be26d65f0
Argo-AscioTech <argo@live.ca>
2024-10-07 14:20:22 +0800

    refactor: 代码重构提高可读性

--- src/BootstrapBlazor/Components/ThemeProvider/ThemeProvider.razor.js
+++ src/BootstrapBlazor/Components/ThemeProvider/ThemeProvider.razor.js
@@ -1,55 +1,49 @@
-﻿import { setTheme, getPreferredTheme, setActiveTheme, switchTheme } from "../../modules/utility.js"
+﻿import { getPreferredTheme, setTheme, switchTheme } from "../../modules/utility.js"
 import EventHandler from "../../modules/event-handler.js"
 import Data from "../../modules/data.js"
 
-const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
 export function init(id, invoke, themeValue, callback) {
     const el = document.getElementById(id);
-    if (el) {
-        Data.set('theme', el);
-        darkModeMediaQuery.addEventListener('change', handleDarkModeChange);
-        let currentTheme = themeValue;
-        if (currentTheme === 'useLocalStorage'){
-            currentTheme = getPreferredTheme();
-        }
-        Data.set('currentTheme', currentTheme);
-        setTheme(currentTheme, false);
-        const activeItem = el.querySelector(`.dropdown-item[data-bb-theme-value="${currentTheme}"]`);
-        if (activeItem) {
-            setActiveTheme(el, activeItem);
-        }
-
-        const items = el.querySelectorAll('.dropdown-item');
-        items.forEach(item => {
-            EventHandler.on(item, 'click', () => {
-                setActiveTheme(el, item);
-
-                let theme = item.getAttribute('data-bb-theme-value');
-                Data.set('currentTheme', theme);
-                switchTheme(theme, window.innerWidth, 0);
-                if (callback) {
-                    invoke.invokeMethodAsync(callback, theme);
-                }
-            });
-        });
+    if (el === null) {
+        return;
     }
+
+    const theme = { el };
+    Data.set(id, theme);
+
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    theme.mediaQueryList = darkModeMediaQuery;
+    EventHandler.on(darkModeMediaQuery, 'change', () => {
+        if (Data.get('currentTheme') === 'auto') {
+            switchTheme('auto');
+        }
+    });
+
+    let currentTheme = themeValue;
+    if (currentTheme === 'useLocalStorage') {
+        currentTheme = getPreferredTheme();
+    }
+    theme.currentTheme = currentTheme;
+    setTheme(currentTheme, true);
+
+    EventHandler.on(el, 'click', '.dropdown-item', e => {
+        const activeTheme = e.delegateTarget.getAttribute('data-bb-theme-value');
+        theme.currentTheme = activeTheme;
+        switchTheme(activeTheme, window.innerWidth, 0);
+        if (callback) {
+            invoke.invokeMethodAsync(callback, activeTheme);
+        }
+    });
 }
 
 export function dispose(id) {
-    const el = document.getElementById(id);
-    const items = el.querySelectorAll('.dropdown-item');
-    items.forEach(item => {
-        EventHandler.off(item, 'click');
-    });
-    darkModeMediaQuery.removeEventListener('change', handleDarkModeChange);
-    Data.remove('theme');
-    Data.remove('currentTheme');
-}
-
-function handleDarkModeChange(e) {
-    console.log(Data.get('currentTheme'))
-    if (Data.get('currentTheme') === 'auto') {
-        switchTheme('auto');
+    const theme = Data.get(id);
+    if (theme === null) {
+        return;
     }
+    Data.remove(id);
+
+    const { el, darkModeMediaQuery } = theme;
+    EventHandler.off(el, 'click');
+    EventHandler.off(darkModeMediaQuery, 'change');
 }
