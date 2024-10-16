97fc831146aaeb4c526dcf8814b72cacab439516
Argo-AsicoTech <argo@live.ca>
2024-10-16 12:18:52 +0800

    chore: 更新格式

--- src/BootstrapBlazor/Components/Tab/Tab.razor.cs
+++ src/BootstrapBlazor/Components/Tab/Tab.razor.cs
@@ -1,9 +1,8 @@
-﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://www.blazor.zone or https://argozhang.github.io/
 
 using Microsoft.Extensions.Localization;
-
 using System.Collections.Concurrent;
 using System.Reflection;
 
