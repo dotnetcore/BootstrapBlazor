61c8386e47f3402af64c4feb322fe3f553cf9dd7
Argo-AsicoTech <argo@live.ca>
2024-10-19 18:27:03 +0800

    feat: 增加蓝牙接口

---
+++ src/BootstrapBlazor/Services/Bluetooth/IBluetoothService.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 蓝牙服务接口
+/// </summary>
+public interface IBluetoothService
+{
+}
