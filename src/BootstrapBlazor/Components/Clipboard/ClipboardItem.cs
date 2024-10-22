088c92d38a08fff2952f40dc58c2b3659add3d4f
Argo-AsicoTech <argo@live.ca>
2024-10-22 10:07:24 +0800

    doc: 更新源代码标注

--- src/BootstrapBlazor/Components/Clipboard/ClipboardItem.cs
+++ src/BootstrapBlazor/Components/Clipboard/ClipboardItem.cs
@@ -1,4 +1,9 @@
-﻿namespace BootstrapBlazor.Components;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Author: Argo Zhang (argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
 
 /// <summary>
 /// Represents an item on the clipboard with associated data and metadata.
