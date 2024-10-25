b9a5bbdb977b2628ec80fbe6341e7720919dc50e
Argo Zhang <argo@live.ca>
2024-10-25 11:45:13 +0800

    feat(FullScreen): remove FullScreen component (#4537)
    * refactor: 移除 FullScreen 组件
    
    * feat: 更改 FullScreen 为服务
    
    * test: 更新单元测试
    
    * refactor: 精简脚本
    
    * refactor: 精简代码逻辑

--- src/BootstrapBlazor/wwwroot/modules/fullscreen.js
+++ src/BootstrapBlazor/wwwroot/modules/fullscreen.js
@@ -1,84 +1,39 @@
 ﻿import { isElement } from "./utility.js"
-import Data from "./data.js"
 
-export function init(id) {
-    const fs = { toggleElement: null };
-    Data.set(id, fs)
+export async function toggle(options) {
+    let el = null;
+    if (options?.id) {
+        el = document.getElementById(options.id);
+    }
+    else if (options?.element && isElement(options.element)) {
+        el = options.element;
+    }
+    else {
+        el = document.documentElement
+    }
 
-    fs.toggle = options => {
-        if (options.id) {
-            fs.toggleElement = document.getElementById(options.id)
-        }
-        else if (options.element && isElement(options.element)) {
-            fs.toggleElement = el
-        }
-        else {
-            fs.toggleElement = document.documentElement
-        }
-
+    if (el !== null) {
         if (isFullscreen()) {
-            exit()
+            await document.exitFullscreen()
         }
         else {
-            fs.enter()
+            await enterFullscreen(el);
         }
     }
-
-    fs.enter = () => {
-        fs.toggleElement.requestFullscreen() ||
-            fs.toggleElement.webkitRequestFullscreen ||
-            fs.toggleElement.mozRequestFullScreen ||
-            fs.toggleElement.msRequestFullscreen
-
-        // 处理 ESC 按键退出全屏
-        var handler = setTimeout(() => {
-            clearTimeout(handler);
-
-            const fullscreenCheck = () => {
-                if (!isFullscreen()) {
-                    fs.toggleElement.classList.remove('bb-fs-open');
-                    document.documentElement.classList.remove('bb-fs-open');
-                }
-                else {
-                    fs.toggleElement.classList.add('bb-fs-open')
-                    requestAnimationFrame(fullscreenCheck);
-                }
-            }
-            requestAnimationFrame(fullscreenCheck);
-        }, 200);
-    }
 }
 
-export function execute(id, options) {
-    const fs = Data.get(id)
-    if (fs) {
-        fs.toggle(options);
-    }
-}
+const enterFullscreen = async el => {
+    await el.requestFullscreen();
 
-export function dispose(id) {
-    Data.remove(id)
+    if (!isFullscreen()) {
+        el.classList.remove('bb-fs-open');
+        document.documentElement.classList.remove('bb-fs-open');
+    }
+    else {
+        el.classList.add('bb-fs-open')
+    }
 }
 
 const isFullscreen = () => {
-    return document.fullscreen ||
-        document.webkitIsFullScreen ||
-        document.webkitFullScreen ||
-        document.mozFullScreen ||
-        document.msFullScreent
-}
-
-const exit = () => {
-    if (document.exitFullscreen) {
-        document.exitFullscreen()
-    }
-    else if (document.mozCancelFullScreen) {
-        document.mozCancelFullScreen()
-    }
-    else if (document.webkitExitFullscreen) {
-        document.webkitExitFullscreen()
-    }
-    else if (document.msExitFullscreen) {
-        document.msExitFullscreen()
-    }
+    return document.fullscreenElement !== null;
 }
