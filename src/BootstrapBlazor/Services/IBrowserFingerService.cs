eca5a3328844347c870f853bbb32fae66936464f
Argo-AsicoTech <argo@live.ca>
2024-10-25 11:48:33 +0800

    refactor: 重构服务

--- src/BootstrapBlazor/Services/IBrowserFingerService.cs
+++ src/BootstrapBlazor/Services/IBrowserFingerService.cs
@@ -11,21 +11,8 @@ namespace BootstrapBlazor.Components;
 public interface IBrowserFingerService
 {
     /// <summary>
-    /// 订阅指纹方法回调
-    /// </summary>
-    /// <param name="target"></param>
-    /// <param name="callback"></param>
-    void Subscribe(object target, Func<Task<string?>> callback);
-
-    /// <summary>
-    /// 取消指纹方法回调
-    /// </summary>
-    /// <param name="target"></param>
-    void Unsubscribe(object target);
-
-    /// <summary>
     /// 获得当前浏览器指纹方法
     /// </summary>
     /// <returns></returns>
-    Task<string?> GetFingerCodeAsync();
+    Task<string?> GetFingerCodeAsync(CancellationToken token = default);
 }
