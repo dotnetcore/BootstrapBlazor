7f747d161c34e5844f46c15e4fc4cfa22a9c5628
Argo-AsicoTech <argo@live.ca>
2024-11-02 08:55:05 +0800

    test: 增加单元测试覆盖率

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -8104,7 +8104,7 @@ public class TableTest : BootstrapBlazorTestBase
                 });
                 pb.Add(a => a.RowContentTemplate, context => builder =>
                 {
-                    builder.OpenElement(0, "div");
+                    builder.OpenElement(0, context.RenderMode == TableRenderMode.CardView ? "div" : "tr");
                     builder.AddContent(1, $"template-{context.Row.Name}");
                     builder.CloseElement();
 
@@ -8141,7 +8141,7 @@ public class TableTest : BootstrapBlazorTestBase
                 });
                 pb.Add(a => a.RowTemplate, context => builder =>
                 {
-                    builder.OpenElement(0, mode == TableRenderMode.CardView ? "div" : "tr");
+                    builder.OpenElement(0, context.RenderMode == TableRenderMode.CardView ? "div" : "tr");
                     builder.AddContent(1, new MarkupString(mode == TableRenderMode.CardView ? $"<div>template-{context.Row.Name}</div>" : $"<td>template-{context.Row.Name}</td>"));
                     builder.CloseElement();
 
