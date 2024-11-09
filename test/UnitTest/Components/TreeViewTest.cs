7d176b8778da4b370552bbf4929464a2dde09332
Argo-AsicoTech <argo@live.ca>
2024-11-09 09:19:33 +0800

    test: 更新单元测试

--- test/UnitTest/Components/TreeViewTest.cs
+++ test/UnitTest/Components/TreeViewTest.cs
@@ -369,26 +369,35 @@ public class TreeViewTest : BootstrapBlazorTestBase
             });
         });
 
-        var checkboxes = cut.FindComponents<Checkbox<CheckboxState>>();
+        var checkboxes = cut.FindComponents<Checkbox<TreeViewItem<TreeFoo>>>();
 
-        // 初始状态
+        // 初始状态 第一节点未选中 第二节点选中
         Assert.Equal(CheckboxState.UnChecked, checkboxes[0].Instance.State);
         Assert.Equal(CheckboxState.Checked, checkboxes[1].Instance.State);
 
+        // 展开第一个节点
         await cut.InvokeAsync(() => cut.Find(".fa-caret-right.visible").Click());
         Assert.True(expanded);
 
         cut.WaitForState(() => cut.Instance.Items[0].Items.Count > 0);
+        // 101 unchecked
+        // -> 101-101 unchecked
+        // -> 101-102 checked
+        // 102 checked
 
         // 展开状态
-        checkboxes = cut.FindComponents<Checkbox<CheckboxState>>();
+        checkboxes = cut.FindComponents<Checkbox<TreeViewItem<TreeFoo>>>();
         Assert.Equal(CheckboxState.Indeterminate, checkboxes[0].Instance.State);
         Assert.Equal(CheckboxState.UnChecked, checkboxes[1].Instance.State);
         Assert.Equal(CheckboxState.Checked, checkboxes[2].Instance.State);
 
         // 级联选中-父级
         await cut.InvokeAsync(() => checkboxes[1].Instance.SetState(CheckboxState.Checked));
-        Assert.Equal(CheckboxState.Checked, checkboxes[0].Instance.State);
+
+        // 由于缺少 JS 回调单元测试中 Instance.State 无法获取到最新状态
+        Assert.Equal(CheckboxState.Checked, checkboxes[0].Instance.Value.CheckedState);
+
+
     }
 
     [Fact]
