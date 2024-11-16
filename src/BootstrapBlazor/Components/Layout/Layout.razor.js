d46f1960201ce361e99308237200db9e29fd5477
Argo-AsicoTech <argo@live.ca>
2024-11-16 21:33:29 +0800

    refactor: 恢复脚本

--- src/BootstrapBlazor/Components/Layout/Layout.razor.js
+++ src/BootstrapBlazor/Components/Layout/Layout.razor.js
@@ -10,37 +10,13 @@ export function init(id, invoke, callback) {
     }
     Data.set(id, layout)
 
-    const tooltip = document.querySelector('.layout-header [data-bs-toggle="tooltip"]')
+    const tooltip = document.querySelector('.layout-header-bar')
     if (tooltip) {
         layout.tooltip = bootstrap.Tooltip.getOrCreateInstance(tooltip)
     }
 
     layout.handler();
     EventHandler.on(window, 'resize', layout.handler);
-}
-
-export function layout_init() {
-    console.log('begin layout init...');
-
-    var handler = setInterval(() => {
-        console.log('fetch header...');
-
-        const layout = document.querySelector('.layout');
-        if (layout) {
-            console.log('fetch header... done');
-            clearInterval(handler);
-
-            const bar = layout.querySelector('.layout-header-bar');
-            if (bar) {
-                console.log('find bar... done');
-
-                bootstrap.Tooltip.getOrCreateInstance(bar);
-                EventHandler.on(bar, 'click', e => {
-                    layout.classList.toggle('is-collapsed');
-                });
-            }
-        }
-    }, 50);
 }
 
 export function dispose(id) {
