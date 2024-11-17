7a3163751b3ee3674fe49755473dbdfd891fdcf3
Argo-AsicoTech <argo@live.ca>
2024-11-17 11:00:19 +0800

    test: 更新单元测试

--- test/UnitTest/Components/LayoutTest.cs
+++ test/UnitTest/Components/LayoutTest.cs
@@ -68,10 +68,8 @@ public class LayoutTest : BootstrapBlazorTestBase
         var cut = Context.RenderComponent<Layout>(pb =>
         {
             pb.Add(a => a.IsPage, true);
-            pb.Add(a => a.UseTabSet, true);
-            pb.Add(a => a.IsFixedTabHeader, true);
         });
-        Assert.Contains("--bb-layout-height: 100vh;", cut.Markup);
+        Assert.Contains("is-page", cut.Markup);
     }
 
     [Fact]
