c74af2264b93b1ac0c298abb93ed444b8a9ec04c
Argo-AsicoTech <argo@live.ca>
2024-11-22 19:32:43 +0800

    refactor: 更改为服务器端逻辑

--- src/BootstrapBlazor/Components/Checkbox/Checkbox.razor.cs
+++ src/BootstrapBlazor/Components/Checkbox/Checkbox.razor.cs
@@ -160,46 +160,40 @@ public partial class Checkbox<TValue> : ValidateBase<TValue>
     protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, nameof(OnTriggerClickAsync));
 
     /// <summary>
+    /// 点击组件触发方法 内部调用 <see cref="OnBeforeStateChanged"/> 回调方法
+    /// </summary>
+    /// <returns></returns>
+    public async Task OnToggleClick()
+    {
+        var valid = true;
+        var state = State == CheckboxState.Checked ? CheckboxState.UnChecked : CheckboxState.Checked;
+        if (OnBeforeStateChanged != null)
+        {
+            valid = await OnBeforeStateChanged(state);
+        }
+
+        if (valid)
+        {
+            await InternalStateChanged(state);
+            StateHasChanged();
+        }
+    }
+
+    /// <summary>
     /// 触发 Click 方法
     /// </summary>
     /// <returns></returns>
-    public async Task TriggerClick() => await OnTriggerClickAsync();
+    public Task TriggerClick() => Task.CompletedTask;
 
     /// <summary>
     /// 触发 Click 方法 由 JavaScript 调用
     /// </summary>
     /// <returns></returns>
     [JSInvokable]
-    public async ValueTask<bool> OnTriggerClickAsync(CheckboxState? state = null)
+    public ValueTask OnTriggerClickAsync(CheckboxState state)
     {
-        // 本组件由于支持 OnBeforeStateChanged 回调方法，所以设计上移除了 onclick 事件，改为通过 JS 调用 TriggerClick 方法
-        // state 有值时表示同步状态功能
-        if (state.HasValue)
-        {
-            State = state.Value;
-            return true;
-        }
-
-        // 调用 OnBeforeStateChanged 回调方法查看是否阻止状态改变
-        // 返回 true 时改变状态，返回 false 时不改变状态阻止状态改变 preventDefault
-        var val = State == CheckboxState.Checked ? CheckboxState.UnChecked : CheckboxState.Checked;
-        if (OnBeforeStateChanged != null)
-        {
-            var ret = await OnBeforeStateChanged(val);
-            if (ret == false)
-            {
-                // 阻止状态改变
-                return false;
-            }
-        }
-
-        // 改变状态 由点击事件触发
-        var render = await InternalStateChanged(val);
-        if (render)
-        {
-            StateHasChanged();
-        }
-        return true;
+        State = state;
+        return ValueTask.CompletedTask;
     }
 
     /// <summary>
@@ -250,20 +244,5 @@ public partial class Checkbox<TValue> : ValidateBase<TValue>
                 StateHasChanged();
             }
         }
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <param name="disposing"></param>
-    /// <returns></returns>
-    protected override async ValueTask DisposeAsync(bool disposing)
-    {
-        if (disposing && Module != null)
-        {
-            await Module.DisposeAsync();
-            Module = null;
-        }
-        await base.DisposeAsync(disposing);
     }
 }
