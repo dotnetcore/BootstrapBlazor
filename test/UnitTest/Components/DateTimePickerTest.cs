dead34efaf2a31c6682ea8720d65bb986a0f925e
Argo-AsicoTech <argo@live.ca>
2024-11-27 19:04:57 +0800

    test: 更新单元测试

--- test/UnitTest/Components/DateTimePickerTest.cs
+++ test/UnitTest/Components/DateTimePickerTest.cs
@@ -928,8 +928,9 @@ public class DateTimePickerTest : BootstrapBlazorTestBase
         {
             pb.Add(a => a.AutoClose, true);
         });
+        button = cut.FindAll(".picker-panel-content .cell").First(i => i.TextContent == $"{DateTime.Today.Day}");
         await cut.InvokeAsync(() => button.Click());
-        Assert.Equal(val, DateTime.MinValue);
+        Assert.NotEqual(val, DateTime.MinValue);
     }
 
     [Fact]
