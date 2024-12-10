a1be14fce54ca705f53745c60b8e3b8fa2bc805f
Argo-AsicoTech <argo@live.ca>
2024-12-10 10:02:27 +0800

    test: 补充单元测试

---
+++ test/UnitTest/Components/DialogCloseButtonTest.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace UnitTest.Components;
+
+public class DialogCloseButtonTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Icon_Ok()
+    {
+        var cut = Context.RenderComponent<DialogCloseButton>();
+        Assert.Equal("fa-solid fa-xmark", cut.Instance.Icon);
+
+        cut.SetParametersAndRender(pb => pb.Add(p => p.Icon, "test-icon"));
+        Assert.Equal("test-icon", cut.Instance.Icon);
+    }
+}
