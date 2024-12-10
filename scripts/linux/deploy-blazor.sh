1aa380d0040eba8307562bd3050a5b7b0cfe6bc5
Argo Zhang <argo@live.ca>
2024-12-10 10:33:29 +0800

    chore(Config): update project toolchain (#4811)
    * chore: 移除 wasn 脚本
    
    * chore: 更新 bug 模板
    
    * chore: 更新部署脚本
    
    * chore: 更新 nginx 配置文件
    
    * chore: 移除 wasm 脚本

--- scripts/linux/deploy-blazor.sh
+++ scripts/linux/deploy-blazor.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
 cd ~/BootstrapBlazor
-curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Publish
-
 git pull
+
+curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Publish
 dotnet build src/BootstrapBlazor.Server
-dotnet publish src/BootstrapBlazor.Server -c Release
+
 curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Reboot
+dotnet publish src/BootstrapBlazor.Server -c Release
 
 systemctl stop ba.blazor
-\cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net8.0/publish/* /usr/local/ba/blazor
+\cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net9.0/publish/* /usr/local/ba/blazor
 systemctl start ba.blazor
 systemctl status ba.blazor -l --no-pager
