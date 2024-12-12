1351560a9d0e45c205afeeaff91ab1905ed0cf13
卢骥 <1429614920@qq.com>
2024-12-12 12:51:13 +0800

    Update SelectTree.razor.cs

--- src/BootstrapBlazor/Components/Select/SelectTree.razor.cs
+++ src/BootstrapBlazor/Components/Select/SelectTree.razor.cs
@@ -179,6 +179,10 @@ public partial class SelectTree<TValue> : IModelEqualityComparer<TValue>
             // 组件未赋值 Value 通过 IsActive 设置默认值
             await TriggerItemChanged(s => s.IsActive);
         }
+        {
+            // 组件已赋值 Value 通过 Value 设置默认值
+            await TriggerItemChanged(s => Equals(s.Value, Value));
+        }
     }
 
     /// <summary>
