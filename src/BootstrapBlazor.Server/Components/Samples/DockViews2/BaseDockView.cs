f11bd8e3a51c612657f6ce1ca8a07c346bb3f304
Argo Zhang <argo@live.ca>
2024-12-19 17:23:14 +0800

    feat(TableColumn): remove Formatter on TableColumn (#4892)
    * refactor: 移除列格式化操作日志
    
    * doc: 更新示例
    
    * test: 更新单元测试
    
    * refactor: 精简逻辑
    
    * test: 更新单元测试
    
    * refactor: 删除冗余代码
    
    * test: 更新单元测试
    
    * revert: 撤销更新文档
    
    * doc: 更新示例
    
    * doc: 精简代码
    
    * refactor: 精简代码
    
    * refactor: 移除可为空标记
    
    * refactor: 代码重构
    
    * refactor: 支持异步 Formatter 方法
    
    * refactor: 撤销代码更改
    
    * doc: 更新示例
    
    * refactor: 撤销代码更改
    
    * refactor: 精简代码
    
    * test: 更新单元测试

--- src/BootstrapBlazor.Server/Components/Samples/DockViews2/BaseDockView.cs
+++ src/BootstrapBlazor.Server/Components/Samples/DockViews2/BaseDockView.cs
@@ -141,10 +141,10 @@ public abstract class BaseDockView : ComponentBase
             ParentId = parentId,
             Name = localizer["Foo.Name", $"{id + i:d4}"],
             DateTime = System.DateTime.Now.AddDays(i - 1),
-            Address = localizer["Foo.Address", $"{Random.Next(1000, 2000)}"],
-            Count = Random.Next(1, 100),
-            Complete = Random.Next(1, 100) > 50,
-            Education = Random.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middle
+            Address = localizer["Foo.Address", $"{Random.Shared.Next(1000, 2000)}"],
+            Count = Random.Shared.Next(1, 100),
+            Complete = Random.Shared.Next(1, 100) > 50,
+            Education = Random.Shared.Next(1, 100) > 50 ? EnumEducation.Primary : EnumEducation.Middle
         }).ToList();
     }
 }
