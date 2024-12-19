bb8699dfb96507d6426aa3333697c84ed2a57ec7
Argo-AsicoTech <argo@live.ca>
2024-12-19 10:16:53 +0800

    doc: 更新 Lookup 示例

--- src/BootstrapBlazor.Server/Services/DemoLookupService.cs
+++ src/BootstrapBlazor.Server/Services/DemoLookupService.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
+using System.Collections.Concurrent;
+
 namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
@@ -18,6 +20,8 @@ internal class DemoLookupService(IServiceProvider provider) : LookupServiceBase
     /// <returns></returns>
     public override IEnumerable<SelectedItem>? GetItemsByKey(string? key, object? data) => null;
 
+    private static readonly ConcurrentDictionary<string, List<SelectedItem>> _cache = [];
+
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
@@ -26,17 +30,28 @@ internal class DemoLookupService(IServiceProvider provider) : LookupServiceBase
     /// <returns></returns>
     public override async Task<IEnumerable<SelectedItem>?> GetItemsByKeyAsync(string? key, object? data)
     {
-        await Task.Delay(1);
-
         IEnumerable<SelectedItem>? items = null;
         if (key == "Foo.Complete")
         {
-            var localizer = provider.GetRequiredService<IStringLocalizer<Foo>>();
-            items = new List<SelectedItem>()
+            // 使用缓存技术防止多次调用提高应用性能
+            if (_cache.TryGetValue(key, out var value))
             {
-                new() { Value = "True", Text = localizer["True"].Value },
-                new() { Value = "False", Text = localizer["False"].Value }
-            };
+                items = value;
+            }
+            else
+            {
+                // 模拟异步延时实战中大概率从数据库中获得数据
+                await Task.Delay(1);
+
+                var localizer = provider.GetRequiredService<IStringLocalizer<Foo>>();
+                var v = new List<SelectedItem>()
+                {
+                    new() { Value = "True", Text = localizer["True"].Value },
+                    new() { Value = "False", Text = localizer["False"].Value }
+                };
+                _cache.TryAdd(key, v);
+                items = v;
+            }
         }
         return items;
     }
