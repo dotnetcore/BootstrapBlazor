ece22077f04c7b8f3df8bc148b52ed97f9d94ab0
Argo Zhang <argo@live.ca>
2024-12-22 20:33:03 +0800

    chore(Deploy): add deploy script (#4921)
    * chore: 增加证书
    
    * chore: 增加部署脚本
    
    * chore: 更新 nginx 配置
    
    * chore: 更新解决方案
    
    * chore: 更新部署脚本
    
    Co-Authored-By: Mr Lu <16606743+tiansfather@users.noreply.github.com>
    
    * refactor: 更新脚本
    
    ---------
    
    Co-authored-by: Mr Lu <16606743+tiansfather@users.noreply.github.com>

---
+++ scripts/linux/deploy.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+echo "clean env"
+
+sudo rm -fr BootstrapBlazor
+
+sudo rm -fr /usr/local/ba/blazor
+
+echo "apt update"
+sudo apt update
+
+echo "install git"
+echo yes|sudo apt install git
+
+echo "install BootstrapBlazor"
+sudo git clone https://gitee.com/LongbowEnterprise/BootstrapBlazor.git
+
+echo "make directory BA/Blazor"
+sudo mkdir /usr/local/ba
+sudo mkdir /usr/local/ba/blazor
+
+echo "copy scripts"
+sudo cp BootstrapBlazor/scripts/linux/deploy-blazor.sh deploy-blazor.sh
+sudo cp BootstrapBlazor/scripts/linux/ba.blazor.service /usr/lib/systemd/system/ba.blazor.service
+
+echo "install ba.blazor.service"
+sudo systemctl enable ba.blazor
+
+echo "install nginx"
+echo yes|sudo apt install nginx
+
+echo "copy nginx config"
+sudo cp BootstrapBlazor/scripts/linux/nginx.conf /etc/nginx/
+
+echo "copy cert"
+sudo mkdir /etc/nginx/cert
+sudo cp BootstrapBlazor/scripts/linux/cert/* /etc/nginx/cert/
