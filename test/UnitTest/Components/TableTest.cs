62022a0266cadbf20a16e2b93a496d02cf476d7e
Argo Zhang <argo@live.ca>
2024-12-23 13:40:17 +0800

    test: 更新单元测试
    Co-Authored-By: ZhYan <19183404+Axxbis@users.noreply.github.com>

--- test/UnitTest/Components/TableTest.cs
+++ test/UnitTest/Components/TableTest.cs
@@ -7665,6 +7665,106 @@ public class TableTest : BootstrapBlazorTestBase
 
         var table = cut.FindComponent<MockTable>();
         await cut.InvokeAsync(() => table.Instance.QueryAsync());
+
+        col.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.ShowTips, true);
+            pb.Add(a => a.Lookup, null);
+            pb.Add(a => a.LookupService, new MockLookupServiceAsync());
+        });
+        await cut.InvokeAsync(() => table.Instance.QueryAsync());
+        cut.WaitForElement("[data-bs-original-title=\"LookupService-Test-True-async\"]");
+    }
+
+    [Fact]
+    public async Task GetValue_LookupServiceKey_Null()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<MockTable>(pb =>
+            {
+                pb.Add(a => a.OnQueryAsync, OnQueryAsync(localizer));
+                pb.Add(a => a.TableColumns, foo => builder =>
+                {
+                    builder.OpenComponent<TableColumn<Foo, bool>>(0);
+                    builder.AddAttribute(1, "Field", true);
+                    builder.AddAttribute(2, "FieldExpression", Utility.GenerateValueExpression(foo, "Complete", typeof(bool)));
+                    builder.AddAttribute(3, "LookupServiceKey", "null");
+                    builder.AddAttribute(4, "LookupServiceData", true);
+                    builder.AddAttribute(5, "ShowTips", true);
+                    builder.AddAttribute(6, "LookupService", new MockLookupServiceAsync());
+                    builder.CloseComponent();
+                });
+            });
+        });
+
+        var table = cut.FindComponent<MockTable>();
+        await cut.InvokeAsync(() => table.Instance.QueryAsync());
+        cut.WaitForElement("[data-bs-original-title=\"True\"]");
+    }
+
+    [Fact]
+    public async Task GetValue_LookupServiceKey_NullText()
+    {
+        var localizer = Context.Services.GetRequiredService<IStringLocalizer<Foo>>();
+        var cut = Context.RenderComponent<BootstrapBlazorRoot>(pb =>
+        {
+            pb.AddChildContent<MockTable>(pb =>
+            {
+                pb.Add(a => a.OnQueryAsync, OnQueryAsync(localizer));
+                pb.Add(a => a.TableColumns, foo => builder =>
+                {
+                    builder.OpenComponent<TableColumn<Foo, bool>>(0);
+                    builder.AddAttribute(1, "Field", true);
+                    builder.AddAttribute(2, "FieldExpression", Utility.GenerateValueExpression(foo, "Complete", typeof(bool)));
+                    builder.AddAttribute(3, "LookupServiceKey", "null-text");
+                    builder.AddAttribute(4, "LookupServiceData", true);
+                    builder.AddAttribute(5, "ShowTips", true);
+                    builder.AddAttribute(6, "LookupService", new MockLookupServiceAsync());
+                    builder.CloseComponent();
+                });
+            });
+        });
+
+        var table = cut.FindComponent<MockTable>();
+        await cut.InvokeAsync(() => table.Instance.QueryAsync());
+        cut.WaitForElement("[data-bs-original-title=\"True\"]");
+    }
+    class MockLookupServiceAsync : LookupServiceBase
+    {
+        public override IEnumerable<SelectedItem>? GetItemsByKey(string? key, object? data) => null;
+
+        public override async Task<IEnumerable<SelectedItem>?> GetItemsByKeyAsync(string? key, object? data)
+        {
+            await Task.Delay(300);
+
+            IEnumerable<SelectedItem>? ret = null;
+
+            if (key == "test")
+            {
+                ret = new SelectedItem[]
+                {
+                    new("True", "LookupService-Test-True-async"),
+                    new("False", "LookupService-Test-False-async")
+                };
+            }
+
+            if (key == "null")
+            {
+                ret = null;
+            }
+
+            if (key == "null-text")
+            {
+                ret = new SelectedItem[]
+                {
+                    new("Fake-True", "Fake-True"),
+                    new("Fake-False", "Fake-False")
+                };
+            }
+            return ret;
+        }
     }
 
     [Fact]
