101a378c08e2b4df4d08e01bd677ede57d0b0f07
Argo Zhang <argo@live.ca>
2024-12-29 11:10:37 +0800

    refactor: 重构代码

--- src/BootstrapBlazor/Components/Display/Display.razor.cs
+++ src/BootstrapBlazor/Components/Display/Display.razor.cs
@@ -137,11 +137,11 @@ public partial class Display<TValue> : ILookup
         return ret ?? string.Empty;
     }
 
-    private Func<TValue, string>? _arrayConvertoString;
+    private Func<TValue, string>? _arrayConvertToString;
     private string ArrayConvertToString(TValue value)
     {
-        _arrayConvertoString ??= LambdaExtensions.ArrayConvertToStringLambda<TValue>(TypeResolver).Compile();
-        return _arrayConvertoString(value);
+        _arrayConvertToString ??= LambdaExtensions.ArrayConvertToStringLambda<TValue>(TypeResolver).Compile();
+        return _arrayConvertToString(value);
     }
 
     private static Func<TValue, string>? _enumerableConvertToString;
