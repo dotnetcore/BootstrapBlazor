bd8f3062d94ca3db4c49ed5f75366426f4746faf
卢骥 <1429614920@qq.com>
2024-12-30 13:43:54 +0800

    Revert "Update IEditorItemExtensions.cs"
    This reverts commit f7f06070daab2e1559c054a3c3d8f50f4b643747.

--- src/BootstrapBlazor/Extensions/IEditorItemExtensions.cs
+++ src/BootstrapBlazor/Extensions/IEditorItemExtensions.cs
@@ -1,5 +1,6 @@
-﻿// Licensed to the .NET Foundation under one or more agreements. The .NET Foundation licenses this
-// file to you under the Apache 2.0 License See the LICENSE file in the project root for more information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
 using System.Reflection;
@@ -76,7 +77,7 @@ public static class IEditorItemExtensions
     /// <returns></returns>
     public static bool CanWrite(this IEditorItem item, Type modelType)
     {
-        return typeof(IDynamicObject).IsAssignableFrom(modelType) || ComplexCanWrite();
+        return modelType == typeof(DynamicObject) || modelType.IsSubclassOf(typeof(DynamicObject)) || ComplexCanWrite();
 
         bool ComplexCanWrite()
         {
