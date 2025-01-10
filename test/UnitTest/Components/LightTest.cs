ed880e58951f6a54e7161c8c3ab50ef0234cf2cf
Argo Zhang <argo@live.ca>
2025-01-10 12:08:09 +0800

    feat(Light): support flash function when set IsFlat to true (#5083)
    * feat: add is-flat-flash style
    
    * doc: 更新文档
    
    * test: 更新单元测试

--- test/UnitTest/Components/LightTest.cs
+++ test/UnitTest/Components/LightTest.cs
@@ -11,14 +11,16 @@ public class LightTest : BootstrapBlazorTestBase
     public void IsFlash_Ok()
     {
         var cut = Context.RenderComponent<Light>(builder => builder.Add(s => s.IsFlash, true));
-        Assert.Contains("flash", cut.Markup);
+        Assert.Contains("light flash", cut.Markup);
+        Assert.DoesNotContain("is-flash-flat", cut.Markup);
 
         cut.SetParametersAndRender(pb =>
         {
             pb.Add(a => a.IsFlat, true);
         });
-        Assert.DoesNotContain("flash", cut.Markup);
+        Assert.DoesNotContain("light flash", cut.Markup);
         Assert.Contains("is-flat", cut.Markup);
+        Assert.Contains("is-flat-flash", cut.Markup);
     }
 
     [Fact]
