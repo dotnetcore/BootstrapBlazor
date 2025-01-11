d53390462594fa82164c3d8112b7cb91c6ead5a2
Argo Zhang <argo@live.ca>
2025-01-11 13:27:36 +0800

    feat(Affix): add Affix component (#5093)
    * chore: bump version 9.2.7-beta05
    
    Co-Authored-By: Zeus <166673003+A5196060@users.noreply.github.com>
    Co-Authored-By: Alex chow <zhouchuanglin@gmail.com>
    
    * feat: 增加 Affix 组件
    
    * doc: 增加 Affix 文档
    
    * doc: 更新标志
    
    * doc: 更新菜单本地化
    
    * chore: bump version 9.2.7-beta06
    
    * test: 增加单元测试
    
    ---------
    
    Co-authored-by: Zeus <166673003+A5196060@users.noreply.github.com>
    Co-authored-by: Alex chow <zhouchuanglin@gmail.com>

---
+++ src/BootstrapBlazor/Components/Affix/Affix.razor.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Affix 固钉组件
+/// </summary>
+public partial class Affix
+{
+    /// <summary>
+    /// 获得/设置 指定偏移量后触发
+    /// </summary>
+    [Parameter]
+    public float Offset { get; set; }
+
+    /// <summary>
+    /// 获得/设置 固定位置枚举 默认 <see cref="AffixPosition.Top"/>
+    /// </summary>
+    [Parameter]
+    public AffixPosition Position { get; set; }
+
+    /// <summary>
+    /// 获得/设置 z-index 值 默认 100
+    /// </summary>
+    [Parameter]
+    public int? ZIndex { get; set; }
+
+    /// <summary>
+    /// 获得/设置 组件内容
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-affix")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private string? StyleString => CssBuilder.Default("position: sticky;")
+        .AddClass($"z-index: {ZIndex};", ZIndex.HasValue)
+        .AddClass($"{Position.ToDescriptionString()}: {Offset}px;")
+        .AddStyleFromAttributes(AdditionalAttributes)
+        .Build();
+}
