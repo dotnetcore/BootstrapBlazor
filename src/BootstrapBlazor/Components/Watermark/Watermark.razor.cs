69f85ce7ffbcf67319f39fa09dc613fb5e677450
Argo Zhang <argo@live.ca>
2025-01-12 14:26:40 +0800

    feat: 增加水印组件

---
+++ src/BootstrapBlazor/Components/Watermark/Watermark.razor.cs
@@ -0,0 +1,76 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Watermark 组件
+/// </summary>
+public partial class Watermark
+{
+    /// <summary>
+    /// 获得/设置 组件内容
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 水印文本 默认 BootstrapBlazor
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// 获得/设置 字体大小 如 1rem 12px 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public string? FontSize { get; set; }
+
+    /// <summary>
+    /// 获得/设置 颜色 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public string? Color { get; set; }
+
+    /// <summary>
+    /// 获得/设置 水印的旋转角度 默认 null 45°
+    /// </summary>
+    [Parameter]
+    public string? Rotate { get; set; }
+
+    /// <summary>
+    /// 获得/设置 水印元素的 z-index 值 默认 null
+    /// </summary>
+    [Parameter]
+    public int? ZIndex { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-watermark")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        Text ??= "BootstrapBlazor";
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, new
+    {
+        Text,
+        FontSize,
+        Color,
+        Rotate,
+        ZIndex
+    });
+}
---
+++ src/BootstrapBlazor/Components/Watermark/Watermark.razor.js
@@ -0,0 +1,66 @@
+﻿import EventHandler from "../../modules/event-handler.js"
+
+export function init(id, options) {
+    const el = document.getElementById(id);
+    if (el === null) {
+        return;
+    }
+
+    createWatermark(el, options);
+}
+
+export function dispose(id) {
+
+}
+
+const createWatermark = (el, options) => {
+    const defaults = {
+        gap: 40,
+        fontSize: 16,
+        text: 'BootstrapBlazor',
+        rotate: -40
+    };
+
+    for(const key in options) {
+        if(options[key] === void 0 || options[key] === null) {
+            delete options[key];
+        }
+    }
+
+    const bg = getWatermark({... defaults, ...options });
+    const div = document.createElement('div');
+    const { base64, styleSize } = bg;
+    div.style.backgroundImage = `url(${base64})`;
+    div.style.backgroundSize = `${styleSize}px ${styleSize}px`;
+    div.classList.add("bb-watermark-bg");
+    el.appendChild(div);
+}
+
+const getWatermark = props => {
+    const canvas = document.createElement('canvas');
+    const devicePixelRatio = window.devicePixelRatio || 1;
+
+    const fontSize = props.fontSize * devicePixelRatio;
+    const font = fontSize + 'px serif';
+    const ctx = canvas.getContext('2d');
+
+    ctx.font = font;
+    const { width } = ctx.measureText(props.text);
+    const canvasSize = Math.max(100, width) + props.gap * devicePixelRatio;
+    canvas.width = canvasSize;
+    canvas.height = canvasSize;
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+
+    ctx.rotate((Math.PI / 180) * props.rotate);
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)';
+    ctx.font = font;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+
+    ctx.fillText(props.text, 0, 0);
+    return {
+        base64: canvas.toDataURL(),
+        size: canvasSize,
+        styleSize: canvasSize / devicePixelRatio,
+    };
+}
