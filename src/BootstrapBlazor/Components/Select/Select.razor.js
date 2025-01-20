eae29448b56b2063f4f9f93d7c68f7c0fc2dd1b3
Argo Zhang <argo@live.ca>
2025-01-20 11:37:28 +0800

    refactor: 更新 base-select 代码复用

--- src/BootstrapBlazor/Components/Select/Select.razor.js
+++ src/BootstrapBlazor/Components/Select/Select.razor.js
@@ -1,8 +1,7 @@
-﻿import { debounce, getHeight, getInnerHeight, getTransitionDelayDurationFromElement } from "../../modules/utility.js"
+﻿import { getTransitionDelayDurationFromElement } from "../../modules/utility.js"
+import { registerSelect, unregisterSelect } from "../../modules/base-select.js"
 import Data from "../../modules/data.js"
-import EventHandler from "../../modules/event-handler.js"
 import Popover from "../../modules/base-popover.js"
-import Input from "../../modules/input.js"
 
 export function init(id, invoke, options) {
     const el = document.getElementById(id)
@@ -10,89 +9,18 @@ export function init(id, invoke, options) {
         return
     }
 
-    const { confirmMethodCallback, searchMethodCallback } = options;
     const search = el.querySelector(".search-text")
     const popover = Popover.init(el)
-
-    const shown = () => {
-        if (search) {
-            search.focus();
-        }
-        const active = popover.toggleMenu.querySelector('.dropdown-item.active');
-        if (active) {
-            scrollIntoView(el, active);
-        }
-    }
-
-    const keydown = e => {
-        const menu = popover.toggleMenu;
-        const key = e.key;
-        if (key === "Enter" || key === 'NumpadEnter') {
-            if (popover.isPopover) {
-                popover.hide();
-            }
-            else {
-                menu.classList.remove('show');
-            }
-            const activeItem = menu.querySelector('.active');
-            let index = indexOf(menu, activeItem)
-            invoke.invokeMethodAsync(confirmMethodCallback, index)
-        }
-        else if (key === 'ArrowUp' || key === 'ArrowDown') {
-            e.preventDefault();
-
-            if (menu.querySelector('.dropdown-virtual')) {
-                return;
-            }
-
-            const items = [...menu.querySelectorAll('.dropdown-item:not(.disabled)')];
-            if (items.length > 0) {
-                let current = menu.querySelector('.active');
-                if (current !== null) {
-                    current.classList.remove('active');
-                }
-
-                let index = current === null ? -1 : items.indexOf(current);
-                index = key === 'ArrowUp' ? index - 1 : index + 1;
-                if (index < 0) {
-                    index = items.length - 1;
-                }
-                else if (index > items.length - 1) {
-                    index = 0;
-                }
-                current = items[index];
-                current.classList.add('active');
-                scrollIntoView(el, current);
-            }
-        }
-    }
-
-    if (popover.isPopover) {
-        EventHandler.on(el, 'shown.bs.popover', shown);
-    }
-    else {
-        EventHandler.on(el, 'shown.bs.dropdown', shown);
-    }
-
     const input = el.querySelector(`#${id}_input`);
-    EventHandler.on(input, 'keydown', keydown)
-
-    const onSearch = debounce(async v => {
-        search.parentElement.classList.add('l');
-        await invoke.invokeMethodAsync(searchMethodCallback, v);
-        search.parentElement.classList.remove('l');
-    });
-    if (search) {
-        Input.composition(search, onSearch);
-        EventHandler.on(search, 'keydown', keydown);
-    }
-
     const select = {
-        el,
+        el, invoke, options,
         search,
+        keydownEl: [search, input],
         popover
     }
     Data.set(id, select);
+
+    registerSelect(select);
 }
 
 export function show(id) {
@@ -122,24 +50,6 @@ export function dispose(id) {
     Data.remove(id)
 
     if (select) {
-        const { el, popover, search } = select
-        EventHandler.off(el, 'shown.bs.dropdown')
-        EventHandler.off(el, 'keydown');
-        Popover.dispose(popover);
-
-        if (search) {
-            Input.dispose(search);
-            EventHandler.off(search, 'keydown');
-        }
+        unregisterSelect(select);
     }
-}
-
-const indexOf = (el, element) => {
-    const items = el.querySelectorAll('.dropdown-item')
-    return Array.prototype.indexOf.call(items, element)
-}
-
-const scrollIntoView = (el, item) => {
-    const behavior = el.getAttribute('data-bb-scroll-behavior') ?? 'smooth';
-    item.scrollIntoView({ behavior: behavior, block: "nearest", inline: "start" });
 }
--- src/BootstrapBlazor/wwwroot/modules/base-select.js
+++ src/BootstrapBlazor/wwwroot/modules/base-select.js
@@ -2,22 +2,19 @@
 import Input from "./input.js"
 import EventHandler from "./event-handler.js"
 
-export function registerSearch(el, options) {
-    Input.composition(search, onSearch);
-    EventHandler.on(search, 'keydown', keydown);
-
-    if (popover.isPopover) {
-        EventHandler.on(el, 'shown.bs.popover', shown);
-    }
-    else {
-        EventHandler.on(el, 'shown.bs.dropdown', shown);
-    }
+export function registerSelect(select) {
+    initSearchHandler(select);
+    initKeydownHandler(select);
+    initShownHandler(select);
 }
 
-export function unregisterSearch(el) {
+export function unregisterSelect(select) {
+    const { el, search, keydownEl, popover } = select;
     Input.dispose(search);
-    EventHandler.off(search, 'keydown');
 
+    keydownEl.forEach(ele => {
+        EventHandler.off(ele, 'keydown');
+    })
     if (popover.isPopover) {
         EventHandler.off(el, 'shown.bs.popover');
     }
@@ -26,62 +23,86 @@ export function unregisterSearch(el) {
     }
 }
 
-const onSearch = debounce(async v => {
-    search.parentElement.classList.add('l');
-    await invoke.invokeMethodAsync('TriggerOnSearch', v);
-    search.parentElement.classList.remove('l');
-});
+const initKeydownHandler = select => {
+    const { el, invoke, options, keydownEl, popover } = select;
+    const { confirmMethodCallback } = options;
+    const keydown = e => {
+        const menu = popover.toggleMenu;
+        const key = e.key;
+        if (key === "Enter" || key === 'NumpadEnter') {
+            if (popover.isPopover) {
+                popover.hide();
+            }
+            else {
+                menu.classList.remove('show');
+            }
+            const activeItem = menu.querySelector('.active');
+            let index = indexOf(menu, activeItem)
+            invoke.invokeMethodAsync(confirmMethodCallback, index)
+        }
+        else if (key === 'ArrowUp' || key === 'ArrowDown') {
+            e.preventDefault();
 
-const shown = () => {
-    if (search) {
-        search.focus();
+            if (menu.querySelector('.dropdown-virtual')) {
+                return;
+            }
+
+            const items = [...menu.querySelectorAll('.dropdown-item:not(.disabled)')];
+            if (items.length > 0) {
+                let current = menu.querySelector('.active');
+                if (current !== null) {
+                    current.classList.remove('active');
+                }
+
+                let index = current === null ? -1 : items.indexOf(current);
+                index = key === 'ArrowUp' ? index - 1 : index + 1;
+                if (index < 0) {
+                    index = items.length - 1;
+                }
+                else if (index > items.length - 1) {
+                    index = 0;
+                }
+                current = items[index];
+                current.classList.add('active');
+                scrollIntoView(el, current);
+            }
+        }
     }
-    const active = popover.toggleMenu.querySelector('.dropdown-item.active');
-    if (active) {
-        scrollIntoView(el, active);
-    }
+
+    keydownEl.forEach(ele => {
+        EventHandler.on(ele, 'keydown', keydown);
+    });
 }
 
-const keydown = e => {
-    const menu = popover.toggleMenu;
-    const key = e.key;
-    if (key === "Enter" || key === 'NumpadEnter') {
-        if (popover.isPopover) {
-            popover.hide();
+const initSearchHandler = select => {
+    const { search, invoke, options } = select;
+    const { searchMethodCallback } = options;
+    const onSearch = debounce(async v => {
+        search.parentElement.classList.add('l');
+        await invoke.invokeMethodAsync(searchMethodCallback, v);
+        search.parentElement.classList.remove('l');
+    });
+
+    Input.composition(search, onSearch);
+}
+
+const initShownHandler = select => {
+    const { el, search, popover } = select;
+    const shown = () => {
+        if (search) {
+            search.focus();
         }
-        else {
-            menu.classList.remove('show');
+        const active = popover.toggleMenu.querySelector('.dropdown-item.active');
+        if (active) {
+            scrollIntoView(el, active);
         }
-        const activeItem = menu.querySelector('.active');
-        let index = indexOf(menu, activeItem)
-        invoke.invokeMethodAsync(confirmMethodCallback, index)
     }
-    else if (key === 'ArrowUp' || key === 'ArrowDown') {
-        e.preventDefault();
 
-        if (menu.querySelector('.dropdown-virtual')) {
-            return;
-        }
-
-        const items = [...menu.querySelectorAll('.dropdown-item:not(.disabled)')];
-        if (items.length > 0) {
-            let current = menu.querySelector('.active');
-            if (current !== null) {
-                current.classList.remove('active');
-            }
-
-            let index = current === null ? -1 : items.indexOf(current);
-            index = key === 'ArrowUp' ? index - 1 : index + 1;
-            if (index < 0) {
-                index = items.length - 1;
-            }
-            else if (index > items.length - 1) {
-                index = 0;
-            }
-            current = items[index];
-            current.classList.add('active');
-            scrollIntoView(el, current);
-        }
+    if (popover.isPopover) {
+        EventHandler.on(el, 'shown.bs.popover', shown);
+    }
+    else {
+        EventHandler.on(el, 'shown.bs.dropdown', shown);
     }
 }
 
