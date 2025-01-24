ffb6411a5802049bc4b06d00b523d48d25397b22
Argo Zhang <argo@live.ca>
2025-01-25 00:12:01 +0800

    doc: 更新示例

---
+++ src/BootstrapBlazor.Server/Components/Components/TabItemContentDemo.razor.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Components;
+
+/// <summary>
+/// TabItemContentDemo 组件
+/// </summary>
+public partial class TabItemContentDemo
+{
+    [CascadingParameter]
+    [NotNull]
+    private TabItem? TabItem { get; set; }
+
+    private Task OnToggleDisable()
+    {
+        TabItem.SetDisabled(true);
+        return Task.CompletedTask;
+    }
+}
