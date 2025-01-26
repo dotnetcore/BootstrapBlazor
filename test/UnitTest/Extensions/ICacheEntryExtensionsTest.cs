b3e6d5704f06f46a98b2995ac06606fbb3fba883
Argo Zhang <argo@live.ca>
2025-01-26 18:57:51 +0800

    test: 更新单元测试

---
+++ test/UnitTest/Extensions/ICacheEntryExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace UnitTest.Extensions;
+
+public class ICacheEntryExtensionsTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void GetLastAccessed_Ok()
+    {
+        Cache.GetOrCreate("test_01", entry =>
+        {
+            return 1;
+        });
+
+        Assert.True(Cache.TryGetCacheEntry("test_01", out var entry));
+        var v = entry.GetLastAccessed();
+        Assert.NotNull(v);
+    }
+}
