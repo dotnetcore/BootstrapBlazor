308ff65d037fce0624ee528ebd29caba30afcb8f
Argo Zhang <argo@live.ca>
2025-02-04 15:29:26 +0800

    feat: 增加 TypedJs 组件

---
+++ src/BootstrapBlazor/Components/TypedJs/TypedJs.razor.cs
@@ -0,0 +1,89 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// TypedJs 组件类
+/// </summary>
+public partial class TypedJs
+{
+    /// <summary>
+    /// 获得/设置 组件显示文字 默认 null 未设置
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    /// <summary>
+    /// 获得/设置 组件配置 <see cref="TypedJsOptions"/> 实例 默认 null
+    /// </summary>
+    [Parameter]
+    public TypedJsOptions? Options { get; set; }
+
+    private string? _lastOptions;
+
+    private string? _text;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        if (firstRender)
+        {
+            _lastOptions = Options?.ToString();
+            _text = Text;
+        }
+        else if (UpdateParameters())
+        {
+            await InvokeVoidAsync("update", Id, Text, Options);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override Task InvokeInitAsync() => InvokeVoidAsync("init", Id, Interop, Text, Options, new
+    {
+        TriggerStop = nameof(TriggerStop)
+    });
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [JSInvokable]
+    public Task TriggerStart()
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [JSInvokable]
+    public Task TriggerStop()
+    {
+        return Task.CompletedTask;
+    }
+
+    private bool UpdateParameters()
+    {
+        var ret = false;
+        if (Text != _text || Options?.ToString() != _lastOptions)
+        {
+            _text = Text;
+            _lastOptions = Options?.ToString();
+            ret = true;
+        }
+        return ret;
+    }
+}
---
+++ src/BootstrapBlazor/Components/TypedJs/TypedJs.razor.js
@@ -0,0 +1,43 @@
+﻿import Data from "../../modules/data.js"
+import Typed from '../../lib/typedjs/typed.module.js'
+
+export function init(id, invoke, text, options, callbacks) {
+    const el = document.getElementById(id);
+    options ??= {};
+    if (text) {
+        options.strings = [text];
+    }
+    if (options.strings === void 0) {
+        options.strings = [""];
+    }
+
+    const typed = new Typed(el, options);
+    Data.set(id, { el, invoke, callbacks, typed });
+}
+
+export function update(id, text, options) {
+    const typedJs = Data.get(id);
+
+    if (typedJs) {
+        const { el, typed } = typedJs;
+        typed.destroy();
+
+        if (text) {
+            options.strings = [text];
+        }
+        if (options.strings === void 0) {
+            options.strings = [""];
+        }
+        typedJs.typed = new Typed(el, options);
+    }
+}
+
+export function dispose(id) {
+    const typedJs = Data.get(id);
+    Data.remove(id);
+
+    if (typedJs) {
+        const { typed } = typedJs;
+        typed.destroy();
+    }
+}
