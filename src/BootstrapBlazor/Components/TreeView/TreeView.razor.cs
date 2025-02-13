3880b09ff95c3ea33a0c80c75278ade076d63a9d
Argo Zhang <argo@live.ca>
2025-02-13 12:24:49 +0800

    revert: 撤销滚动参数更改

--- src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
+++ src/BootstrapBlazor/Components/TreeView/TreeView.razor.cs
@@ -402,7 +402,7 @@ public partial class TreeView<TItem> : IModelEqualityComparer<TItem>
         if (_keyboardArrowUpDownTrigger)
         {
             _keyboardArrowUpDownTrigger = false;
-            await InvokeVoidAsync("scroll", Id, ScrollIntoViewOptions, _activeItem != null && IsVirtualize);
+            await InvokeVoidAsync("scroll", Id, ScrollIntoViewOptions);
         }
     }
 
