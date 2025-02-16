87b57468a4945dcb893f0a1eb951ef7291267791
Argo Zhang <argo@live.ca>
2025-02-16 09:54:57 +0800

    feat: 增加 ContextMenuDivider 组件

---
+++ src/BootstrapBlazor/Components/ContextMenu/ContextMenuDivider.cs
@@ -0,0 +1,61 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ContextMenuDivider 组件
+/// </summary>
+public class ContextMenuDivider : Divider, IContextMenuItem, IDisposable
+{
+    [CascadingParameter]
+    [NotNull]
+    private ContextMenu? ContextMenu { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        ContextMenu.AddItem(this);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="builder"></param>
+    protected override void BuildRenderTree(RenderTreeBuilder builder) { }
+
+    private bool disposedValue;
+
+    /// <summary>
+    /// 释放资源方法
+    /// </summary>
+    /// <param name="disposing"></param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                ContextMenu.RemoveItem(this);
+            }
+            disposedValue = true;
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
