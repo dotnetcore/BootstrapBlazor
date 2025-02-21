88ed8e96cc1c2585921a479489fd6aa11f1bb615
Argo Zhang <argo@live.ca>
2025-02-21 10:24:26 +0800

    refactor: 移除日志输出

--- src/BootstrapBlazor/Components/DateTimePicker/DateTimePicker.razor.js
+++ src/BootstrapBlazor/Components/DateTimePicker/DateTimePicker.razor.js
@@ -15,8 +15,7 @@ export function init(id, invoke, options) {
             return el.classList.contains('disabled');
         },
         hideCallback: () => {
-            console.log('hideCallback');
-            invoke.invokeMethodAsync(options.triggerHideCallback);
+            invoke?.invokeMethodAsync(options.triggerHideCallback);
         }
     });
     const dateTimePicker = {
