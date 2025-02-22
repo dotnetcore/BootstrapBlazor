a04df3ec286b6ed507ada44dd8c03e8d3524bbed
Argo Zhang <argo@live.ca>
2025-02-22 09:38:39 +0800

    feat: 增加 Html2Image 服务

---
+++ src/BootstrapBlazor/Services/IHtml2Image.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// IHtml2Image 接口
+/// </summary>
+public interface IHtml2Image
+{
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="selector">选择器</param>
+    /// <param name="options"></param>
+    Task<string?> GetDataAsync(string selector, Html2ImageOptions options);
+
+    /// <summary>
+    /// Export method
+    /// </summary>
+    /// <param name="selector">选择器</param>
+    /// <param name="options"></param>
+    Task<Stream?> GetStreamAsync(string selector, Html2ImageOptions options);
+}
