02d82eb05b693f3009b59178ea5d88c3e9a15af4
Argo Zhang <argo@live.ca>
2025-02-22 12:41:55 +0800

    test: 更新单元测试

--- test/UnitTest/Services/Html2ImageServiceTest.cs
+++ test/UnitTest/Services/Html2ImageServiceTest.cs
@@ -8,7 +8,7 @@ namespace UnitTest.Services;
 public class Html2ImageServiceTest
 {
     [Fact]
-    public async Task ExportPdf_Error()
+    public async Task Html2Image_Error()
     {
         var serviceCollection = new ServiceCollection();
         serviceCollection.AddBootstrapBlazor();
@@ -18,5 +18,31 @@ public class Html2ImageServiceTest
 
         await Assert.ThrowsAsync<NotImplementedException>(() => html2ImageService.GetDataAsync(".test", new()));
         await Assert.ThrowsAsync<NotImplementedException>(() => html2ImageService.GetStreamAsync(".test", new()));
+    }
+
+    [Fact]
+    public void Html2ImageOptions_Ok()
+    {
+        var options = new Html2ImageOptions()
+        {
+            BackgroundColor = "red",
+            Height = 100,
+            Width = 100,
+            Quality = 0.5,
+            CanvasWidth = 10,
+            CanvasHeight = 10,
+            PixelRatio = 2,
+            Type = "image/png",
+            IncludeStyleProperties = ["background", "color"]
+        };
+        Assert.Equal("red", options.BackgroundColor);
+        Assert.Equal(100, options.Height);
+        Assert.Equal(100, options.Width);
+        Assert.Equal(0.5, options.Quality);
+        Assert.Equal(10, options.CanvasWidth);
+        Assert.Equal(10, options.CanvasHeight);
+        Assert.Equal(2, options.PixelRatio);
+        Assert.Equal("image/png", options.Type);
+        Assert.NotNull(options.IncludeStyleProperties);
     }
 }
