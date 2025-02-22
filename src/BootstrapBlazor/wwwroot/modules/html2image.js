01bc65a8449233177f966ee69e159cb257210859
Argo Zhang <argo@live.ca>
2025-02-22 09:38:51 +0800

    chore: 增加脚本

---
+++ src/BootstrapBlazor/wwwroot/modules/html2image.js
@@ -0,0 +1,11 @@
+﻿import '../lib/html2image/html-to-image.js'
+
+export async function execute(selector, methodName, options) {
+    let data = null;
+    const el = document.querySelector(selector);
+    if (el) {
+        const fn = htmlToImage[methodName];
+        data = await fn(el, {});
+    }
+    return data;
+}
