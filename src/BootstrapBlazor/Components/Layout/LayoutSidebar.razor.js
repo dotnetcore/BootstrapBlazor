3b21c0a7b38d1eb76fb2bcb02198b565a49289fa
Argo Zhang <argo@live.ca>
2025-02-24 19:47:30 +0800

    refactor: 更新参数

--- src/BootstrapBlazor/Components/Layout/LayoutSidebar.razor.js
+++ src/BootstrapBlazor/Components/Layout/LayoutSidebar.razor.js
@@ -7,12 +7,16 @@ export function init(id) {
         return;
     }
 
-    const section = document.querySelector('.layout-side');
+    const min = parseFloat(el.getAttribute("data-bb-min") ?? "0");
+    const max = parseFloat(el.getAttribute("data-bb-max") ?? "0");
+    const selector = el.getAttribute("data-bb-selector");
+    const section = document.querySelector(selector);
+    const bar = el.querySelector(".layout-sidebar-body");
     let originX = 0;
     let width = 0;
-    Drag.drag(layout.bar,
+    Drag.drag(bar,
         e => {
-            navmenu.bar.classList.add('drag')
+            bar.classList.add('drag')
             width = parseInt(getComputedStyle(section).getPropertyValue('--bb-layout-sidebar-width'))
             originX = e.clientX || e.touches[0].clientX
         },
@@ -20,20 +24,22 @@ export function init(id) {
             const eventX = e.clientX || (e.touches.length > 0 && e.touches[0].clientX)
             const moveX = eventX - originX
             const newWidth = width + moveX
-            if (newWidth >= 250 && newWidth <= 380) {
+            if (newWidth >= min && newWidth <= max) {
                 section.style.setProperty('--bb-layout-sidebar-width', `${newWidth}px`)
             }
         },
         e => {
-            navmenu.bar.classList.remove('drag')
+            bar.classList.remove('drag')
         }
     )
 }
 
 export function dispose(id) {
-
-    if (layout.bar) {
-        Drag.dispose(layout.bar)
+    const el = document.getElementById(id);
+    if (el) {
+        const bar = el.querySelector(".layout-sidebar-body");
+        if (bar) {
+            Drag.dispose(bar);
+        }
     }
-
 }
