55d448ba4346d77db81c9ece2d2d00179447b684
Argo Zhang <argo@live.ca>
2025-02-28 11:29:52 +0800

    chore: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/UniverSheets.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/UniverSheets.razor.cs
@@ -10,12 +10,37 @@ namespace BootstrapBlazor.Server.Components.Samples;
 /// </summary>
 public partial class UniverSheets
 {
+    [Inject, NotNull]
+    private ToastService? ToastService { get; set; }
+
+    [Inject, NotNull]
+    private IStringLocalizer<UniverSheets>? Localizer { get; set; }
+
     private readonly Dictionary<string, string> Plugins = new()
     {
         { "ReportPlugin", "univer-sheet/plugin.js" }
     };
 
     private UniverSheet _sheet = default!;
+
+    private async Task OnReadyAsync() => await ToastService.Information(Localizer["ToastOnReadyTitle"], Localizer["ToastOnReadyContent"]);
+
+    private static Task<UniverSheetData?> OnPostDataAsync(UniverSheetData data)
+    {
+        // 这里可以根据 data 的内容进行处理然后返回处理后的数据
+        // 本例返回与时间相关的数据
+        var result = new UniverSheetData()
+        {
+            MessageName = data.MessageName,
+            CommandName = data.CommandName,
+            Data = new
+            {
+                key = "datetime",
+                Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+            }
+        };
+        return Task.FromResult<UniverSheetData?>(result);
+    }
 
     private async Task OnPush()
     {
@@ -26,7 +51,8 @@ public partial class UniverSheets
             Data = new
             {
                 Id = 01,
-                Name = "Test-Name"
+                Name = "Test-Name",
+                Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
             }
         });
     }
