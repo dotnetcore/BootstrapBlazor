e7156e980eac03c3020401c19226198280b06525
Argo Zhang <argo@live.ca>
2025-03-03 20:23:41 +0800

    doc(UniverSheet): update sample code (#5502)
    * doc: 增加保存按钮示例
    
    * chore: 增加推送报表示例
    
    * doc: 更新报表模板
    
    * doc: 更新多语言文件
    
    * feat: 增加保存文档示例
    
    * refactor(Textarea): 重构代码
    
    * refactor: 清空其他单元格
    
    * refactor: 重构代码
    
    * chore: 更新依赖包

--- src/BootstrapBlazor.Server/Components/Samples/UniverSheets.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/UniverSheets.razor.cs
@@ -11,23 +11,44 @@ namespace BootstrapBlazor.Server.Components.Samples;
 public partial class UniverSheets
 {
     [Inject, NotNull]
+    private IWebHostEnvironment? WebHost { get; set; }
+
+    [Inject, NotNull]
     private ToastService? ToastService { get; set; }
 
     [Inject, NotNull]
     private IStringLocalizer<UniverSheets>? Localizer { get; set; }
 
-    private readonly Dictionary<string, string> Plugins = new()
+    private readonly Dictionary<string, string> _plugins = new()
     {
         { "ReportPlugin", "univer-sheet/plugin.js" }
     };
 
-    private UniverSheet _sheetExcel = default!;
+    [NotNull]
+    private UniverSheet? _sheetExcel = null;
 
-    private UniverSheet _sheetPlugin = default!;
+    [NotNull]
+    private UniverSheet? _sheetPlugin = null;
+
+    private static string? _reportData = null;
+
+    private string? _jsonData = null;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override async Task OnInitializedAsync()
+    {
+        var reportFile = Path.Combine(WebHost.WebRootPath, "univer-sheet", "report.json");
+        if (File.Exists(reportFile))
+        {
+            _reportData = await File.ReadAllTextAsync(reportFile);
+        }
+    }
 
     private async Task OnReadyAsync() => await ToastService.Information(Localizer["ToastOnReadyTitle"], Localizer["ToastOnReadyContent"]);
 
-    private static Task<UniverSheetData?> OnPostDataAsync(UniverSheetData data)
+    private static Task<UniverSheetData> OnPostDataAsync(UniverSheetData data)
     {
         // 这里可以根据 data 的内容进行处理然后返回处理后的数据
         // 本例返回与时间相关的数据
@@ -41,21 +62,26 @@ public partial class UniverSheets
                 Value = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
             }
         };
-        return Task.FromResult<UniverSheetData?>(result);
+        return Task.FromResult(result);
     }
 
     private async Task OnPushExcelData()
     {
         await _sheetExcel.PushDataAsync(new UniverSheetData()
         {
-            MessageName = "MessageName",
-            CommandName = "CommandName",
-            Data = new object[]
-            {
-                new object[] { "1", "2", "3", "4", "5" },
-                new object[] { "1", "2", "3", "4", "5" },
-            }
+            CommandName = "SetWorkbook",
+            Data = _reportData
         });
+    }
+
+    private async Task OnSaveExcelData()
+    {
+        var result = await _sheetExcel.PushDataAsync(new UniverSheetData()
+        {
+            CommandName = "GetWorkbook"
+        });
+        _jsonData = result?.Data?.ToString();
+        StateHasChanged();
     }
 
     private async Task OnPushPluginData()
