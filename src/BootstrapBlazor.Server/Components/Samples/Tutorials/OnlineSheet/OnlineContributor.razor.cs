7a2fa79bb3a484cadd46542a63eeb564aa3a173f
Argo Zhang <argo@live.ca>
2025-03-04 14:43:06 +0800

    doc: 增加在线协作示例

---
+++ src/BootstrapBlazor.Server/Components/Samples/Tutorials/OnlineSheet/OnlineContributor.razor.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Samples.Tutorials;
+
+/// <summary>
+/// Online sheet sample code
+/// </summary>
+public partial class OnlineContributor
+{
+    /// <summary>
+    /// Gets or sets Contributor
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public Contributor? Contributor { get; set; }
+}
