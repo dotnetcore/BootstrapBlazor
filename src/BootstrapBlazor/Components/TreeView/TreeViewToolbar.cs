95fcd1f14aea04777efbfbc995c98f2cc20ae04b
Argo Zhang <argo@live.ca>
2025-03-12 16:24:24 +0800

    feat: 增加 TreeViewToolbar 组件

--- src/BootstrapBlazor/Components/TreeView/TreeViewToolbar.cs
+++ src/BootstrapBlazor/Components/TreeView/TreeViewToolbar.cs
@@ -3,10 +3,43 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
+using Microsoft.AspNetCore.Components.Rendering;
+
 namespace BootstrapBlazor.Components;
 
-class TreeViewToolbar : ComponentBase
+class TreeViewToolbar<TItem> : ComponentBase
 {
+    [Parameter, NotNull]
+    public Func<TItem, Task<bool>>? ShowToolbarAsync { get; set; }
+
+    [Parameter, NotNull]
+    public TItem? Item { get; set; }
+
     [Parameter]
-    public Func<Task<bool>>? ShowToolbar { get; set; }
+    public RenderFragment? ChildContent { get; set; }
+
+    private bool _showToolbar = false;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    protected override async Task OnParametersSetAsync()
+    {
+        await base.OnParametersSetAsync();
+
+        _showToolbar = await ShowToolbarAsync(Item);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="builder"></param>
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        if (_showToolbar)
+        {
+            builder.AddContent(0, ChildContent);
+        }
+    }
 }
