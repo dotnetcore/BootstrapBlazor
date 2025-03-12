6c60d9a52248fa56c96b5c84296e773156b0f943
Argo Zhang <argo@live.ca>
2025-03-12 08:53:06 +0800

    doc: 更新文档注释

--- src/BootstrapBlazor/Attributes/AutoGenerateColumnAttribute.cs
+++ src/BootstrapBlazor/Attributes/AutoGenerateColumnAttribute.cs
@@ -6,34 +6,34 @@
 namespace BootstrapBlazor.Components;
 
 /// <summary>
-/// AutoGenerateColumn 标签类，用于 <see cref="Table{TItem}"/> 标识自动生成列
+/// AutoGenerateColumn attribute class, used to mark auto-generated columns in <see cref="Table{TItem}"/>
 /// </summary>
 [AttributeUsage(AttributeTargets.Property)]
 public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColumn
 {
     /// <summary>
-    /// 获得/设置 显示顺序 ，规则如下：
+    /// Gets or sets the display order. The rules are as follows:
     /// <para></para>
-    /// &gt;0时排前面，1,2,3...
+    /// &gt;0 for the front, 1,2,3...
     /// <para></para>
-    /// =0时排中间(默认)
+    /// =0 for the middle (default)
     /// <para></para>
-    /// &lt;0时排后面，...-3,-2,-1
+    /// &lt;0 for the back, ...-3,-2,-1
     /// </summary>
     public int Order { get; set; }
 
     /// <summary>
-    /// 获得/设置 是否为默认排序列 默认为 false
+    /// <inheritdoc/>
     /// </summary>
     public bool DefaultSort { get; set; }
 
     /// <summary>
-    /// 获得/设置 是否不进行验证 默认为 false
+    /// <inheritdoc/>
     /// </summary>
     public bool SkipValidate { get; set; }
 
     /// <summary>
-    /// <inheritdoc/>
+    /// Gets or sets whether the column is read-only when adding a new item. Default is null, using the <see cref="IEditorItem.Readonly"/> value.
     /// </summary>
     public bool IsReadonlyWhenAdd { get; set; }
 
@@ -44,7 +44,7 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     /// <summary>
-    /// <inheritdoc/>
+    /// Gets or sets whether the column is read-only when editing an item. Default is null, using the <see cref="IEditorItem.Readonly"/> value.
     /// </summary>
     public bool IsReadonlyWhenEdit { get; set; }
 
@@ -55,7 +55,7 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     /// <summary>
-    /// <inheritdoc/>
+    /// Gets or sets whether the column is visible when adding a new item. Default is null, using the <see cref="Visible"/> value.
     /// </summary>
     public bool IsVisibleWhenAdd { get; set; } = true;
 
@@ -66,13 +66,10 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     /// <summary>
-    /// <inheritdoc/>
+    /// Gets or sets whether the column is visible when editing an item. Default is null, using the <see cref="Visible"/> value.
     /// </summary>
     public bool IsVisibleWhenEdit { get; set; } = true;
 
-    /// <summary>
-    /// 自定义搜索方法
-    /// </summary>
     Func<ITableColumn, string?, SearchFilterAction>? ITableColumn.CustomSearch { get; set; }
 
     bool? ITableColumn.IsVisibleWhenEdit
@@ -82,7 +79,6 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     bool? IEditorItem.Required { get; set; }
-
 
     bool? ITableColumn.IsRequiredWhenAdd { get; set; }
 
@@ -94,7 +90,7 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     public string? RequiredErrorMessage { get; set; }
 
     /// <summary>
-    /// 获得/设置 是否显示标签 Tooltip 多用于标签文字过长导致裁减时使用 默认 false
+    /// Gets or sets whether to show label tooltip. Mostly used when the label text is too long and gets truncated. Default is false.
     /// </summary>
     public bool ShowLabelTooltip { get; set; }
 
@@ -105,14 +101,14 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     /// <summary>
-    /// 获得/设置 是否为默认排序规则 默认为 SortOrder.Unset
+    /// Gets or sets the default sort order. Default is SortOrder.Unset.
     /// </summary>
     public SortOrder DefaultSortOrder { get; set; }
 
     IEnumerable<SelectedItem>? IEditorItem.Items { get; set; }
 
     /// <summary>
-    /// 获得/设置 列宽
+    /// Gets or sets the column width.
     /// </summary>
     public int Width { get; set; }
 
@@ -123,27 +119,27 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     }
 
     /// <summary>
-    /// 获得/设置 是否固定本列 默认 false 不固定
+    /// <inheritdoc/>
     /// </summary>
     public bool Fixed { get; set; }
 
     /// <summary>
-    /// 获得/设置 列 td 自定义样式 默认为 null 未设置
+    /// <inheritdoc/>
     /// </summary>
     public string? CssClass { get; set; }
 
     /// <summary>
-    /// 获得/设置 显示节点阈值 默认值 BreakPoint.None 未设置
+    /// <inheritdoc/>
     /// </summary>
     public BreakPoint ShownWithBreakPoint { get; set; }
 
     /// <summary>
-    /// 获得/设置 格式化字符串 如时间类型设置 yyyy-MM-dd
+    /// <inheritdoc/>
     /// </summary>
     public string? FormatString { get; set; }
 
     /// <summary>
-    /// 获得/设置 placeholder 文本 默认为 null
+    /// <inheritdoc/>
     /// </summary>
     public string? PlaceHolder { get; set; }
 
@@ -152,104 +148,47 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     /// </summary>
     public Func<object?, Task<string?>>? Formatter { get; set; }
 
-    /// <summary>
-    /// 获得/设置 编辑模板
-    /// </summary>
     RenderFragment<object>? IEditorItem.EditTemplate { get; set; }
 
-    /// <summary>
-    /// 获得/设置 表头模板
-    /// </summary>
     RenderFragment<ITableColumn>? ITableColumn.HeaderTemplate { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     RenderFragment<ITableColumn>? ITableColumn.ToolboxTemplate { get; set; }
 
     /// <summary>
-    /// 获得/设置 组件类型 默认为 null
+    /// <inheritdoc/>
     /// </summary>
     public Type? ComponentType { get; set; }
 
-    /// <summary>
-    /// 获得/设置 组件自定义类型参数集合 默认为 null
-    /// </summary>
     IEnumerable<KeyValuePair<string, object>>? IEditorItem.ComponentParameters { get; set; }
 
-    /// <summary>
-    /// 获得/设置 显示模板
-    /// </summary>
     RenderFragment<object>? ITableColumn.Template { get; set; }
 
-    /// <summary>
-    /// 获得/设置 搜索模板
-    /// </summary>
     RenderFragment<object>? ITableColumn.SearchTemplate { get; set; }
 
-    /// <summary>
-    /// 获得/设置 过滤模板
-    /// </summary>
     RenderFragment? ITableColumn.FilterTemplate { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     Func<object?, Task<string?>>? ITableColumn.GetTooltipTextCallback { get; set; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.Searchable { get => Searchable; set => Searchable = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.Filterable { get => Filterable; set => Filterable = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.Sortable { get => Sortable; set => Sortable = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.TextWrap { get => TextWrap; set => TextWrap = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.TextEllipsis { get => TextEllipsis; set => TextEllipsis = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? IEditorItem.Ignore { get => Ignore; set => Ignore = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? IEditorItem.Readonly { get => Readonly; set => Readonly = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.Visible { get => Visible; set => Visible = value ?? true; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.ShowTips { get => ShowTips; set => ShowTips = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     bool? ITableColumn.ShowCopyColumn { get => ShowCopyColumn; set => ShowCopyColumn = value ?? false; }
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
     Alignment? ITableColumn.Align { get => Align; set => Align = value ?? Alignment.None; }
 
     /// <summary>
@@ -258,28 +197,25 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     public string? Step { get; set; }
 
     /// <summary>
-    /// 获得/设置 行数
+    /// <inheritdoc/>
     /// </summary>
     public int Rows { get; set; }
 
     /// <summary>
-    /// 获得/设置 控件的占列数值范围 1-12
+    /// <inheritdoc/>
     /// </summary>
     public int Cols { get; set; }
 
-    /// <summary>
-    /// 获得/设置 列过滤器
-    /// </summary>
     IFilter? ITableColumn.Filter { get; set; }
 
     /// <summary>
-    /// 获得 属性类型
+    /// <inheritdoc/>
     /// </summary>
     [NotNull]
     public Type? PropertyType { get; internal set; }
 
     /// <summary>
-    /// 获得/设置 当前属性显示文字 列头或者标签名称
+    /// <inheritdoc/>
     /// </summary>
     public string? Text { get; set; }
 
@@ -290,7 +226,7 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     internal string? FieldName { get; set; }
 
     /// <summary>
-    /// 获得/设置 字段数据源下拉框是否显示搜索栏 默认 false 不显示
+    /// <inheritdoc/>
     /// </summary>
     public bool ShowSearchWhenSelect { get; set; }
 
@@ -324,35 +260,27 @@ public class AutoGenerateColumnAttribute : AutoGenerateBaseAttribute, ITableColu
     /// </summary>
     public StringComparison LookupStringComparison { get; set; } = StringComparison.OrdinalIgnoreCase;
 
-    /// <summary>
-    /// 获得/设置 单元格回调方法
-    /// </summary>
     Action<TableCellArgs>? ITableColumn.OnCellRender { get; set; }
 
-    /// <summary>
-    /// 获得/设置 自定义验证集合
-    /// </summary>
     List<IValidator>? IEditorItem.ValidateRules { get; set; }
 
     /// <summary>
-    /// 获取绑定字段显示名称方法
+    /// <inheritdoc/>
     /// </summary>
-    /// <returns></returns>
     public virtual string GetDisplayName() => Text ?? "";
 
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
-    /// <returns></returns>
     public string GetFieldName() => FieldName;
 
     /// <summary>
-    /// 获得/设置 当前属性分组
+    /// <inheritdoc/>
     /// </summary>
     public string? GroupName { get; set; }
 
     /// <summary>
-    /// 获得/设置 当前属性分组排序 默认 0
+    /// <inheritdoc/>
     /// </summary>
     public int GroupOrder { get; set; }
 
