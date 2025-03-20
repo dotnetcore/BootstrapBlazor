70331dd25b2d31e0bb166e829aa30d2fb8b79773
Argo Zhang <argo@live.ca>
2025-03-20 10:49:36 +0800

    refactor: 增加关窗逻辑

--- src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
+++ src/BootstrapBlazor/Components/DateTimeRange/DateTimeRange.razor.cs
@@ -442,6 +442,7 @@ public partial class DateTimeRange
         {
             EditContext.NotifyFieldChanged(FieldIdentifier.Value);
         }
+        await InvokeVoidAsync("hide", Id);
     }
 
     /// <summary>
