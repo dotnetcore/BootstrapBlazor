0a81340e8d115012fac1ddd2dfc8ce03672286a8
Argo Zhang <argo@live.ca>
2025-03-27 17:18:24 +0800

    Revert "refactor: 代码格式化"
    This reverts commit 35b74d6cd0895469bbedb8428b421c4f429c1602.

--- src/BootstrapBlazor/Components/Table/Table.razor.TreeNode.cs
+++ src/BootstrapBlazor/Components/Table/Table.razor.TreeNode.cs
@@ -28,6 +28,7 @@ public partial class Table<TItem>
     /// <summary>
     /// 获得/设置 树形数据集合
     /// </summary>
+    [NotNull]
     private List<TableTreeNode<TItem>> TreeRows { get; } = new(100);
 
     /// <summary>
@@ -35,7 +36,7 @@ public partial class Table<TItem>
     /// </summary>
     private bool IsLoadChildren { get; set; }
 
-    [NotNull] 
+    [NotNull]
     private string? NotSetOnTreeExpandErrorMessage { get; set; }
 
     /// <summary>
@@ -120,7 +121,6 @@ public partial class Table<TItem>
         {
             throw new InvalidOperationException(NotSetOnTreeExpandErrorMessage);
         }
-
         return await OnTreeExpand(node.Value);
     }
 }
