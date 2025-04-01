33cc7ce74bb1a9cf8f789ae4507df77ea5160a03
Argo Zhang <argo@live.ca>
2025-04-01 18:58:07 +0800

    feat(Layout): add ShowTabInHeader parameter (#5762)
    * feat: 增加 RenderTabHeader 方法
    
    * chore: 增加 ShowTabInHeader 方法
    
    * feat: 实现 TabHeader 再其他地方渲染逻辑
    
    * refactor: 精简代码逻辑
    
    * refactor: 增加 Layout 参数用于接管 RenderTabHeader 方法
    
    * refactor: 脚本兼容
    
    * test: 更新单元测试

---
+++ src/BootstrapBlazor/Components/Layout/LayoutHeader.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+internal class LayoutHeader : IComponent, ITabHeader
+{
+    private RenderHandle _renderHandle;
+
+    void IComponent.Attach(RenderHandle renderHandle)
+    {
+        _renderHandle = renderHandle;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="parameters"></param>
+    /// <returns></returns>
+    Task IComponent.SetParametersAsync(ParameterView parameters)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// render tab header method.
+    /// </summary>
+    public void Render(RenderFragment renderFragment)
+    {
+        _renderHandle.Render(builder =>
+        {
+            builder.AddContent(0, renderFragment);
+        });
+    }
+}
---
+++ src/BootstrapBlazor/Components/Tab/ITabHeader.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// ITabHeader interface
+/// </summary>
+public interface ITabHeader
+{
+    /// <summary>
+    /// Render method
+    /// </summary>
+    /// <param name="renderFragment"></param>
+    void Render(RenderFragment renderFragment);
+}
