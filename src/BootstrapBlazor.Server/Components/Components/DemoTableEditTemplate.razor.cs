7a684656d89428d4931bb46106800c74d1f7fd1c
Argo Zhang <argo@live.ca>
2025-04-03 11:17:00 +0800

    doc: 更新 DemoTableEditTemplate 逻辑

---
+++ src/BootstrapBlazor.Server/Components/Components/DemoTableEditTemplate.razor.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Components;
+
+/// <summary>
+/// DemoTableEditTemplate component
+/// </summary>
+public partial class DemoTableEditTemplate
+{
+    /// <summary>
+    /// Gets or sets the Foo instance.
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public Foo? Model { get; set; }
+
+    private IEnumerable<SelectedItem>? Hobbies { get; set; }
+
+    private string? EducationDesc => Model.Education switch
+    {
+        EnumEducation.Primary => Localizer["TablesEditTemplateDisplayDetail1"],
+        EnumEducation.Middle => Localizer["TablesEditTemplateDisplayDetail2"],
+        _ => ""
+    };
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        Hobbies = Foo.GenerateHobbies(LocalizerFoo);
+    }
+}
