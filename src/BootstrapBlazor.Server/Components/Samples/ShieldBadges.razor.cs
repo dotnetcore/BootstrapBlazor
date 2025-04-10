9f4c21d02c34a596d3500e07f04c75cad14d96a4
Argo Zhang <argo@live.ca>
2025-04-10 12:55:33 +0800

    feat(ShieldBadge): add ShieldBadge component (#5804)
    * doc: 文档注释英语化
    
    * doc: 微调代码块宽度
    
    * doc: 增加代码文件映射
    
    * doc: 增加菜单资源文件
    
    * doc: 增加徽章菜单
    
    * doc: 增加示例
    
    * refactor: 增加组件
    
    * refactor: 增加样式 scss 文件
    
    * doc: 增加示例
    
    * refactor: 调整标签宽度
    
    * test: 增加单元测试

---
+++ src/BootstrapBlazor.Server/Components/Samples/ShieldBadges.razor.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// ShieldBadge component sample
+/// </summary>
+public partial class ShieldBadges
+{
+    private string? _icon = "fa-brands fa-github";
+    private string? _iconColor;
+
+    private string? _label = "download";
+    private string? _labelColor;
+    private string? _labelBackgroundColor;
+
+    private string? _text = "12M";
+    private string? _textColor;
+    private string? _textBackgroundColor;
+
+    private int _radius = 3;
+}
---
+++ test/UnitTest/Components/ShieldBadgeTest.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace UnitTest.Components;
+
+public class ShieldBadgeTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void ShieldBadge_Ok()
+    {
+        var cut = Context.RenderComponent<ShieldBadge>();
+        cut.Contains("shield-badge");
+        cut.Contains("shield-badge-label");
+        cut.Contains("shield-badge-text");
+        cut.DoesNotContain("shield-badge-icon");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.Icon, "fa fa-user"));
+        cut.Contains("shield-badge-icon");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.Label, "test-label"));
+        cut.Contains("test-label");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.Text, "test-text"));
+        cut.Contains("test-text");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.IconColor, "#123"));
+        cut.Contains("--bb-shield-badge-icon-color: #123;");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.LabelColor, "#123"));
+        cut.Contains("--bb-shield-badge-label-color: #123;");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.LabelBackgroundColor, "#123"));
+        cut.Contains("--bb-shield-badge-label-bg: #123;");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.TextColor, "#123"));
+        cut.Contains("--bb-shield-badge-text-color: #123;");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.TextBackgroundColor, "#123"));
+        cut.Contains("--bb-shield-badge-text-bg: #123;");
+
+        cut.SetParametersAndRender(p => p.Add(i => i.Radius, 4));
+        cut.Contains("--bb-shield-badge-radius: 4px;");
+    }
+}
