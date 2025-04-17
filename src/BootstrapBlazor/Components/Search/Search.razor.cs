cdbf7786271c6c45ba7f82a001907800545e04e7
Argo Zhang <argo@live.ca>
2025-04-16 18:38:16 -0700

    refactor: 重构代码

--- src/BootstrapBlazor/Components/Search/Search.razor.cs
+++ src/BootstrapBlazor/Components/Search/Search.razor.cs
@@ -172,7 +172,7 @@ public partial class Search<TValue>
     private SearchContext<TValue> _context = default!;
 
     [NotNull]
-    private RenderTemplate? _dropdown = default;
+    private RenderTemplate? _dropdown = null;
 
     /// <summary>
     /// <inheritdoc/>
