81b679d6ddcd5b846ce97d79c168161d8a28f0fe
Argo Zhang <argo@live.ca>
2025-04-26 13:30:54 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/Authenticators.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Authenticators.razor.cs
@@ -3,6 +3,7 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
+
 namespace BootstrapBlazor.Server.Components.Samples;
 
 /// <summary>
@@ -17,4 +18,33 @@ public partial class Authenticators
     [Inject]
     [NotNull]
     private ITOTPService? TOTPService { get; set; }
+
+    private string _content = "";
+
+    private string? _code;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _content = TOTPService.GenerateOtpUri();
+        _code = TOTPService.Compute("OMM2LVLFX6QJHMYI");
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="firstRender"></param>
+    /// <returns></returns>
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await base.OnAfterRenderAsync(firstRender);
+
+        await Task.Delay(1000);
+        _code = TOTPService.Compute("OMM2LVLFX6QJHMYI");
+        StateHasChanged();
+    }
 }
