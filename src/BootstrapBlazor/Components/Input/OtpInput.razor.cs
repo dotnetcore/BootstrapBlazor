b92217c42b31773e0d23f6db6b60fc8537c8bc06
Argo Zhang <argo@live.ca>
2025-04-26 13:30:45 +0800

    feat: 增加 OtpInput 组件

---
+++ src/BootstrapBlazor/Components/Input/OtpInput.razor.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// OTP input component
+/// </summary>
+public partial class OtpInput
+{
+    /// <summary>
+    /// Gets or sets the length of the OTP input. Default is 6.
+    /// </summary>
+    [Parameter]
+    public int Digits { get; set; } = 6;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [Parameter]
+    public OtpInputType Type { get; set; }
+
+    private string? ClassString => CssBuilder.Default("bb-opt-input")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    private string? SpanClassString => CssBuilder.Default("bb-opt-span")
+        .AddClass(ValidCss)
+        .Build();
+
+    private char[] _values = [];
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        _values = Value?.ToCharArray() ?? [];
+    }
+}
