27474e015cb221ee2326c28f172ea2d56b6fa6e2
Argo Zhang <argo@live.ca>
2025-04-27 13:15:09 +0800

    feat(OtpInput): add OtpInput component (#5902)
    * feat: 增加 OtpInput 组件
    
    * refactor: 增加样式文件扩展名
    
    * refactor: 增加 Readonly 功能
    
    * feat: 增加禁用只读功能
    
    * feat: 增加 MaxLength 属性
    
    * refactor: 增加脚本
    
    * doc: 增加路由
    
    * doc: 增加源码映射文件
    
    * feat: 增加 OtpInput 示例
    
    * refactor: 增加禁用样式
    
    * doc: 更新示例
    
    * feat: 增加 ChildContent 模板
    
    * refactor: 增加占位符
    
    * doc: 更新示例
    
    * refactor: 样式变量化
    
    * refactor: 增加脚本
    
    * feat: 增加数字类型仅录入一个约束
    
    * refactor: 重构组件结构
    
    * test: 增加单元测试
    
    * refactor: 重构组件提高性能
    
    * test: 更新单元测试
    
    * refactor: 增加样式
    
    * refactor: 增加过滤条件
    
    * refactor: 增加同步逻辑
    
    * refactor: 支持拷贝粘贴
    
    * refactor: 更新 selector 值
    
    * feat: 支持粘贴自动填充功能
    
    * feat: 增加数据双向绑定支持
    
    * refactor: 复用代码
    
    * chore: 更新字典
    
    * test: 更新单元测试
    
    * refactor: 支持表单
    
    * doc: 增加表单示例
    
    * test: 更新示例
    
    * doc: 增加本地化
    
    * doc: 增加 IsNew 标志
    
    * refactor: 重构代码

---
+++ src/BootstrapBlazor/Enums/OtpInputType.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// OTP input type
+/// </summary>
+public enum OtpInputType
+{
+    /// <summary>
+    /// Number OTP Input 
+    /// </summary>
+    Number,
+
+    /// <summary>
+    /// Text OTP Input 
+    /// </summary>
+    Text,
+
+    /// <summary>
+    /// Password OTP Input 
+    /// </summary>
+    Password
+}
---
+++ test/UnitTest/Components/OtpInputTest.cs
@@ -0,0 +1,96 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace UnitTest.Components;
+
+/// <summary>
+/// Test for OtpInput component
+/// </summary>
+public class OtpInputTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void OtpInput_Ok()
+    {
+        var cut = Context.RenderComponent<OtpInput>(pb =>
+        {
+            pb.Add(a => a.Value, "123");
+            pb.Add(a => a.Digits, 6);
+        });
+
+        var items = cut.FindAll(".bb-opt-item");
+        Assert.Equal(6, items.Count);
+
+        var item = items[0];
+        Assert.Contains("value=\"1\"", item.OuterHtml);
+        item = items[1];
+        Assert.Contains("value=\"2\"", item.OuterHtml);
+    }
+
+    [Fact]
+    public void Readonly_Ok()
+    {
+        var cut = Context.RenderComponent<OtpInput>(pb =>
+        {
+            pb.Add(a => a.IsReadonly, true);
+        });
+
+        var item = cut.Find(".bb-opt-item");
+        Assert.Equal("<span class=\"bb-opt-item\"></span>", item.OuterHtml);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.IsReadonly, false);
+        });
+        item = cut.Find(".bb-opt-item");
+        Assert.Equal("<input type=\"number\" class=\"bb-opt-item input-number-fix\" inputmode=\"numeric\">", item.OuterHtml);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.IsDisabled, true);
+        });
+        item = cut.Find(".bb-opt-item");
+        Assert.Equal("<span class=\"bb-opt-item disabled\"></span>", item.OuterHtml);
+    }
+
+    [Fact]
+    public void Type_Ok()
+    {
+        var cut = Context.RenderComponent<OtpInput>(pb =>
+        {
+            pb.Add(a => a.Type, OtpInputType.Text);
+            pb.Add(a => a.PlaceHolder, "X");
+        });
+
+        var item = cut.Find(".bb-opt-item");
+        Assert.Equal("<input type=\"text\" class=\"bb-opt-item\" maxlength=\"1\" placeholder=\"X\">", item.OuterHtml);
+
+        cut.SetParametersAndRender(pb =>
+        {
+            pb.Add(a => a.Type, OtpInputType.Password);
+            pb.Add(a => a.PlaceHolder, null);
+        });
+        item = cut.Find(".bb-opt-item");
+        Assert.Equal("<input type=\"password\" class=\"bb-opt-item\" maxlength=\"1\">", item.OuterHtml);
+    }
+
+    [Fact]
+    public async Task TriggerSetValue_Ok()
+    {
+        var v = "123456";
+        var cut = Context.RenderComponent<OtpInput>(pb =>
+        {
+            pb.Add(a => a.Type, OtpInputType.Text);
+            pb.Add(a => a.Value, v);
+            pb.Add(a => a.OnValueChanged, val =>
+            {
+                v = val;
+                return Task.CompletedTask;
+            });
+        });
+
+        await cut.InvokeAsync(() => cut.Instance.TriggerSetValue("234567"));
+        Assert.Equal("234567", v);
+    }
+}
