d31e5597eb806ea6abcae9d805043ec99a66daa6
Argo Zhang <argo@live.ca>
2025-05-01 19:34:04 +0800

    refactor: 增加 getUserMedia 方法

--- src/BootstrapBlazor/wwwroot/modules/media.js
+++ src/BootstrapBlazor/wwwroot/modules/media.js
@@ -1,10 +1,16 @@
 ﻿export async function enumerateDevices() {
     let ret = null;
-    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia || !navigator.mediaDevices.enumerateDevices) {
         console.log("enumerateDevices() not supported.");
     }
     else {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        console.log(stream);
+
         const devices = await navigator.mediaDevices.enumerateDevices();
+        devices.forEach(d => {
+            console.log(d);
+        });
         ret = devices;
     }
     return ret;
