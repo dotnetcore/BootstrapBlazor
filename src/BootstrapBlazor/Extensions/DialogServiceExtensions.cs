db9568fb1d8d679a91cddd8fa2b854d4dcd026a1
Argo Zhang <argo@live.ca>
2025-05-07 10:22:20 +0800

    feat: 增加 Show 泛型扩展方法

--- src/BootstrapBlazor/Extensions/DialogServiceExtensions.cs
+++ src/BootstrapBlazor/Extensions/DialogServiceExtensions.cs
@@ -13,6 +13,25 @@ namespace BootstrapBlazor.Components;
 public static class DialogServiceExtensions
 {
     /// <summary>
+    /// 弹出带结果的对话框
+    /// </summary>
+    /// <param name="service">DialogService 服务实例</param>
+    /// <param name="title">对话框标题，优先级高于 <see cref="DialogOption.Title"/></param>
+    /// <param name="parameters">TCom 组件所需要的参数集合</param>
+    /// <param name="dialog">指定弹窗组件 默认为 null 使用 <see cref="BootstrapBlazorRoot"/> 组件内置弹窗组件</param>
+    public static Task Show<TComponent>(this DialogService service, string title, IDictionary<string, object?>? parameters = null, Dialog? dialog = null) where TComponent : IComponent
+    {
+        var option = new DialogOption();
+        if (!string.IsNullOrEmpty(title))
+        {
+            option.Title = title;
+        }
+        option.ShowFooter = false;
+        option.Component = BootstrapDynamicComponent.CreateComponent<TComponent>(parameters);
+        return service.Show(option, dialog);
+    }
+
+    /// <summary>
     /// 弹出搜索对话框
     /// </summary>
     /// <param name="service">DialogService 服务实例</param>
