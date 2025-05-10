26d2878e6bdb5ec902996fad27cdd2edc06ff7b7
Argo Zhang <argo@live.ca>
2025-05-10 10:28:40 +0800

    feat: 增加 TableExportAttribute 标签类

---
+++ src/BootstrapBlazor/Attributes/TableExportAttribute.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// TableExport attribute class
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class TableExportAttribute : Attribute
+{
+    /// <summary>
+    /// Gets or sets whether the current column is ignored when export operation. Default is false. When set to true, the UI will not generate this column.
+    /// </summary>
+    public bool Ignore { get; set; }
+}
