37f261833fde134c8076c7b5aa033b7be699c37e
Argo Zhang <argo@live.ca>
2025-05-12 10:36:29 +0800

    feat: 增加 TableColumnStringFilter 组件

---
+++ src/BootstrapBlazor/Components/Filters/TableColumnStringFilter.razor.cs
@@ -0,0 +1,113 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 
+/// </summary>
+public partial class TableColumnStringFilter
+{
+    private string? _value1;
+    private FilterAction _action1 = FilterAction.Contains;
+    private string? _value2;
+    private FilterAction _action2 = FilterAction.Contains;
+    private IEnumerable<SelectedItem> _items = [];
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _items = new SelectedItem[]
+        {
+            new("Contains", Localizer["Contains"].Value),
+            new("Equal", Localizer["Equal"].Value),
+            new("NotEqual", Localizer["NotEqual"].Value),
+            new("NotContains", Localizer["NotContains"].Value)
+        };
+        Logic = FilterLogic.Or;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override void Reset()
+    {
+        _value1 = null;
+        _value2 = "";
+        _action1 = FilterAction.Contains;
+        _action2 = FilterAction.Contains;
+        Logic = FilterLogic.Or;
+        Count = 0;
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public override FilterKeyValueAction GetFilterConditions()
+    {
+        var filter = new FilterKeyValueAction() { Filters = [] };
+        if (!string.IsNullOrEmpty(_value1))
+        {
+            filter.Filters.Add(new FilterKeyValueAction()
+            {
+                FieldKey = FieldKey,
+                FieldValue = _value1,
+                FilterAction = _action1
+            });
+        }
+
+        if (Count > 0 && !string.IsNullOrEmpty(_value2))
+        {
+            filter.Filters.Add(new FilterKeyValueAction()
+            {
+                FieldKey = FieldKey,
+                FieldValue = _value2,
+                FilterAction = _action2,
+            });
+            filter.FilterLogic = Logic;
+        }
+        return filter;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override async Task SetFilterConditionsAsync(FilterKeyValueAction filter)
+    {
+        FilterKeyValueAction first = filter.Filters?.FirstOrDefault() ?? filter;
+        if (first.FieldValue is string value)
+        {
+            _value1 = value;
+        }
+        else
+        {
+            _value1 = "";
+        }
+        _action1 = first.FilterAction;
+
+        if (filter.Filters != null && filter.Filters.Count == 2)
+        {
+            Count = 1;
+            FilterKeyValueAction second = filter.Filters[1];
+            if (second.FieldValue is string value2)
+            {
+                _value2 = value2;
+            }
+            else
+            {
+                _value2 = "";
+            }
+            _action2 = second.FilterAction;
+            Logic = second.FilterLogic;
+        }
+        await base.SetFilterConditionsAsync(filter);
+    }
+}
