643bdf2e4574ddf5145b8b8c80dfcec8f39c9932
Argo Zhang <argo@live.ca>
2025-05-12 13:22:45 +0800

    doc: 根据最新组件设计重构自定义过滤组件

---
+++ src/BootstrapBlazor.Server/Components/Components/CustomerFilter.razor.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Components;
+
+public partial class CustomerFilter
+{
+    private int? Value;
+
+    private readonly IEnumerable<SelectedItem> _items = new SelectedItem[]
+    {
+        new() { Value = "", Text = "请选择 ..." },
+        new() { Value = "10", Text = "大于 10" },
+        new() { Value = "50", Text = "大于 50" },
+        new() { Value = "100", Text = "大于 100" }
+    };
+
+    /// <summary>
+    /// 重置过滤条件方法
+    /// </summary>
+    public override void Reset()
+    {
+        Value = null;
+        StateHasChanged();
+    }
+
+    /// <summary>
+    /// 生成过滤条件方法
+    /// </summary>
+    /// <returns></returns>
+    public override FilterKeyValueAction GetFilterConditions()
+    {
+        var filter = new FilterKeyValueAction() { Filters = [] };
+        if (Value != null)
+        {
+            filter.Filters.Add(new FilterKeyValueAction()
+            {
+                FieldKey = FieldKey,
+                FieldValue = Value.Value,
+                FilterAction = FilterAction.GreaterThan
+            });
+        }
+        return filter;
+    }
+}
