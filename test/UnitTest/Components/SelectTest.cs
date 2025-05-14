44c3b451124b00b815a62cb83cf44bfca6690b26
Argo Zhang <argo@live.ca>
2025-05-14 17:20:11 +0800

    feat(Select): add IsAutoClearSearchTextWhenCollapsed parameter (#6024)
    * refactor: 重构代码
    
    * feat: 增加 IsAutoClearSearchTextWhenCollapsed 参数
    
    * feat: 增加 OnCollapsed 回调方法
    
    * test: 增加单元测试
    
    * test: 增加单元测试
    
    * doc: 更新文档
    
    * doc: 更新注释文档
    
    * doc: 增加注释
    
    * chore: bump version 9.6.2-beta06

--- test/UnitTest/Components/SelectTest.cs
+++ test/UnitTest/Components/SelectTest.cs
@@ -429,6 +429,34 @@ public class SelectTest : BootstrapBlazorTestBase
     }
 
     [Fact]
+    public async Task OnCollapsed_Ok()
+    {
+        var collapsed = false;
+        var cut = Context.RenderComponent<Select<string>>(pb =>
+        {
+            pb.Add(a => a.OnCollapsed, () => { collapsed = true; return Task.CompletedTask; });
+        });
+        await cut.InvokeAsync(() => cut.Instance.TriggerCollapsed());
+        Assert.True(collapsed);
+    }
+
+    [Fact]
+    public async Task IsAutoClearSearchTextWhenCollapsed_Ok()
+    {
+        var cut = Context.RenderComponent<Select<string>>(pb =>
+        {
+            pb.Add(a => a.ShowSearch, true);
+            pb.Add(a => a.IsAutoClearSearchTextWhenCollapsed, true);
+        });
+
+        await cut.InvokeAsync(() => cut.Instance.TriggerOnSearch("123"));
+        cut.Contains("value=\"123\"");
+
+        await cut.InvokeAsync(() => cut.Instance.TriggerCollapsed());
+        cut.Contains("value=\"\"");
+    }
+
+    [Fact]
     public void Validate_Ok()
     {
         var valid = false;
