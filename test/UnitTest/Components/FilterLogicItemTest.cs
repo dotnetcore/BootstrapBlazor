8ab1b2490fb3c9b57c4b98c1d5a36ac810891ad0
Argo Zhang <argo@live.ca>
2025-05-14 13:12:34 +0800

    test: 增加单元测试

---
+++ test/UnitTest/Components/FilterLogicItemTest.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace UnitTest.Components;
+
+public class FilterLogicItemTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public async Task FilterLogicItem_Ok()
+    {
+        var logic = FilterLogic.And;
+        var cut = Context.RenderComponent<FilterLogicItem>(pb =>
+        {
+            pb.Add(a => a.Logic, FilterLogic.And);
+            pb.Add(a => a.LogicChanged, EventCallback.Factory.Create<FilterLogic>(this, v =>
+            {
+                logic = v;
+            }));
+        });
+
+        var select = cut.FindComponent<Select<FilterLogic>>();
+        await cut.InvokeAsync(() => select.Instance.SetValue(FilterLogic.Or));
+        Assert.Equal(FilterLogic.Or, logic);
+    }
+}
