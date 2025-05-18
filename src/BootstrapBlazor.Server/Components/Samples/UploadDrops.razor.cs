fd3b65db4ab248c493cc87d73cd177e94436c950
Argo Zhang <argo@live.ca>
2025-05-18 10:57:04 +0800

    doc: 更新文档

--- src/BootstrapBlazor.Server/Components/Samples/UploadDrops.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/UploadDrops.razor.cs
@@ -6,21 +6,103 @@
 namespace BootstrapBlazor.Server.Components.Samples;
 
 /// <summary>
-/// DropUpload sampel code
+/// DropUpload sample code
 /// </summary>
 public partial class UploadDrops
 {
     private readonly List<UploadFile> _dropFiles = [];
 
-    private Task OnDropUpload(UploadFile file)
+    private async Task OnDropUpload(UploadFile file)
     {
+        // 模拟保存文件等处理
+        await Task.Delay(1000);
+
+        // 添加文件到集合中，用于 UI 呈现上传列表
         _dropFiles.Add(file);
         StateHasChanged();
-        return Task.CompletedTask;
     }
 
     private List<AttributeItem> GetAttributes() =>
     [
-
+        new()
+        {
+            Name = "IsDirectory",
+            Description = Localizer["UploadsIsDirectory"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new()
+        {
+            Name = "IsMultiple",
+            Description = Localizer["UploadsIsMultiple"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new()
+        {
+            Name = "BodyTemplate",
+            Description = Localizer["UploadsBodyTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "IconTemplate",
+            Description = Localizer["UploadsIconTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "TextTemplate",
+            Description = Localizer["UploadsTextTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "UploadIcon",
+            Description = Localizer["UploadsUploadIcon"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "UploadText",
+            Description = Localizer["UploadsUploadText"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "ShowFooter",
+            Description = Localizer["UploadsShowFooter"],
+            Type = "bool",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new()
+        {
+            Name = "FooterTemplate",
+            Description = Localizer["UploadsFooterTemplate"],
+            Type = "RenderFragment",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new()
+        {
+            Name = "FooterText",
+            Description = Localizer["UploadsFooterText"],
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = " — "
+        }
     ];
 }
