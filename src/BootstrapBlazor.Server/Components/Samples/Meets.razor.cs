5246cce79bb0907add439158136f3b735efd7d38
Argo Zhang <argo@live.ca>
2025-05-25 09:51:03 +0800

    feat(Meeting): add Meeting component (#6092)
    * 添加Meet示例
    
    * 例子中添加更多说明以及支持自定义服务器地址
    
    * doc: 增加菜单
    
    * chore: 增加源码映射关系
    
    * doc: 更新示例
    
    * chore: 更新依赖
    
    * refactor: 精简代码
    
    ---------
    
    Co-authored-by: jx <jx@jvxiang.com>

---
+++ src/BootstrapBlazor.Server/Components/Samples/Meets.razor.cs
@@ -0,0 +1,56 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// Meet 视频会议组件示例
+/// </summary>
+public partial class Meets : ComponentBase
+{
+    private MeetOption? _option;
+    private Meet? _meet;
+    private readonly string _domain = "meet.jit.si";
+
+    [Inject, NotNull]
+    private ToastService? ToastService { get; set; }
+
+    /// <summary>
+    /// <inheritdoc />
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _option = new MeetOption
+        {
+            RoomName = "BootstrapBlazor",
+            Width = "100%",
+            Height = 700,
+            ConfigOverwrite = new
+            {
+                Lobby = new { EnableChat = false },
+                HiddenPremeetingButtons = new string[] { "invite" },
+                DisableInviteFunctions = true,
+                ButtonsWithNotifyClick = new[] { new { key = "invite", preventExecution = true } }
+            },
+            UserInfo = new UserInfo() { DisplayName = "BootstrapBlazor", Email = "a@blazor.zone" }
+        };
+    }
+
+    private void OnLoad()
+    {
+        ToastService.Information("Meet 示例", "会议室加载完成");
+    }
+
+    private async Task RunCommand()
+    {
+        if (_meet != null)
+        {
+            await _meet.ExecuteCommand("toggleChat");
+        }
+    }
+}
+
