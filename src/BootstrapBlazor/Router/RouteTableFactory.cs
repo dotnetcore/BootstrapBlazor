062679bcf81d6b7b8678f949541346da9b1b8158
Argo Zhang <argo@live.ca>
2025-06-12 08:41:51 +0800

    refactor: 移除 RouteTabFactory 老代码

--- src/BootstrapBlazor/Router/RouteTableFactory.cs
+++ src/BootstrapBlazor/Router/RouteTableFactory.cs
@@ -8,46 +8,6 @@ using System.Reflection;
 namespace BootstrapBlazor.Components;
 
 [ExcludeFromCodeCoverage]
-#if NET5_0
-internal static class RouteTableFactory
-{
-    [NotNull]
-    private static Microsoft.AspNetCore.Components.Routing.IRouteTable? Routes { get; set; }
-    private static readonly HashSet<Assembly> _assemblies = new();
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="assemblies"></param>
-    /// <param name="url"></param>
-    /// <returns></returns>
-    public static RouteContext Create(IEnumerable<Assembly>? assemblies, string url)
-    {
-        RefreshRouteTable(assemblies);
-        if (url.IndexOf("?") > 0) url = url[..url.IndexOf("?")];
-        var routeContext = new Microsoft.AspNetCore.Components.Routing.RouteContext(url);
-        Routes.Route(routeContext);
-        return new RouteContext()
-        {
-            Handler = routeContext.Handler,
-            Parameters = routeContext.Parameters,
-            Segments = routeContext.Segments
-        };
-    }
-
-    private static void RefreshRouteTable(IEnumerable<Assembly>? assemblies)
-    {
-        assemblies ??= Enumerable.Empty<Assembly>();
-        var assembliesSet = new HashSet<Assembly>(assemblies);
-        if (!_assemblies.SetEquals(assembliesSet))
-        {
-            Routes = Microsoft.AspNetCore.Components.Routing.RouteTableFactory.Create(assemblies);
-            _assemblies.Clear();
-            _assemblies.UnionWith(assembliesSet);
-        }
-    }
-}
-#else
 internal static class RouteTableFactory
 {
     [NotNull]
@@ -85,4 +45,3 @@ internal static class RouteTableFactory
         }
     }
 }
-#endif
