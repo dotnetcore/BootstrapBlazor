712d37bec9593d758e2b3423e386f9d270846713
Argo Zhang <argo@live.ca>
2025-06-13 12:18:05 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/FlipClocks.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/FlipClocks.razor.cs
@@ -52,6 +52,14 @@ public partial class FlipClocks
     [
         new()
         {
+            Name = nameof(FlipClock.ShowYear),
+            Description = Localizer["ShowYear_Description"],
+            Type = "boolean",
+            ValueList = "true|false",
+            DefaultValue = "false"
+        },
+        new()
+        {
             Name = nameof(FlipClock.ShowMonth),
             Description = Localizer["ShowMonth_Description"],
             Type = "boolean",
