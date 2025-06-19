056147df64d12cbcec729787f6d21f3482aa431d
Argo Zhang <argo@live.ca>
2025-06-19 10:24:56 +0800

    test: 更新单元测试

--- test/UnitTest/Services/TcpSocketFactoryTest.cs
+++ test/UnitTest/Services/TcpSocketFactoryTest.cs
@@ -4,7 +4,6 @@
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
 using Microsoft.Extensions.Logging;
-using System.Buffers;
 using System.Net;
 using System.Net.Sockets;
 
@@ -12,44 +11,6 @@ namespace UnitTest.Services;
 
 public class TcpSocketFactoryTest
 {
-    [Fact]
-    public async Task TcpSocketFactory_Ok()
-    {
-        var server = StartTcpServer();
-
-        var sc = new ServiceCollection();
-        sc.AddLogging(builder =>
-        {
-            builder.AddProvider(new MockLoggerProvider());
-        });
-        sc.AddBootstrapBlazorTcpSocketFactory();
-
-        var provider = sc.BuildServiceProvider();
-        var factory = provider.GetRequiredService<ITcpSocketFactory>();
-        var client = factory.GetOrCreate("localhost", 0);
-
-        // 测试 ConnectAsync 方法
-        var connect = await client.ConnectAsync("localhost", 8888);
-        Assert.True(connect);
-        Assert.True(client.IsConnected);
-
-        // 增加数据库处理适配器
-        client.SetDataHandler(new MockDataHandler()
-        {
-            //ReceivedCallBack = buffer =>
-            //{
-            //}
-        });
-
-        // 测试 SendAsync 方法
-        var data = new Memory<byte>([1, 2, 3, 4, 5]);
-        var result = await client.SendAsync(data);
-        Assert.True(result);
-
-        //Assert.Equal(buffer.ToArray(), [1, 2, 3, 4, 5, 1, 2]);
-        StopTcpServer(server);
-    }
-
     [Fact]
     public void GetOrCreate_Ok()
     {
@@ -71,9 +32,32 @@ public class TcpSocketFactoryTest
     }
 
     [Fact]
+    public async Task ConnectAsync_Cancel()
+    {
+        var client = CreateClient();
+
+        // 测试 ConnectAsync 方法连接取消逻辑
+        var cst = new CancellationTokenSource();
+        cst.Cancel();
+        var connect = await client.ConnectAsync("localhost", 9999, cst.Token);
+        Assert.False(connect);
+    }
+
+    [Fact]
+    public async Task ConnectAsync_Failed()
+    {
+        var client = CreateClient();
+
+        // 测试 ConnectAsync 方法连接失败
+        var connect = await client.ConnectAsync("localhost", 9999);
+        Assert.False(connect);
+    }
+
+    [Fact]
     public async Task FixLengthDataPackageHandler_Ok()
     {
-        var server = StartDataPackageTcpServer();
+        var port = 8888;
+        var server = StartTcpServer(port, MockSplitPackageAsync);
 
         var sc = new ServiceCollection();
         sc.AddLogging(builder =>
@@ -87,13 +71,14 @@ public class TcpSocketFactoryTest
         var client = factory.GetOrCreate("localhost", 0);
 
         // 测试 ConnectAsync 方法
-        var connect = await client.ConnectAsync("localhost", 8899);
+        var connect = await client.ConnectAsync("localhost", port);
         Assert.True(connect);
         Assert.True(client.IsConnected);
 
         var tcs = new TaskCompletionSource();
         Memory<byte> receivedBuffer = Memory<byte>.Empty;
-        // 增加数据库处理适配器
+
+        // 增加数据处理适配器
         client.SetDataHandler(new FixLengthDataPackageHandler(7)
         {
             ReceivedCallBack = buffer =>
@@ -112,86 +97,139 @@ public class TcpSocketFactoryTest
         await tcs.Task;
         Assert.Equal(receivedBuffer.ToArray(), [1, 2, 3, 4, 5, 3, 4]);
 
+        // 模拟延时等待内部继续读取逻辑完成，测试内部 _receiveCancellationTokenSource 取消逻辑
+        await Task.Delay(10);
+
         // 关闭连接
-        await Task.Delay(200);
         client.Close();
-        await Task.Delay(100);
         StopTcpServer(server);
     }
 
-    private static TcpListener StartTcpServer(int port = 8888)
+    [Fact]
+    public async Task FixLengthDataPackageHandler_Sticky()
+    {
+        var port = 8899;
+        var server = StartTcpServer(port, MockStickyPackageAsync);
+
+        var sc = new ServiceCollection();
+        sc.AddLogging(builder =>
+        {
+            builder.AddProvider(new MockLoggerProvider());
+        });
+        sc.AddBootstrapBlazorTcpSocketFactory();
+
+        var provider = sc.BuildServiceProvider();
+        var factory = provider.GetRequiredService<ITcpSocketFactory>();
+        var client = factory.GetOrCreate("localhost", 0);
+
+        // 连接 TCP Server
+        var connect = await client.ConnectAsync("localhost", port);
+
+        var tcs = new TaskCompletionSource();
+        Memory<byte> receivedBuffer = Memory<byte>.Empty;
+
+        // 增加数据库处理适配器
+        client.SetDataHandler(new FixLengthDataPackageHandler(7)
+        {
+            ReceivedCallBack = buffer =>
+            {
+                receivedBuffer = buffer;
+                tcs.SetResult();
+                return Task.CompletedTask;
+            }
+        });
+
+        // 发送数据
+        var data = new Memory<byte>([1, 2, 3, 4, 5]);
+        await client.SendAsync(data);
+
+        // 等待接收数据处理完成
+        await tcs.Task;
+
+        // 验证接收到的数据
+        Assert.Equal(receivedBuffer.ToArray(), [1, 2, 3, 4, 5, 3, 4]);
+
+        // 等待第二次数据
+        receivedBuffer = Memory<byte>.Empty;
+        tcs = new TaskCompletionSource();
+        await tcs.Task;
+
+        // 验证第二次收到的数据
+        Assert.Equal(receivedBuffer.ToArray(), [1, 2, 3, 4, 5, 6, 7]);
+
+        // 关闭连接
+        client.Close();
+        StopTcpServer(server);
+    }
+
+    private static TcpListener StartTcpServer(int port, Func<TcpClient, Task> handler)
     {
         var server = new TcpListener(IPAddress.Loopback, port);
         server.Start();
-        Task.Run(AcceptClientsAsync);
+        Task.Run(() => AcceptClientsAsync(server, handler));
         return server;
+    }
 
-        async Task AcceptClientsAsync()
+    private static async Task AcceptClientsAsync(TcpListener server, Func<TcpClient, Task> handler)
+    {
+        while (true)
         {
-            while (true)
-            {
-                var client = await server.AcceptTcpClientAsync();
-                _ = Task.Run(() => HandleClientAsync(client));
-            }
-        }
-
-        async Task HandleClientAsync(TcpClient client)
-        {
-            using var stream = client.GetStream();
-            while (true)
-            {
-                var buffer = new byte[10240];
-                var len = await stream.ReadAsync(buffer);
-                if (len == 0)
-                {
-                    break;
-                }
-
-                // 回写数据到客户端
-                var block = new Memory<byte>(buffer, 0, len);
-                await stream.WriteAsync(block, CancellationToken.None);
-            }
+            var client = await server.AcceptTcpClientAsync();
+            _ = Task.Run(() => handler(client));
         }
     }
 
-    private static TcpListener StartDataPackageTcpServer(int port = 8899)
+    private static async Task MockSplitPackageAsync(TcpClient client)
     {
-        var server = new TcpListener(IPAddress.Loopback, port);
-        server.Start();
-        Task.Run(AcceptClientsAsync);
-        return server;
-
-        async Task AcceptClientsAsync()
+        using var stream = client.GetStream();
+        while (true)
         {
-            while (true)
+            var buffer = new byte[10240];
+            var len = await stream.ReadAsync(buffer);
+            if (len == 0)
             {
-                var client = await server.AcceptTcpClientAsync();
-                _ = Task.Run(() => HandleClientAsync(client));
+                break;
             }
+
+            // 回写数据到客户端
+            var block = new Memory<byte>(buffer, 0, len);
+            await stream.WriteAsync(block, CancellationToken.None);
+
+            // 模拟延时
+            await Task.Delay(50);
+
+            // 模拟拆包发送第二段数据
+            await stream.WriteAsync(new byte[] { 0x3, 0x4 }, CancellationToken.None);
         }
+    }
 
-        async Task HandleClientAsync(TcpClient client)
+    private static async Task MockStickyPackageAsync(TcpClient client)
+    {
+        using var stream = client.GetStream();
+        while (true)
         {
-            using var stream = client.GetStream();
-            while (true)
+            var buffer = new byte[10240];
+            var len = await stream.ReadAsync(buffer);
+            if (len == 0)
             {
-                var buffer = new byte[10240];
-                var len = await stream.ReadAsync(buffer);
-                if (len == 0)
-                {
-                    break;
-                }
-
-                // 回写数据到客户端
-                var block = new Memory<byte>(buffer, 0, len);
-                await stream.WriteAsync(block, CancellationToken.None);
-
-                // 模拟延时
-                await Task.Delay(50);
-
-                // 模拟拆包发送第二段数据
-                await stream.WriteAsync(new byte[2] { 0x3, 0x4 }, CancellationToken.None);
+                break;
             }
+
+            // 回写数据到客户端
+            var block = new Memory<byte>(buffer, 0, len);
+            await stream.WriteAsync(block, CancellationToken.None);
+
+            // 模拟延时
+            await Task.Delay(50);
+
+            // 模拟拆包发送第二段数据
+            await stream.WriteAsync(new byte[] { 0x3, 0x4, 0x1, 0x2 }, CancellationToken.None);
+
+            // 模拟延时
+            await Task.Delay(50);
+
+            // 模拟粘包发送后续数据
+            await stream.WriteAsync(new byte[] { 0x3, 0x4, 0x5, 0x6, 0x7 }, CancellationToken.None);
         }
     }
 
@@ -200,16 +238,18 @@ public class TcpSocketFactoryTest
         server?.Stop();
     }
 
-    class MockDataHandler : DataPackageHandlerBase
+    private static ITcpSocketClient CreateClient()
     {
-        public override Task<Memory<byte>> ReceiveAsync(Memory<byte> data)
+        var sc = new ServiceCollection();
+        sc.AddLogging(builder =>
         {
-            using var buffer = MemoryPool<byte>.Shared.Rent(data.Length + 2);
-            data.CopyTo(buffer.Memory);
-            buffer.Memory.Span[data.Length] = 0x01;
-            buffer.Memory.Span[data.Length + 1] = 0x02;
-            return Task.FromResult(buffer.Memory[..(data.Length + 2)]);
-        }
+            builder.AddProvider(new MockLoggerProvider());
+        });
+        sc.AddBootstrapBlazorTcpSocketFactory();
+        var provider = sc.BuildServiceProvider();
+        var factory = provider.GetRequiredService<ITcpSocketFactory>();
+        var client = factory.GetOrCreate("localhost", 0);
+        return client;
     }
 
     class MockLoggerProvider : ILoggerProvider
