712e0dc4cda398f3e5dfd12bc9804f052f14dc62
Argo Zhang <argo@live.ca>
2025-06-20 12:15:39 +0800

    test: 增加 TouchSocket 单元测试
    实现功能后单元测试应该可以通过的

---
+++ test/UnitTest/Services/TouchSocketTest.cs
@@ -0,0 +1,150 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UnitTest.Services;
+
+public class TouchSocketTest
+{
+    [Fact]
+    public async Task FixLengthDataPackageHandler_Ok()
+    {
+        var port = 8884;
+        var server = StartTcpServer(port, MockSplitPackageAsync);
+        var client = CreateClient();
+
+        // 测试 ConnectAsync 方法
+        var connect = await client.ConnectAsync("localhost", port);
+        Assert.True(connect);
+        Assert.True(client.IsConnected);
+
+        var tcs = new TaskCompletionSource();
+        ReadOnlyMemory<byte> receivedBuffer = ReadOnlyMemory<byte>.Empty;
+
+        // 增加数据处理适配器
+        client.SetDataHandler(new FixLengthDataPackageHandler(7)
+        {
+            ReceivedCallBack = buffer =>
+            {
+                receivedBuffer = buffer;
+                tcs.SetResult();
+                return ValueTask.CompletedTask;
+            }
+        });
+
+        // 测试 SendAsync 方法
+        var data = new ReadOnlyMemory<byte>([1, 2, 3, 4, 5]);
+        var result = await client.SendAsync(data);
+        Assert.True(result);
+
+        await tcs.Task;
+        Assert.Equal(receivedBuffer.ToArray(), [1, 2, 3, 4, 5, 3, 4]);
+
+        // 模拟延时等待内部继续读取逻辑完成，测试内部 _receiveCancellationTokenSource 取消逻辑
+        await Task.Delay(10);
+
+        // 关闭连接
+        client.Close();
+        StopTcpServer(server);
+    }
+
+    private static async Task MockSplitPackageAsync(TcpClient client)
+    {
+        using var stream = client.GetStream();
+        while (true)
+        {
+            var buffer = new byte[10240];
+            var len = await stream.ReadAsync(buffer);
+            if (len == 0)
+            {
+                break;
+            }
+
+            // 回写数据到客户端
+            var block = new ReadOnlyMemory<byte>(buffer, 0, len);
+            await stream.WriteAsync(block, CancellationToken.None);
+
+            // 模拟延时
+            await Task.Delay(50);
+
+            // 模拟拆包发送第二段数据
+            await stream.WriteAsync(new byte[] { 0x3, 0x4 }, CancellationToken.None);
+        }
+    }
+
+    private static TcpListener StartTcpServer(int port, Func<TcpClient, Task> handler)
+    {
+        var server = new TcpListener(IPAddress.Loopback, port);
+        server.Start();
+        Task.Run(() => AcceptClientsAsync(server, handler));
+        return server;
+    }
+
+    private static async Task AcceptClientsAsync(TcpListener server, Func<TcpClient, Task> handler)
+    {
+        while (true)
+        {
+            var client = await server.AcceptTcpClientAsync();
+            _ = Task.Run(() => handler(client));
+        }
+    }
+
+    private static void StopTcpServer(TcpListener server)
+    {
+        server?.Stop();
+    }
+
+    private static ITcpSocketClient CreateClient()
+    {
+        var sc = new ServiceCollection();
+        sc.AddLogging(builder =>
+        {
+            builder.AddProvider(new MockLoggerProvider());
+        });
+        sc.AddBootstrapBlazorTcpSocketFactory();
+
+        // 增加 TouchSocket 服务
+        sc.AddBootstrapBlazorTouchSocketService();
+
+        var provider = sc.BuildServiceProvider();
+        var factory = provider.GetRequiredService<ITcpSocketFactory>();
+        var client = factory.GetOrCreate("localhost", 0);
+        return client;
+    }
+
+    class MockLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new MockLogger();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+    class MockLogger : ILogger
+    {
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+
+        }
+    }
+}
