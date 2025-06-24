4749f5146a6b78844e5bfd4696cecc2a8162feb7
Argo Zhang <argo@live.ca>
2025-06-24 18:08:17 +0800

    fix: 修复关闭标签页导致后续标签页重绘问题

--- src/BootstrapBlazor/Components/Tab/Tab.razor.cs
+++ src/BootstrapBlazor/Components/Tab/Tab.razor.cs
@@ -486,6 +486,10 @@ public partial class Tab
 
     private bool IsPreventDefault => _contextMenuZone != null;
 
+    private static string? GetTabItemClassString(TabItem item) => CssBuilder.Default("tabs-body-content")
+        .AddClass("d-none", item is { IsActive: false })
+        .Build();
+
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
