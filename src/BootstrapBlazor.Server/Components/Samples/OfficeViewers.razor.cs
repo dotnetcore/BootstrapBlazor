72ba8770b735e6c37c0781e477b62bfb4b621e71
Argo Zhang <argo@live.ca>
2025-06-24 10:09:43 +0800

    wip: 临时提交

---
+++ src/BootstrapBlazor.Server/Components/Samples/OfficeViewers.razor.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// PdfViewers
+/// </summary>
+public partial class OfficeViewers
+{
+    [Inject, NotNull]
+    private ToastService? ToastService { get; set; }
+
+    private List<SelectedItem> _docs = [
+        new SelectedItem("https://www.blazor.zone/samples/sample.docx", "sample"),
+        ]
+
+    private Task OnLoaded() => ToastService.Success("Office Documentation Viewer", Localizer["OfficeViewerToastSuccessfulContent"]);
+}
