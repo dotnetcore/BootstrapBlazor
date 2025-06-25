7044c95de79c550f12459ab3a3fc79315f12fd5c
Argo Zhang <argo@live.ca>
2025-06-25 08:57:34 +0800

    wip: 临时提交

---
+++ src/BootstrapBlazor/Components/Filters/NotSupportFilter.razor.cs
@@ -0,0 +1,33 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NotSupportFilter component
+/// </summary>
+public partial class NotSupportFilter
+{
+    private string? FilterRowClassString => CssBuilder.Default("filter-row")
+        .AddClass("active", TableColumnFilter.HasFilter())
+        .Build();
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public override FilterKeyValueAction GetFilterConditions()
+    {
+        return new();
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public override async Task SetFilterConditionsAsync(FilterKeyValueAction filter)
+    {
+        await base.SetFilterConditionsAsync(filter);
+    }
+}
