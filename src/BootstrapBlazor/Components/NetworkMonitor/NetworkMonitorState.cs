edb44d16c473073025893841c733b735cc7a813e
Argo Zhang <argo@live.ca>
2025-07-12 20:12:30 +0800

    refactor: 增加 NetworkMonitor 组件

---
+++ src/BootstrapBlazor/Components/NetworkMonitor/NetworkMonitorState.cs
@@ -0,0 +1,41 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 网络状态信息类
+/// </summary>
+public class NetworkMonitorState
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether the network is online
+    /// </summary>
+    public bool IsOnline { get; set; }
+
+    /// <summary>
+    /// Gets or sets the current network type
+    /// </summary>
+    public string? NetworkType { get; set; }
+
+    /// <summary>
+    /// Gets or sets the downlink speed in megabits per second (Mbps).
+    /// </summary>
+    public double? Downlink { get; set; }
+
+    /// <summary>
+    /// Gets or sets the round-trip time (RTT) in milliseconds.
+    /// </summary>
+    public int RTT { get; set; }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"IsOnline: {IsOnline} NetworkType: {NetworkType} Downlink: {Downlink} RTT: {RTT}";
+    }
+}
---
+++ src/BootstrapBlazor/wwwroot/modules/net.js
@@ -0,0 +1,38 @@
+﻿import Data from "./data.js"
+import EventHandler from "./event-handler.js";
+
+export function init(id, options) {
+    const { invoke, onlineStateChangedCallback, onNetworkStateChangedCallback } = options;
+    navigator.connection.onchange = e => {
+        const nt = e.target;
+        const { downlink, effectiveType, rtt } = nt;
+        invoke.invokeMethodAsync(onNetworkStateChangedCallback, {
+            downlink, networkType: effectiveType, rTT: rtt
+        });
+    }
+
+    const onlineStateChanged = () => {
+        invoke.invokeMethodAsync(onlineStateChangedCallback, true);
+    }
+    const offlineStateChanged = () => {
+        invoke.invokeMethodAsync(onlineStateChangedCallback, false);
+    }
+    EventHandler.on(window, 'online', onlineStateChanged);
+    EventHandler.on(window, 'offline', offlineStateChanged);
+
+    Data.set(id, {
+        onlineStateChanged,
+        offlineStateChanged
+    })
+}
+
+export async function dispose(id) {
+    var nt = Data.get(id);
+    Data.remove(id);
+
+    if (nt) {
+        const { onlineStateChanged, offlineStateChanged } = nt;
+        EventHandler.off(window, 'online', onlineStateChanged);
+        EventHandler.off(window, 'offline', offlineStateChanged);
+    }
+}
