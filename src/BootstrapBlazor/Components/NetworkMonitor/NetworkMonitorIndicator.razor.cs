af6bbb28d21c857c8121c2e7cb0fc9496a551011
Argo Zhang <argo@live.ca>
2025-07-12 21:13:09 +0800

    feat: 增加网络状态指示器组件

---
+++ src/BootstrapBlazor/Components/NetworkMonitor/NetworkMonitorIndicator.razor.cs
@@ -0,0 +1,72 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// Represents a network monitor indicator with customizable tooltip settings.
+/// </summary>
+/// <remarks>This component allows you to configure the text, placement, and trigger behavior of a tooltip that
+/// appears when interacting with the network monitor indicator. The tooltip can be customized to provide additional
+/// information to users.</remarks>
+public partial class NetworkMonitorIndicator
+{
+    /// <summary>
+    /// 获得/设置 Popover 弹窗标题 默认为 null
+    /// </summary>
+    [Parameter]
+    public string? Title { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Popover 显示位置 默认为 Top
+    /// </summary>
+    [Parameter]
+    public Placement PopoverPlacement { get; set; } = Placement.Top;
+
+    /// <summary>
+    /// 获得/设置 Popover 触发方式 默认为 hover focus
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public string? Trigger { get; set; }
+
+    private NetworkMonitorState _state = new();
+    private List<string> _indicators = [];
+
+    private string? ClassString => CssBuilder.Default("bb-nt-indicator")
+        .AddClass("bb-nt-indicator-4g", _state.NetworkType == "4g")
+        .AddClass("bb-nt-indicator-3g", _state.NetworkType == "3g")
+        .AddClass("bb-nt-indicator-2g", _state.NetworkType == "2g")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        _indicators.Add(Id);
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        Trigger ??= "hover focus";
+        Title ??= "网络状态";
+    }
+
+    private Task OnNetworkStateChanged(NetworkMonitorState state)
+    {
+        _state = state;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+}
