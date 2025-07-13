7287e74c1dadd3b128720b094aea33d489e7f669
Argo Zhang <argo@live.ca>
2025-07-13 16:38:04 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/NetworkMonitors.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/NetworkMonitors.razor.cs
@@ -8,13 +8,43 @@ namespace BootstrapBlazor.Server.Components.Samples;
 /// <summary>
 /// NetworkMonitor Sample code
 /// </summary>
-public partial class NetworkMonitors
+public partial class NetworkMonitors : IDisposable
 {
+    [Inject, NotNull]
+    private INetworkMonitorService? NetworkMonitorService { get; set; }
+
     private ConsoleLogger _logger = null!;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        await NetworkMonitorService.RegisterStateChangedCallback(this, OnNetworkStateChanged);
+    }
 
     private Task OnNetworkStateChanged(NetworkMonitorState state)
     {
-        _logger.Log($"Online: {state.IsOnline} NetworkType: {state.NetworkType} Downlink: {state.Downlink} RTT: {state.RTT}");
+        _logger.Log($"Online: NetworkType: {state.NetworkType} Downlink: {state.Downlink} RTT: {state.RTT}");
         return Task.CompletedTask;
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            NetworkMonitorService.UnregisterStateChangedCallback(this);
+        }
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
     }
 }
