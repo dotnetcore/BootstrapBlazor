e0f10c09d5aaea188ac2c1f01a2ea79918937468
Argo Zhang <argo@live.ca>
2025-07-23 09:59:19 +0800

    doc(Template5): add transition animation (#6469)
    * refactor: 移除广告位
    
    * feat: 增加过渡动画
    
    * doc: 增加打包文件

---
+++ src/BootstrapBlazor.Server/Components/Samples/Tutorials/LoginAndRegister/Template5.razor.js
@@ -0,0 +1,47 @@
+﻿export function init(id) {
+    const el = document.getElementById(id);
+    if (el) {
+        const email = el.querySelector('.login-item-email');
+        if (email) {
+            email.classList.add('show');
+        }
+    }
+}
+
+export function go(id) {
+    const el = document.getElementById(id);
+    if (el) {
+        const email = el.querySelector('.login-item-email');
+        if (email) {
+            email.classList.remove('show');
+            email.classList.remove('animate-fade-in')
+            email.classList.add('animate-fade-out');
+        }
+
+        const password = el.querySelector('.login-item-password');
+        if (password) {
+            password.classList.add('show');
+            password.classList.add('animate-fade-in')
+            password.classList.remove('animate-fade-out')
+        }
+    }
+}
+
+export function back(id) {
+    const el = document.getElementById(id);
+    if (el) {
+        const email = el.querySelector('.login-item-email');
+        if (email) {
+            email.classList.add('show');
+            email.classList.remove('animate-fade-out');
+            email.classList.add('animate-fade-in');
+        }
+
+        const password = el.querySelector('.login-item-password');
+        if (password) {
+            password.classList.remove('show');
+            password.classList.remove('animate-fade-in')
+            password.classList.add('animate-fade-out')
+        }
+    }
+}
