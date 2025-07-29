6fbacbcaf5f06f27ec72fc9c067f8793d57933be
Argo Zhang <argo@live.ca>
2025-07-29 11:25:09 +0800

    fix(MultiSelectGeneric): update value when bind-Value changed (#6505)
    * feat: 增加 IModelEqualityComparer 接口
    
    * chore: bump version 9.8.3-beta01

--- src/BootstrapBlazor/Components/SelectGeneric/MultiSelectGeneric.razor.cs
+++ src/BootstrapBlazor/Components/SelectGeneric/MultiSelectGeneric.razor.cs
@@ -12,7 +12,7 @@ namespace BootstrapBlazor.Components;
 /// MultiSelectGeneric component
 /// </summary>
 [ExcludeFromCodeCoverage]
-public partial class MultiSelectGeneric<TValue>
+public partial class MultiSelectGeneric<TValue> : IModelEqualityComparer<TValue>
 {
     private List<SelectedItem<TValue>> SelectedItems { get; } = [];
 
@@ -189,11 +189,29 @@ public partial class MultiSelectGeneric<TValue>
     [Parameter]
     public RenderFragment<SelectedItem<TValue>>? ItemTemplate { get; set; }
 
+    /// <summary>
+    /// 获得/设置 比较数据是否相同回调方法 默认为 null
+    /// <para>提供此回调方法时忽略 <see cref="CustomKeyAttribute"/> 属性</para>
+    /// </summary>
+    [Parameter]
+    public Func<TValue, TValue, bool>? ValueEqualityComparer { get; set; }
+
+    Func<TValue, TValue, bool>? IModelEqualityComparer<TValue>.ModelEqualityComparer
+    {
+        get => ValueEqualityComparer;
+        set => ValueEqualityComparer = value;
+    }
+
+    /// <summary>
+    /// 获得/设置 数据主键标识标签 默认为 <see cref="KeyAttribute"/>用于判断数据主键标签，如果模型未设置主键时可使用 <see cref="ValueEqualityComparer"/> 参数自定义判断数据模型支持联合主键
+    /// </summary>
+    [Parameter]
+    [NotNull]
+    public Type? CustomKeyAttribute { get; set; } = typeof(KeyAttribute);
+
     [Inject]
     [NotNull]
     private IStringLocalizer<MultiSelect<TValue>>? Localizer { get; set; }
-
-    private string? PlaceholderString => SelectedItems.Count == 0 ? PlaceHolder : null;
 
     private string? ScrollIntoViewBehaviorString => ScrollIntoViewBehavior == ScrollIntoViewBehavior.Smooth
         ? null
@@ -535,4 +553,12 @@ public partial class MultiSelectGeneric<TValue>
             }
         }
     }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public bool Equals(TValue? x, TValue? y) => this.Equals<TValue>(x, y);
 }
