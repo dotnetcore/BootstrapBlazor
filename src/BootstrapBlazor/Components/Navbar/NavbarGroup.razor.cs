0dd4708788b4de4e3866283978c556e2256ab5a1
Argo Zhang <argo@live.ca>
2025-08-08 18:26:20 +0800

    feat: 增加 Group 组件

---
+++ src/BootstrapBlazor/Components/Navbar/NavbarGroup.razor.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NavbarGroup 组件用于在导航栏中分组
+/// </summary>
+public partial class NavbarGroup
+{
+    /// <summary>
+    /// 获得/设置 子组件模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? ClassString => CssBuilder.Default("navbar-nav")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
