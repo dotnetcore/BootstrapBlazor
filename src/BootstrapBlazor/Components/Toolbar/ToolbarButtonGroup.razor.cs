f0c21565c9414335146cd310c4f3066588f867ef
Argo Zhang <argo@live.ca>
2025-08-08 11:47:31 +0800

    refactor: 移除级联参数

--- src/BootstrapBlazor/Components/Toolbar/ToolbarButtonGroup.razor.cs
+++ src/BootstrapBlazor/Components/Toolbar/ToolbarButtonGroup.razor.cs
@@ -8,11 +8,8 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// ToolbarGroup 组件用于在工具栏中添加一组按钮
 /// </summary>
-public partial class ToolbarButtonGroup : IAsyncDisposable
+public partial class ToolbarButtonGroup
 {
-    [CascadingParameter]
-    private Toolbar? Toolbar { get; set; }
-
     /// <summary>
     /// 获得/设置 子组件模板
     /// </summary>
@@ -22,33 +19,4 @@ public partial class ToolbarButtonGroup : IAsyncDisposable
     private string? ClassString => CssBuilder.Default("bb-toolbar-group btn-group")
         .AddClassFromAttributes(AdditionalAttributes)
         .Build();
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    protected override void OnInitialized()
-    {
-        base.OnInitialized();
-
-        Toolbar?.Add(this);
-    }
-
-    private ValueTask DisposeAsync(bool disposing)
-    {
-        if (disposing)
-        {
-            Toolbar?.Remove(this);
-        }
-
-        return ValueTask.CompletedTask;
-    }
-
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    public async ValueTask DisposeAsync()
-    {
-        await DisposeAsync(true);
-        GC.SuppressFinalize(this);
-    }
 }
