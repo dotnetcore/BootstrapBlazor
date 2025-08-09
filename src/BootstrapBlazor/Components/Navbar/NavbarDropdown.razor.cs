21715aaf0513512095fe6e7d00ba058cf794e27d
Argo Zhang <argo@live.ca>
2025-08-09 17:06:47 +0800

    feat: 增加 NavbarDropdown 组件

---
+++ src/BootstrapBlazor/Components/Navbar/NavbarDropdown.razor.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NavbarDropdown 组件用于在导航栏中创建下拉菜单
+/// </summary>
+public partial class NavbarDropdown
+{
+    /// <summary>
+    /// 获得/设置 子组件模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 Dropdown 菜单标题文本
+    /// </summary>
+    [Parameter]
+    public string? Text { get; set; }
+
+    private string? ClassString => CssBuilder.Default("nav-item dropdown")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
