2bb0e8c75a5bd1f7544957b2300629bb5754fa9f
Argo Zhang <argo@live.ca>
2025-08-09 18:42:41 +0800

    refactor: 精简代码

--- src/BootstrapBlazor/Components/Navbar/Navbar.razor.cs
+++ src/BootstrapBlazor/Components/Navbar/Navbar.razor.cs
@@ -3,8 +3,6 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
-using Microsoft.AspNetCore.Components.Forms;
-
 namespace BootstrapBlazor.Components;
 
 /// <summary>
