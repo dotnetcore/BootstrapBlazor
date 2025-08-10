5b575df4a6ab9a5e5b2708436b8b360ff2c4a837
Argo Zhang <argo@live.ca>
2025-08-10 11:58:02 +0800

    chore: add yes tag for install font

--- scripts/linux/deploy.sh
+++ scripts/linux/deploy.sh
@@ -40,7 +40,7 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 yes|sudo apt install ./google-chrome-stable_current_amd64.deb
 
 echo "*********************** install support font ***********************"
-sudo apt install fonts-wqy-microhei
+yes|sudo apt install fonts-wqy-microhei
 
 echo "*********************** install DOTNET ***********************"
 wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
