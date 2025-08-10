e6eec99319bba9e387ef3c632813a3cd8a70d0a2
Argo Zhang <argo@live.ca>
2025-08-10 10:56:58 +0800

    doc: 更新主题加载逻辑防止抖动

---
+++ src/BootstrapBlazor.Server/Components/Layout/BaseLayout.razor.js
@@ -0,0 +1,6 @@
+﻿import { getPreferredTheme, setTheme } from "../../_content/BootstrapBlazor/modules/utility.js"
+
+export function initTheme() {
+    const currentTheme = getPreferredTheme();
+    setTheme(currentTheme, false);
+}
