8e51e3718941c56aabd8fa3dbee4f8a192cc24eb
Argo Zhang <argo@live.ca>
2025-08-11 08:39:32 +0800

    feat(Navbar): add Navbar component (#6586)
    * feat: 增加 Navbar 组件
    
    * feat: 增加 NavBrand 组件
    
    * doc: 增加菜单
    
    * doc: 增加 Navbar 示例页面
    
    * doc: 调整菜单顺序
    
    * refactor: 增加子组件
    
    * doc: 更新示例
    
    * refactor: 更改 Color 默认值
    
    * doc: 更新网站 header 样式
    
    * feat: 增加 ToggleButton 组件
    
    * feat: 增加 Group 组件
    
    * feat: 增加 Collapse 组件
    
    * feat: 增加 Item 组件
    
    * feat: 增加 Brand 组件
    
    * feat: 增加 Navbar 组件
    
    * doc: 更新文档
    
    * style: 增加响应式样式
    
    * doc: 增加注释
    
    * test: 增加单元测试
    
    * doc: 更新注释文档
    
    * refactor: 移除 sealed 关键字
    
    * style: 更新阴影样式
    
    * refactor: 增加 NavbarLink 组件
    
    * test: 更新单元测试
    
    * refactor: 复用 HandlerClick 方法
    
    * doc: 更新示例
    
    * test: 更新单元测试
    
    * feat: 增加 NavbarDropdown 组件
    
    * refactor: 更改 Size 默认值
    
    * doc: 更新示例
    
    * feat: 增加方向与对其方式参数
    
    * doc: 更新示例
    
    * test: 更新单元测试
    
    * refactor: 精简代码
    
    * doc: 更新文档
    
    * refactor: 更新代码消除警告信息
    
    * style: 更新 Dropdown 组件样式
    
    * style: 更新样式
    
    * feat: 增加 IsScrolling 参数
    
    * refactor: 更新示例
    
    * test: 更新单元测试
    
    * feat: 增加高度参数
    
    * style: 增加左右间隙
    
    * doc: 更新文档说明
    
    * refactor: 增加 Home 动画特效

---
+++ src/BootstrapBlazor/Components/Navbar/NavbarCollapse.razor.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NavbarCollapse 组件用于在导航栏中适配响应式布局
+/// </summary>
+public partial class NavbarCollapse
+{
+    /// <summary>
+    /// 获得/设置 子组件模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    private string? ClassString => CssBuilder.Default("collapse navbar-collapse")
+        .AddClassFromAttributes(AdditionalAttributes)
+        .Build();
+}
---
+++ src/BootstrapBlazor/Components/Navbar/NavbarDropdownItem.razor.cs
@@ -0,0 +1,30 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// NavbarDropdownItem 组件用于在导航栏下拉菜单中创建菜单项
+/// </summary>
+public partial class NavbarDropdownItem
+{
+    /// <summary>
+    /// 获得/设置 子组件模板
+    /// </summary>
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    /// <summary>
+    /// 获得/设置 菜单项文本
+    /// </summary>
+    [Parameter]
+    public string? Url { get; set; }
+
+    /// <summary>
+    /// 获得/设置 A 标签 target 参数 默认 null
+    /// </summary>
+    [Parameter]
+    public string? Target { get; set; }
+}
