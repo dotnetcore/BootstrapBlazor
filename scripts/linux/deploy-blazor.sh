4690d1586868e1c34dc88e8a6e999299efb6703f
Argo Zhang <argo@live.ca>
2025-08-12 21:48:32 +0800

    chore: 更改输出目录

--- scripts/linux/deploy-blazor.sh
+++ scripts/linux/deploy-blazor.sh
@@ -10,6 +10,6 @@ curl https://www.blazor.zone/api/dispatch?token=BootstrapBlazor-Reboot
 dotnet publish src/BootstrapBlazor.Server -c Release
 
 systemctl stop ba.blazor
-\cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net9.0/publish/* /usr/local/ba/blazor
+\cp -fr ~/BootstrapBlazor/src/BootstrapBlazor.Server/bin/Release/net10.0/publish/* /usr/local/ba/blazor
 systemctl start ba.blazor
 systemctl status ba.blazor -l --no-pager
