bd3b786740e2c7c951e82821e3fad0b04db6610a
Argo Zhang <argo@live.ca>
2025-08-19 16:04:53 +0800

    feat: 增加示例页面

---
+++ src/BootstrapBlazor.Server/Components/Samples/Dom2Images.razor.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Server.Components.Samples;
+
+/// <summary>
+/// Dom2Image 组件
+/// </summary>
+public partial class Dom2Images
+{
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Foo>? LocalizerFoo { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IHtml2Image? Html2ImageService { get; set; }
+
+    [Inject]
+    [NotNull]
+    private IStringLocalizer<Html2Images>? Localizer { get; set; }
+
+    [NotNull]
+    private List<Foo>? Items { get; set; }
+
+    private string? _imageData;
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        Items = Foo.GenerateFoo(LocalizerFoo);
+    }
+
+    private async Task OnExportAsync()
+    {
+        _imageData = await Html2ImageService.GetDataAsync("#table-9527");
+        StateHasChanged();
+    }
+}
