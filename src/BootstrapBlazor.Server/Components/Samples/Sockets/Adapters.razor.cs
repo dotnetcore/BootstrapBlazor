c5cc5f725bd3014c02a67507596d9fb85f851467
Argo Zhang <argo@live.ca>
2025-08-26 15:17:28 +0800

    doc: 更新扩展方法文档

--- src/BootstrapBlazor.Server/Components/Samples/Sockets/Adapters.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Sockets/Adapters.razor.cs
@@ -25,10 +25,7 @@ public partial class Adapters : IDisposable
     private readonly CancellationTokenSource _connectTokenSource = new();
     private readonly CancellationTokenSource _sendTokenSource = new();
     private readonly CancellationTokenSource _receiveTokenSource = new();
-    private readonly DataPackageAdapter _dataAdapter = new()
-    {
-        DataPackageHandler = new FixLengthDataPackageHandler(12)
-    };
+    private readonly DataPackageAdapter _dataAdapter = new(new FixLengthDataPackageHandler(12));
     private bool _useDataAdapter = true;
 
     /// <summary>
@@ -46,13 +43,14 @@ public partial class Adapters : IDisposable
             // 设置本地使用的 IP地址与端口
             options.LocalEndPoint = new IPEndPoint(IPAddress.Loopback, 0);
         });
-        _client.ReceivedCallBack += OnReceivedAsync;
 
-        _dataAdapter.ReceivedCallBack += async data =>
-        {
-            // 直接处理接收的数据
-            await UpdateReceiveLog(data);
-        };
+        // 此处代码分开写是为了判断 _useDataAdapter 参数
+        _client.ReceivedCallBack += OnReceivedAsync;
+        _dataAdapter.ReceivedCallBack = UpdateReceiveLog;
+
+        // 实战中可以通过下面一句话设置数据适配器与回调方法
+        // _client.SetDataPackageAdapter(_dataAdapter, UpdateReceiveLog);
+
     }
 
     private async Task OnConnectAsync()
@@ -102,7 +100,7 @@ public partial class Adapters : IDisposable
         }
     }
 
-    private async Task UpdateReceiveLog(ReadOnlyMemory<byte> data)
+    private async ValueTask UpdateReceiveLog(ReadOnlyMemory<byte> data)
     {
         var payload = Encoding.UTF8.GetString(data.Span);
         var body = BitConverter.ToString(data.ToArray());
