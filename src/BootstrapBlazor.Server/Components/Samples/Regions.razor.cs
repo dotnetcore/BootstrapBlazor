b9c23a6c5efed1e9589ada80af723fea060a7788
Argo Zhang <argo@live.ca>
2025-10-14 15:19:31 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/Regions.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Regions.razor.cs
@@ -10,10 +10,5 @@ namespace BootstrapBlazor.Server.Components.Samples;
 /// </summary>
 public partial class Regions
 {
-    private string Value = "";
-
-    private void OnClick()
-    {
-        Value = "四川省-自贡市-大安区-团结镇";
-    }
+    private string _value = "";
 }
