ce948adb156f727b15b29a4f8c3883fa46735144
Argo Zhang <argo@live.ca>
2025-10-24 09:07:49 +0800

    revert: 撤销命名空间更改

--- src/BootstrapBlazor/Components/BaseComponents/BootstrapModuleComponentBase.cs
+++ src/BootstrapBlazor/Components/BaseComponents/BootstrapModuleComponentBase.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
+using System.Reflection;
+
 namespace BootstrapBlazor.Components;
 
 /// <summary>
