686d30de50c0d8c14041ec3eb542274cee525e49
Argo Zhang <argo@live.ca>
2025-10-30 19:18:18 +0800

    refactor: 增加 PdfOptions 配置项

---
+++ src/BootstrapBlazor/Options/MarginOptions.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License
+// See the LICENSE file in the project root for more information.
+// Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
+
+namespace BootstrapBlazor.Components;
+
+/// <summary>
+/// 页面边距选项
+/// </summary>
+public record MarginOptions
+{
+    /// <summary>
+    /// Top margin, accepts values labeled with units.
+    /// </summary>
+    public object? Top { get; set; }
+
+    /// <summary>
+    /// Left margin, accepts values labeled with units.
+    /// </summary>
+    public object? Left { get; set; }
+
+    /// <summary>
+    /// Bottom margin, accepts values labeled with units.
+    /// </summary>
+    public object? Bottom { get; set; }
+
+    /// <summary>
+    /// Right margin, accepts values labeled with units.
+    /// </summary>
+    public object? Right { get; set; }
+}
