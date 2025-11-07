3028387b5974fa8f75b53bdbe86beb4aceaed360
Argo Zhang <argo@live.ca>
2025-11-07 10:07:54 +0800

    feat: 增加 CheckboxColumnCompactWidth 参数

--- src/BootstrapBlazor/Options/TableSettings.cs
+++ src/BootstrapBlazor/Options/TableSettings.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -14,6 +14,11 @@ public class TableSettings
     /// 获得/设置 复选框宽度 默认 36
     /// </summary>
     public int CheckboxColumnWidth { get; set; } = 36;
+
+    /// <summary>
+    /// 获得/设置 复选框宽度 默认 28
+    /// </summary>
+    public int CheckboxColumnCompactWidth { get; set; } = 28;
 
     /// <summary>
     /// 获得/设置 明细行 Row Header 宽度 默认 24
