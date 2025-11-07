541b0cc794b23a786ecc8d36588ddb0a5b335674
Argo Zhang <argo@live.ca>
2025-11-07 09:29:43 +0800

    feat: 多选列增加默认居中逻辑

--- src/BootstrapBlazor/Components/Table/Table.razor.cs
+++ src/BootstrapBlazor/Components/Table/Table.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -97,6 +97,7 @@ public partial class Table<TItem> : ITable, IModelEqualityComparer<TItem> where 
         .AddClass("table-resize", AllowResizing)
         .AddClass("table-fixed-body", RenderMode == TableRenderMode.CardView && IsFixedHeader)
         .AddClass("table-striped table-hover", ActiveRenderMode == TableRenderMode.CardView && IsStriped)
+        .AddClass("table-multi", IsMultipleSelect)
         .Build();
 
     private string? FooterClassString => CssBuilder.Default("table-footer")
