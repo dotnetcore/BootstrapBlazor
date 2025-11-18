dc655ec66ac692cd5d52297b76a01dbee862f3b4
Argo Zhang <argo@live.ca>
2025-11-18 14:55:02 +0800

    feat(Mermaid): add DownloadPdfAsync method (#7141)
    * doc: 增加 pdf 导出功能
    
    * feat(Mermaid): add DownloadPdfAsync method

--- src/BootstrapBlazor.Server/Components/Samples/Mermaids.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Mermaids.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -10,6 +10,8 @@ namespace BootstrapBlazor.Server.Components.Samples;
 /// </summary>
 public partial class Mermaids
 {
+    private Mermaid _mermaid = default!;
+
     private MermaidType Options { get; set; } = new();
 
     private string CustomStyleString { get; } = """
@@ -150,6 +152,8 @@ public partial class Mermaids
             """
         }
     };
+
+    private Task OnDownloadPDFAsync() => _mermaid.DownloadPdfAsync($"mermaid-pdf-{DateTime.Now:HHmmss}.pdf");
 
     /// <summary>
     /// GetAttributes
