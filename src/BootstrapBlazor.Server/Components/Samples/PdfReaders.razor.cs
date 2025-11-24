707e9b6f1df61407b67b55fd758339bae8e3ce65
Argo Zhang <argo@live.ca>
2025-11-24 15:10:16 +0800

    feat(PdfReader): redesign PdfReader component (#7171)
    * refactor: 更新示例
    
    * refactor: 更新 header 配置
    
    * chore: 更新 readme 文档
    
    * chore: 更新依赖
    
    * doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/PdfReaders.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/PdfReaders.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -25,7 +25,7 @@ public partial class PdfReaders
 
     private async Task ApplyFilenameStream()
     {
-        await PdfReader!.Refresh();
+        //await PdfReader!.Refresh();
     }
 
     [NotNull]
@@ -81,8 +81,8 @@ public partial class PdfReaders
 
     async Task Refresh()
     {
-        if (AdvancedPdfReader != null)
-            await AdvancedPdfReader.Refresh(Search, Page, PageMode, Zoom, ReadOnly, Watermark);
+        //if (AdvancedPdfReader != null)
+        //await AdvancedPdfReader.Refresh(Search, Page, PageMode, Zoom, ReadOnly, Watermark);
     }
 
     private async Task ApplyPage()
@@ -274,8 +274,8 @@ public partial class PdfReaders
         },
         new()
         {
-            Name = nameof(PdfReader.LocalFileName),
-            Description = Localizer[nameof(PdfReader.LocalFileName)],
+            //Name = nameof(PdfReader.LocalFileName),
+            //Description = Localizer[nameof(PdfReader.LocalFileName)],
             Type = "string",
             ValueList = " — ",
             DefaultValue = " — "
