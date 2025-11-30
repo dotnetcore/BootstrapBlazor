a3479e9548653c06eb8458ae61dbf053d010dc69
Argo Zhang <argo@live.ca>
2025-11-30 09:14:11 +0800

    refactor: 移除 Job 标签

--- tools/Benchmarks/Benmarks.cs
+++ tools/Benchmarks/Benmarks.cs
@@ -4,14 +4,12 @@
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 namespace UnitTest.Benchmarks;
 
-[SimpleJob(RuntimeMoniker.Net90)]
 public class Benchmarks
 {
     static readonly Foo _instance = new();
