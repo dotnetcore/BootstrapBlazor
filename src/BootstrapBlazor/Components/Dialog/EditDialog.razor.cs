30bfd7d44a4d32ffbd7495700f85ecc5e6b01873
Argo Zhang <argo@live.ca>
2025-12-03 12:11:14 +0800

    feat(EditDialog): add IsAsync on save button

--- src/BootstrapBlazor/Components/Dialog/EditDialog.razor.cs
+++ src/BootstrapBlazor/Components/Dialog/EditDialog.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -156,6 +156,7 @@ public partial class EditDialog<TModel>
             builder.AddAttribute(32, nameof(Button.Icon), SaveButtonIcon);
             builder.AddAttribute(33, nameof(Button.Text), SaveButtonText);
             builder.AddAttribute(34, nameof(Button.ButtonType), ButtonType.Submit);
+            builder.AddAttribute(35, nameof(Button.IsAsync), true);
             builder.CloseComponent();
         }
     };
