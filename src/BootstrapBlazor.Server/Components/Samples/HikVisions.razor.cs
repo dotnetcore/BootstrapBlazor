3fb53425bb34c0c589876bbf241b8aff13c36b51
Argo Zhang <argo@live.ca>
2025-12-04 17:14:46 +0800

    chore: 增加示例文档

--- src/BootstrapBlazor.Server/Components/Samples/HikVisions.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/HikVisions.razor.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
 
-
 namespace BootstrapBlazor.Server.Components.Samples;
 
 /// <summary>
@@ -11,21 +10,25 @@ namespace BootstrapBlazor.Server.Components.Samples;
 /// </summary>
 public partial class HikVisions
 {
-    [Inject, NotNull]
-    private IHikVision? HikVision { get; set; }
+    private HikVision _hikVision = default!;
 
-    /// <summary>
-    /// <inheritdoc/>
-    /// </summary>
-    /// <param name="firstRender"></param>
-    /// <returns></returns>
-    protected override async Task OnAfterRenderAsync(bool firstRender)
+    private async Task OnLogin()
     {
-        await base.OnAfterRenderAsync(firstRender);
+        await _hikVision.Login("47.121.113.151", 9980, "admin", "vhbn8888", LoginType.Http);
+    }
 
-        if (firstRender)
-        {
-            await HikVision.Login("47.121.113.151", 9980, "admin", "vhbn8888");
-        }
+    private async Task OnLogout()
+    {
+        await _hikVision.Logout();
+    }
+
+    private async Task OnStartRealPlay()
+    {
+        await _hikVision.StartRealPlay();
+    }
+
+    private async Task OnStopRealPlay()
+    {
+        await _hikVision.StopRealPlay();
     }
 }
