2dc7e9a146b8f89262ab3a8a7006922b990047cc
Argo Zhang <argo@live.ca>
2025-12-04 15:53:48 +0800

    feat(HikVision): add HikVision component (#7245)
    * chore: 增加映射
    
    * feat: 增加海康摄像头服务
    
    * doc: 增加示例文档

--- src/BootstrapBlazor.Server/Extensions/MenusLocalizerExtensions.cs
+++ src/BootstrapBlazor.Server/Extensions/MenusLocalizerExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -730,6 +730,12 @@ internal static class MenusLocalizerExtensions
                 {
                     Text = Localizer["FileViewer"],
                     Url = "file-viewer"
+                },
+                new()
+                {
+                    IsNew = true,
+                    Text = Localizer["HiKVision"],
+                    Url = "hik-vision"
                 },
                 new()
                 {
