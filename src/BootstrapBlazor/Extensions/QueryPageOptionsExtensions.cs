27a7a39724c3fe9df6f730b8c9a2afa5dd9beba6
Argo Zhang <argo@live.ca>
2025-12-12 20:08:13 +0800

    Revert "doc: update test"
    This reverts commit bd61de6016d8d0d6735747daaa3eafaa6ecdc262.

--- src/BootstrapBlazor/Extensions/QueryPageOptionsExtensions.cs
+++ src/BootstrapBlazor/Extensions/QueryPageOptionsExtensions.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -21,7 +21,7 @@ public static class QueryPageOptionsExtensions
     {
         var filter = new FilterKeyValueAction();
 
-        // 处理模糊搜索测试11
+        // 处理模糊搜索
         if (option.Searches.Count != 0)
         {
             filter.Filters.Add(new FilterKeyValueAction()
