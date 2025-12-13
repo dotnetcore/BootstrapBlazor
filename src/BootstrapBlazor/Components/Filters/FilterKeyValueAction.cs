ec4aee02d485f31209d50c9dda28feec319d4021
Argo Zhang <argo@live.ca>
2025-12-13 18:30:14 +0800

    feat: 增加 JsonFilterKeyValueActionConverter 转换器

--- src/BootstrapBlazor/Components/Filters/FilterKeyValueAction.cs
+++ src/BootstrapBlazor/Components/Filters/FilterKeyValueAction.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -10,6 +10,7 @@ namespace BootstrapBlazor.Components;
 /// <summary>
 /// Filter 过滤条件项目 属性 <see cref="Filters"/> 为条件表达式，其之间关系由 <see cref="FilterLogic"/> 来决定
 /// </summary>
+[JsonConverter(typeof(JsonFilterKeyValueActionConverter))]
 public class FilterKeyValueAction
 {
     /// <summary>
