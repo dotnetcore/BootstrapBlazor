bd3102df5bf2c4be8e99d5a5d8f57f473744c891
Argo Zhang <argo@live.ca>
2025-12-18 16:29:49 +0800

    chore: 增加空检查

--- src/BootstrapBlazor/Components/Drawer/Drawer.razor.js
+++ src/BootstrapBlazor/Components/Drawer/Drawer.razor.js
@@ -9,58 +9,63 @@ const initDrag = el => {
     let height = 0;
     let isVertical = false;
     const drawerBody = el.querySelector('.drawer-body');
-    const bar = [...drawerBody.children].find(i => i.classList.contains('drawer-bar'));
-    if (bar != null) {
-        Drag.drag(bar,
-            e => {
-                isVertical = drawerBody.classList.contains("top") || drawerBody.classList.contains("bottom")
-                bar.classList.add('drag')
-                if (isVertical) {
-                    height = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-height'))
-                    originY = e.clientY || e.touches[0].clientY
-                }
-                else {
-                    width = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-width'))
-                    originX = e.clientX || e.touches[0].clientX
-                }
-            },
-            e => {
-                if (isVertical) {
-                    const eventY = e.clientY || (e.touches.length || e.touches.length > 0 && e.touches[0].clientY)
-                    const moveY = eventY - originY
-                    let newHeight = 0;
-                    if (drawerBody.classList.contains("bottom")) {
-                        newHeight = height - moveY
-                    }
-                    else {
-                        newHeight = height + moveY
-                    }
-                    const maxHeight = window.innerHeight;
-                    if (newHeight > 100 && newHeight < maxHeight) {
-                        drawerBody.style.setProperty('--bb-drawer-height', `${newHeight}px`)
-                    }
-                }
-                else {
-                    const eventX = e.clientX || (e.touches.length || e.touches.length > 0 && e.touches[0].clientX)
-                    const moveX = eventX - originX
-                    let newWidth = 0;
-                    if (drawerBody.classList.contains("right")) {
-                        newWidth = width - moveX
-                    }
-                    else {
-                        newWidth = width + moveX
-                    }
-                    const maxWidth = window.innerWidth;
-                    if (newWidth > 100 && newWidth < maxWidth) {
-                        drawerBody.style.setProperty('--bb-drawer-width', `${newWidth}px`)
-                    }
-                }
-            },
-            e => {
-                bar.classList.remove('drag')
-            }
-        )
+    if (drawerBody === null) {
+        return;
     }
+    const bar = [...drawerBody.children].find(i => i.classList.contains('drawer-bar'));
+    if (bar === null) {
+        return;
+    }
+
+    Drag.drag(bar,
+        e => {
+            isVertical = drawerBody.classList.contains("top") || drawerBody.classList.contains("bottom")
+            bar.classList.add('drag')
+            if (isVertical) {
+                height = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-height'))
+                originY = e.clientY || e.touches[0].clientY
+            }
+            else {
+                width = parseInt(getComputedStyle(drawerBody).getPropertyValue('--bb-drawer-width'))
+                originX = e.clientX || e.touches[0].clientX
+            }
+        },
+        e => {
+            if (isVertical) {
+                const eventY = e.clientY || (e.touches.length || e.touches.length > 0 && e.touches[0].clientY)
+                const moveY = eventY - originY
+                let newHeight = 0;
+                if (drawerBody.classList.contains("bottom")) {
+                    newHeight = height - moveY
+                }
+                else {
+                    newHeight = height + moveY
+                }
+                const maxHeight = window.innerHeight;
+                if (newHeight > 100 && newHeight < maxHeight) {
+                    drawerBody.style.setProperty('--bb-drawer-height', `${newHeight}px`)
+                }
+            }
+            else {
+                const eventX = e.clientX || (e.touches.length || e.touches.length > 0 && e.touches[0].clientX)
+                const moveX = eventX - originX
+                let newWidth = 0;
+                if (drawerBody.classList.contains("right")) {
+                    newWidth = width - moveX
+                }
+                else {
+                    newWidth = width + moveX
+                }
+                const maxWidth = window.innerWidth;
+                if (newWidth > 100 && newWidth < maxWidth) {
+                    drawerBody.style.setProperty('--bb-drawer-width', `${newWidth}px`)
+                }
+            }
+        },
+        e => {
+            bar.classList.remove('drag')
+        }
+    )
 }
 
 export function init(id, invoke, method) {
@@ -178,8 +183,10 @@ export function dispose(id) {
     }
 
     const drawerBody = el.querySelector('.drawer-body');
-    const bar = [...drawerBody.children].find(i => i.classList.contains('drawer-bar'));
-    if (bar) {
-        Drag.dispose(bar)
+    if (drawerBody !== null) {
+        const bar = [...drawerBody.children].find(i => i.classList.contains('drawer-bar'));
+        if (bar) {
+            Drag.dispose(bar)
+        }
     }
 }
