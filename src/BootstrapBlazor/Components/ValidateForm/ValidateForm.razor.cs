20fa20c273dce6fe98c98c2994c06a25755f77a6
Argo Zhang <argo@live.ca>
2025-12-26 13:58:43 +0800

    fix: 修复空集合不报错问题

--- src/BootstrapBlazor/Components/ValidateForm/ValidateForm.razor.cs
+++ src/BootstrapBlazor/Components/ValidateForm/ValidateForm.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -508,6 +508,7 @@ public partial class ValidateForm
             else
             {
                 // 未选择文件
+                propertyValue = null;
                 ValidateDataAnnotations(propertyValue, context, messages, pi);
             }
 
