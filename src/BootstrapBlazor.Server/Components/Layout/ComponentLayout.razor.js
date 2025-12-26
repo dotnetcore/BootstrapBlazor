89af2540b99e320bee6dc5c411cf9735d72e504b
Argo Zhang <argo@live.ca>
2025-12-26 10:31:34 +0800

    doc: 更新锚点滚动逻辑

--- src/BootstrapBlazor.Server/Components/Layout/ComponentLayout.razor.js
+++ src/BootstrapBlazor.Server/Components/Layout/ComponentLayout.razor.js
@@ -1,12 +1,12 @@
-﻿export function scrollToAnchor() {
+export function scrollToAnchor() {
     const hash = decodeURI(location.hash)
     if (hash) {
         const anchor = hash.split('-')[0]
         const el = document.querySelector(anchor)
         if (el) {
             const handler = setTimeout(() => {
-                el.scrollIntoView({ behavior: 'smooth', block: 'start' })
                 clearTimeout(handler)
+                el.scrollIntoView({ behavior: 'smooth', block: 'start' })
             }, 1000)
         }
     }
