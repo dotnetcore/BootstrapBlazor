8c329b92378735de901f7d96e93bac6341da1c5a
Argo Zhang <argo@live.ca>
2025-12-26 11:17:13 +0800

    doc(Table): update ShowMoreButton documentation (#7427)
    * doc: 移动 ShowMoreButton 示例
    
    * doc: 更新 ShowMoreButton 示例
    
    * doc: 更新更多按钮模板文档

--- src/BootstrapBlazor.Server/Components/Samples/Table/TablesSearch.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Table/TablesSearch.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -183,9 +183,4 @@ public partial class TablesSearch
             IsAdvanceSearch = options.CustomerSearches.Count > 0 && string.IsNullOrEmpty(options.SearchText),
         });
     }
-
-    [Inject, NotNull]
-    private ToastService? ToastService { get; set; }
-
-    private Task OnAction(Foo foo, string actionName) => ToastService.Information(foo.Name, $"Trigger {actionName}");
 }
