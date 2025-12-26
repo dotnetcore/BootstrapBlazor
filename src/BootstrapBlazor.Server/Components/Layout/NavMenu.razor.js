925a92b97bec26a968912ad24a51303f294cac3b
Argo Zhang <argo@live.ca>
2025-12-26 10:32:01 +0800

    refactor: 重构代码

--- src/BootstrapBlazor.Server/Components/Layout/NavMenu.razor.js
+++ src/BootstrapBlazor.Server/Components/Layout/NavMenu.razor.js
@@ -1,21 +1,19 @@
-﻿import Data from "../../_content/BootstrapBlazor/modules/data.js"
+import Data from "../../_content/BootstrapBlazor/modules/data.js"
 import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
 
 export function init(id) {
-    const navmenu = {
-        navbar: document.querySelector('.navbar-toggler'),
-        menu: document.querySelector('.sidebar-content')
-    }
-    Data.set(id, navmenu)
+    const navbar = document.querySelector('.navbar-toggler');
+    const menu = document.querySelector('.sidebar-content');
+    Data.set(id, { navbar, menu });
 
-    EventHandler.on(navmenu.navbar, 'click', () => {
-        navmenu.menu.classList.toggle('show')
+    EventHandler.on(navbar, 'click', () => {
+        menu.classList.toggle('show')
     })
-    EventHandler.on(navmenu.menu, 'click', '.nav-link', e => {
+    EventHandler.on(menu, 'click', '.nav-link', e => {
         const link = e.delegateTarget
         const url = link.getAttribute('href');
         if (url !== '#') {
-            navmenu.menu.classList.remove('show')
+            menu.classList.remove('show')
         }
     })
 }
@@ -24,6 +22,7 @@ export function dispose(id) {
     const data = Data.get(id);
     Data.remove(id);
 
-    EventHandler.off(data.navbar, 'click');
-    EventHandler.off(data.menu, 'click', '.nav-link');
+    const { navbar, menu } = data;
+    EventHandler.off(navbar, 'click');
+    EventHandler.off(menu, 'click', '.nav-link');
 }
