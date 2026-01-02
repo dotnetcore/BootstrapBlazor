d1601d6649e37793c1338c55c920ffdd9226f371
Argo Zhang <argo@live.ca>
2026-01-02 16:35:24 +0800

    doc(Component): update annotation text of components (#7465)
    * doc: 更改 StopPropagation 参数说明
    
    * doc: 更改 SlideButton Placement 参数说明文档
    
    * doc: 更新类名注释

--- src/BootstrapBlazor/Components/Button/SlideButton.razor.cs
+++ src/BootstrapBlazor/Components/Button/SlideButton.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -55,7 +55,7 @@ public partial class SlideButton
     public Color Color { get; set; } = Color.Primary;
 
     /// <summary>
-    /// 获得/设置 按钮颜色
+    /// 获得/设置 展开项显示位置
     /// </summary>
     [Parameter]
     public Placement Placement { get; set; }
