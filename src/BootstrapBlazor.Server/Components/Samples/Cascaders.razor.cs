57ea894bc9c3a40bc302e30c50bcd5cf45da4866
Argo Zhang <argo@live.ca>
2026-01-04 11:21:03 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/Cascaders.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Cascaders.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -88,9 +88,9 @@ public sealed partial class Cascaders
         }
     ];
 
-    private void OnValidate()
+    private async Task OnValidate()
     {
-        ValidateForm1.Validate();
+        await ValidateForm1.ValidateAsync();
     }
 
     /// <summary>
