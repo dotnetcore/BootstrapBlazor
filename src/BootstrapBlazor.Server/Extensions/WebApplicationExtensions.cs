57809c6f4bac13e9455ec669a3435fb2cd3c5814
Argo Zhang <argo@live.ca>
2026-01-05 19:40:05 +0800

    chore(LLms): add llms.txt router (#7471)

--- src/BootstrapBlazor.Server/Extensions/WebApplicationExtensions.cs
+++ src/BootstrapBlazor.Server/Extensions/WebApplicationExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -18,6 +18,41 @@ static class WebApplicationExtensions
         {
             FileProvider = new PhysicalFileProvider(uploader),
             RequestPath = "/images/uploader"
+        });
+    }
+
+    public static void UseLLMsStaticFiles(this WebApplication app)
+    {
+        var llms = Path.Combine(app.Environment.WebRootPath, "llms");
+        Directory.CreateDirectory(llms);
+
+        app.UseStaticFiles(new StaticFileOptions
+        {
+            FileProvider = new PhysicalFileProvider(llms),
+            RequestPath = "/llms",
+            OnPrepareResponse = context =>
+            {
+                context.Context.Response.ContentType = "text/plain; charset=utf-8";
+            }
+        });
+
+        app.MapGet("/llms.txt", async context =>
+        {
+            context.Response.Headers.ContentType = "text/plain; charset=utf-8";
+            var filePath = Path.Combine(app.Environment.WebRootPath, "llms", "llms.txt");
+            if (File.Exists(filePath))
+            {
+                var fileInfo = new FileInfo(filePath);
+                context.Response.ContentLength = fileInfo.Length;
+
+                using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read, bufferSize: 4096, useAsync: true);
+                await fileStream.CopyToAsync(context.Response.Body);
+                return;
+            }
+
+            var defaultContent = "no llms.txt";
+            context.Response.ContentLength = defaultContent.Length;
+            await context.Response.WriteAsync(defaultContent);
         });
     }
 }
--- src/BootstrapBlazor.Server/Program.cs
+++ src/BootstrapBlazor.Server/Program.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -37,6 +37,9 @@ if (!app.Environment.IsDevelopment())
 
 // 增加上传目录静态资源文件
 app.UseUploaderStaticFiles();
+
+// 增加 llms 静态资源文件
+app.UseLLMsStaticFiles();
 
 app.UseAntiforgery();
 app.UseBootstrapBlazor();
