2f0d52205bc5e62276749a364e4de4db27c20cef
Argo Zhang <argo@live.ca>
2026-01-10 11:09:56 +0800

    refactor: 撤销对 BaseLayout 的更改

--- src/BootstrapBlazor.Server/Components/Layout/BaseLayout.razor.js
+++ src/BootstrapBlazor.Server/Components/Layout/BaseLayout.razor.js
@@ -1,44 +1,6 @@
 import { getTheme, setTheme } from "../../_content/BootstrapBlazor/modules/utility.js"
-import EventHandler from "../../_content/BootstrapBlazor/modules/event-handler.js"
 
-function initTheme() {
+export function initTheme() {
     const currentTheme = getTheme();
     setTheme(currentTheme, false);
-}
-
-export function doTask(invoke, debug) {
-    initTheme();
-
-    const v = localStorage.getItem('bb-gitee-vote');
-    if (v) {
-        try {
-            const differ = new Date().getTime() - v;
-            if (differ < 86400000) {
-                return;
-            }
-        }
-        catch {
-            localStorage.removeItem('bb-gitee-vote');
-        }
-    }
-
-    if (debug !== true) {
-        const handler = setTimeout(async () => {
-            clearTimeout(handler);
-            await invoke.invokeMethodAsync("ShowVoteToast");
-        }, 10000);
-    }
-
-    EventHandler.on(document, 'click', '#bb-gitee-vote', e => {
-        const toast = e.delegateTarget.closest('.toast');
-        if (toast) {
-            toast.classList.remove('show');
-
-            localStorage.setItem('bb-gitee-vote', new Date().getTime());
-        }
-    });
-}
-
-export function dispose() {
-    EventHandler.off(document, 'click', '#bb-gitee-vote');
 }
