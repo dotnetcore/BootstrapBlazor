c202c42ec810b56c7be49162e6b96040d76bb314
Argo Zhang <argo@live.ca>
2026-01-31 09:56:46 +0800

    doc: 更新示例

--- src/BootstrapBlazor.Server/Components/Samples/EditorForms.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/EditorForms.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -24,6 +24,21 @@ public sealed partial class EditorForms
     [Inject]
     [NotNull]
     private IStringLocalizer<Foo>? FooLocalizer { get; set; }
+
+    private List<string> _ignoreItems = [];
+
+    private Task OnSwitchIgnoreItems()
+    {
+        if (_ignoreItems.Count != 0)
+        {
+            _ignoreItems.Clear();
+        }
+        else
+        {
+            _ignoreItems = [nameof(Foo.Hobby)];
+        }
+        return Task.CompletedTask;
+    }
 
     private List<SelectedItem> DummyItems { get; } =
     [
