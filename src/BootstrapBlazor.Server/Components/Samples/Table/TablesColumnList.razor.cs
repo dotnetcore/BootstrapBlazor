f2b942767aed22a08e94ed40e5bb571b9bb272df
Argo Zhang <argo@live.ca>
2026-02-13 09:23:14 +0800

    refactor: 重构代码

--- src/BootstrapBlazor.Server/Components/Samples/Table/TablesColumnList.razor.cs
+++ src/BootstrapBlazor.Server/Components/Samples/Table/TablesColumnList.razor.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the Apache 2.0 License
 // See the LICENSE file in the project root for more information.
 // Maintainer: Argo Zhang(argo@live.ca) Website: https://www.blazor.zone
@@ -54,7 +54,7 @@ public partial class TablesColumnList
         IEnumerable<Foo> items = Items;
         // 过滤
         var isFiltered = false;
-        if (options.Filters.Any())
+        if (options.Filters.Count > 0)
         {
             items = items.Where(options.Filters.GetFilterFunc<Foo>());
             isFiltered = true;
