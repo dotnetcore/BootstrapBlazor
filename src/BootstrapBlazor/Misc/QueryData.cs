2ad75ae50ce23a8cc0e43caa98f79ca01da67526
Argo Zhang <argo@live.ca>
2026-04-11 10:34:45 +0800

    doc(QueryData): update IsFilter/IsSearch/IsSort comments

--- src/BootstrapBlazor/Misc/QueryData.cs
+++ src/BootstrapBlazor/Misc/QueryData.cs
@@ -25,29 +25,30 @@ public class QueryData<TItem>
     public int TotalCount { get; set; }
 
     /// <summary>
-    /// <para lang="zh">获得/设置 数据是否被过滤 默认为 false 未被过滤</para>
-    /// <para lang="en">Gets or sets whether data is filtered default false</para>
+    /// <para lang="zh">获得/设置 数据是否被过滤 默认为 false 未被过滤，设置为 true 表示数据已过滤组件内部不做任何处理，设置为 false 表示数据未过滤组件内部将进行过滤操作</para>
+    /// <para lang="en">Gets or sets whether data is filtered. Default is false. Setting to true indicates data is already filtered and the component will not perform any filtering. Setting to false indicates data is not filtered and the component will perform filtering.</para>
     /// </summary>
-    /// <remarks>组件内部通过此属性判断，如果外部未进行数据过滤，内部将进行数据过滤操作</remarks>
     public bool IsFiltered { get; set; }
 
     /// <summary>
-    /// <para lang="zh">获得/设置 数据是否被排序 默认为 false 未被排序</para>
-    /// <para lang="en">Gets or sets whether data is sorted default false</para>
+    /// <para lang="zh">获得/设置 数据是否被排序 默认为 false 未被排序，设置为 true 表示数据已排序组件内部不做任何处理，设置为 false 表示数据未排序组件内部将进行排序操作</para>
+    /// <para lang="en">Gets or sets whether data is sorted. Default is false. Setting to true indicates data is already sorted and the component will not perform any sorting. Setting to false indicates data is not sorted and the component will perform sorting.</para>
     /// </summary>
     public bool IsSorted { get; set; }
 
     /// <summary>
-    /// <para lang="zh">获得/设置 数据是否已处理 SearchText <see cref="Table{TItem}.SearchText"/> 默认为 false</para>
-    /// <para lang="en">Gets or sets whether data has processed SearchText <see cref="Table{TItem}.SearchText"/> default false</para>
+    /// <para lang="zh">获得/设置 数据是否被过滤 默认为 false 未被过滤，设置为 true 表示数据已过滤组件内部不做任何处理，设置为 false 表示数据未过滤组件内部将进行过滤操作</para>
+    /// <para lang="en">Gets or sets whether data is filtered. Default is false. Setting to true indicates data is already filtered and the component will not perform any filtering. Setting to false indicates data is not filtered and the component will perform filtering.</para>
     /// </summary>
-    /// <remarks>设置本属性为 true 时，Table 组件的高级搜索按钮改变颜色</remarks>
     public bool IsSearch { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 数据是否为已处理自定义高级搜索 <see cref="Table{TItem}.CustomerSearchTemplate"/> 默认为 false</para>
     /// <para lang="en">Gets or sets whether data has processed custom advanced search <see cref="Table{TItem}.CustomerSearchTemplate"/> default false</para>
     /// </summary>
-    /// <remarks>设置本属性为 true 时，Table 组件的高级搜索按钮改变颜色</remarks>
+    /// <remarks>
+    ///  <para lang="zh">设置本属性为 true 时，Table 组件的高级搜索按钮改变颜色</para>
+    ///  <para lang="en">When this property is set to true, the advanced search button of the Table component changes color</para>
+    ///</remarks>
     public bool IsAdvanceSearch { get; set; }
 }
