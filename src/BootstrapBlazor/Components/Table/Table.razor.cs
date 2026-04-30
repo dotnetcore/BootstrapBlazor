be1b46b91328f311275ece5c6257899a047de9fa
Argo Zhang <argo@live.ca>
2026-04-30 09:43:17 +0800

    feat(ChangeDetectionCleanTask): rename Rent/Release method (#7932)

--- src/BootstrapBlazor/Components/Table/Table.razor.cs
+++ src/BootstrapBlazor/Components/Table/Table.razor.cs
@@ -1139,7 +1139,7 @@ public partial class Table<TItem> : ITable, IModelEqualityComparer<TItem> where 
         // 如果 TItem 是动态数据类型添加到自动清理任务中
         if (IsDataTableDynamicContext)
         {
-            ChangeDetectionCleanTask.Rent(this);
+            ChangeDetectionCleanTask.Register(this);
         }
 
         if (!_firstRender)
@@ -2056,7 +2056,7 @@ public partial class Table<TItem> : ITable, IModelEqualityComparer<TItem> where 
         {
             if (IsDataTableDynamicContext)
             {
-                ChangeDetectionCleanTask.Release(this);
+                ChangeDetectionCleanTask.UnRegister(this);
             }
 
             AutoRefreshCancelTokenSource?.Cancel();
