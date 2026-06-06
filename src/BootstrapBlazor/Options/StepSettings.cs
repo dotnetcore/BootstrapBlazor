d246fb7e62a6569ba13c031040c58ac5aa867321
Tony-ST0754 <6914529@qq.com>
2026-06-06 20:42:27 +0800

    feat(BootstrapInputNumber): improve step functionality (#8085)
    * feat(BootstrapInputNumber): 组件或配置中设置了Step的HTML标准属性导致表单提交时引导HTML的标准校验
    
    fix:当组件中启用HTML标准属性Step后，在表单提交时会引导HTML的标准校验，从而导致表单的提交按钮会被禁用
    
    refactor：修改为在组件或配置中当Step值不为any时，组件将自动将输入的值处理为Step的精度
    
    chore:补充单元测试
    
    * feat(BootstrapInputNumber): 组件或配置中设置了Step的HTML标准属性导致表单提交时引导HTML的标准校验
    
    refactor:单元测试修改补充
    
    * feat(BootstrapInputNumber): 组件或配置中设置了Step的HTML标准属性导致表单提交时引导HTML的标准校验
    
    refactor:补单元测试，当Step值为any时的情况
    
    * 更新 InputNumberTest.cs
    
    refactor:继续补充单元测试，Step，以便达到100%覆盖率
    
    * refactor: 移除 ParseDecimal 方法使用原生方法
    
    * refactor: 移除 static 关键字
    
    * refactor: 重构代码
    
    * refactor: 重构 Index
    
    * test: 消除警告信息
    
    * test: 消除警告信息
    
    * refactor: 重构代码提高可读性
    
    * test: 更新单元测试
    
    * docs：更新InputNumber组件文档
    
    更新组件文档，主要注意HTML标准中input标签中step、min、max同时存在时可能带来意想不到的结果，该问题由HTML标准引起，无法修复
    
    * revert: 撤销四舍五入内置逻辑
    
    * test: 撤销单元测试
    
    * revert: 撤销 IsDecimalType 判断方法
    
    * Revert "docs：更新InputNumber组件文档"
    
    This reverts commit 6be2fb24c57e5ea1fe66c2b504e46611ccb0ad7d.
    
    * test: 补全 StepSettings 设置
    
    * test: 更新单元测试
    
    * feat: 增加 Step 默认值规则允许小数点的使用 any 其他使用 null
    
    * refactor: 重构 GetStepString 方法支持 细化 any 逻辑
    
    * test: 更新单元测试
    
    * refactor: 更新逻辑
    
    * test: 更新单元测试
    
    * doc: 调整顺序
    
    * refactor: 调整数据类型接收 any 设置
    
    * test: 更新单元测试
    
    * chore: bump version 10.7.1
    
    * doc: 增加 Step 文档
    
    * doc: 支持多语言
    
    ---------
    
    Co-authored-by: Argo Zhang <argo@live.ca>

--- src/BootstrapBlazor/Options/StepSettings.cs
+++ src/BootstrapBlazor/Options/StepSettings.cs
@@ -12,40 +12,70 @@ namespace BootstrapBlazor.Components;
 public class StepSettings
 {
     /// <summary>
+    /// <para lang="zh">获得/设置 sbyte 数据类型步长 默认 null 未设置</para>
+    /// <para lang="en">Gets or sets sbyte type step default null</para>
+    /// </summary>
+    public string? SByte { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">获得/设置 byte 数据类型步长 默认 null 未设置</para>
+    /// <para lang="en">Gets or sets byte type step default null</para>
+    /// </summary>
+    public string? Byte { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">获得/设置 uint 数据类型步长 默认 null 未设置</para>
+    /// <para lang="en">Gets or sets uint type step default null</para>
+    /// </summary>
+    public string? UInt { get; set; }
+
+    /// <summary>
     /// <para lang="zh">获得/设置 int 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets int type step default null</para>
     /// </summary>
-    public int? Int { get; set; }
+    public string? Int { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 long 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets long type step default null</para>
     /// </summary>
-    public int? Long { get; set; }
+    public string? Long { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">获得/设置 ulong 数据类型步长 默认 null 未设置</para>
+    /// <para lang="en">Gets or sets ulong type step default null</para>
+    /// </summary>
+    public string? ULong { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 short 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets short type step default null</para>
     /// </summary>
-    public int? Short { get; set; }
+    public string? Short { get; set; }
+
+    /// <summary>
+    /// <para lang="zh">获得/设置 ushort 数据类型步长 默认 null 未设置</para>
+    /// <para lang="en">Gets or sets ushort type step default null</para>
+    /// </summary>
+    public string? UShort { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 float 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets float type step default null</para>
     /// </summary>
-    public float? Float { get; set; }
+    public string? Float { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 double 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets double type step default null</para>
     /// </summary>
-    public double? Double { get; set; }
+    public string? Double { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得/设置 decimal 数据类型步长 默认 null 未设置</para>
     /// <para lang="en">Gets or sets decimal type step default null</para>
     /// </summary>
-    public decimal? Decimal { get; set; }
+    public string? Decimal { get; set; }
 
     /// <summary>
     /// <para lang="zh">获得步长字符串</para>
@@ -55,29 +85,49 @@ public class StepSettings
     public string? GetStep(Type type)
     {
         string? ret = null;
-        if (type == typeof(int))
+        if (type == typeof(sbyte))
         {
-            ret = Int?.ToString();
+            ret = SByte;
         }
-        if (type == typeof(long))
+        else if (type == typeof(byte))
         {
-            ret = Long?.ToString();
+            ret = Byte;
         }
-        if (type == typeof(short))
+        else if (type == typeof(uint))
         {
-            ret = Short?.ToString();
+            ret = UInt;
         }
-        if (type == typeof(float))
+        else if (type == typeof(int))
         {
-            ret = Float?.ToString();
+            ret = Int;
         }
-        if (type == typeof(double))
+        else if (type == typeof(long))
         {
-            ret = Double?.ToString();
+            ret = Long;
         }
-        if (type == typeof(decimal))
+        else if (type == typeof(short))
         {
-            ret = Decimal?.ToString();
+            ret = Short;
+        }
+        else if (type == typeof(ushort))
+        {
+            ret = UShort;
+        }
+        else if (type == typeof(ulong))
+        {
+            ret = ULong;
+        }
+        else if (type == typeof(float))
+        {
+            ret = Float;
+        }
+        else if (type == typeof(double))
+        {
+            ret = Double;
+        }
+        else if (type == typeof(decimal))
+        {
+            ret = Decimal;
         }
         return ret;
     }
--- test/UnitTest/Options/BootstrapBlazorOptionsTest.cs
+++ test/UnitTest/Options/BootstrapBlazorOptionsTest.cs
@@ -60,27 +60,50 @@ public class BootstrapBlazorOptionsTest
 
         options.StepSettings = new();
 
+        Assert.Null(options.GetStep<sbyte?>());
+        Assert.Null(options.GetStep<byte?>());
         Assert.Null(options.GetStep<short?>());
+        Assert.Null(options.GetStep<ushort?>());
         Assert.Null(options.GetStep<int?>());
+        Assert.Null(options.GetStep<uint?>());
         Assert.Null(options.GetStep<long?>());
+        Assert.Null(options.GetStep<ulong?>());
         Assert.Null(options.GetStep<float?>());
         Assert.Null(options.GetStep<double?>());
         Assert.Null(options.GetStep<decimal?>());
 
-        options.StepSettings.Short = 1;
-        options.StepSettings.Int = 2;
-        options.StepSettings.Long = 3;
-        options.StepSettings.Float = 0.1f;
-        options.StepSettings.Double = 0.01d;
-        options.StepSettings.Decimal = 0.001M;
+        options.StepSettings.SByte = "1";
+        options.StepSettings.Byte = "2";
+        options.StepSettings.Short = "1";
+        options.StepSettings.UShort = "2";
+        options.StepSettings.Int = "2";
+        options.StepSettings.UInt = "3";
+        options.StepSettings.Long = "3";
+        options.StepSettings.ULong = "4";
+        options.StepSettings.Float = "0.1";
+        options.StepSettings.Double = "0.01";
+        options.StepSettings.Decimal = "0.001";
 
+        Assert.Equal("1", options.GetStep<sbyte?>());
+        Assert.Equal("2", options.GetStep<byte?>());
         Assert.Equal("1", options.GetStep<short?>());
+        Assert.Equal("2", options.GetStep<ushort?>());
         Assert.Equal("2", options.GetStep<int?>());
+        Assert.Equal("3", options.GetStep<uint?>());
         Assert.Equal("3", options.GetStep(typeof(long?)));
+        Assert.Equal("4", options.GetStep<ulong?>());
 
         Assert.Equal("0.1", options.GetStep<float?>());
         Assert.Equal("0.01", options.GetStep<double?>());
         Assert.Equal("0.001", options.GetStep<decimal?>());
+
+        options.StepSettings.Float = "any";
+        options.StepSettings.Double = "any";
+        options.StepSettings.Decimal = "any";
+
+        Assert.Equal("any", options.GetStep<float?>());
+        Assert.Equal("any", options.GetStep<double?>());
+        Assert.Equal("any", options.GetStep<decimal?>());
     }
 
     [Fact]
