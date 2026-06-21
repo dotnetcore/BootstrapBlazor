3aeb67c81a2e6829c1d0a54498ddcb237ea70fa4
Argo Zhang <argo@live.ca>
2026-06-21 15:05:55 +0800

    doc(NavMenu): update component category (#8138)
    * doc: 调整键值与组件名称一致
    
    * refactor: 更改图标键值
    
    * doc: 更新标签键值

--- src/BootstrapBlazor.Server/Extensions/MenusLocalizerExtensions.cs
+++ src/BootstrapBlazor.Server/Extensions/MenusLocalizerExtensions.cs
@@ -321,7 +321,7 @@ internal static class MenusLocalizerExtensions
                 },
                 new()
                 {
-                    Text = Localizer["Labels"],
+                    Text = Localizer["BootstrapLabel"],
                     Url = "label"
                 },
                 new()
@@ -437,22 +437,22 @@ internal static class MenusLocalizerExtensions
                 new()
                 {
                     Match = NavLinkMatch.All,
-                    Text = Localizer["Input"],
+                    Text = Localizer["BootstrapInput"],
                     Url = "input"
                 },
                 new()
                 {
-                    Text = Localizer["InputNumber"],
+                    Text = Localizer["BootstrapInputNumber"],
                     Url = "input-number"
                 },
                 new()
                 {
-                    Text = Localizer["InputGroup"],
+                    Text = Localizer["BootstrapInputGroup"],
                     Url = "input-group"
                 },
                 new()
                 {
-                    Text = Localizer["Ip"],
+                    Text = Localizer["IpAddress"],
                     Url = "ip"
                 },
                 new()
@@ -751,7 +751,7 @@ internal static class MenusLocalizerExtensions
                 },
                 new()
                 {
-                    Text = Localizer["Icon"],
+                    Text = Localizer["BootstrapBlazorIcon"],
                     Url = "icon"
                 },
                 new()
@@ -1265,7 +1265,7 @@ internal static class MenusLocalizerExtensions
                 },
                 new()
                 {
-                    Text = Localizer["Console"],
+                    Text = Localizer["ConsoleLogger"],
                     Url = "console"
                 },
                 new()
