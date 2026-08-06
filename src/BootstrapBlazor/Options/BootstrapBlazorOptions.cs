dd921df107ed9d5030277d0a314436abb1e19071
Argo Zhang <argo@live.ca>
2026-08-06 15:16:20 +0800

    doc: 增加版本信息

--- src/BootstrapBlazor/Options/BootstrapBlazorOptions.cs
+++ src/BootstrapBlazor/Options/BootstrapBlazorOptions.cs
@@ -193,6 +193,7 @@ public class BootstrapBlazorOptions : IOptions<BootstrapBlazorOptions>
     /// <summary>
     /// <para lang="zh">获得/设置 <see cref="DateTimeSettings"/> 配置实例</para>
     /// <para lang="en">Gets or sets the <see cref="DateTimeSettings"/> configuration instance</para>
+    /// <para>v<version>10.9.1</version></para>
     /// </summary>
     public DateTimeSettings DateTimeSettings { get; set; } = new();
 
