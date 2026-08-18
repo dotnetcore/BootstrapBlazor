084780ef038fee6d881e01912729f2908705942d
Argo Zhang <argo@live.ca>
2026-08-18 10:41:47 +0800

    revert: 撤销更改

--- src/BootstrapBlazor/wwwroot/modules/validate.js
+++ src/BootstrapBlazor/wwwroot/modules/validate.js
@@ -2,8 +2,7 @@ export function execute(id, title) {
     const el = document.getElementById(id);
 
     if (el) {
-        const tip = bootstrap.Tooltip.getOrCreateInstance(el, { customClass: 'is-invalid', title })
-        tooltipCache.set(id, tip); // 缓存实例
+        const tip = bootstrap.Tooltip.getOrCreateInstance(el, { customClass: 'is-invalid', title });
         if (!tip._isShown()) {
             if (title !== tip._config.title) {
                 tip._config.title = title;
@@ -28,8 +27,6 @@ const showResult = el => {
     return ret;
 }
 
-const tooltipCache = new Map(); // 全局缓存池，用来处理上传时增加按钮未渲染，导致提示不能正确删除
-
 export function dispose(id) {
     const el = document.getElementById(id)
 
@@ -37,14 +34,6 @@ export function dispose(id) {
         const tip = bootstrap.Tooltip.getInstance(el)
         if (tip) {
             tip.dispose()
-            tooltipCache.delete(id); // 清理缓存
-        }
-    }
-    else {
-        const cachedTip = tooltipCache.get(id);
-        if (cachedTip) {
-            cachedTip.dispose();
-            tooltipCache.delete(id);
         }
     }
 }
